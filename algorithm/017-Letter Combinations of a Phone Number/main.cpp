cb91e8b4d5ab464a2e9f635586fa050917f7a834
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-06 23:00:19 +0800

    iterative and recursive of 017

--- algorithm/017-Letter Combinations of a Phone Number/main.cpp
+++ algorithm/017-Letter Combinations of a Phone Number/main.cpp
@@ -1,60 +1,87 @@
 class Solution {
 public:
-    const string getLetters(char d) {
-        switch (d) {
-        case '2':
-            return "abc";
-        case '3':
-            return "def";
-        case '4':
-            return "ghi";
-        case '5':
-            return "jkl";
-        case '6':
-            return "mno";
-        case '7':
-            return "pqrs";
-        case '8':
-            return "tuv";
-        case '9':
-            return "wxyz";
-        default:
-            break;
-        }
+    static vector<string> s_lettersList;
 
-        return "";
+    static const string& getLetters(char c) {
+        return s_lettersList[c - '0'];
     }
 
-    void letterCombinationsHelper(const string& digits, string& result, vector<string>& results) {
+    void letterCombinationsRecv(const string& digits, string& result, vector<string>& results) {
         if (result.size() == digits.size()) {
             results.push_back(result);
         }
         else {
-            string letters = getLetters(digits.at(result.size()));
-            for (size_t j = 0; j < letters.size(); ++j) {
-                result.push_back(letters.at(j));
-                letterCombinationsHelper(digits, result, results);
+            const string& letters = getLetters(digits[result.size()]);
+            for (size_t i = 0; i < letters.size(); ++i) {
+                result.push_back(letters[i]);
+                letterCombinationsRecv(digits, result, results);
                 result.pop_back();
             }
         }
     }
 
-    vector<string> letterCombinations(string digits) {
+    vector<string> letterCombinationsRecv(const string& digits) {
         if (digits.empty()) {
             return {};
         }
-        
+
+        string result;
+
         vector<string> results; {
-            size_t s = 0;
+            size_t num = 1;
             for (size_t i = 0; i < digits.size(); ++i) {
-                s += getLetters(digits.at(i)).size();
+                num *= getLetters(digits[i]).size();
             }
-            results.reserve(s);
+            results.reserve(num);
+        }
+
+        letterCombinationsRecv(digits, result, results);
+        return results;
+    }
+
+    vector<string> letterCombinationsIter(const string& digits) {
+        if (digits.empty()) {
+            return{};
         }
 
         string result;
-        letterCombinationsHelper(digits, result, results);
+        vector<string> results;
+        size_t num = 1;
+        for (size_t i = 0; i < digits.size(); ++i) {
+            const string& letters = getLetters(digits[i]);
+            num *= letters.size();
+            result.push_back(letters.front());
+        }
+        results.reserve(num);
+        results.push_back(result);
+
+        while (true) {
+            size_t index = digits.size();
+            for (; index > 0; --index) {
+                const string& letters = getLetters(digits[index - 1]);
+                if (result[index - 1] == letters.back()) {
+                    result[index - 1] = letters.front();
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (index == 0) {
+                break;
+            }
+
+            const string& letters = getLetters(digits[index - 1]);
+            result[index - 1] = letters[letters.find(result[index - 1]) + 1];
+            results.push_back(result);
+        }
 
         return results;
     }
+
+    vector<string> letterCombinations(string digits) {
+        return letterCombinationsRecv(digits);
+    }
 };
+
+vector<string> Solution::s_lettersList = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
