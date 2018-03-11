2819d15c702c8e3022e8cd63ed43996c648b0d93
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-11 23:38:53 +0800

    Initial solution of 096

---
+++ algorithm/096-Unique Binary Search Trees/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> nums = {1};
+        for (int i = 1; i <= n; ++i) {
+            int num = 0;
+            for (int j = 0; j < i; ++j) {
+                num += (nums[j] * nums[i - j - 1]);
+            }
+            nums.push_back(num);
+        }
+
+        return nums.back();
+    }
+};
