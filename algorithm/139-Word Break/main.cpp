8d880267ffed226ba01d01dcd141869cf2f8cae8
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-25 12:13:43 +0800

    updated solution of 139

--- algorithm/139-Word Break/main.cpp
+++ algorithm/139-Word Break/main.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    bool wordBreak(const string& s, size_t index, vector<bool>& cacheFails, const vector<string>& wordDict) {
+    bool wordBreakMemoDfs(const string& s, size_t index, vector<bool>& cacheFails, const vector<string>& wordDict) {
         size_t len = s.size();
         if (index == len) {
             return true;
@@ -17,7 +17,7 @@ public:
             size_t wordLen = word.size();
             if (leftLen >= wordLen) {
                 if (strncmp(s.c_str() + index, word.c_str(), wordLen) == 0) {
-                    if (wordBreak(s, index + wordLen, cacheFails, wordDict)) {
+                    if (wordBreakMemoDfs(s, index + wordLen, cacheFails, wordDict)) {
                         return true;
                     }
                 }
@@ -28,8 +28,94 @@ public:
         return false;
     }
 
-    bool wordBreak(string s, vector<string>& wordDict) {
+    bool wordBreakMemoDfs(const string& s, const vector<string>& wordDict) {
         vector<bool> cacheFails(s.size(), false);
-        return wordBreak(s, 0, cacheFails, wordDict);
+        return wordBreakMemoDfs(s, 0, cacheFails, wordDict);
     }
+
+
+    struct TrieNode {
+        TrieNode()
+            : childs()
+            , end(false) {
+        }
+        map<char, TrieNode*> childs;
+        bool end;
+    };
+
+    TrieNode* buildTrie(const vector<string>& words) {
+        TrieNode* root = new TrieNode();
+        for (size_t j = 0; j < words.size(); ++j) {
+            TrieNode* node = root;
+            const string& word = words[j];
+            for (size_t i = 0; i < word.size(); ++i) {
+                TrieNode* child = NULL; {
+                    char c = word[i];
+                    pair<map<char, TrieNode*>::iterator, bool> p = node->childs.emplace(c, nullptr);
+                    if (p.second) {
+                        child = new TrieNode();
+                        p.first->second = child;
+                    }
+                    else {
+                        child = p.first->second;
+                    }
+
+                }
+                node = child;
+            }
+            node->end = true;
+        }
+
+        return root;
+    }
+
+    bool wordBreakTrieMemoDfs(const string& s, size_t index, const TrieNode* root, vector<bool>& dones, vector<bool>& breakables) {
+        size_t len = s.size();
+        assert(index < len);
+
+        if (dones[index]) {
+            return breakables[index];
+        }
+
+        bool breakable = false;
+        const TrieNode* node = root;
+        for (size_t i = index; i < len && !breakable; ++i) {
+            map<char, TrieNode*>::const_iterator itr = node->childs.find(s[i]);
+            if (itr == node->childs.end()) {
+                break;
+            }
+
+            node = itr->second;
+            if (node->end) {
+                if (i + 1 < len) {
+                    breakable = wordBreakTrieMemoDfs(s, i + 1, root, dones, breakables);
+                }
+                else {
+                    breakable = true;
+                }
+            }
+        }
+
+        dones[index] = true;
+        breakables[index] = breakable;
+
+        return breakable;
+    }
+
+    bool wordBreakTrieMemoDfs(const string& s, const vector<string>& wordDict) {
+        if (s.empty()) {
+            return true;
+        }
+
+        const TrieNode* root = buildTrie(wordDict);
+        vector<bool> dones(s.size(), false);
+        vector<bool> breakables(s.size(), false);
+        return wordBreakTrieMemoDfs(s, 0, root, dones, breakables);
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        //return wordBreakMemoDfs(s, wordDict);
+        return wordBreakTrieMemoDfs(s, wordDict);
+    }
+
 };
