dce255e46d62800334bdb5e709122188613db0a4
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-25 12:13:36 +0800

    updated solution of 069

--- algorithm/069-Sqrt(x)/main.cpp
+++ algorithm/069-Sqrt(x)/main.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long root = x;
+        // Newton's method
+        long int root = x;
         while (root * root > x) {
             root = (root + x / root) / 2;
         }
-    
+
         return root;
     }
 };
