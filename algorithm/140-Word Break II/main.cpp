08cdad266a2cc6f8955eb1944ac1b5ea778b6ee5
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-25 12:13:53 +0800

    updated solution of 140

--- algorithm/140-Word Break II/main.cpp
+++ algorithm/140-Word Break II/main.cpp
@@ -7,6 +7,7 @@ public:
         }
         map<char, TrieNode*> childs;
         bool end;
+        size_t index;
     };
 
     TrieNode* buildTrie(const vector<string>& words) {
@@ -29,60 +30,73 @@ public:
                 }
                 node = child;
             }
-            node->end = true;
+            if (node->end == false) {
+                node->end = true;
+                node->index = j;
+            }
         }
 
         return root;
     }
 
-    bool wordBreak(string s, size_t index, vector<bool>& cacheFails, TrieNode* root, string& sentence, vector<string>& sentences) {
-        if (index == s.size()) {
-            sentences.emplace_back(sentence);
-            return true;
+    const vector<vector<size_t>>& wordBreak(const string& s, size_t index, const TrieNode* root, vector<bool>& dones, vector<vector<vector<size_t>>>& results) {
+        size_t len = s.size();
+        assert(index < len);
+
+        if (dones[index]) {
+            return results[index];
         }
 
-        if (cacheFails[index]) {
-            return false;
-        }
-
-        bool success = false;
-        string word;
-        TrieNode* node = root;
-        for (size_t i = index; i < s.size(); ++i) {
-            char c = s[i];
-            if (node->childs[c]) {
-                word.push_back(c);
-                node = node->childs[c];
-                if (node->end) {
-                    size_t sentenceLen = sentence.size();
-                    if (sentenceLen != 0) {
-                        sentence.push_back(' ');
-                    }
-                    sentence.append(word);
-                    if (wordBreak(s, i + 1, cacheFails, root, sentence, sentences)) {
-                        success = true;
-                    }
-                    sentence.resize(sentenceLen);
-                }
-            }
-            else {
+        const TrieNode* node = root;
+        vector<vector<size_t>>& result = results[index];
+        for (size_t i = index; i < len; ++i) {
+            map<char, TrieNode*>::const_iterator itr = node->childs.find(s[i]);
+            if (itr == node->childs.end()) {
                 break;
             }
+
+            node = itr->second;
+            if (node->end) {
+                if (i + 1 < len) {
+                    const vector<vector<size_t>>& subResults = wordBreak(s, i + 1, root, dones, results);
+                    for (size_t j = 0; j < subResults.size(); ++j) {
+                        const vector<size_t>& subResult = subResults[j];
+                        result.emplace_back(subResult.begin(), subResult.end());
+                        result.back().insert(result.back().begin(), node->index);
+                    }
+                }
+                else {
+                    result.emplace_back(1, node->index);
+                }
+            }
         }
 
-        if (!success) {
-            cacheFails[index] = true;
-        }
-        return success;
+        dones[index] = true;
+        return result;
     }
 
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        TrieNode* root = buildTrie(wordDict);
-        vector<bool> cacheFails(s.size(), false);
-        string sentence;
-        vector<string> sentences;
+        if (s.empty()) {
+            return {};
+        }
 
-        wordBreak(s, 0, cacheFails, root, sentence, sentences);
+        const TrieNode* root = buildTrie(wordDict);
+        vector<bool> dones(s.size(), false);
+        vector<vector<vector<size_t>>> results(s.size());
+        const vector<vector<size_t>>& result = wordBreak(s, 0, root, dones, results);
+
+        vector<string> sentences;
+        for (size_t i = 0; i < result.size(); ++i) {
+            const vector<size_t>& indexes = result[i];
+            string sentence;
+            for (size_t j = 0; j < indexes.size(); ++j) {
+                sentence += wordDict[indexes[j]];
+                if (j + 1 < indexes.size()) {
+                    sentence += " ";
+                }
+            }
+            sentences.emplace_back(sentence);
+        }
 
         return sentences;
     }
