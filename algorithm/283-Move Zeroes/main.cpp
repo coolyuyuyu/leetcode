0694f573365e5fb19691a5717eb4b2853604e6e0
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-25 12:14:11 +0800

    updated solution of 283

--- algorithm/283-Move Zeroes/main.cpp
+++ algorithm/283-Move Zeroes/main.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        size_t count = 0;
+        size_t index = 0;
         for (size_t i = 0; i < nums.size(); ++i) {
             if (nums[i] != 0) {
-                nums[count++] = nums[i];
+                nums[index++] = nums[i];
             }
         }
 
-        fill(nums.begin() + count, nums.end(), 0);
+        fill(nums.begin() + index, nums.end(), 0);
     }
 };
