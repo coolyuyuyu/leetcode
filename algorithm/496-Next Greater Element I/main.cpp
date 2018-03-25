f852a2fbd809b31216facccf821450134f408323
coolyuyuyu <coolyuyuyu@gmail.com>
2018-03-25 12:14:35 +0800

    updated solution of 496

--- algorithm/496-Next Greater Element I/main.cpp
+++ algorithm/496-Next Greater Element I/main.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        unordered_map<int, int> nexts;
         stack<int> stk;
-        unordered_map<int, int> m;
         for (size_t i = 0; i < nums.size(); ++i) {
             while (!stk.empty() && stk.top() < nums[i]) {
-                m.emplace(stk.top(), nums[i]);
+                nexts[stk.top()] = nums[i];
                 stk.pop();
             }
             stk.push(nums[i]);
         }
 
         while (!stk.empty()) {
-            m.emplace(stk.top(), -1);
+            nexts[stk.top()] = -1;
             stk.pop();
         }
 
-        vector<int> result;
-        result.reserve(findNums.size());
+        vector<int> result(findNums.size());
         for (size_t i = 0; i < findNums.size(); ++i) {
-            result.push_back(m[findNums[i]]);
+            result[i] = nexts[findNums[i]];
         }
 
         return result;
