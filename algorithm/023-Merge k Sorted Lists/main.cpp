485004e22189c55953ccd810443faa4bee46f87d
coolyuyuyu <coolyuyuyu@gmail.com>
2018-05-22 22:52:57 +0800

    Updated solution of 023

--- algorithm/023-Merge k Sorted Lists/main.cpp
+++ algorithm/023-Merge k Sorted Lists/main.cpp
@@ -24,7 +24,19 @@ public:
         return pHead;
     }
 
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    ListNode* mergeKListsOneByOne(vector<ListNode*>& lists) {
+        if (lists.empty()) {
+            return nullptr;
+        }
+
+        for (size_t i = 1; i < lists.size(); ++i) {
+            lists[0] = mergeTwoLists(lists[0], lists[i]);
+        }
+
+        return lists.front();
+    }
+
+    ListNode* mergeKListsBottomUpMerge(vector<ListNode*>& lists) {
         if (lists.empty()) {
             return nullptr;
         }
@@ -38,4 +50,35 @@ public:
 
         return lists.front();
     }
+
+    ListNode* mergeKListsByMinHeap(vector<ListNode*>& lists)
+    {
+        auto itrBgn = lists.begin(), itrEnd = lists.end();
+        itrEnd = remove(itrBgn, itrEnd, nullptr);
+        auto comp = [](ListNode* lft, ListNode* rht) { return lft->val > rht->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(comp)> pq(itrBgn, itrEnd, comp);
+
+        ListNode* pHead = nullptr;
+        ListNode** ppCur = &pHead;
+        while (!pq.empty()) {
+            ListNode* pNode = pq.top();
+            pq.pop();
+
+            *ppCur = pNode;
+            ppCur = &((*ppCur)->next);
+
+            pNode = pNode->next;
+            if (pNode) {
+                pq.push(pNode);
+            }
+        }
+
+        return pHead;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        return mergeKListsByMinHeap(lists);
+    }
+
 };
