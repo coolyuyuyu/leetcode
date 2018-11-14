ffbe881b02cd876eb88bc9dc53fe4414ebc850a6
coolyuyuyu <coolyuyuyu@gmail.com>
2018-11-14 23:20:24 +0800

    Initial solution of 908

---
+++ algorithm/908-Smallest Range I/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+        assert(!A.empty());
+        
+        int maxNum = A.front();
+        int minNum = A.front();
+        for (size_t i = 1; i < A.size(); ++i) {
+            if (A[i] < minNum) {
+                minNum = A[i];
+            }
+            else if (A[i] > maxNum) {
+                maxNum = A[i];
+            }
+        }
+
+        int diff = maxNum - minNum - 2 * K;
+        if (diff < 0) {
+            diff = 0;
+        }
+        return diff;
+    }
+};
