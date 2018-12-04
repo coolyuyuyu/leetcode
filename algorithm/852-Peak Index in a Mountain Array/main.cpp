06594a6535304bcfd3f91bb0b1870d828da90960
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-05 03:17:55 +0800

    Revised solution of 852

--- algorithm/852-Peak Index in a Mountain Array/main.cpp
+++ algorithm/852-Peak Index in a Mountain Array/main.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    /*
-    int peakIndexInMountainArray(vector<int>& A) {
+    int peakIndexInMountainArrayLinearSearch(vector<int>& A) {
         size_t i = 0;
         while (A[i] < A[i + 1]) {
             ++i;
@@ -9,21 +8,28 @@ public:
 
         return i;
     }
-    */
 
-    int peakIndexInMountainArray(vector<int>& A) {
-        size_t lo = 0, hi = A.size() - 1;
+    int peakIndexInMountainArrayBinarySearch(const vector<int>& A) {
+        size_t lo = 0;
+        size_t hi = A.size() - 1;
+
         while (lo < hi) {
             size_t mid = lo + (hi - lo) / 2;
             if (A[mid] < A[mid + 1]) {
                 lo = mid + 1;
             }
             else {
-                assert(A[mid] != A[mid + 1]);
                 hi = mid;
             }
         }
 
+        assert(lo == hi);
         return lo;
+    }
+
+    int peakIndexInMountainArray(vector<int>& A) {
+        //return peakIndexInMountainArrayLinearSearch(A);
+
+        return peakIndexInMountainArrayBinarySearch(A);
     }
 };
