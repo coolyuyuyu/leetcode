35decf7acdca4288b84c14ceea25fa6649fa2fda
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-05 21:53:48 +0800

    Revised solution of 256

--- algorithm/256-Paint House/main.cpp
+++ algorithm/256-Paint House/main.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int minCost(vector<vector<int>>& costs) {
-        size_t n = costs.size();
-        if (n == 0) {
+    int minCost1(const vector<vector<int>>& costs) {
+        if (costs.empty()) {
             return 0;
         }
 
+        size_t n = costs.size();
         vector<vector<int>> dp(n, vector<int>(3));
         dp[0] = costs[0];
         for (size_t i = 1; i < n; ++i) {
@@ -14,6 +14,29 @@ public:
             dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
 
-        return *min_element(dp[n - 1].begin() , dp[n - 1].end());
+        return min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
+    }
+
+    int minCost2(const vector<vector<int>>& costs) {
+        if (costs.empty()) {
+            return 0;
+        }
+
+        size_t n = costs.size();
+        vector<int> dp(costs[0]);
+        for (size_t i = 1; i < n; ++i) {
+            int r = min(dp[1], dp[2]) + costs[i][0];
+            int b = min(dp[0], dp[2]) + costs[i][1];
+            int g = min(dp[0], dp[1]) + costs[i][2];
+            dp = {r, b, g};
+        }
+
+        return min(min(dp[0], dp[1]), dp[2]);
+    }
+
+    int minCost(vector<vector<int>>& costs) {
+        //return minCost1(costs);
+
+        return minCost2(costs);
     }
 };
