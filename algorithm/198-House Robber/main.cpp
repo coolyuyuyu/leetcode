b410b2665837ae183d258ff307b078044e6b5f25
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-05 21:53:34 +0800

    Revised solution of 198

--- algorithm/198-House Robber/main.cpp
+++ algorithm/198-House Robber/main.cpp
@@ -1,13 +1,42 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int cur = 0;
-        int pre = 0;
-        for (size_t index = 0; index < nums.size(); ++index) {
-            int tmpCur = cur;
-            cur = max(pre + nums[index], cur);
-            pre = tmpCur;
+    int rob1(const vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
         }
-        return cur;
+        else if (nums.size() == 1) {
+            return nums.front();
+        }
+        else if (nums.size() == 2){
+            return max(nums[0], nums[1]);
+        }
+
+        size_t n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (size_t i = 2; i < n; ++i) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+
+    int rob2(const vector<int>& nums) {
+        int curMax = 0;
+        int preMax = 0;
+        for (int num : nums) {
+            int tmp = curMax;
+            curMax = max(curMax, preMax + num);
+            preMax = tmp;
+        }
+
+        return curMax;
+    }
+
+    int rob(vector<int>& nums) {
+        //return rob1(nums);
+
+        return rob2(nums);
     }
 };
