7b497b2e65db607e3264cd3c9e4f87c8a01c3e5e
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-10 21:09:58 +0800

    Revised solution of 215

--- algorithm/215-Kth Largest Element in an Array/main.cpp
+++ algorithm/215-Kth Largest Element in an Array/main.cpp
@@ -187,6 +187,74 @@ public:
         assert(false);
     }
 
+    // Use decision tree to sort 5 elements within 7 steps
+    void sort5Elems(vector<int>& nums, size_t first) {
+        sort(nums, first, first + 5);
+    }
+
+    int findKthSmallestBestRecv(vector<int>& nums,size_t first, size_t last, size_t k) {
+        size_t len = last - first;
+        assert(k < len);
+        if (len <= 5) {
+            sort(nums, first, last);
+            return nums[first + k];
+        }
+
+        int medianOfMedians = 0; {
+            vector<int> medians;
+            medians.reserve(len / 5);
+            for (size_t start = first; start < last; start += 5) {
+                size_t stop = start + 5;
+                if (stop <= last) {
+                    sort5Elems(nums, start);
+                    medians.emplace_back(nums[start + 2]);
+                }
+            }
+
+            medianOfMedians = findKthSmallestBestRecv(medians, 0, medians.size(), medians.size() / 2);
+        }
+
+        size_t len1, len2, len3;
+        {
+            vector<int> nums1, nums2, nums3;
+            for (size_t i = first; i < last; ++i) {
+                if (nums[i] < medianOfMedians) {
+                    nums1.emplace_back(nums[i]);
+                }
+                else if (nums[i] == medianOfMedians) {
+                    nums2.emplace_back(nums[i]);
+                }
+                else {
+                    nums3.emplace_back(nums[i]);
+                }
+            }
+            len1 = nums1.size();
+            len2 = nums2.size();
+            len3 = nums3.size();
+
+            size_t i = first;
+            for (size_t j = 0; j < nums1.size(); ++j) {
+                nums[i++] = nums1[j];
+            }
+            for (size_t j = 0; j < nums2.size(); ++j) {
+                nums[i++] = nums2[j];
+            }
+            for (size_t j = 0; j < nums3.size(); ++j) {
+                nums[i++] = nums3[j];
+            }
+        }
+
+        if (k < len1) {
+            return findKthSmallestBestRecv(nums, first, first + len1, k);
+        }
+        else if (k < len1 + len2){
+            return nums[first + len1];
+        }
+        else {
+            return findKthSmallestBestRecv(nums, first + len1 + len2, last, k - len1 - len2);
+        }
+    }
+
     int findKthSmallest(vector<int>& nums, size_t k) {
         //Time: T(n) = O(nlogn)
         //return findKthSmallestBySort(nums, k);
@@ -197,7 +265,11 @@ public:
 
         //Iterative Partition
         //Time: T(n) = O(n) for average case, O(n^2) for worst case
-        return findKthSmallestByPartitionIter(nums, k);
+        //return findKthSmallestByPartitionIter(nums, k);
+
+        //Recursive Partition
+        //Time: T(n) = O(n) for average case, O(n) for worst case
+        return findKthSmallestBestRecv(nums, 0, nums.size(), k);
     }
 
     int findKthLargest(vector<int>& nums, size_t k) {
