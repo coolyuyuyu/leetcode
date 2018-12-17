829d3d79777f743016ba517f313935c00b79668b
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-18 00:51:07 +0800

    148: add merge sort

--- algorithm/148-Sort List/main.cpp
+++ algorithm/148-Sort List/main.cpp
@@ -51,7 +51,76 @@ public:
         return pHead1;
     }
 
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* pHead = NULL;
+        ListNode** ppCur = &pHead;
+        while (l1 && l2) {
+            ListNode** ppNode = l1->val < l2->val ? &l1 : &l2;
+            *ppCur = *ppNode;
+            *ppNode = (*ppNode)->next;
+
+            ppCur = &((*ppCur)->next);
+        }
+
+        *ppCur = l1 ? l1 : l2;
+
+        return pHead;
+    }
+
+    ListNode* mergesortListMergeByMinHeap(ListNode* head) {
+        auto comp = [](const pair<ListNode*, size_t>& lft, const pair<ListNode*, size_t>& rht) { return rht.second < lft.second; };
+        priority_queue<pair<ListNode*, size_t>, vector<pair<ListNode*, size_t>>, decltype(comp)> pq(comp);
+        for (ListNode *prev = nullptr, *node = head; node;) {
+            prev = node;
+            node = node->next;
+
+            prev->next = nullptr;
+            pq.push(pair<ListNode*, size_t>(prev, 1));
+        }
+        if (pq.empty()) {
+            return nullptr;
+        }
+
+        while (1 < pq.size()) {
+            pair<ListNode*, size_t> p1 = pq.top();
+            pq.pop();
+            pair<ListNode*, size_t> p2 = pq.top();
+            pq.pop();
+
+            ListNode* head = mergeTwoLists(p1.first, p2.first);
+            pq.emplace(head, p1.second + p2.second);
+        }
+
+        return pq.top().first;
+    }
+
+    ListNode* mergesortListBottomUpMerge(ListNode* head) {
+        vector<ListNode*> lists;
+        for (ListNode *prev = nullptr, *node = head; node;) {
+            prev = node;
+            node = node->next;
+
+            prev->next = nullptr;
+            lists.emplace_back(prev);
+        }
+        if (lists.empty()) {
+            return nullptr;
+        }
+
+        for (size_t iteration = ceil(log2(lists.size())), space = 1; iteration > 0; --iteration, space *= 2) {
+            size_t step = space * 2;
+            for (size_t i = 0; i + space < lists.size(); i += step) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + space]);
+            }
+        }
+
+        return lists.front();
+    }
+
     ListNode* sortList(ListNode* head) {
-        return quicksortList(head);
+        //return quicksortList(head);
+
+        //return mergesortListMergeByMinHeap(head);
+        return mergesortListBottomUpMerge(head);
     }
 };
