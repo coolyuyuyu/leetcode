a9ac833733f4798ee269ea4bf5f6075f48b66fe2
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-19 21:26:55 +0800

    221: another solution

--- algorithm/221-Maximal Square/main.cpp
+++ algorithm/221-Maximal Square/main.cpp
@@ -31,7 +31,46 @@ public:
         return maxLength * maxLength;
     }
 
+    // Space(rowCnt, colCnt) = colCnt
+    size_t maximalSquareV2(vector<vector<char>>& matrix) {
+        size_t rowCnt = matrix.size();
+        size_t colCnt = matrix.empty() ? 0 : matrix.front().size();
+
+        size_t maxLength = 0;
+        vector<size_t> dp(colCnt, 0);
+        for (size_t col = 0; col < colCnt; ++col) {
+            dp[col] = matrix[0][col] - '0';
+            maxLength = max(maxLength, dp[col]);
+        }
+        for (size_t row = 0; row < rowCnt && maxLength == 0; ++row) {
+            if (matrix[row][0] == '1') {
+                maxLength = 1;
+            }
+        }
+
+        for (size_t row = 1; row < rowCnt; ++row) {
+            size_t prevLength = dp[0];
+            dp[0] = matrix[row][0] - '0';
+            for (size_t col = 1; col < colCnt; ++col) {
+                size_t tmp = dp[col];
+                if (matrix[row][col] == '1') {
+                    dp[col] = min(prevLength, min(dp[col], dp[col - 1])) + 1;
+                    maxLength = max(maxLength, dp[col]);
+                }
+                else {
+                    dp[col] = 0;
+                }
+
+                prevLength = tmp;
+            }
+        }
+
+        return maxLength * maxLength;
+    }
+
     size_t maximalSquare(vector<vector<char>>& matrix) {
-        return maximalSquareV1(matrix);
+        //return maximalSquareV1(matrix);
+
+        return maximalSquareV2(matrix);
     }
 };
