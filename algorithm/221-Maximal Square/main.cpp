db0db59846fa5e1a2b3dc67e656cfe5e40e3c48b
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-19 20:59:21 +0800

    221: rename variable

--- algorithm/221-Maximal Square/main.cpp
+++ algorithm/221-Maximal Square/main.cpp
@@ -5,22 +5,22 @@ public:
         size_t rowCnt = matrix.size();
         size_t colCnt = matrix.empty() ? 0 : matrix.front().size();
 
-        size_t length = 0;
+        size_t maxLength = 0;
         vector<vector<size_t>> dp(rowCnt, vector<size_t>(colCnt, 0));
         for (size_t row = 0; row < rowCnt; ++row) {
             dp[row][0] = matrix[row][0] - '0';
-            length = max(length, dp[row][0]);
+            maxLength = max(maxLength, dp[row][0]);
         }
         for (size_t col = 0; col < colCnt; ++col) {
             dp[0][col] = matrix[0][col] - '0';
-            length = max(length, dp[0][col]);
+            maxLength = max(maxLength, dp[0][col]);
         }
 
         for (size_t row = 1; row < rowCnt; ++row) {
             for (size_t col = 1; col < colCnt; ++col) {
                 if (matrix[row][col] == '1') {
                     dp[row][col] = min(dp[row - 1][col - 1], min(dp[row - 1][col], dp[row][col - 1])) + 1;
-                    length = max(length, dp[row][col]);
+                    maxLength = max(maxLength, dp[row][col]);
                 }
                 else {
                     dp[row][col] = 0;
@@ -28,7 +28,7 @@ public:
             }
         }
 
-        return length * length;
+        return maxLength * maxLength;
     }
 
     size_t maximalSquare(vector<vector<char>>& matrix) {
