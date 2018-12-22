44c307930f346429d748e772a10a1ecf748c1543
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-22 15:07:47 +0800

    120: add a new solution

--- algorithm/120-Triangle/main.cpp
+++ algorithm/120-Triangle/main.cpp
@@ -20,9 +20,27 @@ public:
         return *min_element(dp.back().begin(), dp.back().end());
     }
 
+    // Space(m, n) = n
+    int minimumTotalV2(const vector<vector<int>>& triangle) {
+        vector<int> dp(triangle.size(), 0);
+        dp.front() = triangle.front().front();
+        for (size_t row = 1; row < triangle.size(); ++row) {
+            size_t colCnt = row + 1;
+            dp[colCnt - 1] = dp[colCnt - 2] + triangle[row][colCnt - 1];
+            for (size_t col = colCnt - 1; 1 < col--;) {
+                dp[col] = min(dp[col - 1], dp[col]) + triangle[row][col];
+            }
+            dp[0] = dp[0] + triangle[row][0];
+        }
+
+        return *min_element(dp.begin(), dp.end());
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
         assert(!triangle.empty());
 
-        return minimumTotalV1(triangle);
+        //return minimumTotalV1(triangle);
+
+        return minimumTotalV2(triangle);
     }
 };
