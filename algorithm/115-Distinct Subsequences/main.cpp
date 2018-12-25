56fa75820754c996ad129d50fbb466794bdb1a48
coolyuyuyu <coolyuyuyu@gmail.com>
2018-12-25 16:54:02 +0800

    115: another dp approach

--- algorithm/115-Distinct Subsequences/main.cpp
+++ algorithm/115-Distinct Subsequences/main.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // Time(m, n) = O(m * n), Space(m, n) = O(m, n)
-    size_t numDistinctDp(const string& s, const string& t) {
+    size_t numDistinctDpV1(const string& s, const string& t) {
         size_t rowCnt = t.size() + 1, colCnt = s.size() + 1;
         vector<vector<size_t>> dp(rowCnt, vector<size_t>(colCnt));
         for (size_t col = 0; col < colCnt; ++col) {
@@ -20,7 +20,31 @@ public:
         return dp[rowCnt - 1][colCnt - 1];
     }
 
+    // Time(m, n) = O(m * n), Space(m, n) = O(m)
+    size_t numDistinctDpV2(const string& s, const string& t) {
+        size_t rowCnt = t.size() + 1, colCnt = s.size() + 1;
+        vector<size_t> dp(colCnt, 1);
+        for (size_t row = 1; row < rowCnt; ++row) {
+            size_t prev = dp[0];
+            dp[0] = 0;
+            for(size_t col = 1; col < colCnt; ++col) {
+                size_t tmp = dp[col];
+
+                dp[col] = dp[col - 1];
+                if (t[row - 1] == s[col - 1]) {
+                    dp[col] += prev;
+                }
+
+                prev = tmp;
+            }
+        }
+
+        return dp[colCnt - 1];
+    }
+
     size_t numDistinct(string s, string t) {
-        return numDistinctDp(s, t);
+        //return numDistinctDpV1(s, t);
+
+        return numDistinctDpV2(s, t);
     }
 };
