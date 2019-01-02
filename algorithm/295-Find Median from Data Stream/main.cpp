e844495dfc2e9b5bd42b01b1998cf9b7ca51d7cd
coolyuyuyu <coolyuyuyu@gmail.com>
2019-01-02 17:32:45 +0800

    295: code cleanup

--- algorithm/295-Find Median from Data Stream/main.cpp
+++ algorithm/295-Find Median from Data Stream/main.cpp
@@ -1,7 +1,12 @@
-class MedianFinder {
+class MediaFinderStrategy {
 public:
-    /** initialize your data structure here. */
-    MedianFinder() {
+    virtual void addNum(int num) = 0;
+    virtual double findMedian() const = 0;
+};
+
+class MediaFinderStrategyByHeap : public MediaFinderStrategy {
+public:
+    MediaFinderStrategyByHeap() {
     }
 
     void addNum(int num) {
@@ -15,13 +20,36 @@ public:
         }
     }
 
-    double findMedian() {
+    double findMedian() const {
         return m_lo.size() > m_hi.size() ? m_lo.top() : (m_lo.top() + m_hi.top()) * 0.5;
     }
 
 private:
     priority_queue<int> m_lo; // max-heap
     priority_queue<int, vector<int>, greater<int>> m_hi; // min-heap
+};
+
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {
+        m_strategy = new MediaFinderStrategyByHeap();
+    }
+
+    ~MedianFinder() {
+        delete m_strategy;
+    }
+
+    void addNum(int num) {
+        m_strategy->addNum(num);
+    }
+
+    double findMedian() {
+        return m_strategy->findMedian();
+    }
+
+private:
+    MediaFinderStrategy* m_strategy;
 };
 
 /**
