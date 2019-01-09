d2a1e78796feda2fd2a433f5fd5b9bd6c8663bda
coolyuyuyu <coolyuyuyu@gmail.com>
2019-01-09 13:32:39 +0800

    495: add one more solution

--- algorithm/495-Teemo Attacking/main.cpp
+++ algorithm/495-Teemo Attacking/main.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        size_t lo = 0;
-        size_t hi = nums.size();
-
+    inline int searchInsert(const vector<int>& nums, size_t lo, size_t hi, int target) {
         while (lo < hi) {
             size_t mid = lo + (hi - lo) / 2;
             if (target <= nums[mid]) {
@@ -19,8 +16,8 @@ public:
         return hi;
     }
 
-    // Time(n): O(nlogn)
-    int findPoisonedDurationV1(vector<int>& timeSeries, int duration) {
+    // Time(n): O((n/duration)(logn))
+    int findPoisonedDurationV1(const vector<int>& timeSeries, int duration) {
         if (duration == 0) {
             return 0;
         }
@@ -28,7 +25,7 @@ public:
         int totalTime = 0;
         for (size_t index = 0; index < timeSeries.size();) {
             int target = timeSeries[index] + duration;
-            size_t indexTmp = searchInsert(timeSeries, target);
+            size_t indexTmp = searchInsert(timeSeries, index + 1, timeSeries.size(), target);
             if (index + 1 == indexTmp) {
                 totalTime += duration;
                 index = indexTmp;
@@ -43,7 +40,7 @@ public:
     }
 
     // Time(n): O(n)
-    int findPoisonedDurationV2(vector<int>& timeSeries, int duration) {
+    int findPoisonedDurationV2(const vector<int>& timeSeries, int duration) {
         if (timeSeries.empty()) {
             return 0;
         }
@@ -56,9 +53,34 @@ public:
         return totalTime;
     }
 
+    // Mix of V1 and V2
+    int findPoisonedDurationV3(const vector<int>& timeSeries, int duration) {
+        if (timeSeries.empty()) {
+            return 0;
+        }
+
+        int totalTime = duration;
+        for (size_t index = 0; index + 1 < timeSeries.size();) {
+            if (timeSeries[index + 1] - timeSeries[index] >= duration) {
+                totalTime += duration;
+                index += 1;
+            }
+            else {
+                int target = timeSeries[index] + duration;
+                size_t indexTmp = searchInsert(timeSeries, index + 1, timeSeries.size(), target);
+                totalTime += (timeSeries[indexTmp - 1] - timeSeries[index]);
+                index = indexTmp - 1;
+            }
+        }
+
+        return totalTime;
+    }
+
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         //return findPoisonedDurationV1(timeSeries, duration);
 
-        return findPoisonedDurationV2(timeSeries, duration);
+        //return findPoisonedDurationV2(timeSeries, duration);
+
+        return findPoisonedDurationV3(timeSeries, duration);
     }
 };
