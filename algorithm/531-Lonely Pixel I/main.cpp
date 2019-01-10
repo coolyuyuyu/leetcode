d6021e51f57d36599d74f9b311c40ce3c2c568b5
coolyuyuyu <coolyuyuyu@gmail.com>
2019-01-10 18:37:46 +0800

    531: add one more dp solution

--- algorithm/531-Lonely Pixel I/main.cpp
+++ algorithm/531-Lonely Pixel I/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Time(m,n) = O(m*n), Space(m,n) = O(m + n)
+    // Time(m,n) = O(m*n), Space(m,n) = O(m+n)
     size_t findLonelyPixelV1(const vector<vector<char>>& picture) {
         size_t rowCnt = picture.size(), colCnt = picture.empty() ? 0 : picture.front().size();
         vector<int> dpRow(rowCnt, 0), dpCol(colCnt, 0);
@@ -24,8 +24,43 @@ public:
 
         return count;
     }
-    
+
+    // Time(m,n) = O(m*n), Space(m,n) = O(1)
+    size_t findLonelyPixelV2(const vector<vector<char>>& picture) {
+        size_t rowCnt = picture.size(), colCnt = picture.empty() ? 0 : picture.front().size();
+
+        size_t lonelyRowCnt = 0;
+        for (size_t row = 0; row < rowCnt; ++row) {
+            size_t n = 0;
+            for (size_t col = 0; col < colCnt; ++col) {
+                if (picture[row][col] == 'B') {
+                    ++n;
+                }
+            }
+            if (n == 1) {
+                ++lonelyRowCnt;
+            }
+        }
+
+        size_t lonelyColCnt = 0;
+        for (size_t col = 0; col < colCnt; ++col) {
+            size_t n = 0;
+            for (size_t row = 0; row < rowCnt; ++row) {
+                if (picture[row][col] == 'B') {
+                    ++n;
+                }
+            }
+            if (n == 1) {
+                ++lonelyColCnt;
+            }
+        }
+
+        return min(lonelyRowCnt, lonelyColCnt);
+    }
+
     size_t findLonelyPixel(const vector<vector<char>>& picture) {
-        return findLonelyPixelV1(picture);
+        //return findLonelyPixelV1(picture);
+
+        return findLonelyPixelV2(picture);
     }
 };
