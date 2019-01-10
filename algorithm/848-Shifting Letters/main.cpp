157ed16224ced7c5d79a04079acb0570c3fdf552
coolyuyuyu <coolyuyuyu@gmail.com>
2019-01-10 13:15:09 +0800

    848: revise solution using partial sum

--- algorithm/848-Shifting Letters/main.cpp
+++ algorithm/848-Shifting Letters/main.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string shiftingLetters(string S, vector<int>& shifts) {
-        for (size_t i = shifts.size(); 0 < i--;) {
-            if (i + 1 == shifts.size()) {
-                shifts[i] %= 26;
-            }
-            else {
-                shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
-            }
-        }
+        assert(!shifts.empty());
+
+        shifts.back() %= 26;
+        auto lambda = [](int a, int b) {
+            return (a  + (b % 26) % 26);
+        };
+        partial_sum(shifts.rbegin(), shifts.rend(), shifts.rbegin(), lambda);
 
         for (size_t i = 0; i < S.size(); ++i) {
             S[i] = (S[i] - 'a' + shifts[i]) % 26 + 'a';
