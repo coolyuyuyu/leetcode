1c304b6c5e6f07f17947b43cd70d325cc5cb932e
coolyuyuyu <coolyuyuyu@gmail.com>
2019-01-10 17:41:20 +0800

    518: onre more dp solution with less space

--- algorithm/518-Coin Change 2/main.cpp
+++ algorithm/518-Coin Change 2/main.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // Time(amount, size_of_coins) = O(amount * size_of_coins), Space(amount, size_of_coins) = O(amount * size_of_coins)
-    size_t changeDp(int amount, const vector<int>& coins) {
+    size_t changeDpV1(int amount, const vector<int>& coins) {
         vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
         dp[0][0] = 1;
 
@@ -14,7 +14,22 @@ public:
         return dp.back().back();
     }
 
+    // Time(amount, size_of_coins) = O(amount * size_of_coins), Space(amount, size_of_coins) = O(amount)
+    size_t changeDpV2(int amount, const vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= coins.size(); ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                dp[j] = dp[j] + (coins[i - 1] <= j ? dp[j - coins[i - 1]] : 0);
+            }
+        }
+        return dp.back();
+    }
+
     size_t change(int amount, const vector<int>& coins) {
-        return changeDp(amount, coins);
+        //return changeDpV1(amount, coins);
+
+        return changeDpV2(amount, coins);
     }
 };
