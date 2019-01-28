e24df6855cc07cc569e672c9748aed5b9d612640
coolyuyuyu <coolyuyuyu@gmail.com>
2019-01-28 16:14:14 +0800

    834: add a iterative solution

--- algorithm/834-Sum of Distances in Tree/main.cpp
+++ algorithm/834-Sum of Distances in Tree/main.cpp
@@ -43,7 +43,87 @@ public:
         return distances;
     }
 
+    vector<int> sumOfDistancesInTreeIter(int N, vector<vector<int>>& edges) {
+        int root = 0;
+        vector<vector<int>> tree(N);
+        vector<int> parents(N); {
+            vector<unordered_set<int>> graph(N);
+            for (vector<int>& edge : edges) {
+                graph[edge[0]].emplace(edge[1]);
+                graph[edge[1]].emplace(edge[0]);
+            }
+
+            queue<int> nodes;
+            nodes.emplace(root);
+            while (!nodes.empty()) {
+                size_t n = nodes.size();
+                for (size_t i = 0; i < n; ++i) {
+                    int node = nodes.front();
+                    nodes.pop();
+
+                    for (int child : graph[node]) {
+                        graph[child].erase(node);
+                        nodes.emplace(child);
+
+                        parents[child] = node;
+                    }
+                }
+            }
+            for (int node = 0; node < N; ++node) {
+                tree[node] = vector(graph[node].begin(), graph[node].end());
+            }
+
+            parents[root] = -1;
+        }
+
+        vector<int> counts(N, 0);
+        int distance = 0; {
+            queue<int> leaves; {
+                for (int node = 0; node < N; ++node) {
+                    if (tree[node].empty()) {
+                        leaves.emplace(node);
+                    }
+                }
+            }
+            vector<int> childCounts(N); {
+                for (int node = 0; node < N; ++node) {
+                    childCounts[node] = tree[node].size();
+                }
+            }
+            while (!leaves.empty()) {
+                int leave = leaves.front();
+                leaves.pop();
+
+                int parent = parents[leave];
+                if (0 <= parent) {
+                    counts[parent] += (counts[leave] + 1);
+                    if (--childCounts[parent] == 0) {
+                        leaves.emplace(parent);
+                    }
+                }
+                distance += counts[leave];
+                counts[leave] += 1;
+            }
+        }
+
+        vector<int> distances(N); distances[root] = distance; {
+            queue<int> nodes;
+            nodes.emplace(root);
+            while (!nodes.empty()) {
+                int node = nodes.front();
+                nodes.pop();
+                for (int child : tree[node]) {
+                    distances[child] = distances[node] - counts[child] + (N - counts[child]);
+                    nodes.emplace(child);
+                }
+            }
+        }
+        return distances;
+    }
+
     vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
-        return sumOfDistancesInTreeRecv(N, edges);
+        //return sumOfDistancesInTreeRecv(N, edges);
+
+        return sumOfDistancesInTreeIter(N, edges);
     }
 };
