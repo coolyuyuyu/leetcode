d03b1e183f4bdc1baefe514cf984ba4b915dd8cd
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-11 02:57:42 +0800

    021: revised solution

--- algorithm/021-Merge Two Sorted Lists/main.cpp
+++ algorithm/021-Merge Two Sorted Lists/main.cpp
@@ -3,23 +3,24 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* pHead = NULL;
+        ListNode* pHead = nullptr;
         ListNode** ppCur = &pHead;
         while (l1 && l2) {
-            ListNode **ppNode = &(l1->val < l2->val ? l1 : l2);
+            ListNode** ppNode = &(l1->val <= l2->val ? l1 : l2);
             *ppCur = *ppNode;
             *ppNode = (*ppNode)->next;
 
             ppCur = &((*ppCur)->next);
         }
-
-        *ppCur = l1 ? l1 : l2;
+        *ppCur = (l1 ? l1 : l2);
 
         return pHead;
     }
