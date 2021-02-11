ee0934876bda3367ea2974364b1fdba68df34eff
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-11 17:16:01 +0800

    1721: initial solution

---
+++ algorithm/1721-Swapping Nodes in a Linked List/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode** ppForeward;
+        ListNode** ppBackward;
+        {
+            ListNode** ppFast = &head;
+            for (int i = 1; i < k; ++i) {
+                ppFast = &((*ppFast)->next);
+            }
+            ppForeward = ppFast;
+
+            ListNode** ppSlow = &head;
+            while ((*ppFast)->next) {
+                ppSlow = &((*ppSlow)->next);
+                ppFast = &((*ppFast)->next);
+            }
+            ppBackward = ppSlow;
+
+        }
+        swap(*ppForeward, *ppBackward);
+        swap((*ppForeward)->next, (*ppBackward)->next);
+
+        return head;
+    }
+};
