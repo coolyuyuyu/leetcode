94622136fa28b3b5c93cc402fde5dabc03e8bf29
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-15 18:51:44 +0800

    190: linear scan implementation

--- algorithm/190-Reverse Bits/main.cpp
+++ algorithm/190-Reverse Bits/main.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
+    uint32_t reverseBits_linerScan(uint32_t n) {
         uint32_t m = 0;
         for (int i = 0; i < 32; ++i) {
             m <<= 1;
-            m |= n & 1;
+            m |= (n & 1);
             n >>= 1;
         }
+
         return m;
+    }
+
+    uint32_t reverseBits(uint32_t n) {
+        return reverseBits_linerScan(n);
     }
 };
