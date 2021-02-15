36d904777d218fbe0f47b36142251cd566e0a03f
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-16 01:06:09 +0800

    836: deal with line and point case

--- algorithm/836-Rectangle Overlap/main.cpp
+++ algorithm/836-Rectangle Overlap/main.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
+    bool checkRec(vector<int>& rec) {
+        return rec[0] != rec[2] && rec[1] != rec[3];
+    }
+
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        bool hOverlap = rec1[0] < rec2[2] && rec2[0] < rec1[2];
-        bool vOverlap = rec1[1] < rec2[3] && rec2[1] < rec1[3];
+        if (!(checkRec(rec1) && checkRec(rec2))) {
+            return false;
+        }
+
+        bool hOverlap = rec2[0] < rec1[2] && rec1[0] < rec2[2];
+        bool vOverlap = rec2[1] < rec1[3] && rec1[1] < rec2[3];
         return hOverlap && vOverlap;
     }
 };
