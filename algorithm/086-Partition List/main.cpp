5da6305badbd563f75402d1c3a459b36fa8ae086
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-20 18:35:32 +0800

    086: revise implementation

--- algorithm/086-Partition List/main.cpp
+++ algorithm/086-Partition List/main.cpp
@@ -3,32 +3,31 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* pLftHead = NULL;
+        ListNode* pLftHead = nullptr;
         ListNode** ppLftCur = &pLftHead;
-        ListNode* pRhtHead = NULL;
+        ListNode* pRhtHead = nullptr;
         ListNode** ppRhtCur = &pRhtHead;
-
-        ListNode* pCur = head;
-        while (pCur) {
+        for (ListNode* pCur = head; pCur; pCur = pCur->next) {
             if (pCur->val < x) {
                 *ppLftCur = pCur;
-                ppLftCur = &((*ppLftCur)->next);
+                ppLftCur = &(pCur->next);
             }
             else {
                 *ppRhtCur = pCur;
-                ppRhtCur = &((*ppRhtCur)->next);
+                ppRhtCur = &(pCur->next);
             }
-            pCur = pCur->next;
         }
 
         *ppLftCur = pRhtHead;
-        *ppRhtCur = NULL;
+        *ppRhtCur = nullptr;
 
         return pLftHead;
     }
