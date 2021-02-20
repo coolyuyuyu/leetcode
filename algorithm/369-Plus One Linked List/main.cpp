5093c054c51aa4f1319d3cefdf4896b6fbe7cff4
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-20 23:23:11 +0800

    369: another solution. Space: O(1)

--- algorithm/369-Plus One Linked List/main.cpp
+++ algorithm/369-Plus One Linked List/main.cpp
@@ -3,12 +3,15 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* plusOne(ListNode* head) {
+    // Time: O(n), Space: O(n)
+    ListNode* plusOne_v1(ListNode* head) {
         stack<ListNode*> stk;
         ListNode* pCur = head;
         while (pCur) {
@@ -42,5 +45,47 @@ public:
         }
 
         return head;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        if (!head) {
+            return nullptr;
+        }
+
+        ListNode** ppHead = &head;
+        ListNode* pNode = *ppHead;
+        while (pNode->next) {
+            ListNode* pTmp = pNode->next;
+            pNode->next= pTmp->next;
+            pTmp->next = *ppHead;
+            *ppHead = pTmp;
+        }
+
+        return head;
+    }
+
+    // Time: O(n), Space: O(1)
+    ListNode* plusOne_v2(ListNode* head) {
+        head = reverseList(head);
+
+        bool carry = true;
+        for (ListNode** ppNode = &head; carry; ppNode = &((*ppNode)->next)) {
+            if (!(*ppNode)) {
+                *ppNode = new ListNode(1);
+            }
+            else {
+                (*ppNode)->val += 1;
+            }
+
+            carry = (10 <= (*ppNode)->val);
+            (*ppNode)->val = (*ppNode)->val % 10;
+        }
+
+        return reverseList(head);
+    }
+
+    ListNode* plusOne(ListNode* head) {
+        //return plusOne_v1(head);
+        return plusOne_v2(head);
     }
 };
