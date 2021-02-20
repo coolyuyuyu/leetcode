bb9225d33798d92d3f98a4ca04552ac63da8d15d
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-20 18:24:27 +0800

    109: revise implementation

--- algorithm/109-Convert Sorted List to Binary Search Tree/main.cpp
+++ algorithm/109-Convert Sorted List to Binary Search Tree/main.cpp
@@ -3,7 +3,9 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 /**
@@ -12,29 +14,28 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* sortedListToBSTHelper(ListNode* head, ListNode* tail) {
-        if (head == tail) {
-            return NULL;
-        }
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast->next && fast->next != tail && fast->next->next != tail) {
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        
-        TreeNode* pRoot = new TreeNode(slow->val);
-        pRoot->left = sortedListToBSTHelper(head, slow);
-        pRoot->right = sortedListToBSTHelper(slow->next, tail);
-        return pRoot;
-    }
     TreeNode* sortedListToBST(ListNode* head) {
-        return sortedListToBSTHelper(head, NULL);
+        if (!head) {
+            return nullptr;
+        }
+
+        ListNode** ppFast = &head;
+        ListNode** ppSlow = &head;
+        while (*ppFast && (*ppFast)->next) {
+            ppFast = &((*ppFast)->next->next);
+            ppSlow = &((*ppSlow)->next);
+        }
+        ListNode* pMid = *ppSlow;
+        *ppSlow = nullptr;
+
+        TreeNode* pRoot = new TreeNode(pMid->val, sortedListToBST(head), sortedListToBST(pMid->next));
+        return pRoot;
     }
 };
