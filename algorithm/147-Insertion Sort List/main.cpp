a29710d9992b8f3c13c70f373ea2b4b286ccc6d2
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-21 19:11:30 +0800

    147: another implementation
    Speedup for already/nearly sorted case

--- algorithm/147-Insertion Sort List/main.cpp
+++ algorithm/147-Insertion Sort List/main.cpp
@@ -27,7 +27,37 @@ public:
         return headNew;
     }
 
+    // Record the last sorted element. Speedup for alreay/nearly sorted case.
+    ListNode* insertionSortList_v2(ListNode* head) {
+        ListNode* headNew = nullptr;
+        ListNode* pSortedTail = nullptr;
+        while (head) {
+            ListNode* pNode = head;
+            head = head->next;
+
+            if (pSortedTail && pSortedTail->val <= pNode->val) {
+                pSortedTail->next = pNode;
+                pSortedTail = pNode;
+                pNode->next = nullptr;
+            }
+            else {
+                ListNode** ppSortedCur = &headNew;
+                while (*ppSortedCur && (*ppSortedCur)->val < pNode->val) {
+                    ppSortedCur = &((*ppSortedCur)->next);
+                }
+                if (*ppSortedCur == nullptr) {
+                    pSortedTail = pNode;
+                }
+                pNode->next = *ppSortedCur;
+                *ppSortedCur = pNode;
+            }
+        }
+
+        return headNew;
+    }
+
     ListNode* insertionSortList(ListNode* head) {
-        return insertionSortList_v1(head);
+        //return insertionSortList_v1(head);
+        return insertionSortList_v2(head);
     }
 };
