b4bba62365e2663b8fbede9927d0f7626f9c5a25
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-22 22:10:50 +0800

    148: mergesort bottomup inplace

--- algorithm/148-Sort List/main.cpp
+++ algorithm/148-Sort List/main.cpp
@@ -25,6 +25,39 @@ private:
         return head;
     }
 
+    ListNode* splitList(ListNode* head, size_t k) {
+        ListNode** ppCur = &head;
+        while (*ppCur && 0 < k--) {
+            ppCur = &((*ppCur)->next);
+        }
+
+        ListNode* headLeft = *ppCur;
+        *ppCur = nullptr;
+        return headLeft;
+    }
+
+    ListNode* mergeSortedList(ListNode* l1, ListNode* l2, ListNode**& ppTail) {
+        assert(l1 || l2);
+
+        ListNode* head = nullptr;
+        ListNode** ppCur = &head;
+        while (l1 && l2) {
+            ListNode** ppNode = &(l1->val <= l2->val ? l1 : l2);
+            *ppCur = *ppNode;
+            *ppNode = (*ppNode)->next;
+
+            ppCur = &((*ppCur)->next);
+        }
+        *ppCur = (l1 ? l1 : l2);
+
+        while (*ppCur) {
+            ppCur = &((*ppCur)->next);
+        }
+        ppTail = ppCur;
+
+        return head;
+    }
+
 public:
     ListNode* quicksortList(ListNode* pHead) {
         if (pHead == nullptr) {
@@ -235,21 +268,42 @@ public:
             pNode->next = nullptr;
             pq.emplace(pNode, 1);
         }
-        
+
         while (1 < pq.size()) {
             auto p1 = pq.top();
             pq.pop();
             auto p2 = pq.top();
             pq.pop();
-            
+
             pq.emplace(mergeSortedList(p1.first, p2.first), p1.second + p2.second);
         }
-        
+
         return pq.top().first;
     }
 
+    // Time: O(nlog(n)), Space: O(1)
     ListNode* mergesort_BottomUpInplace(ListNode* head) {
-        return nullptr;
+        size_t size = 0;
+        for (ListNode* pCur = head; pCur; pCur = pCur->next) {
+            ++size;
+        }
+
+        for (size_t count = 1; count < size; count *= 2) {
+            ListNode** ppCur = &head;
+            while (*ppCur) {
+                ListNode* pLft = *ppCur;
+                ListNode* pRht = splitList(pLft, count);
+                ListNode* pPending = splitList(pRht, count);
+
+                ListNode** ppTail;
+                *ppCur = mergeSortedList(pLft, pRht, ppTail);
+                *ppTail = pPending;
+
+                ppCur = ppTail;
+            }
+        }
+
+        return head;
     }
 
     ListNode* sortList(ListNode* head) {
@@ -262,9 +316,6 @@ public:
         quicksortListV2(head, pHeadNew, ppTailNew);
         return pHeadNew;
         */
-
-        //return mergesortListMergeByMinHeap(head);
-        //return mergesortListBottomUpMerge(head);
 
         //return mergesort_TopDown(head);
         //return mergesort_BottomUpQueue(head);
