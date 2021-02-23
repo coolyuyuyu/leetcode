9592281ffbc4f3ad71d2af0efc957b83376d592c
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-24 00:12:21 +0800

    1441: initial solution

---
+++ algorithm/1441-Build an Array With Stack Operations/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> operations;
+        
+        int i = 1;
+        for (int t : target) {
+            for (; i < t; ++i) {
+                operations.push_back("Push");
+                operations.push_back("Pop");
+            }
+            operations.push_back("Push");
+            ++i;
+        }
+        
+        return operations;
+    }
+};
