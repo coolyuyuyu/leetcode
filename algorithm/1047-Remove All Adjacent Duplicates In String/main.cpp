5ce533f32bfd70a00ea4ee95763aab2c85e0f48d
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-24 00:02:30 +0800

    1047: linear approach

--- algorithm/1047-Remove All Adjacent Duplicates In String/main.cpp
+++ algorithm/1047-Remove All Adjacent Duplicates In String/main.cpp
@@ -20,7 +20,27 @@ public:
         return S;
     }
 
+    string removeDuplicates_Linear(string S) {
+        string result;
+        result.reserve(S.size());
+
+        if (!S.empty()) {
+            result.push_back(S.front());
+        }
+        for (size_t i = 1; i < S.size(); ++i) {
+            if (!result.empty() && S[i] == result.back()) {
+                result.pop_back();
+            }
+            else {
+                result.push_back(S[i]);
+            }
+        }
+
+        return result;
+    }
+
     string removeDuplicates(string S) {
-        return removeDuplicates_Stack(S);
+        //return removeDuplicates_Stack(S);
+        return removeDuplicates_Linear(S);
     }
 };
