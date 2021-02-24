3a04450784a30be5fdddf7d0bc870466fa8b9909
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-25 00:51:59 +0800

    346: update implementation

--- algorithm/346-Moving Average from Data Stream/main.cpp
+++ algorithm/346-Moving Average from Data Stream/main.cpp
@@ -2,29 +2,31 @@ class MovingAverage {
 public:
     /** Initialize your data structure here. */
     MovingAverage(int size)
-        : m_capacity(size) {
+        : m_capacity(size)
+        , m_sum(0.0) {
     }
 
     double next(int val) {
-        if (m_vals.size() == m_capacity) {
+        if (m_capacity <= m_vals.size()) {
             m_sum -= m_vals.front();
             m_vals.pop();
         }
-
-        m_sum += val;
         m_vals.push(val);
 
-        return m_sum * 1.0 / m_vals.size();
+        m_sum += val;
+
+        return static_cast<double>(m_sum) / m_vals.size();
     }
 
 private:
-    size_t m_capacity;
+    int m_capacity;
     queue<int> m_vals;
-    int m_sum = 0;
+
+    int m_sum;
 };
 
 /**
  * Your MovingAverage object will be instantiated and called as such:
- * MovingAverage obj = new MovingAverage(size);
- * double param_1 = obj.next(val);
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
  */
