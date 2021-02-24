c0d16c7f0ccdaa2fc23341af8e4b2c78062674c6
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-24 21:54:52 +0800

    232: all amortized O(1) implementation

--- algorithm/232-Implement Queue using Stacks/main.cpp
+++ algorithm/232-Implement Queue using Stacks/main.cpp
@@ -5,46 +5,53 @@ public:
     }
 
     /** Push element x to the back of queue. */
+    // Time: O(1)
     void push(int x) {
-        m_in.push(x);
+        m_stkI.push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
+    // Time: Amortized O(1)
     int pop() {
-        if (m_out.empty()) {
-            peek();
+        if (m_stkO.empty()) {
+            while (!m_stkI.empty()) {
+                m_stkO.push(m_stkI.top());
+                m_stkI.pop();
+            }
         }
-        int d = m_out.top();
-        m_out.pop();
-        return d;
+        int val = m_stkO.top();
+        m_stkO.pop();
+        return val;
     }
 
     /** Get the front element. */
+    // Time: Amortized O(1)
     int peek() {
-        if (m_out.empty()) {
-            while (!m_in.empty()) {
-                m_out.push(m_in.top());
-                m_in.pop();
+        if (m_stkO.empty()) {
+            while (!m_stkI.empty()) {
+                m_stkO.push(m_stkI.top());
+                m_stkI.pop();
             }
         }
-        return m_out.top();
+        return m_stkO.top();
     }
 
     /** Returns whether the queue is empty. */
+    // Time: O(1)
     bool empty() {
-        return m_in.empty() && m_out.empty();
+        return m_stkI.empty() && m_stkO.empty();
     }
 
 private:
-    stack<int> m_in;
-    stack<int> m_out;
+    stack<int> m_stkI;
+    stack<int> m_stkO;
 };
 
 /**
  * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * bool param_4 = obj.empty();
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
  */
