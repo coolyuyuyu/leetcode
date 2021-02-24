e5d6086a538a40f15d198f6ce8c536bb0ab50ceb
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-24 20:23:19 +0800

    844: implement 2 approaches

--- algorithm/844-Backspace String Compare/main.cpp
+++ algorithm/844-Backspace String Compare/main.cpp
@@ -1,50 +1,79 @@
 class Solution {
+private:
+    string reduceBackspace(const string& s) {
+        string result;
+        result.reserve(s.size());
+
+        for (char c : s) {
+            if (c == '#') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            }
+            else {
+                result.push_back(c);
+            }
+        }
+
+        return result;
+    }
+
+    class ReverseIterator {
+    public:
+        ReverseIterator(const string& str)
+            : m_str(str)
+            , m_itr(m_str.crbegin()) {
+        }
+
+        const char* next() {
+            for (size_t i = 1; m_itr != m_str.crend(); ++m_itr) {
+                i += (*m_itr == '#' ? 1 : -1);
+                if (i == 0) {
+                    break;
+                }
+            }
+
+            if (m_itr == m_str.crend()) {
+                return nullptr;
+            }
+            else {
+                return &(*(m_itr++));
+            }
+        }
+
+    private:
+        const string& m_str;
+        string::const_reverse_iterator m_itr;
+    };
+
 public:
-    bool backspaceCompare(string S, string T) {
-        int pos1 = S.size();
-        int pos2 = T.size();
-        do {
-            --pos1;
-            for (int skip = 0; 0 <= pos1 ; --pos1) {
-                if (S[pos1] == '#') {
-                    skip++;
-                }
-                else {
-                    if (skip == 0) {
-                        break;
-                    }
-                    --skip;
-                }
-            }
+    bool backspaceCompare_Reduce(const string& S, const string& T) {
+        return reduceBackspace(S) == reduceBackspace(T);
+    }
 
-            --pos2;
-            for (int skip = 0; 0 <= pos2; --pos2) {
-                if (T[pos2] == '#') {
-                    skip++;
-                }
-                else {
-                    if (skip == 0) {
-                        break;
-                    }
-                    --skip;
-                }
-            }
-
-            if (pos1 >= 0 && pos2 >= 0) {
-                if (S[pos1] != T[pos2]) {
+    bool backspaceCompare_ScanBackward(string S, string T) {
+        ReverseIterator itr1(S), itr2(T);
+        while (true) {
+            const char* pChar1 = itr1.next();
+            const char* pChar2 = itr2.next();
+            if (pChar1 && pChar2) {
+                if (*pChar1 != *pChar2) {
                     return false;
                 }
             }
-        } while (pos1 >= 0 && pos2 >= 0);
+            else if (!pChar1 && !pChar2) {
+                break;
+            }
+            else {
+                return false;
+            }
+        }
 
-        if (pos1 >= 0 && pos2 < 0) {
-            return false;
-        }
-        else if (pos1 < 0 && pos2 >= 0) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return true;
+    }
+
+    bool backspaceCompare(string S, string T) {
+        //return backspaceCompare_Reduce(S, T);
+        return backspaceCompare_ScanBackward(S, T);
     }
 };
