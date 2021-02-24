095337d31d158c2afe9042911fb2513d689e708d
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-24 21:21:15 +0800

    225: very fast solution

--- algorithm/225-Implement Stack using Queues/main.cpp
+++ algorithm/225-Implement Stack using Queues/main.cpp
@@ -5,42 +5,46 @@ public:
     }
 
     /** Push element x onto stack. */
+    // Time: O(n)
     void push(int x) {
-        m_q.push(x);
+        m_q2.push(x);
+        while (!m_q1.empty()) {
+            m_q2.push(m_q1.front());
+            m_q1.pop();
+        }
+        m_q1.swap(m_q2);
     }
 
     /** Removes the element on top of the stack and returns that element. */
+    // Time: O(1)
     int pop() {
-        size_t count = m_q.size();
-        for (size_t i = 0; i < (count - 1); ++i) {
-            int d = m_q.front();
-            m_q.pop();
-            m_q.push(d);
-        }
-        int d = m_q.front();
-        m_q.pop();
-        return d;
+        int val = top();
+        m_q1.pop();
+        return val;
     }
 
     /** Get the top element. */
+    // Time: O(1)
     int top() {
-        return m_q.back();
+        return m_q1.front();
     }
 
     /** Returns whether the stack is empty. */
+    // Time: O(1)
     bool empty() {
-        return m_q.empty();
+        return m_q1.empty();
     }
 
 private:
-    queue<int> m_q;
+    queue<int> m_q1;
+    queue<int> m_q2;
 };
 
 /**
  * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * bool param_4 = obj.empty();
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
  */
