9447e5d7f60bf94f8ec2a7b67c1c851108c933f0
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-24 22:48:32 +0800

    225: update implementation

--- algorithm/225-Implement Stack using Queues/main.cpp
+++ algorithm/225-Implement Stack using Queues/main.cpp
@@ -5,39 +5,44 @@ public:
     }
 
     /** Push element x onto stack. */
-    // Time: O(n)
+    // Time: O(1)
     void push(int x) {
-        m_q2.push(x);
-        while (!m_q1.empty()) {
-            m_q2.push(m_q1.front());
-            m_q1.pop();
+        if (!m_qO.empty()) {
+            m_qI.push(m_qO.front());
+            m_qO.pop();
         }
-        m_q1.swap(m_q2);
+        m_qO.push(x);
     }
 
     /** Removes the element on top of the stack and returns that element. */
-    // Time: O(1)
+    // Time: O(n)
     int pop() {
         int val = top();
-        m_q1.pop();
+        m_qO.pop();
+
+        while (1 < m_qI.size()) {
+            m_qO.push(m_qI.front());
+            m_qI.pop();
+        }
+        m_qO.swap(m_qI);
+
         return val;
     }
 
     /** Get the top element. */
     // Time: O(1)
     int top() {
-        return m_q1.front();
+        return m_qO.front();
     }
 
     /** Returns whether the stack is empty. */
-    // Time: O(1)
     bool empty() {
-        return m_q1.empty();
+        return m_qO.empty();
     }
 
 private:
-    queue<int> m_q1;
-    queue<int> m_q2;
+    queue<int> m_qI;
+    queue<int> m_qO;
 };
 
 /**
