1c25b502caad9fc1e682c0ebcb2d4e18561711e2
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-26 00:14:17 +0800

    622: update implementation

---
+++ algorithm/622-Design Circular Queue/main.cpp
@@ -0,0 +1,68 @@
+class MyCircularQueue {
+public:
+    MyCircularQueue(size_t capacity)
+        : m_size(0)
+        , m_capacity(capacity)
+        , m_index(0)
+        , m_data(new int[m_capacity]) {
+    }
+
+    ~MyCircularQueue() {
+        delete[] m_data;
+    }
+
+    bool enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        else {
+            m_data[(m_index + m_size) % m_capacity] = value;
+            ++m_size;
+            return true;
+        }
+    }
+
+    bool deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        else {
+            m_index = (m_index + 1) % m_capacity;
+            --m_size;
+            return true;
+        }
+    }
+
+    int Front() {
+        return (isEmpty() ? -1 : m_data[m_index]);
+    }
+
+    int Rear() {
+        return (isEmpty() ? -1 : m_data[(m_index + m_size - 1) % m_capacity]);
+    }
+
+    bool isEmpty() {
+        return (m_size == 0);
+    }
+
+    bool isFull() {
+        return (m_size == m_capacity);
+    }
+
+private:
+    size_t m_size;
+    size_t m_capacity;
+    size_t m_index;
+    int* m_data;
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
