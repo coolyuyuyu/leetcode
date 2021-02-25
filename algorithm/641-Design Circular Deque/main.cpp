9e93433964bb204ebc2f1222d29c127665397ee5
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-26 00:15:05 +0800

    641: update implementation

--- algorithm/641-Design Circular Deque/main.cpp
+++ algorithm/641-Design Circular Deque/main.cpp
@@ -2,10 +2,14 @@ class MyCircularDeque {
 public:
     /** Initialize your data structure here. Set the size of the deque to be k. */
     MyCircularDeque(size_t capacity)
-        : m_data(capacity)
-        , m_size(0)
-        , m_indexFront(capacity - 1)
-        , m_indexRear(0) {
+        : m_size(0)
+        , m_capacity(capacity)
+        , m_index(0)
+        , m_data(new int[m_capacity]) {
+    }
+
+    ~MyCircularDeque() {
+        delete[] m_data;
     }
 
     /** Adds an item at the front of Deque. Return true if the operation is successful. */
@@ -13,13 +17,12 @@ public:
         if (isFull()) {
             return false;
         }
-
-        m_data[m_indexFront] = value;
-        m_indexFront = (m_indexFront + m_data.size() - 1) % m_data.size();
-
-        ++m_size;
-
-        return true;
+        else {
+            m_index = (m_index + m_capacity - 1) % m_capacity;
+            m_data[m_index] = value;
+            ++m_size;
+            return true;
+        }
     }
 
     /** Adds an item at the rear of Deque. Return true if the operation is successful. */
@@ -27,13 +30,11 @@ public:
         if (isFull()) {
             return false;
         }
-
-        m_data[m_indexRear] = value;
-        m_indexRear = (m_indexRear + 1) % m_data.size();
-
-        ++m_size;
-
-        return true;
+        else {
+            m_data[(m_index + m_size) % m_capacity] = value;
+            ++m_size;
+            return true;
+        }
     }
 
     /** Deletes an item from the front of Deque. Return true if the operation is successful. */
@@ -41,12 +42,11 @@ public:
         if (isEmpty()) {
             return false;
         }
-
-        m_indexFront = (m_indexFront + 1) % m_data.size();
-
-        --m_size;
-
-        return true;
+        else {
+            m_index = (m_index + 1) % m_capacity;
+            --m_size;
+            return true;
+        }
     }
 
     /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
@@ -54,58 +54,48 @@ public:
         if (isEmpty()) {
             return false;
         }
-
-         m_indexRear = (m_indexRear + m_data.size() - 1) % m_data.size();
-
-        --m_size;
-
-        return true;
+        else {
+            --m_size;
+            return true;
+        }
     }
 
     /** Get the front item from the deque. */
-    int getFront() const {
-        if (isEmpty()) {
-            return -1;
-        }
-
-        return m_data[(m_indexFront + 1) % m_data.size()];
+    int getFront() {
+        return (isEmpty() ? -1 : m_data[m_index]);
     }
 
     /** Get the last item from the deque. */
-    int getRear() const {
-        if (isEmpty()) {
-            return -1;
-        }
-
-        return m_data[(m_indexRear + m_data.size() - 1) % m_data.size()];
+    int getRear() {
+        return (isEmpty() ? -1 : m_data[(m_index + m_size - 1) % m_capacity]);
     }
 
     /** Checks whether the circular deque is empty or not. */
-    bool isEmpty() const {
+    bool isEmpty() {
         return (m_size == 0);
     }
 
     /** Checks whether the circular deque is full or not. */
-    bool isFull() const {
-        return (m_size == m_data.size());
+    bool isFull() {
+        return (m_size == m_capacity);
     }
 
 private:
-    vector<int> m_data;
     size_t m_size;
-    size_t m_indexFront;
-    size_t m_indexRear;
+    size_t m_capacity;
+    size_t m_index;
+    int* m_data;
 };
 
 /**
  * Your MyCircularDeque object will be instantiated and called as such:
- * MyCircularDeque obj = new MyCircularDeque(k);
- * bool param_1 = obj.insertFront(value);
- * bool param_2 = obj.insertLast(value);
- * bool param_3 = obj.deleteFront();
- * bool param_4 = obj.deleteLast();
- * int param_5 = obj.getFront();
- * int param_6 = obj.getRear();
- * bool param_7 = obj.isEmpty();
- * bool param_8 = obj.isFull();
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
  */
