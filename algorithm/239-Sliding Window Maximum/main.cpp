764e6d6f8b71de48d6ce28de9c32556b5d96f198
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-28 20:32:52 +0800

    239: by MaxQueue

--- algorithm/239-Sliding Window Maximum/main.cpp
+++ algorithm/239-Sliding Window Maximum/main.cpp
@@ -1,57 +1,156 @@
-class MonoQueue {
+template <typename T>
+class MaxStack {
 public:
-    MonoQueue() {
+    MaxStack() {
     }
 
-    void push(int val) {
-        size_t count = 0;
-        while (!m_deque.empty() && m_deque.back().first < val) {
-            count += m_deque.back().second + 1;
-            m_deque.pop_back();
-        }
+    void push(const T& val) {
+        m_data.push_back(val);
 
-        m_deque.emplace_back(val, count);
+        if (m_max.empty() || m_max.back() <= val) {
+            m_max.push_back(val);
+        }
+    }
+
+    template <class... Args>
+    void emplace(Args&&... args) {
+        m_data.emplace_back(args...);
+
+        if (m_max.empty() || m_max.back() <= m_data.back()) {
+            m_max.push_back(m_data.back());
+        }
     }
 
     void pop() {
-        if (0 < m_deque.front().second) {
-            --(m_deque.front().second);
-            return;
+        if (m_data.back() == max()) {
+            m_max.pop_back();
         }
 
-        m_deque.pop_front();
+        m_data.pop_back();
     }
 
-    int max() {
-        return m_deque.front().first;
+    const T& top() {
+        return m_data.back();
+    }
+
+    const T& top() const {
+        return const_cast<MaxStack<T>*>(this)->top();
+    }
+
+    const T& max() {
+        return m_max.back();
+    }
+
+    const T& max() const {
+        return const_cast<MaxStack<T>*>(this)->max();
+    }
+
+    bool empty() const {
+        return m_data.empty();
+    }
+
+    size_t size() const {
+        return m_data.size();
     }
 
 private:
-    deque<pair<int, size_t>> m_deque;
+    std::vector<T> m_data;
+    std::vector<T> m_max;
+};
+
+template <typename T>
+class MaxQueue {
+public:
+    MaxQueue () {
+    }
+
+    void push(const T& v) {
+        m_stkI.push(v);
+    }
+
+    template <class... Args>
+    void emplace (Args&&... args) {
+        m_stkI.emplace(args...);
+    }
+
+    void pop() {
+        if (m_stkO.empty()) {
+            while (!m_stkI.empty()) {
+                m_stkO.push(m_stkI.top());
+                m_stkI.pop();
+            }
+        }
+
+        m_stkO.pop();
+    }
+
+    const T& front() {
+        if (m_stkO.empty()) {
+            while (!m_stkI.empty()) {
+                m_stkO.push(m_stkI.top());
+                m_stkI.pop();
+            }
+        }
+
+        return m_stkO.top();
+    }
+
+    const T& front() const {
+        return const_cast<MaxStack<T>*>(this)->front();
+    }
+
+    const T& max() {
+        if (m_stkI.empty() && m_stkO.empty()) {
+            throw std::domain_error(std::string("empty"));
+        }
+        else if (!m_stkI.empty() && m_stkO.empty()) {
+            return m_stkI.max();
+        }
+        else if (m_stkI.empty() && !m_stkO.empty()) {
+            return m_stkO.max();
+        }
+        else {
+            return std::max(m_stkI.max(), m_stkO.max());
+        }
+    }
+
+    const T& max() const {
+        return const_cast<MaxStack<T>*>(this)->max();
+    }
+
+    bool empty() const {
+        return (m_stkI.empty() && m_stkO.empty());
+    }
+
+    size_t size() const {
+        return (m_stkI.size() + m_stkO.size());
+    }
+
+private:
+    MaxStack<T> m_stkI;
+    MaxStack<T> m_stkO;
 };
 
 class Solution {
 public:
-    vector<int> maxSlidingWindowMonoQueue(vector<int>& nums, size_t k) {
-        size_t i;
-        MonoQueue mq;
-        for (i = 0; i + 1 < k; ++i) {
-            mq.push(nums[i]);
-        }
-
-        vector<int> ans;
-        for (; i < nums.size(); ++i) {
-            mq.push(nums[i]);
-            ans.emplace_back(mq.max());
-            mq.pop();
-        }
-
-        return ans;
-    }
-
     vector<int> maxSlidingWindow(vector<int>& nums, size_t k) {
         assert(k <= nums.size());
 
-        return maxSlidingWindowMonoQueue(nums, k);
+        vector<int> ans;
+        ans.reserve(nums.size() - k + 1);
+
+        MaxQueue<int> mq;
+        for (size_t i = 0; i < k; ++i) {
+            mq.push(nums[i]);
+        }
+        ans.emplace_back(mq.max());
+
+        for (size_t i = k; i < nums.size(); ++i) {
+            mq.push(nums[i]);
+            mq.pop();
+            ans.emplace_back(mq.max());
+        }
+
+        return ans;
     }
 };
