420e11745bda73c585c1677e73d87b555107b934
coolyuyuyu <coolyuyuyu@gmail.com>
2021-02-28 21:22:29 +0800

    071: update implementation

--- algorithm/071-Simplify Path/main.cpp
+++ algorithm/071-Simplify Path/main.cpp
@@ -1,54 +1,30 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string> names;
-
-        string name;
-        for (size_t i = 0; i < path.size(); ++i) {
-            char c = path[i];
-            switch (c) {
-            case '/':
-                if (name.empty()) {
+        vector<string> tokens;
+        istringstream iss(path);
+        for (string token; getline(iss, token, '/');) {
+            if (token == "..") {
+                if (!tokens.empty()) {
+                    tokens.pop_back();
                 }
-                else if (name == ".") {
-                }
-                else if (name == "..") {
-                    if (!names.empty()) {
-                        names.pop_back();
-                    }
-                }
-                else {
-                    names.push_back(name);
-                }
-                name.clear();
-                break;
-            default:
-                name.push_back(c);
-                break;
             }
-        }
-
-        if (name.empty()) {
-        }
-        else if (name == ".") {
-        }
-        else if (name == "..") {
-            if (!names.empty()) {
-                names.pop_back();
+            else if (token == ".") {
             }
-        }
-        else {
-            names.push_back(name);
+            else if (token == "") {
+            }
+            else {
+                tokens.push_back(token);
+            }
         }
 
         ostringstream oss;
-        if (names.empty()) {
+        if (tokens.empty()) {
             oss << "/";
         }
         else {
-            for (size_t i = 0; i < names.size(); ++i) {
-                oss << "/";
-                oss << names[i];
+            for (const string& token : tokens) {
+                oss << "/" << token;
             }
         }
 
