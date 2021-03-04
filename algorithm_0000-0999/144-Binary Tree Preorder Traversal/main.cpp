a6b485f498685ed0c7a9fea8de6c91cbcd3333d7
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-04 22:06:04 +0800

    144: update implementation

--- algorithm_0000-0999/144-Binary Tree Preorder Traversal/main.cpp
+++ algorithm_0000-0999/144-Binary Tree Preorder Traversal/main.cpp
@@ -4,47 +4,49 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void preorderTraversalIter(TreeNode* root, vector<int>& vals) {
+    void preorderTraversal_Recursive(TreeNode* root, vector<int>& vals) {
+        if (!root) {
+            return;
+        }
+
+        vals.push_back(root->val);
+        preorderTraversal_Recursive(root->left, vals);
+        preorderTraversal_Recursive(root->right, vals);
+    }
+
+    void preorderTraversal_Iterative(TreeNode* root, vector<int>& vals) {
         stack<TreeNode*> stk;
         if (root) {
             stk.push(root);
         }
 
         while (!stk.empty()) {
-            TreeNode* node = stk.top();
+            root = stk.top();
             stk.pop();
 
-            vals.push_back(node->val);
+            vals.push_back(root->val);
 
-            if (node->right) {
-                stk.push(node->right);
+            if (root->right) {
+                stk.push(root->right);
             }
-            if (node->left) {
-                stk.push(node->left);
+            if (root->left) {
+                stk.push(root->left);
             }
         }
-    }
-
-    void preorderTraversalRecv(TreeNode* root, vector<int>& vals) {
-        if (!root) {
-            return;
-        }
-
-        vals.push_back(root->val);
-        preorderTraversalRecv(root->left, vals);
-        preorderTraversalRecv(root->right, vals);
     }
 
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> vals;
 
-        preorderTraversalIter(root, vals);
-        //preorderTraversalRecv(root, vals);
+        //preorderTraversal_Recursive(root, vals);
+        preorderTraversal_Iterative(root, vals);
 
         return vals;
     }
