7893bb63b0fe3fd1ab26df75242ad56ead36b497
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-06 23:52:30 +0800

    897: recursive approach

--- algorithm_0000-0999/897-Increasing Order Search Tree/main.cpp
+++ algorithm_0000-0999/897-Increasing Order Search Tree/main.cpp
@@ -11,7 +11,21 @@
  */
 class Solution {
 public:
-    TreeNode* increasingBST(TreeNode* root) {
+    void increasingBST_Recursive(TreeNode* root, TreeNode**& ppCur) {
+        if (!root) {
+            return;
+        }
+
+        increasingBST_Recursive(root->left, ppCur);
+
+        *ppCur = root;
+        (*ppCur)->left = nullptr;
+        ppCur = &((*ppCur)->right);
+
+        increasingBST_Recursive(root->right, ppCur);
+    }
+
+    TreeNode* increasingBST_Iterative(TreeNode* root) {
         TreeNode* pHeadNew = nullptr;
         TreeNode** ppHeadNew = &pHeadNew;
 
@@ -35,5 +49,14 @@ public:
         assert(!(*ppHeadNew));
 
         return pHeadNew;
+    }
+
+    TreeNode* increasingBST(TreeNode* root) {
+        //TreeNode* pHeadNew = nullptr;
+        //TreeNode** ppHeadNew = &pHeadNew;
+        //increasingBST_Recursive(root, ppHeadNew);
+        //return pHeadNew;
+
+        return increasingBST_Iterative(root);
     }
 };
