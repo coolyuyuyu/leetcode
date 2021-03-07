e660c7f92d82d529e266c8b4ed748b82d0ab8315
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-07 16:26:00 +0800

    700: recursive and iterative implementation

--- algorithm_0000-0999/700-Search in a Binary Search Tree/main.cpp
+++ algorithm_0000-0999/700-Search in a Binary Search Tree/main.cpp
@@ -4,24 +4,41 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        while (root) {
-            if (root->val == val) {
-                return root;
-            }
-            else if (root->val < val) {
-                root = root->right;
+    TreeNode* searchBST_Recursive(TreeNode* root, int val) {
+        if (!root || root->val == val) {
+            return root;
+        }
+        else if (val < root->val) {
+            return searchBST_Recursive(root->left, val);
+        }
+        else {
+            return searchBST_Recursive(root->right, val);
+        }
+    }
+
+    TreeNode* searchBST_Iterative(TreeNode* root, int val) {
+        while (root && root->val != val) {
+            if (val < root->val) {
+                root = root->left;
             }
             else {
-                root = root->left;
+                root = root->right;
             }
         }
 
-        return nullptr;
+        return root;
+    }
+
+    TreeNode* searchBST(TreeNode* root, int val) {
+        //return searchBST_Recursive(root, val);
+
+        return searchBST_Iterative(root, val);
     }
 };
