46b81b37bc06a3a60500da4a7963b7493dfc0e37
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-07 17:04:24 +0800

    783: recursive and iterative implementation

--- algorithm_0000-0999/783-Minimum Distance Between BST Nodes/main.cpp
+++ algorithm_0000-0999/783-Minimum Distance Between BST Nodes/main.cpp
@@ -4,34 +4,69 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int minDiffInBST(TreeNode* root) {
+    int minDiffInBST_Recursive(TreeNode* root, int*& pMinVal, int*& pMaxVal) {
+        if (!root) {
+            pMinVal = pMaxVal = nullptr;
+            return numeric_limits<int>::max();
+        }
+
+        int *pLftMinVal, *pLftMaxVal;
+        int minDiffLft = minDiffInBST_Recursive(root->left, pLftMinVal, pLftMaxVal);
+        pMinVal = pLftMinVal ? pLftMinVal : &(root->val);
+
+        int *pRhtMinVal, *pRhtMaxVal;
+        int minDiffRht = minDiffInBST_Recursive(root->right, pRhtMinVal, pRhtMaxVal);
+        pMaxVal = pRhtMaxVal ? pRhtMaxVal : &(root->val);
+
+        int minDiff = min(minDiffLft, minDiffRht);
+        if (pLftMaxVal) {
+            minDiff = min(minDiff, root->val - *pLftMaxVal);
+        }
+        if (pRhtMaxVal) {
+            minDiff = min(minDiff, *pRhtMinVal - root->val);
+        }
+
+        return minDiff;
+    }
+
+    int minDiffInBST_Iterative(TreeNode* root) {
+        int preVal = numeric_limits<int>::min();
         int minDiff = numeric_limits<int>::max();
-        TreeNode* pre = NULL;
-        stack<TreeNode*> nodes;
-        TreeNode* cur = root;
-        while (cur || !nodes.empty()) {
-            if (cur) {
-                nodes.push(cur);
-                cur = cur->left;
+
+        stack<TreeNode*> stk;
+        while (root || !stk.empty()) {
+            if (root) {
+                stk.push(root);
+                root = root->left;
             }
             else {
-                cur = nodes.top();
-                nodes.pop();
+                root = stk.top();
+                stk.pop();
 
-                if (pre) {
-                    minDiff = min(minDiff, abs(cur->val - pre->val));
+                int diff = (preVal == numeric_limits<int>::min() ? numeric_limits<int>::max() : (root->val - preVal));
+                if (diff < minDiff) {
+                    minDiff = diff;
                 }
-                pre = cur;
+                preVal = root->val;
 
-                cur = cur->right;
+                root = root->right;
             }
         }
 
         return minDiff;
+    }
+
+    int minDiffInBST(TreeNode* root) {
+        //int *pMinVal, *pMaxVal;
+        //return minDiffInBST_Recursive(root, pMinVal, pMaxVal);
+
+        return minDiffInBST_Iterative(root);
     }
 };
