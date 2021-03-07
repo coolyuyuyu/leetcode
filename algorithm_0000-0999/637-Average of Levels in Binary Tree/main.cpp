7b1ec8b6daacf8240ea2ad2f122614f4e7b4bf77
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-07 18:15:44 +0800

    637: recursive and iterative implementation

--- algorithm_0000-0999/637-Average of Levels in Binary Tree/main.cpp
+++ algorithm_0000-0999/637-Average of Levels in Binary Tree/main.cpp
@@ -4,37 +4,68 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        queue<TreeNode*> nodes;
-        if (root) {
-            nodes.push(root);
+    void averageOfLevels_Recursive(TreeNode* root, int depth, vector<double>& sums, vector<size_t>& cnts) {
+        if (!root) {
+            return;
         }
 
-        vector<double> averages;
-        while (!nodes.empty()) {
+        assert(sums.size() == cnts.size());
+        if (sums.size() <= depth) {
+            sums.resize(depth + 1, 0.0);
+            cnts.resize(depth + 1, 0);
+        }
+        sums[depth] += root->val;
+        cnts[depth] += 1;
+
+        averageOfLevels_Recursive(root->left, depth + 1, sums, cnts);
+        averageOfLevels_Recursive(root->right, depth + 1, sums, cnts);
+    }
+
+    vector<double> averageOfLevels_Iterative(TreeNode* root) {
+        vector<double> avgs;
+
+        queue<TreeNode*> q;
+        if (root) {
+            q.push(root);
+        }
+        while (!q.empty()) {
             double sum = 0;
-            size_t n = nodes.size();
+            size_t n = q.size();
             for (size_t i = 0; i < n; ++i) {
-                TreeNode* node = nodes.front();
-                nodes.pop();
+                root = q.front();
+                q.pop();
 
-                sum += node->val;
+                sum += root->val;
 
-                if (node->left) {
-                    nodes.push(node->left);
+                if (root->left) {
+                    q.push(root->left);
                 }
-                if (node->right) {
-                    nodes.push(node->right);
+                if (root->right) {
+                    q.push(root->right);
                 }
             }
-            averages.push_back(sum / n);
+            avgs.push_back(sum / n);
         }
 
-        return averages;
+        return avgs;
+    }
+
+    vector<double> averageOfLevels(TreeNode* root) {
+        //vector<double> sums;
+        //vector<size_t> cnts;
+        //averageOfLevels_Recursive(root, 0, sums, cnts);
+        //for (size_t i = 0; i < sums.size(); ++i) {
+        //    sums[i] /= cnts[i];
+        //}
+        //return sums;
+
+        return averageOfLevels_Iterative(root);
     }
 };
