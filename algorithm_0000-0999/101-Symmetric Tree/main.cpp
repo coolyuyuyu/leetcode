48a58bae0b9e7432163529f48a325e22e8447080
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-09 00:05:15 +0800

    101: recursive and iterative approach

--- algorithm_0000-0999/101-Symmetric Tree/main.cpp
+++ algorithm_0000-0999/101-Symmetric Tree/main.cpp
@@ -4,39 +4,68 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        if (!root)
-            return true;
+    bool isSymmetric_Recursive(TreeNode* root) {
+        std::function<bool(TreeNode*, TreeNode*)> compare = [&compare](TreeNode* p, TreeNode* q) -> bool {
+            if ((p && !q) || (!p && q)) {
+                return false;
+            }
 
-        return isSymmetricHelper(root->left, root->right);
+            return (!p && !q) || ((p->val == q->val) && compare(p->left, q->right) && compare(p->right, q->left));
+        };
+
+        return !root || compare(root->left, root->right);
     }
 
-    bool isSymmetricHelper(TreeNode* lft, TreeNode* rht) {
-        if (lft) {
-            if (rht) {
-                if (lft->val == rht->val) {
-                    return isSymmetricHelper(lft->left, rht->right) && isSymmetricHelper(lft->right, rht->left);
-                }
-                else {
-                    return false;
-                }
-            }
-            else {
+    bool isSymmetric_Iterative(TreeNode* root) {
+        assert(root);
+        TreeNode *p = root->left, *q = root->right;
+        if ((p && !q) || (!p && q)) {
+            return false;
+        }
+        assert((p && q) || (!p && !q));
+
+        stack<pair<TreeNode*, TreeNode*>> stk;
+        if (p) {
+            stk.emplace(p, q);
+        }
+        while (!stk.empty()) {
+            p = stk.top().first;
+            q = stk.top().second;
+            stk.pop();
+
+            if (p->val != q->val) {
                 return false;
             }
-        }
-        else {
-            if (rht) {
+            if ((p->left && !q->right) || (!p->left && q->right)) {
                 return false;
             }
-            else {
-                return true;
+            if ((p->right && !q->left) || (!p->right && q->left)) {
+                return false;
+            }
+
+            assert(p->left && q->right || (!p->left && !q->right));
+            if (p->left) {
+                stk.emplace(p->left, q->right);
+            }
+
+            assert(p->right && q->left || (!p->right && !q->left));
+            if (p->right) {
+                stk.emplace(p->right, q->left);
             }
         }
+
+        return true;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        //return isSymmetric_Recursive(root);
+        return isSymmetric_Iterative(root);
     }
 };
