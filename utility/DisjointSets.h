e64e284bd359a30d90b37486821f6aa648718306
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-09 00:47:25 +0800

    DisjointSets: update prototype

--- utility/DisjointSets.h
+++ utility/DisjointSets.h
@@ -3,11 +3,13 @@ class DisjointSets {
 public:
     DisjointSets() {
     }
-    DisjointSets(std::initializer_lis)
-    
-    void make_set(int elem);
-    void merge(int elem1, int elem2) // link?
-    size_t size();
+    DisjointSets(std::initializer_list<T> list)
+    void clear();
+    void make_set(const T& elem);
+    void merge(const T& elem1, const T& elem2) // link?
+    const T root(const T& elem) const
+    size_t size() const;
     
 private:
+    std::map<T, T> m_map;
 };
