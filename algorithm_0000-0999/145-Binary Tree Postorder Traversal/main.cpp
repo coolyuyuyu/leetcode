d9c3e04b960567c866790d515446f04884cc6a5d
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-09 23:19:20 +0800

    145: another iterative implementation

--- algorithm_0000-0999/145-Binary Tree Postorder Traversal/main.cpp
+++ algorithm_0000-0999/145-Binary Tree Postorder Traversal/main.cpp
@@ -21,7 +21,7 @@ public:
         vals.push_back(root->val);
     }
 
-    void postorderTraversal_Iterative(TreeNode* root, vector<int>& vals) {
+    void postorderTraversal_Iterative1(TreeNode* root, vector<int>& vals) {
         stack<TreeNode*> stk;
         if (root) {
             stk.push(root);
@@ -43,11 +43,39 @@ public:
         reverse(vals.begin(), vals.end());
     }
 
+    void postorderTraversal_Iterative2(TreeNode* root, vector<int>& vals) {
+        stack<pair<TreeNode*, bool>> stk;
+        if (root) {
+            stk.emplace(root, false);
+        }
+
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                vals.push_back(node->val);
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
+    }
+
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> vals;
 
         //postorderTraversal_Recursive(root, vals);
-        postorderTraversal_Iterative(root, vals);
+        //postorderTraversal_Iterative1(root, vals);
+        postorderTraversal_Iterative2(root, vals);
+
 
         return vals;
     }
