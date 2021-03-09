7d35b45da3844b3ec3916f140a19edbf69a3fb7b
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-09 23:18:09 +0800

    094: another iterative implementation

--- algorithm_0000-0999/094-Binary Tree Inorder Traversal/main.cpp
+++ algorithm_0000-0999/094-Binary Tree Inorder Traversal/main.cpp
@@ -21,7 +21,7 @@ public:
         inorderTraversal_Recursive(root->right, vals);
     }
 
-    void inorderTraversal_Iterative(TreeNode* root, vector<int>& vals) {
+    void inorderTraversal_Iterative1(TreeNode* root, vector<int>& vals) {
         stack<TreeNode*> stk;
         while (root || !stk.empty()) {
             if (root) {
@@ -40,11 +40,38 @@ public:
         }
     }
 
+    void inorderTraversal_Iterative2(TreeNode* root, vector<int>& vals) {
+        stack<pair<TreeNode*, bool>> stk;
+        if (root) {
+            stk.emplace(root, false);
+        }
+
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                vals.push_back(node->val);
+            }
+            else {
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                stk.emplace(node, true);
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
+    }
+
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> vals;
 
         //inorderTraversal_Recursive(root, vals);
-        inorderTraversal_Iterative(root, vals);
+        //inorderTraversal_Iterative1(root, vals);
+        inorderTraversal_Iterative2(root, vals);
 
         return vals;
     }
