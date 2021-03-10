dfe13e36ec68b86147274c9fe51b33d2cf319c0b
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-11 00:37:06 +0800

    404: recursive and iterative approach

--- algorithm_0000-0999/404-Sum of Left Leaves/main.cpp
+++ algorithm_0000-0999/404-Sum of Left Leaves/main.cpp
@@ -4,51 +4,63 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        struct State {
-            State(TreeNode* n, bool b)
-                : node(n)
-                , leftChild(b)
-            {}
-            TreeNode* node;
-            bool leftChild;
-        };
-
-        unordered_set<int> results;
-        stack<State> stk;
-        if (root) {
-            stk.push(State(root, false));
+    int sumOfLeftLeaves_Recursive(TreeNode* root, bool leftChildOfParent = false) {
+        if (!root) {
+            return 0;
         }
 
         int sum = 0;
+
+        sum += sumOfLeftLeaves_Recursive(root->left, true);
+        if (!root->left && !root->right && leftChildOfParent) {
+            sum += root->val;
+        }
+        sum += sumOfLeftLeaves_Recursive(root->right, false);
+
+        return sum;
+    }
+
+    int sumOfLeftLeaves_Iterative(TreeNode* root) {
+        int sum = 0;
+
+        stack<pair<pair<TreeNode*, bool>, bool>> stk; // <<node, leftChildOfParent>, visited>
+        if (root) {
+            stk.emplace(make_pair(root, false), false);
+        }
         while (!stk.empty()) {
-            State state = stk.top();
+            TreeNode* node = stk.top().first.first;
+            bool leftChildOfParent = stk.top().first.second;
+            bool visited = stk.top().second;
             stk.pop();
 
-            if (state.node->left) {
-                stk.push(State(state.node->left, true));
-                if (state.node->right) {
-                    stk.push(State(state.node->right, false));
+            if (visited) {
+                if (!node->left && !node->right && leftChildOfParent) {
+                    sum += node->val;
                 }
             }
             else {
-                if (state.node->right) {
-                    stk.push(State(state.node->right, false));
+                if (node->right) {
+                    stk.emplace(make_pair(node->right, false), false);
                 }
-                else {
-                    if (state.leftChild) {
-                        sum += state.node->val;
-                    }
+                stk.emplace(make_pair(node, leftChildOfParent), true);
+                if (node->left) {
+                    stk.emplace(make_pair(node->left, true), false);
                 }
-
             }
         }
 
         return sum;
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        //return sumOfLeftLeaves_Recursive(root);
+        return sumOfLeftLeaves_Iterative(root);
     }
 };
