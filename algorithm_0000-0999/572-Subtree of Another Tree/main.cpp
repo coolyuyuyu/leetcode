9e2e6c3094813afa9a2af17125b5d195b814e448
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-11 01:25:54 +0800

    572: recursive and iterative approach

--- algorithm_0000-0999/572-Subtree of Another Tree/main.cpp
+++ algorithm_0000-0999/572-Subtree of Another Tree/main.cpp
@@ -4,41 +4,86 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isIdentical(TreeNode* s, TreeNode* t) {
-        if (s) {
-            if (t) {
-                return s->val == t->val && isIdentical(s->left, t->left) && isIdentical(s->right, t->right);
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            if (t) {
-                return false;
-            }
-            else {
-                return true;
-            }
-        }
-    }
+    bool isIdentical_Recursive(TreeNode* s, TreeNode* t) {
+        return (s == t) || (s && t && s->val == t->val && isIdentical_Recursive(s->left, t->left) && isIdentical_Recursive(s->right, t->right));
+    };
 
-    bool isSubtree(TreeNode* s, TreeNode* t) {
-        if (isIdentical(s, t)) {
+    bool isSubtree_Recursive(TreeNode* s, TreeNode* t) {
+        if (isIdentical_Recursive(s, t)) {
             return true;
         }
         else {
             if (s) {
-                return isSubtree(s->left, t) || isSubtree(s->right, t);
+                return (isSubtree_Recursive(s->left, t) || isSubtree_Recursive(s->right, t));
             }
             else {
+                assert(!s && t);
                 return false;
             }
         }
+    }
+
+    bool isIdentical_Iterative(TreeNode* s, TreeNode* t) {
+        queue<pair<TreeNode*, TreeNode*>> q({{s, t}});
+        while (!q.empty()) {
+            s = q.front().first;
+            t = q.front().second;
+            q.pop();
+
+            if (s != t) {
+                if (s && t) {
+                    if (s->val != t->val) {
+                        return false;
+                    }
+                    q.emplace(s->left, t->left);
+                    q.emplace(s->right, t->right);
+                }
+                else if ((s && !t) || (!s && t)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool isSubtree_Iterative(TreeNode* s, TreeNode* t) {
+        if (isIdentical_Iterative(s, t)) {
+            return true;
+        }
+
+        stack<TreeNode*> stk;
+        if (s) {
+            stk.push(s);
+        }
+        while (!stk.empty()) {
+            s = stk.top();
+            stk.pop();
+
+            if (isIdentical_Iterative(s, t)) {
+                return true;
+            }
+
+            if (s->right) {
+                stk.push(s->right);
+            }
+            if (s->left) {
+                stk.push(s->left);
+            }
+        }
+
+        return false;
+    }
+
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        //return isSubtree_Recursive(s, t);
+        return isSubtree_Iterative(s, t);
     }
 };
