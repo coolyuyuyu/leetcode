7f50da75d8af96683bcd4875c970669252e484eb
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-11 00:37:47 +0800

    530: recursive and iterative approach

--- algorithm_0000-0999/530-Minimum Absolute Difference in BST/main.cpp
+++ algorithm_0000-0999/530-Minimum Absolute Difference in BST/main.cpp
@@ -4,48 +4,75 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int getMinimumDifferenceHelper(TreeNode* root, int& minVal, int& maxVal) {
-        assert(root);
-        int lftMinDiff, rhtMinDiff = INT_MAX;
-        int minDiff = INT_MAX;
-        if (root->left) {
-            int lftMinVal, lftMaxVal;
-            lftMinDiff = getMinimumDifferenceHelper(root->left, lftMinVal, lftMaxVal);
+    void getMinimumDifference_Recursive(TreeNode* root, int& minDiff, int*& pPreVal) {
+        if (!root) {
+            return;
+        }
 
-            minVal = lftMinVal;
-            minDiff = min(minDiff, lftMinDiff);
-            minDiff = min(minDiff, root->val - lftMaxVal);
+        getMinimumDifference_Recursive(root->left, minDiff, pPreVal);
+
+        if (pPreVal) {
+            int diff = root->val - *pPreVal;
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
         }
         else {
-            minVal = root->val;
+            minDiff = numeric_limits<int>::max();
         }
+        pPreVal = &(root->val);
 
-        if (root->right) {
-            int rhtMinVal, rhtMaxVal;
-            rhtMinDiff = getMinimumDifferenceHelper(root->right, rhtMinVal, rhtMaxVal);
-
-            maxVal = rhtMaxVal;
-            minDiff = min(minDiff, rhtMinDiff);
-            minDiff = min(minDiff, rhtMinVal - root->val);
-        }
-        else {
-            maxVal = root->val;
-        }
-
-        return minDiff;
+        getMinimumDifference_Recursive(root->right, minDiff, pPreVal);
     }
-    
-    int getMinimumDifference(TreeNode* root) {
-        int minDiff = INT_MAX;
+
+    void getMinimumDifference_Iterative(TreeNode* root, int& minDiff, int*& pPreVal) {
+        stack<pair<TreeNode*, bool>> stk;
         if (root) {
-            int dummy;
-            minDiff = getMinimumDifferenceHelper(root, dummy, dummy);
+            stk.emplace(root, false);
         }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                if (pPreVal) {
+                    int diff = node->val - *pPreVal;
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                    }
+                }
+                else {
+                    minDiff = numeric_limits<int>::max();
+                }
+                pPreVal = &(node->val);
+            }
+            else {
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                stk.emplace(node, true);
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        int minDiff;
+        int* pPreVal = nullptr;
+
+        //getMinimumDifference_Recursive(root, minDiff, pPreVal);
+        getMinimumDifference_Iterative(root, minDiff, pPreVal);
+
         return minDiff;
     }
 };
