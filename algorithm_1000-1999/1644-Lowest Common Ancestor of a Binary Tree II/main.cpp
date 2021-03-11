1227fa4970d5058fc6116799b42aa181af20986c
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-11 23:49:49 +0800

    1644: iterative approach

---
+++ algorithm_1000-1999/1644-Lowest Common Ancestor of a Binary Tree II/main.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor_Recursive(TreeNode* root, TreeNode* p, TreeNode* q, int& count) {
+        if (!root) {
+            count = 0;
+            return root;
+        }
+
+        int lftCount;
+        TreeNode* lftLCA = lowestCommonAncestor_Recursive(root->left, p, q, lftCount);
+        if (lftCount == 2) {
+            count = 2;
+            return lftLCA;
+        }
+
+        int rhtCount;
+        TreeNode* rhtLCA = lowestCommonAncestor_Recursive(root->right, p, q, rhtCount);
+        if (rhtCount == 2) {
+            count = 2;
+            return rhtLCA;
+        }
+
+        count = lftCount + rhtCount;
+        count += (root == p ? 1 : 0);
+        count += (root == q ? 1 : 0);
+        return root;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int count;
+        TreeNode* lca = lowestCommonAncestor_Recursive(root, p, q, count);
+        return (count == 2 ? lca : nullptr);
+    }
+};
