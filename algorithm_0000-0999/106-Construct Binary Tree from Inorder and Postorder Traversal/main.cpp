5ec68e4a12c75380317916b920803e0e83fc0ee6
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-12 22:37:35 +0800

    106: update implementation

--- algorithm_0000-0999/106-Construct Binary Tree from Inorder and Postorder Traversal/main.cpp
+++ algorithm_0000-0999/106-Construct Binary Tree from Inorder and Postorder Traversal/main.cpp
@@ -4,36 +4,33 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        assert(inorder.size() == postorder.size());
-        return buildTree(inorder, 0, postorder, 0, inorder.size());
-    }
-
-    TreeNode* buildTree(vector<int>& inorder, size_t inBgn, vector<int>& postorder, size_t postBgn, size_t len) {
-        if (len == 0) {
-            return NULL;
+    TreeNode* buildTree_Recursive(vector<int>& inorder, size_t inBgn, vector<int>& postorder, size_t postBgn, size_t size, map<int, size_t>& m) {
+        if (size == 0) {
+            return nullptr;
         }
 
-        int val = postorder[postBgn + len - 1];
-        size_t inIndex = inBgn;
-        for (size_t inEnd = inBgn + len; inIndex < inEnd; ++inIndex) {
-            if (inorder[inIndex] == val) {
-                break;
-            }
-        }
-
-        size_t lftLen = inIndex - inBgn;
-        size_t rhtLen = len - lftLen - 1;
+        int val = postorder[postBgn + size - 1];
+        size_t lftSize = m[val] - inBgn;
+        size_t rhtSize = size - lftSize - 1;
 
         TreeNode* root = new TreeNode(val);
-        root->left = buildTree(inorder, inBgn, postorder, postBgn, lftLen);
-        root->right = buildTree(inorder, inIndex + 1, postorder, postBgn + lftLen, rhtLen);
-
+        root->left = buildTree_Recursive(inorder, inBgn, postorder, postBgn, lftSize, m);
+        root->right = buildTree_Recursive(inorder, inBgn + lftSize + 1, postorder, postBgn + lftSize, rhtSize, m);
         return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        map<int, size_t> m;
+        for (size_t i = 0; i < inorder.size(); ++i) {
+            m[inorder[i]] = i;
+        }
+        return buildTree_Recursive(inorder, 0, postorder, 0, inorder.size(), m);
     }
 };
