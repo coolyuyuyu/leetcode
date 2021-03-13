d9ab2e05bd51cd5e0e2aa3e690e25c61acce012d
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-13 19:22:56 +0800

    DisjointSets: update signature

--- utility/DisjointSets.h
+++ utility/DisjointSets.h
@@ -7,11 +7,18 @@ public:
     void clear();
     void make_set(const T& elem);
     void merge(const T& elem1, const T& elem2) // link? // return false if already merged
-    const T& root(const T& elem) const
+    bool contain() const;
+    
     size_t size() const;
     
+    set<T> set(const T& elem) const;
+    vector<set<T>> sets(const T& elem) const;
+    
 private:
+    const T& root(const T& elem) const
+
     std::map<T, T> m_map;
+    size_t m_size;
 };
 
 /*
