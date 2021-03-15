84286e1f7faaa1a9d9106dea0688edd841b296d5
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-16 00:24:52 +0800

    106: recursive and iterative implementation

--- algorithm_0000-0999/106-Construct Binary Tree from Inorder and Postorder Traversal/main.cpp
+++ algorithm_0000-0999/106-Construct Binary Tree from Inorder and Postorder Traversal/main.cpp
@@ -26,11 +26,43 @@ public:
         return root;
     }
 
+    TreeNode* buildTree_Iterative(vector<int>& inorder, vector<int>& postorder, map<int, size_t>& m) {
+        TreeNode* pRoot = nullptr;
+
+        stack<pair<TreeNode**, tuple<size_t, size_t, size_t>>> stk; // <root, <inBgn, postBgn, size>>;
+        if (0 < inorder.size()) {
+            stk.emplace(&pRoot, make_tuple(0, 0, inorder.size()));
+        }
+        while (!stk.empty()) {
+            TreeNode** ppNode = stk.top().first;
+            size_t inBgn = get<0>(stk.top().second);
+            size_t postBgn = get<1>(stk.top().second);
+            size_t size = get<2>(stk.top().second);
+            stk.pop();
+
+            int val = postorder[postBgn + size - 1];
+            size_t lftSize = m[val] - inBgn;
+            size_t rhtSize = size - lftSize - 1;
+
+            *ppNode = new TreeNode(val);
+            if (0 < lftSize) {
+                stk.emplace(&((*ppNode)->left), make_tuple(inBgn, postBgn, lftSize));
+            }
+            if (0 < rhtSize) {
+                stk.emplace(&((*ppNode)->right), make_tuple(inBgn + lftSize + 1, postBgn + lftSize, rhtSize));
+            }
+        }
+
+        return pRoot;
+    }
+
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         map<int, size_t> m;
         for (size_t i = 0; i < inorder.size(); ++i) {
             m[inorder[i]] = i;
         }
-        return buildTree_Recursive(inorder, 0, postorder, 0, inorder.size(), m);
+
+        //return buildTree_Recursive(inorder, 0, postorder, 0, inorder.size(), m);
+        return buildTree_Iterative(inorder, postorder, m);
     }
 };
