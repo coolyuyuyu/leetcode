0f6bc897c85f735a4c700f387b94a4fa764bcd64
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-16 00:24:05 +0800

    095: recursive and iterative implementation

--- algorithm_0000-0999/095-Unique Binary Search Trees II/main.cpp
+++ algorithm_0000-0999/095-Unique Binary Search Trees II/main.cpp
@@ -11,6 +11,10 @@
  */
 class Solution {
 public:
+    Solution()
+        : m_treeVecs({{nullptr}}) {
+    }
+
     TreeNode* cloneTree(TreeNode* root, int diff) {
         if (!root) {
             return nullptr;
@@ -18,20 +22,54 @@ public:
         return new TreeNode(root->val + diff, cloneTree(root->left, diff), cloneTree(root->right, diff));
     }
 
-    vector<TreeNode*> generateTrees(int n) {
-        vector<vector<TreeNode*>> trees = {{nullptr}};
-        for (int i = 1; i <= n; ++i) {
-            trees.push_back({});
-            for (int j = 0; j < i; ++j) {
-                for (TreeNode* lft : trees[j]) {
-                    for (TreeNode* rht : trees[i - j - 1]) {
-                        TreeNode* root = new TreeNode(j + 1, lft, cloneTree(rht, j + 1));
-                        trees[i].push_back(root);
-                    }
+    const vector<TreeNode*>& generateTrees_Recursive(int n) {
+        if (n < m_treeVecs.size()) {
+            return m_treeVecs[n];
+        }
+
+        vector<TreeNode*> trees;
+        for (int i = 0; i < n; ++i) {
+            for (TreeNode* lft : generateTrees_Recursive(i)) {
+                for (TreeNode* rht : generateTrees_Recursive(n - i - 1)) {
+                    TreeNode* root = new TreeNode(i + 1, lft, cloneTree(rht, i + 1));
+                    trees.push_back(root);
                 }
             }
         }
+        if (m_treeVecs.size() <= n) {
+            m_treeVecs.resize(n + 1, {});
+        }
+        m_treeVecs[n] = trees;
 
-        return trees[n];
+        return m_treeVecs[n];
     }
+
+    const vector<TreeNode*>& generateTrees_Iterative(int n) {
+        if (n < m_treeVecs.size()) {
+            return m_treeVecs[n];
+        }
+
+        for (int i = m_treeVecs.size(); i <= n; ++i) {
+            vector<TreeNode*> trees;
+            for (int j = 0; j < i; ++j) {
+                for (TreeNode* lft : m_treeVecs[j]) {
+                    for (TreeNode* rht : m_treeVecs[i - j - 1]) {
+                        TreeNode* root = new TreeNode(j + 1, lft, cloneTree(rht, j + 1));
+                        trees.push_back(root);
+                    }
+                }
+            }
+            m_treeVecs.push_back(trees);
+        }
+
+        return m_treeVecs[n];
+    }
+
+    const vector<TreeNode*>& generateTrees(int n) {
+        //return generateTrees_Recursive(n);
+        return generateTrees_Iterative(n);
+    }
+
+private:
+    vector<vector<TreeNode*>> m_treeVecs;
 };
