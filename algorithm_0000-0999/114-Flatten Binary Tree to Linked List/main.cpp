2f451da3d2ad4fc00e9c504d77c803a506ef33ba
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-17 01:44:15 +0800

    114: recursive and iterative implementation

--- algorithm_0000-0999/114-Flatten Binary Tree to Linked List/main.cpp
+++ algorithm_0000-0999/114-Flatten Binary Tree to Linked List/main.cpp
@@ -4,25 +4,47 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        while (root) {
-            TreeNode* lft = root->left;
-            if (lft) {
-                TreeNode* cur = lft;
-                while (cur->right) {
-                    cur = cur->right;
-                }
-                cur->right = root->right;
-                root->left = NULL;
-                root->right = lft;
-            }
-
-            root = root->right;
+    void flatten_Recursive(TreeNode* root) {
+        if (!root) {
+            return;
         }
+
+        flatten_Recursive(root->left);
+        if (root->left) {
+            TreeNode* node = root->left;
+            while (node->right) {
+                node = node->right;
+            }
+            node->right = root->right;
+            root->right = root->left;
+            root->left = nullptr;
+        }
+        flatten_Recursive(root->right);
+    }
+
+    void flatten_Iterative(TreeNode* root) {
+        for (; root; root = root->right) {
+            if (root->left) {
+                TreeNode* node = root->left;
+                while (node->right) {
+                    node = node->right;
+                }
+                node->right = root->right;
+                root->right = root->left;
+                root->left = nullptr;
+            }
+        }
+    }
+
+    void flatten(TreeNode* root) {
+        //flatten_Recursive(root);
+        flatten_Iterative(root);
     }
 };
