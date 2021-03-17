4b7fc3ed55a6d109b1956a15276cd01ae13edd7a
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-17 20:26:00 +0800

    129: recursive and iterative solution

--- algorithm_0000-0999/129-Sum Root to Leaf Numbers/main.cpp
+++ algorithm_0000-0999/129-Sum Root to Leaf Numbers/main.cpp
@@ -4,87 +4,59 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int sumNumbers(TreeNode* root) {
-        return sumNumbersDFS(root);
+    int sumNumbers_Recursive(TreeNode* root, int num = 0) {
+        if (!root) {
+            return 0;
+        }
+
+        num = num * 10 + root->val;
+        if (!root->left && !root->right) {
+            return num;
+        }
+        else {
+            return sumNumbers_Recursive(root->left, num) + sumNumbers_Recursive(root->right, num);
+        }
     }
 
-    int sumNumbersBFS(TreeNode* root) {
-        int sumNums = 0;
+    int sumNumbers_Iterative(TreeNode* root) {
+        int sum = 0;
 
-        int num = 0;
-        TreeNode* cur = root;
-
-        queue<pair<TreeNode*, int>> pairs;
+        // level order traversal
+        queue<pair<TreeNode*, int>> q;
         if (root) {
-            pairs.push(pair<TreeNode*, int>(root, 0));
+            q.emplace(root, 0);
         }
+        while (!q.empty()) {
+            TreeNode* node = q.front().first;
+            int num = q.front().second;
+            q.pop();
 
-        while (!pairs.empty()) {
-            for (size_t n = pairs.size(); n > 0; --n) {
-                pair<TreeNode*, int> p = pairs.front();
-                pairs.pop();
-
-                TreeNode* node = p.first;
-                int num = p.second * 10 + node->val;
-                if (node->left) {
-                    pairs.push(pair<TreeNode*, int>(node->left, num));
-                    if (node->right) {
-                        pairs.push(pair<TreeNode*, int>(node->right, num));
-                    }
-                    else {
-                    }
-                }
-                else {
-                    if (node->right) {
-                        pairs.push(pair<TreeNode*, int>(node->right, num));
-                    }
-                    else {
-                        sumNums += num;
-                    }
-                }
-            }
-        }
-
-        return sumNums;
-    }
-
-    int sumNumbersDFS(TreeNode* root) {
-        int sumNums = 0;
-
-        int num = 0;
-        TreeNode* cur = root;
-
-        stack<pair<TreeNode*, int>> pairs;
-        while (cur || !pairs.empty()) {
-            if (cur) {
-                pairs.push(pair<TreeNode*, int>(cur, num));
-
-                num = num * 10 + cur->val;
-                cur = cur->left;
+            num = num * 10 + node->val;
+            if (!node->left && !node->right) {
+                sum += num;
             }
             else {
-                pair<TreeNode*, int> p = pairs.top();
-                pairs.pop();
-
-
-
-                cur = p.first;
-                num = p.second;
-
-                num = num * 10 + cur->val;
-                if (cur->left == NULL && cur->right == NULL) {
-                    sumNums += num;
+                if (node->left) {
+                    q.emplace(node->left, num);
                 }
-                cur = cur->right;
+                if (node->right) {
+                    q.emplace(node->right, num);
+                }
             }
-
         }
 
-        return sumNums;
+        return sum;
+    }
+
+    int sumNumbers(TreeNode* root) {
+        //return sumNumbers_Recursive(root);
+        return sumNumbers_Iterative(root);
     }
 };
