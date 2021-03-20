0fb0092951317dffaf4a1008e1883e027d18cf21
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-20 18:42:14 +0800

    255:  recursive and iterative solution

--- algorithm_0000-0999/255-Verify Preorder Sequence in Binary Search/main.cpp
+++ algorithm_0000-0999/255-Verify Preorder Sequence in Binary Search/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool verifyPreorderHelper(vector<int>::const_iterator first, vector<int>::const_iterator last) {
+    bool verifyPreorder_Recursive(vector<int>::const_iterator first, vector<int>::const_iterator last) {
         assert(first <= last);
         if (distance(first, last) <= 2) {
             return true;
@@ -16,10 +16,36 @@ public:
         for (; rhtLast != last && root < *rhtLast; ++rhtLast) {
         }
 
-        return rhtLast == last && verifyPreorderHelper(lftFirst, lftLast) && verifyPreorderHelper(rhtFirst, rhtLast);
+        return rhtLast == last && verifyPreorder_Recursive(lftFirst, lftLast) && verifyPreorder_Recursive(rhtFirst, rhtLast);
+    }
+
+    bool verifyPreorder_Iterative(vector<int>& preorder) {
+        int tmp;
+        int* pRootVal = nullptr;
+
+        stack<int> stk;
+        for (int num : preorder) {
+            if (pRootVal && num < *pRootVal) {
+                return false;
+            }
+
+            while (!stk.empty() && stk.top() < num) {
+                if (!pRootVal) {
+                    pRootVal = &tmp;
+                }
+                *pRootVal = stk.top();
+
+                stk.pop();
+            }
+
+            stk.push(num);
+        }
+
+        return true;
     }
 
     bool verifyPreorder(vector<int>& preorder) {
-        return verifyPreorderHelper(preorder.begin(), preorder.end());
+        //return verifyPreorder_Recursive(preorder.begin(), preorder.end());
+        return verifyPreorder_Iterative(preorder);
     }
 };
