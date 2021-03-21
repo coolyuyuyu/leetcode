07b00beadd329d57ea7fb92b15fe832bd9b288a8
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-21 16:30:23 +0800

    337: recursive and iterative implementation

--- algorithm_0000-0999/337-House Robber III/main.cpp
+++ algorithm_0000-0999/337-House Robber III/main.cpp
@@ -4,27 +4,68 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    pair<int, int> robHelper(TreeNode* root) {
+    pair<int, int> rob_Recursibve(TreeNode* root) {
         if (!root) {
-            return {0,0};
+            return {0, 0};
         }
 
-        pair<int, int> vPairLft = robHelper(root->left);
-        pair<int, int> vPairRht = robHelper(root->right);
+        auto lftResult = rob_Recursibve(root->left);
+        auto rhtResult = rob_Recursibve(root->right);
 
-        int noRobbed = max(vPairLft.first, vPairLft.second) + max(vPairRht.first, vPairRht.second);
-        int robbed = root->val + vPairLft.first + vPairRht.first;
-        return {noRobbed, robbed};
+        int robbedN = max(lftResult.first, lftResult.second) + max(rhtResult.first, rhtResult.second);
+        int robbedY = lftResult.first + root->val + rhtResult.first;
+        return {robbedN, robbedY};
+    }
+
+    int rob_Iterative(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+
+        map<TreeNode*, pair<int, int>> m; // <node, <robbedN, robbedY>>
+
+        // postorder traversal
+        stack<pair<TreeNode*, bool>> stk({{root, false}});
+        if (root) {
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                pair<int, int> lftResult = (node->left ? m[node->left] : make_pair(0, 0));
+                pair<int, int> rhtResult = (node->right ? m[node->right] : make_pair(0, 0));
+                int robbedN = max(lftResult.first, lftResult.second) + max(rhtResult.first, rhtResult.second);
+                int robbedY = lftResult.first + node->val + rhtResult.first;
+                m[node] = {robbedN, robbedY};
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
+
+        return max(m[root].first, m[root].second);
     }
 
     int rob(TreeNode* root) {
-         //first:not robed, second:robbed
-        pair<int, int> vPair = robHelper(root);
-        return max(vPair.first, vPair.second);
+        //auto result = rob_Recursibve(root);
+        //return max(result.first, result.second);
+
+        return rob_Iterative(root);
     }
 };
