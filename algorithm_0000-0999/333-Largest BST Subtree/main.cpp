9588f2f8ae3b11b95278b150affe282a39bcdd26
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-21 16:30:12 +0800

    333: recursive and iterative implementation

--- algorithm_0000-0999/333-Largest BST Subtree/main.cpp
+++ algorithm_0000-0999/333-Largest BST Subtree/main.cpp
@@ -4,44 +4,96 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int largestBSTSubtree(TreeNode* root) {
-        int nodeCnt;
-        int minVal, maxVal;
-        int maxNodeCount = 0;
-        largestBSTSubtreeHelper(root, nodeCnt, minVal, maxVal, maxNodeCount);
-        return maxNodeCount;
-    }
-
-    bool largestBSTSubtreeHelper(TreeNode* root, int& nodeCnt, int& minVal, int& maxVal, int& maxNodeCount) {
-        if (root) {
-            int lftNodeCnt;
-            int lftMinVal, lftMaxVal;
-            bool lftBST = largestBSTSubtreeHelper(root->left, lftNodeCnt, lftMinVal, lftMaxVal, maxNodeCount);
-
-            int rhtNodeCnt;
-            int rhtMinVal, rhtMaxVal;
-            bool rhtBST = largestBSTSubtreeHelper(root->right, rhtNodeCnt, rhtMinVal, rhtMaxVal, maxNodeCount);
-
-            if (lftBST && rhtBST) {
-                if ((lftNodeCnt == 0 || lftMaxVal < root->val) && (rhtNodeCnt == 0 || root->val < rhtMinVal)) {
-                    nodeCnt = lftNodeCnt + 1 + rhtNodeCnt;
-                    minVal = (lftNodeCnt == 0 ? root->val : lftMinVal);
-                    maxVal = (rhtNodeCnt == 0 ? root->val : rhtMaxVal);
-                    maxNodeCount = max(maxNodeCount, nodeCnt);
-                    return true;
-                }
-            }
-
-            return false;
-        }
-        else {
-            nodeCnt = 0;
+    bool largestBSTSubtree_Recursive(TreeNode* root, int& numNodes, int& maxNumNodes, int* pMinVal = nullptr, int* pMaxVal = nullptr) {
+        if (!root) {
+            numNodes = 0;
             return true;
         }
+
+        int lftMinVal, lftMaxVal, lftNumNodes;
+        bool lftBST = largestBSTSubtree_Recursive(root->left, lftNumNodes, maxNumNodes, &lftMinVal, &lftMaxVal);
+        int rhtMinVal, rhtMaxVal, rhtNumNodes;
+        bool rhtBST = largestBSTSubtree_Recursive(root->right, rhtNumNodes, maxNumNodes, &rhtMinVal, &rhtMaxVal);
+        if (lftBST && rhtBST && (lftNumNodes == 0 || lftMaxVal < root->val) && (rhtNumNodes == 0 || root->val < rhtMinVal)) {
+            numNodes = lftNumNodes + 1 + rhtNumNodes;
+            if (maxNumNodes < numNodes) {
+                maxNumNodes = numNodes;
+            }
+
+            if (pMinVal) {
+                *pMinVal = (lftNumNodes == 0 ? root->val : lftMinVal);
+            }
+            if (pMaxVal) {
+                *pMaxVal = (rhtNumNodes == 0 ? root->val : rhtMaxVal);
+            }
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    int largestBSTSubtree_Iterative(TreeNode* root) {
+        int maxNumNodes = 0;
+
+        map<TreeNode*, tuple<int, int, int>> m; // <node, <numNodes, minVal, maxVal>>
+        // postorder traversal
+        stack<pair<TreeNode*, bool>> stk; // <node, visited>
+        if (root) {
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                bool lftBST = (node->left ? m.find(node->left) != m.end() : true);
+                bool rhtBST = (node->right ? m.find(node->right) != m.end() : true);
+                if (lftBST && rhtBST) {
+                    int lftNumNodes = (node->left ? get<0>(m[node->left]) : 0);
+                    int rhtNumNodes = (node->right ? get<0>(m[node->right]) : 0);
+                    if ((lftNumNodes == 0 || get<2>(m[node->left]) < node->val) && (rhtNumNodes == 0 || node->val < get<1>(m[node->right]))) {
+                        int numNodes = lftNumNodes + 1 + rhtNumNodes;
+                        if (maxNumNodes < numNodes) {
+                            maxNumNodes = numNodes;
+                        }
+
+                        int minVal = (lftNumNodes == 0 ? node->val : get<1>(m[node->left]));
+                        int maxVal = (rhtNumNodes == 0 ? node->val : get<2>(m[node->right]));
+                        m.emplace(node, make_tuple(numNodes, minVal, maxVal));
+                    }
+                }
+
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
+
+        return maxNumNodes;
+
+    }
+
+    int largestBSTSubtree(TreeNode* root) {
+        //int numNodes, maxNumNodes = 0;
+        //largestBSTSubtree_Recursive(root, numNodes, maxNumNodes);
+        //return maxNumNodes;
+
+        return largestBSTSubtree_Iterative(root);
     }
 };
