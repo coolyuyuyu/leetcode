d29649c6903dbd2891555317b62b4f0623233335
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-21 20:51:52 +0800

    437: recursive and iterative solution

--- algorithm_0000-0999/437-Path Sum III/main.cpp
+++ algorithm_0000-0999/437-Path Sum III/main.cpp
@@ -4,35 +4,84 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int pathSum(TreeNode* root, int sum) {
-        int count = pathSumFrom(root, sum);
-        if (root) {
-            count += pathSum(root->left, sum);
-            count += pathSum(root->right, sum);
+    void pathSum_Recursive(TreeNode* root, int sum, int target, map<int, int>& m, int& numPaths) {
+        if (!root) {
+            return;
         }
-        
-        return count;
+
+        sum += root->val;
+        if (sum == target) {
+            ++numPaths;
+        }
+
+        auto itr = m.find(sum - target);
+        if (itr != m.end()) {
+            numPaths += itr->second;
+        }
+
+        ++m[sum];
+        pathSum_Recursive(root->left, sum, target, m, numPaths);
+        pathSum_Recursive(root->right, sum, target, m, numPaths);
+        --m[sum];
     }
 
+    int pathSum_Iterative(TreeNode* root, int target) {
+        int sum = 0;
+        int numPaths = 0;
+        map<int, int> m;
 
-    int pathSumFrom(TreeNode* root, int sum) {
+        stack<pair<TreeNode*, bool>> stk;
         if (root) {
-            int count = 0;
-            if (root->val == sum) {
-                ++count;
-            }
-            count += pathSumFrom(root->left, sum - root->val);
-            count += pathSumFrom(root->right, sum - root->val);
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
 
-            return count;
+            if (visited) {
+                --m[sum];
+                sum -= node->val;
+            }
+            else {
+                sum += node->val;
+                if (sum == target) {
+                    ++numPaths;
+                }
+
+                auto itr = m.find(sum - target);
+                if (itr != m.end()) {
+                    numPaths += itr->second;
+                }
+
+                ++m[sum];
+
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
         }
-        else {
-            return 0;
-        }
+
+        return numPaths;
+    }
+
+    int pathSum(TreeNode* root, int sum) {
+        //map<int, int> m; // <sum, number of path (from root to some in middle) equal to sum>
+        //int numPaths = 0;
+        //pathSum_Recursive(root, 0, sum, m, numPaths);
+        //return numPaths;
+
+        return pathSum_Iterative(root, sum);
     }
 };
