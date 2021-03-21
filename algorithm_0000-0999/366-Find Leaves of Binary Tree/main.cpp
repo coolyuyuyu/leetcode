037b1e06dab90655e9e3c1b40e2b19d6316a4786
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-21 16:30:33 +0800

    366: recursive and iterative implementation

--- algorithm_0000-0999/366-Find Leaves of Binary Tree/main.cpp
+++ algorithm_0000-0999/366-Find Leaves of Binary Tree/main.cpp
@@ -4,27 +4,68 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root, vector<vector<int>>& ret) {
-        int h = 0;
-        if (root){
-            h = max(height(root->left, ret), height(root->right, ret)) + 1;
-
-            while (ret.size() < h) {
-                ret.push_back(vector<int>());
-            }
-            ret[h - 1].push_back(root->val);
+    int findLeaves_Recursive(TreeNode* root, vector<vector<int>>& ret) {
+        if (!root) {
+            return 0;
         }
-        return h;
+
+        int height = max(findLeaves_Recursive(root->left, ret), findLeaves_Recursive(root->right, ret)) + 1;
+        if (ret.size() <= height) {
+            ret.resize(height);
+        }
+        ret[height - 1].push_back(root->val);
+
+        return height;
+    }
+
+    void findLeaves_Iterative(TreeNode* root, vector<vector<int>>& ret) {
+        map<TreeNode*, int> m; // <node, height>
+
+        // postorder traversal
+        stack<pair<TreeNode*, bool>> stk;
+        if (root) {
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                int height = max((node->left ? m[node->left] : 0), (node->right ? m[node->right] : 0)) + 1;
+
+                if (ret.size() <= height) {
+                    ret.resize(height);
+                }
+                ret[height - 1].push_back(node->val);
+
+                m[node] = height;
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
     }
 
     vector<vector<int>> findLeaves(TreeNode* root) {
         vector<vector<int>> ret;
-        height(root, ret);
+
+        //findLeaves_Recursive(root, ret);
+        findLeaves_Iterative(root, ret);
+
         return ret;
     }
 };
