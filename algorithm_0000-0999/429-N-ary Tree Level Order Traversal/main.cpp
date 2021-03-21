1b39aa66a61af95366230c3c93fe7cdefabe9a3e
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-21 18:04:51 +0800

    429: recursive and iterative solution

--- algorithm_0000-0999/429-N-ary Tree Level Order Traversal/main.cpp
+++ algorithm_0000-0999/429-N-ary Tree Level Order Traversal/main.cpp
@@ -2,10 +2,14 @@
 // Definition for a Node.
 class Node {
 public:
-    int val = NULL;
+    int val;
     vector<Node*> children;
 
     Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
 
     Node(int _val, vector<Node*> _children) {
         val = _val;
@@ -13,33 +17,51 @@ public:
     }
 };
 */
+
 class Solution {
 public:
-    vector<vector<int>> levelOrder(Node* root) {
-        queue<Node*> nodes;
+    void levelOrder_Recursive(Node* root, vector<vector<int>>& ret, int depth = 0) {
+        if (!root) {
+            return;
+        }
+
+        if (ret.size() <= depth) {
+            ret.resize(depth + 1);
+        }
+        ret[depth].push_back(root->val);
+
+        for (Node* child : root->children) {
+            levelOrder_Recursive(child, ret, depth + 1);
+        }
+    }
+
+    void levelOrder_Iterative(Node* root, vector<vector<int>>& ret) {
+        queue<Node*> q;
         if (root) {
-            nodes.push(root);
+            q.push(root);
         }
+        while (!q.empty()) {
+            ret.emplace_back(0);
+            for (size_t i = q.size(); 0 < i; --i) {
+                Node* node = q.front();
+                q.pop();
 
-        vector<vector<int>> rows;
-        while (!nodes.empty()) {
-            rows.emplace_back();
-            vector<int>& row = rows.back();
+                ret.back().push_back(node->val);
 
-            queue<Node*> childs;
-            while (!nodes.empty()) {
-                Node* node = nodes.front();
-                nodes.pop();
-
-                for (size_t i = 0; i < node->children.size(); ++i) {
-                    childs.push(node->children[i]);
+                for (Node* child : node->children) {
+                    q.push(child);
                 }
-
-                row.emplace_back(node->val);
             }
-            nodes.swap(childs);
-        }
 
-        return rows;
+        }
+    }
+
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> ret;
+
+        //levelOrder_Recursive(root, ret);
+        levelOrder_Iterative(root, ret);
+
+        return ret;
     }
 };
