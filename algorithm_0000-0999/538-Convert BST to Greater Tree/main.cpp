b0c53927f83b02b584c9a26d6af87a889518bcda
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 00:11:58 +0800

    538: recursive and iterative solution

--- algorithm_0000-0999/538-Convert BST to Greater Tree/main.cpp
+++ algorithm_0000-0999/538-Convert BST to Greater Tree/main.cpp
@@ -4,23 +4,63 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* convertBST(TreeNode* root) {
-        int sum = 0;
-        travelBSTHelper(root, sum);
-        return root;
+    void convertBST_Recursive(TreeNode* root, int& sum) {
+        if (!root) {
+            return;
+        }
+
+        convertBST_Recursive(root->right, sum);
+
+        int val = root->val;
+        root->val += sum;
+        sum += val;
+
+        convertBST_Recursive(root->left, sum);
     }
 
-    void travelBSTHelper(TreeNode* root, int& sum) {
+    void convertBST_Iterative(TreeNode* root) {
+        int sum = 0;
+
+        // reverse inorder traversal
+        stack<pair<TreeNode*, bool>> stk;
         if (root) {
-            travelBSTHelper(root->right, sum);
-            root->val += sum;
-            sum = root->val;
-            travelBSTHelper(root->left, sum);
+            stk.emplace(root, false);
         }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                int val = node->val;
+                node->val += sum;
+                sum += val;
+            }
+            else {
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+            }
+        }
+    }
+
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        convertBST_Recursive(root, sum);
+
+        //convertBST_Iterative(root);
+
+        return root;
     }
 };
