9a083d571d995770df00f50a8c7e439c6d21b1ae
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 00:11:22 +0800

    508: recursive and iterative solution

--- algorithm_0000-0999/508-Most Frequent Subtree Sum/main.cpp
+++ algorithm_0000-0999/508-Most Frequent Subtree Sum/main.cpp
@@ -4,35 +4,75 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int findFrequentTreeSum(TreeNode* root, unordered_map<int, int>& sumCounts, int& maxSumCount) {
+    int findTreeSums_Recursive(TreeNode* root, unordered_map<int, int>& counts, int& maxCount) {
         if (!root) {
             return 0;
         }
 
-        int sum = root->val + findFrequentTreeSum(root->left, sumCounts, maxSumCount) + findFrequentTreeSum(root->right, sumCounts, maxSumCount);
-        ++sumCounts[sum];
-        maxSumCount = max(maxSumCount, sumCounts[sum]);
-
+        int sum = findTreeSums_Recursive(root->left, counts, maxCount) + root->val + findTreeSums_Recursive(root->right, counts, maxCount);
+        ++counts[sum];
+        if (maxCount < counts[sum]) {
+            maxCount = counts[sum];
+        }
         return sum;
     }
 
-    vector<int> findFrequentTreeSum(TreeNode* root) {
-        unordered_map<int, int> sumCounts;
-        int maxSumCount = 0;
-        findFrequentTreeSum(root, sumCounts, maxSumCount);
+    void findTreeSums_Iterative(TreeNode* root, unordered_map<int, int>& counts, int& maxCount) {
+        map<TreeNode*, int> m; // <node, sum>
 
-        vector<int> sums;
-        for (const pair<int, int>& p : sumCounts) {
-            if (p.second == maxSumCount) {
-                sums.emplace_back(p.first);
+        // postorder traversal
+        stack<pair<TreeNode*, bool>> stk;
+        if (root) {
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                int lftSum = (node->left ? m[node->left] : 0);
+                int rhtSum = (node->right ? m[node->right] : 0);
+                int sum = lftSum + node->val + rhtSum;
+                m[node] = sum;
+
+                ++counts[sum];
+                if (maxCount < counts[sum]) {
+                    maxCount = counts[sum];
+                }
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
             }
         }
+    }
 
-        return sums;
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        unordered_map<int, int> counts;
+        int maxCount = 0;
+
+        //findTreeSums_Recursive(root, counts, maxCount);
+        findTreeSums_Iterative(root, counts, maxCount);
+
+        vector<int> ret;
+        for (const auto& p : counts) {
+            if (p.second == maxCount) {
+                ret.push_back(p.first);
+            }
+        }
+        return ret;
     }
 };
