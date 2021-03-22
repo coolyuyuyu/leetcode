fe1ddc69d5d3ca7552e2bb29ce48d21ca6c602e2
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 00:11:43 +0800

    513: recursive and iterative solution

--- algorithm_0000-0999/513-Find Bottom Left Tree Value/main.cpp
+++ algorithm_0000-0999/513-Find Bottom Left Tree Value/main.cpp
@@ -4,34 +4,54 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
-        int val = root->val;
+    void findBottomLeftValue_Recursive(TreeNode* root, int depth, int& outDepth, int& outVal) {
+        if (!root) {
+            return;
+        }
 
-        queue<TreeNode*> nodes;
-        nodes.push(root);
-        while (!nodes.empty()) {
-            val = nodes.front()->val;
+        if (outDepth < depth) {
+            outDepth = depth;
+            outVal = root->val;
+        }
 
-            for (size_t n = nodes.size(); n > 0; --n) {
-                TreeNode* node = nodes.front();
-                nodes.pop();
+        findBottomLeftValue_Recursive(root->left, depth + 1, outDepth, outVal);
+        findBottomLeftValue_Recursive(root->right, depth + 1, outDepth, outVal);
+    }
 
-                TreeNode* lft = node->left;
-                if (lft) {
-                    nodes.push(lft);
-                }
-                TreeNode* rht = node->right;
-                if (rht) {
-                    nodes.push(rht);
-                }
+    int findBottomLeftValue_Iterative(TreeNode* root) {
+        // reverse level-order traversal
+        queue<TreeNode*> q;
+        if (root) {
+            q.push(root);
+        }
+        while (!q.empty()) {
+            root = q.front();
+            q.pop();
+
+            if (root->right) {
+                q.push(root->right);
+            }
+            if (root->left) {
+                q.push(root->left);
             }
         }
 
-        return val;
+        return root->val;
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        //int outDepth = -1;
+        //int outVal;
+        //findBottomLeftValue_Recursive(root, 0, outDepth, outVal);
+        //return outVal;
+
+        return findBottomLeftValue_Iterative(root);
     }
 };
