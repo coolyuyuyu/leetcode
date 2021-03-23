132216e89c2343f2c94f0623620cd959f09daa6c
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 21:20:51 +0800

    652: recursive and iterative implementation

--- algorithm_0000-0999/652-Find Duplicate Subtrees/main.cpp
+++ algorithm_0000-0999/652-Find Duplicate Subtrees/main.cpp
@@ -4,50 +4,71 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    string findDuplicateSubtreesV1(TreeNode* root, map<string, size_t>& counts, vector<TreeNode*>& duplicates) {
-        if (!root) {
-            return "";
-        }
-
-        ostringstream oss;
-        oss << '(' << findDuplicateSubtreesV1(root->left, counts, duplicates) << ')';
-        oss << root->val;
-        oss << '(' << findDuplicateSubtreesV1(root->right, counts, duplicates) << ')';
-
-        string id = oss.str();
-        ++counts[id];
-        if (counts[id] == 2) {
-            duplicates.emplace_back(root);
-        }
-
-        return id;
-    }
-
-    string findDuplicateSubtreesV2(TreeNode* root, map<string, size_t>& counts, vector<TreeNode*>& duplicates) {
+    string findDuplicateSubtrees_Recursive(TreeNode* root, map<string, int>& counts, vector<TreeNode*>& duplicates) {
         if (!root) {
             return "#";
         }
 
-        string id = to_string(root->val) + "," + findDuplicateSubtreesV2(root->left, counts, duplicates) + ","  + findDuplicateSubtreesV2(root->right, counts, duplicates);
-        ++counts[id];
-        if (counts[id] == 2) {
-            duplicates.emplace_back(root);
+        ostringstream oss;
+        oss << root->val
+            << "," << findDuplicateSubtrees_Recursive(root->left, counts, duplicates)
+            << "," << findDuplicateSubtrees_Recursive(root->right, counts, duplicates);
+        string id = oss.str();
+        if (++counts[id] == 2) {
+            duplicates.push_back(root);
         }
 
         return id;
     }
+    void findDuplicateSubtrees_Iterative(TreeNode* root, map<string, int>& counts, vector<TreeNode*>& duplicates) {
+        map<TreeNode*, string> m; // <tree, id>
+
+        stack<pair<TreeNode*, bool>> stk;
+        if (root) {
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                string id; {
+                    ostringstream oss;
+                    oss << node->val << "," << (node->left ? m[node->left] : "#") << "," << (node->right ? m[node->right] : "#");
+                    id = oss.str();
+                }
+
+                m[node] = id;
+                if (++counts[id] == 2) {
+                    duplicates.push_back(node);
+                }
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+        }
+    }
 
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        map<string, size_t> counts;
+        map<string, int> counts;
         vector<TreeNode*> duplicates;
 
-        //findDuplicateSubtreesV1(root, counts, duplicates);
-        findDuplicateSubtreesV2(root, counts, duplicates);
+        //findDuplicateSubtrees_Recursive(root, counts, duplicates);
+        findDuplicateSubtrees_Iterative(root, counts, duplicates);
 
         return duplicates;
     }
