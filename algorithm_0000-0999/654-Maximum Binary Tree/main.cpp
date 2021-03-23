6d3374f596a95a127f807c697f67068dedfcd05d
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 21:21:04 +0800

    654: : recursive and iterative implementation

--- algorithm_0000-0999/654-Maximum Binary Tree/main.cpp
+++ algorithm_0000-0999/654-Maximum Binary Tree/main.cpp
@@ -4,24 +4,50 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return constructMaximumBinaryTreeHelper(nums.begin(), nums.end());
-    }
-
-    TreeNode* constructMaximumBinaryTreeHelper(vector<int>::const_iterator iterBgn, vector<int>::const_iterator iterEnd) {
-        if (iterBgn == iterEnd) {
-            return NULL;
+    TreeNode* constructMaximumBinaryTree_Recursive(vector<int>::iterator first, vector<int>::iterator last) {
+        assert(first <= last);
+        if (first == last) {
+            return nullptr;
         }
 
-        vector<int>::const_iterator iterMax = max_element(iterBgn, iterEnd);
-        TreeNode* root = new TreeNode(*iterMax);
-        root->left = constructMaximumBinaryTreeHelper(iterBgn, iterMax);
-        root->right = constructMaximumBinaryTreeHelper(iterMax + 1,iterEnd);
+        vector<int>::iterator mid = max_element(first, last);
+        TreeNode* root = new TreeNode(*mid);
+        root->left = constructMaximumBinaryTree_Recursive(first, mid);
+        root->right = constructMaximumBinaryTree_Recursive(mid + 1, last);
         return root;
+    }
+
+    TreeNode* constructMaximumBinaryTree_Iterative(vector<int>::iterator first, vector<int>::iterator last) {
+        TreeNode* pRoot = nullptr;
+
+        stack<pair<TreeNode**, pair<vector<int>::iterator, vector<int>::iterator>>> stk({{&pRoot, make_pair(first, last)}});
+        while (!stk.empty()) {
+            TreeNode** ppNode = stk.top().first;
+            auto range = stk.top().second;
+            stk.pop();
+
+            if (range.second <= range.first) {
+                continue;
+            }
+            auto mid = max_element(range.first, range.second);
+            *ppNode = new TreeNode(*mid);
+
+            stk.emplace(&((*ppNode)->left), make_pair(range.first, mid));
+            stk.emplace(&((*ppNode)->right), make_pair(mid + 1, range.second));
+        }
+
+        return pRoot;
+    }
+
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        //return constructMaximumBinaryTree_Recursive(nums.begin(), nums.end());
+        return constructMaximumBinaryTree_Iterative(nums.begin(), nums.end());
     }
 };
