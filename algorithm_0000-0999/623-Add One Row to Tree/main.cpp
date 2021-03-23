300390884d2e454857e82db69464d6243fd01624
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 21:20:34 +0800

    623: recursive and iterative implementation

--- algorithm_0000-0999/623-Add One Row to Tree/main.cpp
+++ algorithm_0000-0999/623-Add One Row to Tree/main.cpp
@@ -4,48 +4,76 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void insertOneRowRecv(TreeNode* root, int v, int d) {
-        assert(0 <= d);
+    void addOneRow_Recursive(TreeNode* root, int val, int depth) {
         if (!root) {
             return;
         }
 
-        if (d == 0) {
-            TreeNode* lftChild = new TreeNode(v);
-            lftChild->left = root->left;
-            root->left = lftChild;
+        if (--depth == 0) {
+            //cout << "root->val:" << root->val << endl;
+            root->left = new TreeNode(val, root->left, nullptr);
+            root->right = new TreeNode(val, nullptr, root->right);
 
-            TreeNode* rhtChild = new TreeNode(v);
-            rhtChild->right = root->right;
-            root->right = rhtChild;
         }
         else {
-            insertOneRowRecv(root->left, v, d - 1);
-            insertOneRowRecv(root->right, v, d - 1);
+            addOneRow_Recursive(root->left, val, depth);
+            addOneRow_Recursive(root->right, val, depth);
         }
     }
 
-    TreeNode* addOneRowHelper(TreeNode* root, int v, int d) {
-        assert(1 <= d);
+    void addOneRow_Iterative(TreeNode* root, int val, int depth) {
+        if (!root) {
+            return;
+        }
 
-        if (d == 1) {
-            TreeNode* tmp = new TreeNode(v);
-            tmp->left = root;
-            root = tmp;
+        queue<TreeNode*> q;
+        if (root) {
+            q.push(root);
+        }
+        while (0 <--depth && !q.empty()) {
+            for (size_t i = q.size(); 0 < i; --i) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            node->left = new TreeNode(val, node->left, nullptr);
+            node->right = new TreeNode(val, nullptr, node->right);
+        }
+    }
+
+    TreeNode* addOneRow_Helper(TreeNode* root, int val, int depth) {
+        assert(1 <= depth);
+        if (depth == 1) {
+            root = new TreeNode(val, root, nullptr);
         }
         else {
-            insertOneRowRecv(root, v, d - 2);
+            //addOneRow_Recursive(root, val, --depth);
+            addOneRow_Iterative(root, val, --depth);
         }
 
         return root;
     }
 
-    TreeNode* addOneRow(TreeNode* root, int v, int d) {
-        return addOneRowHelper(root, v, d);
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        return addOneRow_Helper(root, val, depth);
     }
 };
