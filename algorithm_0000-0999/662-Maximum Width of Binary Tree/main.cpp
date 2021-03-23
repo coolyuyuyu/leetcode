4ee2f95c25e37312e1b84ae89a1af30751b881a0
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-23 21:21:33 +0800

    662: recursive and iterative implementation

--- algorithm_0000-0999/662-Maximum Width of Binary Tree/main.cpp
+++ algorithm_0000-0999/662-Maximum Width of Binary Tree/main.cpp
@@ -4,45 +4,68 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*, int>> pairs;
-        if (root) {
-            pairs.push(pair<TreeNode*, int>(root, 0));
+    void widthOfBinaryTree_Recursive(TreeNode* root, int depth, size_t index, vector<pair<size_t, size_t>>& ranges, int& maxWidth) {
+        if (!root) {
+            return;
         }
 
+        if (ranges.size() <= depth) {
+            ranges.emplace_back(index, index);
+        }
+        ranges[depth].second = index;
+
+        int width = ranges[depth].second - ranges[depth].first + 1;
+        if (maxWidth < width) {
+            maxWidth = width;
+        }
+
+        widthOfBinaryTree_Recursive(root->left, depth + 1, index * 2, ranges, maxWidth);
+        widthOfBinaryTree_Recursive(root->right, depth + 1, index * 2 + 1, ranges, maxWidth);
+    }
+
+    int widthOfBinaryTree_Iterative(TreeNode* root) {
         int maxWidth = 0;
-        while (!pairs.empty()) {
-            if (pairs.size() == 1) {
-                maxWidth = max(maxWidth, 1);
+
+        queue<pair<TreeNode*, size_t>> q;
+        if (root) {
+            q.emplace(root, 1);
+        }
+        while (!q.empty()) {
+            int width = q.back().second - q.front().second + 1;
+            if (maxWidth < width) {
+                maxWidth = width;
             }
-            else {
-                maxWidth = max(maxWidth, pairs.back().second - pairs.front().second + 1);
-            }
 
-            for (size_t n = pairs.size(); n > 0; --n) {
-                const pair<TreeNode*, int>& p = pairs.front();
-                TreeNode* node = p.first;
-                int index = p.second;
+            for (size_t i = q.size(); 0 < i; --i) {
+                TreeNode* node = q.front().first;
+                size_t index = q.front().second;
+                q.pop();
 
-                TreeNode* lft = node->left;
-                if (lft) {
-                    pairs.push(pair<TreeNode*, int>(lft, index * 2));
+                if (node->left) {
+                    q.emplace(node->left, index * 2);
                 }
-
-                TreeNode* rht = node->right;
-                if (rht) {
-                    pairs.push(pair<TreeNode*, int>(rht, index * 2 + 1));
+                if (node->right) {
+                    q.emplace(node->right, index * 2 + 1);
                 }
-                pairs.pop();
             }
         }
-
 
         return maxWidth;
+    }
+
+    int widthOfBinaryTree(TreeNode* root) {
+        //vector<pair<size_t, size_t>> ranges;
+        //int width = 0;
+        //widthOfBinaryTree_Recursive(root, 0, 1, ranges, width);
+        //return width;
+
+        return widthOfBinaryTree_Iterative(root);
     }
 };
