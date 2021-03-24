3633d40e76078d7d3538b707d5a56e4c1bf64af2
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-25 00:49:32 +0800

    669: recursive and iterative solution

--- algorithm_0000-0999/669-Trim a Binary Search Tree/main.cpp
+++ algorithm_0000-0999/669-Trim a Binary Search Tree/main.cpp
@@ -4,26 +4,68 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* trimBST(TreeNode* root, int L, int R) {
+    TreeNode* trimBST_Recursive(TreeNode* root, int low, int high) {
         if (!root) {
-            return NULL;
+            return nullptr;
+        }
+        if (root->val < low) {
+            return trimBST_Recursive(root->right, low, high);
+        }
+        if (high < root->val) {
+            return trimBST_Recursive(root->left, low, high);
         }
 
-        if (root->val < L) {
-            return trimBST(root->right, L, R);
-        }
-
-        if (root->val > R) {
-            return trimBST(root->left, L, R);
-        }
-
-        root->left = trimBST(root->left, L, R);
-        root->right = trimBST(root->right, L, R);
+        root->left = trimBST_Recursive(root->left, low, root->val);
+        root->right = trimBST_Recursive(root->right, root->val, high);
         return root;
+    }
+
+    TreeNode* trimBST_Iterative(TreeNode* root, int low, int high) {
+        queue<tuple<TreeNode**, int, int>> q; // <root, low, high>;
+        if (root) {
+            q.emplace(&root, low, high);
+        }
+        while (!q.empty()) {
+            TreeNode** ppNode = get<0>(q.front());
+            int low = get<1>(q.front());
+            int high = get<2>(q.front());
+            q.pop();
+
+            if ((*ppNode)->val < low) {
+                cout << (*ppNode)->val << endl;
+                *ppNode = (*ppNode)->right;
+                if (*ppNode) {
+                    q.emplace(ppNode, low, high);
+                }
+            }
+            else if (high < (*ppNode)->val) {
+                *ppNode = (*ppNode)->left;
+                if (*ppNode) {
+                    q.emplace(ppNode, low, high);
+                }
+            }
+            else {
+                if ((*ppNode)->left) {
+                    q.emplace(&((*ppNode)->left), low, (*ppNode)->val);
+                }
+                if ((*ppNode)->right) {
+                    q.emplace(&((*ppNode)->right), (*ppNode)->val, high);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        //return trimBST_Recursive(root, low, high) ;
+        return trimBST_Iterative(root, low, high) ;
     }
 };
