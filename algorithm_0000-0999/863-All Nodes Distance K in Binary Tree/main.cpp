4e3ce6b3ea40fa3820252d75d1ea7c7a50f1e903
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-27 18:16:35 +0800

    863: bfs solution

--- algorithm_0000-0999/863-All Nodes Distance K in Binary Tree/main.cpp
+++ algorithm_0000-0999/863-All Nodes Distance K in Binary Tree/main.cpp
@@ -9,74 +9,59 @@
  */
 class Solution {
 public:
-    bool search(TreeNode* node, TreeNode* parent, TreeNode* target, unordered_map<TreeNode*, TreeNode*>& backEdges) {
-        assert(target);
-
-        if (!node) {
+    bool buildBackEdgesFromTarget2Root(TreeNode* root, TreeNode* target, unordered_map<TreeNode*, TreeNode*>& backEdges) {
+        if (!root) {
             return false;
         }
-
-        if (node == target) {
-            if (parent) {
-                backEdges.emplace(node, parent);
-            }
+        else if (root == target) {
             return true;
         }
 
-        bool foundLft = search(node->left, node, target, backEdges);
-        if (foundLft) {
-            if (parent) {
-                backEdges.emplace(node, parent);
-            }
+        if (buildBackEdgesFromTarget2Root(root->left, target, backEdges)) {
+            backEdges.emplace(root->left, root);
             return true;
         }
-        else {
-            bool foundRht = search(node->right, node, target, backEdges);
-            if (foundRht) {
-                if (parent) {
-                    backEdges.emplace(node, parent);
-                }
-            }
-            return foundRht;
+
+        if (buildBackEdgesFromTarget2Root(root->right, target, backEdges)) {
+            backEdges.emplace(root->right, root);
+            return true;
         }
+
+        return false;
     }
 
     vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
         unordered_map<TreeNode*, TreeNode*> backEdges;
-        search(root, nullptr, target, backEdges);
+        buildBackEdgesFromTarget2Root(root, target, backEdges);
 
-        queue<TreeNode*> q;
-        q.emplace(target);
-        unordered_set<TreeNode*> seen;
-        while (0 < K--) {
-            size_t n = q.size();
-            for (size_t i = 0; i < n; ++i) {
-                TreeNode* node = q.front();
-                q.pop();
+        // bfs
+        queue<TreeNode*> candidates({target});
+        unordered_set<TreeNode*> visited({target});
+        while (!candidates.empty() && 0 < K--) {
+            for (size_t i = candidates.size(); 0 < i; --i) {
+                TreeNode* node = candidates.front();
+                candidates.pop();
 
-                if (seen.emplace(node).second == false) {
-                    continue;
+                if (node->left && visited.insert(node->left).second) {
+                    candidates.push(node->left);
                 }
 
-                if (node->left && seen.find(node->left) == seen.end()) {
-                    q.emplace(node->left);
+                if (node->right && visited.insert(node->right).second) {
+                    candidates.push(node->right);
                 }
 
-                if (node->right && seen.find(node->right) == seen.end()) {
-                    q.emplace(node->right);
-                }
-
-                if (backEdges.find(node) != backEdges.end() && seen.find(backEdges[node]) == seen.end()) {
-                    q.emplace(backEdges[node]);
+                auto backItr = backEdges.find(node);
+                if (backItr != backEdges.end() && visited.insert(backItr->second).second) {
+                    candidates.push(backItr->second);
                 }
             }
         }
 
-        vector<int> nums(q.size());
-        for (size_t i = 0; i < nums.size(); ++i) {
-            nums[i] = q.front()->val;
-            q.pop();
+        vector<int> ret;
+        while (!candidates.empty()) {
+            ret.push_back(candidates.front()->val);
+            candidates.pop();
         }
-        return nums;
+        return ret;
     }
 };
