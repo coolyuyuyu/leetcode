4ab2ee0a5e5da18326c80ead6772b9c64ac3c7da
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-27 18:16:05 +0800

    814: recursive and iterative solution

--- algorithm_0000-0999/814-Binary Tree Pruning/main.cpp
+++ algorithm_0000-0999/814-Binary Tree Pruning/main.cpp
@@ -4,28 +4,55 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* pruneTreeRecv(TreeNode* root) {
+    TreeNode* pruneTree_Recursive(TreeNode* root) {
         if (!root) {
             return nullptr;
         }
 
-        root->left = pruneTreeRecv(root->left);
-        root->right = pruneTreeRecv(root->right);
+        root->left = pruneTree_Recursive(root->left);
+        root->right = pruneTree_Recursive(root->right);
+        return ((!root->left && !root->right && root->val == 0) ? nullptr : root);
+    }
 
-        bool contains1 = root->val == 1 || root->left != nullptr || root->right != nullptr;
-        if (!contains1) {
-            root = nullptr;
+    TreeNode* pruneTree_Iterative(TreeNode* root) {
+        // postorder traversal
+        stack<pair<TreeNode**, bool>> stk;
+        if (root) {
+            stk.emplace(&root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode** ppNode = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                if (!((*ppNode)->left) && !((*ppNode)->right) && (*ppNode)->val == 0) {
+                    *ppNode = nullptr;
+                }
+            }
+            else {
+                stk.emplace(ppNode, true);
+                if ((*ppNode)->right) {
+                    stk.emplace(&((*ppNode)->right), false);
+                }
+                if ((*ppNode)->left) {
+                    stk.emplace(&((*ppNode)->left), false);
+                }
+            }
         }
 
         return root;
     }
 
     TreeNode* pruneTree(TreeNode* root) {
-        return pruneTreeRecv(root);
+        //return pruneTree_Recursive(root);
+        return pruneTree_Iterative(root);
     }
 };
