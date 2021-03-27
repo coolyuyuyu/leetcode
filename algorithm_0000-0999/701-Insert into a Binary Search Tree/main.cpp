bf3e6fe50904045596ed94021763b950842177ce
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-27 15:40:19 +0800

    701: recursive and iterative implementation

--- algorithm_0000-0999/701-Insert into a Binary Search Tree/main.cpp
+++ algorithm_0000-0999/701-Insert into a Binary Search Tree/main.cpp
@@ -4,24 +4,46 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        TreeNode** ppCur = &root;
-        while (*ppCur) {
-            assert((*ppCur)->val != val);
-            if ((*ppCur)->val < val) {
-                ppCur = &((*ppCur)->right);
+    TreeNode* insertIntoBST_Recursive(TreeNode* root, int val) {
+        if (!root) {
+            return (new TreeNode(val));
+        }
+
+        if (val < root->val) {
+            root->left = insertIntoBST_Recursive(root->left, val);
+        }
+        else {
+            assert(root->val < val);
+            root->right = insertIntoBST_Recursive(root->right, val);
+        }
+        return root;
+    }
+
+    TreeNode* insertIntoBST_Iterative(TreeNode* root, int val) {
+        TreeNode** ppNode = &root;
+        while (*ppNode) {
+            if (val < (*ppNode)->val) {
+                ppNode = &((*ppNode)->left);
             }
             else {
-                ppCur = &((*ppCur)->left);
+                assert((*ppNode)->val < val);
+                ppNode = &((*ppNode)->right);
             }
         }
-        *ppCur = new TreeNode(val);
+        *ppNode = new TreeNode(val);
 
         return root;
+    }
+
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        //return insertIntoBST_Recursive(root, val);
+        return insertIntoBST_Iterative(root, val);
     }
 };
