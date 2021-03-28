ded06c1d0f1f784ca98890294aae2a36591b555c
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-28 18:09:44 +0800

    971: recursive and iterative implementation

--- algorithm_0000-0999/971-Flip Binary Tree To Match Preorder Traversal/main.cpp
+++ algorithm_0000-0999/971-Flip Binary Tree To Match Preorder Traversal/main.cpp
@@ -4,43 +4,72 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
-        stack<TreeNode*> stk;
-        if (root) {
-            stk.push(root);
+    bool flipMatchVoyage_Recursive(TreeNode* root, vector<int>::iterator& itr, vector<int>& ret) {
+        if (!root) {
+            return true;
         }
 
-        vector<int> ans;
-        vector<int>::iterator itr = voyage.begin();
-        while (!stk.empty()) {
+        if (root->val != *itr) {
+            return false;
+        }
+        ++itr;
+
+        if (root->left && root->right && root->right->val == *itr) {
+            swap(root->left, root->right);
+            ret.push_back(root->val);
+        }
+
+        return flipMatchVoyage_Recursive(root->left, itr, ret) && flipMatchVoyage_Recursive(root->right, itr, ret);
+    }
+
+    vector<int> flipMatchVoyage_Iterative(TreeNode* root, vector<int>& voyage) {
+        // preorder traversal
+        stack<TreeNode*> stk;
+        if (root) {
+            stk.emplace(root);
+        }
+
+        vector<int> ret;
+        for (vector<int>::iterator itr = voyage.begin(); !stk.empty(); ++itr) {
             TreeNode* node = stk.top();
             stk.pop();
 
             if (node->val != *itr) {
-                ans.clear();
-                ans.emplace_back(-1);
+                ret = {-1};
                 break;
             }
-            ++itr;
 
-            if (node->left && node->right && node->right->val == *itr) {
-                ans.emplace_back(node->val);
+            if (node->left && node->right && node->right->val == *(itr + 1)) {
                 swap(node->left, node->right);
+                ret.push_back(node->val);
             }
 
             if (node->right) {
-                stk.push(node->right);
+                stk.emplace(node->right);
             }
             if (node->left) {
-                stk.push(node->left);
+                stk.emplace(node->left);
             }
         }
 
-        return ans;
+        return ret;
+    }
+
+    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
+        vector<int>::iterator itr = voyage.begin();
+        vector<int> ret;
+        if (!flipMatchVoyage_Recursive(root, itr, ret)) {
+            ret = {-1};
+        }
+        return ret;
+
+        //return flipMatchVoyage_Iterative(root, voyage);
     }
 };
