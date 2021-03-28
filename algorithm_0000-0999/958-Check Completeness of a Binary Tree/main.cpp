cb4dd63d5412cb7513f26834a1e70fc246363c93
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-28 18:09:35 +0800

    958: update solution

--- algorithm_0000-0999/958-Check Completeness of a Binary Tree/main.cpp
+++ algorithm_0000-0999/958-Check Completeness of a Binary Tree/main.cpp
@@ -4,41 +4,33 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        queue<pair<TreeNode*, size_t>> q;
+        queue<pair<TreeNode*, size_t>> q; // <node, id>;
         if (root) {
             q.emplace(root, 0);
         }
 
-        size_t index = 0;
-        while (!q.empty()) {
-            size_t n = q.size();
-            for (size_t i = 0; i < n; ++i) {
-                TreeNode* node = q.front().first;
-                size_t id = q.front().second;
+        for (size_t index = 0; !q.empty(); ++index) {
+            TreeNode* node = q.front().first;
+            size_t id = q.front().second;
+            q.pop();
 
-                q.pop();
+            if (id != index) {
+                return false;
+            }
 
-                if (id != index++) {
-                    return false;
-                }
-
-                if (node->left) {
-                    q.emplace(node->left, id * 2 + 1);
-                    if (node->right) {
-                        q.emplace(node->right, id * 2 + 2);
-                    }
-                }
-                else {
-                    if (node->right) {
-                        return false;
-                    }
-                }
+            if (node->left) {
+                q.emplace(node->left, id * 2 + 1);
+            }
+            if (node->right) {
+                q.emplace(node->right, id * 2 + 2);
             }
         }
 
