b6e180b33d6b47061fd9a1ae8ae6812e342d4055
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-28 18:08:48 +0800

    889: recursive and iterative implementation

--- algorithm_0000-0999/889-Construct Binary Tree from Preorder and Postorder Traversal/main.cpp
+++ algorithm_0000-0999/889-Construct Binary Tree from Preorder and Postorder Traversal/main.cpp
@@ -4,41 +4,69 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        assert(pre.size() == post.size());
-        return buildTree(pre, 0, post, 0, pre.size());
-    }
-
-    TreeNode* buildTree(vector<int>& pre, size_t preBgn, vector<int>& post, size_t postBgn, size_t len) {
-        if (len == 0) {
-            return NULL;
+    TreeNode* constructFromPrePost_Recursive(vector<int>& pre, size_t preIndex, vector<int>& post, size_t postIndex, size_t size, unordered_map<int, size_t>& m) {
+        if (size <= 0) {
+            return nullptr;
         }
 
-        size_t lftLen, rhtLen;
-        if (len > 1) {
-            int target = pre[preBgn + 1];
-            size_t postIndex = postBgn;
-            for (size_t postEnd = postBgn + len - 1; postIndex < postEnd; ++postIndex) {
-                if (post[postIndex] == target) {
-                    break;
-                }
-            }
-            lftLen = postIndex - postBgn + 1;
-            rhtLen = len - lftLen - 1;
+        TreeNode* root = new TreeNode(pre[preIndex]);
+        if (1 < size) {
+            size_t lftSize = m[pre[preIndex + 1]] - postIndex + 1;
+            size_t rhtSize = size - lftSize - 1;
+            root->left = constructFromPrePost_Recursive(pre, preIndex + 1, post, postIndex, lftSize, m);
+            root->right = constructFromPrePost_Recursive(pre, preIndex + 1 + lftSize, post, postIndex + lftSize, rhtSize, m);
         }
-        else {
-            lftLen = rhtLen = 0;
-        }
-
-        TreeNode* root = new TreeNode(pre[preBgn]);
-        root->left = buildTree(pre, preBgn + 1, post, postBgn, lftLen);
-        root->right = buildTree(pre, preBgn + 1 + lftLen, post, postBgn + lftLen, rhtLen);
 
         return root;
+    }
+
+    TreeNode* constructFromPrePost_Iterative(vector<int>& pre, vector<int>& post, unordered_map<int, size_t>& m) {
+        TreeNode* pRoot = nullptr;
+
+        // BFS
+        queue<tuple<TreeNode**, size_t, size_t, size_t>> q; // <node, preIndex, postIndex, size>
+        if (!post.empty()) {
+            q.emplace(&pRoot, 0, 0, post.size());
+        }
+        while (!q.empty()) {
+            TreeNode** ppNode = get<0>(q.front());
+            size_t preIndex = get<1>(q.front());
+            size_t postIndex = get<2>(q.front());
+            size_t size = get<3>(q.front());
+            q.pop();
+
+            *ppNode = new TreeNode(pre[preIndex]);
+            if (1 < size) {
+                size_t lftSize = m[pre[preIndex + 1]] - postIndex + 1;
+                if (0 < lftSize) {
+                    q.emplace(&((*ppNode)->left), preIndex + 1, postIndex, lftSize);
+                }
+
+                size_t rhtSize = size - lftSize - 1;
+                if (0 < rhtSize) {
+                    q.emplace(&((*ppNode)->right), preIndex + 1 + lftSize, postIndex + lftSize, rhtSize);
+                }
+            }
+        }
+
+
+        return pRoot;
+    }
+
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        unordered_map<int, size_t> m;
+        for (size_t i = 0; i < post.size(); ++i) {
+            m.emplace(post[i], i);
+        }
+
+        //return constructFromPrePost_Recursive(pre, 0, post, 0, post.size(), m);
+        return constructFromPrePost_Iterative(pre, post, m);
     }
 };
