5e4a5b789da291a67e7b9b713641fdb13740d689
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-28 22:33:46 +0800

    979: recursive and iterative solution

--- algorithm_0000-0999/979-Distribute Coins in Binary Tree/main.cpp
+++ algorithm_0000-0999/979-Distribute Coins in Binary Tree/main.cpp
@@ -4,25 +4,65 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int distributeCoins(TreeNode* root, int& count) {
+    int distributeCoins_Recursive(TreeNode* root, int& numOfMoves) {
         if (!root) {
             return 0;
         }
 
-        int lft = distributeCoins(root->left, count), rht = distributeCoins(root->right, count);
-        count += (abs(lft) + abs(rht));
+        int lftRemainingCoins = distributeCoins_Recursive(root->left, numOfMoves);
+        int rhtRemainingCoins = distributeCoins_Recursive(root->right, numOfMoves);
 
-        return (root->val + lft + rht - 1);
+        numOfMoves += (abs(lftRemainingCoins) + abs(rhtRemainingCoins));
+
+        return (root->val + lftRemainingCoins + rhtRemainingCoins - 1);
+    }
+
+    void distributeCoins_Iterative(TreeNode* root, int& numOfMoves) {
+        map<TreeNode*, int> m; // <node, remainingCoins>
+
+        stack<pair<TreeNode*, bool>> stk; // <node, visited>;
+        if (root) {
+            stk.emplace(root, false);
+        }
+        while (!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (visited) {
+                int lftRemainingCoins = (node->left ? m[node->left] : 0);
+                int rhtRemainingCoins = (node->right ? m[node->right] : 0);
+
+                numOfMoves += (abs(lftRemainingCoins) + abs(rhtRemainingCoins));
+
+                m[node] = node->val + lftRemainingCoins + rhtRemainingCoins - 1;
+            }
+            else {
+                stk.emplace(node, true);
+                if (node->right) {
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
+                }
+            }
+
+        }
     }
 
     int distributeCoins(TreeNode* root) {
-        int count = 0;
-        distributeCoins(root, count);
-        return count;
+        int numOfMoves = 0;
+
+        //distributeCoins_Recursive(root, numOfMoves);
+        distributeCoins_Iterative(root, numOfMoves);
+
+        return numOfMoves;
     }
 };
