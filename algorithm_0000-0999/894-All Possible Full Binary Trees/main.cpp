86e3a7f70dd4b1aeb89ca066084de88be98f46ad
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-28 18:09:05 +0800

    894: recursive and iterative implementation

--- algorithm_0000-0999/894-All Possible Full Binary Trees/main.cpp
+++ algorithm_0000-0999/894-All Possible Full Binary Trees/main.cpp
@@ -4,38 +4,62 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<TreeNode*> allPossibleFBT(int N) {
-        static vector<vector<TreeNode*>> cache;
-        if (cache.size() <= N) {
-            cache.resize(N + 1);
+    Solution()
+        : m_fbtGroups({{}, {new TreeNode(0)}}) {
+    }
+
+    vector<TreeNode*>& allPossibleFBT_Recursive(size_t n) {
+        if (m_fbtGroups.size() <= n) {
+            m_fbtGroups.resize(n + 1);
         }
 
-        if (cache[N].empty() == false) {
-            return cache[N];
+        if (n % 2 == 0 || !m_fbtGroups[n].empty()) {
+            return m_fbtGroups[n];
         }
 
-        if (N == 1) {
-             cache[1] = {new TreeNode(0)};
+        for (size_t i = 1; i < n; i += 2) {
+            size_t j = n - i - 1;
+            for (TreeNode* lft : allPossibleFBT_Recursive(i)) {
+                for (TreeNode* rht : allPossibleFBT_Recursive(j)) {
+                    m_fbtGroups[n].push_back(new TreeNode(0, lft, rht));
+                }
+            }
         }
-        else if (N % 2 == 1) {
-            for (int i = 1; i < N; i += 2) {
-                int j = N - i - 1;
-                for (TreeNode* root1 : allPossibleFBT(i)) {
-                    for (TreeNode* root2 : allPossibleFBT(j)) {
-                        TreeNode* root = new TreeNode(0);
-                        root->left = root1;
-                        root->right = root2;
-                        cache[N].emplace_back(root);
+        return m_fbtGroups[n];
+    }
+
+    vector<TreeNode*>& allPossibleFBT_Iterative(size_t n) {
+        for (size_t i = m_fbtGroups.size(); i <= n; ++i) {
+            m_fbtGroups.push_back(vector<TreeNode*>(0));
+            if (i % 2 == 0) {
+                continue;
+            }
+
+            for (size_t lftIndex = 1; lftIndex < i; lftIndex += 2) {
+                size_t rhtIndex = i - lftIndex - 1;
+                for (TreeNode* lft : allPossibleFBT_Recursive(lftIndex)) {
+                    for (TreeNode* rht : allPossibleFBT_Recursive(rhtIndex)) {
+                        m_fbtGroups[i].push_back(new TreeNode(0, lft, rht));
                     }
                 }
             }
         }
 
-        return cache[N];
+        return m_fbtGroups[n];
     }
+
+    vector<TreeNode*>& allPossibleFBT(size_t n) {
+        //return allPossibleFBT_Recursive(n);
+        return allPossibleFBT_Iterative(n);
+    }
+
+private:
+    vector<vector<TreeNode*>> m_fbtGroups;
 };
