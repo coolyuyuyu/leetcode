dffb7661700ba056f1d46f06545f1bbfc955ddbc
coolyuyuyu <coolyuyuyu@gmail.com>
2021-03-31 13:16:21 +0800

    653: two pointer apporach

--- algorithm_0000-0999/653-Two Sum IV - Input is a BST/main.cpp
+++ algorithm_0000-0999/653-Two Sum IV - Input is a BST/main.cpp
@@ -51,10 +51,62 @@ public:
         return false;
     }
 
+    class BSTIterator {
+    public:
+        BSTIterator(TreeNode* root, bool reverse = false)
+            : m_reverse(reverse) {
+            for (; root; root = (m_reverse ? root->right : root->left)) {
+                m_stk.emplace(root);
+            }
+        }
+
+        bool hasNext() {
+            return (!m_stk.empty());
+        }
+
+        TreeNode* next() {
+            TreeNode* root = m_stk.top();
+            m_stk.pop();
+
+            for (TreeNode* node = (m_reverse ? root->left : root->right); node; node = (m_reverse ? node->right : node->left)) {
+                m_stk.emplace(node);
+            }
+
+            return root;
+        }
+
+    private:
+        bool m_reverse;
+        stack<TreeNode*> m_stk;
+    };
+
+    bool findTarget_SearchFromBothEnd(TreeNode* root, int k) {
+        BSTIterator itr1(root, false), itr2(root, true);
+        TreeNode* node1 = (itr1.hasNext() ? itr1.next() : nullptr);
+        TreeNode* node2 = (itr2.hasNext() ? itr2.next() : nullptr);
+        while (node1 != node2) {
+            int sum = node1->val + node2->val;
+            if (sum < k) {
+                node1 = itr1.next();
+            }
+            else if (sum == k) {
+                return true;
+            }
+            else {
+                node2 = itr2.next();
+            }
+        }
+
+        return false;
+    }
+
+
     bool findTarget(TreeNode* root, int k) {
         //unordered_set<int> visited;
         //return findTarget_Recursive(root, k, visited);
 
-        return findTarget_Iterative(root, k);
+        //return findTarget_Iterative(root, k);
+
+        return findTarget_SearchFromBothEnd(root, k);
     }
 };
