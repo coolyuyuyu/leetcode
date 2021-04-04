87c90486f79673f69aee18175dc4bf20fba886df
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-04 13:13:01 +0800

    253: sort and heap apporach

--- algorithm_0000-0999/253-Meeting Rooms II/main.cpp
+++ algorithm_0000-0999/253-Meeting Rooms II/main.cpp
@@ -1,54 +1,56 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    int minMeetingRooms(vector<Interval>& intervals) {
-        vector<State> states;
-        for (const Interval& interval : intervals) {
-            states.emplace_back(interval.start, true);
-            states.emplace_back(interval.end, false);
+    int minMeetingRooms_Sort(vector<vector<int>>& intervals) {
+        vector<pair<bool, int>> states; // <bool: open/close, int: num>
+        for (const vector<int>& interval : intervals) {
+            states.emplace_back(true, interval[0]);
+            states.emplace_back(false, interval[1]);
         }
 
-        auto comp = [](const State& lft, const State& rht) {
-            if (lft.time == rht.time) {
-                return lft.flag < rht.flag;
+        auto comp = [](const pair<bool, int>& state1, const pair<bool, int>& state2) {
+            if (state1.second == state2.second) {
+                return (state1.first == false);
             }
             else {
-                return lft.time < rht.time;
+                return (state1.second < state2.second);
             }
         };
         sort(states.begin(), states.end(), comp);
 
-        int height = 0, maxHeight = 0;
-        for (const State& state : states) {
-            if (state.flag) {
-                ++height;
-                maxHeight = max(maxHeight, height);
+        int numRooms = 0, maxNumRooms = 0;
+        for (const pair<bool, int>& state : states) {
+            if (state.first) {
+                ++numRooms;
+                maxNumRooms = max(maxNumRooms, numRooms);
             }
             else {
-                --height;
+                --numRooms;
             }
         }
 
-        return maxHeight;
+        return maxNumRooms;
     }
 
-private:
-    class State{
-    public:
-        State(int t, bool f)
-            : time(t)
-            , flag(f) {
-        }
+    int minMeetingRooms_Heap(vector<vector<int>>& intervals) {
+        auto comp = [](const vector<int>& interval1, const vector<int>& interval2) {
+            return interval1[0] < interval2[0];
+        };
+        sort(intervals.begin(), intervals.end(), comp);
 
-        int time;
-        bool flag;
-    };
+        int numRooms = 0;
+        priority_queue<int, vector<int>, greater<int>> pq; // min_heap
+        for (const vector<int>& interval : intervals) {
+            while (!pq.empty() && pq.top() <= interval[0]) {
+                pq.pop();
+            }
+            pq.push(interval[1]);
+            numRooms = max(numRooms, (int)pq.size());
+        }
+        return numRooms;
+    }
+
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        return minMeetingRooms_Sort(intervals);
+        //return minMeetingRooms_Heap(intervals);
+    }
 };
