48cbabc368f832f0349de677aaeb046a7e6ca967
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-04 20:24:54 +0800

    451: simple sort, bucket sort and heap solution

--- algorithm_0000-0999/451-Sort Characters By Frequency/main.cpp
+++ algorithm_0000-0999/451-Sort Characters By Frequency/main.cpp
@@ -1,20 +1,76 @@
 class Solution {
 public:
-    string frequencySort(string s) {
-        int counts[128] = {0};
+    string frequencySort_SimpleSort(string s) {
+        vector<size_t> counts(128, 0);
         for (char c : s) {
             ++counts[c];
         }
 
-        vector<string> buckets(s.size() + 1);
-        for (size_t i = 0; i < 128; ++i) {
-            buckets[counts[i]].append(counts[i], (char)i);
+        auto comp = [&](char c1, char c2) {
+            if (counts[c1] == counts[c2]) {
+                return c1 < c2;
+            }
+            else {
+                return (counts[c1] > counts[c2]);
+            }
+        };
+        sort(s.begin(), s.end(), comp);
+
+        return s;
+    }
+
+    string frequencySort_BucketSort(string s) {
+        vector<size_t> counts(128, 0);
+        for (char c : s) {
+            ++counts[c];
         }
 
-        ostringstream oss;
-        for (auto itr = buckets.rbegin(); itr != buckets.rend(); ++itr) {
-            oss << *itr;
+        vector<vector<char>> buckets;
+        for (char c = 0; c < counts.size(); ++c) {
+            if (counts[c] == 0) {
+                continue;
+            }
+
+            if (buckets.size() <= counts[c]) {
+                buckets.resize(counts[c] + 1);
+            }
+            buckets[counts[c]].push_back(c);
         }
-        return oss.str();
+
+        s.clear();
+        for (size_t i = buckets.size(); 0 < i--;) {
+            for (char c : buckets[i]) {
+                s.append(i, c);
+            }
+        }
+        return s;
+    }
+
+    string frequencySort_Heap(string s) {
+        unordered_map<char, size_t> counts;
+        for (char c : s) {
+            ++counts[c];
+        }
+
+        auto comp = [&](const pair<char, size_t>& p1, const pair<char, size_t>& p2) {
+            return (p1.second < p2.second);
+        };
+        priority_queue<pair<char, size_t>, vector<pair<char, size_t>>, decltype(comp)> pq(comp); // max_heap
+        for (const pair<char, size_t>& p : counts) {
+            pq.push(p);
+        }
+
+        s.clear();
+        for (; !pq.empty(); pq.pop()) {
+            const pair<char, size_t>& p = pq.top();
+            s.append(p.second, p.first);
+        }
+        return s;
+    }
+
+    string frequencySort(string s) {
+        //return frequencySort_SimpleSort(s);
+        //return frequencySort_BucketSort(s);
+        return frequencySort_Heap(s);
     }
 };
