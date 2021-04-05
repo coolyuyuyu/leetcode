f737c49fbccc3ecf4b5e4379a4a0783dcfc3b7dc
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-06 00:07:29 +0800

    Update SegmentTree.h

--- utility/SegmentTree.h
+++ utility/SegmentTree.h
@@ -179,34 +179,149 @@ public:
 protected:
     template<typename InputIterator>
     void build(InputIterator first, InputIterator last) {
+        if (!empty()) {
+            m_cntr.resize(m_size - 1);
+            
+            
+            size_t minIndex = std::numeric_limits<size_t>::max()
+            buildTopDown(0, size() - 1, 0, minIndex);
+            
+            
+            build();
+        }
         typename iterator_traits<InputIterator>::difference_type size = std::distance(first, last);
+    }
+
+    void build() {
+        assert(!empty());
+
+        std::stack<pair<std::tuple<size_t, size_t, size_t>, bool>> stk;
+        stk.emplace({{0, size() - 1, 0}, false});
+        while (!stk.empty()) {
+            size_t lo = std::get<0>(stk.top().first);
+            size_t hi = std::get<1>(stk.top().first);
+            size_t index = std::get<2>(stk.top().first);
+            bool visited = stk.top().second;
+            stk.pop();
+
+            if (lo == hi) {
+                continue;
+            }
+
+            if (visited) {
+                m_cntr[index] = m_op(m_cntr[lftChildIndex(index)], m_cntr[rhtChildIndex(index)]);
+            }
+            else {
+                size_t mid = lo + (hi - lo) / 2;
+                stk.emplace(lo, mid, lftChildIndex(index), false);
+                stk.emplace(mid + 1, hi, rhtChildIndex(index), false);
+                stk.emplace(lo, hi, index, true);
+            }
+        }
     }
 
     Container m_cntr;
     BinaryOperation m_op;
     size_t m_size;
+
+private:
+    inline size_t lftChildIndex(size_t index) {
+        return (index * 2 + 1);
+    }
+
+    inline size_t rhtChildIndex(size_t index) {
+        return (index * 2 + 2);
+    }
 };
 
 
 
 
 /*
-priority_queue
-priority_queue::priority_queue
-member functions:
-priority_queue::emplace
-priority_queue::empty
-priority_queue::pop
-priority_queue::push
-priority_queue::size
-priority_queue::swap
-priority_queue::top
-non-member overloads:
-swap (priority_queue)
-non-member specializations:
-uses_allocator<priority_queue>
-*/
+class RangeSumSegmentTreeByHeap : public RangeSumStrategy {
+public:
+    RangeSumSegmentTreeByHeap(vector<int>& nums)
+        : m_size(nums.size()) {
+        if (0 < m_size) {
+            build(0, m_size - 1, 0, nums);
+        }
+    }
 
+    void update(size_t i, int val) {
+        update(0, m_size - 1, 0, i, val);
+    }
+
+    int sum(size_t lo, size_t hi) const {
+        return query(0, m_size - 1, 0, lo , hi);
+    }
+
+private:
+    inline size_t lftChildIndex(size_t parent) const {
+        return parent * 2 + 1;
+    }
+
+    inline size_t rhtChildIndex(size_t parent) const {
+        return parent * 2 + 2;
+    }
+
+    void build(size_t lo, size_t hi, size_t parent, const vector<int>& nums) {
+        assert(lo <= hi);
+
+        if (lo == hi) {
+            if (m_sums.size() <= parent) {
+                m_sums.resize(parent + 1);
+            }
+            m_sums[parent] = nums[lo];
+            return;
+        }
+
+        size_t mid = lo + (hi - lo) / 2;
+        size_t lftChild = lftChildIndex(parent), rhtChild = rhtChildIndex(parent);
+        build(lo, mid, lftChild, nums);
+        build(mid + 1, hi, rhtChild, nums);
+        m_sums[parent] = m_sums[lftChild] + m_sums[rhtChild];
+    }
+
+    void update(size_t lo, size_t hi, size_t parent, size_t i, int val) {
+        assert(lo <= hi);
+
+        if (i < lo || hi < i) {
+            return;
+        }
+
+        if (lo == hi) {
+            assert(i == lo);
+            m_sums[parent] = val;
+        }
+        else {
+            size_t mid = lo + (hi - lo) / 2;
+            size_t lftChild = lftChildIndex(parent), rhtChild = rhtChildIndex(parent);
+            update(lo, mid, lftChild, i, val);
+            update(mid + 1, hi, rhtChild, i, val);
+            m_sums[parent] = m_sums[lftChild] + m_sums[rhtChild];
+        }
+    }
+
+    int query(size_t lo, size_t hi, size_t parent, size_t i, size_t j) const {
+        assert(lo <= hi);
+
+        if (j < lo || hi < i) {
+            return 0;
+        }
+
+        if (i <= lo && hi <= j) {
+            return m_sums[parent];
+        }
+        else {
+            size_t mid = lo + (hi - lo) / 2;
+            return query(lo, mid, lftChildIndex(parent), i, j) + query(mid + 1, hi, rhtChildIndex(parent), i, j);
+        }
+    }
+
+    size_t m_size;
+    vector<int> m_sums;
+};
+*/
 
 
 int main() {
