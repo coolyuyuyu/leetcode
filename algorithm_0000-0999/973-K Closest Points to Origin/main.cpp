26df47c9aa28a6c4aa65b8b6052311f146164876
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-06 15:00:08 +0800

    973: sort and heap solution

--- algorithm_0000-0999/973-K Closest Points to Origin/main.cpp
+++ algorithm_0000-0999/973-K Closest Points to Origin/main.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        auto comp = [](const vector<int>& a, const vector<int>& b) {
-            return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
+    vector<vector<int>> kClosest_Sort(vector<vector<int>>& points, int k) {
+        typedef vector<int> Point;
+        auto comp = [](const Point& p1, const Point& p2) {
+            return ((p1[0] * p1[0] + p1[1] * p1[1]) < (p2[0] * p2[0] + p2[1] * p2[1]));
         };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> heap(comp); // max heap
-        for (const vector<int>& point : points) {
-            if (heap.size() < K) {
-                heap.push(point);
+        sort(points.begin(), points.end(), comp);
+
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+
+    vector<vector<int>> kClosest_Heap(vector<vector<int>>& points, int k) {
+        typedef vector<int> Point;
+        auto comp = [](const Point& p1, const Point& p2) {
+            return ((p1[0] * p1[0] + p1[1] * p1[1]) < (p2[0] * p2[0] + p2[1] * p2[1]));
+        };
+        priority_queue<Point, vector<Point>, decltype(comp)> pq(comp); // max_heap
+        for (const Point& p : points) {
+            if (pq.size() < k) {
+                pq.push(p);
             }
-            else {
-                if (comp(point, heap.top())) {
-                    heap.pop();
-                    heap.push(point);
-                }
+            else if (comp(p, pq.top())) {
+                pq.pop();
+                pq.push(p);
             }
         }
 
-        vector<vector<int>> ans(heap.size());
-        for (size_t i = 0; !heap.empty(); ++i) {
-            ans[i] = heap.top();
-            heap.pop();
+        vector<Point> ret(pq.size());
+        for (size_t i = 0; i < ret.size(); ++i, pq.pop()) {
+            ret[i] = pq.top();
         }
-        return ans;
+        return ret;
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // Time: O(NlogN), Space: O(logN) <- implicit stack size
+        //return kClosest_Sort(points, k);
+
+        // Time: O(NlogK), Space: O(K)
+        return kClosest_Heap(points, k);
     }
 };
