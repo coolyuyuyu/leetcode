0353ed2203f4fa4a06b0dffa1832ee1f33ee4407
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-06 17:37:39 +0800

    1054: heap solution

---
+++ algorithm_1000-1999/1054-Distant Barcodes/main.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> rearrangeBarcodes_Heap(vector<int>& barcodes) {
+        typedef pair<int, int> NumCount;
+        auto comp = [](const NumCount& nc1, const NumCount& nc2) {
+            return (nc1.second < nc2.second);
+        };
+        priority_queue<NumCount, vector<NumCount>, decltype(comp)> pq(comp); { // max_heap
+            unordered_map<int, int> counts;
+            for (int barcode : barcodes) {
+                ++counts[barcode];
+            }
+
+            for (const auto& p : counts) {
+                pq.push(p);
+            }
+        }
+
+        size_t index = 0;
+        for (; !pq.empty(); pq.pop()) {
+            const NumCount& nc = pq.top();
+            for (int i = 0; i < nc.second; ++i) {
+                if (barcodes.size() <= index) {
+                    index = 1;
+                }
+                barcodes[index] = nc.first;
+                index += 2;
+            }
+        }
+
+        return barcodes;
+    }
+
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        return rearrangeBarcodes_Heap(barcodes);
+    }
+};
