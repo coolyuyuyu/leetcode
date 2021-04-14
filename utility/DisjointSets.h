c73e1b07d725936085ecd624c11ee0f5b80ff8c4
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-14 22:11:25 +0800

    DisjointSets: update

--- utility/DisjointSets.h
+++ utility/DisjointSets.h
@@ -11,6 +11,7 @@ Optimization:
     merge by rank
 */
 
+//Container = std::map<T, pair<T, size_t>> <val, <parent, cardinality>>
 template<typename T, class Container = std::map<T, T>, class Equal = std::equal_to<typename Container::key_type>>
 class DisjointSets {
 public:
@@ -40,28 +41,45 @@ public:
         m_map.swap(rhs.m_map);
     }
 
-    bool make_set(const T& elem);
+    // false: alreay belong to 1 set
+    bool make_set(const T& val);
 
-    bool merge(const T& elem1, const T& elem2) // link? // return false if already merged
+    bool merge(const T& val1, const T& val2) // link? // return false if already merged
+    bool connected(const T& val1, const T& val2)
+    
 
-    bool contain(const T& elem) const {
-        T root;
-        return (root(elem, root));
+    bool contain(const T& val) const {
+        bool found;
+        findRoot(val, found);
+        
+        return found;
     }
 
     size_t size() const {
         return m_size;
+    }
+    
+    // num of elment in one set
+    size_t cardinality(const T& val) const {
+        bool found;
+        findRoot(val, found);
+    }
+    
+    bool sigleton const (const T& val) {
+        
     }
 
     std::set<T> set(const T& elem) const;
     std::vector<std::set<T>> sets(const T& elem) const;
 
 private:
-    bool root(const T& elem, T& root) const // return found
+    const T& findRoot(const T& elem, bool& found) const {
+        
+    } // return found
 
+    mutable Container m_map;
     Equal m_equal;
     size_t m_size;
-    mutable Container m_map;
 };
 
 #endif
