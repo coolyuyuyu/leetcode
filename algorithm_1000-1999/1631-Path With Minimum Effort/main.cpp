87fcd02ff2a32b9028910f8da1d90361da71f45f
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-19 15:39:32 +0800

    1631: union find

---
+++ algorithm_1000-1999/1631-Path With Minimum Effort/main.cpp
@@ -0,0 +1,348 @@
+#ifndef __DISJOINT_SETS_HPP__493A883A_6A4F_4328_8CB1_58CB15279742
+#define __DISJOINT_SETS_HPP__493A883A_6A4F_4328_8CB1_58CB15279742
+
+#include <algorithm>
+#include <cassert>
+#include <initializer_list>
+#include <set>
+#include <map>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+template <typename Map>
+struct FindNoCompress {
+    bool operator()(Map& map, typename Map::key_type elem, typename Map::key_type& root) const {
+        typename Map::iterator itr = map.find(elem);
+        if (itr == map.end()) {
+            return false;
+        }
+        else {
+            root = elem;
+            while (itr->second != root) {
+                root = itr->second;
+                itr = map.find(root);
+            }
+
+            return true;
+        }
+    }
+};
+
+template <typename Map>
+struct FindFullCompress {
+    bool operator()(Map& map, typename Map::key_type elem, typename Map::key_type& root) const {
+        typename Map::iterator itr = map.find(elem);
+        if (itr == map.end()) {
+            return false;
+        }
+        else {
+            root = itr->second;
+            if (root == elem) {
+                return true;
+            }
+
+            std::vector<typename Map::key_type> candidates;
+            while (true) {
+                itr = map.find(root);
+                assert(itr != map.end());
+
+                if (itr->second == root) {
+                    break;
+                }
+
+                candidates.push_back(elem);
+                elem = root;
+                root = itr->second;
+            }
+
+            for (typename Map::key_type candidate : candidates) {
+                map[candidate] = root;
+            }
+
+            return true;
+        }
+    }
+};
+
+template<typename T, typename Map = std::map<T, T>, typename Find = FindFullCompress<Map>, typename = typename std::enable_if<std::is_integral<T>::value, T>::type>
+class DisjointSets {
+public:
+    typedef T value_type;
+    typedef Map map_type;
+
+    template<typename T1, typename Map1, typename Find1>
+    friend bool operator==(const DisjointSets& x, const DisjointSets<T1, Map1, Find1>& y) {
+        return (x.size() == y.size() && x.sets<std::set>() == y.template sets<std::set>());
+    }
+
+    template<typename T1, typename Map1, typename Find1>
+    friend bool operator!=(const DisjointSets& x, const DisjointSets<T1, Map1, Find1>& y) {
+        return !(x == y);
+    }
+
+    static_assert(std::is_same<T, typename Map::key_type>::value, "value_type must be the same as the underlying container key_type");
+    static_assert(std::is_same<T, typename Map::mapped_type>::value, "value_type must be the same as the underlying container mapped_type");
+
+    DisjointSets(const Find& find = Find())
+        : m_map()
+        , m_find(find)
+        , m_size(0) {
+    }
+
+    DisjointSets(const DisjointSets&) = default;
+    DisjointSets(DisjointSets&&) = default;
+
+    template<typename InputIterator>
+    explicit DisjointSets(InputIterator first, InputIterator last, const Find& find = Find())
+        : m_map()
+        , m_find(find)
+        , m_size(0) {
+        for (InputIterator itr = first; itr != last; ++itr) {
+            merge(itr->first, itr->second);
+        }
+    }
+
+    explicit DisjointSets(std::initializer_list<std::pair<const T, T>> l, const Find& find = Find())
+        : m_map()
+        , m_find(find)
+        , m_size(0) {
+        for (const std::pair<const T, T>& p : l) {
+            merge(p.first, p.second);
+        }
+    }
+
+    void clear() {
+        m_map.clear();
+        m_size = 0;
+    }
+
+    void swap(DisjointSets& rhs) noexcept {
+        std::swap(m_map, rhs.m_map);
+        std::swap(m_find, rhs.m_find);
+        std::swap(m_size, rhs.m_size);
+    }
+
+    bool make_set(T elem) {
+        T root;
+        if (m_find(m_map, elem, root)) {
+            return false;
+        }
+        else {
+            ++m_size;
+            m_map[elem] = elem;
+            return true;
+        }
+    }
+
+    bool merge(T elem1, T elem2) {
+        assert(elem1 != elem2);
+
+        T root1;
+        bool found1 = m_find(m_map, elem1, root1);
+        T root2;
+        bool found2 = m_find(m_map, elem2, root2);
+
+        if (found1) {
+            if (found2) {
+                if (root1 == root2) {
+                    return false;
+                }
+                else {
+                    --m_size;
+                    m_map[root2] = root1;
+                }
+            }
+            else {
+                m_map[elem2] = root1;
+            }
+        }
+        else {
+            if (found2) {
+                m_map[elem1] = root2;
+            }
+            else {
+                ++m_size;
+                m_map[elem1] = m_map[elem2] = elem1;
+            }
+        }
+
+        return true;
+    }
+
+    bool connected(T elem1, T elem2) const {
+        T root1, root2;
+        return (m_find(m_map, elem1, root1) && m_find(m_map, elem2, root2) && root1 == root2);
+    }
+
+    T root(T elem) const {
+        T r;
+        bool found = m_find(m_map, elem, r);
+        assert(found);
+
+        return r;
+    }
+
+    bool contain(T elem) const {
+        T root;
+        return (m_find(m_map, elem, root));
+    }
+
+    size_t size() const {
+        return m_size;
+    }
+
+    size_t cardinality(T elem) const {
+        T root;
+        if (!m_find(m_map, elem, root)) {
+            return 0;
+        }
+
+        size_t c = 0;
+        for (const pair<T, T>& p : m_map) {
+            T rootTmp;
+            m_find(m_map, p.first, rootTmp);
+            if (rootTmp == root) {
+                ++c;
+            }
+        }
+
+        return c;
+    }
+
+    bool sigleton(T elem) const {
+        T root;
+        if (!m_find(m_map, elem, root)) {
+            return false;
+        }
+
+        size_t count = 0;
+        for (const pair<T, T>& p : m_map) {
+            T rootTmp;
+            m_find(m_map, p.first, rootTmp);
+            if (rootTmp == root) {
+                if (1 < ++count) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    template<template<typename, typename...> class Container = std::vector, typename... Args>
+    Container<T, Args...> set(T elem) const {
+        Container<T, Args...> ret;
+
+        T root;
+        if (!m_find(m_map, elem, root)) {
+            return ret;
+        }
+
+        for (const std::pair<const T, T>& p : m_map) {
+            T rootTmp;
+            m_find(m_map, p.first, rootTmp);
+            if (rootTmp == root) {
+                ret.insert(ret.end(), p.first);
+            }
+        }
+
+        return ret;
+    }
+
+    template<template<typename, typename...> class Container = std::vector, typename... Args>
+    Container<Container<T, Args...>, Args...> sets() const {
+        std::map<T, Container<T, Args...>> ss;
+        for (const std::pair<const T, T>& p : m_map) {
+            T root;
+            m_find(m_map, p.first, root);
+
+            ss[root].insert(ss[root].end(), p.first);
+        }
+
+        Container<Container<T, Args...>, Args...> ret;
+        for (std::pair<const T, Container<T, Args...>>& p : ss) {
+            ret.insert(ret.end(), std::move(p.second));
+        }
+
+        return ret;
+    }
+
+protected:
+    mutable Map m_map;
+    Find m_find;
+    size_t m_size;
+};
+
+namespace std {
+    template<typename T, typename Map, typename Find>
+    inline void swap(DisjointSets<T, Map, Find>& x, DisjointSets<T, Map, Find>& y) noexcept(noexcept(x.swap(y))) {
+        x.swap(y);
+    }
+}
+
+#endif
+
+class Solution {
+public:
+    int minimumEffortPath_UnionFind(vector<vector<int>>& heights) {
+        int rowCnt = heights.size();
+        int colCnt = heights.empty() ? 0 : heights.front().size();
+        assert(0 < rowCnt && 0 < colCnt);
+
+        function<int(int, int)> getId = [&rowCnt, &colCnt](int row, int col) {
+            return row * colCnt + col;
+        };
+        int srcId = getId(0, 0), dstId = getId(rowCnt - 1, colCnt - 1);
+
+        struct Edge {
+            bool operator>(const Edge& e2) const {
+                return diff > e2.diff;
+            }
+
+            Edge(int id1_, int id2_, int diff_)
+                : id1(id1_)
+                , id2(id2_)
+                , diff(diff_) {
+            }
+
+            int id1;
+            int id2;
+            int diff;
+        };
+
+        priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+        for (int row = 0; row < rowCnt; ++row) {
+            for (int col = 0; col < colCnt; ++col) {
+                if ((row + 1) < rowCnt) {
+                    int diff = abs(heights[row][col] - heights[row + 1][col]);
+                    pq.emplace(getId(row, col), getId(row + 1, col), diff);
+                }
+
+                if ((col + 1) < colCnt) {
+                    int diff = abs(heights[row][col] - heights[row][col + 1]);
+                    pq.emplace(getId(row, col), getId(row, col + 1), diff);
+                }
+            }
+        }
+
+        int effort = 0;
+
+        DisjointSets<int> ds;
+        ds.make_set(srcId);
+        ds.make_set(dstId);
+        while (!ds.connected(srcId, dstId)) {
+            Edge e = pq.top();
+            pq.pop();
+
+            effort = e.diff;
+            ds.merge(e.id1, e.id2);
+        }
+
+        return effort;
+    }
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        return minimumEffortPath_UnionFind(heights);
+    }
+};
