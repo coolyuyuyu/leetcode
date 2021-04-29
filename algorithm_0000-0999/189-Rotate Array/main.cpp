bc5ad5b532d4ced20a4566f907bb516cdc2a97e2
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-29 14:46:49 +0800

    189: reverse and rotate solution

--- algorithm_0000-0999/189-Rotate Array/main.cpp
+++ algorithm_0000-0999/189-Rotate Array/main.cpp
@@ -1,8 +1,33 @@
+template<typename ForwardIterator>
+void ROTATE(ForwardIterator first, ForwardIterator middle, ForwardIterator last) {
+    ForwardIterator next = middle;
+    while (first != next) {
+        swap(*first++, *next++);
+        if (next == last) {
+            next = middle;
+        }
+        else if (first == middle) {
+            middle = next;
+        }
+    }
+}
+
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+    void rotate_reverse(vector<int>& nums, int k) {
+        k %= nums.size();
         reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k % nums.size());
-        reverse(nums.begin() + k % nums.size(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+
+    void rotate_rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        ROTATE(nums.begin(), nums.begin() + (0 < k ? (nums.size() - k) : 0), nums.end());
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        //rotate_reverse(nums, k);
+        rotate_rotate(nums, k);
     }
 };
