39be3369b586f31939bb04cd2be8cb9149f058df
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-29 23:59:43 +0800

    Algorithm: new file

---
+++ utility/Algorithm.hpp
@@ -0,0 +1,16 @@
+#ifndef __ALGORITHM_HPP__D3CA27F7_E64B_410C_8018_06F09F77DE96
+#define __ALGORITHM_HPP__D3CA27F7_E64B_410C_8018_06F09F77DE96
+
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <initializer_list>
+#include <iterator>
+#include <stdexcept>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+
+#endif
