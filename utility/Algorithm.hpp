17a68bb80a7bd44b1ecc4365f077787a2000d5b0
coolyuyuyu <coolyuyuyu@gmail.com>
2021-04-30 00:35:24 +0800

    Algorithm: findSmallest_Heap

--- utility/Algorithm.hpp
+++ utility/Algorithm.hpp
@@ -1,16 +1,24 @@
 #ifndef __ALGORITHM_HPP__D3CA27F7_E64B_410C_8018_06F09F77DE96
 #define __ALGORITHM_HPP__D3CA27F7_E64B_410C_8018_06F09F77DE96
 
-#include <algorithm>
-#include <cassert>
-#include <functional>
-#include <initializer_list>
-#include <iterator>
-#include <stdexcept>
-#include <tuple>
-#include <type_traits>
-#include <utility>
-#include <vector>
 
+#include <functional>
+#include <iterator>
+
+
+template<typename InputIterator, typename Compare = std::less<typename std::iterator_traits<InputIterator>::value_type>>
+typename std::iterator_traits<InputIterator>::value_type findSmallest_Heap(InputIterator first, InputIterator last, size_t k, Compare comp = Compare()) {
+    priority_queue<typename std::iterator_traits<InputIterator>::value_type> pq;
+    for (; first != last; ++first) {
+        if (pq.size() <= k) {
+            pq.push(*first);
+        }
+        else if (comp(*first, pq.top())) {
+            pq.pop();
+            pq.push(*first);
+        }
+    }
+    return pq.top();
+}
 
 #endif
