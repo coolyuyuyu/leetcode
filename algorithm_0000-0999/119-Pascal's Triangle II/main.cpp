7bcb782b990bc23665fd820d56af8299ab1f6732
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-04 15:24:49 +0800

    0119: math solution

--- algorithm_0000-0999/119-Pascal's Triangle II/main.cpp
+++ algorithm_0000-0999/119-Pascal's Triangle II/main.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    vector<int> getRow_BruteForce(size_t rowIndex) {
+    vector<int> getRow_BruteForce(int rowIndex) {
         vector<int> row = {1};
-        for (; 0 < rowIndex; -- rowIndex) {
+        for (; 0 < rowIndex; --rowIndex) {
             vector<int> tmpRow;
             tmpRow.push_back(1);
             for (size_t i = 1; i < row.size(); ++i) {
@@ -16,7 +16,17 @@ public:
         return row;
     }
 
+    vector<int> getRow_Math(int rowIndex) {
+        vector<int> row = {1};
+        for (int k = 1; k <= rowIndex; ++k) {
+            row.push_back(static_cast<long long int>(row.back()) * (rowIndex - k + 1) / k);
+        }
+
+        return row;
+    }
+
     vector<int> getRow(int rowIndex) {
-        return getRow_BruteForce(rowIndex);
+        //return getRow_BruteForce(rowIndex);
+        return getRow_Math(rowIndex);
     }
 };
