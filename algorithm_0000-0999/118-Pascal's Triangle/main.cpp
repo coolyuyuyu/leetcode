5ead1d31a0acd989d2a3d8e9016525651349f2f0
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-04 15:39:26 +0800

    0118: math solution

--- algorithm_0000-0999/118-Pascal's Triangle/main.cpp
+++ algorithm_0000-0999/118-Pascal's Triangle/main.cpp
@@ -18,7 +18,20 @@ public:
         return ret;
     }
 
+    vector<vector<int>> generate_Math(int numRows) {
+        vector<vector<int>> ret(numRows);
+        for (size_t n = 0; n < numRows; ++n) {
+            ret[n].push_back(1);
+            for (size_t k = 1; k <= n; ++k) {
+                ret[n].push_back(static_cast<long long int>(ret[n].back()) * (n - k + 1) / k);
+            }
+        }
+
+        return ret;
+    }
+
     vector<vector<int>> generate(int numRows) {
-        return generate_BruteForce(numRows);
+        //return generate_BruteForce(numRows);
+        return generate_Math(numRows);
     }
 };
