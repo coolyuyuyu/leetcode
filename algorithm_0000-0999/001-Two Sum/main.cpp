df180574b6396108bdca770f02925fcf89411754
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-04 11:51:54 +0800

    0001: updated solution

--- algorithm_0000-0999/001-Two Sum/main.cpp
+++ algorithm_0000-0999/001-Two Sum/main.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> indexes;
         for (int i = 0; i < nums.size(); ++i) {
-            int x = nums[i];
-            int y = target - x;
-
-            unordered_map<int, int>::const_iterator it = indexes.find(y);
-            if (it != indexes.end()) {
-                return {it->second, i};
-            } 
-            indexes.emplace(x, i);
+            auto itr = indexes.find(target - nums[i]);
+            if (itr != indexes.end()) {
+                return {itr->second, i};
+            }
+            else {
+                indexes.emplace(nums[i], i);
+            }
         }
-        return {0, 0};
+
+        return {-1, -1};
     }
 };
