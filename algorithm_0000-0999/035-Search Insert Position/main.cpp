fc9d263a199bd94c11120b4aea45356af505d649
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-04 12:57:48 +0800

    0035: brute force and binary search solution

--- algorithm_0000-0999/035-Search Insert Position/main.cpp
+++ algorithm_0000-0999/035-Search Insert Position/main.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    size_t searchInsert(vector<int>& nums, int target) {
-        size_t lo = 0;
-        size_t hi = nums.size();
-
-        while (lo < hi) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (target <= nums[mid]) {
-                hi = mid;
-            }
-            else {
-                assert(nums[mid] < target);
-                lo = mid + 1;
+    size_t searchInsert_BruteForce(vector<int>& nums, int target) {
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (target <= nums[i]) {
+                return i;
             }
         }
 
+        return nums.size();
+    }
+
+    size_t searchInsert_BinarySearch(vector<int>& nums, int target) {
+        size_t lo = 0, hi = nums.size();
+        while (lo < hi) {
+            size_t mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid;
+            }
+        }
         assert(lo == hi);
-        return hi;
+
+        return lo;
+    }
+
+    int searchInsert(vector<int>& nums, int target) {
+        //return searchInsert_BruteForce(nums, target);
+        return searchInsert_BinarySearch(nums, target);
     }
 };
