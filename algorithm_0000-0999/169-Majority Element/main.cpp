7a1c50407452684d15c514f3564b02d6047aad89
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-05 16:54:40 +0800

    0169: hash count and linear solution

--- algorithm_0000-0999/169-Majority Element/main.cpp
+++ algorithm_0000-0999/169-Majority Element/main.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        // Boyer–Moore majority vote algorithm
-        int major = nums[0];
-        int count = 1;
-        for (int i = 1; i < nums.size(); ++i) {
-            major == nums[i] ? ++count : --count;
-            if (count == 0) {
-                major = nums[i];
-                count = 1;
+    int majorityElement_HashCount(vector<int>& nums) {
+        int halfSize = nums.size() / 2;
+        unordered_map<int, int> counts;
+        for (int num : nums) {
+            if (++counts[num] > halfSize) {
+                return num;
             }
         }
-        return major;
+
+        assert(false);
+        return 0;
+    }
+
+    // Boyer-Moore majority vote algorithm
+    int majorityElement_Linear(vector<int>& nums) {
+        int majority, cnt = 0;
+        for (int num : nums) {
+            if (cnt == 0) {
+                majority = num;
+                cnt = 1;
+            }
+            else {
+                majority == num ? ++cnt : --cnt;
+            }
+
+        }
+
+        return majority;
+    }
+
+    int majorityElement(vector<int>& nums) {
+        //return majorityElement_HashCount(nums);
+        return majorityElement_Linear(nums);
     }
 };
