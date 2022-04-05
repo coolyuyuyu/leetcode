3d6525efb1d3b6d804f6a72775c08ca0346be070
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-05 15:39:35 +0800

    0219: update solution

--- algorithm_0000-0999/219-Contains Duplicate II/main.cpp
+++ algorithm_0000-0999/219-Contains Duplicate II/main.cpp
@@ -3,8 +3,8 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int, int> indexes;
         for (int i = 0; i < nums.size(); ++i) {
-            pair<unordered_map<int, int>::iterator, bool> p = indexes.emplace(nums[i], i);
-            if (p.second == false) {
+            auto p = indexes.emplace(nums[i], i);
+            if (!p.second) {
                 if ((i - p.first->second) <= k) {
                     return true;
                 }
