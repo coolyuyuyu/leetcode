79b7d50df1058db52f64731cc142f2c68c407f48
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-08 20:22:09 +0800

    0283: swap forward solution

--- algorithm_0000-0999/283-Move Zeroes/main.cpp
+++ algorithm_0000-0999/283-Move Zeroes/main.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        size_t index = 0;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            if (nums[i] != 0) {
-                nums[index++] = nums[i];
+    void moveZeroes_MoveForward(vector<int>& nums) {
+        size_t preIdx = 0;
+        for (size_t idx = 0; idx < nums.size(); ++idx) {
+            if (nums[idx] != 0) {
+                nums[preIdx++] = nums[idx];
             }
         }
+        std::fill(nums.begin() + preIdx, nums.end(), 0);
+    }
 
-        fill(nums.begin() + index, nums.end(), 0);
+    void moveZeroes_SwapForward(vector<int>& nums) {
+        for (size_t j = 0, i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                std::swap(nums[j++], nums[i]);
+            }
+        }
+    }
+
+    void moveZeroes(vector<int>& nums) {
+        //return moveZeroes_MoveForward(nums);
+        return moveZeroes_SwapForward(nums);
     }
 };
