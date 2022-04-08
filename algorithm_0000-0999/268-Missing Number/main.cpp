2ff511404e981d82f1aa19eb71ce3acb8995b177
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-08 21:19:12 +0800

    0268: sum and bit solution

--- algorithm_0000-0999/268-Missing Number/main.cpp
+++ algorithm_0000-0999/268-Missing Number/main.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int result = nums.size();
-        for (int i = 0; i < nums.size(); ++i) {
-            result ^= nums[i];
-            result ^= i;
+    int missingNumber_Sum(vector<int>& nums) {
+        int cnt = nums.size();
+        int sum = cnt * (cnt + 1) / 2;
+        for (int num : nums) {
+            sum -= num;
         }
-        return result;
+
+        return sum;
+    }
+
+    int missingNumber_Bit(vector<int>& nums) {
+        int target = nums.size();
+        for (int i = 0; i < nums.size(); ++i) {
+            target ^= i;
+            target ^= nums[i];
+        }
+
+        return target;
+    }
+
+    int missingNumber(vector<int>& nums) {
+        //return missingNumber_Sum(nums);
+        return missingNumber_Bit(nums);
     }
 };
