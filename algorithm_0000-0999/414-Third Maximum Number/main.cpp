7f2cea68d626f4c38df1b177fce2dbfd16cc1ad3
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-13 00:01:55 +0800

    0414: linear and set solution

--- algorithm_0000-0999/414-Third Maximum Number/main.cpp
+++ algorithm_0000-0999/414-Third Maximum Number/main.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
-    int thirdMax(vector<int>& nums) {
+    // Time: O(NlogN), Space: O(N)
+    int thirdMax_Set(vector<int>& nums) {
         set<int> s;
-        for (vector<int>::const_iterator iter = nums.begin(); iter != nums.end(); ++iter) {
-            s.emplace(*iter);
+        for (int num : nums) {
+            s.insert(num);
         }
 
-        if (s.size() >= 3) {
-            return *(++++s.rbegin());
+        return *(s.size() < 3 ? s.rbegin() : ++++s.rbegin());
+    }
+
+    // Time: O(N), Space: O(1)
+    int thirdMax_Linear(vector<int>& nums) {
+        vector<int> sorted;
+        for (int num : nums) {
+            auto insertedItr = std::lower_bound(sorted.begin(), sorted.end(), num);
+            if (insertedItr != sorted.end() && *insertedItr == num) {
+                continue; // duplicate
+            }
+
+            size_t insertedIdx = insertedItr - sorted.begin();
+            if (sorted.size() < 3) {
+                sorted.insert(insertedItr, num);
+            }
+            else if (insertedItr == sorted.begin()) {
+            }
+            else {
+                for (auto itr = sorted.begin() + 1; itr < insertedItr; ++itr) {
+                    *(itr - 1) = *itr;
+                }
+                *(insertedItr - 1) = num;
+            }
         }
-        else {
-            return *s.rbegin();
-        }
+
+        return sorted.size() < 3 ? sorted.back() : sorted.front();
+    }
+
+    int thirdMax(vector<int>& nums) {
+        //return thirdMax_Set(nums);
+        return thirdMax_Linear(nums);
     }
 };
