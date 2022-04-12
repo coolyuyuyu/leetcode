f69e549e38be093e4404c51948d658de8e8268d4
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-13 00:02:09 +0800

    0422: update solution

--- algorithm_0000-0999/422-Valid Word Square/main.cpp
+++ algorithm_0000-0999/422-Valid Word Square/main.cpp
@@ -5,17 +5,11 @@ public:
             if (words.size() < words[i].size()) {
                 return false;
             }
-
             for (size_t j = i + 1; j < words[i].size(); ++j) {
-                if (words[j].size() < (i + 1)) {
-                    return false;
-                }
-
                 if (words[i][j] != words[j][i]) {
                     return false;
                 }
             }
-
             for (size_t k = words[i].size(); k < words.size(); ++k) {
                 if (i < words[k].size()) {
                     return false;
