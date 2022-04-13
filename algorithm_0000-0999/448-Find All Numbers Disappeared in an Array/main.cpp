728654542e7f75c9488c1f512ff76067370210ae
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-13 21:33:04 +0800

    0448: set, sort and linear solution

--- algorithm_0000-0999/448-Find All Numbers Disappeared in an Array/main.cpp
+++ algorithm_0000-0999/448-Find All Numbers Disappeared in an Array/main.cpp
@@ -1,21 +1,56 @@
 class Solution {
 public:
+    // Time: O(NlogN), Space: O(N)
+    vector<int> findDisappearedNumbers_Set(vector<int>& nums) {
+        set<int> s;
+        for (int i = 1; i <= nums.size(); ++i) {
+            s.insert(i);
+        }
+        for (int num : nums) {
+            s.erase(num);
+        }
+
+        return vector<int>(s.begin(), s.end());
+    }
+
+    // Time: O(NlogN), Space: O(1)
+    vector<int> findDisappearedNumbers_Sort(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+
+        vector<int> missing;
+        for (int pre = 0, i = 0; i <= nums.size(); ++i) {
+            int cur = i < nums.size() ? nums[i] : (nums.size() + 1);
+            for (int x = pre + 1; x < cur; ++x) {
+                missing.push_back(x);
+            }
+            pre = cur;
+        }
+
+        return missing;
+    }
+
+    // Time: O(N), Space: O(1)
+    vector<int> findDisappearedNumbers_Linear(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            int j = abs(nums[i]) - 1;
+            if (0 < nums[j]) {
+                nums[j] = -nums[j];
+            }
+        }
+
+        vector<int> missing;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (0 < nums[i]) {
+                missing.push_back(i + 1);
+            }
+        }
+
+        return missing;
+    }
+
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            int index = abs(nums[i]) - 1;
-            if (0 < nums[index]) {
-                nums[index] = -nums[index];
-            }
-        }
-
-        vector<int> result;
-        result.reserve(nums.size() / 2);
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] > 0) {
-                result.push_back(i + 1);
-            }
-        }
-
-        return result;
+        //return findDisappearedNumbers_Set(nums);
+        //return findDisappearedNumbers_Sort(nums);
+        return findDisappearedNumbers_Linear(nums);
     }
 };
