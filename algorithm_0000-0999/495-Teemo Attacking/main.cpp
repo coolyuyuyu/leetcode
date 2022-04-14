3c9499715ec66749e6c2ed4a9441a1350df5b3d1
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-15 00:03:26 +0800

    0495: linear and binary search solution

--- algorithm_0000-0999/495-Teemo Attacking/main.cpp
+++ algorithm_0000-0999/495-Teemo Attacking/main.cpp
@@ -1,86 +1,37 @@
 class Solution {
 public:
-    inline int searchInsert(const vector<int>& nums, size_t lo, size_t hi, int target) {
-        while (lo < hi) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (target <= nums[mid]) {
-                hi = mid;
+    // Time: O(N), Jump 1 by 1
+    int findPoisonedDuration_JumpOne(vector<int>& timeSeries, int duration) {
+        int totalDuration = 0;
+        for (size_t i = 0; i < timeSeries.size(); ++i) {
+            int diff = (i == 0 ? duration : (timeSeries[i] - timeSeries[i - 1]));
+            totalDuration += std::min(diff, duration);
+        }
+        
+        return totalDuration;
+    }
+    
+    // Jump across distance as duration
+    int findPoisonedDuration_JumpDuration(vector<int>& timeSeries, int duration) {
+        int totalDuration = 0;
+        for (size_t i = 0; i < timeSeries.size();) {
+            totalDuration += duration;
+            
+            size_t j = std::lower_bound(timeSeries.begin() + i + 1, timeSeries.end(), timeSeries[i] + duration) - timeSeries.begin();            
+            if (i + 1 == j) {
+                i = j;
             }
             else {
-                assert(nums[mid] < target);
-                lo = mid + 1;
-            }
+                totalDuration -= (timeSeries[i] + duration - timeSeries[j - 1]);
+                i = j - 1;
+            }            
         }
-
-        assert(lo == hi);
-        return hi;
+        
+        return totalDuration;
     }
-
-    // Time(n): O((n/duration)(logn))
-    int findPoisonedDurationV1(const vector<int>& timeSeries, int duration) {
-        if (duration == 0) {
-            return 0;
-        }
-
-        int totalTime = 0;
-        for (size_t index = 0; index < timeSeries.size();) {
-            int target = timeSeries[index] + duration;
-            size_t indexTmp = searchInsert(timeSeries, index + 1, timeSeries.size(), target);
-            if (index + 1 == indexTmp) {
-                totalTime += duration;
-                index = indexTmp;
-            }
-            else {
-                totalTime += (timeSeries[indexTmp - 1] - timeSeries[index]);
-                index = indexTmp - 1;
-            }
-        }
-
-        return totalTime;
-    }
-
-    // Time(n): O(n)
-    int findPoisonedDurationV2(const vector<int>& timeSeries, int duration) {
-        if (timeSeries.empty()) {
-            return 0;
-        }
-
-        int totalTime = duration;
-        for (size_t i = 1; i < timeSeries.size(); ++i) {
-            totalTime += min(duration, timeSeries[i] - timeSeries[i - 1]);
-        }
-
-        return totalTime;
-    }
-
-    // Mix of V1 and V2
-    int findPoisonedDurationV3(const vector<int>& timeSeries, int duration) {
-        if (timeSeries.empty()) {
-            return 0;
-        }
-
-        int totalTime = duration;
-        for (size_t index = 0; index + 1 < timeSeries.size();) {
-            if (timeSeries[index + 1] - timeSeries[index] >= duration) {
-                totalTime += duration;
-                index += 1;
-            }
-            else {
-                int target = timeSeries[index] + duration;
-                size_t indexTmp = searchInsert(timeSeries, index + 1, timeSeries.size(), target);
-                totalTime += (timeSeries[indexTmp - 1] - timeSeries[index]);
-                index = indexTmp - 1;
-            }
-        }
-
-        return totalTime;
-    }
-
+    
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        //return findPoisonedDurationV1(timeSeries, duration);
-
-        //return findPoisonedDurationV2(timeSeries, duration);
-
-        return findPoisonedDurationV3(timeSeries, duration);
+        //return findPoisonedDuration_JumpOne(timeSeries, duration);
+        return findPoisonedDuration_JumpDuration(timeSeries, duration);
     }
 };
