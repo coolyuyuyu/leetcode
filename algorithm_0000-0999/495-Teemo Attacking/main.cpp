d5ca14a37dbd5d0611289c20c9e4fa0c9cdaaecb
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-15 21:30:55 +0800

    0495: cleanup

--- algorithm_0000-0999/495-Teemo Attacking/main.cpp
+++ algorithm_0000-0999/495-Teemo Attacking/main.cpp
@@ -7,29 +7,29 @@ public:
             int diff = (i == 0 ? duration : (timeSeries[i] - timeSeries[i - 1]));
             totalDuration += std::min(diff, duration);
         }
-        
+
         return totalDuration;
     }
-    
+
     // Jump across distance as duration
     int findPoisonedDuration_JumpDuration(vector<int>& timeSeries, int duration) {
         int totalDuration = 0;
         for (size_t i = 0; i < timeSeries.size();) {
             totalDuration += duration;
-            
-            size_t j = std::lower_bound(timeSeries.begin() + i + 1, timeSeries.end(), timeSeries[i] + duration) - timeSeries.begin();            
+
+            size_t j = std::lower_bound(timeSeries.begin() + i + 1, timeSeries.end(), timeSeries[i] + duration) - timeSeries.begin();
             if (i + 1 == j) {
                 i = j;
             }
             else {
                 totalDuration -= (timeSeries[i] + duration - timeSeries[j - 1]);
                 i = j - 1;
-            }            
+            }
         }
-        
+
         return totalDuration;
     }
-    
+
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         //return findPoisonedDuration_JumpOne(timeSeries, duration);
         return findPoisonedDuration_JumpDuration(timeSeries, duration);
