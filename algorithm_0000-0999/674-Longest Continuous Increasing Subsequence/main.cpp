788d11337d408503ec2d5a3c8fbd63e1473fb08d
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-17 18:37:59 +0800

    0674: update solution

--- algorithm_0000-0999/674-Longest Continuous Increasing Subsequence/main.cpp
+++ algorithm_0000-0999/674-Longest Continuous Increasing Subsequence/main.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        }
+        assert(!nums.empty());
 
         size_t maxLen = 0;
-        size_t len = 1;
-        for (size_t i = 1; i < nums.size(); ++i) {
-            if (nums[i - 1] < nums[i]) {
+        for (size_t len = 1, i = 1; i <= nums.size(); ++i) {
+            int cur = (i == nums.size() ? nums.back() : nums[i]);
+            if (nums[i - 1] < cur) {
                 ++len;
             }
             else {
-                maxLen = max(maxLen, len);
+                maxLen = std::max(maxLen, len);
                 len = 1;
             }
-
         }
-        maxLen = max(maxLen, len);
 
         return maxLen;
     }
