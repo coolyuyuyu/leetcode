9342d9671e9c3abc6231eb098a6a41f93bb59840
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-17 16:26:01 +0800

    0682: vector solution

--- algorithm_0000-0999/682-Baseball Game/main.cpp
+++ algorithm_0000-0999/682-Baseball Game/main.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
-        stack<int> stk;
+        vector<int> scores;
         for (const string& op : ops) {
-            if (op == "+") {
-                int tmp1 = stk.top();
-                stk.pop();
-                int tmp2 = stk.top() + tmp1;
-
-                stk.push(tmp1);
-                stk.push(tmp2);
-            }
-            else if (op == "D") {
-                stk.push(stk.top() * 2);
-            }
-            else if (op == "C") {
-                stk.pop();
-            }
-            else {
-                stk.push(stoi(op));
+            switch (op.front()) {
+                case 'C':
+                    scores.pop_back();
+                    break;
+                case 'D':
+                    scores.push_back(scores.back() * 2);
+                    break;
+                case '+':
+                    scores.push_back(scores[scores.size() - 1] + scores[scores.size() - 2]);
+                    break;
+                default:
+                    scores.push_back(stoi(op, nullptr));
+                    break;
             }
         }
 
-        int sum = 0;
-        for (; !stk.empty(); stk.pop()) {
-            sum += stk.top();
-        }
-        return sum;
+        return std::accumulate(scores.begin(), scores.end(), 0);
     }
 };
