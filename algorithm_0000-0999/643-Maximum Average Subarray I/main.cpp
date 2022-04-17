8ae28dcdf0a20d501a508642417035b8702b8555
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-17 18:37:32 +0800

    0643: update solution

--- algorithm_0000-0999/643-Maximum Average Subarray I/main.cpp
+++ algorithm_0000-0999/643-Maximum Average Subarray I/main.cpp
@@ -3,18 +3,16 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         assert(k <= nums.size());
 
-        int sum = 0;
-        for (size_t i = 0; i < k; ++i) {
-            sum += nums[i];
-        }
-        double avg = sum * 1.0 / k;
-
+        int sum = std::accumulate(nums.begin(), nums.begin() + k, 0);
+        int maxSum = sum;
         for (size_t i = k; i < nums.size(); ++i) {
             sum -= nums[i - k];
             sum += nums[i];
-            avg = max(avg, sum * 1.0 / k);
+            if (maxSum < sum) {
+                maxSum = sum;
+            }
         }
 
-        return avg;
+        return maxSum * 1.0 / k;
     }
 };
