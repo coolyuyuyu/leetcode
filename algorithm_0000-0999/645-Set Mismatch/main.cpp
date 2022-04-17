f7b59180d124994fa04e6bffaa06cc1c2f952565
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-17 18:37:47 +0800

    0645: update solution

--- algorithm_0000-0999/645-Set Mismatch/main.cpp
+++ algorithm_0000-0999/645-Set Mismatch/main.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
+    // Time: O(N), Space: O(1)
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
 
-        int xor0 = 0;
-        for (int i = 1; i <= n; ++i) {
-            xor0 ^= i;
-            xor0 ^= nums[i - 1];
+        int dup;
+        int x = n * (n + 1) / 2; // 1 + 2 + ... + n
+        int y = 0; // sum of nums
+        for (int i = 0; i < n; ++i) {
+            int val = abs(nums[i]);
+            if (0 < nums[val - 1]) {
+                nums[val - 1] = -nums[val - 1];
+            }
+            else {
+                dup = val;
+            }
+
+            y += val;
         }
 
-        int xor1 = 0, xor2 = 0; {
-            int rhtMostbit = xor0 & ~(xor0 -1);
-            for (int i = 1; i <= n; ++i) {
-                if (rhtMostbit & i) {
-                    xor1 ^= i;
-                }
-                else {
-                    xor2 ^= i;
-                }
-
-                if (rhtMostbit & nums[i - 1]) {
-                    xor1 ^= nums[i - 1];
-                }
-                else {
-                    xor2 ^= nums[i - 1];
-                }
-            }
-        }
-
-        for (auto num : nums) {
-            if (num == xor1) {
-                return { xor1, xor2 };
-            }
-            else if (num == xor2) {
-                return { xor2, xor1 };
-            }
-        }
-
-        assert(false);
-        return { -1, -1 };
+        return {dup, dup + x - y};
     }
 };
