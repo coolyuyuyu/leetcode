94d89a72890b09f6bcfe0feee75ce3d78331e5ca
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-18 23:47:45 +0800

    0760: update 3 solution

--- algorithm_0000-0999/760-Find Anagram Mappings/main.cpp
+++ algorithm_0000-0999/760-Find Anagram Mappings/main.cpp
@@ -1,16 +1,50 @@
 class Solution {
 public:
-    vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
+    vector<int> anagramMappings_ArrayLookup(vector<int>& nums1, vector<int>& nums2) {
         vector<int> indexes(100001);
-        for (size_t i = 0; i < B.size(); ++i) {
-            indexes[B[i]] = i;
+        for (int i = 0; i < nums2.size(); ++i) {
+            indexes[nums2[i]] = i;
         }
 
-        vector<int> results(A.size());
-        for (size_t i = 0; i < A.size(); ++i) {
-            results[i] = indexes[A[i]];
+        vector<int> ans(nums1.size());
+        for (size_t i = 0; i < nums1.size(); ++i) {
+            ans[i] = indexes[nums1[i]];
         }
 
-        return results;
+        return ans;
+    }
+
+    vector<int> anagramMappings_MapLookup(vector<int>& nums1, vector<int>& nums2) {
+        map<int, int> indexes;
+        for (int i = 0; i < nums2.size(); ++i) {
+            indexes[nums2[i]] = i;
+        }
+
+        vector<int> ans(nums1.size());
+        for (size_t i = 0; i < nums1.size(); ++i) {
+            ans[i] = indexes[nums1[i]];
+        }
+
+        return ans;
+    }
+
+    vector<int> anagramMappings_HashLookup(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> indexes;
+        for (int i = 0; i < nums2.size(); ++i) {
+            indexes[nums2[i]] = i;
+        }
+
+        vector<int> ans(nums1.size());
+        for (size_t i = 0; i < nums1.size(); ++i) {
+            ans[i] = indexes[nums1[i]];
+        }
+
+        return ans;
+    }
+
+    vector<int> anagramMappings(vector<int>& nums1, vector<int>& nums2) {
+        //return anagramMappings_ArrayLookup(nums1, nums2);
+        //return anagramMappings_MapLookup(nums1, nums2);
+        return anagramMappings_HashLookup(nums1, nums2);
     }
 };
