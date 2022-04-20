fa19661ca70368414d9999d033827e1a334cadfd
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-20 21:28:40 +0800

    0832: std library solution

--- algorithm_0000-0999/832-Flipping an Image/main.cpp
+++ algorithm_0000-0999/832-Flipping an Image/main.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
-        size_t n = A.size();
-        for (size_t row = 0; row < n; ++row) {
-            for (size_t col = 0; col * 2 + 1 < n; ++col) {
-                int tmp = A[row][col];
-                A[row][col] = (A[row][n - col - 1] == 1 ? 0 : 1);
-                A[row][n - col - 1] = (tmp == 1 ? 0 : 1);
-            }
-
-            if (n % 2 == 1) {
-                A[row][n / 2] = (A[row][n / 2] == 1 ? 0 : 1);
-            }
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        for (vector<int>& row : image) {
+            std::reverse(row.begin(), row.end());
+            std::for_each(row.begin(), row.end(), [&](int& b) { b = (b == 1 ? 0 : 1); });
         }
-
-        return A;
+        return image;
     }
 };
