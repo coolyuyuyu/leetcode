cf5d693c7ae6a96fa1973d150223df18f212d9fa
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-20 21:29:23 +0800

    0766: update solution

--- algorithm_0000-0999/766-Toeplitz Matrix/main.cpp
+++ algorithm_0000-0999/766-Toeplitz Matrix/main.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        size_t rowCnt = matrix.size();
-        size_t colCnt = matrix.empty() ? 0 : matrix.front().size();
-        for (size_t row = 1; row < rowCnt; ++row) {
-            for (size_t col = 1; col < colCnt; ++col) {
-                if (matrix[row][col] != matrix[row - 1][col - 1]) {
-                    return false;
-                }
+    int uniqueMorseRepresentations(vector<string>& words) {
+        static const vector<string> codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        unordered_set<string> dict;
+        for (const string& word : words) {
+            string morse;
+            for (char c : word) {
+                morse += codes[c - 'a'];
             }
+            dict.insert(morse);
         }
 
-        return true;
+        return dict.size();
     }
 };
