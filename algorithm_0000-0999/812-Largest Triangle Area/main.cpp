7fd64f8cdb9d5930c6e3509e344203a52090ce52
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-20 21:28:59 +0800

    0812: update solution

--- algorithm_0000-0999/812-Largest Triangle Area/main.cpp
+++ algorithm_0000-0999/812-Largest Triangle Area/main.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     double largestTriangleArea(vector<vector<int>>& points) {
-        double maxArea = 0.0;
-
-        size_t n = points.size();
-        for (size_t i = 0; i + 2 < n; ++i) {
-            double x0 = points[i][0], y0 = points[i][1];
-            for (size_t j = i + 1; j + 1 < n; ++j) {
-                double x1 = points[j][0], y1 = points[j][1];
-                for (size_t k = j + 1; k < n; ++k) {
-                    double x2 = points[k][0], y2 = points[k][1];
-                    double area = fabs((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)) / 2;
-                    if (maxArea < area) {
-                        maxArea = area;
-                    }
+        int product = 0;
+        for (size_t i = 0; (i + 2) < points.size(); ++i) {
+            int xa = points[i][0], ya = points[i][1];
+            for (size_t j = i + 1; (j + 1) < points.size(); ++j) {
+                int xb = points[j][0], yb = points[j][1];
+                for (size_t k = j + 1; k < points.size(); ++k) {
+                    int xc = points[k][0], yc = points[k][1];
+                    product = std::max(product, std::abs((xb - xa) * (yc - ya) - (xc - xa) * (yb - ya)));
                 }
             }
         }
 
-        return maxArea;
+        return double(product) / 2;
     }
 };
