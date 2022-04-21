1f92af1d2d9a4195720a44e164e072b13294661c
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-22 00:05:20 +0800

    0914: update solution

--- algorithm_0000-0999/914-X of a Kind in a Deck of Cards/main.cpp
+++ algorithm_0000-0999/914-X of a Kind in a Deck of Cards/main.cpp
@@ -11,30 +11,19 @@ public:
     }
 
     bool hasGroupsSizeX(vector<int>& deck) {
-        unordered_map<int, int> sizes;
-        for (const int num : deck) {
-            sizes[num]++;
+        unordered_map<int, int> cnts;
+        for (int num : deck) {
+            ++cnts[num];
         }
 
-        unordered_map<int, int>::iterator itr = sizes.begin();
-        if (itr == sizes.end()) {
-            return false;
-        }
-
-        int divisor = itr->second;
-        if (divisor < 2) {
-            return false;
-        }
-
-        while (itr != sizes.end()) {
-            divisor = gcd(divisor, itr->second);
-            if (divisor < 2) {
-                return false;
+        int g = cnts.begin()->second;
+        for (const auto p : cnts) {
+            g = gcd(g, p.second);
+            if (g < 2) {
+                break;
             }
-
-            ++itr;
         }
 
-        return true;
+        return g != 1;
     }
 };
