0fe3e644677097487effee99d076a70cc4c9ff60
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-22 00:05:34 +0800

    0922: sort and swap solution

--- algorithm_0000-0999/922-Sort Array By Parity II/main.cpp
+++ algorithm_0000-0999/922-Sort Array By Parity II/main.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    vector<int> sortArrayByParityII(vector<int>& A) {
-        assert(2 <= A.size());
-
-        for (size_t i = 0, j = 1; i < A.size() && j < A.size(); i += 2) {
-            if (A[i] % 2 == 1) {
-                for (; j < A.size(); j += 2) {
-                    if (A[j] % 2 == 0) {
-                        swap(A[i], A[j]);
+    // Time: O(N)
+    vector<int> linearSwap(vector<int>& nums) {
+        for (size_t i = 0, j = 1; i < nums.size() && j < nums.size(); i += 2) {
+            if (nums[i] % 2 == 1) {
+                for (; j < nums.size(); j += 2) {
+                    if (nums[j] % 2 == 0) {
+                        swap(nums[i], nums[j]);
                         j += 2;
                         break;
                     }
@@ -15,6 +14,24 @@ public:
             }
         }
 
-        return A;
+        return nums;
+    }
+
+    // Time: O(NlogN)
+    vector<int> sortAndSwap(vector<int>& nums) {
+        // move even to left half, odd to right half.
+        std::sort(nums.begin(), nums.end(), [](int a, int b) { return a % 2 < b % 2; });
+
+        size_t n = nums.size();
+        for (size_t i = 1, j = ((n / 2) % 2 == 0 ? (n / 2): (n / 2 + 1)); j < n; i += 2, j += 2) {
+            std::swap(nums[i], nums[j]);
+        }
+
+        return nums;
+    }
+
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        //return linearSwap(nums);
+        return sortAndSwap(nums);
     }
 };
