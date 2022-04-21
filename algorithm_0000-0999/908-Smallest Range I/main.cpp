36f3a1583d42bfaaebb3b1e31708ebaed871e869
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-22 00:05:06 +0800

    0908: update solution

--- algorithm_0000-0999/908-Smallest Range I/main.cpp
+++ algorithm_0000-0999/908-Smallest Range I/main.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
-    int smallestRangeI(vector<int>& A, int K) {
-        assert(!A.empty());
+    int smallestRangeI(vector<int>& nums, int k) {
+        assert(!nums.empty());
 
-        int maxNum = A.front();
-        int minNum = A.front();
-        for (size_t i = 1; i < A.size(); ++i) {
-            if (A[i] < minNum) {
-                minNum = A[i];
-            }
-            else if (A[i] > maxNum) {
-                maxNum = A[i];
-            }
-        }
-
-        int diff = maxNum - minNum - 2 * K;
-        if (diff < 0) {
-            diff = 0;
-        }
-        return diff;
+        const auto minmax = std::minmax_element(nums.begin(), nums.end());
+        int diff = *(minmax.second) - *(minmax.first) - 2 * k;
+        return diff <= 0 ? 0 : diff;
     }
 };
