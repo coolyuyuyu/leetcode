e904b689702f4474020be216869d0fe3ca53564d
coolyuyuyu <coolyuyuyu@gmail.com>
2022-04-24 01:05:05 +0800

    0977: linear and super linear approaches

--- algorithm_0000-0999/977-Squares of a Sorted Array/main.cpp
+++ algorithm_0000-0999/977-Squares of a Sorted Array/main.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& A) {
-        vector<int> squares(A.size());
-        for (size_t front = 0, back = A.size(), index = squares.size(); front < back;) {
-            int squareFront = A[front] * A[front];
-            int squareBack = A[back - 1] * A[back - 1];
-            if (squareFront <= squareBack) {
-                squares[index - 1] = squareBack;
-                --back;
-            }
-            else {
-                squares[index - 1] = squareFront;
-                ++front;
-            }
-            --index;
+    // Time: O(NlogN)
+    vector<int> sortAndSquare(vector<int>& nums) {
+        auto comp = [](int lft, int rht) {
+            return abs(lft) < abs(rht);
+        };
+        std::sort(nums.begin(), nums.end(), comp);
+
+        for (int& num : nums) {
+            num *= num;
         }
 
-        return squares;
+        return nums;
+    }
+
+    // Time: O(N)
+    vector<int> linearConverge(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        for (size_t lft = 0, rht = nums.size(), i = nums.size(); 0 < i--;) {
+            if (abs(nums[lft]) < abs(nums[--rht])) {
+                ans[i] = nums[rht] * nums[rht];
+            }
+            else {
+                ans[i] = nums[lft] * nums[lft];
+                ++lft;
+                ++rht;
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        //return sortAndSquare(nums);
+        return linearConverge(nums);
     }
 };
