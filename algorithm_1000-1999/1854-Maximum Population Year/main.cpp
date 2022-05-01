431645a3db4959f12062b12282070e31359ea6ea
coolyuyuyu <coolyuyuyu@gmail.com>
2022-05-02 00:41:30 +0800

    1854: initial solution

--- algorithm_1000-1999/1854-Maximum Population Year/main.cpp
+++ algorithm_1000-1999/1854-Maximum Population Year/main.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
-    int maximumPopulation(vector<vector<int>>& logs) {
+    // Time: O(NlogN), Space: O(N)
+    int bySort(vector<vector<int>>& logs) {
+        vector<int> years;
+        for (const auto& log : logs) {
+            years.push_back(log[0]);
+            years.push_back(-(log[1] - 1));
+        }
+
+        auto comp = [](int y1, int y2) -> bool {
+            if (abs(y1) != abs(y2)) {
+                return abs(y1) < abs(y2);
+            }
+            else {
+                return y1 > y2;
+            }
+        };
+        std::sort(years.begin(), years.end(), comp);
+
+        int year = 0, popcnt = 0;
+        for (size_t curPopCnt = 0, maxPopCnt = 0, i = 0; i < years.size(); ++i) {
+            curPopCnt += (0 < years[i] ? 1 : -1);
+            if (maxPopCnt < curPopCnt) {
+                maxPopCnt = curPopCnt;
+                year = years[i];
+            }
+        }
+
+        return abs(year);
+    }
+
+    // Time: O(N), Space: O(1)
+    int byCount(vector<vector<int>>& logs) {
         vector<int> counts(102, 0);
         for (const auto& log : logs) {
             ++counts[log[0] - 1950];
@@ -16,5 +47,10 @@ public:
         }
 
         return (year + 1950);
+    }
+
+    int maximumPopulation(vector<vector<int>>& logs) {
+        //return bySort(logs);
+        return byCount(logs);
     }
 };
