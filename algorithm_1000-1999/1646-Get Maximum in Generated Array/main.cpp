1f607800a49e07e5f43b584c7eb7eebe5bb2e335
coolyuyuyu <coolyuyuyu@gmail.com>
2022-05-04 14:02:34 +0800

    1646: update solution

--- algorithm_1000-1999/1646-Get Maximum in Generated Array/main.cpp
+++ algorithm_1000-1999/1646-Get Maximum in Generated Array/main.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int getMaximumGenerated(int n) {
-        if (n == 0) {
+        if (n <= 0) {
             return 0;
         }
 
-        int maxVal = 1;
         vector<int> nums(n + 1);
         nums[0] = 0, nums[1] = 1;
-        for (int i = 1; i <= (n / 2); ++i) {
+
+        int maxNum = 1;
+        for (int i = 1; i <= (n / 2);  ++i) {
             nums[2 * i] = nums[i];
-            if ((2 * i + 1) <= n) {
+            if ((2 * i + 1) < nums.size()) {
                 nums[2 * i + 1] = nums[i] + nums[i + 1];
-                maxVal = max(maxVal, nums[2 * i + 1]);
+                maxNum = std::max(maxNum, nums[2 * i + 1]);
             }
         }
 
-        return maxVal;
+        return maxNum;
     }
 };
