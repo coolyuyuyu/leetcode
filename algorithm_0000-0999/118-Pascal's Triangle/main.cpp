66f8b7beb0961ba8b323405f79794996becaf0f0
coolyuyuyu <coolyuyuyu@gmail.com>
2022-05-04 13:46:51 +0800

    0118: update solution

--- algorithm_0000-0999/118-Pascal's Triangle/main.cpp
+++ algorithm_0000-0999/118-Pascal's Triangle/main.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> generate_BruteForce(int numRows) {
+    vector<vector<int>> dp(size_t numRows) {
         if (numRows == 0) {
             return {};
         }
 
         vector<vector<int>> ret(numRows);
-        ret[0].push_back(1);
+        ret[0]= {1};
         for (size_t i = 1; i < numRows; ++i) {
-            ret[i].push_back(1);
-            for (size_t j = 1; j < ret[i - 1].size(); ++j) {
-                ret[i].push_back(ret[i - 1][j - 1] + ret[i - 1][j]);
+            ret[i].resize(i + 1);
+
+            ret[i][0] = 1;
+            for (size_t j = 1; j < i; ++j) {
+                ret[i][j] = ret[i - 1][j - 1] + ret[i - 1][j];
             }
-            ret[i].push_back(1);
+            ret[i][i] = 1;
         }
 
         return ret;
     }
 
-    vector<vector<int>> generate_Math(int numRows) {
+    vector<vector<int>> math(size_t numRows) {
+        if (numRows == 0) {
+            return {};
+        }
+
         vector<vector<int>> ret(numRows);
-        for (size_t n = 0; n < numRows; ++n) {
-            ret[n].push_back(1);
-            for (size_t k = 1; k <= n; ++k) {
-                ret[n].push_back(static_cast<long long int>(ret[n].back()) * (n - k + 1) / k);
+        ret[0]= {1};
+        for (size_t i = 1; i < numRows; ++i) {
+            ret[i].resize(i + 1);
+
+            ret[i][0] = 1;
+            for (size_t k = 1; k < i; ++k) {
+                ret[i][k] = ret[i][k - 1] * (i - k + 1) / k;
             }
+            ret[i][i] = 1;
         }
 
         return ret;
     }
 
     vector<vector<int>> generate(int numRows) {
-        //return generate_BruteForce(numRows);
-        return generate_Math(numRows);
+        //return dp(numRows);
+        return math(numRows);
     }
 };
