e23dd98c34eeeb65da63d6d6a1bdff1f9d3998cb
coolyuyuyu <coolyuyuyu@gmail.com>
2022-10-16 16:30:16 +0800

    883: update solution

--- algorithm_0000-0999/883-Projection Area of 3D Shapes/main.cpp
+++ algorithm_0000-0999/883-Projection Area of 3D Shapes/main.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int projectionArea(vector<vector<int>>& grid) {
-        int area = 0;
-
-        size_t n = grid.size();
-        for (size_t i = 0; i < n; ++i) {
-            int maxRowHeight = grid[i][0];
-            int maxColHeight = grid[0][i];
-            for (size_t j = 0; j < n; ++j) {
-                if (0 < grid[i][j]) {
-                    ++area;
-
-                    if (maxRowHeight < grid[i][j]) {
-                        maxRowHeight = grid[i][j];
-                    }
+        int a = 0;
+        for (size_t x = 0, n = grid.size(); x < n; ++x) {
+            int m1 = 0, m2 = 0;
+            for (size_t y = 0; y < n; ++y) {
+                // xy
+                if (0 < grid[x][y]) {
+                    a += 1;
                 }
 
-                if (maxColHeight < grid[j][i]) {
-                    maxColHeight = grid[j][i];
+                // xz
+                if (m1 < grid[x][y]) {
+                    m1 = grid[x][y];
+                }
+
+                // yz
+                if (m2 < grid[y][x]) {
+                    m2 = grid[y][x];
                 }
             }
-
-            area += maxRowHeight;
-            area += maxColHeight;
+            a += m1;
+            a += m2;
         }
 
-        return area;
+        return a;
     }
 };
