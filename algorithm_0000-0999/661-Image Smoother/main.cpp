202621c2c05c25cee4ffca370d1decacb5cf9eb5
coolyuyuyu <coolyuyuyu@gmail.com>
2022-10-16 16:30:06 +0800

    661: update solution

--- algorithm_0000-0999/661-Image Smoother/main.cpp
+++ algorithm_0000-0999/661-Image Smoother/main.cpp
@@ -1,61 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
-        size_t rowCnt = M.size();
-        size_t colCnt = M.front().size();
-        vector<vector<int>> image(rowCnt, vector<int>(colCnt));
-        for (size_t row = 0; row < rowCnt; ++row) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                int count = 0;
-                int scale = 0;
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        size_t rowCnt = img.size(), colCnt = img.empty() ? 0 : img.front().size();
 
-                if (row > 0) {
-                    if (col > 0) {
-                        count += 1;
-                        scale += M[row - 1][col - 1];
-                    }
+        vector<vector<int>> ans(rowCnt, vector<int>(colCnt));
+        for (size_t r = 0; r < rowCnt; ++r) {
+            size_t rb = (0 < r) ? (r - 1) : r;
+            size_t re = ((r + 1) < rowCnt) ? (r + 2) : (r + 1);
+            for (size_t c = 0; c < colCnt; ++c) {
+                size_t cb = (0 < c) ? (c - 1) : c;
+                size_t ce = ((c + 1) < colCnt) ? (c + 2) : (c + 1);
 
-                    count += 1;
-                    scale += M[row - 1][col];
-
-                    if (col + 1 < colCnt) {
-                        count += 1;
-                        scale += M[row - 1][col + 1];
+                int sum = 0;
+                for (size_t ri = rb; ri < re; ++ri) {
+                    for (size_t ci = cb; ci < ce; ++ci) {
+                        sum += img[ri][ci];
                     }
                 }
-
-                if (col > 0) {
-                    count += 1;
-                    scale += M[row][col - 1];
-                }
-
-                count += 1;
-                scale += M[row][col];
-
-                if (col + 1 < colCnt) {
-                    count += 1;
-                    scale += M[row][col + 1];
-                }
-
-                if (row + 1 < rowCnt) {
-                    if (col > 0) {
-                        count += 1;
-                        scale += M[row + 1][col - 1];
-                    }
-
-                    count += 1;
-                    scale += M[row + 1][col];
-
-                    if (col + 1 < colCnt) {
-                        count += 1;
-                        scale += M[row + 1][col + 1];
-                    }
-                }
-
-                image[row][col] = scale / count;
+                ans[r][c] = sum / (re - rb) / (ce - cb);
             }
         }
-
-        return image;
+        return ans;
     }
 };
