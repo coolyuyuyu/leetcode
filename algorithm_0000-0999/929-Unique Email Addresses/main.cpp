c799ab66bc1ad3bb9a49a17e833a0d53311a0003
coolyuyuyu <coolyuyuyu@gmail.com>
2022-10-16 16:30:25 +0800

    929: update solution

--- algorithm_0000-0999/929-Unique Email Addresses/main.cpp
+++ algorithm_0000-0999/929-Unique Email Addresses/main.cpp
@@ -3,27 +3,22 @@ public:
     int numUniqueEmails(vector<string>& emails) {
         unordered_set<string> s;
         for (const string& email : emails) {
-            size_t pos = 0;
-
             string local;
-            for (bool end = false; !end;) {
-                switch (email[pos]) {
-                    case '+':
-                        pos = email.find_first_of('@', pos);
-                    case '@':
-                        end = true;
-                        break;
+            for (size_t pos = 0; pos < email.size(); ++pos) {
+                switch(email[pos]) {
                     case '.':
                         break;
+                    case '+':
+                        pos = email.find('@', pos + 1);
+                    case '@':
+                        s.emplace(local + "@" + email.substr(pos + 1));
+                        pos = email.size();
+                        break;
                     default:
-                        local.push_back(email[pos]);
+                        local += email[pos];
+                        break;
                 }
-                ++pos;
-
             }
-            string domain = email.substr(pos);
-
-            s.insert(local + "@" + domain);
         }
 
         return s.size();
