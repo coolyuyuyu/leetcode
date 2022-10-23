8112d8b6505f2798c738470955bff4679225c499
coolyuyuyu <coolyuyuyu@gmail.com>
2022-10-23 17:47:41 +0800

    003: update solution

--- algorithm_0000-0999/003-Longest Substring Without Repeating Characters/main.cpp
+++ algorithm_0000-0999/003-Longest Substring Without Repeating Characters/main.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int start = 0;
-        int length = 0;
-        vector<int> indexes(256, -1);
-        for (int i = 0; i < s.size(); ++i) {
-            start = max(start, indexes[s[i]] + 1);
-            indexes[s[i]] = i;
-            length = max(length, i - start + 1);
+    // Time: O(N^2)
+    int quadratic(const string& s) {
+        size_t maxLen = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            set<char> chars;
+            for (size_t j = i; j < s.size(); ++j) {
+                if (chars.insert(s[j]).second == false) {
+                    break;
+                }
+            }
+            maxLen = std::max(maxLen, chars.size());
         }
 
-        return length;
+        return maxLen;
+    }
+
+    // Time: O(N^2)
+    int linerar(const string& s) {
+        vector<int> indexes(256, -1);
+        int maxLen = 0;
+        for (int i = 0, start = 0; i < s.size(); ++i) {
+            start = std::max(start, indexes[s[i]] + 1); // no repeating from start to ...
+            indexes[s[i]] = i;
+            maxLen = std::max(maxLen, i - start + 1);
+        }
+
+        return maxLen;
+    }
+
+    int lengthOfLongestSubstring(string s) {
+        //return quadratic(s);
+        return linerar(s);
     }
 };
