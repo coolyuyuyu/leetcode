c45fe68319c9a25b39fb747fd51440bbf103ac41
coolyuyuyu <coolyuyuyu@gmail.com>
2022-10-26 15:58:55 +0800

    011: update solution

--- algorithm_0000-0999/011-Container With Most Water/main.cpp
+++ algorithm_0000-0999/011-Container With Most Water/main.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
+    // two pointer, Time: O(n)
     int maxArea(vector<int>& height) {
-        int area = 0;
-        int lft = 0, rht = height.size() - 1;
-        while (lft < rht) {
-            int h = min(height[lft], height[rht]);
-            area = max(area, (rht - lft) * h);
-            
+        size_t ans = 0;
+        for (size_t lft = 0, rht = height.size() - 1; lft < rht;) {
+            size_t h = std::min(height[lft], height[rht]);
+            size_t area = h * (rht - lft);
+            if (ans < area) {
+                ans = area;
+            }
+
             while (lft < rht && height[lft] <= h) {
                 ++lft;
             }
@@ -15,6 +18,6 @@ public:
             }
         }
 
-        return area;
+        return ans;
     }
 };
