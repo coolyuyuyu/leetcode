0b86bdccc552e786ac06b56461f0eb5e9f31138c
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-06 15:56:02 +0800

    2462: update solution

--- algorithm_2000-2999/2462-Total Cost to Hire K Workers/main.cpp
+++ algorithm_2000-2999/2462-Total Cost to Hire K Workers/main.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     long long totalCost(vector<int> costs, int k, int candidates) {
-        auto comp = [](pair<int, int> lft, pair<int, int> rht) {
-            if (lft.first == rht.first) {
-                return lft.second > rht.second;
-            }
-            else {
-                return lft.first > rht.first;
-            }
-        };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
-
+        priority_queue<int, vector<int>, std::greater<int>> lftPQ, rhtPQ;
 
         int lft = 0, rht = costs.size() - 1;
-        for (int i = 0; i < candidates && lft <= rht; ++lft, ++i) {
-            pq.emplace(costs[lft], lft);
-        }
-        for (int i = 0; i < candidates && lft <= rht; --rht, ++i) {
-            pq.emplace(costs[rht], rht);
+        for (int i = 0; i < candidates && lft <= rht; ++i) {
+            lftPQ.emplace(costs[lft++]);
+            if (lft <= rht) {
+                rhtPQ.emplace(costs[rht--]);
+            }
         }
 
         long long c = 0;
         for (int i = 0; i < k; ++i) {
-            pair<int, int> p = pq.top();
-            pq.pop();
-
-            c += p.first;
-
-            if (lft <= rht) {
-                if (p.second < lft) {
-                    pq.emplace(costs[lft], lft);
-                    ++lft;
+            if (lftPQ.empty()) {
+                c += rhtPQ.top();
+                rhtPQ.pop();
+            }
+            else if (rhtPQ.empty()) {
+                c += lftPQ.top();
+                lftPQ.pop();
+            }
+            else if (lftPQ.top() <= rhtPQ.top()) {
+                c += lftPQ.top();
+                lftPQ.pop();
+                if (lft <= rht) {
+                    lftPQ.push(costs[lft++]);
                 }
-                else {
-                    pq.emplace(costs[rht], rht);
-                    --rht;
+            }
+            else {
+                c += rhtPQ.top();
+                rhtPQ.pop();
+                if (lft <= rht) {
+                    rhtPQ.push(costs[rht--]);
                 }
             }
         }
-
 
         return c;
     }
