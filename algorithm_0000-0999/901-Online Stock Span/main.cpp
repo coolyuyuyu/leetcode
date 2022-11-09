a019e3fae3e9d3c1794d021c5997d70302820ce9
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-09 10:10:06 +0800

    901: correct solution

--- algorithm_0000-0999/901-Online Stock Span/main.cpp
+++ algorithm_0000-0999/901-Online Stock Span/main.cpp
@@ -1,38 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class StockSpanner {
 public:
-    pair<TreeNode*, size_t> findParentDepth(TreeNode* parent, TreeNode* node, int val, size_t depth) {
-        if (!node) {
-            return {nullptr, 0};
-        }
-
-        if (node->val == val) {
-            return {parent, depth};
-        }
-        else {
-            auto p = findParentDepth(node, node->left, val, depth + 1);
-            if (p.first) {
-                return p;
-            }
-            else {
-                return findParentDepth(node, node->right, val, depth + 1);
-            }
-        }
+    StockSpanner() {
     }
 
-    bool isCousins(TreeNode* root, int x, int y) {
-        auto p1 = findParentDepth(nullptr, root, x, 0);
-        auto p2 = findParentDepth(nullptr, root, y, 0);
-        return (p1.first != p2.first && p1.second == p2.second);
+    int next(int price) {
+        int count = 1;
+        while (!m_stk.empty() && m_stk.top().first <= price) {
+            count += m_stk.top().second;
+            m_stk.pop();
+        }
+        m_stk.emplace(price, count);
+
+        return count;
     }
+
+private:
+    stack<pair<int, int>> m_stk; // <price, count>
 };
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+ 
