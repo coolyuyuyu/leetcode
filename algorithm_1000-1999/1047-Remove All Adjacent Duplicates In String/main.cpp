ef639d251243da02b0a1b9b00b60830aa883c913
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-10 21:47:36 +0800

    1047: update solution

--- algorithm_1000-1999/1047-Remove All Adjacent Duplicates In String/main.cpp
+++ algorithm_1000-1999/1047-Remove All Adjacent Duplicates In String/main.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
-    string removeDuplicates_Stack(string S) {
-        stack<char> stk;
-        for (char c : S) {
-            if (!stk.empty() && stk.top() == c) {
-                stk.pop();
+    string removeDuplicates(string s) {
+        string ans;
+        for (char c : s) {
+            if (!ans.empty() && ans.back() == c) {
+                ans.pop_back();
             }
             else {
-                stk.push(c);
+                ans.push_back(c);
             }
         }
 
-        S.resize(stk.size());
-        for (size_t i = S.size(); 0 < i--;) {
-            S[i] = stk.top();
-            stk.pop();
-        }
-
-        return S;
-    }
-
-    string removeDuplicates_Linear(string S) {
-        string result;
-        result.reserve(S.size());
-        for (size_t i = 0; i < S.size(); ++i) {
-            if (!result.empty() && S[i] == result.back()) {
-                result.pop_back();
-            }
-            else {
-                result.push_back(S[i]);
-            }
-        }
-
-        return result;
-    }
-
-    string removeDuplicates(string S) {
-        //return removeDuplicates_Stack(S);
-        return removeDuplicates_Linear(S);
+        return ans;
     }
 };
