687874ba85c56ee186995c020ca40b06b04a6c75
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-11 21:37:22 +0800

    026: update solution

--- algorithm_0000-0999/026-Remove Duplicates from Sorted Array/main.cpp
+++ algorithm_0000-0999/026-Remove Duplicates from Sorted Array/main.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    int removeDuplicates_BruteForce(vector<int>& nums) {
+    int bruteforce(vector<int>& nums) {
         size_t i = 0;
-        for (size_t j = i + 1; j < nums.size(); ++j) {
+        for (size_t j = 1; j < nums.size(); ++j) {
             if (nums[i] < nums[j]) {
                 nums[++i] = nums[j];
             }
         }
-
         return i + 1;
     }
 
-    int removeDuplicates_BinarySearch(vector<int>& nums) {
+    int binarySearch(vector<int>& nums) {
         size_t i = 0;
-        auto first = nums.begin() + i + 1;
-        auto last = nums.end();
-        while (true) {
-            auto itr = std::lower_bound(first, last, nums[i] + 1);
-            if (itr != last) {
-                nums[++i] = *itr;
-                first += 1;
-            }
-            else {
-                break;
-            }
+        for (auto itr = nums.begin(); (itr = std::lower_bound (itr + 1, nums.end(), *itr + 1)) != nums.end();) {
+            nums[++i] = *itr;
         }
-
         return i + 1;
     }
 
     int removeDuplicates(vector<int>& nums) {
         assert(!nums.empty());
 
-        //return removeDuplicates_BruteForce(nums);
-        return removeDuplicates_BinarySearch(nums);
+        //return bruteforce(nums);
+        return binarySearch(nums);
     }
 };
