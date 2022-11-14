02485e2f10d6093328b545c01fc240c669c260f8
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-15 00:20:57 +0800

    1933: update solution

--- algorithm_1000-1999/1933-Check if String Is Decomposable Into Value-Equal Substrings/main.cpp
+++ algorithm_1000-1999/1933-Check if String Is Decomposable Into Value-Equal Substrings/main.cpp
@@ -5,19 +5,19 @@ public:
         for (size_t i = 0; i < s.size(); ) {
             size_t pos = s.find_first_not_of(s[i], i + 1);
             size_t len = pos == string::npos ? s.size() - i : pos - i;
-            if (len == 2 || (2 <= len && (len - 2) % 3 == 0)) {
+            if (2 <= len && (len - 2) % 3 == 0) {
                 if (found2Once) {
                     return false;
                 }
                 found2Once = true;
             }
             else if (len % 3 != 0) {
-
                 return false;
             }
+
             i = pos;
         }
-
+        
         return found2Once;
     }
 };
