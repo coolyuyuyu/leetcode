49856bd08e85e265510e8ed979bc4996029f3b9b
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-17 15:32:04 +0800

    223: update solution

--- algorithm_0000-0999/223-Rectangle Area/main.cpp
+++ algorithm_0000-0999/223-Rectangle Area/main.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
-    int computeIntersect(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        int lft = max(x1, x3), rht = min(x2, x4);
-        int btm = max(y1, y3), top = min(y2, y4);
+    int computeArea(int x1, int y1, int x2, int y2) {
+        return (x2 - x1) * (y2 - y1);
+    }
+
+    int computeInterset(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int lft = std::max(ax1, bx1), rht = std::min(ax2, bx2);
+        int btm = std::max(ay1, by1), top = std::min(ay2, by2);
         if (rht <= lft || top <= btm) {
             return 0;
         }
@@ -10,11 +14,7 @@ public:
         return (rht - lft) * (top - btm);
     }
 
-    int computeArea(int x1, int y1, int x2, int y2) {
-        return (x2 - x1) * (y2 - y1);
-    }
-
-    int computeArea(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return computeArea(x1, y1, x2, y2) - computeIntersect(x1, y1, x2, y2, x3, y3, x4, y4) + computeArea(x3, y3, x4, y4);
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        return computeArea(ax1, ay1, ax2, ay2) + computeArea(bx1, by1, bx2, by2) - computeInterset(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2);
     }
 };
