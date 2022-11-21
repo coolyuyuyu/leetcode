2652d57783ccf0097e45885da5cc7a6cb9db937e
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-22 00:12:09 +0800

    1099: update solution

--- algorithm_1000-1999/1099-Two Sum Less Than K/main.cpp
+++ algorithm_1000-1999/1099-Two Sum Less Than K/main.cpp
@@ -1,38 +1,82 @@
 class Solution {
 public:
-    int twoSumLessThanK_BruteForce(vector<int>& nums, int k) {
-        int sum = -1;
+    // Time: O(n^2)
+    int bruteforce(vector<int>& nums, int k) {
+        int ans = -1;
         for (size_t i = 0; i < nums.size(); ++i) {
             for (size_t j = i + 1; j < nums.size(); ++j) {
-                int s = nums[i] + nums[j];
-                if (sum < s && s < k) {
-                    sum = s;
+                int sum = nums[i] + nums[j];
+                if (sum < k) {
+                    ans = std::max(ans, sum);
                 }
             }
         }
 
-        return sum;
+        return ans;
     }
 
-    int twoSumLessThanK_Sort(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int sum = -1;
+    // Time: O(n^2)
+    int linearScanSorted(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
 
+        int ans = -1;
         for (size_t i = 0; i < nums.size(); ++i) {
-            size_t j = lower_bound(nums.begin() + i + 1, nums.end(), k - nums[i]) - nums.begin() - 1;
-            if (i < j) {
-                sum = max(sum, nums[i] + nums[j]);
+            for (size_t j = i + 1; j < nums.size(); ++j) {
+                int sum = nums[i] + nums[j];
+                if (k <= sum) {
+                    break;
+                }
+                ans = std::max(ans, sum);
             }
         }
 
-        return sum;
+        return ans;
     }
 
-    int twoSumLessThanK(vector<int>& nums, int k) {
-        // Time: O(N^2)
-        //return twoSumLessThanK_BruteForce(nums, k);
+    // Time: O(nlogn)
+    int binarySearchSorted(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
 
-        // Time: O(NlogN)
-        return twoSumLessThanK_Sort(nums, k);
+        int ans = -1;
+        for (size_t lft = 0, rht = nums.size(); lft < rht; ++lft) {
+            if (0 < lft && nums[lft - 1] == nums[lft]) {
+                continue;
+            }
+
+            size_t mid = std::lower_bound(nums.begin() + lft + 1, nums.begin() + rht, k - nums[lft]) - nums.begin();
+            if ((lft + 1) < mid) {
+                ans = std::max(ans, nums[lft] + nums[mid - 1]);
+            }
+            rht = mid;
+        }
+
+        return ans;
+    }
+
+    // Time: O(nlogn)
+    int convergeSorted(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+
+        int ans = -1;
+        for (size_t lft = 0, rht = nums.size(); (lft + 1) < rht;) {
+            int sum = nums[lft] + nums[rht - 1];
+            if (sum < k) {
+                ans = std::max(ans, sum);
+                ++lft;
+            }
+            else {
+                --rht;
+            }
+        }
+
+        return ans;
+    }
+
+
+    int twoSumLessThanK(vector<int>& nums, int k) {
+        //return bruteforce(nums, k);
+        //return linearScanSorted(nums, k);
+        //return binarySearchSorted(nums, k);
+        return convergeSorted(nums, k);
     }
 };
