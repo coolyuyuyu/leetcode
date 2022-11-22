3020344c5ef07375b641aac00d51231d963578e4
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-22 23:06:49 +0800

    053: update solution

--- algorithm_0000-0999/053-Maximum Subarray/main.cpp
+++ algorithm_0000-0999/053-Maximum Subarray/main.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN, curSum = 0;
+        int maxSum = INT_MIN, curMax = 0;
         for (int num : nums) {
-            curSum += num;
-            if (maxSum < curSum) {
-                maxSum = curSum;
-            }
-            if (curSum < 0) {
-                curSum = 0;
+            curMax += num;
+            maxSum = std::max(maxSum, curMax);
+            if (curMax < 0) {
+                curMax = 0;
             }
         }
-
         return maxSum;
     }
 };
