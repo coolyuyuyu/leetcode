dea27a99e4fa0a34feb78c51f61dfba990069f7e
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-24 23:00:28 +0800

    079: update solution

--- algorithm_0000-0999/079-Word Search/main.cpp
+++ algorithm_0000-0999/079-Word Search/main.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, const string& word, size_t index, size_t row, size_t col) {
-        ++index;
-        if (index >= word.size()) {
+    bool existRecv(vector<vector<char>>& board, size_t r, size_t c, size_t depth, const string& word) {
+        assert(depth < word.size());
+
+        if (board[r][c] != word[depth]) {
+            return false;
+        }
+        if (word.size() <= (depth + 1)) {
             return true;
         }
 
-        char c = board[row][col];
-        board[row][col] = NULL;
+        char ch = board[r][c];
 
+        board[r][c] = '.';
         bool found =
-            (0 < col && word[index] == board[row][col - 1] && exist(board, word, index, row, col - 1)) ||
-            (0 < row && word[index] == board[row - 1][col] && exist(board, word, index, row - 1, col)) ||
-            (col + 1 < board.front().size() && word[index] == board[row][col + 1] && exist(board, word, index, row, col + 1)) ||
-            (row + 1 < board.size() && word[index] == board[row + 1][col] && exist(board, word, index, row + 1, col));
-
-        board[row][col] = c;
+            (0 < c && existRecv(board, r, c - 1, depth + 1, word)) ||
+            (0 < r && existRecv(board, r - 1, c, depth + 1, word)) ||
+            ((c + 1) < board[0].size() && existRecv(board, r, c + 1, depth + 1, word)) ||
+            ((r + 1) < board.size() && existRecv(board, r + 1, c, depth + 1, word));
+        board[r][c] = ch;
 
         return found;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        size_t rowCnt = board.size();
-        size_t colCnt = board.front().size();
-        for (size_t row = 0; row < rowCnt; ++row) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                if (board[row][col] == word.front()) {
-                    if (exist(board, word, 0, row, col)) {
-                        return true;
-                    }
+        size_t row = board.size(), col = board.empty() ? 0 : board.front().size();
+        for (size_t r = 0; r < row; ++r) {
+            for (size_t c = 0; c < col; ++c) {
+                if (existRecv(board, r, c, 0, word)) {
+                    return true;
                 }
             }
         }
