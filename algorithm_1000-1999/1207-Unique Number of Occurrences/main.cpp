4571a487be0b0d6b7e0fbaa998b8888f7b37fca3
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-30 23:53:54 +0800

    1207: update solution

--- algorithm_1000-1999/1207-Unique Number of Occurrences/main.cpp
+++ algorithm_1000-1999/1207-Unique Number of Occurrences/main.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, size_t> cnts;
+        unordered_map<int, int> cnts;
         for (int num : arr) {
             ++cnts[num];
         }
 
-        unordered_set<size_t> s;
+        unordered_set<int> s;
         for (const auto& p : cnts) {
-            if (s.insert(p.second).second == false) {
+            if (s.emplace(p.second).second == false) {
                 return false;
             }
         }
