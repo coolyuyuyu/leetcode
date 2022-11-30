01ad0e14dfc84e27b328b395fb644e54be3fbe72
coolyuyuyu <coolyuyuyu@gmail.com>
2022-11-30 23:53:24 +0800

    057: add std solution

--- algorithm_0000-0999/057-Insert Interval/main.cpp
+++ algorithm_0000-0999/057-Insert Interval/main.cpp
@@ -1,61 +1,62 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    pair<size_t, size_t> searchInsert(vector<Interval>& intervals, const Interval& newInterval) {
-        size_t lft, rht;
-        size_t bgn, end;
-
-        lft = 0, rht = intervals.size();
-        while (lft < rht) {
-            size_t mid = lft + (rht - lft) / 2;
-            if (newInterval.start <= intervals[mid].end) {
+    // binary search
+    std::pair<vector<vector<int>>::iterator, vector<vector<int>>::iterator> searchInsert1(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        auto bgn = intervals.begin();
+        for (auto rht = intervals.end(); bgn < rht;) {
+            auto mid = bgn + std::distance(bgn, rht) / 2;
+            if (newInterval[0] <= mid->at(1)) {
                 rht = mid;
+            }
+            else {
+                bgn = mid + 1;
+            }
+        }
+
+        auto end = intervals.end();
+        for (auto lft = bgn; lft < end;) {
+            auto mid = lft + std::distance(lft, end) / 2;
+            if (newInterval[1] < mid->at(0)) {
+                end = mid;
             }
             else {
                 lft = mid + 1;
             }
         }
-        bgn = rht;
-
-        lft = 0, rht = intervals.size();
-        while (lft < rht) {
-            size_t mid = lft + (rht - lft) / 2;
-            if (newInterval.end + 1 <= intervals[mid].start) {
-                rht = mid;
-            }
-            else {
-                lft = mid + 1;
-            }
-        }
-        end = rht;
 
         return {bgn, end};
     }
 
-    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
-        pair<size_t, size_t> p = searchInsert(intervals, newInterval);
-        size_t bgn = p.first, end = p.second;
+    // pure std
+    std::pair<vector<vector<int>>::iterator, vector<vector<int>>::iterator> searchInsert2(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        auto bgn = std::lower_bound(
+            intervals.begin(), intervals.end(),
+            vector<int>{-1, newInterval[0]},
+            [](const vector<int>& i1, const vector<int>& i2) {
+                return i1[1] < i2[1];
+            });
+        auto end = std::upper_bound(
+            bgn, intervals.end(),
+            vector<int>{newInterval[1], -1},
+            [](const vector<int>& i1, const vector<int>& i2) {
+                return i1[0] < i2[0];
+            });
+        return {bgn, end};
+    }
 
-        vector<Interval> result;
-        result.insert(result.end(), intervals.begin(), intervals.begin() + bgn);
-
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        //auto p = searchInsert1(intervals, newInterval);
+        auto p = searchInsert2(intervals, newInterval);
+        auto bgn = p.first, end = p.second;
         if (bgn == end) {
-            result.emplace_back(newInterval);
+            intervals.insert(bgn, newInterval);
         }
         else {
-            result.emplace_back(Interval(min(intervals[bgn].start, newInterval.start), max(intervals[end - 1].end, newInterval.end)));
+            bgn->at(0) = std::min(bgn->at(0), newInterval[0]);
+            bgn->at(1) = std::max((end - 1)->at(1), newInterval[1]);
+            intervals.erase(bgn + 1, end);
         }
 
-        result.insert(result.end(), intervals.begin() + end, intervals.end());
-
-        return result;
+        return intervals;
     }
 };
