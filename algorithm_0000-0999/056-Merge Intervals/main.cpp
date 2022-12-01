c7bbc1ce46fa76e7f13d0b099e8e3c9bb970dda0
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-01 13:25:58 +0800

    056: cleanup

--- algorithm_0000-0999/056-Merge Intervals/main.cpp
+++ algorithm_0000-0999/056-Merge Intervals/main.cpp
@@ -58,7 +58,6 @@ public:
         for (size_t j = 1; j < intervals.size(); ++j) {
             if (intervals[j][0] <= intervals[i][1]) {
                 intervals[i][1] = std::max(intervals[i][1], intervals[j][1]);
-
             }
             else if (i < j){
                 intervals[++i] = intervals[j];
