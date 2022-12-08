d259ba8a87650e66090937e4f93e8143bd94b532
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-09 00:39:56 +0800

    974: prefix sum + hash solution

--- algorithm_0000-0999/974-Subarray Sums Divisible by K/main.cpp
+++ algorithm_0000-0999/974-Subarray Sums Divisible by K/main.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int subarraysDivByK(vector<int>& A, int K) {
-        vector<int> reminderCounts(K, 0);
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
 
-        int remainder = 0;
-        for (int num : A) {
-            remainder = ((remainder + num) % K + K) % K;
-            ++reminderCounts[remainder];
+        vector<int> m(k, 0); // index: reminder of presum => val: number of prefix
+        //unordered_map<int, int> m; // reminder of presum => number of prefix
+        m[0] = 1;
+
+        int ret = 0;
+        for (int presum = 0, i = 0; i < n; ++i) {
+            presum += nums[i];
+            int reminder = (presum % k + k) % k;
+
+            ret += m[reminder];
+            ++m[reminder];
         }
-
-        int count = 0;
-        for (int reminderCount : reminderCounts) {
-            if (0 < reminderCount) {
-                count += reminderCount * (reminderCount - 1) / 2;
-            }
-        }
-        count += reminderCounts[0];
-
-        return count;
+        return ret;
     }
 };
+
+/*
+X X X X X X X X
+      j       i
+num[j+1, i] is divisible by k
+presum[i] - presum[j] is divisible by k
+=> presum[i] % k == presum[j] % k
+*/
