1d720cab75ebb79f666ebb14002b764310e6862b
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-11 12:17:26 +0800

    124: update solution

--- algorithm_0000-0999/124-Binary Tree Maximum Path Sum/main.cpp
+++ algorithm_0000-0999/124-Binary Tree Maximum Path Sum/main.cpp
@@ -4,26 +4,27 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int maxPathSum(TreeNode* root) {
-        int maxPathSum = numeric_limits<int>::min();
-        maxVerticalSum(root, maxPathSum);
-        return maxPathSum;
-    }
-
-    int maxVerticalSum(TreeNode* root, int& maxPathSum) {
-        if(root == nullptr) {
+    int maxDownwardSum(TreeNode* root, int& maxPathSum) {
+        if (!root) {
             return 0;
         }
 
-        int lftMaxVerticalSum = max(maxVerticalSum(root->left, maxPathSum), 0);
-        int rhtMaxVerticalSum = max(maxVerticalSum(root->right, maxPathSum), 0);
-        maxPathSum = max(maxPathSum, root->val + lftMaxVerticalSum + rhtMaxVerticalSum);
+        int maxLftDownwardSum = max(maxDownwardSum(root->left, maxPathSum), 0);
+        int maxRhtDownwardSum = max(maxDownwardSum(root->right, maxPathSum), 0);
+        maxPathSum = std::max(maxPathSum, root->val + maxLftDownwardSum + maxRhtDownwardSum);
+        return root->val + std::max(maxLftDownwardSum, maxRhtDownwardSum);
+    }
 
-        return root->val + max(lftMaxVerticalSum, rhtMaxVerticalSum);
+    int maxPathSum(TreeNode* root) {
+        int ans = INT_MIN;
+        maxDownwardSum(root, ans);
+        return ans;
     }
 };
