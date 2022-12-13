f0cb721701e334ffde6c4293c02b6c4b1e17f351
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-13 15:36:34 +0800

    931: update solution

--- algorithm_0000-0999/931-Minimum Falling Path Sum/main.cpp
+++ algorithm_0000-0999/931-Minimum Falling Path Sum/main.cpp
@@ -1,20 +1,80 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& A) {
-        size_t rowCnt = A.size(), colCnt = A.empty() ? 0 : A.front().size();
-        for (size_t row = 1; row < rowCnt; ++row) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                int cost = A[row - 1][col];
-                if (0 < col) {
-                    cost = min(cost, A[row - 1][col - 1]);
+    // Time: O(mn), Space: O(mn)
+    int dp1(vector<vector<int>>& matrix) {
+        assert(!matrix.empty() && !matrix[0].empty());
+
+        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0] = matrix[0];
+        for (size_t i = 1; i < m; ++i) {
+            for (size_t j = 0; j < n; ++j) {
+                dp[i][j] = matrix[i][j] + dp[i - 1][j];
+                if (0 < j) {
+                    dp[i][j] = std::min(dp[i][j], matrix[i][j] + dp[i - 1][j - 1]);
                 }
-                if (col + 1 < colCnt) {
-                    cost = min(cost, A[row - 1][col + 1]);
+                if (j + 1 < n) {
+                    dp[i][j] = std::min(dp[i][j], matrix[i][j] + dp[i - 1][j + 1]);
                 }
-                A[row][col] += cost;
             }
         }
 
-        return *min_element(A.back().begin(), A.back().end());
+        return *std::min_element(dp[m - 1].begin(), dp[m - 1].end());
+    }
+
+    // Time: O(mn), Space: O(n)
+    int dp2(vector<vector<int>>& matrix) {
+        assert(!matrix.empty() && !matrix[0].empty());
+
+        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+
+        vector<int> dp(matrix[0]);
+        for (size_t i = 1; i < m; ++i) {
+            int pre;
+            for (size_t j = 0; j < n; ++j) {
+                int tmp = dp[j];
+
+                dp[j] = matrix[i][j] + dp[j];
+                if (0 < j) {
+                    dp[j] = std::min(dp[j], matrix[i][j] + pre);
+                }
+                if (j + 1 < n) {
+                    dp[j] = std::min(dp[j], matrix[i][j] + dp[j + 1]);
+                }
+
+                pre = tmp;
+            }
+        }
+
+        return *std::min_element(dp.begin(), dp.end());
+    }
+
+    // Time: O(mn), Space: O(1))
+    int dp3(vector<vector<int>>& matrix) {
+        assert(!matrix.empty() && !matrix[0].empty());
+
+        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+
+        for (size_t i = 1; i < m; ++i) {
+            for (size_t j = 0; j < n; ++j) {
+                int cost = matrix[i - 1][j];
+                if (0 < j) {
+                    cost = std::min(cost, matrix[i - 1][j - 1]);
+                }
+                if (j + 1 < n) {
+                    cost = std::min(cost, matrix[i - 1][j + 1]);
+                }
+                matrix[i][j] += cost;
+            }
+        }
+
+        return *std::min_element(matrix[m - 1].begin(), matrix[m - 1].end());
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        //return dp1(matrix);
+        //return dp2(matrix);
+        return dp3(matrix);
     }
 };
