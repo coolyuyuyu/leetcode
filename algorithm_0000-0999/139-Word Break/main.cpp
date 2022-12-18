7efaf35d42e18a0ef909f99bfbd080405405b326
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-19 00:52:04 +0800

    139: dfs and trie solution

--- algorithm_0000-0999/139-Word Break/main.cpp
+++ algorithm_0000-0999/139-Word Break/main.cpp
@@ -1,67 +1,62 @@
 class Solution {
 public:
-
-    bool wordBreakMemoDfs(const string& s, size_t index, vector<bool>& cacheFails, const vector<string>& wordDict) {
-        size_t len = s.size();
-        if (index == len) {
+    // TLE
+    bool dfs(const vector<string>& words, const string& s, size_t index = 0) {
+        if (s.size() <= index) {
             return true;
         }
 
-        if (cacheFails[index]) {
-            return false;
-        }
-
-        size_t leftLen = len - index;
-        for (size_t i = 0; i < wordDict.size(); ++i) {
-            const string& word = wordDict[i];
-            size_t wordLen = word.size();
-            if (leftLen >= wordLen) {
-                if (strncmp(s.c_str() + index, word.c_str(), wordLen) == 0) {
-                    if (wordBreakMemoDfs(s, index + wordLen, cacheFails, wordDict)) {
-                        return true;
-                    }
-                }
+        for (const string& word : words) {
+            if (s.compare(index, word.size(), word) == 0 && dfs(words, s, index + word.size())) {
+                return true;
             }
         }
 
-        cacheFails[index] = true;
         return false;
     }
 
-    bool wordBreakMemoDfs(const string& s, const vector<string>& wordDict) {
-        vector<bool> cacheFails(s.size(), false);
-        return wordBreakMemoDfs(s, 0, cacheFails, wordDict);
+    bool dfs_memo(const vector<string>& words, const string& s, size_t index, vector<bool>& cache) {
+        if (s.size() <= index) {
+            return true;
+        }
+        if (!cache[index]) {
+            return false;
+        }
+
+        for (const string& word : words) {
+            if (s.compare(index, word.size(), word) == 0 && dfs_memo(words, s, index + word.size(), cache)) {
+                return true;
+            }
+        }
+
+        return cache[index] = false;
     }
 
+    bool dfs_memo(const vector<string>& words, const string& s) {
+        vector<bool> cahce(s.size(), true);
+        return dfs_memo(words, s, 0, cahce);
+    }
 
-    struct TrieNode {
-        TrieNode()
-            : childs()
-            , end(false) {
+    class TrieNode {
+    public:
+        TrieNode() {
+            childs.fill(nullptr);
+            end = false;
         }
-        map<char, TrieNode*> childs;
+
+        array<TrieNode*, 26> childs;
         bool end;
     };
 
     TrieNode* buildTrie(const vector<string>& words) {
-        TrieNode* root = new TrieNode();
-        for (size_t j = 0; j < words.size(); ++j) {
+        TrieNode* root = new TrieNode;
+        for (const string& word : words) {
             TrieNode* node = root;
-            const string& word = words[j];
-            for (size_t i = 0; i < word.size(); ++i) {
-                TrieNode* child = NULL; {
-                    char c = word[i];
-                    pair<map<char, TrieNode*>::iterator, bool> p = node->childs.emplace(c, nullptr);
-                    if (p.second) {
-                        child = new TrieNode();
-                        p.first->second = child;
-                    }
-                    else {
-                        child = p.first->second;
-                    }
-
+            for (char c : word) {
+                if (node->childs[c - 'a'] == nullptr) {
+                    node->childs[c - 'a'] = new TrieNode;
                 }
-                node = child;
+                node = node->childs[c - 'a'];
             }
             node->end = true;
         }
@@ -69,53 +64,55 @@ public:
         return root;
     }
 
-    bool wordBreakTrieMemoDfs(const string& s, size_t index, const TrieNode* root, vector<bool>& dones, vector<bool>& breakables) {
-        size_t len = s.size();
-        assert(index < len);
-
-        if (dones[index]) {
-            return breakables[index];
-        }
-
-        bool breakable = false;
-        const TrieNode* node = root;
-        for (size_t i = index; i < len && !breakable; ++i) {
-            map<char, TrieNode*>::const_iterator itr = node->childs.find(s[i]);
-            if (itr == node->childs.end()) {
-                break;
-            }
-
-            node = itr->second;
-            if (node->end) {
-                if (i + 1 < len) {
-                    breakable = wordBreakTrieMemoDfs(s, i + 1, root, dones, breakables);
-                }
-                else {
-                    breakable = true;
-                }
-            }
-        }
-
-        dones[index] = true;
-        breakables[index] = breakable;
-
-        return breakable;
-    }
-
-    bool wordBreakTrieMemoDfs(const string& s, const vector<string>& wordDict) {
-        if (s.empty()) {
+    bool trie(TrieNode* root, const string& s, size_t index = 0) {
+        if (s.size() <= index) {
             return true;
         }
 
-        const TrieNode* root = buildTrie(wordDict);
-        vector<bool> dones(s.size(), false);
-        vector<bool> breakables(s.size(), false);
-        return wordBreakTrieMemoDfs(s, 0, root, dones, breakables);
+        TrieNode* node = root;
+        for (size_t i = index; i < s.size() && node; ++i) {
+            node = node->childs[s[i] - 'a'];
+            if (node && node->end && trie(root, s, i + 1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // TLE
+    bool trie(const vector<string>& words, const string& s) {
+        return trie(buildTrie(words), s);
+    }
+
+    bool trie_memo(TrieNode* root, const string& s, size_t index, vector<bool>& cache) {
+        if (s.size() <= index) {
+            return true;
+        }
+        if (!cache[index]) {
+            return false;
+        }
+
+        TrieNode* node = root;
+        for (size_t i = index; i < s.size() && node; ++i) {
+            node = node->childs[s[i] - 'a'];
+            if (node && node->end && trie_memo(root, s, i + 1, cache)) {
+                return true;
+            }
+        }
+
+        return cache[index] = false;
+    }
+
+    bool trie_memo(const vector<string>& words, const string& s) {
+        vector<bool> cache(s.size(), true);
+        return trie_memo(buildTrie(words), s, 0, cache);
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
-        //return wordBreakMemoDfs(s, wordDict);
-        return wordBreakTrieMemoDfs(s, wordDict);
+        //return dfs(wordDict, s);
+        //return dfs_memo(wordDict, s);
+        //return trie(wordDict, s);
+        return trie_memo(wordDict, s);
     }
-
 };
