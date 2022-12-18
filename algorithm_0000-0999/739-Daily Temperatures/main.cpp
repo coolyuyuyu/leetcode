075e0233643965f18ad63a05f4819efe6ab4e650
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-18 17:30:34 +0800

    739: update solution

--- algorithm_0000-0999/739-Daily Temperatures/main.cpp
+++ algorithm_0000-0999/739-Daily Temperatures/main.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
+    // Time: O(n), Space: O(n)
+    vector<int> f1(const vector<int>& temperatures) {
         vector<int> ret(temperatures.size(), 0);
 
-        stack<pair<int, int>> stk; // <temperature, index>
+        stack<int> stk; // <index>
         for (int i = 0; i < temperatures.size(); ++i) {
-            while (!stk.empty() && stk.top().first < temperatures[i]) {
-                ret[stk.top().second] = i - stk.top().second;
+            while (!stk.empty() && temperatures[stk.top()] < temperatures[i]) {
+                ret[stk.top()] = i - stk.top();
                 stk.pop();
             }
-            stk.emplace(temperatures[i], i);
+            stk.emplace(i);
         }
 
         return ret;
+    }
+
+    // Time: O(n), Space: O(1);
+    vector<int> f2(const vector<int>& temperatures) {
+        vector<int> ret(temperatures.size(), 0);
+
+        stack<int> stk; // <index>
+        for (int hottest = temperatures.back(), i = temperatures.size(); 0 < i--;) {
+            if (hottest <= temperatures[i]) {
+                hottest = temperatures[i];
+                continue;
+            }
+
+            int ans = 1;
+            while (temperatures[i + ans] <= temperatures[i]) {
+                ans += ret[i + ans];
+            }
+            ret[i] = ans;
+        }
+
+        return ret;
+    }
+
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        //return f1(temperatures);
+        return f2(temperatures);
     }
 };
