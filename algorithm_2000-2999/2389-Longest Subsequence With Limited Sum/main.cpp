367fe6672156dcb285951d0f65f55df6c1bda294
coolyuyuyu <coolyuyuyu@gmail.com>
2022-12-25 12:35:26 +0800

    2389: update solution

--- algorithm_2000-2999/2389-Longest Subsequence With Limited Sum/main.cpp
+++ algorithm_2000-2999/2389-Longest Subsequence With Limited Sum/main.cpp
@@ -4,11 +4,12 @@ public:
         std::sort(nums.begin(), nums.end());
         std::partial_sum(nums.begin(), nums.end(), nums.begin());
 
-        vector<int> ans(queries.size());
-        for (size_t i = 0; i < queries.size(); ++i) {
-            ans[i] = std::upper_bound(nums.begin(), nums.end(), queries[i]) - nums.begin();
+        size_t n = queries.size();
+        vector<int> ret(queries.size());
+        for (size_t i = 0; i < n; ++i) {
+            ret[i] = std::upper_bound(nums.begin(), nums.end(), queries[i]) - nums.begin();
         }
 
-        return ans;
+        return ret;
     }
 };
