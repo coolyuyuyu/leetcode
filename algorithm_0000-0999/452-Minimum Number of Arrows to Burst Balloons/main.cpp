4f3580083660f6a292a8eea0a3a29ff99aec56ce
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-06 09:12:38 +0800

    452: update solution

--- algorithm_0000-0999/452-Minimum Number of Arrows to Burst Balloons/main.cpp
+++ algorithm_0000-0999/452-Minimum Number of Arrows to Burst Balloons/main.cpp
@@ -4,18 +4,13 @@ public:
         std::sort(points.begin(), points.end());
 
         int ret = 1;
-        for (int coverL = points[0][0], coverR = points[0][1], cnt = 1, i = 1; i < points.size(); ++i) {
-            int newCoverL = std::max(coverL, points[i][0]);
-            int newCoverR = std::min(coverR, points[i][1]);
-            if (newCoverR < newCoverL) { // not covered
-                ret += cnt;
-                coverL = points[i][0];
+        for (int coverR = points[0][1], i = 1; i < points.size(); ++i) {
+            if (coverR < points[i][0]) { // not covered
+                ++ret;
                 coverR = points[i][1];
-                cnt = 1;
             }
             else {
-                coverL = newCoverL;
-                coverR = newCoverR;
+                coverR = std::min(coverR, points[i][1]);
             }
         }
 
