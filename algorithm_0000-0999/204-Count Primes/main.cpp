3d192dc277a0b9c274708aec2f797db9f9b8748f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-07 20:41:15 +0800

    204: update solution

--- algorithm_0000-0999/204-Count Primes/main.cpp
+++ algorithm_0000-0999/204-Count Primes/main.cpp
@@ -1,54 +1,26 @@
 class Solution {
 public:
-    // slow version
-    int slow(int n) {
+    int countPrimes(int n) {
         if (n < 3) {
             return 0;
         }
 
-        int ret = 0;
+        vector<int> isPrimes(n, true);
 
-        vector<bool> isPrimes(n, true);
-        isPrimes[0] = isPrimes[1] = false;
-        for (int i = 2; i < n; ++i) {
-            if (isPrimes[i]) {
-                ++ret;
-                for (int num = i*2; num < n; num += i) {
-                    isPrimes[num] = false;
-                }
-            }
-        }
-
-        return ret;
-    }
-
-    // fast version
-    int fast(int n) {
-        if (n < 3) {
-            return 0;
-        }
-
-        int primeCount = n / 2;
-        vector<bool> isPrimes(n, true);
-        int upperLimit = sqrt(n - 1);
-        for (int i = 3; i <= upperLimit; i += 2) {
+        int count = 0;
+        for (int i = 2, limit = ceil(sqrt(n)); i < n; ++i) {
             if (!isPrimes[i]) {
                 continue;
             }
+            ++count;
 
-            for (int j = i * i; j < n; j += (2 * i)) {
-                if (isPrimes[j]) {
-                    --primeCount;
+            if (i < limit) {
+                for (int j = i * i; j < n; j += i) {
                     isPrimes[j] = false;
                 }
             }
         }
 
-        return primeCount;
-    }
-
-    int countPrimes(int n) {
-        //return slow(n);
-        return fast(n);
+        return count;
     }
 };
