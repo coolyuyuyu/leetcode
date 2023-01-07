38110f283312de17ec3c579627d58142814b05c6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-07 20:41:20 +0800

    2461: update solution

--- algorithm_2000-2999/2461-Maximum Sum of Distinct Subarrays With Length K/main.cpp
+++ algorithm_2000-2999/2461-Maximum Sum of Distinct Subarrays With Length K/main.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long sum = 0;
-        vector<size_t> cnts(*std::max_element(nums.begin(), nums.end()) + 1, 0);
-        size_t dupCnt = 0;
-        for (size_t i = 0; i < k; ++i) {
+        unordered_map<int, int> cnts;
+        long sum = 0;
+        for (int i = 0; i < k; ++i) {
             sum += nums[i];
-
-            if (cnts[nums[i]] == 1) {
-                ++dupCnt;
-            }
             ++cnts[nums[i]];
         }
 
-        long long ans = (dupCnt == 0 ? sum : 0);
-        for (size_t i = k; i < nums.size(); ++i) {
+        long ret = 0;
+        if (cnts.size() == k) {
+            ret = sum;
+        }
+        for (int i = k; i < nums.size(); ++i) {
             sum -= nums[i - k];
+            if (--cnts[nums[i - k]] == 0) {
+                cnts.erase(nums[i - k]);
+            }
             sum += nums[i];
-
-            --cnts[nums[i - k]];
-            if (cnts[nums[i - k]] == 1) {
-                --dupCnt;
-            }
-
-            if (cnts[nums[i]] == 1) {
-                ++dupCnt;
-            }
             ++cnts[nums[i]];
 
-            if (dupCnt == 0 && ans < sum) {
-                ans = sum;
+            if (cnts.size() == k) {
+                ret = std::max(ret, sum);
             }
         }
 
-        return ans;
+        return ret;
     }
 };
