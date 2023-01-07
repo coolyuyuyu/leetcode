e836d863ef70f5bc18c511efa4bf408e2c5dd545
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-07 20:41:25 +0800

    2509: update solution

--- algorithm_2000-2999/2509-Cycle Length Queries in a Tree/main.cpp
+++ algorithm_2000-2999/2509-Cycle Length Queries in a Tree/main.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
-        vector<int> ret(queries.size());
+        vector<int> lengths(queries.size());
         for (size_t i = 0; i < queries.size(); ++i) {
-            vector<int> nodes1 = {queries[i][0]};
-            while (nodes1.back() != 1) {
-                nodes1.push_back(nodes1.back() / 2);
+            int len = 1;
+            for (int a = queries[i][0], b = queries[i][1]; a != b; ++len) {
+                if (a < b) {
+                    b /= 2;
+                }
+                else {
+                    a /= 2;
+                }
             }
-            vector<int> nodes2 = {queries[i][1]};
-            while (nodes2.back() != 1) {
-                nodes2.push_back(nodes2.back() / 2);
-            }
-            while (!nodes1.empty() && !nodes2.empty() && nodes1.back() == nodes2.back()) {
-                nodes1.pop_back();
-                nodes2.pop_back();
-            }
-
-            ret[i] = nodes1.size() + nodes2.size() + 1;
-
+            lengths[i] = len;
         }
 
-        return ret;
+        return lengths;
     }
 };
