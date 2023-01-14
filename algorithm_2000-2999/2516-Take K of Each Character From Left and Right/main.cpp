f3aee671e97d05616b521ea266259e7f01b44172
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-15 01:08:14 +0800

    2516: update solution

--- algorithm_2000-2999/2516-Take K of Each Character From Left and Right/main.cpp
+++ algorithm_2000-2999/2516-Take K of Each Character From Left and Right/main.cpp
@@ -1,73 +1,67 @@
 class Solution {
 public:
     int hash_presum(const string& s, int k) {
-        if (k == 0) {
-            return 0;
+        vector<int> cntMaxs(3, 0);
+        for (char c : s) {
+            ++cntMaxs[c - 'a'];
         }
-        int n = s.size();
-
-        int totalA = 0, totalB = 0, totalC = 0;
-        for (int i = 0; i < n; ++i) {
-            char c = s[i];
-            if (c == 'a') {
-                ++totalA;
-            }
-            else if (c == 'b') {
-                ++totalB;
-            }
-            else if (c == 'c') {
-                ++totalC;
-            }
-        }
-        
-        if (!(k <= totalA && k <= totalB && k <= totalC)) {
+        if (std::any_of(cntMaxs.begin(), cntMaxs.end(), [&](int cnt) { return cnt < k; })) {
             return -1;
         }
 
+        int n = s.size();
+        int maxRetainLen = 0;
+        vector<int> cnts(3, 0);
+        vector<map<int, int>> indexes = {{{0, -1}}, {{0, -1}}, {{0, -1}}};
+        for (int i = 0; i < n; ++i) {
+            ++cnts[s[i] - 'a'];
 
-        int maxRetainLen = INT_MIN;
-
-        map<int, int> cntIndexA, cntIndexB, cntIndexC;
-        cntIndexA[0] = -1;
-        cntIndexB[0] = -1;
-        cntIndexC[0] = -1;
-        for (int aCnt = 0, bCnt = 0, cCnt = 0, i = 0; i < n; ++i) {
-            char c = s[i];
-            if (c == 'a') {
-                ++aCnt;
-            }
-            else if (c == 'b') {
-                ++bCnt;
-            }
-            else if (c == 'c') {
-                ++cCnt;
-            }
-
-            auto itrA = cntIndexA.lower_bound(k - (totalA - aCnt));
-            auto itrB = cntIndexB.lower_bound(k - (totalB - bCnt));
-            auto itrC = cntIndexC.lower_bound(k - (totalC - cCnt));
-            if (itrA != cntIndexA.end() && itrB != cntIndexB.end() && itrC != cntIndexC.end()) {
+            auto itrA = indexes[0].lower_bound(k - (cntMaxs[0] - cnts[0]));
+            auto itrB = indexes[1].lower_bound(k - (cntMaxs[1] - cnts[1]));
+            auto itrC = indexes[2].lower_bound(k - (cntMaxs[2] - cnts[2]));
+            if (itrA != indexes[0].end() && itrB != indexes[1].end() && itrC != indexes[2].end()) {
                 int i1 = i - itrA->second;
                 int i2 = i - itrB->second;
                 int i3 = i - itrC->second;
-                int p = std::min({ i1,i2,i3 });
-                maxRetainLen = std::max(maxRetainLen, p);
+
+                maxRetainLen = std::max(maxRetainLen, std::min({i1, i2, i3}));
             }
 
-            cntIndexA.emplace(aCnt, i);
-            cntIndexB.emplace(bCnt, i);
-            cntIndexC.emplace(cCnt, i);
+            indexes[s[i] - 'a'][cnts[s[i] - 'a']] = i;
         }
 
-        if (maxRetainLen == INT_MIN) {
-            return s.size();
+        return n - maxRetainLen;
+    }
+
+    int slide_window(const string& s, int k) {
+        vector<int> cnts(3, 0);
+        for (char c : s) {
+            ++cnts[c - 'a'];
         }
-        else {
-            return s.size() - maxRetainLen;
+        if (std::any_of(cnts.begin(), cnts.end(), [&](int cnt) { return cnt < k; })) {
+            return -1;
         }
+
+        int n = s.size();
+        int maxRetainLen = 0;
+        for (int lft = 0, rht = 0; lft < n; ++lft) { // [lft, rht)
+            for (; std::all_of(cnts.begin(), cnts.end(), [&](int cnt) { return k <= cnt; }); ++rht) {
+                maxRetainLen = std::max(maxRetainLen, rht - lft);
+                if (rht == n) {
+                    break;
+                }
+                
+                --cnts[s[rht] - 'a'];
+            }
+
+            ++cnts[s[lft] - 'a'];
+        }
+
+        return n - maxRetainLen;
     }
 
     int takeCharacters(string s, int k) {
-        return hash_presum(s, k);
+        //return hash_presum(s, k);
+        return slide_window(s, k);
     }
 };
