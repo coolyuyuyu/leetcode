defc884bdf77e659f530749a0818a83719f95ceb
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-25 01:48:28 +0800

    491: update solution

--- algorithm_0000-0999/491-Increasing Subsequences/main.cpp
+++ algorithm_0000-0999/491-Increasing Subsequences/main.cpp
@@ -1,52 +1,67 @@
 class Solution {
 public:
-    void recursive(const vector<int>& nums, size_t depth, vector<int>& sub, vector<vector<int>>& subs) {
-        if (1 < sub.size()) {
-            subs.push_back(sub);
-        }
-        if (nums.size() <= depth) {
+    void recursive1(const vector<int>& nums, int start, vector<int>& seq, vector<vector<int>>& seqs) {
+        if (nums.size() <= start) {
+            if (1 < seq.size()) {
+                seqs.emplace_back(seq.begin(), seq.end());
+            }
             return;
         }
 
-        unordered_set<int> s;
-        for (size_t i = depth; i < nums.size(); ++i) {
-            if (!sub.empty() && nums[i] < sub.back()) {
-                continue;
-            }
-            if (s.find(nums[i]) != s.end()) {
-                continue;
-            }
-            s.emplace(nums[i]);
+        if (seq.empty() || seq.back() < nums[start]) {
+            recursive1(nums, start + 1, seq, seqs);
 
-            sub.push_back(nums[i]);
-            recursive(nums, i + 1, sub, subs);
-            sub.pop_back();
+            seq.push_back(nums[start]);
+            recursive1(nums, start + 1, seq, seqs);
+            seq.pop_back();
+        }
+        else if (seq.back() == nums[start]) {
+            seq.push_back(nums[start]);
+            recursive1(nums, start + 1, seq, seqs);
+            seq.pop_back();
+        }
+        else {
+            recursive1(nums, start + 1, seq, seqs);
         }
     }
 
-    void recursive2(const vector<int>& nums, size_t depth, vector<int>& sub, vector<vector<int>>& subs) {
-        if (nums.size() <= depth) {
-            if (1 < sub.size()) {
-                subs.push_back(sub);
-            }
+    void recursive2(const vector<int>& nums, int start, vector<int>& seq, vector<vector<int>>& seqs) {
+        if (1 < seq.size()) {
+            seqs.emplace_back(seq.begin(), seq.end());
+        }
+        if (nums.size() <= start) {
             return;
         }
 
-        if (sub.empty() || sub.back() <= nums[depth]) {
-            sub.push_back(nums[depth]);
-            recursive2(nums, depth + 1, sub, subs);
-            sub.pop_back();
-        }
+        unordered_set<int> visited;
+        for (int i = start; i < nums.size(); ++i) {
+            if (visited.find(nums[i]) != visited.end()) {
+                continue;
+            }
 
-        if (sub.empty() || sub.back() != nums[depth]) {
-            recursive2(nums, depth + 1, sub, subs);
+            if (seq.empty() || seq.back() <= nums[i]) {
+                visited.insert(nums[i]);
+
+                seq.push_back(nums[i]);
+                recursive2(nums, i + 1, seq, seqs);
+                seq.pop_back();
+            }
         }
     }
 
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<int> sub;
-        vector<vector<int>> subs;
-        recursive2(nums, 0, sub, subs);
-        return subs;
+        vector<int> seq;
+        vector<vector<int>> seqs;
+        //recursive1(nums, 0, seq, seqs);
+        recursive2(nums, 0, seq, seqs);
+        return seqs;
     }
 };
+
+// 0 1111 222 33
+
+// 0 1111
+// 0 .111
+// 0 ..11
+// 0 ...1
+// 0 ....
