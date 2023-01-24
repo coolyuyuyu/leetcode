576f493de13a674699510bbf4cc645d48661a9c3
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-25 01:47:46 +0800

    211: update solution

--- algorithm_0000-0999/211-Add and Search Word - Data structure design/main.cpp
+++ algorithm_0000-0999/211-Add and Search Word - Data structure design/main.cpp
@@ -1,12 +1,22 @@
 class WordDictionary {
+private:
+    class TrieNode {
+    public:
+        TrieNode()
+            : end(false) {
+            childs.fill(nullptr);
+        }
+
+        bool end;
+        array<TrieNode*, 26> childs;
+    };
+
 public:
-    /** Initialize your data structure here. */
     WordDictionary()
         : m_pRoot(new TrieNode()) {
     }
 
-    /** Adds a word into the data structure. */
-    void addWord(const string& word) {
+    void addWord(string word) {
         TrieNode** ppNode = &m_pRoot;
         for (char c : word) {
             ppNode = &((*ppNode)->childs[c - 'a']);
@@ -18,45 +28,25 @@ public:
         (*ppNode)->end = true;
     }
 
-    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     bool search(const string& word) {
         return search(word, 0, m_pRoot);
     }
 
 private:
-    class TrieNode {
-    public:
-        TrieNode()
-            : childs(26, nullptr)
-            , end(false) {
-        }
-
-        vector<TrieNode*> childs;
-        bool end;
-    };
-
-    bool search(const string& word, size_t index, TrieNode* pRoot) {
-        if (!pRoot) {
-            return false;
-        }
-
-        if (word.size() <= index) {
-            return pRoot->end;
-        }
-
-        char c = word[index];
-        if (c == '.') {
-            for (TrieNode* pChild : pRoot->childs) {
-                if (search(word, index + 1, pChild)) {
-                    return true;
-                }
+    bool search(const string& word, size_t start, TrieNode* pNode) {
+        for (size_t i = start; i < word.size() && pNode; ++i) {
+            if (word[i] == '.') {
+                return std::any_of(
+                    pNode->childs.begin(),
+                    pNode->childs.end(),
+                    [&](TrieNode* pChild) { return pChild && search(word, i + 1, pChild); });
             }
+            else {
+                pNode = pNode->childs[word[i] - 'a'];
+            }
+        }
 
-            return false;
-        }
-        else {
-            return search(word, index + 1, pRoot->childs[c - 'a']);
-        }
+        return pNode && pNode->end;
     }
 
     TrieNode* m_pRoot;
@@ -64,7 +54,7 @@ private:
 
 /**
  * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * bool param_2 = obj.search(word);
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
  */
