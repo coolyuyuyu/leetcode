ae0fe3b59e62558deb8de5fa8960446d602599d7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-25 01:48:41 +0800

    997: update solution

--- algorithm_0000-0999/997-Find the Town Judge/main.cpp
+++ algorithm_0000-0999/997-Find the Town Judge/main.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    // Time: O(n)
     int checkInOutDegree(int n, vector<vector<int>>& trust) {
         if ((trust.size() + 1) < n) {
             return -1;
         }
 
-        vector<pair<int, int>> degrees(n); // vector of <indegree, outdegree>
-        for (const auto& t : trust) {
-            degrees[t[0] - 1].second += 1;
-            degrees[t[1] - 1].first += 1;
+        vector<pair<int, int>> degrees(n); // <in, out>
+        for (const auto& edge : trust) {
+            ++degrees[edge[1] - 1].first;
+            ++degrees[edge[0] - 1].second;
         }
 
-        for (int i = 0; i < degrees.size(); ++i) {
-            if (degrees[i].first + 1 == n && degrees[i].second == 0) {
+        for (int i = 0; i < n; ++i) {
+            const auto& [in, out] = degrees[i];
+            if (in == (n - 1) && out == 0) {
                 return i + 1;
             }
         }
@@ -20,19 +22,20 @@ public:
         return -1;
     }
 
+    // Variation of in/out degree
     int checkScore(int n, vector<vector<int>>& trust) {
         if ((trust.size() + 1) < n) {
             return -1;
         }
 
-        vector<int> scores(n); // vector of <indegree, outdegree>
-        for (const auto& t : trust) {
-            scores[t[0] - 1] -= 1;
-            scores[t[1] - 1] += 1;
+        vector<int> scores(n);
+        for (const auto& edge : trust) {
+            ++scores[edge[1] - 1];
+            --scores[edge[0] - 1];
         }
 
-        for (int i = 0; i < scores.size(); ++i) {
-            if (scores[i] + 1 == n) {
+        for (int i = 0; i < n; ++i) {
+            if (scores[i] == (n - 1)) {
                 return i + 1;
             }
         }
