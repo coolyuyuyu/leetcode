cfeff686765d994f9c3165c0ab0ab0a0d68b7117
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-25 01:47:14 +0800

    093: update solution

--- algorithm_0000-0999/093-Restore IP Addresses/main.cpp
+++ algorithm_0000-0999/093-Restore IP Addresses/main.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
-    void restoreIpAddresses(const string& s, size_t index, size_t level, string& result, vector<string>& results) {
-        if (level == 0) {
-            results.emplace_back(result);
-            return;
-        }
-
-        size_t len = s.size() - index;
-        if (level * 3 < len || len < level) {
-            return;
-        }
-
-        if (1 < level) {
-            if (s[index] == '0') {
-                result += "0.";
-                restoreIpAddresses(s, index + 1, level - 1, result, results);
-                result.resize(result.size() - 2);
-            }
-            else {
-                string numStr;
-                for (size_t i = 0, num = 0; i < 3; ++i) {
-                    num = num * 10 + s[index + i] - '0';
-                    if (255 < num) {
-                        break;
+    vector<string> bruteforce(const string& s) {
+        vector<string> ret;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = i + 1; j < (i + 4); ++j) {
+                for (int k = j + 1; k < (j + 4); ++k) {
+                    if (s.size() <= (k + 1) || (k + 4) < s.size()) {
+                        continue;
                     }
-                    numStr.push_back(s[index + i]);
 
-                    result += (numStr + ".");
-                    restoreIpAddresses(s, index + i + 1, level - 1, result, results);
-                    result.resize(result.size() - i - 2);
+                    string a = s.substr(0, i + 1);
+                    if (255 < stoi(a) || to_string(stoi(a)) != a) {
+                        continue;
+                    }
+                    string b = s.substr(i + 1, j - i);
+                    if (255 < stoi(b) || to_string(stoi(b)) != b) {
+                        continue;
+                    }
+                    string c = s.substr(j + 1, k - j);
+                    if (255 < stoi(c) || to_string(stoi(c)) != c) {
+                        continue;
+                    }
+                    string d = s.substr(k + 1);
+                    if (255 < stoi(d) || to_string(stoi(d)) != d) {
+                        continue;
+                    }
+
+                    ret.push_back(a + "." + b + "." + c + "." + d);
                 }
             }
         }
-        else {
-            if (len < 2 || s[index] != '0') {
-                string numStr = s.substr(index);
-                if (atoi(numStr.c_str()) <= 255) {
-                    result += numStr;
-                    restoreIpAddresses(s, s.size(), level - 1, result, results);
-                    result.resize(result.size() - len);
-                }
-            }
-        }
+
+        // s.size() == 11
+        // 2 5 5 2 5 5 1 1 1 3 5
+        //     i     j   k
+        // 0 1 2 3 4 5 6 7 8 9 10
+        return ret;
     }
 
     vector<string> restoreIpAddresses(string s) {
-        string result;
-        vector<string> results;
-        restoreIpAddresses(s, 0, 4, result, results);
-        return results;
+        return bruteforce(s);
     }
 };
