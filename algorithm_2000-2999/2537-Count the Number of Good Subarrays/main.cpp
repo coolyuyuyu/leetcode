7d16046f3195f8f6a7da1c811c59021452c27e92
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-25 15:47:36 +0800

    2537: update solution

--- algorithm_2000-2999/2537-Count the Number of Good Subarrays/main.cpp
+++ algorithm_2000-2999/2537-Count the Number of Good Subarrays/main.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    // slide window, Time: O(n)
+    // sliding window, Time: O(n)
     long long countGood(vector<int>& nums, int k) {
         int n = nums.size();
 
-        map<int, long long> m;
-        long long sum = 0, cnt = 0;
+        unordered_map<int, int> cnts;
+        long long numPairs = 0, ret = 0;
         for (int lft = 0, rht = 0; lft < n; ++lft) {
-            while (sum < k) {
-                if (rht == n) {
-                    break;
-                }
-
-                sum += m[nums[rht]]++;
-                ++rht;
+            for (; rht < n && numPairs < k; ++rht) {
+                numPairs += cnts[nums[rht]]++;
             }
-            if (sum >= k) {
-                cnt += (n - rht + 1);
+            if (k <= numPairs) {
+                ret += (n - rht + 1);
             }
 
-            sum -= --m[nums[lft]];
+            numPairs -= --cnts[nums[lft]];
         }
 
-        return cnt;
+        return ret;
     }
 };
