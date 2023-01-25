7fa1ec50a1aaf40066aceac25a360437eaf3953a
coolyuyuyu <coolyuyuyu@gmail.com>
2023-01-25 15:26:22 +0800

    2508: update solution

--- algorithm_2000-2999/2508-Add Edges to Make Degrees of All Nodes Even/main.cpp
+++ algorithm_2000-2999/2508-Add Edges to Make Degrees of All Nodes Even/main.cpp
@@ -1,56 +1,46 @@
 class Solution {
 public:
     bool isPossible(int n, vector<vector<int>>& edges) {
-        vector<set<int>> graph(n + 1);
+        vector<unordered_set<int>> neighbors(n + 1);
         for (const auto& edge : edges) {
-            graph[edge[0]].insert(edge[1]);
-            graph[edge[1]].insert(edge[0]);
+            neighbors[edge[0]].insert(edge[1]);
+            neighbors[edge[1]].insert(edge[0]);
         }
-        auto f = [&graph](int i, int j) {
-            return graph[i].find(j) == graph[i].end();
-
+        auto disconnected = [&](int i, int j) {
+            return neighbors[i].find(j) == neighbors[i].end();
         };
 
         vector<int> odds;
         for (int i = 1; i <= n; ++i) {
-            if (graph[i].size() % 2 == 1) {
+            if ((neighbors[i].size() % 2) == 1) {
                 odds.push_back(i);
             }
         }
+
         if (odds.size() == 0) {
             return true;
         }
-        else if (odds.size() == 1 || odds.size() == 3) {
-            return false;
-        }
         else if (odds.size() == 2) {
             int a = odds[0], b = odds[1];
-            if (f(a, b)) {
+            if (disconnected(a, b)) {
                 return true;
             }
-            else {
-                for (int i = 1; i <= n; ++i) {
-                    if (f(i, a) && f(i, b)) {
-                        return true;
-                    }
-                }
 
-                return false;
+            for (int i = 1; i <= n; ++i) {
+                if (disconnected(a, i) && disconnected(b, i)) {
+                    return true;
+                }
             }
         }
         else if (odds.size() == 4) {
             int a = odds[0], b = odds[1], c = odds[2], d = odds[3];
-            if ((f(a, b) && f(c, d)) ||
-                (f(a, c) && f(b, d)) ||
-                (f(a, d) && f(b, c))) {
+            if ((disconnected(a, b) && disconnected(c, d)) ||
+                (disconnected(a, c) && disconnected(b, d)) ||
+                (disconnected(a, d) && disconnected(b, c))) {
                 return true;
             }
-            else {
-                return false;
-            }
         }
-        else {
-            return false;
-        }
+
+        return false;
     }
 };
