077fcf28d7e3886fe375d362ef794516aa9ff2d6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-02 01:34:31 +0800

    28: update solution

--- algorithm_0000-0999/028-Implement strStr()/main.cpp
+++ algorithm_0000-0999/028-Implement strStr()/main.cpp
@@ -1,75 +1,78 @@
 class Solution {
 public:
-    vector<size_t> getNexts1(const string& pattern) {
-        if (pattern.empty()) {
-            return {};
+
+    vector<size_t> longestPrefix(const string& s) {
+        // suffix array
+        // dp[i]: the maximal value k such that s[0:k-1] == s[i-k+1:i]
+        vector<size_t> dp(s.size());
+        dp[0] = 0;
+        for (size_t i = 1; i < s.size(); ++i) {
+            size_t j = dp[i - 1];
+            while (0 < j  && s[j] != s[i]) {
+                j = dp[j - 1];
+            }
+            dp[i] = j + (s[j] == s[i] ? 1 : 0);
         }
 
-        vector<size_t> nexts(pattern.size());
-        nexts[0] = 0;
-
-        size_t i = 1, j = 0;
-        while (i < pattern.size()) {
-            if (j == 0 || pattern[i - 1] == pattern[j - 1]) {
-                ++i; ++j;
-                nexts[i - 1] = j;
-            }
-            else {
-                j = nexts[j - 1];
-            }
-        }
-
-        return nexts;
+        return dp;
     }
 
-    vector<size_t> getNexts2(const string& pattern) {
-        if (pattern.empty()) {
-            return {};
+    int dp1(const string& s, const string& t) {
+        vector<size_t> suffix = longestPrefix(t);
+
+        // kmp
+        // dp[i]: the maximum value k such that s[i-k+1:i] == t[0:k-1]
+        vector<size_t> dp(s.size());
+        dp[0] = (s[0] == t[0] ? 1 : 0);
+        if (dp[0] == 1 && t.size() == 1) {
+            return 0;
         }
-
-        vector<size_t> nexts(pattern.size());
-        nexts[0] = 0;
-
-        size_t i = 1, j = 0;
-        while (i < pattern.size()) {
-            if (j == 0 || pattern[i - 1] == pattern[j - 1]) {
-                ++i; ++j;
-                if (pattern[i - 1] == pattern[j - 1]) {
-                    nexts[i - 1] = nexts[j - 1];
-                }
-                else {
-                    nexts[i - 1] = j;
-                }
+        for (size_t i = 1; i < s.size(); ++i) {
+            size_t j = dp[i - 1];
+            while (0 < j && s[i] != t[j]) {
+                j = suffix[j - 1];
             }
-            else {
-                j = nexts[j - 1];
+            dp[i] = j + (s[i] == t[j] ? 1 : 0);
+
+            if (dp[i] == t.size()) {
+                return i - t.size() + 1;
             }
         }
 
-        return nexts;
+        return -1;
+    }
+
+    // space optimized
+    int dp2(const string& s, const string& t) {
+        vector<size_t> suffix = longestPrefix(t);
+
+        // kmp
+        // dp[i]: the maximum value k such that s[i-k+1:i] == t[0:k-1]
+        // cur: dp[i], pre: dp[i-1]
+        size_t cur = (s[0] == t[0] ? 1 : 0);
+        if (cur == 1 && t.size() == 1) {
+            return 0;
+        }
+        size_t pre = cur;
+        for (size_t i = 1; i < s.size(); ++i) {
+            size_t j = pre;
+            while (0 < j && s[i] != t[j]) {
+                j = suffix[j - 1];
+            }
+            cur = j + (s[i] == t[j] ? 1 : 0);
+
+            if (cur == t.size()) {
+                return i - t.size() + 1;
+            }
+
+            pre = cur;
+        }
+
+        return -1;
     }
 
     int strStr(string haystack, string needle) {
-        if (haystack.size() < needle.size()) {
-            return -1;
-        }
-
-        vector<size_t> nexts = getNexts1(needle);
-        size_t i = 1, j = 1;
-        while (i <= haystack.size() && j <= needle.size()) {
-            if (j == 0 || haystack[i - 1] == needle[j - 1]) {
-                ++i; ++j;
-            }
-            else {
-                j = nexts[j - 1];
-            }
-        }
-
-        if (needle.size() < j) {
-            return i - needle.size() - 1;
-        }
-        else {
-            return -1;
-        }
+        //return dp1(haystack, needle);
+        return dp2(haystack, needle);
     }
 };
