fbb7255b713f566a6fa0330f4168963878a091a1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-03 11:04:09 +0800

    006: update solution

--- algorithm_0000-0999/006-ZigZag Conversion/main.cpp
+++ algorithm_0000-0999/006-ZigZag Conversion/main.cpp
@@ -1,37 +1,57 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
+    // Time: O(n), Space(n)
+    string f1(const string&s, int numRows) {
+        vector<string> lines(numRows);
+
+        for (int i = 0; i < s.size();) {
+            for (int r = 0; r < numRows && i < s.size(); ++r) {
+                lines[r].push_back(s[i++]);
+            }
+
+            for (int r = numRows - 1; 1 < r-- && i < s.size();) {
+                lines[r].push_back(s[i++]);
+            }
+        }
+
+        string ret;
+        for (const string& line : lines) {
+            ret += line;
+        }
+
+        return ret;
+    }
+
+    // Time: O(n), Space(1)
+    string f2(const string&s, int numRows) {
         if (numRows == 1) {
             return s;
         }
 
-        string t = "";
-        for (int i = 0; i < s.size(); i += (2 * numRows - 2)) {
-            t.push_back(s.at(i));
+        string ret;
+
+        int numOfCharsInSection = 2 * (numRows - 1);
+        for (int i = 0; i < s.size(); i += numOfCharsInSection) {
+            ret += s[i];
         }
-
-        for (int i = 1; (i + 1) < numRows; ++i) {
-            int x = i;
-            int y = i + (2 * numRows - 2) - i * 2;
-            while (true) {
-                if (s.size() <= x) {
-                    break;
+        for (int r = 1; r < (numRows - 1); ++r) {
+            int dist = numOfCharsInSection - 2 * r;
+            for (int i = r; i < s.size(); i += numOfCharsInSection) {
+                ret += s[i];
+                if ((i + dist) < s.size()) {
+                    ret += s[i + dist];
                 }
-                t.push_back(s.at(x));
-                x += (2 * numRows - 2);
-
-                if (s.size() <= y) {
-                    break;
-                }
-                t.push_back(s.at(y));
-                y += (2 * numRows - 2);
             }
         }
-
-        for (int i = numRows - 1; i < s.size(); i += (2 * numRows - 2)) {
-            t.push_back(s.at(i));
+        for (int i = numRows - 1; i < s.size(); i += numOfCharsInSection) {
+            ret += s[i];
         }
 
-        return t;
+        return ret;
+    }
+
+    string convert(string s, int numRows) {
+        //return f1(s, numRows);
+        return f2(s, numRows);
     }
 };
