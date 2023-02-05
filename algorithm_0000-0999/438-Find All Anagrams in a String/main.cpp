20fc4f9cbc012a21b8aea28a4284bf683a1ecb2d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-06 01:00:34 +0800

    438: update solution

--- algorithm_0000-0999/438-Find All Anagrams in a String/main.cpp
+++ algorithm_0000-0999/438-Find All Anagrams in a String/main.cpp
@@ -1,52 +1,29 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> counts(26, 0);
-        for (auto c : p) {
-            ++counts[c - 'a'];
+        unordered_map<char, int> cnts;
+        for (char c : p) {
+            ++cnts[c];
         }
+        int diffCharCnt = cnts.size();
 
-        size_t bgn = 0;
-        size_t end = 0;
-        vector<int> result;
+        vector<int> ret;
         for (size_t i = 0; i < s.size(); ++i) {
-            if ((end - bgn) == p.size()) {
-                result.push_back(bgn);
-                if (!p.empty()) {
-                    ++counts[s[bgn] - 'a'];
-                }
-                ++bgn;
-                end = max(bgn, end);
+            if (--cnts[s[i]] == 0) {
+                --diffCharCnt;
             }
 
-            if (counts[s[i] - 'a']) {
-                --counts[s[i] - 'a'];
-                ++end;
-            }
-            else {
-                bool found = false;
-                for (size_t j = bgn; j < end; ++j) {
-                    ++counts[s[j] - 'a'];
-                    if (s[j] == s[i]) {
-                        --counts[s[j] - 'a'];
-                        bgn = j + 1;
-                        ++end;
-                        found = true;
-                        break;
-                    }
+            if (p.size() <= (i + 1)) {
+                if (diffCharCnt == 0) {
+                    ret.push_back(i + 1 - p.size());
                 }
 
-                if (!found) {
-                    bgn = i + 1;
-                    end = max(bgn, end);
+                if (cnts[s[i + 1 - p.size()]]++ == 0) {
+                    ++diffCharCnt;
                 }
             }
         }
 
-        if ((end - bgn) == p.size()) {
-            result.push_back(bgn);
-        }
-
-        return result;
+        return ret;
     }
 };
