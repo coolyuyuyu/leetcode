a3cafe653e2d7fe78cc41141a806195cb20a359b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-09 02:54:36 +0800

    280: update solution

--- algorithm_0000-0999/280-Wiggle Sort/main.cpp
+++ algorithm_0000-0999/280-Wiggle Sort/main.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    void wiggleSort(vector<int>& nums) {
-        for (size_t i = 0; i + 1 < nums.size(); ++i) {
-            if (i % 2 == 0) {
-                if (nums[i] > nums[i + 1]) {
-                    swap(nums[i], nums[i + 1]);
+    // Time: O(NlogN)
+    void bySort(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for (size_t i = 2; i < nums.size(); i += 2) {
+            std::swap(nums[i - 1], nums[i]);
+        }
+    }
+
+    // Time: O(N)
+    void byGreedy(vector<int>& nums) {
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (i % 2) { // odd index
+                if (nums[i] < nums[i - 1]) {
+                    std::swap(nums[i - 1], nums[i]);
                 }
             }
-            else {
-                if (nums[i] < nums[i + 1]) {
-                    swap(nums[i], nums[i + 1]);
+            else { // even index
+                if (nums[i - 1] < nums[i]) {
+                    std::swap(nums[i - 1], nums[i]);
                 }
             }
         }
+    }
+    void wiggleSort(vector<int>& nums) {
+        //bySort(nums);
+        byGreedy(nums);
     }
 };
