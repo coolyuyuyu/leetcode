0dc2001658c35a5af2f0596bfb7ebef16f4944cf
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-09 02:54:48 +0800

    2558: initial solution

---
+++ algorithm_2000-2999/2558-Take Gifts From the Richest Pile/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq(gifts.begin(), gifts.end());
+        for (; 1 < pq.top() && 0 < k--;) {
+            int gift = pq.top();
+            pq.pop();
+
+            pq.push(sqrt(gift));
+        }
+
+        long long ret = 0;
+        for (; !pq.empty(); pq.pop()) {
+            ret += pq.top();
+        }
+
+        return ret;
+    }
+};
