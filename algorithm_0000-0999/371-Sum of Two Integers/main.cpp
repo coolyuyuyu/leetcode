456b6ea2f5d8b561733eb4eae27825ff8b1ad6a7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-15 00:56:42 +0800

    371: update solution

--- algorithm_0000-0999/371-Sum of Two Integers/main.cpp
+++ algorithm_0000-0999/371-Sum of Two Integers/main.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while (b != 0) {
-            int carry = (a & b) << 1;
-            a = a ^ b;
+            unsigned int carry = (unsigned int)(a & b) << 1;
+            a ^= b;
             b = carry;
         }
+
         return a;
     }
 };
