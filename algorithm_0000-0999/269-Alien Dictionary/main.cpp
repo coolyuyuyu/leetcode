2f16d97d83d5655495ba2d9b715dd89b8226a5ea
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-17 17:26:03 +0800

    269: update solution

--- algorithm_0000-0999/269-Alien Dictionary/main.cpp
+++ algorithm_0000-0999/269-Alien Dictionary/main.cpp
@@ -1,101 +1,150 @@
-class Solution {
+class Trie {
 public:
-    class Trie {
-    public:
-        Trie()
-            : m_pRoot(new TrieNode()) {
-        }
-
-        ~Trie() {
-            assert(m_pRoot);
-            destruct(m_pRoot);
-        }
-
-        void addWord(const string& word) {
-            TrieNode* pNode = m_pRoot;
-            for (char c : word) {
-                size_t index = c - 'a';
-                if (pNode->m_childs[index] == nullptr) {
-                    pNode->m_childs[index] = new TrieNode();
-                }
-
-                m_adjLists.emplace(c, unordered_set<char>());
-                for (size_t i = 0; i < TrieNode::CHILD_COUNT; ++i) {
-                    if (pNode->m_childs[i] == nullptr || i == index) {
-                        continue;
-                    }
-                    m_adjLists[char('a' + i)].emplace(c);
-                }
-
-                pNode = pNode->m_childs[index];
-            }
-        }
-
-        unordered_map<char, unordered_set<char>>& getAdjLists() {
-            return m_adjLists;
-        }
-
-    private:
-        class TrieNode {
-        public:
-            TrieNode() {
-            }
-
-            static const size_t CHILD_COUNT = 26;
-            TrieNode* m_childs[CHILD_COUNT] = {nullptr};
-        };
-
-        void destruct(TrieNode* root) {
-            if (root) {
-                for (size_t i = 0; i < TrieNode::CHILD_COUNT; ++i) {
-                    if (root->m_childs[i]) {
-                        destruct(root->m_childs[i]);
-                    }
-                }
-
-                delete root;
-            }
-        }
-
-        TrieNode* m_pRoot;
-        unordered_map<char, unordered_set<char>> m_adjLists;
-    };
-
-    string alienOrder(vector<string>& words) {
-        Trie trie;
+    Trie(const vector<string>& words)
+        : m_root(new Node)
+        , m_success(true) {
         for (const string& word : words) {
-            trie.addWord(word);
+            addWord(word);
         }
-        unordered_map<char, unordered_set<char>>& adjLists = trie.getAdjLists();
+    }
 
-        unordered_map<char, int> inCounts;
-        for (auto p : adjLists) {
-            const unordered_set<char>& adjList = p.second;
-            inCounts.emplace(p.first, 0);
-            for (char neighbor : adjList) {
-                ++inCounts[neighbor];
-            }
-        }
-
-        string letters;
-        queue<char> q;
-        for (auto p : inCounts) {
-            if (p.second == 0) {
-                q.emplace(p.first);
-            }
-        }
+    virtual ~Trie() {
+        queue<Node*> q({m_root});
         while (!q.empty()) {
-            char c = q.front();
+            Node* node = q.front();
             q.pop();
 
-            letters.push_back(c);
-            for (char neighbor : adjLists[c]) {
-                if (--inCounts[neighbor] == 0) {
-                    q.emplace(neighbor);
+            for (Node* child : node->m_childs) {
+                if (child) {
+                    q.push(child);
+                }
+            }
+
+            delete node;
+        }
+    }
+
+    void addWord(const string& word) {
+        if (!m_success) {
+            return;
+        }
+
+        Node* node = m_root;
+        for (char c : word) {
+            if (node->m_last != c && node->m_childs[c - 'a']) {
+                m_success = false;
+                return;
+            }
+
+            if (node->m_childs[c - 'a'] == nullptr) {
+                node->m_childs[c - 'a'] = new Node();
+
+                if (node->m_last) {
+                    m_adjLists[node->m_last].insert(c);
+                }
+            }
+
+            node->m_last = c;
+            node = node->m_childs[c - 'a'];
+        }
+
+        if (std::any_of(node->m_childs.begin(), node->m_childs.end(), [](Node* node) { return node != nullptr; })) {
+            m_success = false;
+        }
+    }
+
+    const unordered_map<char, unordered_set<char>>& getAdjLists(bool& success) {
+        success = m_success;
+        return m_adjLists;
+    }
+
+private:
+    struct Node {
+        Node() {
+            m_childs.fill(nullptr);
+            m_last = '\0';
+        }
+
+        array<Node*, 26> m_childs;
+        char m_last;
+    };
+
+    Node* m_root;
+    bool m_success;
+    unordered_map<char, unordered_set<char>> m_adjLists;
+};
+
+class Solution {
+public:
+    unordered_map<char, unordered_set<char>> constructGraphByStrComp(const vector<string>& words, bool& success) {
+        unordered_map<char, unordered_set<char>> adjLists;
+        for (size_t i = 1; i < words.size(); ++i) {
+            const string& s = words[i - 1];
+            const string& t = words[i];
+
+            int j = 0, n = std::min(s.size(), t.size());
+            for (; j < n; ++j) {
+                if (s[j] != t[j]) {
+                    adjLists[s[j]].insert(t[j]);
+                    break;
+                }
+            }
+            if (j == n && t.size() < s.size()) {
+                success = false;
+                return {};
+            }
+        }
+
+        success = true;
+        return adjLists;
+    }
+
+    unordered_map<char, unordered_set<char>> constructGraphByTrie(const vector<string>& words, bool& success) {
+        unordered_map<char, unordered_set<char>> adjLists = Trie(words).getAdjLists(success);
+        return adjLists;
+    }
+
+    string alienOrder(vector<string>& words) {
+        bool success;
+        //unordered_map<char, unordered_set<char>> adjLists = constructGraphByStrComp(words, success);
+        unordered_map<char, unordered_set<char>> adjLists = constructGraphByTrie(words, success);
+        if (!success) {
+            return "";
+        }
+
+        unordered_map<char, int> inDegrees;
+        for (const string& word : words) {
+            for (char c : word) {
+                inDegrees.emplace(c, 0);
+            }
+        }
+        for (const auto& [node, nexts] : adjLists) {
+            for (char next : nexts) {
+                ++inDegrees[next];
+            }
+        }
+
+        queue<int> q;
+        for (const auto& [node, inDegree] : inDegrees) {
+            if (inDegree == 0) {
+                q.push(node);
+            }
+        }
+
+        string ret;
+        while (!q.empty()) {
+            char node = q.front();
+            q.pop();
+
+            ret.push_back(node);
+
+            for (char next : adjLists[node]) {
+                if (--inDegrees[next] == 0) {
+                    q.push(next);
                 }
             }
         }
 
-        return letters.size() == adjLists.size() ? letters : "";
+        return ret.size() == inDegrees.size() ? ret : "";
     }
 };
