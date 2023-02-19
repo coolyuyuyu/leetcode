a5332fd15313a459705de0a1d97337e4442df258
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-20 01:56:42 +0800

    2570: initial solution

---
+++ algorithm_2000-2999/2570-Merge Two 2D Arrays by Summing Values/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>> ret;
+
+        int i = 0, j = 0;
+        for (; i < nums1.size() && j < nums2.size();) {
+            if (nums1[i][0] < nums2[j][0]) {
+                ret.push_back(nums1[i++]);
+            }
+            else if (nums1[i][0] == nums2[j][0]) {
+                ret.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                ++i;
+                ++j;
+            }
+            else {
+                ret.push_back(nums2[j++]);
+            }
+        }
+        ret.insert(ret.end(), nums1.begin() + i, nums1.end());
+        ret.insert(ret.end(), nums2.begin() + j, nums2.end());
+
+        return ret;
+    }
+};
