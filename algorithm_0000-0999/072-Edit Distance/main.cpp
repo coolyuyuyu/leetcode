4eae1bffbb6688bbe392f7b6f62e36bb569d16f1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-02-28 00:45:27 +0800

    072: update solution

--- algorithm_0000-0999/072-Edit Distance/main.cpp
+++ algorithm_0000-0999/072-Edit Distance/main.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        size_t m = word1.size(), n = word2.size();
+        int m = word1.size(), n = word2.size();
 
-        // dp[i,j]: distance between word1[0,...,i-1] and word2[0,...,j-1]
-        vector<vector<size_t>> dp(m + 1, vector<size_t>(n + 1));
-        for (size_t i = 0; i <= m; ++i) {
+        word1 = "#" + word1, word2 = "#" + word2;
+
+        // dp[i][j]: minimum number of operations required to convert word1[1:i] to word2[1:j]
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        dp[0][0] = 0;
+        for (int i = 1; i <= m; ++i) {
             dp[i][0] = i;
         }
-        for (size_t j = 0; j <= n; ++j) {
+        for (int j = 1; j <= n; ++j) {
             dp[0][j] = j;
         }
-
-        for (size_t i = 1; i <= m; ++i) {
-            for (size_t j = 1; j <= n; ++j) {
-                dp[i][j] = min(dp[i - 1][j - 1] + (word1[i - 1] == word2[j - 1] ? 0 : 1), min(dp[i - 1][j], dp[i][j - 1]) + 1);
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i] == word2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = std::min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                }
             }
         }
 
