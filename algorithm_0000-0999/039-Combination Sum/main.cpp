1b7e5ac09b1f1f6363920040d7175e9c75b0f3eb
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-02 00:17:02 +0800

    039: update solution

--- algorithm_0000-0999/039-Combination Sum/main.cpp
+++ algorithm_0000-0999/039-Combination Sum/main.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> recursive1(const vector<int>& candidates, size_t index, int target) {
-        assert(0 < target);
-
-        vector<vector<int>> combs;
-        for (size_t i = index; i < candidates.size() ; ++i) {
-            if (candidates[i] == target) {
-                combs.push_back({candidates[i]});
-            }
-            else if (candidates[i] < target) {
-                vector<vector<int>> subCombs = recursive1(candidates, i, target - candidates[i]);
-                for (auto itr = combs.insert(combs.end(), subCombs.begin(), subCombs.end()); itr != combs.end(); ++itr) {
-                    itr->push_back(candidates[i]);
-                }
-            }
+    void recursive(const vector<int>& candidates, int target, int index, vector<int>& comb, vector<vector<int>>& combs) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            combs.push_back(comb);
+            return;
         }
 
-        return combs;
-    }
-
-    void recursive2(const vector<int>& candidates, size_t index, int target, vector<int>& comb, vector<vector<int>>& combs) {
-        assert(0 < target);
-
-        for (size_t i = index; i < candidates.size() ; ++i) {
-            if (candidates[i] == target) {
-                combs.push_back(comb);
-                combs.back().push_back(candidates[i]);
-            }
-            else if (candidates[i] < target) {
-                comb.push_back(candidates[i]);
-                recursive2(candidates, i, target - candidates[i], comb, combs);
-                comb.pop_back();
-            }
+        for (int i = index; i < candidates.size(); ++i) {
+            comb.push_back(candidates[i]);
+            recursive(candidates, target - candidates[i], i, comb, combs);
+            comb.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        //return recursive1(candidates, 0, target);
-
         vector<int> comb;
         vector<vector<int>> combs;
-        recursive2(candidates, 0, target, comb, combs);
+        recursive(candidates, target, 0, comb, combs);
         return combs;
     }
 };
