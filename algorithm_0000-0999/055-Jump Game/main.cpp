403950fc6e3292cb42b344a2086636928dab5bf8
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-08 23:49:12 +0800

    055: update solution

--- algorithm_0000-0999/055-Jump Game/main.cpp
+++ algorithm_0000-0999/055-Jump Game/main.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    bool canJump1(vector<int>& nums) {
-        vector<bool> dp(nums.size(), false);
-        dp[0] = true;
-        for (size_t i = 0; i < dp.size(); ++i) {
-            if (!dp[i]) {
-                continue;
-            }
-
-            for (size_t j = i + 1, end = std::min(i + nums[i] + 1, dp.size()); j < end; ++j) {
-                dp[j] = true;
-            }
-        }
-
-        return dp.back();
-    }
-
-    // Time: O(n)
-    bool canJump2(vector<int>& nums) {
-        for (size_t pos = 0, reach = 0; pos < nums.size() && pos <= reach; ++pos) {
-            reach = std::max(reach, pos + nums[pos]);
-            if (nums.size() <= (reach + 1)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     bool canJump(vector<int>& nums) {
-        //return canJump1(nums);
-        return canJump2(nums);
+        int far = 0;
+        for (int i = 0; i < nums.size() && (far + 1) < nums.size(); ++i) {
+            if (far < i) {
+                return false;
+            }
+            far = std::max(far, i + nums[i]);
+        }
+
+        return true;
     }
 };
