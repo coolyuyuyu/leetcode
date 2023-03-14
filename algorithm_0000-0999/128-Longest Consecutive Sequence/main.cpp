a42b20d387c395662678b645e59577584c7aa845
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-15 01:31:56 +0800

    128: update solution

--- algorithm_0000-0999/128-Longest Consecutive Sequence/main.cpp
+++ algorithm_0000-0999/128-Longest Consecutive Sequence/main.cpp
@@ -1,106 +1,98 @@
 class Solution {
 public:
-    class DisjointSet {
-    public:
-        DisjointSet() {
-        }
-        int findByNum(int num) {
-            return find(getIndex(num));
-        }
-        void mergeByNum(int num1, int num2) {
-            merge(getIndex(num1), getIndex(num2));
-        }
-    private:
-        int getIndex(int num) {
-            auto p = m_dict.emplace(num, m_parents.size());
-            if (p.second) {
-                m_parents.push_back(m_parents.size());
-            }
-            return p.first->second;
-        }
-        int find(int i) {
-            if (m_parents[i] != i) {
-                m_parents[i] = find(m_parents[i]);
-            }
-            return m_parents[i];
-        }
-        void merge(int i, int j) {
-            int rootI = find(i);
-            int rootJ = find(j);
-            m_parents[rootI] = rootJ;
-        }
-        vector<int> m_parents;
-        unordered_map<int, int> m_dict;
-    };
+    // Time: O(nlogn)
+    int byUniqSort(vector<int>& nums) {
+        int n = nums.size();
+        std::sort(nums.begin(), nums.end());
+        nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
 
-    int union_find(vector<int>& nums) {
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        DisjointSet ds;
-        for (int num : numSet) {
-            if (numSet.find(num - 1) != numSet.end()) {
-                ds.mergeByNum(num, num - 1);
+        int ret = 0;
+        for (int lft = 0, rht; lft < n; lft = rht) {
+            for (rht = lft + 1; rht < n; ++rht) {
+                if ((nums[rht - 1] + 1) != nums[rht]) {
+                    break;
+                }
             }
-            if (numSet.find(num + 1) != numSet.end()) {
-                ds.mergeByNum(num, num + 1);
-            }
+
+            ret = std::max(ret, rht - lft);
         }
-        unordered_map<int, int> rootCounts;
-        for (int num : numSet) {
-            ++rootCounts[ds.findByNum(num)];
-        }
-        int maxCount = 0;
-        for (auto p : rootCounts) {
-            maxCount = max(maxCount, p.second);
-        }
-        return maxCount;
+
+        return ret;
     }
 
-    // Time: O(nlogn)
-    int uniq_sort(vector<int>& nums) {
-        if (nums.size() <= 1) {
-            return nums.size();
+    class DisjointSets {
+    public:
+        bool contain(int elem) {
+            return m_roots.find(elem) != m_roots.end();
         }
 
-        std::sort(nums.begin(), nums.end());
+        void insert(int elem) {
+            assert(!contain(elem));
+            m_roots[elem] = elem;
+        }
 
-        int ret = 1;
-        for (int cnt = 1, i = 1; i < nums.size(); ++i) {
-            if ((nums[i - 1] + 1) == nums[i]) {
-                ++cnt;
-                ret = std::max(ret, cnt);
+        void merge(int elem1, int elem2) {
+            int root1 = root(elem1), root2 = root(elem2);
+            if (root1 != root2) {
+                m_roots[root1] = root2;
             }
-            else if (nums[i - 1] == nums[i]) {
+        }
+
+        int root(int elem) {
+            if (m_roots[elem] != elem) {
+                m_roots[elem] = root(m_roots[elem]);
             }
-            else {
-                cnt = 1;
+
+            return m_roots[elem];
+        }
+
+private:
+        unordered_map<int, int> m_roots;
+    };
+
+    // Time: O(n)
+    int byDisjointSets(const unordered_set<int>& nums) {
+        DisjointSets ds;
+        for (int num : nums) {
+            ds.insert(num);
+            if (ds.contain(num - 1)) {
+                ds.merge(num, num - 1);
             }
+            if (ds.contain(num + 1)) {
+                ds.merge(num, num + 1);
+            }
+        }
+
+        unordered_map<int, int> counts;
+        int ret = 0;
+        for (int num : nums) {
+            ret = std::max(ret, ++counts[ds.root(num)]);
         }
 
         return ret;
     }
 
     // Time: O(n)
-    int hash_set(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-
+    int bySet(const unordered_set<int>& nums) {
         int ret = 0;
-        for (const auto& num: s) {
-            if (s.find(num - 1) == s.end()) {
-                int cnt = 1;
-                while (s.find(num + cnt) != s.end()) {
-                    ++cnt;
-                }
-
-                ret = std::max(ret, cnt);
+        for (int num : nums) {
+            if (nums.find(num - 1) != nums.end()) {
+                continue;
             }
+
+            int count = 1;
+            for (; nums.find(num + count) != nums.end() ;++count) {
+                ;
+            }
+            ret = std::max(ret, count);
         }
 
         return ret;
     }
 
     int longestConsecutive(vector<int>& nums) {
-        //return union_find(nums);
-        //return uniq_sort(nums);
-        return hash_set(nums);
+        //return byUniqSort(nums);
+        //return byDisjointSets(unordered_set<int>(nums.begin(), nums.end()));
+        return bySet(unordered_set<int>(nums.begin(), nums.end()));
     }
 };
