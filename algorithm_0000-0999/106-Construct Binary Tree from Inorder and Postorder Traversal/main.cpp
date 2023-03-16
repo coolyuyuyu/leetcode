10c65d78ddabf65170ac0b47ced12004ceb21c9b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-16 22:14:25 +0800

    106: update solution

--- algorithm_0000-0999/106-Construct Binary Tree from Inorder and Postorder Traversal/main.cpp
+++ algorithm_0000-0999/106-Construct Binary Tree from Inorder and Postorder Traversal/main.cpp
@@ -11,58 +11,52 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree_Recursive(vector<int>& inorder, size_t inBgn, vector<int>& postorder, size_t postBgn, size_t size, map<int, size_t>& m) {
+    TreeNode* recursive(const vector<int>& inorder, size_t inIdx, const vector<int>& postorder, size_t postIdx, size_t size, unordered_map<int, size_t>& inIndexes) {
         if (size == 0) {
             return nullptr;
         }
 
-        int val = postorder[postBgn + size - 1];
-        size_t lftSize = m[val] - inBgn;
-        size_t rhtSize = size - lftSize - 1;
+        int val = postorder[postIdx + size - 1];
+        size_t lftSize = (inIndexes[val] - inIdx);
+        size_t rhtSize = size - 1 - lftSize;
+        return new TreeNode(
+            val,
+            recursive(inorder, inIdx, postorder, postIdx, lftSize, inIndexes),
+            recursive(inorder, inIdx + lftSize + 1, postorder, postIdx + lftSize, rhtSize, inIndexes));
+    }
 
-        TreeNode* root = new TreeNode(val);
-        root->left = buildTree_Recursive(inorder, inBgn, postorder, postBgn, lftSize, m);
-        root->right = buildTree_Recursive(inorder, inBgn + lftSize + 1, postorder, postBgn + lftSize, rhtSize, m);
+    TreeNode* iterative(const vector<int>& inorder, const vector<int>& postorder, unordered_map<int, size_t>& inIndexes) {
+        TreeNode* root = nullptr;
+        stack<tuple<TreeNode**, size_t, size_t, size_t>> stk({{&root, 0, 0, inorder.size()}});
+        while (!stk.empty()) {
+            auto [ppNode, inIdx, postIdx, size] = stk.top();
+            stk.pop();
+
+            if (size == 0) {
+                *ppNode = nullptr;
+                continue;
+            }
+
+            int val = postorder[postIdx + size - 1];
+            size_t lftSize = (inIndexes[val] - inIdx);
+            size_t rhtSize = size - 1 - lftSize;
+            *ppNode = new TreeNode(val);
+
+            stk.emplace(&((*ppNode)->left), inIdx, postIdx, lftSize);
+            stk.emplace(&((*ppNode)->right), inIdx + lftSize + 1, postIdx + lftSize, rhtSize);
+        }
+        
         return root;
     }
 
-    TreeNode* buildTree_Iterative(vector<int>& inorder, vector<int>& postorder, map<int, size_t>& m) {
-        TreeNode* pRoot = nullptr;
-
-        stack<pair<TreeNode**, tuple<size_t, size_t, size_t>>> stk; // <root, <inBgn, postBgn, size>>;
-        if (0 < inorder.size()) {
-            stk.emplace(&pRoot, make_tuple(0, 0, inorder.size()));
-        }
-        while (!stk.empty()) {
-            TreeNode** ppNode = stk.top().first;
-            size_t inBgn = get<0>(stk.top().second);
-            size_t postBgn = get<1>(stk.top().second);
-            size_t size = get<2>(stk.top().second);
-            stk.pop();
-
-            int val = postorder[postBgn + size - 1];
-            size_t lftSize = m[val] - inBgn;
-            size_t rhtSize = size - lftSize - 1;
-
-            *ppNode = new TreeNode(val);
-            if (0 < lftSize) {
-                stk.emplace(&((*ppNode)->left), make_tuple(inBgn, postBgn, lftSize));
-            }
-            if (0 < rhtSize) {
-                stk.emplace(&((*ppNode)->right), make_tuple(inBgn + lftSize + 1, postBgn + lftSize, rhtSize));
-            }
-        }
-
-        return pRoot;
-    }
-
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int, size_t> m;
-        for (size_t i = 0; i < inorder.size(); ++i) {
-            m[inorder[i]] = i;
+        size_t n = inorder.size();
+        unordered_map<int, size_t> inIndexes;
+        for (size_t i = 0; i < n; ++i) {
+            inIndexes[inorder[i]] = i;
         }
-
-        //return buildTree_Recursive(inorder, 0, postorder, 0, inorder.size(), m);
-        return buildTree_Iterative(inorder, postorder, m);
+        
+        //return recursive(inorder, 0, postorder, 0, n, inIndexes);
+        return iterative(inorder, postorder, inIndexes);
     }
 };
