e489e9caea1e2f0075afd501deb14940a9be57f2
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-22 02:32:16 +0800

    605: update solution

--- algorithm_0000-0999/605-Can Place Flowers/main.cpp
+++ algorithm_0000-0999/605-Can Place Flowers/main.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        for (size_t i = 0; i < flowerbed.size(); ++i) {
-            if (flowerbed[i] == 0) {
-                if (i + 1 == flowerbed.size() || flowerbed[i + 1] == 0) {
-                    if (--n <= 0) {
-                        break;
-                    }
-                    ++i;
+        for (size_t i = 0; i < flowerbed.size();) {
+            if (flowerbed[i]) {
+                i += 2;
+            }
+            else if ((i + 1) == flowerbed.size() || flowerbed[i + 1] == 0) {
+                if (--n <= 0) {
+                    break;
                 }
+                i += 2;
             }
             else {
-                ++i;
+                i += 1;
             }
         }
 
