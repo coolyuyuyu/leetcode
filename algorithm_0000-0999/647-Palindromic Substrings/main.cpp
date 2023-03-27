19c9d9a0411c19c706e906627be2d57758d05b32
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-28 00:59:19 +0800

    647: manacher solution

--- algorithm_0000-0999/647-Palindromic Substrings/main.cpp
+++ algorithm_0000-0999/647-Palindromic Substrings/main.cpp
@@ -1,19 +1,81 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-        size_t cnt = s.size();
+    // Time: O(n^2), Space: O(n^2)
+    int dp1(const string& s) {
+        size_t n = s.size();
 
-        vector<vector<bool>> dp(2, vector<bool>(s.size(), true));
-        for (size_t len = 2; len <= s.size(); ++len) {
-            for (size_t lft = 0, rht = lft + len - 1; rht < s.size(); ++lft, ++rht) {
-                auto& cache = dp[(len % 2 == 0 ? 0 : 1)];
-                cache[lft] = (s[lft] == s[rht]) && cache[lft + 1];
-                if (cache[lft]) {
-                    ++cnt;
+        size_t ret = n;
+        vector<vector<bool>> dp(n, vector<bool>(n, true));
+        for (size_t len = 2; len <= n; ++len) {
+            for (size_t i = 0, j = i + len - 1; j < n; ++i, ++j) {
+                dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j];
+                if (dp[i][j]) {
+                    ++ret;
                 }
             }
         }
 
-        return cnt;
+        return ret;
+    }
+
+    // Time: O(n^2), Space: O(n)
+    int dp2(const string& s) {
+        size_t n = s.size();
+
+        size_t ret = n;
+        vector<bool> dp0(n, true), dp1(n, true);
+        for (size_t len = 2; len <= n; ++len) {
+            for (size_t i = 0, j = i + len - 1; j < n; ++i, ++j) {
+                auto& dp = (len & 1 ? dp1 : dp0);
+                dp[i] = dp[i + 1] && s[i] == s[j];
+                if (dp[i]) {
+                    ++ret;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    // Time: O(n), Space: O(n)
+    int manacher(const string& s) {
+        string t = "#";
+        for (char c : s) {
+            t.append(1, c).append("#");
+        }
+
+        int n = t.size();
+
+        int ret = 0;
+        vector<int> p(n); // p[i]: the longest extended radius of palindromic substring centered at i
+        for (int i = 0, maxCtr = -1, maxRht = -1; i < n; ++i) {
+            int r = 0;
+            if (i < maxRht) {
+                int j = maxCtr * 2 - i;
+                r = std::min(p[j], maxRht - i);
+            }
+            while (0 <= (i - r - 1) && (i + r + 1) < n && t[i - r - 1] == t[i + r + 1]) {
+                ++r;
+            }
+            p[i] = r;
+
+            if (maxRht < (i + p[i])) {
+                maxCtr = i;
+                maxRht = i + p[i];
+            }
+
+            if (0 < p[i]) {
+                ret += ((p[i] + 1) / 2);
+            }
+        }
+
+        return ret;
+    }
+
+    int countSubstrings(string s) {
+        //return dp1(s);
+        //return dp2(s);
+
+        return manacher(s);
     }
 };
