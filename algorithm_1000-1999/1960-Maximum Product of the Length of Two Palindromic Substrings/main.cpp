974c68e0efd24cf638c326de2c215d9b9444758f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-29 22:36:57 +0800

    1960: initial manacher solution

---
+++ algorithm_1000-1999/1960-Maximum Product of the Length of Two Palindromic Substrings/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isDecomposable(string s) {
+        bool found2Once = false;
+        for (size_t i = 0; i < s.size(); ) {
+            size_t pos = s.find_first_not_of(s[i], i + 1);
+            size_t len = pos == string::npos ? s.size() - i : pos - i;
+            if (2 <= len && (len - 2) % 3 == 0) {
+                if (found2Once) {
+                    return false;
+                }
+                found2Once = true;
+            }
+            else if (len % 3 != 0) {
+                return false;
+            }
+
+            i = pos;
+        }
+        
+        return found2Once;
+    }
+};
