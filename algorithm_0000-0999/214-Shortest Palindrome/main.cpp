755c691ce41202da43eb9133a1273a91cf2d77f6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-03-29 22:36:45 +0800

    214: manacher solution

--- algorithm_0000-0999/214-Shortest Palindrome/main.cpp
+++ algorithm_0000-0999/214-Shortest Palindrome/main.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    vector<size_t> longestPrefix(const string& s) {
-        // suffix array
-        // dp[i]: the longest length k such that s[0:k-1] = s[i-k+1:i]
-        vector<size_t> dp(s.size());
+    vector<size_t> computeLPS(const string& s) {
+        size_t n = s.size();
+
+        vector<size_t> dp(n);
         dp[0] = 0;
-        for (size_t i = 1; i < s.size(); ++i) {
+        for (size_t i = 1; i < n; ++i) {
             size_t j = dp[i - 1];
             while (0 < j && s[j] != s[i]) {
                 j = dp[j - 1];
@@ -16,37 +16,79 @@ public:
         return dp;
     }
 
-    // kmp
-    string shortestPalindrome(string s) {
+    string kmp(const string& s) {
         if (s.empty()) {
             return "";
         }
 
-        size_t n = s.size();
-
         string t = s;
         std::reverse(t.begin(), t.end());
-        vector<size_t> suffix = longestPrefix(s);
-        
+
+        size_t n = t.size();
+
+        vector<size_t> lps = computeLPS(s);
         size_t dp = (s[0] == t[0] ? 1 : 0);
         for (size_t i = 1; i < n; ++i) {
             size_t j = dp;
             while (0 < j && s[j] != t[i]) {
-                j = suffix[j - 1];
+                j = lps[j - 1];
             }
             dp = j + (s[j] == t[i] ? 1 : 0);
         }
 
-        string ret = t.substr(0, n - dp) + s
+        string ret = s.substr(dp);
+        std::reverse(ret.begin(), ret.end());
+        ret = ret + s;
+
         return ret;
+    }
+
+    string manacher(const string& s) {
+        string t = "#";
+        for (char c : s) {
+            t.append(1, c).append("#");
+        }
+
+        int n = t.size();
+        int prefixPalindromeLen;
+        vector<int> p(n); // p[i]: the longest extended radius of palindromic substring centered at i
+        for (int i = 0, maxCtr = -1, maxRht = -1; i < n; ++i) {
+            int r = 0;
+            if (i < maxRht) {
+                int j = maxCtr * 2 - i;
+                r = std::min(p[j], maxRht - i);
+            }
+            while (0 <= (i - r - 1) && (i + r + 1) < n && t[i - r - 1] == t[i + r + 1]) {
+                ++r;
+            }
+            p[i] = r;
+
+            if (maxRht < (i + p[i])) {
+                maxCtr = i;
+                maxRht = i + p[i];
+            }
+
+            if (i == p[i]) {
+                prefixPalindromeLen = p[i];
+            }
+        }
+
+        string ret = s.substr(prefixPalindromeLen);
+        std::reverse(ret.begin(), ret.end());
+        ret = ret + s;
+
+        return ret;
+    }
+
+    string shortestPalindrome(string s) {
+        return kmp(s);
+        //return manacher(s);
     }
 };
 
 /*
-s': reverse of s
-decompose s as :    A A' B
-reverse s as t : B' A A'
+s:                  A A' B
+Shortest palindrome can be found by adding B' in front of s => B' A A' B
 
-Find the shortest B'
-=> Find the longest length k such that s[0:k-1] = t[i-k+1:i]
+t: reverse of s: B' A A'
 */
