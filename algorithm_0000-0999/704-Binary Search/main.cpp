5be7f163cecba9a6ce173784399c86b34302350f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-01 11:29:00 +0800

    704: update solution

--- algorithm_0000-0999/704-Binary Search/main.cpp
+++ algorithm_0000-0999/704-Binary Search/main.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.empty()) {
-            return -1;
-        }
+        assert(!nums.empty());
 
         for (size_t lo = 0, hi = nums.size() - 1; lo <= hi;) {
             size_t mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (nums[mid] < target) {
+            if (nums[mid] < target) {
                 lo = mid + 1;
+            }
+            else if (nums[mid] == target) {
+                return mid;
             }
             else {
                 if (mid == 0) {
-                    return -1;
+                    break;
                 }
                 hi = mid - 1;
             }
