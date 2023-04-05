cead99e9d48d7a2dba40df36630d85c6d1ac5b95
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-05 19:35:58 +0800

    253: update solution

--- algorithm_0000-0999/253-Meeting Rooms II/main.cpp
+++ algorithm_0000-0999/253-Meeting Rooms II/main.cpp
@@ -1,57 +1,43 @@
 class Solution {
 public:
-    size_t minMeetingRooms_Sort(vector<vector<int>>& intervals) {
-        vector<pair<bool, int>> states; // <bool: open/close, int: time>
-        for (const vector<int>& interval : intervals) {
-            states.emplace_back(true, interval[0]);
-            states.emplace_back(false, interval[1]);
+    int sweepline(const vector<vector<int>>& intervals) {
+        map<int, int> m; // time -> diff
+        for (const auto& interval : intervals) {
+            ++m[interval[0]];
+            --m[interval[1]];
         }
 
-        auto comp = [](const pair<bool, int>& state1, const pair<bool, int>& state2) {
-            if (state1.second == state2.second) {
-                return (state1.first == false);
-            }
-            else {
-                return (state1.second < state2.second);
-            }
-        };
-        sort(states.begin(), states.end(), comp);
-
-        size_t numRooms = 0, maxNumRooms = 0;
-        for (const pair<bool, int>& state : states) {
-            if (state.first) {
-                ++numRooms;
-                maxNumRooms = max(maxNumRooms, numRooms);
-            }
-            else {
-                --numRooms;
-            }
+        int ret = 0;
+        int sum = 0;
+        for (const auto& [_, diff] : m) {
+            sum += diff;
+            ret = std::max(ret, sum);
         }
 
-        return maxNumRooms;
+        return ret;
     }
 
-    size_t minMeetingRooms_Heap(vector<vector<int>>& intervals) {
-        auto comp = [](const vector<int>& interval1, const vector<int>& interval2) {
-            return (interval1[0] < interval2[0]);
-        };
-        sort (intervals.begin(), intervals.end(), comp);
+    int heap(vector<vector<int>>& intervals) {
+        auto cmp1 = [](const vector<int>& i1, const vector<int>& i2) { return i1[0] < i2[0]; };
+        sort(intervals.begin(), intervals.end(), cmp1);
 
-        size_t maxNumRooms = 0;
-        priority_queue<int, vector<int>, greater<int>> pq; // min_heap
-        for (const vector<int>& interval : intervals) {
-            while (!pq.empty() && pq.top() <= interval[0]) {
+        int ret = 0;
+        auto cmp2 = [](const vector<int>& i1, const vector<int>& i2) { return i1[1] > i2[1]; };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp2)> pq(cmp2);
+        for (const auto& interval : intervals) {
+            while (!pq.empty() && pq.top()[1] <= interval[0]) {
                 pq.pop();
             }
-            pq.push(interval[1]);
-            maxNumRooms = max(maxNumRooms, pq.size());
+            pq.push(interval);
+
+            ret = std::max<int>(ret, pq.size());
         }
 
-        return maxNumRooms;
+        return ret;
     }
 
-    size_t minMeetingRooms(vector<vector<int>>& intervals) {
-        return minMeetingRooms_Sort(intervals);
-        //return minMeetingRooms_Heap(intervals);
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        //return sweepline(intervals);
+        return heap(intervals);
     }
 };
