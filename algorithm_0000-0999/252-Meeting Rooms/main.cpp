31d205e79532bb0db57526c4a7340df2f1b8e80c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-05 19:35:45 +0800

    252: update solution

--- algorithm_0000-0999/252-Meeting Rooms/main.cpp
+++ algorithm_0000-0999/252-Meeting Rooms/main.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
-    // Time: O(N^2)
-    bool canAttendMeetings_BruteForce(vector<vector<int>>& intervals) {
+    // Time: O(n^2), Space: O(1)
+    bool bruteforce(const vector<vector<int>>& intervals) {
+        auto overlapped = [](const vector<int>& i1, const vector<int>& i2) -> bool {
+            return i1[0] < i2[1] && i2[0] < i1[1];
+        };
         for (size_t i = 0; i < intervals.size(); ++i) {
             for (size_t j = i + 1; j < intervals.size(); ++j) {
-                if (intervals[j][0] < intervals[i][1] && intervals[i][0] < intervals[j][1]) {
+                if (overlapped(intervals[i], intervals[j])) {
                     return false;
                 }
+            }
+        } 
+
+        return true;
+    }
+
+    // Time: O(nlogn), Space(n)
+    bool sweepline(const vector<vector<int>>& intervals) {
+        map<int, int> m; // time -> diff
+        for (const auto& interval : intervals) {
+            ++m[interval[0]];
+            --m[interval[1]];
+        }
+
+        int sum = 0;
+        for (const auto [_, diff] : m) {
+            sum += diff;
+            if (1 < sum) {
+                return false;
             }
         }
 
         return true;
     }
 
-    // Time: O(NlogN)
-    bool canAttendMeetings_Sort(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        for (size_t i = 1; i < intervals.size(); ++i) {
+    // Time: O(nlogn), Space(1)
+    bool sort(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](const vector<int>& i1, const vector<int>& i2) { return i1[0] < i2[0]; });
+        for (int i = 1; i < intervals.size(); ++i) {
             if (intervals[i][0] < intervals[i - 1][1]) {
                 return false;
             }
@@ -26,7 +48,8 @@ public:
     }
 
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        //return canAttendMeetings_BruteForce(intervals);
-        return canAttendMeetings_Sort(intervals);
+        //return bruteforce(intervals);
+        //return sweepline(intervals);
+        return sort(intervals);
     }
 };
