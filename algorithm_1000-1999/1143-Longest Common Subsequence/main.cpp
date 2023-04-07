2584b2476449ca012eae9a7ff8a42c5da5dbb42f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-07 10:13:59 +0800

    1143: updatesolution

--- algorithm_1000-1999/1143-Longest Common Subsequence/main.cpp
+++ algorithm_1000-1999/1143-Longest Common Subsequence/main.cpp
@@ -1,55 +1,69 @@
 class Solution {
 public:
-    int topdn_dp(const string& s, size_t i, const string& t, size_t j, vector<vector<int>>& cache) {
-        if (0 <= cache[i][j]) {
-            return cache[i][j];
+    int btmup_dp(string& s1, string& s2) {
+        int m = s1.size(), n = s2.size();
+
+        // dp[i][j]: the length of longest common subsequence of s1[0:i] and s2[0:j]
+        int dp[m + 1][n + 1];
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= n; ++j) {
+            dp[0][j] = 0;
         }
 
-        if (s[i] == t[j]) {
-            cache[i][j] = ((i == 0 || j == 0) ? 0 : topdn_dp(s, i - 1, t, j - 1, cache)) + 1;
-        }
-        else {
-            cache[i][j] = std::max(i == 0 ? 0 : topdn_dp(s, i - 1, t, j, cache), j == 0 ? 0 : topdn_dp(s, i, t, j - 1, cache));
-        }
-
-        return cache[i][j];
-    }
-
-    int topdn_dp(const string& s, const string& t) {
-        size_t m = s.size(), n = t.size();
-        if (m == 0 || n == 0) {
-            return 0;
-        }
-
-        vector<vector<int>> cache(m, vector<int>(n, -1));
-        return topdn_dp(s, s.size() - 1, t, t.size() - 1, cache);
-    }
-
-
-    int btmup_dp(const string& s, const string& t) {
-        size_t m = s.size(), n = t.size();
-        if (m == 0 || n == 0) {
-            return 0;
-        }
-
-        // dp[i][j]: the length of their longest common subsequence of s[0:i] and t[0:j];
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (size_t i = 0; i < m; ++i) {
-            for (size_t j = 0; j < n; ++j) {
-                if (s[i] == t[j]) {
-                    dp[i][j] = ((i == 0 || j == 0) ? 0 : dp[i - 1][j - 1]) + 1;
+        s1 = "#" + s1, s2 = "#" + s2;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else {
-                    dp[i][j] = std::max(i == 0 ? 0 : dp[i - 1][j], j == 0 ? 0 : dp[i][j - 1]);
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[m][n];
     }
 
+    int topdn_dp(string& s1, string& s2) {
+        int m = s1.size(), n = s2.size();
+
+        // cache[i][j]: the length of longest common subsequence of s1[0:i] and s2[0:j]
+        int cache[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                cache[i][j] = -1;
+            }
+        }
+
+        s1 = "#" + s1, s2 = "#" + s2;
+        std::function<int(int, int)> f = [&s1, &s2, &cache, &f](int i, int j) {
+            if (i == 0 || j == 0) {
+                return 0;
+            }
+            if (0 <= cache[i][j]) {
+                return cache[i][j];
+            }
+
+            int& ret = cache[i][j];
+            if (s1[i] == s2[j]) {
+                ret = f(i - 1, j - 1) + 1;
+            }
+            else {
+                ret = std::max(f(i - 1, j), f(i, j - 1));
+            }
+
+            return ret;
+        };
+
+        return f(m, n);
+    }
+
+
     int longestCommonSubsequence(string text1, string text2) {
-        //return topdn_dp(text1, text2);
-        return btmup_dp(text1, text2);
+        //return btmup_dp(text1, text2);
+        return topdn_dp(text1, text2);
     }
 };
