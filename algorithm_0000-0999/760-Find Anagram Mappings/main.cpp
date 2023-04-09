566a2b09c8da289d397091d25d67e7ccb68033e3
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-10 01:05:41 +0800

    760: update solution

--- algorithm_0000-0999/760-Find Anagram Mappings/main.cpp
+++ algorithm_0000-0999/760-Find Anagram Mappings/main.cpp
@@ -1,50 +1,56 @@
 class Solution {
 public:
     vector<int> anagramMappings_ArrayLookup(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> indexes(100001);
-        for (int i = 0; i < nums2.size(); ++i) {
+        int n = nums1.size();
+
+        array<int, 100001> indexes;
+        for (int i = 0; i < n; ++i) {
             indexes[nums2[i]] = i;
         }
 
-        vector<int> ans(nums1.size());
-        for (size_t i = 0; i < nums1.size(); ++i) {
-            ans[i] = indexes[nums1[i]];
+        vector<int> ret(n);
+        for (int i = 0; i < n; ++i) {
+            ret[i] = indexes[nums1[i]];
         }
 
-        return ans;
+        return ret;
     }
 
     vector<int> anagramMappings_MapLookup(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+
         map<int, int> indexes;
-        for (int i = 0; i < nums2.size(); ++i) {
+        for (int i = 0; i < n; ++i) {
             indexes[nums2[i]] = i;
         }
 
-        vector<int> ans(nums1.size());
-        for (size_t i = 0; i < nums1.size(); ++i) {
-            ans[i] = indexes[nums1[i]];
+        vector<int> ret(n);
+        for (int i = 0; i < n; ++i) {
+            ret[i] = indexes[nums1[i]];
         }
 
-        return ans;
+        return ret;
     }
 
     vector<int> anagramMappings_HashLookup(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+
         unordered_map<int, int> indexes;
-        for (int i = 0; i < nums2.size(); ++i) {
+        for (int i = 0; i < n; ++i) {
             indexes[nums2[i]] = i;
         }
 
-        vector<int> ans(nums1.size());
-        for (size_t i = 0; i < nums1.size(); ++i) {
-            ans[i] = indexes[nums1[i]];
+        vector<int> ret(n);
+        for (int i = 0; i < n; ++i) {
+            ret[i] = indexes[nums1[i]];
         }
 
-        return ans;
+        return ret;
     }
 
     vector<int> anagramMappings(vector<int>& nums1, vector<int>& nums2) {
-        //return anagramMappings_ArrayLookup(nums1, nums2);
+        return anagramMappings_ArrayLookup(nums1, nums2);
         //return anagramMappings_MapLookup(nums1, nums2);
-        return anagramMappings_HashLookup(nums1, nums2);
+        //return anagramMappings_HashLookup(nums1, nums2);
     }
 };
