2ebb2f47cce9dab2c19fb7519a3432ebae5386b7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:04:46 +0800

    1491: update solution

--- algorithm_1000-1999/1491-Average Salary Excluding the Minimum and Maximum Salary/main.cpp
+++ algorithm_1000-1999/1491-Average Salary Excluding the Minimum and Maximum Salary/main.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        int maxVal = numeric_limits<int>::min(), minVal = numeric_limits<int>::max();
-        int sum = 0;
-        for (int num : salary) {
-            sum += num;
-
-            if (maxVal < num) {
-                maxVal = num;
-            }
-            if (num < minVal) {
-                minVal = num;
-            }
+        int sumVal = 0, minVal = INT_MAX, maxVal = INT_MIN;
+        for (int val : salary) {
+            sumVal += val;
+            minVal = std::min(minVal, val);
+            maxVal = std::max(maxVal, val);
         }
 
-        sum -= (maxVal + minVal);
-        return (static_cast<double>(sum) / (salary.size() - 2));
+        return static_cast<double>(sumVal - minVal - maxVal) / (salary.size() - 2);
     }
 };
