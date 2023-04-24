382e34231b29207f11248bdca3990d86260a3da7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:04:34 +0800

    1372: update solution

--- algorithm_1000-1999/1372-Longest ZigZag Path in a Binary Tree/main.cpp
+++ algorithm_1000-1999/1372-Longest ZigZag Path in a Binary Tree/main.cpp
@@ -11,63 +11,21 @@
  */
 class Solution {
 public:
-    int longestZigZag_Recursive(TreeNode* root, pair<int, int>& lenPair) {
-        if (!root) {
-            lenPair = {0, 0};
-            return 0;
-        }
-
-        pair<int, int> lftLenPair, rhtLenPair;
-        int lftMax = longestZigZag_Recursive(root->left, lftLenPair);
-        int rhtMax = longestZigZag_Recursive(root->right, rhtLenPair);
-
-        lenPair = {lftLenPair.second + 1, rhtLenPair.first + 1};
-
-        return max(max(lftMax, rhtMax), max(lenPair.first, lenPair.second));
-    }
-
-    int longestZigZag_Iterative(TreeNode* root) {
-        int maxLen = 0;
-
-        unordered_map<TreeNode*, pair<int, int>> m; // <node, len pair>
-
-        // postorder traversal
-        stack<pair<TreeNode*, bool>> stk;
-        if (root) {
-            stk.emplace(root, false);
-        }
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            bool visited = stk.top().second;
-            stk.pop();
-
-            if (visited) {
-                pair<int, int> lftLenPair = (node->left ? m[node->left] : make_pair(0, 0));
-                pair<int, int> rhtLenPair = (node->right ? m[node->right] : make_pair(0, 0));
-                pair<int, int> lenPair = {lftLenPair.second + 1, rhtLenPair.first + 1};
-
-                maxLen = max(maxLen, max(lenPair.first, lenPair.second));
-
-                m[node] = lenPair;
-            }
-            else {
-                stk.emplace(node, true);
-                if (node->right) {
-                    stk.emplace(node->right, false);
-                }
-                if (node->left) {
-                    stk.emplace(node->left, false);
-                }
-            }
-        }
-
-        return maxLen;
-    }
-
     int longestZigZag(TreeNode* root) {
-        //pair<int, int> lenPair;
-        //return longestZigZag_Recursive(root, lenPair) - 1;
+        return recursive(root);
+    }
 
-        return longestZigZag_Iterative(root) - 1;
+    int recursive(TreeNode* root) {
+        std::function<int(TreeNode*, int, bool)> dfs = [&dfs](TreeNode* root, int zlen, bool flag) {
+            if (!root) {
+                return zlen;
+            }
+
+            return std::max(
+                dfs(root->left, (flag ? zlen : 0) + 1, false),
+                dfs(root->right, (!flag ? zlen : 0) + 1, true));
+        };
+
+        return std::max(dfs(root->left, 1, false), dfs(root->right, 1, true)) - 1;
     }
 };
