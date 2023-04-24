64edaa4895de9cfc00c6475c52c537cfe89c5ceb
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:03:56 +0800

    496: update solution

--- algorithm_0000-0999/496-Next Greater Element I/main.cpp
+++ algorithm_0000-0999/496-Next Greater Element I/main.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        map<int, int> greaters; {
-            stack<int> stk;
-            for (int num : nums2) {
-                while (!stk.empty() && stk.top() < num) {
-                    greaters[stk.top()] = num;
-                    stk.pop();
-                }
-                stk.push(num);
+        map<int, int> greaters;
+        stack<int> stk;
+        for (int num : nums2) {
+            while (!stk.empty() && stk.top() <num) {
+                greaters[stk.top()] = num;
+                stk.pop();
             }
+            stk.push(num);
         }
 
-        vector<int> ans;
-        for (int num : nums1) {
-            auto itr = greaters.find(num);
-            if (itr != greaters.end()) {
-                ans.push_back(itr->second);
-            }
-            else {
-                ans.push_back(-1);
-            }
+        vector<int> ret(nums1.size());
+        for (size_t i = 0; i < nums1.size(); ++i){
+            auto itr = greaters.find(nums1[i]);
+            ret[i] = (itr == greaters.end() ? -1 : itr->second);
         }
 
-        return ans;
+        return ret;
     }
 };
