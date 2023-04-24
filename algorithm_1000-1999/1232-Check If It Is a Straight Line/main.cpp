222ce6e6ece9d95bcb199f22a1be550d20ae8180
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:04:28 +0800

    1232: update solution

--- algorithm_1000-1999/1232-Check If It Is a Straight Line/main.cpp
+++ algorithm_1000-1999/1232-Check If It Is a Straight Line/main.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        if (coordinates[0][0] == coordinates[1][0]) { // vertical
-            int x = coordinates[0][0];
-            auto pred = [&x](const vector<int>& coordinate) {
-                return (coordinate[0] == x);
-            };
-            return all_of(coordinates.begin() + 1, coordinates.end(), pred);
+        int diffX = coordinates[1][0] - coordinates[0][0], diffY = coordinates[1][1] - coordinates[0][1];
+        for (size_t i = 2; i < coordinates.size(); ++i) {
+            if (diffY * (coordinates[i][0] - coordinates[0][0]) != diffX * (coordinates[i][1] - coordinates[0][1])) {
+                return false;
+            }
         }
-        else if (coordinates[0][1] == coordinates[1][1]) { // horizontal
-            int y = coordinates[0][1];
-            auto pred = [&y](const vector<int>& coordinate) {
-                return (coordinate[1] == y);
-            };
-            return all_of(coordinates.begin() + 1, coordinates.end(), pred);
-        }
-        else {
-            double slope = static_cast<double>(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
-            for (size_t i = 2; i < coordinates.size(); ++i) {
-                if (coordinates[i][0] == coordinates[i - 1][0]) {
-                    return false;
-                }
 
-                double slopeTmp = static_cast<double>(coordinates[i][1] - coordinates[i - 1][1]) / (coordinates[i][0] - coordinates[i - 1][0]);
-                if (slopeTmp != slope) {
-                    return false;
-                }
-            };
-            return true;
-        }
+        return true;
     }
 };
