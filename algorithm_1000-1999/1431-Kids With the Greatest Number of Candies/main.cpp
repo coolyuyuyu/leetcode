7a41bbc678b1fc7d78c6fcbe1147a16a755bae7c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:04:41 +0800

    1431: update solution

--- algorithm_1000-1999/1431-Kids With the Greatest Number of Candies/main.cpp
+++ algorithm_1000-1999/1431-Kids With the Greatest Number of Candies/main.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        assert(!candies.empty());
         int m = *std::max_element(candies.begin(), candies.end());
-        if (m <= extraCandies) {
-            return vector<bool>(candies.size(), true);
+
+        vector<bool> ret(candies.size());
+        int threshold = m - extraCandies;
+        for (int i = 0; i < candies.size(); ++i) {
+            ret[i] = (threshold <= candies[i]);
         }
 
-        vector<bool> ans(candies.size());
-        int remaining = m - extraCandies;
-        for (size_t i = 0; i < candies.size(); ++i) {
-            ans[i] = (remaining <= candies[i]);
-        }
-
-        return ans;
+        return ret;
     }
 };
