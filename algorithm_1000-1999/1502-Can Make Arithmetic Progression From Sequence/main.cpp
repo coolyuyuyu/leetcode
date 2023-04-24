05bc8b312e6c2fd317f40700d893a817e5a90475
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:04:51 +0800

    1502: update solution

--- algorithm_1000-1999/1502-Can Make Arithmetic Progression From Sequence/main.cpp
+++ algorithm_1000-1999/1502-Can Make Arithmetic Progression From Sequence/main.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
+        std::sort(arr.begin(), arr.end());
+
         int diff = arr[1] - arr[0];
         for (size_t i = 2; i < arr.size(); ++i) {
-            if ((arr[i] - arr[i - 1]) != diff) {
+            if ((arr[i - 1] + diff) != arr[i]) {
                 return false;
             }
         }
