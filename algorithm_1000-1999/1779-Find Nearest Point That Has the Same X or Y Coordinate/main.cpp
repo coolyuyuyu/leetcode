46c78f627aa0b58fabdbae833c2a085ce2a6b0fd
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:05:08 +0800

    1779: update solution

--- algorithm_1000-1999/1779-Find Nearest Point That Has the Same X or Y Coordinate/main.cpp
+++ algorithm_1000-1999/1779-Find Nearest Point That Has the Same X or Y Coordinate/main.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
-        int minDistance = numeric_limits<int>::max();
-        int index = -1;
-        for (int i = 0; i < points.size(); ++i) {
+        int ret = -1;
+        for (int i = 0, minDist = INT_MAX; i < points.size(); ++i) {
             if (points[i][0] == x || points[i][1] == y) {
-                int distance = abs(points[i][0] - x + points[i][1] - y);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    index = i;
+                int dist = abs(points[i][0] - x) + abs(points[i][1] - y);
+                if (dist < minDist) {
+                    minDist = dist;
+                    ret = i;
                 }
             }
         }
 
-        return index;
+        return ret;
     }
 };
