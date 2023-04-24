28f6caf2a6c8aa8a35ee7c18bbc289c19b8d757c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-25 01:04:13 +0800

    976: update solution

--- algorithm_0000-0999/976-Largest Perimeter Triangle/main.cpp
+++ algorithm_0000-0999/976-Largest Perimeter Triangle/main.cpp
@@ -3,11 +3,9 @@ public:
     int largestPerimeter(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
 
-        assert(3 <= nums.size());
-        for (size_t i = nums.size(); 2 < i--;) {
+        for (int i = nums.size(); 2 < i--;) {
             if (nums[i] < (nums[i - 2] + nums[i - 1])) {
-                cout << i << endl;
-                return (nums[i - 2] + nums[i - 1] + nums[i]);
+                return nums[i - 2] + nums[i - 1] + nums[i];
             }
         }
 
