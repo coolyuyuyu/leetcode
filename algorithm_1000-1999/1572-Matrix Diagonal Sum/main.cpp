7b5ef5eff0d6c65bb25ba0545e81e4717b501353
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-26 23:20:56 +0800

    1572: update solution

--- algorithm_1000-1999/1572-Matrix Diagonal Sum/main.cpp
+++ algorithm_1000-1999/1572-Matrix Diagonal Sum/main.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        size_t n = mat.size();
+        int n = mat.size();
 
-        int sum = 0;
-        for (size_t i = 0; i < n; ++i) {
-            sum += mat[i][i];
-            sum += mat[i][n - i - 1];
+        int ret = 0;
+        for (int i = 0; i < n; ++i) {
+            ret += mat[i][i];
+            ret += mat[i][n - i - 1];
         }
-        if ((n % 2) == 1) {
-            sum -= mat[n / 2][n / 2];
+        if (n & 1) {
+            ret -= mat[n / 2][n / 2];
         }
 
-        return sum;
+        return ret;
     }
 };
