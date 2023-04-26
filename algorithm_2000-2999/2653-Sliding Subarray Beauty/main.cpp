396e264e64949954a7e593fd9abf85ba7e553f7f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-26 23:21:38 +0800

    2653: update solution

--- algorithm_2000-2999/2653-Sliding Subarray Beauty/main.cpp
+++ algorithm_2000-2999/2653-Sliding Subarray Beauty/main.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
-        vector<int> cnts(101, 0);
-        for (int i = 0; (i + 1) < k; ++i) {
-            ++cnts[nums[i] + 50];
-        }
+    vector<int> byBucket(const vector<int>& nums, int k, int x) {
+        vector<int> ret;
 
-        auto f = [&cnts, x]() {
+        vector<int> cnts(102, 0);
+        auto findSmallest = [&cnts, x]() {
             int cnt = 0;
             for (int i = 0; i < 50; ++i) {
                 cnt += cnts[i];
@@ -14,20 +12,62 @@ public:
                     return i - 50;
                 }
             }
-
             return 0;
         };
-
-        vector<int> ret;
-        for (int i = k - 1; i < nums.size(); ++i) {
-            if (0 <= (i - k)) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (k <= i) {
                 --cnts[nums[i - k] + 50];
             }
             ++cnts[nums[i] + 50];
 
-            ret.push_back(f());
+            if (k <= i + 1) {
+                ret.push_back(findSmallest());
+            }
         }
 
         return ret;
+    }
+
+    vector<int> byMultiset(const vector<int>& nums, int k, int x) {
+        vector<int> ret;
+
+        multiset<int> ms1, ms2;
+        auto findSmallest = [&ms1, &ms2, x]() {
+            return std::min(*ms1.rbegin(), 0);
+        };
+        for (int i = 0; i < nums.size(); ++i) {
+            if (k <= i) {
+                int v = nums[i - k];
+                auto itr1 = ms1.find(v);
+                if (itr1 != ms1.end()) {
+                    ms1.erase(itr1);
+                }
+                else {
+                    ms2.erase(ms2.find(v));
+                }
+
+                if (ms1.size() < x && !ms2.empty()) {
+                    ms1.insert(*ms2.begin());
+                    ms2.erase(ms2.begin());
+                }
+            }
+            ms1.insert(nums[i]);
+            if (x < ms1.size()) {
+                ms2.insert(*ms1.rbegin());
+                ms1.erase(std::prev(ms1.end()));
+            }
+
+            if (k <= i + 1) {
+                ret.push_back(findSmallest());
+            }
+        }
+
+        return ret;
+    }
+
+
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        //return byBucket(nums, k, x);
+        return byMultiset(nums, k, x);
     }
 };
