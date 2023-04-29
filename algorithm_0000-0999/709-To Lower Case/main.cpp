c8347f327ba221350d1b37a249c51a89ecd42223
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-29 13:14:37 +0800

    709: update solution

--- algorithm_0000-0999/709-To Lower Case/main.cpp
+++ algorithm_0000-0999/709-To Lower Case/main.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    string toLowerCase(string str) {
-        for (size_t i = 0; i < str.size(); ++i) {
-            char c = str[i];
-            if ('A'<= c && c <= 'Z') {
-                str[i] = c - 'A' + 'a';
+    string toLowerCase(string s) {
+        for (char& c : s) {
+            if ('A' <= c && c <= 'Z') {
+                c = c - 'A' + 'a';
             }
         }
 
-        //transform(str.begin(), str.end(), str.begin(), ::tolower);
-        return str;
+        return s;
     }
 };
