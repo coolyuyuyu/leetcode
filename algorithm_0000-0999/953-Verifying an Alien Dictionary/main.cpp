841f55063101b0ff4a691f9cd56954687b912356
coolyuyuyu <coolyuyuyu@gmail.com>
2023-04-29 13:15:03 +0800

    953: update solution

--- algorithm_0000-0999/953-Verifying an Alien Dictionary/main.cpp
+++ algorithm_0000-0999/953-Verifying an Alien Dictionary/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isAlienSorted(vector<string>& words, string order) {
+    bool mancraft(vector<string>& words, const string& order) {
         array<unsigned, 26> weights;
         for (int i = 0; i < 26; ++i) {
             weights[order[i] - 'a'] = i;
@@ -21,5 +21,25 @@ public:
         };
 
         return std::is_sorted(words.begin(), words.end(), comp);
+    }
+
+    bool stdlib(vector<string>& words, const string& order) {
+        array<unsigned char, 26> weights;
+        for (unsigned char i = 0; i < 26; ++i) {
+            weights[order[i] - 'a'] = i;
+        }
+
+        auto charComp = [&weights](char c1, char c2) {
+            return weights[c1 - 'a'] < weights[c2 - 'a'];
+        };
+        auto wordComp = [&charComp](const string& s, const string& t) {
+            return std::lexicographical_compare(s.begin(), s.end(), t.begin(), t.end(), charComp);
+        };
+        return std::is_sorted(words.begin(), words.end(), wordComp);
+    }
+
+    bool isAlienSorted(vector<string>& words, string order) {
+        //return mancraft(words, order);
+        return stdlib(words, order);
     }
 };
