d6772d40f027ebee4b40c9ba876eab294e868013
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-01 16:42:14 +0800

    242: update solution

--- algorithm_0000-0999/242-Valid Anagram/main.cpp
+++ algorithm_0000-0999/242-Valid Anagram/main.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    // Time: O(NlogN), Space: O(1)
+    bool compareSorted(string& s, string& t) {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+
+        return s == t;
+    }
+
+    // Time: O(N), Space: O(1)
+    bool compareFreq1(const string& s, const string& t) {
+        vector<int> freq1(26, 0), freq2(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            ++freq1[s[i] - 'a'];
+            ++freq2[t[i] - 'a'];
+        }
+
+        return freq1 == freq2;
+    }
+
+    // Time: O(N), Space: O(1)
+    bool compareFreq2(const string& s, const string& t) {
+        vector<int> freq(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            ++freq[s[i] - 'a'];
+            --freq[t[i] - 'a'];
+        }
+
+        return std::all_of(freq.begin(), freq.end(), [](int f) { return f == 0; });
+    }
+
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> table;
-        for (auto c : t) {
-            pair<unordered_map<char, int>::iterator, bool> p = table.emplace(c, 1);
-            if (!p.second) {
-                ++p.first->second;
-            }
+        if (s.size() != t.size()) {
+            return false;
         }
 
-        for (auto c : s) {
-            unordered_map<char, int>::iterator iter = table.find(c);
-            if (iter != table.end()) {
-                if (iter->second == 1) {
-                    table.erase(iter);
-                }
-                else if (iter->second == 0) {
-                    return false;
-                }
-                else {
-                    --iter->second;
-                }
-            }
-            else {
-                return false;
-            }
-        }
-
-        return table.empty();
+        //return compareSorted(s, t);
+        //return compareFreq1(s, t);
+        return compareFreq2(s, t);
     }
 };
