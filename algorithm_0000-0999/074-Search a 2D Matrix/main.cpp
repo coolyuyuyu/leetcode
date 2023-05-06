cf18c2b2e40cef9356a36cb77e1b12c9eb9202cd
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-06 12:04:30 +0800

    074: update solution

--- algorithm_0000-0999/074-Search a 2D Matrix/main.cpp
+++ algorithm_0000-0999/074-Search a 2D Matrix/main.cpp
@@ -1,56 +1,46 @@
 class Solution {
 public:
-    int binarySearch(const vector<int>& nums, int target) {
-        int lo = 0, hi = nums.size() - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (nums[mid] < target) {
+    size_t lower_bound(vector<vector<int>>& matrix, int target) {
+        size_t lo = 0, hi = matrix.size();
+        while (lo < hi) {
+            size_t mid = lo + (hi - lo) / 2;
+            if (matrix[mid][0] < target) {
                 lo = mid + 1;
             }
             else {
-                hi = mid - 1;
+                hi = mid;
             }
         }
 
-        return -1;
+        return lo;
     }
 
-    int binarySearch(const vector<vector<int>>& matrix, int target) {
-        int lo = 0, hi = matrix.size() - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (matrix[mid].empty()) {
-                return false;
-            }
-
-            if (matrix[mid].front() <= target && target <= matrix[mid].back()) {
-                return mid;
-            }
-            else if (matrix[mid].back() < target) {
+    size_t lower_bound(vector<int>& nums, int target) {
+        size_t lo = 0, hi = nums.size();
+        while (lo < hi) {
+            size_t mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) {
                 lo = mid + 1;
             }
             else {
-                hi = mid - 1;
+                hi = mid;
             }
         }
 
-        return -1;
+        return lo;
     }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = binarySearch(matrix, target);
-        if (row < 0) {
+        size_t r = lower_bound(matrix, target);
+        if (r == 0 && target < matrix[r][0]) {
             return false;
         }
-
-        int col = binarySearch(matrix[row], target);
-        if (col < 0) {
-            return false;
+        else if (r < matrix.size() && matrix[r][0] == target) {
+            return true;
         }
+        --r;
 
-        return true;
+        size_t c = lower_bound(matrix[r], target);
+        return c != matrix[r].size() && matrix[r][c] == target;
     }
 };
