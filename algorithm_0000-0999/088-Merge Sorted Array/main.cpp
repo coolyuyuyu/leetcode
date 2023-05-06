2bdaecc7cb9e66de2814e64a02f730f4551c0854
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-06 12:04:37 +0800

    088: update solution

--- algorithm_0000-0999/088-Merge Sorted Array/main.cpp
+++ algorithm_0000-0999/088-Merge Sorted Array/main.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m + n;
         for (int i = m + n; 0 < m && 0 < n; --i) {
             if (nums1[m - 1] <= nums2[n - 1]) {
                 nums1[i - 1] = nums2[--n];
