b399b2681f37ef0f94211d974cdada90cb1ee393
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-06 12:05:13 +0800

    387: update solution

--- algorithm_0000-0999/387-First Unique Character in a String/main.cpp
+++ algorithm_0000-0999/387-First Unique Character in a String/main.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int firstUniqChar(string s) {
+    int scan_after_count(const string& s) {
+        vector<int> cnts(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            ++cnts[s[i] - 'a'];
+        }
 
-        vector<int> counts(26, 0);
-        string::iterator slow = s.begin();
-        string::iterator fast = s.begin();
-        while (fast != s.end()) {
-            ++counts[*fast - 'a'];
-            while (slow <= fast && 1 < counts[*slow - 'a']) {
+        for (int i = 0; i < s.size(); ++i) {
+            if (cnts[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    int scan_while_count(const string& s) {
+        vector<int> cnts(26, 0);
+        int slow = 0, fast = 0;
+        while (fast < s.size()) {
+            ++cnts[s[fast++] - 'a'];
+            while (slow < fast && 1 < cnts[s[slow] - 'a']) {
                 ++slow;
             }
-            ++fast;
         }
 
-        if (slow == s.end()) {
-            return -1;
-        }
-        else {
-            return slow - s.begin();
-        }
+        return (slow == s.size() ? -1 : slow);
+    }
+
+    int firstUniqChar(string s) {
+        //return scan_after_count(s);
+        return scan_while_count(s);
     }
 };
