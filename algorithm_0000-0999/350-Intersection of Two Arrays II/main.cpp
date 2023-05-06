c1e2941f71e791cb81080c0aa8d9a55abcd8091c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-06 12:04:59 +0800

    350: update solution

--- algorithm_0000-0999/350-Intersection of Two Arrays II/main.cpp
+++ algorithm_0000-0999/350-Intersection of Two Arrays II/main.cpp
@@ -1,79 +1,77 @@
 class Solution {
 public:
     // Time: O(MlogM + NlogN)
-    vector<int> intersection_SearchSortedNonOverlapped(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> byBinarySearchSorted(vector<int>& nums1, vector<int>& nums2) {
         std::sort(nums1.begin(), nums1.end());
         std::sort(nums2.begin(), nums2.end());
 
-        vector<int> nums;
+        vector<int> ret;
         for (auto itr1 = nums1.begin(), itr2 = nums2.begin(); itr1 != nums1.end() && itr2 != nums2.end(); ++itr1) {
             itr2 = std::lower_bound(itr2, nums2.end(), *itr1);
-            if (itr2 != nums2.end() && *itr2 == *itr1 ) {
-                nums.push_back(*itr1);
-                ++itr2;
+            if (itr2 != nums2.end() && *itr1 == *itr2) {
+                ret.push_back(*itr2++);
             }
         }
-        return nums;
+
+        return ret;
     }
 
     // Time: O(MlogM + NlogN)
-    vector<int> intersection_LinearOnSorted(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> byLinearSearchSorted(vector<int>& nums1, vector<int>& nums2) {
         std::sort(nums1.begin(), nums1.end());
         std::sort(nums2.begin(), nums2.end());
 
-        vector<int> nums;
+        vector<int> ret;
         for (auto itr1 = nums1.begin(), itr2 = nums2.begin(); itr1 != nums1.end() && itr2 != nums2.end();) {
-            if (*itr1 < *itr2) {
-                ++itr1;
+            if (*itr1 == *itr2) {
+                ret.push_back(*itr1);
+                ++itr1, ++itr2;
             }
-            else if (*itr1 == *itr2) {
-                nums.push_back(*itr1);
+            else if (*itr1 < *itr2) {
                 ++itr1;
-                ++itr2;
             }
             else {
                 ++itr2;
             }
         }
-        return nums;
+
+        return ret;
     }
 
     // Time: O(MlogM + NlogN)
-    vector<int> intersection_StdLibrary(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> byStdLib(vector<int>& nums1, vector<int>& nums2) {
         std::sort(nums1.begin(), nums1.end());
         std::sort(nums2.begin(), nums2.end());
 
-        vector<int> nums(std::min(nums1.size(), nums2.size()));
-        auto it = std::set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums.begin());
-        nums.resize(it - nums.begin());
-        return nums;
+        vector<int> ret;
+        std::set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(ret));
+
+        return ret;
     }
 
-    // Time: O((M + N)logN), Space: O(NlogN)
-    vector<int> intersection_MapCount(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() < nums2.size()) {
-            std::swap(nums1, nums2);
-        }
-        map<int, int> counts;
+    // Time: O(M + N), Space: O(N)
+    vector<int> byHashCount(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> counts;
         for (int num : nums2) {
             ++counts[num];
         }
 
-        vector<int> nums;
-        for (int num : nums1) {
+        vector<int> ret;
+        for (int num: nums1) {
             auto itr = counts.find(num);
             if (itr != counts.end() && 0 < itr->second) {
                 --(itr->second);
-                nums.push_back(num);
+                ret.push_back(num);
             }
         }
-        return nums;
+
+        return ret;
     }
 
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        //return intersection_SearchSortedNonOverlapped(nums1, nums2);
-        //return intersection_LinearOnSorted(nums1, nums2);
-        //return intersection_StdLibrary(nums1, nums2);
-        return intersection_MapCount(nums1, nums2);
+        //return byBinarySearchSorted(nums1, nums2);
+        //return byLinearSearchSorted(nums1, nums2);
+        //return byStdLib(nums1, nums2);
+        return byHashCount(nums1, nums2);
     }
 };
