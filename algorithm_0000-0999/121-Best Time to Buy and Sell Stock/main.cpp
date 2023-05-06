9075f07ccd347611323ddef79be9de2a038e224e
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-06 12:04:50 +0800

    121: update solution

--- algorithm_0000-0999/121-Best Time to Buy and Sell Stock/main.cpp
+++ algorithm_0000-0999/121-Best Time to Buy and Sell Stock/main.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
         int ret = 0;
+        int minPrice = INT_MAX;
         for (int price : prices) {
             ret = std::max(ret, price - minPrice);
             minPrice = std::min(minPrice, price);
