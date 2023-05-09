805a514212a61feb7bfe84058a614fb70d9bfd2d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-09 10:00:21 +0800

    054: update solution

--- algorithm_0000-0999/054-Spiral Matrix/main.cpp
+++ algorithm_0000-0999/054-Spiral Matrix/main.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        if (matrix.empty() || matrix.front().empty()) {
+        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+        if (m == 0 || n == 0) {
             return {};
         }
 
-        vector<int> nums;
-        for (size_t lft = 0, top = 0, rht = matrix.front().size() -1, btm = matrix.size() - 1; true;) {
-            for (size_t col = lft; col <= rht; ++col) { 
-                nums.push_back(matrix[top][col]);
+        vector<int> nums(m * n);
+        for (size_t lft = 0, top = 0, rht = n - 1, btm = m - 1, i = 0; true;) {
+            for (size_t col = lft; col <= rht; ++col) {
+                nums[i++] = matrix[top][col];
             }
             if (btm < ++top) {
                 break;
             }
 
-            for (size_t row = top; row <= btm; ++row) { 
-                nums.push_back(matrix[row][rht]);
+            for (size_t row = top; row <= btm; ++row) {
+                nums[i++] = matrix[row][rht];
             }
             if (rht-- <= lft) {
                 break;
             }
 
-            for (size_t col = rht + 1; lft < col--;) { 
-                nums.push_back(matrix[btm][col]);
+            for (size_t col = rht + 1; lft < col--;) {
+                nums[i++] = matrix[btm][col];
             }
             if (btm-- <= top) {
                 break;
             }
 
-            for (size_t row = btm + 1; top < row--;) { 
-                nums.push_back(matrix[row][lft]);
+            for (size_t row = btm + 1; top < row--;) {
+                nums[i++] = matrix[row][lft];
             }
             if (rht < ++lft) {
                 break;
