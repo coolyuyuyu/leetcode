2e91240910344d26ca7fce90bfcc14495548b5a0
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-12 00:01:17 +0800

    1035: update solution

--- algorithm_1000-1999/1035-Uncrossed Lines/main.cpp
+++ algorithm_1000-1999/1035-Uncrossed Lines/main.cpp
@@ -1,59 +1,67 @@
 class Solution {
 public:
-    int topdn_dp(const vector<int>& s, size_t i, const vector<int>& t, size_t j, vector<vector<int>>& cache) {
-        if (0 <= cache[i][j]) {
-            return cache[i][j];
+    int btmup_dp(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+
+        nums1.insert(nums1.begin(), 0);
+        nums2.insert(nums2.begin(), 0);
+
+        // dp[i][j]: the maximum number of connecting lines we can draw from nums1[1:i] and nums2[1:j]
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        dp[0][0] = 0;
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = 0;
         }
-
-        if (s[i] == t[j]) {
-            cache[i][j] = ((i == 0 || j == 0) ? 0 : topdn_dp(s, i - 1, t, j - 1, cache)) + 1;
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = 0;
         }
-        else {
-            cache[i][j] = std::max(i == 0 ? 0 : topdn_dp(s, i - 1, t, j, cache), j == 0 ? 0 : topdn_dp(s, i, t, j - 1, cache));
-        }
-
-        return cache[i][j];
-    }
-
-    int topdn_dp(const vector<int>& s, const vector<int>& t) {
-        size_t m = s.size(), n = t.size();
-        if (m == 0 || n == 0) {
-            return 0;
-        }
-
-        vector<vector<int>> cache(m, vector<int>(n, -1));
-        return topdn_dp(s, s.size() - 1, t, t.size() - 1, cache);
-    }
-
-
-    int btmup_dp(const vector<int>& s, const vector<int>& t) {
-        size_t m = s.size(), n = t.size();
-        if (m == 0 || n == 0) {
-            return 0;
-        }
-
-        // dp[i][j]: the length of their longest common subsequence of s[0:i] and t[0:j];
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (size_t i = 0; i < m; ++i) {
-            for (size_t j = 0; j < n; ++j) {
-                if (s[i] == t[j]) {
-                    dp[i][j] = ((i == 0 || j == 0) ? 0 : dp[i - 1][j - 1]) + 1;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else {
-                    dp[i][j] = std::max(i == 0 ? 0 : dp[i - 1][j], j == 0 ? 0 : dp[i][j - 1]);
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[m][n];
     }
 
+    int topdn_dp(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+
+        nums1.insert(nums1.begin(), 0);
+        nums2.insert(nums2.begin(), 0);
+
+        vector<vector<int>> cache(m + 1, vector<int>(n + 1, -1));
+        std::function<int(int, int)> f = [&](int i, int j) {
+            if (i == 0 || j == 0) {
+                return 0;
+            }
+
+            int& ret = cache[i][j];
+            if (0 <= ret) {
+                return ret;
+            }
+
+            if (nums1[i] == nums2[j]) {
+                ret = f(i - 1, j - 1) + 1;
+            }
+            else {
+                ret = std::max(f(i - 1, j), f(i, j - 1));
+            }
+
+            return ret;
+        };
+
+        return f(m, n);
+    }
+
+    // Same as 1143. Longest Common Subsequence
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        //return topdn_dp(nums1, nums2);
-        return btmup_dp(nums1, nums2);
+        //return btmup_dp(nums1, nums2);
+        return topdn_dp(nums1, nums2);
     }
 };
-
-/*
-1143. longest common subsequence
-*/
