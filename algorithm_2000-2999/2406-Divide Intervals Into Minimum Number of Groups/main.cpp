c1ac518445f71ba3927b3072ea4bdcd32dc98c7d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-14 20:09:46 +0800

    2406: initial solution

---
+++ algorithm_2000-2999/2406-Divide Intervals Into Minimum Number of Groups/main.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int sweepline(const vector<vector<int>>& intervals) {
+        map<int, int> m; // time -> diff
+        for (const auto& interval : intervals) {
+            ++m[interval[0]];
+            --m[interval[1] + 1];
+        }
+
+        int ret = 0, sum = 0;
+        for (const auto [_, diff] : m) {
+            sum += diff;
+            ret = std::max(ret, sum);
+        }
+
+        return ret;
+    }
+
+    int heap(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end());
+
+        int ret = 0;
+        priority_queue<int, vector<int>, std::greater<>> pq;
+        for (const auto& interval : intervals) {
+            while (!pq.empty() && pq.top() < interval[0]) {
+                pq.pop();
+            }
+            pq.push(interval[1]);
+
+            ret = std::max<int>(ret, pq.size());
+        }
+
+        return ret;
+    }
+
+    int minGroups(vector<vector<int>>& intervals) {
+        //return sweepline(intervals);
+        return heap(intervals);
+    }
+};
