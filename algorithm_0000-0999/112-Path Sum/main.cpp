82b882e5a30b17deacc23fd5f4395de44a465d5d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-14 20:08:31 +0800

    112: update solution

--- algorithm_0000-0999/112-Path Sum/main.cpp
+++ algorithm_0000-0999/112-Path Sum/main.cpp
@@ -11,50 +11,46 @@
  */
 class Solution {
 public:
-    bool hasPathSum_Recursive(TreeNode* root, int targetSum) {
-        if (!root) {
-            return false;
-        }
+    bool recursive(TreeNode* root, int targetSum) {
+        std::function<bool(TreeNode*, int)> f = [&](TreeNode* root, int targetSum) {
+            if(!root) {
+                return false;
+            }
 
-        targetSum -= root->val;
-        if (!root->left && !root->right) {
-            return targetSum == 0;
-        }
-        else {
-            return (hasPathSum_Recursive(root->left, targetSum) || hasPathSum_Recursive(root->right, targetSum));
-        }
+            if (!root->left && !root->right) {
+                return (root->val == targetSum);
+            }
+            else {
+                targetSum -= root->val;
+                return f(root->left, targetSum) || f(root->right, targetSum);
+            }
+        };
+        return f(root, targetSum);
     }
 
-    bool hasPathSum_Iterative(TreeNode* root, int targetSum) {
-        queue<pair<TreeNode*, int>> q;
-        if (root) {
-            q.emplace(root, targetSum);
-        }
-        while (!q.empty()) {
-            size_t n = q.size();
-            for (size_t i = 0; i < n; ++i) {
-                root = q.front().first;
-                targetSum = q.front().second - root->val;
-                q.pop();
+    bool iterative(TreeNode* root, int targetSum) {
+        for (stack<pair<TreeNode*, int>> stk({{root, targetSum}}); !stk.empty();) {
+            auto [root, targetSum] = stk.top();
+            stk.pop();
 
-                if (!root->left && !root->right && targetSum == 0) {
-                    return true;
-                }
-
-                if (root->left) {
-                    q.emplace(root->left, targetSum);
-                }
-                if (root->right) {
-                    q.emplace(root->right, targetSum);
-                }
+            if (!root) {
+                continue;
             }
+
+            if (!root->left && !root->right && root->val == targetSum) {
+                return true;
+            }
+
+            targetSum -= root->val;
+            stk.emplace(root->left, targetSum);
+            stk.emplace(root->right, targetSum);
         }
 
         return false;
     }
 
     bool hasPathSum(TreeNode* root, int targetSum) {
-        //return hasPathSum_Recursive(root, targetSum);
-        return hasPathSum_Iterative(root, targetSum);
+        return recursive(root, targetSum);
+        //return iterative(root, targetSum);
     }
 };
