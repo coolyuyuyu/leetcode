31fce47f5af948f16249397c57894859ba6bf2ea
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-18 21:29:55 +0800

    265: update solution

--- algorithm_0000-0999/265-Paint House II/main.cpp
+++ algorithm_0000-0999/265-Paint House II/main.cpp
@@ -1,28 +1,86 @@
 class Solution {
 public:
-    int minCostII(vector<vector<int>>& costs) {
-        if (costs.empty()) {
-            return 0;
-        }
+    // Time: O(nk^2)
+    int dp1(vector<vector<int>>& costs) {
+        int n = costs.size(), colors = costs.empty() ? 0 : costs[0].size();
 
-        size_t n = costs.size();
-        size_t k = costs.front().size();
-        vector<vector<int>> dp(n, vector<int>(k));
-        dp[0] = costs[0];
-        for (size_t x = 1; x < n; ++x) {
-            for (size_t y = 0; y < k; ++y) {
-                int minDpOfOthers = INT_MAX;
-                for (size_t z = 0; z < k; ++z) {
-                    if (z == y) {
+        // dp[i][j]: the minimum cost to paint houses[0:i], and house i is painted jth color
+        vector<vector<int>> dp(n + 1, vector<int>(colors));
+        for (int color = 0; color < colors; ++color) {
+            dp[0][color] = 0;
+        }
+        costs.insert(costs.begin(), vector<int>(colors));
+
+        for (int i = 1; i <= n; ++i) {
+            for (int color1 = 0; color1 < colors; ++color1) {
+                int minVal = INT_MAX;
+                for (int color2 = 0; color2 < colors; ++color2) {
+                    if (color2 == color1) {
                         continue;
                     }
-                    minDpOfOthers = min(minDpOfOthers, dp[x - 1][z]);
+                    minVal = std::min(minVal, dp[i - 1][color2]);
                 }
 
-                dp[x][y] = minDpOfOthers + costs[x][y];
+                dp[i][color1] = minVal + costs[i][color1];
+
             }
         }
 
-        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
+        int ret = INT_MAX;
+        for (int color = 0; color < colors; ++color) {
+            ret = std::min(ret, dp[n][color]);
+        }
+
+        return ret;
+    }
+
+    // Time: O(nk)
+    int dp2(vector<vector<int>>& costs) {
+        int n = costs.size(), colors = costs.empty() ? 0 : costs[0].size();
+
+        // dp[i][j]: the minimum cost to paint houses[0:i], and house i is painted jth color
+        vector<vector<int>> dp(n + 1, vector<int>(colors));
+        for (int color = 0; color < colors; ++color) {
+            dp[0][color] = 0;
+        }
+        costs.insert(costs.begin(), vector<int>(colors));
+
+        for (int i = 1; i <= n; ++i) {
+            int minVal1 = INT_MAX, colorMin1;
+            int minVal2 = INT_MAX, colorMin2;
+            for (int color = 0; color < colors; ++color) {
+                if (dp[i - 1][color] < minVal1) {
+                    minVal2 = minVal1;
+                    colorMin2 = colorMin1;
+                    minVal1 = dp[i - 1][color];
+                    colorMin1 = color;
+                }
+                else if (dp[i - 1][color] < minVal2) {
+                    minVal2 = dp[i - 1][color];
+                    colorMin2 = color;
+                }
+            }
+
+            for (int color = 0; color < colors; ++color) {
+                if (color == colorMin1) {
+                    dp[i][color] = minVal2 + costs[i][color];
+                }
+                else {
+                    dp[i][color] = minVal1 + costs[i][color];
+                }
+            }
+        }
+
+        int ret = INT_MAX;
+        for (int color = 0; color < colors; ++color) {
+            ret = std::min(ret, dp[n][color]);
+        }
+
+        return ret;
+    }
+
+    int minCostII(vector<vector<int>>& costs) {
+        //return dp1(costs);
+        return dp2(costs);
     }
 };
