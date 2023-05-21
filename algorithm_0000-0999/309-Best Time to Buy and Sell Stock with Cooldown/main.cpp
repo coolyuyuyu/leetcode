f751a6d317f759844fb89fbccfa231e86d45b351
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-21 23:47:31 +0800

    309: update solution

--- algorithm_0000-0999/309-Best Time to Buy and Sell Stock with Cooldown/main.cpp
+++ algorithm_0000-0999/309-Best Time to Buy and Sell Stock with Cooldown/main.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int hold = INT_MIN, sold = 0, cd = 0;
+        int hold = INT_MIN;
+        int sold = 0;
+        int cool = 0;
         for (int price : prices) {
-            int holdPre = hold;
-            int soldPre = sold;
-            int cdPre = cd;
-
-            hold = std::max(holdPre, cdPre - price);
-            sold = holdPre + price;
-            cd = std::max(cdPre, soldPre);
+            int tmpHold = hold, tmpSold = sold, tmpCool = cool;
+            hold = std::max(tmpHold, cool - price);
+            sold = tmpHold + price;
+            cool = std::max(tmpCool, tmpSold);
         }
 
-        return std::max(sold, cd);
+        return std::max(sold, cool);
     }
 };
