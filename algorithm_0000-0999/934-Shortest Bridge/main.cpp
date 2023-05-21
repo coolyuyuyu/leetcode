6bb29d84105478dff1599425f12fb09367dc7682
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-21 23:18:09 +0800

    934: update solution

--- algorithm_0000-0999/934-Shortest Bridge/main.cpp
+++ algorithm_0000-0999/934-Shortest Bridge/main.cpp
@@ -1,96 +1,70 @@
 class Solution {
 public:
-    void collect(vector<vector<int>>& grid, size_t r, size_t c, unordered_set<size_t>& candidates) {
-        size_t rowCnt = grid.size(), colCnt = grid.empty() ? 0 : grid.front().size();
-        switch (grid[r][c]) {
-            case 0:
-                candidates.emplace(r * colCnt + c);
-                break;
-            case 1:
-                grid[r][c] = -1;
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 
-                // lft
-                if (0 < c) {
-                    collect(grid, r, c - 1, candidates);
+    int shortestBridge(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        queue<pair<int, int>> q;
+        auto collectSurrounding = [&](int r, int c) {
+            for (stack<pair<int, int>> stk({{r,c}}); !stk.empty();) {
+                auto [r, c] = stk.top();
+                stk.pop();
+
+                if (grid[r][c] == 0) {
+                    q.emplace(r, c);
+                    continue;
+                }
+                else if (grid[r][c] == 2) {
+                    continue;
+                }
+                else {
+                    grid[r][c] = 2; // mark 2 as visited
                 }
 
-                // upr
-                if (0 < r) {
-                    collect(grid, r - 1, c, candidates);
+                for (const auto& [dr, dc] : dirs) {
+                    int x = r + dr, y = c + dc;
+                    if (x < 0 || n <= x || y < 0 || n <= y) {
+                        continue;
+                    }
+                    stk.emplace(x, y);
                 }
-
-                // rht
-                if (c + 1 < colCnt) {
-                    collect(grid, r, c + 1, candidates);
-                }
-
-                // btm
-                if (r + 1 < rowCnt) {
-                    collect(grid, r + 1, c, candidates);
-                }
-
-                break;
-            case -1:
-                break;
-        }
-    }
-
-    size_t shortestBridge(vector<vector<int>>& grid) {
-        size_t rowCnt = grid.size(), colCnt = grid.empty() ? 0 : grid.front().size();
-
-        unordered_set<size_t> candidates;
-        bool collected = false;
-        for (size_t r = 0; r < rowCnt && !collected; ++r) {
-            for (size_t c = 0; c < colCnt && !collected; ++c) {
+            }
+        };
+        bool found = false;
+        for (int r = 0; r < n && !found; ++r) {
+            for (int c = 0; c < n && !found; ++c) {
                 if (grid[r][c] == 1) {
-                    collect(grid, r, c, candidates);
-                    collected = true;
+                    collectSurrounding(r, c);
+                    found = true;
+                }
+            }
+        }
+        
+        for (int steps = 0; !q.empty(); ++steps) {
+            for (int i = q.size(); 0 < i--;) {
+                auto [r, c] = q.front();
+                q.pop();
+
+                if (grid[r][c] == 1) {
+                    return steps;
+                }
+
+                if (grid[r][c] == 2) {
+                    continue;
+                }
+                grid[r][c] = 2; // mark 2 as visited
+
+                for (const auto& [dr, dc] : dirs) {
+                    int x = r + dr, y = c + dc;
+                    if (x < 0 || n <= x || y < 0 || n <= y) {
+                        continue;
+                    }
+                    q.emplace(x, y);
                 }
             }
         }
 
-        size_t count = 0;
-        while (!candidates.empty()) {
-            unordered_set<size_t> candidatesTmp;
-            for (size_t candidate : candidates) {
-                size_t r = candidate / colCnt, c = candidate % colCnt;
-                switch (grid[r][c]) {
-                    case 0:
-                        grid[r][c] = -1;
-
-                        // lft
-                        if (0 < c) {
-                            candidatesTmp.emplace((r) * colCnt + (c - 1));
-                        }
-
-                        // upr
-                        if (0 < r) {
-                            candidatesTmp.emplace((r - 1) * colCnt + (c));
-                        }
-
-                        // rht
-                        if (c + 1 < colCnt) {
-                            candidatesTmp.emplace((r) * colCnt + (c + 1));
-                        }
-
-                        // btm
-                        if (r + 1 < rowCnt) {
-                            candidatesTmp.emplace((r + 1) * colCnt + (c));
-                        }
-
-                        break;
-                    case 1:
-                        return count;
-                    case -1:
-                        break;
-                }
-
-            }
-            candidates.swap(candidatesTmp);
-
-            ++count;
-        }
-
-        return count;
+        return -1;;
     }
 };
