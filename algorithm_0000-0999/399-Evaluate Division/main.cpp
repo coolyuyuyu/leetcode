f60475f494080900499c40c7eb81edfedc72b66b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-21 23:18:04 +0800

    399: update solution

--- algorithm_0000-0999/399-Evaluate Division/main.cpp
+++ algorithm_0000-0999/399-Evaluate Division/main.cpp
@@ -1,123 +1,46 @@
 class Solution {
 public:
-    struct Node {
-        vector<pair<size_t, double>> neighbors;
-    };
-
-    struct Graph {
-        vector<Node> nodes;
-        unordered_map<string, size_t> indexMap;
-    };
-
-    Graph buildGraph(const vector<pair<string, string>>& equations, const vector<double>& values) {
-        assert(equations.size() == values.size());
-
-        Graph g;
-
-        size_t num = equations.size();
-        for (size_t i = 0; i < num; ++i) {
-            const pair<string, string>& terminalPair = equations[i];
-            size_t index1; {
-                pair<unordered_map<string, size_t>::iterator, bool> p = g.indexMap.emplace(terminalPair.first, 0);
-                if (p.second) {
-                    g.nodes.emplace_back();
-                    index1 = g.nodes.size() - 1;
-                    p.first->second = index1;
-                }
-                else {
-                    index1 = p.first->second;
-                }
-            }
-            size_t index2; {
-                pair<unordered_map<string, size_t>::iterator, bool> p = g.indexMap.emplace(terminalPair.second, 0);
-                if (p.second) {
-                    g.nodes.emplace_back();
-                    index2 = g.nodes.size() - 1;
-                    p.first->second = index2;
-                }
-                else {
-                    index2 = p.first->second;
-                }
-            }
-            g.nodes[index1].neighbors.emplace_back(index2, values[i]);
-            g.nodes[index2].neighbors.emplace_back(index1, 1 / values[i]);
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string, vector<pair<string, double>>> graph;
+        for (size_t i = 0; i < equations.size(); ++i) {
+            graph[equations[i][0]].emplace_back(equations[i][1], values[i]);
+            graph[equations[i][1]].emplace_back(equations[i][0], 1.0 / values[i]);
         }
 
-        return g;
-    }
+        std::function<double(const string&, const string&)> dfs = [&](const string& src, const string& dst) {
+            stack<pair<string, double>> stk({{src, 1.0}});
+            unordered_set<string> visited;
+            for (; !stk.empty();) {
+                auto [cur, val] = stk.top();
+                stk.pop();
 
-    double calcResult(const Graph& g, const pair<string, string>& terminalPair) {
-        size_t indexSrc; {
-            unordered_map<string, size_t>::const_iterator itr = g.indexMap.find(terminalPair.first);
-            if (itr == g.indexMap.end()) {
-                return -1;
-            }
-            indexSrc = itr->second;
-        }
-        size_t indexDst; {
-            unordered_map<string, size_t>::const_iterator itr = g.indexMap.find(terminalPair.second);
-            if (itr == g.indexMap.end()) {
-                return -1;
-            }
-            indexDst = itr->second;
-        }
-
-        vector<bool> visited(g.nodes.size(), false);
-        visited[indexSrc] = true;
-
-        struct Frame {
-            Frame(size_t pfi, size_t ni, double c)
-                : prevFrameIndex(pfi)
-                , nodeIndex(ni)
-                , cost(c) {
-            }
-            size_t prevFrameIndex;
-            size_t nodeIndex;
-            double cost;
-        };
-
-        vector<Frame> frames;
-        frames.emplace_back(0, indexSrc, 1.0);
-        size_t fIndex = 0;
-        while (fIndex < frames.size()) {
-            const Frame& frame = frames[fIndex];
-            if (frame.nodeIndex == indexDst) {
-                break;
-            }
-
-            const vector<pair<size_t, double>>& neighbors = g.nodes[frame.nodeIndex].neighbors;
-            for (size_t i = 0; i < neighbors.size(); ++i) {
-                size_t indexNbr = neighbors[i].first;
-                if (visited[indexNbr]) {
+                if (visited.find(cur) != visited.end()) {
                     continue;
                 }
-                double cost = neighbors[i].second;
-                frames.emplace_back(fIndex, indexNbr, cost);
+                visited.insert(cur);
+
+                if (cur == dst) {
+                    return val;
+                }
+
+                for (const auto& [next, cost] : graph[cur]) {
+                    stk.emplace(next, val * cost);
+                }
             }
-            ++fIndex;
-        }
 
-        if (fIndex >= frames.size()) {
             return -1.0;
-        }
+        };
 
-        double costAll = 1.0;
-        while (fIndex != 0) {
-            costAll *= frames[fIndex].cost;
-            fIndex = frames[fIndex].prevFrameIndex;
-        }
-
-        return costAll;
-    }
-
-    vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
-        Graph g = buildGraph(equations, values);
-
-        vector<double> results;
+        vector<double> ret(queries.size());
         for (size_t i = 0; i < queries.size(); ++i) {
-            results.emplace_back(calcResult(g, queries[i]));
+            if (graph.find(queries[i][0]) == graph.end() || graph.find(queries[i][1]) == graph.end()) {
+                ret[i] = -1.0;
+                continue;
+            }
+            ret[i] = dfs(queries[i][0], queries[i][1]);
         }
 
-        return results;
+        return ret;
     }
 };
+
