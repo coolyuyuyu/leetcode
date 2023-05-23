eea405a56889b1c66ed68dd84962c14c265f50a9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-23 23:10:55 +0800

    276: update solution

--- algorithm_0000-0999/276-Paint Fence/main.cpp
+++ algorithm_0000-0999/276-Paint Fence/main.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int numWays(int n, int k) {
-        if (n <= 0) {
-            return 0;
-        }
-        else if (n == 1) {
+        if (n == 1) {
             return k;
         }
-        else if (n == 2) {
-            return k * k;
-        }
-        else {
-            int lastTwoDiffCount = k * (k - 1);
-            int lastTwoSameCount = k;
-            for (int i = 2; i < n; ++i) {
-                int tmp = lastTwoDiffCount;
-                lastTwoDiffCount = (lastTwoDiffCount + lastTwoSameCount) * (k - 1);
-                lastTwoSameCount = tmp;
-            }
 
-            return lastTwoDiffCount + lastTwoSameCount;
+        int diff = k * (k - 1); // diff[i]: the number of ways to paint posts[0:i] and colors[i-1] != colors[i]
+        int same = k;           // same[i]: the number of ways to paint posts[0:i] and colors[i-1] == colors[i]
+        for (int i = 2; i < n; ++i) {
+            int tmpDiff = diff, tmpSame = same;
+            diff = tmpDiff * (k - 1) + tmpSame * (k - 1);
+            same = tmpDiff;
         }
+
+        return diff + same;
     }
 };
