dbcdb3fb2dc4d99f2f64df6d3045ccb5206ab75c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-24 12:58:01 +0800

    2542: correct solution

--- algorithm_2000-2999/2542-Maximum Subsequence Score/main.cpp
+++ algorithm_2000-2999/2542-Maximum Subsequence Score/main.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    // Time: O(NlogN + NlogK)
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
         size_t n = nums1.size();
 
         vector<pair<int, int>> pairs(n);
         for (size_t i = 0; i < n; ++i) {
-            pairs[i] = {nums1[i], nums2[i];
+            pairs[i] = {nums1[i], nums2[i]};
         }
-        std::sort(pairs.begin(), pairs.end(), [](const pair<int, int>& p1, const pair<int, int>& p2) { return p1.second < p2.second; });
+        std::sort(pairs.rbegin(), pairs.rend(), [](const auto& p1, const auto& p2) { return p1.second < p2.second; });
 
-        long long ret = 0, sum = 0;
+        long long sum = 0, ret = 0;
         priority_queue<int, vector<int>, greater<>> pq; // min_heap
-        for (size_t i = n; 0 < i--;) {
-            sum += pairs[i].first;
-            pq.push(pairs[i].first);
-            if (k < pq.size()) {
+        for (size_t i = 0; i < n; ++i) {
+            if (k <= i) {
                 sum -= pq.top();
                 pq.pop();
             }
-            if (k == pq.size()) {
+            sum += pairs[i].first;
+            pq.push(pairs[i].first);
+
+            if (pq.size() == k) {
                 ret = std::max(ret, sum * pairs[i].second);
             }
         }
