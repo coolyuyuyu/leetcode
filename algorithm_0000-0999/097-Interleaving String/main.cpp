d4d726e395bf8c0397ad10303200ab7002049ce2
coolyuyuyu <coolyuyuyu@gmail.com>
2023-05-29 23:39:23 +0800

    097: update solution

--- algorithm_0000-0999/097-Interleaving String/main.cpp
+++ algorithm_0000-0999/097-Interleaving String/main.cpp
@@ -1,96 +1,35 @@
 class Solution {
 public:
-    // Time(m, n) = O(2 ^ (m + n)), Space(m, n) = O(m + n)
-    bool isInterleaveRecv(const string& s1, size_t i1, const string& s2, size_t i2, const string& s3, size_t i3) {
-        if (s3.size() <= i3) {
-            if (s1.size() <= i1 && s2.size() <= i2) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        if (i1 < s1.size() && s1[i1] == s3[i3]) {
-            if (isInterleaveRecv(s1, i1 + 1, s2, i2, s3, i3 + 1)) {
-                return true;
-            }
-        }
-        if (i2 < s2.size() && s2[i2] == s3[i3]) {
-            if (isInterleaveRecv(s1, i1, s2, i2 + 1, s3, i3 + 1)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    // Time(m, n) = O(m * n), Space(m, n) = O(m * n)
-    bool isInterleaveDpV1(const string& s1, const string& s2, const string& s3) {
-        size_t rowCnt = s1.size() + 1, colCnt = s2.size() + 1;
-        vector<vector<bool>> dp(rowCnt, vector<bool>(colCnt));
-        for (size_t row = 0; row < rowCnt; ++row) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                if (row == 0 && col == 0) {
-                    dp[row][col] = true;
-                }
-                else if (row == 0) {
-                    dp[row][col] = dp[row][col - 1] && s2[col - 1] == s3[row + col - 1];
-                }
-                else if (col == 0) {
-                    dp[row][col] = dp[row - 1][col] && s1[row - 1] == s3[row + col - 1];
-                }
-                else {
-                    dp[row][col] =
-                        dp[row - 1][col] && s1[row - 1] == s3[row + col - 1] ||
-                        dp[row][col - 1] && s2[col - 1] == s3[row + col - 1];
-                }
-            }
-        }
-
-        return dp[rowCnt - 1][colCnt - 1];
-    }
-
-    // Time(m, n) = O(m * n), Space(m, n) = O(min(m, n))
-    bool isInterleaveDpV2(string s1, string s2, const string& s3) {
-        if (s1.size() < s2.size()) {
-            swap(s1, s2);
-        }
-
-        size_t rowCnt = s1.size() + 1, colCnt = s2.size() + 1;
-        vector<bool> dp(colCnt);
-        for (size_t row = 0; row < rowCnt; ++row) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                bool tmp = dp[col];
-                if (row == 0 && col == 0) {
-                    dp[col] = true;
-                }
-                else if (row == 0) {
-                    dp[col] = dp[col - 1] && s2[col - 1] == s3[row + col - 1];
-                }
-                else if (col == 0) {
-                    dp[col] = dp[col] && s1[row - 1] == s3[row + col - 1];
-                }
-                else {
-                    dp[col] =
-                        dp[col] && s1[row - 1] == s3[row + col - 1] ||
-                        dp[col - 1] && s2[col - 1] == s3[row + col - 1];
-                }
-            }
-        }
-
-        return dp[colCnt - 1];
-    }
-
     bool isInterleave(string s1, string s2, string s3) {
-        if (s1.size() + s2.size() != s3.size()) {
+        if ((s1.size() + s2.size()) != s3.size()) {
             return false;
         }
 
-        //return isInterleaveRecv(s1, 0, s2, 0, s3, 0);
+        int m = s1.size(), n = s2.size();
+        s1.insert(s1.begin(), '#');
+        s2.insert(s2.begin(), '#');
+        s3.insert(s3.begin(), '#');
 
-        //return isInterleaveDpV1(s1, s2, s3);
+        // dp[i][j]: whether s3[:i+j] is formed by an interleaving of s1[:i] and s2[j]
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= m && s1[i] == s3[i]; ++i) {
+            dp[i][0] = true;
+        }
+        for (int j = 1; j <= n && s2[j] == s3[j]; ++j) {
+            dp[0][j] = true;
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s1[i] == s3[i + j] && dp[i - 1][j]) {
+                    dp[i][j] = true;
+                }
+                else if (s2[j] == s3[i + j] && dp[i][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
 
-        return isInterleaveDpV2(s1, s2, s3);
+        return dp[m][n];
     }
 };
