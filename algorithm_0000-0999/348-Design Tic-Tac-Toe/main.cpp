e812bf22b200cd44ee479d34f5ef583818f5c311
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-03 21:04:05 +0800

    348: update solution

--- algorithm_0000-0999/348-Design Tic-Tac-Toe/main.cpp
+++ algorithm_0000-0999/348-Design Tic-Tac-Toe/main.cpp
@@ -1,72 +1,38 @@
 class TicTacToe {
 public:
-    /** Initialize your data structure here. */
     TicTacToe(int n)
         : m_board(n, vector<int>(n, 0)) {
     }
 
-    /** Player {player} makes a move at ({row}, {col}).
-    @param row The row of the board.
-    @param col The column of the board.
-    @param player The player, can be either 1 or 2.
-    @return The current winning condition, can be either:
-    0: No one wins.
-    1: Player 1 wins.
-    2: Player 2 wins. */
     int move(int row, int col, int player) {
         m_board[row][col] = player;
 
+        int n = m_board.size();
 
-        size_t n = m_board.size();
-        bool done;
-
-        // vertical;
-        done = true;
-        for (size_t r = 0; r < n; ++r) {
-            if (m_board[r][col] != player) {
-                done = false;
-                break;
+        bool rowComplete = true, colComplete = true;
+        for (int i = 0; i < n; ++i) {
+            if (m_board[i][col] != player) {
+                colComplete = false;
+            }
+            if (m_board[row][i] != player) {
+                rowComplete = false;
             }
         }
-        if (done) {
+        if (rowComplete || colComplete) {
             return player;
         }
 
-        // horizontal
-        done = true;
-        for (size_t c = 0; c < n; ++c) {
-            if (m_board[row][c] != player) {
-                done = false;
-                break;
-            }
-        }
-        if (done) {
-            return player;
-        }
-
-
-        if (row == col) {
-            done = true;
-            for (size_t i = 0; i < n; ++i) {
+        if (row == col || (row + col + 1) == n) {
+            bool diagComplete = true, antidiagComplete = true;
+            for (int i = 0; i < n; ++i) {
                 if (m_board[i][i] != player) {
-                    done = false;
-                    break;
+                    diagComplete = false;
                 }
-            }
-            if (done) {
-                return player;
-            }
-        }
-
-        if (row + col + 1 == n) {
-            done = true;
-            for (size_t i = 0; i < n; ++i) {
                 if (m_board[i][n - i - 1] != player) {
-                    done = false;
-                    break;
+                    antidiagComplete = false;
                 }
             }
-            if (done) {
+            if (diagComplete || antidiagComplete) {
                 return player;
             }
         }
@@ -77,3 +43,9 @@ public:
 private:
     vector<vector<int>> m_board;
 };
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe* obj = new TicTacToe(n);
+ * int param_1 = obj->move(row,col,player);
+ */
