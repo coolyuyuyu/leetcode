db0bfe04bfbe17b89baaecba02fa73c946bca97f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-04 12:35:00 +0800

    2718: update solution

--- algorithm_2000-2999/2718-Sum of Matrix After Queries/main.cpp
+++ algorithm_2000-2999/2718-Sum of Matrix After Queries/main.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     long long matrixSumQueries(int n, vector<vector<int>>& queries) {
         long long ret = 0;
-        unordered_set<int> rows, cols;
+        vector<bool> rowEmpty(n, true), colEmpty(n, true);
+        int rowRemaining = n, colRemaining = n;
         for (int i = queries.size(); 0 < i--;) {
             if (queries[i][0] == 0) {
-                if (rows.insert(queries[i][1]).second) {
-                    ret += (n - cols.size()) * queries[i][2];
+                if (rowEmpty[queries[i][1]]) {
+                    rowEmpty[queries[i][1]] = false;
+                    ret += (colRemaining) * queries[i][2];
+                    --rowRemaining;
                 }
             }
             else {
-                if (cols.insert(queries[i][1]).second) {
-                    ret += (n - rows.size()) * queries[i][2];
+                if (colEmpty[queries[i][1]]) {
+                    colEmpty[queries[i][1]] = false;
+                    ret += (rowRemaining) * queries[i][2];
+                    --colRemaining;
                 }
             }
         }
