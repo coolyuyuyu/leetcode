ce4b33b551bbaec6d75ce03612f979d41b7c2245
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-06 00:27:33 +0800

    712: initial dp solution

---
+++ algorithm_0000-0999/712-Minimum ASCII Delete Sum for Two Strings/main.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+
+        s1 = "#" + s1;
+        s2 = "#" + s2;
+
+        // dp[i][j]: the lowest ASCII sum of deleted characters to make s1[1:i] == s2[1:j]
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[0][0] = 0;
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = dp[i - 1][0] + s1[i];
+        }
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = dp[0][j - 1] + s2[j];
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = std::min(dp[i - 1][j] + s1[i], dp[i][j - 1] + s2[j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
