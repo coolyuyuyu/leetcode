c94ed5eaf85d2c48a6cf77f9bc5452b3c561389a
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-06 00:10:21 +0800

    1232: update solution

--- algorithm_1000-1999/1232-Check If It Is a Straight Line/main.cpp
+++ algorithm_1000-1999/1232-Check If It Is a Straight Line/main.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int diffX = coordinates[1][0] - coordinates[0][0], diffY = coordinates[1][1] - coordinates[0][1];
+        int dx = coordinates[1][0] - coordinates[0][0], dy = coordinates[1][1] - coordinates[0][1];
         for (size_t i = 2; i < coordinates.size(); ++i) {
-            if (diffY * (coordinates[i][0] - coordinates[0][0]) != diffX * (coordinates[i][1] - coordinates[0][1])) {
+            if (dy * (coordinates[i][0] - coordinates[0][0]) != dx * (coordinates[i][1] - coordinates[0][1])) {
                 return false;
             }
         }
