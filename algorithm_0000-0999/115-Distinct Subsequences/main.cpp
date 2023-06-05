8d0534d602af4e53c025242d49fd3b9360df6463
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-06 00:27:15 +0800

    115: update solution

--- algorithm_0000-0999/115-Distinct Subsequences/main.cpp
+++ algorithm_0000-0999/115-Distinct Subsequences/main.cpp
@@ -1,50 +1,35 @@
 class Solution {
 public:
-    // Time(m, n) = O(m * n), Space(m, n) = O(m, n)
-    size_t numDistinctDpV1(const string& s, const string& t) {
-        size_t rowCnt = t.size() + 1, colCnt = s.size() + 1;
-        vector<vector<size_t>> dp(rowCnt, vector<size_t>(colCnt));
-        for (size_t col = 0; col < colCnt; ++col) {
-            dp[0][col] = 1;
+    int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        if (m < n) {
+            return 0;
         }
-        for (size_t row = 1; row < rowCnt; ++row) {
-            dp[row][0] = 0;
-            for(size_t col = 1; col < colCnt; ++col) {
-                dp[row][col] = dp [row][col - 1];
-                if (t[row - 1] == s[col - 1]) {
-                    dp[row][col] += dp[row - 1][col - 1];
+        if (m == n) {
+            return (s == t ? 1 : 0);
+        }
+
+        s = "#" + s;
+        t = "#" + t;
+
+        // dp[i][j]: the number of distinct subsequences of s[1:i] which equals t[1:j]
+        vector<vector<long>> dp(m + 1, vector<long>(n + 1));
+        dp[0][0] = 1;
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (s[i] == t[j]) {
+                    dp[i][j] += dp[i - 1][j - 1];
                 }
             }
         }
 
-        return dp[rowCnt - 1][colCnt - 1];
-    }
-
-    // Time(m, n) = O(m * n), Space(m, n) = O(m)
-    size_t numDistinctDpV2(const string& s, const string& t) {
-        size_t rowCnt = t.size() + 1, colCnt = s.size() + 1;
-        vector<size_t> dp(colCnt, 1);
-        for (size_t row = 1; row < rowCnt; ++row) {
-            size_t prev = dp[0];
-            dp[0] = 0;
-            for(size_t col = 1; col < colCnt; ++col) {
-                size_t tmp = dp[col];
-
-                dp[col] = dp[col - 1];
-                if (t[row - 1] == s[col - 1]) {
-                    dp[col] += prev;
-                }
-
-                prev = tmp;
-            }
-        }
-
-        return dp[colCnt - 1];
-    }
-
-    size_t numDistinct(string s, string t) {
-        //return numDistinctDpV1(s, t);
-
-        return numDistinctDpV2(s, t);
+        return dp[m][n];
     }
 };
