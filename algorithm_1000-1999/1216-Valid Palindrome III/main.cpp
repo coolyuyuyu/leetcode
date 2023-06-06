1a007ffebd0589dfacb69c5ecc4fd46f53ee2eae
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-07 00:18:54 +0800

    1216: initial dp solution

---
+++ algorithm_1000-1999/1216-Valid Palindrome III/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValidPalindrome(string s, int k) {
+        string t(s.rbegin(), s.rend());
+
+        int n = s.size();
+
+        s = "#" + s;
+        t = "#" + t;
+
+        // dp[i][j]: the length of longest common subsequence of s[1:i] and t[1:j]
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i] == t[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return (n - dp[n][n]) <= k;
+    }
+};
