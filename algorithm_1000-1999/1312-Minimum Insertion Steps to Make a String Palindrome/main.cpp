c63c3458cf75c3df4cf3c63c5d944b5889c0df94
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-11 02:00:12 +0800

    1312: update solution

--- algorithm_1000-1999/1312-Minimum Insertion Steps to Make a String Palindrome/main.cpp
+++ algorithm_1000-1999/1312-Minimum Insertion Steps to Make a String Palindrome/main.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int byDp(string s) {
+    // interval dp
+    int dp1(const string& s) {
         int n = s.size();
 
-        //dp[i][j]: the minimum number of steps to make s[i:j] palindrome
+        // dp[i][j]: the minimum number of steps to make s[i:j] palindrome.
         vector<vector<int>> dp(n, vector<int>(n));
         for (int i = 0; i < n; ++i) { // len = 1
             dp[i][i] = 0;
         }
-        for (int i = 0; (i + 1) < n; ++i) { // len = 2
-            dp[i][i + 1] = (s[i] == s[i + 1] ? 0 : 1);
+        for (int i = 1; i < n; ++i) { // len = 2
+            dp[i - 1][i] = (s[i - 1] == s[i] ? 0 : 1);
         }
-        for (int len = 2; len <= n; ++len) {
+        for (int len = 3; len <= n; ++len) {
             for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
                 if (s[i] == s[j]) {
                     dp[i][j] = dp[i + 1][j - 1];
                 }
                 else {
-                    dp[i][j] = std::min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                    dp[i][j] = std::min(dp[i + 1][j] + 1, dp[i][j - 1] + 1);
                 }
             }
         }
@@ -25,24 +26,25 @@ public:
         return dp[0][n - 1];
     }
 
-    // Leetcode 1092
-    int minLenShortestCommonSupersequence(string str1, string str2) {
-        int m = str1.size(), n = str2.size();
-        str1.insert(str1.begin(), '#');
-        str2.insert(str2.begin(), '#');
+    int dp2(string s) {
+        int n = s.size();
 
-        // dp[i][j]: the length of shortest common supersequence of str1[1:i] and str2[1:j]
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        string t(s.rbegin(), s.rend());
+        s = "#" + s;
+        t = "#" + t;
+
+        // dp[i][j]: the length of shortest common supersequence of s[1:i] and t[1:j]
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
         dp[0][0] = 0;
-        for (int i = 1; i <= m; ++i) {
+        for (int i = 1; i <= n; ++i) {
             dp[i][0] = i;
         }
         for (int j = 1; j <= n; ++j) {
             dp[0][j] = j;
         }
-        for (int i = 1; i <= m; ++i) {
+        for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (str1[i] == str2[j]) {
+                if (s[i] == t[j]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else {
@@ -51,18 +53,11 @@ public:
             }
         }
 
-        return dp[m][n];
-    }
-
-    int byShortestCommonSupersequence(string s) {
-        string t = s;
-        std::reverse(t.begin(), t.end());
-
-        return minLenShortestCommonSupersequence(s, t) - s.size();
+        return dp[n][n] - n;
     }
 
     int minInsertions(string s) {
-        //return byDp(s);
-        return byShortestCommonSupersequence(s);
+        //return dp1(s);
+        return dp2(s);
     }
 };
