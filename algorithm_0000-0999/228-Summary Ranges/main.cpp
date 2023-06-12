a42e5c69f349317c92fd612813c7a193961ebd2d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-12 23:59:52 +0800

    228: update solution

--- algorithm_0000-0999/228-Summary Ranges/main.cpp
+++ algorithm_0000-0999/228-Summary Ranges/main.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        vector<string> ranges;
-        for (size_t preIdx = 0, idx = 1; idx <= nums.size(); ++idx) {
-            if (idx == nums.size() || (nums[idx - 1] + 1 < nums[idx])) {
-                string range = std::to_string(nums[preIdx]);
-                if ((preIdx + 1) < idx) {
-                    range += "->";
-                    range += std::to_string(nums[idx - 1]);
+        vector<string> ret;
+        for (size_t lft = 0, rht = 1; rht <= nums.size(); ++rht) {
+            if (rht == nums.size() || (nums[rht - 1] + 1) < nums[rht]) {
+                string range = std::to_string(nums[lft]);
+                if ((lft + 1) < rht) {
+                    range += ("->" + std::to_string(nums[rht - 1]));
                 }
-                ranges.push_back(range);
+                ret.push_back(range);
 
-                preIdx = idx;
+                lft = rht;
             }
         }
 
-        return ranges;
+        return ret;
     }
 };
