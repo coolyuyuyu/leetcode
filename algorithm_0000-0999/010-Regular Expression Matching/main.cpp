2db9c53d9415a6d41ec2d1aef094acb36c601171
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-14 00:30:16 +0800

    010: update solution

--- algorithm_0000-0999/010-Regular Expression Matching/main.cpp
+++ algorithm_0000-0999/010-Regular Expression Matching/main.cpp
@@ -28,8 +28,51 @@ public:
         }
     }
 
+    bool btmup_dp(string s, string p) {
+        int m = s.size(), n = p.size();
+
+        s = "#" + s;
+        p = "#" + p;
+
+        // dp[i][j]: whether s[1:i] matches p[1:j]
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+
+        dp[0][0] = true;
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = false;
+        }
+        for (int j = 1; j <= n; ++j) {
+            if (j & 1) {
+                dp[0][j] = false;
+            }
+            else {
+                dp[0][j] = (p[j] == '*' && (j < 2 || dp[0][j - 2]));
+            }
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (std::isalpha(p[j])) {
+                    dp[i][j] = (s[i] == p[j]) && dp[i - 1][j - 1];
+                }
+                else if (p[j] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    assert(0 <= (j - 2));
+                    bool cond1 = dp[i][j - 2];
+                    bool cond2 = dp[i - 1][j] && (s[i] == p[j - 1] || p[j - 1] == '.');
+                    dp[i][j] = cond1 || cond2;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
     bool isMatch(string s, string p) {
         //return isMatchRecursive1(s, p);
-        return isMatchRecursive2(s, 0, p, 0);
+        //return isMatchRecursive2(s, 0, p, 0);
+        return btmup_dp(s, p);
     }
 };
