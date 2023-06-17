ba5e32a41ae5debc787e3f3416a4595076c2ec9d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-17 23:53:12 +0800

    1649: initial bit solution

---
+++ algorithm_1000-1999/1649-Create Sorted Array through Instructions/main.cpp
@@ -0,0 +1,94 @@
+class BinaryIndexedTree {
+public:
+    BinaryIndexedTree(std::size_t size)
+        : m_size(size)
+        , m_nums(m_size + 1)
+        , m_sums(m_size + 1) {
+    }
+
+    BinaryIndexedTree(const std::vector<int>& nums)
+        : m_size(nums.size())
+        , m_nums(m_size + 1)
+        , m_sums(m_size + 1) {
+        std::size_t n = size();
+        for (std::size_t i = 0; i < n; ++i) {
+            set(i, nums[i]);
+        }
+    }
+
+    inline std::size_t size() const {
+        return m_size;
+    }
+
+    void set(std::size_t i, int val) {
+        setByIdx(i + 1, val);
+    }
+
+    int get(std::size_t i) const {
+        return getByIdx(i + 1);
+    }
+
+    int sum(std::size_t i) const {
+       return sumByIdx(i + 1);
+    }
+
+    int sum(std::size_t lo, std::size_t hi) const {
+        return sumByIdx(hi + 1) - sumByIdx(lo);
+    }
+
+private:
+    void setByIdx(std::size_t i, int val) {
+        assert(0 < i && i <= size());
+
+        int diff = val - m_nums[i];
+        m_nums[i] = val;
+
+        std::size_t n = size();
+        for (; i <= n; i += lowbit(i)) {
+            m_sums[i] += diff;
+        }
+    }
+
+    int getByIdx(std::size_t i) const {
+        assert(0 < i && i <= size());
+
+        return m_nums[i];
+    }
+
+    int sumByIdx(std::size_t i) const {
+        int ret = 0;
+        for (; i; i -= lowbit(i)) {
+            ret += m_sums[i];
+        }
+
+        return ret;
+    }
+
+    inline std::size_t lowbit(std::size_t i) const {
+        return i & ~(i - 1);
+    }
+
+    std::size_t m_size;
+    std::vector<int> m_nums;
+    std::vector<int> m_sums;
+};
+
+class Solution {
+public:
+    int M = 1e9 + 7;
+
+    int createSortedArray(vector<int>& instructions) {
+        int n = *std::max_element(instructions.begin(), instructions.end()) + 1;
+        BinaryIndexedTree bit(n);
+
+        int ret = 0;
+        for (int num : instructions) {
+            ret += std::min(bit.sum(num - 1), bit.sum(num + 1, n - 1));
+            ret %= M;
+
+            bit.set(num, bit.get(num) + 1);
+        }
+
+        return ret;
+    }
+};
