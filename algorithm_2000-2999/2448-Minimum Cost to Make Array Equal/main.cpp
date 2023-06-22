af88ba9eaacb0c95c8f8a2f7be3def93f1e9d753
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-22 15:40:55 +0800

    2448: update 3 solutions

--- algorithm_2000-2999/2448-Minimum Cost to Make Array Equal/main.cpp
+++ algorithm_2000-2999/2448-Minimum Cost to Make Array Equal/main.cpp
@@ -1,67 +1,98 @@
 class Solution {
 public:
-    long long binarySearch(vector<int>& nums, vector<int>& costs) {
-        auto computeCost = [&](int target) {
+    long long byBinarySearch(const vector<int>& nums, const vector<int>& costs) {
+        int n = nums.size();
+        std::function<long long(int)> computeCost = [&](int target) {
             long long totalCost = 0;
-            for (size_t i = 0; i < nums.size(); ++i) {
-                totalCost += (long long)(abs(nums[i] - target)) * costs[i];
+            for (int i = 0; i < n; ++i) {
+                totalCost += static_cast<long long>(costs[i]) * std::abs(target - nums[i]);
             }
-
             return totalCost;
         };
 
-        auto result = std::minmax_element(nums.begin(), nums.end());
-        int lo = *result.first, hi = *result.second;
-        long long minCost = computeCost(lo);
+        int lo, hi; {
+            auto result = std::minmax_element(nums.begin(), nums.end());
+            lo = *(result.first);
+            hi = *(result.second);
+        }
+        long long ret = computeCost(lo);
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            long long aCost = computeCost(mid);
-            long long bCost = computeCost( mid + 1);
-            if (aCost < bCost) {
-                minCost = aCost;
+            long long totalCost1 = computeCost(mid), totalCost2 = computeCost(mid + 1);
+            if (totalCost1 < totalCost2) {
+                ret = std::min(ret, totalCost1);
                 hi = mid;
             }
+            else if (totalCost1 == totalCost2) {
+                ret = std::min(ret, totalCost1);
+                break;
+            }
             else {
-                minCost = bCost;
+                ret = std::min(ret, totalCost2);
                 lo = mid + 1;
             }
         }
 
-        return minCost;
+        return ret;
     }
 
-    long long linearScan(vector<int>& nums, vector<int>& costs) {
-        vector<int> indexes(nums.size());
+    long long byThreePass(const vector<int>& nums, const vector<int>& costs) {
+        int n = nums.size();
+
+        vector<int> indexes(n);
         std::iota(indexes.begin(), indexes.end(), 0);
         std::sort(indexes.begin(), indexes.end(), [&](int index1, int index2) { return nums[index1] < nums[index2]; });
 
-        vector<long long> lftCosts(nums.size()), rhtCosts(nums.size());
-        long long totalCost;
-
-        lftCosts.front() = 0 ;
-        totalCost = 0;
-        for (size_t i = 0; (i + 1) < nums.size(); ++i) {
-            totalCost += costs[indexes[i]];
-            lftCosts[i + 1] = lftCosts[i] + totalCost * (nums[indexes[i + 1]] - nums[indexes[i]]);
+        vector<long long> costLft(n); // costLft[i]: the total cost to make nums[0:i-1] as nums[i]
+        costLft[0] = 0;
+        for (long long i = 1, sum = costs[indexes[0]]; i < n; ++i) {
+            costLft[i] = costLft[i - 1] + (nums[indexes[i]] - nums[indexes[i - 1]]) * sum;
+            sum += costs[indexes[i]];
         }
 
-        rhtCosts.back() = 0 ;
-        totalCost = 0;
-        for (size_t i = nums.size(); 1 < i--;) {
-            totalCost += costs[indexes[i]];
-            rhtCosts[i - 1] = rhtCosts[i] + totalCost * (nums[indexes[i]] - nums[indexes[i - 1]]);
+        vector<long long> costRht(n); // costRht[i]: the total cost to make nums[i+1:n-1] as nums[i]
+        costRht[n - 1] = 0;
+        for (long long i = n - 1, sum = costs[indexes[n - 1]]; 0 < i--;) {
+            costRht[i] = costRht[i + 1] + (nums[indexes[i + 1]] - nums[indexes[i]]) * sum;
+            sum += costs[indexes[i]];
         }
 
-        long long minCost = std::numeric_limits<long long>::max();
-        for (size_t i = 0; i < nums.size(); ++i) {
-            minCost = min(minCost, lftCosts[i] + rhtCosts[i]);
+        long long ret = LLONG_MAX;
+        for (int i = 0; i < n; ++i) {
+            ret = std::min(ret, costLft[i] + costRht[i]);
         }
 
-        return minCost;
+        return ret;
     }
 
-    long long minCost(vector<int>& nums, vector<int>& costs) {
-        //return binarySearch(nums, costs);
-        return linearScan(nums, costs);
+    long long byMath(const vector<int>& nums, const vector<int>& costs) {
+        int n = nums.size();
+
+        vector<int> indexes(n);
+        std::iota(indexes.begin(), indexes.end(), 0);
+        std::sort(indexes.begin(), indexes.end(), [&](int index1, int index2) { return nums[index1] < nums[index2]; });
+
+        long long sumCosts = std::accumulate(costs.begin(), costs.end(), 0LL);
+        int target;
+        for (long long i = 0, count = 0; i < n; ++i) {
+            count += costs[indexes[i]];
+            if ((sumCosts * 1.0 / 2) < count) {
+                target = nums[indexes[i]];
+                break;
+            }
+        }
+
+        long long totalCost = 0;
+        for (int i = 0; i < n; ++i) {
+            totalCost += static_cast<long long>(costs[i]) * std::abs(target - nums[i]);
+        }
+
+        return totalCost;
+    }
+
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        return byBinarySearch(nums, cost);
+        //return byThreePass(nums, cost);
+        //return byMath(nums, cost);  
     }
 };
