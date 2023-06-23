1830e8ccdad24328548130f06b596a00c9fcb0c2
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-23 15:05:20 +0800

    1027: update solution

--- algorithm_1000-1999/1027-Longest Arithmetic Subsequence/main.cpp
+++ algorithm_1000-1999/1027-Longest Arithmetic Subsequence/main.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
         int n = nums.size();
+        int offset = 500;
 
-        //dp[i][j]: the length of the longest arithmetic subsequence with difference as j ending at nums[i]
+        // dp[i][j]: the length of longest arithematic sequence with common difference as j and ending at nums[i]
         vector<vector<int>> dp(n, vector<int>(1001, 1));
 
         int ret = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 int diff = nums[i] - nums[j];
-                dp[i][diff + 500] = dp[j][diff + 500] + 1;
-                ret = std::max(ret, dp[i][diff + 500]);
+                dp[i][diff + offset] = dp[j][diff + offset] + 1;
+                ret = std::max(ret, dp[i][diff + offset]);
             }
         }
 
