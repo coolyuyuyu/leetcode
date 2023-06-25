c4209fb74c7920f7867528c269315d7d833c4b06
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-25 21:35:51 +0800

    903: update solution

--- algorithm_0000-0999/903-Valid Permutations for DI Sequence/main.cpp
+++ algorithm_0000-0999/903-Valid Permutations for DI Sequence/main.cpp
@@ -3,15 +3,16 @@ public:
     int M = 1e9 + 7;
 
     int numPermsDISequence(string s) {
-        int m = s.size() + 1;
+        int m = s.size() + 1, n = s.size();
+
         s = "#" + s;
 
-        // the number of valid length as i+1 permutation with last element as j
-        vector<vector<int>> dp(m, vector<int>(m + 1));
+        // dp[i][j]: the number of valid permutations of [0:i] such that ith number is j
+        vector<vector<int>> dp(m, vector<int>(m));
         dp[0][0] = 1;
         for (int i = 1; i < m; ++i) {
             for (int j = 0; j <= i; ++j) {
-                dp[i][j] = 0;
+                dp[i][j] == 0;
                 if (s[i] == 'I') {
                     for (int k = 0; k < j; ++k) {
                         dp[i][j] += dp[i - 1][k];
@@ -28,8 +29,8 @@ public:
         }
 
         int ret = 0;
-        for (int j = 0; j <= m; ++j) {
-            ret += dp[m - 1][j];
+        for (int i = 0; i < m; ++i) {
+            ret += dp[m - 1][i];
             ret %= M;
         }
 
