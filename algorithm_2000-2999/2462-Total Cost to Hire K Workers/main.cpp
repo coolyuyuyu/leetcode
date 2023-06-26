410827f497a9996c0205e23f8e42ea533f9cdd14
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-26 11:30:49 +0800

    2462: update solution

--- algorithm_2000-2999/2462-Total Cost to Hire K Workers/main.cpp
+++ algorithm_2000-2999/2462-Total Cost to Hire K Workers/main.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    long long totalCost(vector<int> costs, int k, int candidates) {
-        priority_queue<int, vector<int>, std::greater<int>> lftPQ, rhtPQ;
-
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int, vector<int>, greater<>> lftPQ, rhtPQ;
         int lft = 0, rht = costs.size() - 1;
         for (int i = 0; i < candidates && lft <= rht; ++i) {
-            lftPQ.emplace(costs[lft++]);
+            lftPQ.push(costs[lft++]);
             if (lft <= rht) {
-                rhtPQ.emplace(costs[rht--]);
+                rhtPQ.push(costs[rht--]);
             }
         }
 
-        long long c = 0;
+        long long ret = 0;
         for (int i = 0; i < k; ++i) {
-            if (lftPQ.empty()) {
-                c += rhtPQ.top();
-                rhtPQ.pop();
-            }
-            else if (rhtPQ.empty()) {
-                c += lftPQ.top();
-                lftPQ.pop();
-            }
-            else if (lftPQ.top() <= rhtPQ.top()) {
-                c += lftPQ.top();
+            if (rhtPQ.empty() || (!lftPQ.empty() && lftPQ.top() <= rhtPQ.top())) {
+                ret += lftPQ.top();
                 lftPQ.pop();
                 if (lft <= rht) {
                     lftPQ.push(costs[lft++]);
                 }
             }
             else {
-                c += rhtPQ.top();
+                ret += rhtPQ.top();
                 rhtPQ.pop();
                 if (lft <= rht) {
                     rhtPQ.push(costs[rht--]);
@@ -37,6 +28,6 @@ public:
             }
         }
 
-        return c;
+        return ret;
     }
 };
