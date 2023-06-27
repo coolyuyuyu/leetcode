1ba9505c804306d7d245ef4a11e73d87fc1ea955
coolyuyuyu <coolyuyuyu@gmail.com>
2023-06-27 23:33:18 +0800

    813: update solution

--- algorithm_0000-0999/813-Largest Sum of Averages/main.cpp
+++ algorithm_0000-0999/813-Largest Sum of Averages/main.cpp
@@ -7,12 +7,11 @@ public:
 
         vector<int> presum(n + 1);
         std::partial_sum(nums.begin(), nums.end(), presum.begin());
-        std::function<double(int lft, int rht)> avg = [&](int lft, int rht) {
-            int sum = presum[rht] - presum[lft - 1];
-            return static_cast<double>(sum) / (rht - lft + 1);
+        std::function<double(int, int)> avg = [&](int lo, int hi) {
+            return (presum[hi] - presum[lo - 1]) * 1.0 / (hi - lo + 1);
         };
 
-        // dp[i][numParts]: the maximum score of nums[1:i] such that nums[1:i] is partitioned into numParts subarrays
+        // dp[i][numParts]: the maximum sum of average of numParts subarray for nums[1:i]
         vector<vector<double>> dp(n + 1, vector<double>(k + 1));
         dp[0][0] = 0;
         for (int i = 1; i <= n; ++i) {
@@ -20,13 +19,14 @@ public:
         }
         for (int i = 1; i <= n; ++i) {
             for (int numParts = 1; numParts <= std::min(i, k); ++numParts) {
-                for (int j = i; numParts <= j; j--) {
+                //dp[i][numParts] = -DBL_MAX;
+                for (int j = i; numParts <= j; --j) {
                     dp[i][numParts] = std::max(dp[i][numParts], dp[j - 1][numParts - 1] + avg(j, i));
                 }
             }
         }
 
-        double ret = 0.0;
+        double ret = -DBL_MAX;
         for (int numParts = 1; numParts <= k; ++numParts) {
             ret = std::max(ret, dp[n][numParts]);
         }
