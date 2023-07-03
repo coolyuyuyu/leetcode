d714c3d86b302185fc0e901aacf6b8600f365489
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-03 18:34:57 +0800

    047: update solution

--- algorithm_0000-0999/047-Permutations II/main.cpp
+++ algorithm_0000-0999/047-Permutations II/main.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void permuteUniqueHelper(vector<int>& nums, int begin, vector<vector<int>>& results) {
-        if (nums.size() <= begin) {
-            results.push_back(nums);
-        }
-        else {
-            unordered_set<int> used;
-            for (int i = begin; i < nums.size(); ++i) {
-                pair<unordered_set<int>::iterator, bool> p = used.insert(nums[i]);
-                if (!p.second) {
-                    continue;
-                }
-
-                swap(nums[begin], nums[i]);
-                permuteUniqueHelper(nums, begin + 1, results);
-                swap(nums[begin], nums[i]);
-            }
-        }
-    }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> results;
-        permuteUniqueHelper(nums, 0, results);
-        return results;
+        vector<vector<int>> ret;
+        std::function<void(int)> f = [&](int begin) {
+            if (nums.size() <= begin) {
+                ret.push_back(nums);
+            }
+
+            unordered_set<int> visited;
+            for (int i = begin; i < nums.size(); ++i) {
+                if (visited.insert(nums[i]).second) {
+                    std::swap(nums[begin], nums[i]);
+                    f(begin + 1);
+                    std::swap(nums[begin], nums[i]);
+                }
+            }
+        };
+        f(0);
+
+        return ret;
     }
 };
