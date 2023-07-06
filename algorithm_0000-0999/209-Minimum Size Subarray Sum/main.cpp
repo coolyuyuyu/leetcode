b2bf874b980bb9382651bb6e8ba8c28cb02e92c0
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-07 00:28:08 +0800

    209: update solution

--- algorithm_0000-0999/209-Minimum Size Subarray Sum/main.cpp
+++ algorithm_0000-0999/209-Minimum Size Subarray Sum/main.cpp
@@ -1,62 +1,21 @@
 class Solution {
 public:
-    size_t minSubArrayLenByPartialSum(int s, vector<int>& nums) {
-        partial_sum(nums.begin(), nums.end(), nums.begin());
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
 
-        auto itr = lower_bound(nums.begin(), nums.end(), s);
-        if (itr == nums.end()) {
-            return 0;
-        }
-
-        size_t len = itr - nums.begin() + 1;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            size_t end = min(len + i - 1, nums.size());
-            for (size_t j = i; j < end; ++j) {
-                if (nums[j] - nums[i - 1] >= s) {
-                    len = min(len, j - i + 1);
-                    break;
-                }
-            }
-        }
-
-        return len;
-    }
-
-    size_t minSubArrayLenByLinearScan(int s, vector<int>& nums) {
-        size_t len = 0;
-        int sum = 0;
-        while (len < nums.size() && sum < s) {
-            sum += nums[len];
-            ++len;
-        }
-        if (sum < s) {
-            return 0;
-        }
-
-        size_t lft = 0, rht = len;
-        while (rht < nums.size()) {
-            if (sum < s) {
+        int ret = INT_MAX;
+        for (int lft = 0, rht = 0, sum = 0; lft < n; ++lft) {
+            while (rht < n && sum < target) {
                 sum += nums[rht++];
             }
-            else {
-                len = min(len, rht - lft);
-                sum -= nums[lft++];
+
+            if (target <= sum) {
+                ret = std::min(ret, rht - lft);
             }
+
+            sum -= nums[lft];
         }
 
-        while (lft < nums.size() && sum >= s) {
-            len = min(len, rht - lft);
-            sum -= nums[lft++];
-        }
-
-        return len;
-    }
-
-    size_t minSubArrayLen(int s, vector<int>& nums) {
-        assert(0 < s);
-
-        //return minSubArrayLenByPartialSum(s, nums);
-
-        return minSubArrayLenByLinearScan(s, nums);
+        return ret == INT_MAX ? 0 : ret;
     }
 };
