df088cc57be5afe121dd1dd9945eb82f3e7002f9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-07 23:56:50 +0800

    2024: update solution

--- algorithm_2000-2999/2024-Maximize the Confusion of an Exam/main.cpp
+++ algorithm_2000-2999/2024-Maximize the Confusion of an Exam/main.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int maxConsecutiveTargets(const string& s, char t, int k) {
-        int ret = 0;
-        for (int lft = 0, rht = 0, n = s.size(); lft < n; ++lft) {
-            for (; rht < n && (s[rht] == t || 0 < k); ++rht) {
-                if (s[rht] != t) {
-                    --k;
-                }
-            }
-            ret = std::max(ret, rht - lft); // [lft, rht);
-
-            if (s[lft] != t) {
-                ++k;
-            }
-        }
-
-        return ret;
-    }
-
-    // two pointers
     // Time: O(n)
     int maxConsecutiveAnswers(string answerKey, int k) {
-        return std::max(maxConsecutiveTargets(answerKey, 'T', k), maxConsecutiveTargets(answerKey, 'F', k));
+        int n = answerKey.size();
+        std::function<int(char, int)> f = [&](char target, int k) {
+            int ret = 0;
+            for (int lft = 0, rht = 0; lft < n; ++lft) {
+                for (; rht < n && (answerKey[rht] == target || 0 < k); ++rht) {
+                    if (answerKey[rht] != target) {
+                        --k;
+                    }
+                }
+
+                ret = std::max(ret, rht - lft);
+
+                if (answerKey[lft] != target) {
+                    ++k;
+                }
+            }
+
+            return ret;
+        };
+
+        return std::max(f('T', k), f('F', k));
     }
 };
