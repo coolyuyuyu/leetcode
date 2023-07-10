07196210293635cc592146e4a39f12cc83c3f03f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-11 00:13:12 +0800

    340: initial two pointers solution

---
+++ algorithm_0000-0999/340-Longest Substring with At Most K Distinct Characters/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        vector<int> cnts(256, 0);
+
+        int ret = 0;
+        for (int lft = 0, rht = 0, n = s.size(); lft < n; ++lft) {
+            for (;rht < n && (0 < k || (k == 0 && 0 < cnts[s[rht]])); ++rht) {
+                if (cnts[s[rht]]++ == 0) {
+                    k--;
+                }
+            }
+
+            ret = std::max(ret, rht - lft);
+
+            if (--cnts[s[lft]] == 0) {
+                ++k;
+            }
+        }
+
+        return ret;
+    }
+};
