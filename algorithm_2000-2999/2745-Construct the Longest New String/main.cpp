16bd74015ab45c1d4faa695738312513fed7691c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-10 23:59:36 +0800

    2745: update solution

--- algorithm_2000-2999/2745-Construct the Longest New String/main.cpp
+++ algorithm_2000-2999/2745-Construct the Longest New String/main.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int longestString(int x, int y, int z) {
-        int ret = 0;
-
-        if (x == y) {
-            ret += (4 * x);
-        }
-        else {
-            ret += 2 * (std::min(x, y) * 2  + 1);
-        }
-        ret += (2 * z);
-
-        return ret;
+        int cnt = 2 * std::min(x, y) + (x == y ? 0 : 1) + z;
+        return cnt * 2;
     }
 };
