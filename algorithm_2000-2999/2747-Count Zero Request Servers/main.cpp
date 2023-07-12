e3f044a5df417e58b7d334f9d5153d386be0c6a5
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-12 23:37:47 +0800

    2747: update solution

--- algorithm_2000-2999/2747-Count Zero Request Servers/main.cpp
+++ algorithm_2000-2999/2747-Count Zero Request Servers/main.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<int> countServers(int n, vector<vector<int>>& logs, int x, vector<int>& queries) {
-        std::sort(logs.begin(), logs.end(), [](const auto& log1, const auto& log2) { return log1[1] < log2[1]; });
+        std::sort(logs.begin(), logs.end(), [](const vector<int>& log1, const vector<int>& log2) { return log1[1] < log2[1]; });
 
         vector<int> qIndexes(queries.size());
         std::iota(qIndexes.begin(), qIndexes.end(), 0);
-        std::sort(qIndexes.begin(), qIndexes.end(), [&](int i1, int i2) { return queries[i1] < queries[i2]; });
+        std::sort(qIndexes.begin(), qIndexes.end(), [&](int qIdx1, int qIdx2) { return queries[qIdx1] < queries[qIdx2]; } );
 
         vector<int> cnts(n, 0);
-
         vector<int> ret(queries.size());
-        for (int lft = 0, rht = 0, numNonEmpty = 0, i = 0; i < queries.size(); ++i) {
+        for (int lft = 0, rht = 0, cntNonEmpty = 0, i = 0; i < qIndexes.size(); ++i) {
             int query = queries[qIndexes[i]];
-            while (rht < logs.size() && logs[rht][1] <= query) {
+
+            for (; rht < logs.size() && logs[rht][1] <= query; ++rht) {
                 if (cnts[logs[rht][0] - 1]++ == 0) {
-                    ++numNonEmpty;
+                    ++cntNonEmpty;
                 }
-                ++rht;
-            }
-            while (lft < logs.size() && logs[lft][1] < (query - x)) {
-                if (--cnts[logs[lft][0] - 1] == 0) {
-                    --numNonEmpty;
-                }
-                ++lft;
             }
 
-            ret[qIndexes[i]] = n - numNonEmpty;
+            for (; lft < logs.size() && logs[lft][1] < (query - x); ++lft) {
+                if (--cnts[logs[lft][0] - 1] == 0) {
+                    --cntNonEmpty;
+                }
+            }
+
+            ret[qIndexes[i]] = n - cntNonEmpty;
         }
 
         return ret;
