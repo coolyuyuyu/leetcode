481aed9b635010f7a6809bf21347532b1cf3b82a
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-12 23:15:15 +0800

    802: update solution

--- algorithm_0000-0999/802-Find Eventual Safe States/main.cpp
+++ algorithm_0000-0999/802-Find Eventual Safe States/main.cpp
@@ -1,50 +1,83 @@
 class Solution {
 public:
-    void eventualSafeNodesDfs(vector<vector<int>>& graph, int node, set<int>& nodes, vector<bool>& done, vector<bool>& unsafe) {
-        if (done[node]) {
-            return;
+    vector<int> bfs(const vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        vector<int> oDegrees(n, 0);
+        vector<vector<int>> pres(n);
+        for (int from = 0; from < n; ++from) {
+            for (int to : graph[from]) {
+                pres[to].push_back(from);
+                ++oDegrees[from];
+            }
         }
 
-        for (size_t i = 0; i < graph[node].size(); ++i) {
-            int nbr = graph[node][i];
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (oDegrees[i] == 0) {
+                q.push(i);
+            }
+        }
 
-            if (nodes.find(nbr) != nodes.end() || unsafe[nbr]) {
-                for (auto prevNode: nodes) {
-                    done[prevNode] = true;
-                    unsafe[prevNode] = true;
+        vector<int> ret;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+
+            ret.push_back(cur);
+
+            for (int pre : pres[cur]) {
+                if (--oDegrees[pre] == 0) {
+                    q.push(pre);
                 }
-                done[node] = true;
-                unsafe[node] = true;
-                return;
+            }
+        }
+        std::sort(ret.begin(), ret.end());
+
+        return ret;
+    }
+
+    vector<int> dfs(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        enum class State {
+            kNone,
+            kProcessing,
+            kVisited,
+        };
+        vector<State> states(n, State::kNone);
+
+        std::function<bool(int)> checkCycle = [&](int cur) {
+            if (states[cur] == State::kProcessing) {
+                return true;
+            }
+            else if (states[cur] == State::kVisited) {
+                return false;
             }
 
-            nodes.emplace(nbr);
-            eventualSafeNodesDfs(graph, nbr, nodes, done, unsafe);
-            nodes.erase(nbr);
+            states[cur] = State::kProcessing;
+            for (int nxt : graph[cur]) {
+                if (checkCycle(nxt)) {
+                    return true;
+                }
+            }
+            states[cur] = State::kVisited;
+
+            return false;
+        };
+
+        vector<int> ret;
+        for (int i = 0; i < n; ++i) {
+            if (!checkCycle(i)) {
+                ret.push_back(i);
+            }
         }
 
-        done[node] = true;
+        return ret;
     }
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        size_t nodeCnt = graph.size();
-        set<int> nodes;
-        vector<bool> done(nodeCnt, false);
-        vector<bool> unsafe(nodeCnt, false);
-        for (int node = 0; node < nodeCnt; ++node) {
-            nodes.emplace(node);
-            eventualSafeNodesDfs(graph, node, nodes, done, unsafe);
-            nodes.erase(node);
-        }
-
-        vector<int> result;
-        for (int node = 0; node < nodeCnt; ++node) {
-            if (unsafe[node] == false) {
-                result.emplace_back(node);
-            }
-        }
-
-        return result;
+        //return bfs(graph);
+        return dfs(graph);
     }
-
 };
