6633619f8ecf489da265fb5db2d56b10fd279098
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-15 11:04:54 +0800

    1751: initial dp solution

---
+++ algorithm_1000-1999/1751-Maximum Number of Events That Can Be Attended II/main.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxValue(vector<vector<int>>& events, int k) {
+        int n = events.size();
+
+        std::sort(events.begin(), events.end(), [](const auto& e1, const auto& e2) { return e1[1] < e2[1]; });
+        events.insert(events.begin(), {0, 0, 0});
+
+        // dp[i][j]: the maximum sum of values received by considering the first i events and attending j events
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= k; ++j) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= n; ++i) {
+            auto itr = std::lower_bound(
+                events.begin(), events.begin() + i,
+                vector<int>{0, events[i][0], 0},
+                [](const auto& e1, const auto& e2) { return e1[1] < e2[1]; });
+            int x = std::distance(events.begin(), itr) - 1;
+            for (int j = 1; j <= k; ++j) {
+                dp[i][j] = std::max(dp[i - 1][j], dp[x][j - 1] + events[i][2]);
+            }
+        }
+
+        int ret = 0;
+        for (int j = 1; j <= k; ++j) {
+            ret = std::max(ret, dp[n][j]);
+        }
+
+        return ret;
+    }
+};
