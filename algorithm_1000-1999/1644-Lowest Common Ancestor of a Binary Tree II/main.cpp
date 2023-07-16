ce03ed6d7cc221f0bdbeabc13411b54a27463d7b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-16 23:21:20 +0800

    1644: initial dp solution

--- algorithm_1000-1999/1644-Lowest Common Ancestor of a Binary Tree II/main.cpp
+++ algorithm_1000-1999/1644-Lowest Common Ancestor of a Binary Tree II/main.cpp
@@ -9,35 +9,24 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor_Recursive(TreeNode* root, TreeNode* p, TreeNode* q, int& count) {
-        if (!root) {
-            count = 0;
-            return root;
-        }
-
-        int lftCount;
-        TreeNode* lftLCA = lowestCommonAncestor_Recursive(root->left, p, q, lftCount);
-        if (lftCount == 2) {
-            count = 2;
-            return lftLCA;
-        }
-
-        int rhtCount;
-        TreeNode* rhtLCA = lowestCommonAncestor_Recursive(root->right, p, q, rhtCount);
-        if (rhtCount == 2) {
-            count = 2;
-            return rhtLCA;
-        }
-
-        count = lftCount + rhtCount;
-        count += (root == p ? 1 : 0);
-        count += (root == q ? 1 : 0);
-        return root;
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int count;
-        TreeNode* lca = lowestCommonAncestor_Recursive(root, p, q, count);
-        return (count == 2 ? lca : nullptr);
+        TreeNode* ret = nullptr;
+        std::function<int(TreeNode*)> f = [&](TreeNode* root) {
+            if (!root) {
+                return 0;
+            }
+
+            int cnt = f(root->left) + f(root->right);
+            cnt += (root == p ? 1 : 0);
+            cnt += (root == q ? 1 : 0);
+            if (cnt == 2 && ret == nullptr) {
+                ret = root;
+            }
+
+            return cnt;
+        };
+        f(root);
+
+        return ret;
     }
 };
