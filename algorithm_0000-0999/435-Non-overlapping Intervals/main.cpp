2cb559958b96ad5cde901262a7f6425f79237325
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-19 10:53:29 +0800

    435: update solution

--- algorithm_0000-0999/435-Non-overlapping Intervals/main.cpp
+++ algorithm_0000-0999/435-Non-overlapping Intervals/main.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& i1, const vector<int>& i2) { return i1[1] < i2[1]; });
+        std::sort(intervals.begin(), intervals.end(), [](const auto& i1, const auto& i2) { return i1[1] < i2[1]; });
 
-        size_t count = 0;
+        int ret = 0;
         for (size_t i = 0; i < intervals.size();) {
             size_t j = i + 1;
-            while (j < intervals.size() && intervals[j][0] < intervals[i][1]) {
-                ++j;
+            for (; j < intervals.size() && intervals[j][0] < intervals[i][1]; ++j) {
+                ++ret;
             }
-            count += (j - i - 1);
             i = j;
         }
 
-        return count;
+        return ret;
     }
 };
