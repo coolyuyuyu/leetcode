a6d72fac84eeb8f6f898d4071563b1d4ca12b474
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-22 07:32:50 +0800

    673: update solution

--- algorithm_0000-0999/673-Number of Longest Increasing Subsequence/main.cpp
+++ algorithm_0000-0999/673-Number of Longest Increasing Subsequence/main.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int, int>> dp(n, {1, 1}); // dp[i]: {len of LIS ending at nums[i], the number of LIS ending at nums[i]};
 
+        // dp[i]: {the length of LIS ending at nums[i], the number of LIS ending at nums[i]}
+        vector<pair<int, int>> dp(n, {1, 1});
+
+        int lenLIS = 0, cntLIS = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[j] < nums[i]) {
                     if ((dp[j].first + 1) > dp[i].first) {
-                        dp[i].first = std::max(dp[i].first, dp[j].first + 1);
+                        dp[i].first = dp[j].first + 1;
                         dp[i].second = dp[j].second;
                     }
                     else if ((dp[j].first + 1) == dp[i].first) {
@@ -16,20 +19,16 @@ public:
                     }
                 }
             }
-        }
 
-        int maxLen = 0;
-        int ret = 0;
-        for (int i = 0; i < n; ++i) {
-            if (maxLen < dp[i].first) {
-                maxLen = dp[i].first;
-                ret = dp[i].second;
+            if (lenLIS < dp[i].first) {
+                lenLIS = dp[i].first;
+                cntLIS = dp[i].second;
             }
-            else if (maxLen == dp[i].first) {
-                ret += dp[i].second;
+            else if (lenLIS == dp[i].first) {
+                cntLIS += dp[i].second;
             }
         }
 
-        return ret;
+        return cntLIS;
     }
 };
