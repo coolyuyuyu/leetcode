04c3c1cdb206b9ef32de130f6b472d3ffd59a9ef
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-22 07:33:01 +0800

    735: update solution

--- algorithm_0000-0999/735-Asteroid Collision/main.cpp
+++ algorithm_0000-0999/735-Asteroid Collision/main.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> stk;
+        vector<int> ret;
         for (int asteroid : asteroids) {
-            assert(asteroid != 0);
-
-            if (asteroid > 0) {
-                stk.push_back(asteroid);
-            }
-            else {
-                while (true) {
-                    if (stk.empty() || stk.back() < 0) {
-                        stk.push_back(asteroid);
-                        break;
-                    }
-
-                    int diff = stk.back() + asteroid;
-                    if (0 < diff) {
-                        break;
-                    }
-                    else if (diff == 0) {
-                        stk.pop_back();
-                        break;
-                    }
-                    else {
-                        stk.pop_back();
-                    }
+            while (!ret.empty() && 0 < ret.back() && asteroid < 0) {
+                int diff = abs(ret.back()) - abs(asteroid);
+                if (diff == 0) {
+                    ret.pop_back();
+                    asteroid = 0;
                 }
+                else if (diff < 0) {
+                    ret.pop_back();
+                }
+                else {
+                    asteroid = 0;
+                }
+            }
+
+            if (asteroid != 0) {
+                ret.push_back(asteroid);
             }
         }
 
-        return stk;
+        return ret;
     }
 };
