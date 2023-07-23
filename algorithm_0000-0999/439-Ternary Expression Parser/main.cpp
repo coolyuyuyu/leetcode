28e4355013842998b1d72678692824d72d7a6fbb
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-23 18:39:01 +0800

    439: update solution

--- algorithm_0000-0999/439-Ternary Expression Parser/main.cpp
+++ algorithm_0000-0999/439-Ternary Expression Parser/main.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
-    string parseTernary(string expression) {
+    string forward(const string& expression) {
+        int n = expression.size();
+
+        std::function<string(const string&)> eval = [](const string& expression) -> string {
+            if (expression[0] == 'T') {
+                return expression.substr(2, 1);
+            }
+            else {
+                return expression.substr(4, 1);
+            }
+        };
+
+        string token;
+        stack<string> stk;
+        for (int i = 0; i < n; ++i) {
+            if ((i + 1) < n && expression[i + 1] == '?') {
+                stk.push(std::move(token));
+                token += expression[i];
+            }
+            else {
+                token += expression[i];
+                while (token.size() == 5) {
+                    token = stk.top() + eval(token);
+                    stk.pop();
+                }
+            }
+        }
+
+        return token;
+    }
+
+    string backward(const string& expression) {
         stack<char> stk;
-        for (size_t i = expression.size(); 0 < i--;) {
+        for (int i = expression.size(); 0 < i--;) {
             char c = expression[i];
-            if ((c == 'T' || c == 'F') && !stk.empty() && stk.top() == '?') {
-                stk.pop();
-                char a = stk.top();
-                stk.pop();
-
-                stk.pop();
-                char b = stk.top();
-                stk.pop();
-
-                stk.push(c == 'T' ? a : b);
+            if (!stk.empty() && stk.top() == '?') {
+                stk.pop(); // question mark
+                char e1 = stk.top(); stk.pop();
+                stk.pop(); // colon mark
+                char e2 = stk.top(); stk.pop();
+                stk.push(c == 'T' ? e1 : e2);
             }
             else {
                 stk.push(c);
@@ -21,5 +48,10 @@ public:
         }
 
         return string(1, stk.top());
+    }
+
+    string parseTernary(string expression) {
+        //return forward(expression);
+        return backward(expression);
     }
 };
