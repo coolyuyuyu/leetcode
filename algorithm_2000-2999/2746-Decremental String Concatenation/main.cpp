d0912170c8f3cfd71970f79dcb4457e715291dfa
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-23 18:39:10 +0800

    2746: update solution

--- algorithm_2000-2999/2746-Decremental String Concatenation/main.cpp
+++ algorithm_2000-2999/2746-Decremental String Concatenation/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int minimizeConcatenatedLength(vector<string>& words) {
+    int dp(vector<string>& words) {
         int n = words.size();
 
         map<pair<char, char>, int> dp;
@@ -29,5 +29,42 @@ public:
         }
 
         return ret;
+    }
+
+    int dfs(vector<string>& words) {
+        int cache[words.size()][26][26];
+        for (int i = 0; i < words.size(); ++i) {
+            for (char bgn = 'a'; bgn <= 'z'; ++bgn) {
+                for (char end = 'a'; end <= 'z'; ++end) {
+                    cache[i][bgn - 'a'][end - 'a'] = -1;
+                }
+            }
+        }
+
+        // f(i, bgn, end): the minimum appended length if we already constructed first i words with bgn as first and end as last character
+        std::function<int(int, char, char)>f = [&](int i, char bgn, char end) -> int {
+            if (words.size() <= i) {
+                return 0;
+            }
+
+            int& ret = cache[i][bgn - 'a'][end - 'a'];
+            if (0 <= ret) {
+                return ret;
+            }
+            ret = INT_MAX;
+
+            int len = words[i].size();
+            ret = std::min(ret, len - (end == words[i].front() ? 1 : 0) + f(i + 1, bgn, words[i].back()));
+            ret = std::min(ret, len - (words[i].back() == bgn ? 1 : 0) + f(i + 1, words[i].front(), end));
+
+            return ret;
+        };
+
+        return words[0].size() + f(1,  words[0].front(),  words[0].back());
+    }
+
+    int minimizeConcatenatedLength(vector<string>& words) {
+        //return dp(words);
+        return dfs(words);
     }
 };
