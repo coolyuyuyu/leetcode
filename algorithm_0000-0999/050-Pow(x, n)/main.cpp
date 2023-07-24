0abbccba73a80a9cf3b527dd9b9baaab31783278
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-24 21:49:33 +0800

    050: update solution

--- algorithm_0000-0999/050-Pow(x, n)/main.cpp
+++ algorithm_0000-0999/050-Pow(x, n)/main.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        unsigned long long exp = n;
-        if (n < 0) {
-            x = 1 / x;
+    double recursive(double x, int n) {
+        std::function<double(double, long)> f = [&](double x, long exp) {
+            if (exp == 0) {
+                return 1.0;
+            }
+            else if (exp & 1) {
+                return x * f(x * x, exp >> 1);
+            }
+            else {
+                return f(x * x, exp >> 1);
+            }
+        };
+
+        long long exp = n;
+        if (exp < 0) {
             exp = -exp;
+            x = 1 / x;
         }
 
-        double result = 1.0;
-        while (exp) {
+        return f(x, exp);
+    }
+
+    double iterative(double x, int n) {
+        long long exp = n;
+        if (exp < 0) {
+            exp = -exp;
+            x = 1 / x;
+        }
+
+        double ret = 1.0;
+        for (; exp; exp >>= 1) {
             if (exp & 1) {
-                result *= x;
+                ret *= x;
             }
-            exp >>= 1;
             x *= x;
         }
 
-        return result;
+        return ret;
+    }
+
+    double myPow(double x, int n) {
+        return recursive(x, n);
+        //return iterative(x, n);
     }
 };
