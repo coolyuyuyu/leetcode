e2262285462c309ce7370762107bfedc1fd6e472
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-25 11:10:48 +0800

    852: update solution

--- algorithm_0000-0999/852-Peak Index in a Mountain Array/main.cpp
+++ algorithm_0000-0999/852-Peak Index in a Mountain Array/main.cpp
@@ -3,7 +3,7 @@ public:
     // Time: O(N), linear search
     int linearSearch(vector<int>& arr) {
         for (size_t i = 1; i < arr.size(); ++i) {
-            if (arr[i] < arr[i - 1]) {
+            if (arr[i - 1] > arr[i]) {
                 return i - 1;
             }
         }
@@ -17,14 +17,13 @@ public:
         size_t lo = 0, hi = arr.size() - 1;
         while (lo < hi) {
             size_t mid = lo + (hi - lo) / 2;
-            if (arr[mid + 1] < arr[mid]) {
-                hi = mid;
-            }
-            else {
+            if (arr[mid] < arr[mid + 1]) {
                 lo = mid + 1;
             }
+            else {
+                hi = mid;
+            }
         }
-        assert(lo == hi);
 
         return lo;
     }
