ff77ef91cb184fcec5bdac70678f3add128b5be9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-07-27 00:08:40 +0800

    746: update solution

--- algorithm_0000-0999/746-Min Cost Climbing Stairs/main.cpp
+++ algorithm_0000-0999/746-Min Cost Climbing Stairs/main.cpp
@@ -1,58 +1,71 @@
 class Solution {
 public:
     // TLE
-    int dp_TopDown(vector<int>& cost, size_t n) {
-        if (n == 0 || n == 1) {
-            return 0;
-        }
-        return std::min(dp_TopDown(cost, n - 2) + cost[n - 2], dp_TopDown(cost, n - 1) + cost[n - 1]);
+    int recursion(const vector<int>& cost) {
+        // f(i): minimum cost to reach the i step on a staircase
+        std::function<int(int)> f = [&](int i) {
+            if (i <= 1) {
+                return 0;
+            }
+
+            return std::min(f(i - 2) + cost[i - 2], f(i - 1) + cost[i - 1]);
+        };
+
+        return f(cost.size());
     }
 
-    int dp_TopDownMemorization(vector<int>& ans, vector<int>& cost, size_t n) {
-        if (n <= 1) {
-            return 0;
-        }
-        else if (ans[n] != -1) {
-            return ans[n];
-        }
+    int recursion_memo(const vector<int>& cost) {
+        vector<int> cache(cost.size() + 1, -1);
 
-        int c1 = dp_TopDownMemorization(ans, cost, n - 2) + cost[n - 2];
-        int c2 = dp_TopDownMemorization(ans, cost, n - 1) + cost[n - 1];
-        ans[n] = std::min(c1, c2);
+        // f(i): minimum cost to reach the i step on a staircase
+        std::function<int(int)> f = [&](int i) {
+            int& ret = cache[i];
+            if (0 <= ret) {
+                return ret;
+            }
 
-        return ans[n];
+            if (i <= 1) {
+                return ret = 0;
+            }
+
+            return ret = std::min(f(i - 2) + cost[i - 2], f(i - 1) + cost[i - 1]);
+        };
+
+        return f(cost.size());
     }
 
     // Space: O(N)
-    int dp_BtmUp(vector<int>& cost) {
-        vector<int> ans(cost.size() + 1);
-        for (size_t i = 2; i < ans.size(); ++i) {
-            ans[i] = std::min(ans[i - 2] + cost[i - 2], ans[i - 1] + cost[i - 1]);
+    int dp_BtmUp_v1(const vector<int>& cost) {
+        int n = cost.size();
+
+        // dp[i]: minimum cost to reach the i step on a staircase
+        vector<int> dp(n + 1);
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = std::min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
 
-        return ans.back();
+        return dp[n];
     }
 
     // Space: O(1)
-    int dp_BtmUpConstSpace(vector<int>& cost) {
-        int x = 0, y = 0;
-        for (size_t i = 2; i <= cost.size(); ++i) {
-            int tmp = std::min(x + cost[i - 2], y + cost[i - 1]);
-            x = y;
-            y = tmp;
+    int dp_BtmUp_v2(const vector<int>& cost) {
+        int n = cost.size();
+
+        int cur = 0, pre = 0;
+        for (int i = 2; i <= n; ++i) {
+            int tmp = std::min(pre + cost[i - 2], cur + cost[i - 1]);
+            pre = cur;
+            cur = tmp;
         }
 
-        return y;
+        return cur;
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        //return dp_TopDown(cost, cost.size());
-
-        //vector<int> ans(cost.size() + 1, -1);
-        //return dp_TopDownMemorization(ans, cost, cost.size());
-
-        //return dp_BtmUp(cost);
-
-        return dp_BtmUpConstSpace(cost);
+        //return recursion(cost);
+        //return recursion_memo(cost);
+        //return dp_BtmUp_v1(cost);
+        return dp_BtmUp_v2(cost);
     }
 };
