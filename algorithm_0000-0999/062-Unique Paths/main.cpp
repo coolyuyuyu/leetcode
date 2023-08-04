71eaae9874f7947f0afa3893f90ee76b962aada2
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-04 11:38:45 +0800

    062: update solution

--- algorithm_0000-0999/062-Unique Paths/main.cpp
+++ algorithm_0000-0999/062-Unique Paths/main.cpp
@@ -2,69 +2,54 @@ class Solution {
 public:
     // Time: O(m*n), Space: O(m*n)
     int dp1(int m, int n) {
-        vector<vector<int>> cnts(m, vector<int>(n));
-        for (size_t r = 0; r < m; ++r) {
-            for (size_t c = 0; c < n; ++c) {
-                if (r == 0 || c == 0) {
-                    cnts[r][c] = 1;
-                }
-                else {
-                    cnts[r][c] = cnts[r - 1][c] + cnts[r][c - 1];
-                }
+        // dp[r][c] the number of possible unique paths that the robot can take to reach (r, c)
+        vector<vector<int>> dp(m, vector<int>(n));
+        for (int r = 0; r < m; ++r) {
+            dp[r][0] = 1;
+        }
+        for (int c = 0; c < n; ++c) {
+            dp[0][c] = 1;
+        }
+        for (int r = 1; r < m; ++r) {
+            for (int c = 1; c < n; ++c) {
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
             }
         }
 
-        return cnts[m - 1][n - 1];
+        return dp[m - 1][n - 1];
     }
 
     // Time: O(m*n), Space: O(m*2)
     int dp2(int m, int n) {
-        vector<int> curCnts(n), preCnts(n);
-        for (size_t r = 0; r < m; ++r) {
-            curCnts.swap(preCnts);
-            for (size_t c = 0; c < n; ++c) {
-                if (r == 0 || c == 0) {
-                    curCnts[c] = 1;
-                }
-                else {
-                    curCnts[c] = preCnts[c] + curCnts[c - 1];
-                }
+        vector<int> dp(n);
+        for (int c = 0; c < n; ++c) {
+            dp[c] = 1;
+        }
+        for (int r = 1; r < m; ++r) {
+            dp[0] = 1;
+            for (int c = 1; c < n; ++c) {
+                dp[c] += dp[c - 1];
             }
         }
 
-        return curCnts[n - 1];
+        return dp[n - 1];
     }
 
-    // Time: O(m*n), Space: O(m)
-    int dp3(int m, int n) {
-        vector<int> cnts(n);
-        for (size_t r = 0; r < m; ++r) {
-            for (size_t c = 0; c < n; ++c) {
-                if (r == 0 || c == 0) {
-                    cnts[c] = 1;
-                }
-                else {
-                    cnts[c] += cnts[c - 1];
-                }
-            }
-        }
-
-        return cnts[n - 1];
-    }
-
-    // Time: O(m+n), Space: O(1)
     int math(int m, int n) {
-        auto combination = [](int n, int r) -> long {
-            if (r < (n / 2)) {
-                r = n - r;
+        std::map<pair<int, int>, int> cache;
+        std::function<int(int, int)> combination = [&](int n, int r) {
+            if (r == 0 || r == n) {
+                return 1;
+            }
+            else if (r == 1) {
+                return n;
             }
 
-            long ans = 1;
-            for (int i = r + 1, j = 1; j <= (n - r); ++i, ++j) {
-                ans *= i;
-                ans /= j;
+            if (cache.find({n, r}) != cache.end()) {
+                return cache[{n, r}];
             }
-            return ans;
+
+            return cache[{n, r}] = combination(n - 1, r - 1) + combination(n - 1, r);
         };
 
         return combination(m + n - 2, m - 1);
@@ -73,7 +58,6 @@ public:
     int uniquePaths(int m, int n) {
         //return dp1(m, n);
         //return dp2(m, n);
-        //return dp3(m, n);
         return math(m, n);
     }
 };
