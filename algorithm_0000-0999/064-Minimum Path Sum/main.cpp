62770037e16915dbb086e96d1ab08fae0ca72723
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-05 00:15:16 +0800

    064: update solution

--- algorithm_0000-0999/064-Minimum Path Sum/main.cpp
+++ algorithm_0000-0999/064-Minimum Path Sum/main.cpp
@@ -3,12 +3,14 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         size_t m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
-        for (size_t r = 0; r < m; ++r) {
-            for (size_t c = 0; c < n; ++c) {
-                if (r == 0 && c == 0) {
-                    continue;
-                }
-
+        for (size_t r = 1; r < m; ++r) {
+            grid[r][0] += grid[r - 1][0];
+        }
+        for (size_t c = 1; c < n; ++c) {
+            grid[0][c] += grid[0][c - 1];
+        }
+        for (size_t r = 1; r < m; ++r) {
+            for (size_t c = 1; c < n; ++c) {
                 grid[r][c] += std::min(0 < r ? grid[r - 1][c] : INT_MAX, 0 < c ? grid[r][c - 1] : INT_MAX);
             }
         }
