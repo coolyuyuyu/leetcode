e77832dddd2fd849c87a0c6445adb67e07a40bb1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-07 01:52:51 +0800

    221: update solution

--- algorithm_0000-0999/221-Maximal Square/main.cpp
+++ algorithm_0000-0999/221-Maximal Square/main.cpp
@@ -1,76 +1,31 @@
 class Solution {
 public:
-    // Space(rowCnt, colCnt) = rowCnt * colCnt
-    size_t maximalSquareV1(vector<vector<char>>& matrix) {
-        size_t rowCnt = matrix.size();
-        size_t colCnt = matrix.empty() ? 0 : matrix.front().size();
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
-        size_t maxLength = 0;
-        vector<vector<size_t>> dp(rowCnt, vector<size_t>(colCnt, 0));
-        for (size_t row = 0; row < rowCnt; ++row) {
-            dp[row][0] = matrix[row][0] - '0';
-            maxLength = max(maxLength, dp[row][0]);
+        // dp[i][j]: the length of largest square containing only 1's and its bottom-right corner is (i, j)
+        vector<vector<int>> dp(m, vector<int>(n));
+        int len = 0;
+        for (int r = 0; r < m; ++r) {
+            dp[r][0] = matrix[r][0] - '0';
+            len = std::max(len, dp[r][0]);
         }
-        for (size_t col = 0; col < colCnt; ++col) {
-            dp[0][col] = matrix[0][col] - '0';
-            maxLength = max(maxLength, dp[0][col]);
+        for (int c = 0; c < n; ++c) {
+            dp[0][c] = matrix[0][c] - '0';
+            len = std::max(len, dp[0][c]);
         }
-
-        for (size_t row = 1; row < rowCnt; ++row) {
-            for (size_t col = 1; col < colCnt; ++col) {
-                if (matrix[row][col] == '1') {
-                    dp[row][col] = min(dp[row - 1][col - 1], min(dp[row - 1][col], dp[row][col - 1])) + 1;
-                    maxLength = max(maxLength, dp[row][col]);
+        for (int r = 1; r < m; ++r) {
+            for (int c = 1; c < n; ++c) {
+                if (matrix[r][c] == '1') {
+                    dp[r][c] = std::min({dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]}) + 1;
+                    len = std::max(len, dp[r][c]);
                 }
                 else {
-                    dp[row][col] = 0;
+                    dp[r][c] = 0;
                 }
             }
         }
 
-        return maxLength * maxLength;
-    }
-
-    // Space(rowCnt, colCnt) = colCnt
-    size_t maximalSquareV2(vector<vector<char>>& matrix) {
-        size_t rowCnt = matrix.size();
-        size_t colCnt = matrix.empty() ? 0 : matrix.front().size();
-
-        size_t maxLength = 0;
-        vector<size_t> dp(colCnt, 0);
-        for (size_t col = 0; col < colCnt; ++col) {
-            dp[col] = matrix[0][col] - '0';
-            maxLength = max(maxLength, dp[col]);
-        }
-        for (size_t row = 0; row < rowCnt && maxLength == 0; ++row) {
-            if (matrix[row][0] == '1') {
-                maxLength = 1;
-            }
-        }
-
-        for (size_t row = 1; row < rowCnt; ++row) {
-            size_t prevLength = dp[0];
-            dp[0] = matrix[row][0] - '0';
-            for (size_t col = 1; col < colCnt; ++col) {
-                size_t tmp = dp[col];
-                if (matrix[row][col] == '1') {
-                    dp[col] = min(prevLength, min(dp[col], dp[col - 1])) + 1;
-                    maxLength = max(maxLength, dp[col]);
-                }
-                else {
-                    dp[col] = 0;
-                }
-
-                prevLength = tmp;
-            }
-        }
-
-        return maxLength * maxLength;
-    }
-
-    size_t maximalSquare(vector<vector<char>>& matrix) {
-        //return maximalSquareV1(matrix);
-
-        return maximalSquareV2(matrix);
+        return len * len;
     }
 };
