4d2ce83a217a5eb6b3c4f4a30e03ccfcbd46ed08
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-07 01:53:04 +0800

    920: initial solution

---
+++ algorithm_0000-0999/920-Number of Music Playlists/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int M = 1e9 + 7;
+
+    int numMusicPlaylists(int n, int goal, int k) {
+        // dp[i][j]: the number of playlists of length i using j different songs;
+        vector<vector<long>> dp(goal + 1, vector<long>(n + 1));
+        dp[1][1] = n;
+        for (int i = 2; i <= goal; ++i) {
+            for (int j = 1; j <= std::min(n, i); ++j) {
+                dp[i][j] = dp[i - 1][j] * std::max(j - k, 0) + dp[i - 1][j - 1] * (n - (j - 1));
+                dp[i][j] %= M;
+            }
+        }
+
+        return dp[goal][n];
+    }
+};
