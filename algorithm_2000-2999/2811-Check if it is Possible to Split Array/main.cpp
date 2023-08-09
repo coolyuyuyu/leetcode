04b448a74887e283b4e418e857b0d93765946b81
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-10 02:44:20 +0800

    2811: update solution

--- algorithm_2000-2999/2811-Check if it is Possible to Split Array/main.cpp
+++ algorithm_2000-2999/2811-Check if it is Possible to Split Array/main.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
-    bool canSplitArray(vector<int>& nums, int m) {
+    int dynamicProgramming(const vector<int>& nums, int m) {
         int n = nums.size();
-        if (n <= 2) {
-            return true;
-        }
 
         vector<int> presum(n);
         std::partial_sum(nums.begin(), nums.end(), presum.begin());
@@ -17,21 +14,35 @@ public:
         for (int i = 0; i < n; ++i) {
             dp[i][i] = true;
         }
-        for (int len = 2; len <= n; ++len) {
+        for (int i = 0, j = 1; j < n; ++i, ++j) {
+            dp[i][j] = true;
+        }
+        for (int len = 3; len <= n; ++len) {
             for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
-                dp[i][j] = false;
-                if (sum(i, j) < m) {
-                    continue;
-                }
-                for (int k = i; k < j; ++k) {
-                    if (dp[i][k] && dp [k + 1][j]) {
-                        dp[i][j] = true;
-                        break;
-                    }
-                }
+                dp[i][j] = (dp[i + 1][j] && m <= sum(i + 1, j)) || (dp[i][j - 1] && m <= sum(i, j - 1));
             }
         }
 
         return dp[0][n - 1];
+    }
+
+    int greedy(const vector<int>& nums, int m) {
+        int n = nums.size();
+        if (n <= 2) {
+            return true;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            if (m <= (nums[i - 1] + nums[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool canSplitArray(vector<int>& nums, int m) {
+        //return dynamicProgramming(nums, m);
+        return greedy(nums, m);
     }
 };
