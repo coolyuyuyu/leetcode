515bc052f0dab87fb36c51a79de3e213ccfa8a6b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-09 15:35:59 +0800

    2616: update solution

--- algorithm_2000-2999/2616-Minimize the Maximum Difference of Pairs/main.cpp
+++ algorithm_2000-2999/2616-Minimize the Maximum Difference of Pairs/main.cpp
@@ -3,19 +3,19 @@ public:
     int minimizeMax(vector<int>& nums, int p) {
         std::sort(nums.begin(), nums.end());
 
-        auto f = [&nums, p](int maxDiff) {
-            int numPairs = 0;
-            for (int i = 1; i < nums.size() && numPairs < p; ++i) {
+        std::function<bool(int)> f = [&](int maxDiff) {
+            int cnt = 0;
+            for (int i = 1; i < nums.size() && cnt < p; ++i) {
                 if ((nums[i] - nums[i - 1]) <= maxDiff) {
+                    ++cnt;
                     ++i;
-                    ++numPairs;
                 }
             }
 
-            return p <= numPairs;
+            return p <= cnt;
         };
 
-        int lo = 0, hi = 1e9;
+        int lo = 0, hi = nums.back() - nums.front();
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             if (f(mid)) {
