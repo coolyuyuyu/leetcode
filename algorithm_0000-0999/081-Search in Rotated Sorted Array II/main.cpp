3a97c35a39110e8561d131780be4121427f6de4a
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-10 19:56:22 +0800

    081: update solution

--- algorithm_0000-0999/081-Search in Rotated Sorted Array II/main.cpp
+++ algorithm_0000-0999/081-Search in Rotated Sorted Array II/main.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         size_t lo = 0, hi = nums.size() - 1;
-        while (hi > 0 && nums[hi] == nums[0]) {
+        while (lo < hi && nums[lo] == nums[hi]) {
             --hi;
         }
         for (; lo <= hi;) {
             size_t mid = lo + (hi - lo) / 2;
             if (nums[mid] == target) { return true; }
-            if ((nums[0] <= nums[mid]) == (nums[0] <= target)) { 
-                if (nums[mid] <= target) {
+            if ((nums[0] <= nums[mid]) == (nums[0] <= target)) {
+                if (nums[mid] < target) {
                     lo = mid + 1;
                 }
                 else {
