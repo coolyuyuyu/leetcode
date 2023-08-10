139224b9386429e2ca1830d1a1519ada75551194
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-10 19:56:13 +0800

    033: update solution

--- algorithm_0000-0999/033-Search in Rotated Sorted Array/main.cpp
+++ algorithm_0000-0999/033-Search in Rotated Sorted Array/main.cpp
@@ -38,8 +38,8 @@ public:
         for (size_t lo = 0, hi = nums.size() - 1; lo <= hi;) {
             size_t mid = lo + (hi - lo) / 2;
             if (nums[mid] == target) { return mid; }
-            if ((nums[0] <= nums[mid] && nums[0] <= target) || (nums[0] > nums[mid] && nums[0] > target)) {
-                if (nums[mid] <= target) {
+            if ((nums[0] <= nums[mid]) == (nums[0] <= target)) {
+                if (nums[mid] < target) {
                     lo = mid + 1;
                 }
                 else {
