b3f340c42bfd8c467c7d55ecd2fa4e7c3c986f66
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-12 23:45:39 +0800

    546: update solution

--- algorithm_0000-0999/546-Remove Boxes/main.cpp
+++ algorithm_0000-0999/546-Remove Boxes/main.cpp
@@ -1,130 +1,49 @@
 class Solution {
 public:
-    struct Box {
-        Box(int num_, int count_)
-            : num(num_)
-            , count(count_) {
-        }
-
-        bool operator<(const Box rhs)const {
-            if (num == rhs.num) {
-                return count < rhs.count;
-            }
-            else {
-                return num < rhs.num;
-            }
-        }
-
-        int num;
-        int count;
-    };
-
-    struct State {
-        State(int score_, const vector<Box>& boxes_)
-            : score(score_)
-            , boxes(boxes_)
-            , scoreUpr(score_)
-            , scoreLwr(score_) {
-            unordered_map<int, int> counts;
-            for (const Box& box : boxes) {
-                scoreUpr -= (box.count * box.count);
-                counts[box.num] += box.count;
+    int dp[105][105][105];
+    int removeBoxes(vector<int>& boxes) {
+        std::function<int(int, int, int)> dfs = [&](int l, int r, int k) {
+            if (l > r) {
+                return 0;
             }
 
-            if (counts.size() == boxes.size()) {
-                boxes.clear();
-                score = scoreLwr = scoreUpr;
+            int& ret = dp[l][r][k];
+            if (0 < ret) {
+                return ret;
             }
-            else {
-                for (const pair<int, int>& p : counts) {
-                    scoreLwr -= (p.second * p.second);
-                }
-            }
-        }
 
-        int score;
-        vector<Box> boxes;
-        int scoreUpr;
-        int scoreLwr;
-    };
+            int cnt = k;
+            int i = r;
+            while (l <= i && boxes[i] == boxes[r]) {
+                --i;
+                ++cnt;
+            }
+            ret = dfs(l, i, 0) + cnt * cnt;
 
-    // A* Branch and Bound + Memorization, TLE
-    int removeBoxesRecv(const vector<Box>& boxes) {
-        auto comp = [](const State& a, const State& b) {
-            if (b.scoreLwr != a.scoreLwr) {
-                return b.scoreLwr < a.scoreLwr;
+            for (int j = i; l <= j; --j) {
+                if (boxes[j] != boxes[r]) { continue; }
+                if (boxes[j + 1] == boxes[r]) { continue; }
+
+                ret = std::max(ret, dfs(l, j, cnt) + dfs(j + 1, i, 0));
             }
-            else {
-                return b.scoreUpr < a.scoreUpr;
-            }
+
+            return ret;
         };
-        priority_queue<State, vector<State>, decltype(comp)> pq(comp);
-        pq.emplace(0, boxes);
 
-        int totalStateCount = 0;
-        int cacheHitCount = 0;
-
-        int scoreLowest = 0;
-        map<vector<Box>, int> cache;
-        while (!pq.empty()) {
-            State state = pq.top();
-            pq.pop();
-            ++totalStateCount;
-
-            auto p = cache.emplace(state.boxes, state.score);
-            if (!p.second) {
-                cacheHitCount++;
-                if (p.first->second <= state.score) {
-                    continue;
-                }
-                else {
-                    p.first->second = state.score;
-                }
-            }
-
-            if (state.scoreLwr == state.scoreUpr) {
-                scoreLowest = state.score;
-                break;
-            }
-
-            const vector<Box>& boxes = state.boxes;
-            for (size_t i = 0; i + 1 < boxes.size(); ++i) {
-                for (size_t j = i + 2; j < boxes.size(); ++j) {
-                    if (boxes[i].num == boxes[j].num) {
-                        int scoreTmp = state.score;
-                        for (size_t k = i + 1; k < j; ++k) {
-                            scoreTmp -= boxes[k].count * boxes[k].count;
-                        }
-
-                        vector<Box> boxesTmp(boxes.begin(), boxes.begin() + i);
-                        boxesTmp.emplace_back(boxes[i].num, boxes[i].count + boxes[j].count);
-                        boxesTmp.insert(boxesTmp.end(), boxes.begin() + j + 1, boxes.end());
-
-
-                        pq.emplace(scoreTmp, boxesTmp);
-
-                        break;
-                    }
-                }
-            }
-        }
-
-        cout << "totalStateCount=" << totalStateCount << endl;
-        cout << "cacheHitCount=" << cacheHitCount << endl;
-
-        return -scoreLowest;
-    }
-
-    int removeBoxes(vector<int>& nums) {
-        vector<Box> boxes;
-        for (int num : nums) {
-            if (boxes.empty() || boxes.back().num != num) {
-                boxes.emplace_back(num, 1);
-            }
-            else {
-                ++boxes.back().count;
-            }
-        }
-        return removeBoxesRecv(boxes);
+        return dfs(0, boxes.size() - 1, 0);
     }
 };
+
+// dp[l][r][k]: maximum points from boxes[l:r] with k boxes of same color as boxes[r] appened at the end
+//         ? ... OOO   XXX   OOO   XXX   OOO   [...]
+//         ^       ^     ^     ^     ^     ^   ^   ^
+//         l       j1    i1    j0    i0    r   --k--
+
+// 1.     |                            |
+//     dp[l][i0][0] + (r - i0 + k) ^ 2
+
+// 2.     |                |
+//     dp[l][j0][r - i0 + k] + dp[j0 + 1][i0][0]
+
+// 3.     |          |
+//     dp[l][j1][r - i0 + k] + dp[j1 + 1][i0][0]
