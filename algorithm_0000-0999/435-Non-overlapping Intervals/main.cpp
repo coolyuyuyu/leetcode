292699e68f7cc3a01d9a7d3edc6b05d13ca0822d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-08-26 23:35:32 +0800

    435: update 2 greedy solutions

--- algorithm_0000-0999/435-Non-overlapping Intervals/main.cpp
+++ algorithm_0000-0999/435-Non-overlapping Intervals/main.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+    int greedy1(vector<vector<int>>& intervals) {
         std::sort(intervals.begin(), intervals.end(), [](const auto& i1, const auto& i2) { return i1[1] < i2[1]; });
 
-        int ret = 0;
-        for (size_t i = 0; i < intervals.size();) {
-            size_t j = i + 1;
-            for (; j < intervals.size() && intervals[j][0] < intervals[i][1]; ++j) {
-                ++ret;
+        int n = intervals.size();
+
+        int maxNonOverlapping = 0;
+        for (int i = 0; i < n;) {
+            int j = i + 1;
+            for (; j < n && intervals[i][1] > intervals[j][0]; ++j) {
+                ;
             }
+            ++maxNonOverlapping;
+
             i = j;
         }
 
-        return ret;
+        return n - maxNonOverlapping;
+    }
+
+    int greedy2(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](const auto& i1, const auto& i2) { return i1[0] < i2[0]; });
+
+        int n = intervals.size();
+
+        int maxNonOverlapping = 0;
+        int far = INT_MIN;
+        for (const auto& interval : intervals) {
+            if (far <= interval[0]) {
+                far = interval[1];
+                ++maxNonOverlapping;
+            }
+            else if (interval[1] < far) {
+                far = interval[1];
+            }
+        }
+
+        return n - maxNonOverlapping;
+    }
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        //return greedy1(intervals);
+        return greedy2(intervals);
     }
 };
