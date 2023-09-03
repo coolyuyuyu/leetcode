30b3fcdd2bfccb76cba447cea5f6c8599c34de93
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-03 18:38:51 +0800

    062: update solution

--- algorithm_0000-0999/062-Unique Paths/main.cpp
+++ algorithm_0000-0999/062-Unique Paths/main.cpp
@@ -3,11 +3,12 @@ public:
     // Time: O(m*n), Space: O(m*n)
     int dp1(int m, int n) {
         // dp[r][c] the number of possible unique paths that the robot can take to reach (r, c)
-        vector<vector<int>> dp(m, vector<int>(n));
-        for (int r = 0; r < m; ++r) {
+        int dp[m][n];
+        dp[0][0] = 1;
+        for (int r = 1; r < m; ++r) {
             dp[r][0] = 1;
         }
-        for (int c = 0; c < n; ++c) {
+        for (int c = 1; c < n; ++c) {
             dp[0][c] = 1;
         }
         for (int r = 1; r < m; ++r) {
@@ -21,35 +22,38 @@ public:
 
     // Time: O(m*n), Space: O(m*2)
     int dp2(int m, int n) {
-        vector<int> dp(n);
+        int dp[n];
         for (int c = 0; c < n; ++c) {
             dp[c] = 1;
         }
         for (int r = 1; r < m; ++r) {
-            dp[0] = 1;
             for (int c = 1; c < n; ++c) {
                 dp[c] += dp[c - 1];
             }
         }
-
         return dp[n - 1];
     }
 
     int math(int m, int n) {
         std::map<pair<int, int>, int> cache;
         std::function<int(int, int)> combination = [&](int n, int r) {
+            if ((n - r) < r) {
+                return combination(n, n - r);
+            }
+
             if (r == 0 || r == n) {
                 return 1;
             }
             else if (r == 1) {
                 return n;
             }
-
-            if (cache.find({n, r}) != cache.end()) {
+            else if (cache.find({n, r}) != cache.end()) {
                 return cache[{n, r}];
             }
 
-            return cache[{n, r}] = combination(n - 1, r - 1) + combination(n - 1, r);
+            int& ret = cache[{n, r}];
+            ret = combination(n - 1, r - 1) + combination(n - 1, r);
+            return ret;
         };
 
         return combination(m + n - 2, m - 1);
