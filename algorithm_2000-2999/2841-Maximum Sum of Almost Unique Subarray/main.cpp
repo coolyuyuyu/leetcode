d047a5e2b10bf7da2854817f11288a6986ca5f97
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-04 11:59:15 +0800

    2841: update solution

--- algorithm_2000-2999/2841-Maximum Sum of Almost Unique Subarray/main.cpp
+++ algorithm_2000-2999/2841-Maximum Sum of Almost Unique Subarray/main.cpp
@@ -3,26 +3,30 @@ public:
     long long maxSum(vector<int>& nums, int m, int k) {
         long long sum = 0;
         unordered_map<int, int> cnts;
+        int uniqueCnt = 0;
         for (int i = 0; i < k; ++i) {
             sum += nums[i];
-            ++cnts[nums[i]];
+            if (++cnts[nums[i]] == 1) {
+                ++uniqueCnt;
+            }
         }
 
         long long ret = 0;
-        if (m <= cnts.size()) {
+        if (m <= uniqueCnt) {
             ret = sum;
         }
         for (int i = k; i < nums.size(); ++i) {
             sum -= nums[i - k];
             sum += nums[i];
 
-            --cnts[nums[i - k]];
-            ++cnts[nums[i]];
-            if (cnts[nums[i - k]] == 0) {
-                cnts.erase(nums[i - k]);
+            if (--cnts[nums[i - k]] == 0) {
+                --uniqueCnt;
+            }
+            if (++cnts[nums[i]] == 1) {
+                ++uniqueCnt;
             }
 
-            if (m <= cnts.size()) {
+            if (m <= uniqueCnt) {
                 ret = std::max(ret, sum);
             }
         }
