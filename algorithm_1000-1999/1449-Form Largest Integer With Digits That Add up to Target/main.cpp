9fa58af6ab13d1d4fa3f9545f4972a7125de652b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-05 23:38:28 +0800

    1449: update solution

--- algorithm_1000-1999/1449-Form Largest Integer With Digits That Add up to Target/main.cpp
+++ algorithm_1000-1999/1449-Form Largest Integer With Digits That Add up to Target/main.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     string largestNumber(vector<int>& cost, int target) {
-        // dp[i]: the maximum integer which the total cost is equal to i
+        cost.insert(cost.begin(), -1);
+
+        // dp[i]: the maximum integer you can paint where the cost is i
         vector<string> dp(target + 1);
+        dp[0] = "";
         for (int i = 1; i <= target; ++i) {
             dp[i] = "0";
         }
         for (int i = 1; i <= target; ++i) {
-            for (int d = 1; d <= 9; ++d) {
-                if (i < cost[d - 1]) { continue; }
-                if (dp[i - cost[d - 1]]== "0") { continue; }
-                string tmp = dp[i - cost[d - 1]] + std::to_string(d);
+            for (int j = 1; j <= 9; ++j) {
+                if (i < cost[j]) { continue; }
+                if (dp[i - cost[j]] == "0") { continue; }
+                string tmp = dp[i - cost[j]] + std::to_string(j);
                 if (dp[i].size() < tmp.size() || (dp[i].size() == tmp.size() && dp[i] < tmp)) {
                     dp[i] = tmp;
                 }
