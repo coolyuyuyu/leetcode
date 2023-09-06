f2986e9e5fd03af658f341038585f4826563d952
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-06 23:41:15 +0800

    956: update solution

--- algorithm_0000-0999/956-Tallest Billboard/main.cpp
+++ algorithm_0000-0999/956-Tallest Billboard/main.cpp
@@ -4,27 +4,27 @@ public:
         int minDiff = -5000, maxDiff = 5000;
         int offset = 0 - minDiff;
 
-        // dp[diff]: the maximum support of left length such that "left length - right right = diff"
-        vector<int> dp(maxDiff - minDiff + 1, -1);
+        // dp[diff]: the maximum left support such that diff = left height - right height
+        int dp[maxDiff - minDiff + 1];
+        for (int diff = minDiff; diff <= maxDiff; ++diff) {
+            dp[diff + offset] = INT_MIN;
+        }
         dp[0 + offset] = 0;
 
-        for (int rod : rods) {
-            vector<int> tmp = dp;
-            for (int diff = minDiff; diff <= maxDiff; ++diff) {
-                if (tmp[diff + offset] == -1) {
-                    continue;
-                } 
 
-                if (diff + rod + offset < dp.size()) {
+        for (int rod : rods) {
+            int tmp[maxDiff - minDiff + 1];
+            memcpy(tmp, dp, sizeof(dp));
+            for (int diff = minDiff; diff <= maxDiff; ++diff) {
+                if (diff + rod <= maxDiff) {
                     dp[diff + rod + offset] = std::max(dp[diff + rod + offset], tmp[diff + offset] + rod);
                 }
-
-                if (0 <= diff - rod + offset) {
+                if (minDiff <= diff - rod) {
                     dp[diff - rod + offset] = std::max(dp[diff - rod + offset], tmp[diff + offset]);
                 }
             }
         }
-
+        
         return dp[0 + offset];
     }
 };
