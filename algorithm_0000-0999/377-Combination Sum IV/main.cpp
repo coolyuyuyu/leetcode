2e9fe575c6c6deae5b8ece01b911ec17690d9840
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-09 10:48:11 +0800

    377: update solution

--- algorithm_0000-0999/377-Combination Sum IV/main.cpp
+++ algorithm_0000-0999/377-Combination Sum IV/main.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
-    int greedy(const vector<int>& nums) {
-        // -1: minus, 0: 0, 1: plus, -2: initial default
-        int slope = -2;
-
-        int ret = 1;
-        for (int i = 1; i < nums.size(); ++i) {
-            int preSlope = slope;
-            if ((nums[i] - nums[i - 1]) < 0) {
-                slope = -1;
-            }
-            else if ((nums[i] - nums[i - 1]) == 0) {
-                slope = preSlope;
-            }
-            else {
-                slope = 1;
-            }
-
-            if (slope != preSlope) {
-                ++ret;
+    int combinationSum4(vector<int>& nums, int target) {
+        // dp[i]: the number of possible combinations that add up to target
+        vector<unsigned long long> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
             }
         }
 
-        return ret;
-    }
-
-    int dp(const vector<int>& nums) {
-        int up = 1; // up[i]: the length of the longest wiggle subsequence of nums[0:i] and last segment goes up
-        int dn = 1; // dn[i]: the length of the longest wiggle subsequence of nums[0:i] and last segment goes down
-        for (int i = 1; i < nums.size(); ++i) {
-            if ((nums[i] - nums[i - 1]) < 0) {
-                dn = up + 1;
-            }
-            else if (0 < (nums[i] - nums[i - 1])) {
-                up = dn + 1;
-            }
-        }
-
-        return std::max(up, dn);
-    }
-
-    int wiggleMaxLength(vector<int>& nums) {
-        //return greedy(nums);
-        return dp(nums);
+        return dp[target];
     }
 };
