658ed28d82785d11cd2feaa611861d8f80822c7e
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-17 21:10:24 +0800

    2858: update solution

--- algorithm_2000-2999/2858-Minimum Edge Reversals So Every Node Is Reachable/main.cpp
+++ algorithm_2000-2999/2858-Minimum Edge Reversals So Every Node Is Reachable/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+    vector<int> dfs1(int n, vector<vector<int>>& edges) {
         vector<vector<int>> fwdGraph(n), bwdGraph(n);
         for (const auto& edge : edges) {
             fwdGraph[edge[0]].push_back(edge[1]);
@@ -8,7 +8,7 @@ public:
         }
 
         map<pair<int, int>, int> cache;
-        std::function<int(int, int)> dfs = [&](int cur, int pre) {
+        std::function<int(int, int)> f = [&](int cur, int pre) {
             if (cache.find({cur, pre}) != cache.end()) {
                 return cache[{cur, pre}];
             }
@@ -16,13 +16,12 @@ public:
             int& ret = cache[{cur, pre}] = 0;
             for (int nxt : fwdGraph[cur]) {
                 if (nxt != pre) {
-                    ret += dfs(nxt, cur);
+                    ret += f(nxt, cur);
                 }
             }
-
             for (int nxt : bwdGraph[cur]) {
                 if (nxt != pre) {
-                    ret += dfs(nxt, cur) + 1;
+                    ret += f(nxt, cur) + 1;
                 }
             }
 
@@ -31,9 +30,59 @@ public:
 
         vector<int> ret(n);
         for (int i = 0; i < n; ++i) {
-            ret[i] = dfs(i, -1);
+            ret[i] = f(i, -1);
         }
 
         return ret;
+    }
+
+    // Re-root approach
+    vector<int> dfs2(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> fwdGraph(n), bwdGraph(n);
+        for (const auto& edge : edges) {
+            fwdGraph[edge[0]].push_back(edge[1]);
+            bwdGraph[edge[1]].push_back(edge[0]);
+        }
+
+        std::function<int(int, int)> f1 = [&](int cur, int pre) {
+            int ret = 0;
+            for (int nxt : fwdGraph[cur]) {
+                if (nxt != pre) {
+                    ret += f1(nxt, cur);
+                }
+            }
+            for (int nxt : bwdGraph[cur]) {
+                if (nxt != pre) {
+                    ret += f1(nxt, cur) + 1;
+                }
+            }
+
+            return ret;
+        };
+        int cnt = f1(0, -1);
+
+        vector<int> ret(n);
+        std::function<void(int, int, int)> f2 = [&](int cur, int pre, int cnt) {
+            ret[cur] = cnt;
+
+            for (int nxt : fwdGraph[cur]) {
+                if (nxt != pre) {
+                    f2(nxt, cur, cnt + 1);
+                }
+            }
+            for (int nxt : bwdGraph[cur]) {
+                if (nxt != pre) {
+                    f2(nxt, cur, cnt - 1);
+                }
+            }
+        };
+        f2(0, -1, cnt);
+
+        return ret;
+    }
+
+    vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+        //return dfs1(n, edges);
+        return dfs2(n, edges);
     }
 };
