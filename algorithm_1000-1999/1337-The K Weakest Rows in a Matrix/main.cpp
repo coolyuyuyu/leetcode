4a18d21e6bd3b85690d90cb940bacc24221efe31
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-19 03:30:48 +0800

    1337: update solution

--- algorithm_1000-1999/1337-The K Weakest Rows in a Matrix/main.cpp
+++ algorithm_1000-1999/1337-The K Weakest Rows in a Matrix/main.cpp
@@ -1,62 +1,43 @@
 class Solution {
 public:
-    vector<int> kWeakestRows_Sort(vector<vector<int>>& mat, int k) {
-        vector<pair<int, int>> pairs(mat.size()); // vector<count, index>
-        for (int index = 0; index < mat.size(); ++index) {
-            pairs[index] = {count(mat[index].begin(), mat[index].end(), 1), index};
+    vector<int> bySort(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat.empty() ? 0 : mat[0].size();
+
+        vector<pair<int, int>> pairs(m); // <cnt, idx>
+        for (int r = 0; r < m; ++r) {
+            pairs[r] = {std::distance(mat[r].begin(), std::find(mat[r].begin(), mat[r].end(), 0)), r};
         }
+        std::partial_sort(pairs.begin(), pairs.begin() + k, pairs.end());
 
-        auto comp = [](const pair<int, int>& p1, const pair<int, int>& p2) {
-            if (p1.first == p2.first) {
-                return (p1.second < p2.second);
-            }
-            else {
-                return (p1.first < p2.first);
-            }
-        };
-        partial_sort(pairs.begin(), pairs.begin() + k, pairs.end(), comp);
-
-        vector<int> indexes;
+        vector<int> ret(k);
         for (int i = 0; i < k; ++i) {
-            indexes.push_back(pairs[i].second);
+            ret[i] = pairs[i].second;
         }
 
-        return indexes;
+        return ret;
     }
 
-    vector<int> kWeakestRows_Heap(vector<vector<int>>& mat, int k) {
-        auto comp = [](const pair<int, int>& p1, const pair<int, int>& p2) {
-            if (p1.first == p2.first) {
-                return (p1.second < p2.second);
-            }
-            else {
-                return (p1.first < p2.first);
-            }
-        };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+    vector<int> byHeap(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat.empty() ? 0 : mat[0].size();
 
-        for (int index = 0; index < mat.size(); ++index) {
-            pair<int, int> p(count(mat[index].begin(), mat[index].end(), 1), index);
-            if (pq.size() < k) {
-                pq.push(p);
-            }
-            else if (comp(p, pq.top())) {
+        priority_queue<pair<int, int>> pq; // <cnt, idx>
+        for (int r = 0; r < m; ++r) {
+            pq.emplace((int)std::distance(mat[r].begin(), std::find(mat[r].begin(), mat[r].end(), 0)), r);
+            if (k < pq.size()) {
                 pq.pop();
-                pq.push(p);
             }
         }
 
-        vector<int> indexes(k);
-        for (int i = k - 1; 0 <= i; --i) {
-            indexes[i] = pq.top().second;
-            pq.pop();
+        vector<int> ret(k);
+        for (int i = k; 0 < i--; pq.pop()) {
+            ret[i] = pq.top().second;
         }
 
-        return indexes;
+        return ret;
     }
 
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        //return kWeakestRows_Sort(mat, k);
-        return kWeakestRows_Heap(mat, k);
+        //return bySort(mat, k);
+        return byHeap(mat, k);
     }
 };
