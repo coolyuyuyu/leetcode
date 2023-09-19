700a619965471ef0138cb137119b2a84cf03931d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-20 02:44:29 +0800

    1964: update solution

--- algorithm_1000-1999/1964-Find the Longest Valid Obstacle Course at Each Position/main.cpp
+++ algorithm_1000-1999/1964-Find the Longest Valid Obstacle Course at Each Position/main.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
-    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+    // Time: O(n^2)
+    vector<int> dp(vector<int>& obstacles) {
         int n = obstacles.size();
 
+        // dp[i]: length of the longest obstacle course from obstacles[0:i] and ending at i
+        vector<int> dp(n, 1);
         vector<int> ret(n);
-        vector<int> lis;
         for (int i = 0; i < n; ++i) {
-            auto itr = std::upper_bound(lis.begin(), lis.end(), obstacles[i]);
-            ret[i] = std::distance(lis.begin(), itr) + 1;
-            if (itr == lis.end()) {
-                lis.push_back(obstacles[i]);
+            for (int j = 0; j < i; ++j) {
+                if (obstacles[j] <= obstacles[i]) {
+                    dp[i] = std::max(dp[i], dp[j] + 1);
+                }
             }
-            else {
-                *itr = obstacles[i];
-            }
-        } 
+
+            ret[i] = dp[i];
+        }
 
         return ret;
+    }
+
+    // Time: O(nlogn)
+    vector<int> greedy(vector<int>& obstacles) {
+        int n = obstacles.size();
+
+        vector<int> lis; // longest increasing subsequence;
+        vector<int> ret(n);
+        for (int i = 0; i < n; ++i) {
+            if (lis.empty() || lis.back() <= obstacles[i]) {
+                lis.push_back(obstacles[i]);
+                ret[i] = lis.size();
+            }
+            else {
+                auto itr = std::upper_bound(lis.begin(), lis.end(), obstacles[i]);
+                *itr = obstacles[i];
+                ret[i] = std::distance(lis.begin(), itr) + 1;
+            }
+        }
+
+        return ret;
+    }
+
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+        //return dp(obstacles);
+        return greedy(obstacles);
     }
 };
