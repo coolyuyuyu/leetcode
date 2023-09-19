ded7fad37fd7ae932b634177876c1afe85f6cd81
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-19 23:02:26 +0800

    229: update solution

---
+++ algorithm_0000-0999/229-Majority Element II/main.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // Boyer-Moore Majority Voting
+        int num1 = INT_MIN, num2 = INT_MIN;
+        int cnt1 = 0, cnt2 = 0;
+        for (int num : nums) {
+            if (num == num1) {
+                ++cnt1;
+            }
+            else if (num == num2) {
+                ++cnt2;
+            }
+            else if (num1 != INT_MIN && num2 != INT_MIN) {
+                if (--cnt1 == 0) {
+                    num1 = INT_MIN;
+                }
+                if (--cnt2 == 0) {
+                    num2 = INT_MIN;
+                }
+            }
+            else if (num1 == INT_MIN) {
+                num1 = num;
+                cnt1 = 1;
+            }
+            else if (num2 == INT_MIN) {
+                num2 = num;
+                cnt2 = 1;
+            }
+            else {
+                assert(false);
+            }
+        }
+
+        cnt1 = 0, cnt2 = 0;
+        for (int num : nums) {
+            if (num == num1) {
+                ++cnt1;
+            }
+            if (num == num2) {
+                ++cnt2;
+            }
+        }
+
+        vector<int> ret;
+        if (cnt1 > nums.size() / 3) {
+            ret.push_back(num1);
+        }
+        if (cnt2 > nums.size() / 3) {
+            ret.push_back(num2);
+        }
+
+        return ret;
+    }
+};
