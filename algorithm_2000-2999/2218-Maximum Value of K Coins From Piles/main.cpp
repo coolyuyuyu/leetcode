78fe9df2861e9cfe6993ec7b996fd43082e5a799
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-20 02:44:55 +0800

    2218: update solution

--- algorithm_2000-2999/2218-Maximum Value of K Coins From Piles/main.cpp
+++ algorithm_2000-2999/2218-Maximum Value of K Coins From Piles/main.cpp
@@ -3,25 +3,32 @@ public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n = piles.size();
 
-        vector<vector<int>> presum(n);
-        for (int i = 0; i < n; ++i) {
-            presum[i].push_back(0);
-            for (int j = 0, sum = 0; j < piles[i].size(); ++j) {
-                sum += piles[i][j];
-                presum[i].push_back(sum);
-            }
+        piles.insert(piles.begin(), vector<int>());
+
+        int presum[n + 1][k + 1];
+        for (int i = 1; i <= n; ++i) {
+            presum[i][0] = 0;
+            std::partial_sum(piles[i].begin(), piles[i].begin() + std::min<int>(k, piles[i].size()), presum[i] + 1);
         }
 
-        //dp[i][j]: the maximum total value of j coins from piles[0:i]
-        vector<vector<int>> dp(n, vector<int>(k + 1));
-        for (int i = 0; i < n; ++i) {
+        // dp[i][j]: the maximum total value of j coins from piles[0:i]
+        int dp[n + 1][k + 1];
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                dp[i][j] = INT_MIN;
+            }
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = 0;
+        }
+        for (int i = 1; i <= n; ++i) {
             for (int j = 0; j <= k; ++j) {
                 for (int t = 0; t <= std::min<int>(j, piles[i].size()); ++t) {
-                    dp[i][j] = std::max(dp[i][j], (i == 0 ? 0 : dp[i - 1][j - t]) + presum[i][t]);
+                    dp[i][j] = std::max(dp[i][j], dp[i - 1][j - t] + presum[i][t]);
                 }
             }
         }
-
-        return dp[n - 1][k];
+        return dp[n][k];
     }
 };
