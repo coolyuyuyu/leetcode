1c6edeba3c4e4c5084c78a705ccb0d281c6f0368
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-19 23:02:35 +0800

    2856: update solution

--- algorithm_2000-2999/2856-Minimum Array Length After Pair Removals/main.cpp
+++ algorithm_2000-2999/2856-Minimum Array Length After Pair Removals/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int minLengthAfterRemovals(vector<int>& nums) {
+    int f1(vector<int>& nums) {
         unordered_map<int, int> cnts;
         for (int num : nums) {
             ++cnts[num];
@@ -32,5 +32,30 @@ public:
         }
 
         return ret;
+    }
+
+    int f2(vector<int>& nums) {
+        unordered_map<int, int> cnts;
+        for (int num : nums) {
+            ++cnts[num];
+        }
+
+        int maxCnt = 0;
+        for (const auto& [_, cnt] : cnts) {
+            maxCnt = std::max(maxCnt, cnt);
+        }
+
+        int n = nums.size();
+        if (maxCnt > n / 2) {
+            return n - (n - maxCnt) * 2;
+        }
+        else {
+            return n & 1;
+        }
+    }
+
+    int minLengthAfterRemovals(vector<int>& nums) {
+        //return f1(nums);
+        return f2(nums);
     }
 };
