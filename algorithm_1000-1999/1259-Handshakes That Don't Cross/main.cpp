af7b59c2a29150fbcfac6c473dc2e1f62e167a68
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-22 00:35:17 +0800

    1259: update solution

--- algorithm_1000-1999/1259-Handshakes That Don't Cross/main.cpp
+++ algorithm_1000-1999/1259-Handshakes That Don't Cross/main.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int M = 1e9 + 7;
 
-    int numberOfWays(int numPeople, vector<int>& cache) {
-        if (numPeople % 2) {
-            return 0;
-        }
-        if (numPeople == 0) {
-            return 1;
-        }
-        if (cache[numPeople] != -1) {
-            return cache[numPeople];
-        }
-
-        int ret = 0;
-        for (int i = 0; (i + 2) <= numPeople; i += 2) {
-            ret = (ret + (long long)numberOfWays(i, cache) * numberOfWays(numPeople - i - 2, cache)) % M;
-        }
-
-        return cache[numPeople] = ret;
-    }
-
     int numberOfWays(int numPeople) {
-        vector<int> cache(numPeople + 1, -1);
-        return numberOfWays(numPeople, cache);
+        int cache[numPeople + 1];
+        cache[0] = 1;
+        for (int i = 1; i <= numPeople; ++i) {
+            cache[i] = -1;
+        }
+        std::function<int(int)> f = [&](int n) {
+            int& ret = cache[n];
+            if (0 <= ret) {
+                return ret;
+            }
+
+            ret = 0;
+            for (int i = 0; i + 2 <= n; i += 2) {
+                ret += (long long)f(i) * f(n - i - 2) % M;
+                ret %= M;
+            }
+
+            return ret;
+        };
+
+        return f(numPeople);
     }
 };
