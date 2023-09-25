e9e6eff2e648226d53ba613815bee344051e746c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-25 10:41:28 +0800

    1259: update solution

--- algorithm_1000-1999/1259-Handshakes That Don't Cross/main.cpp
+++ algorithm_1000-1999/1259-Handshakes That Don't Cross/main.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int M = 1e9 + 7;
 
-    int numberOfWays(int numPeople) {
+    int topdn(int numPeople) {
         int cache[numPeople + 1];
         cache[0] = 1;
-        for (int i = 1; i <= numPeople; ++i) {
+        for (int i = 2; i <= numPeople; i += 2) {
             cache[i] = -1;
         }
         std::function<int(int)> f = [&](int n) {
@@ -24,5 +24,26 @@ public:
         };
 
         return f(numPeople);
+    }
+
+    int btmup(int numPeople) {
+        // dp[i]: the number of ways these handshakes could occur for i people
+        int dp[numPeople + 1];
+        dp[0] = 1;
+
+        for (int i = 2; i <= numPeople; i += 2) {
+            dp[i] = 0;
+            for (int j = 0; j + 2 <= i; j += 2) {
+                dp[i] += ((long long)dp[j] * dp[i - j - 2]) % M;
+                dp[i] %= M;
+            }
+        }
+
+        return dp[numPeople];
+    }
+
+    int numberOfWays(int numPeople) {
+        return topdn(numPeople);
+        //return btmup(numPeople);
     }
 };
