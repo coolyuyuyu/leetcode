e7c2a5ddc10c70b3bf1d08871301ebb243c9bb76
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-26 17:38:28 +0800

    2850: update solution

--- algorithm_2000-2999/2850-Minimum Moves to Spread Stones Over Grid/main.cpp
+++ algorithm_2000-2999/2850-Minimum Moves to Spread Stones Over Grid/main.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int minimumMoves(vector<vector<int>>& grid) {
-        std::function<int()> dfs = [&]() {
-            int ret = INT_MAX;
-            for (int r1 = 0; r1 < 3; ++r1) {
-                for (int c1 = 0; c1 < 3; ++c1) {
-                    if (grid[r1][c1] == 0) {
-                        for (int r2 = 0; r2 < 3; ++r2) {
-                            for (int c2 = 0; c2 < 3; ++c2) {
-                                if (grid[r2][c2] > 1) {
-                                    ++grid[r1][c1];
-                                    --grid[r2][c2];
-                                    ret = std::min(ret, dfs() + abs(r2 - r1) + abs(c2 - c1));
-                                    ++grid[r2][c2];
-                                    --grid[r1][c1];
-                                }
-                            }
+        int ret = INT_MAX;
+        std::function<void(int, int)> dfs = [&](int pos, int move) {
+            if (move >= ret) { return; }
+            if (pos >= 9) {
+                ret = std::min(ret, move);
+                return;
+            }
+
+            int r = pos / 3, c = pos % 3;
+            if (grid[r][c] == 0) {
+                for (int x = 0; x < 3; ++x) {
+                    for (int y = 0; y < 3; ++y) {
+                        if (grid[x][y] > 1) {
+                            --grid[x][y];
+                            ++grid[r][c];
+
+                            dfs(pos + 1, move + abs(x - r) + abs(y - c));
+                            --grid[r][c];
+                            ++grid[x][y];
                         }
                     }
                 }
             }
-
-            return ret == INT_MAX ? 0 : ret;
+            else if (grid[r][c] >= 1) {
+                dfs(pos + 1, move);
+            }
         };
+        dfs(0, 0);
 
-        return dfs();
+        return ret;
     }
 };
