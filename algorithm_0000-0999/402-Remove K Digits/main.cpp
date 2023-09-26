3785efcb68f872262400108f69bf639cbabcd079
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-27 02:33:17 +0800

    402: update solution

--- algorithm_0000-0999/402-Remove K Digits/main.cpp
+++ algorithm_0000-0999/402-Remove K Digits/main.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        deque<char> stk;
-        for (size_t i = 0; i < num.size(); ++i) {
-            while (!stk.empty() && num[i] < stk.back() && i < (stk.size() + k)) {
+        string stk;
+        for (char c : num) {
+            while (!stk.empty() && stk.back() > c && 0 < k--) {
                 stk.pop_back();
             }
-
-            if (num.size() <= (stk.size() + k)) {
-            }
-            else {
-                stk.push_back(num[i]);
-            }
+            stk.push_back(c);
         }
-        assert(num.size() == (stk.size() + k));
-
-        while (!stk.empty() && stk.front() == '0') {
-            stk.pop_front();
+        while (0 < k--) {
+            stk.pop_back();
         }
-        return (stk.begin() == stk.end() ? "0" : string(stk.begin(), stk.end()));
+
+        auto pos = stk.find_first_not_of('0');
+        if (pos != string::npos) {
+            stk = stk.substr(pos);
+        }
+        else {
+            stk.clear();
+        }
+
+        return stk.empty() ? "0" : stk;
     }
 };
