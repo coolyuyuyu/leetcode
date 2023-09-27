2fa2be358b22dd03a0de3434f05e332f298c3d55
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-28 01:59:53 +0800

    920: update solution

--- algorithm_0000-0999/920-Number of Music Playlists/main.cpp
+++ algorithm_0000-0999/920-Number of Music Playlists/main.cpp
@@ -3,13 +3,27 @@ public:
     int M = 1e9 + 7;
 
     int numMusicPlaylists(int n, int goal, int k) {
-        // dp[i][j]: the number of playlists of length i using j different songs
-        vector<vector<long>> dp(goal + 1, vector<long>(n + 1));
-        dp[1][1] = n;
-        for (int i = 2; i <= goal; ++i) {
-            for (int j = 1; j <= std::min(n, i); ++j) {
-                dp[i][j] = dp[i - 1][j] * std::max(j - k, 0) + dp[i - 1][j - 1] * (n - (j - 1));
-                dp[i][j] %= M;
+        // dp[i][j]: the number of playlists of length i using exactly j different songs
+        long dp[goal + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= goal; ++i) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= goal; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                dp[i][j] = 0;
+                if (j > k) {
+                    dp[i][j] += dp[i - 1][j] * (j - k);
+                    dp[i][j] %= M;
+                }
+                if (n > (j - 1)) {
+                    dp[i][j] += dp[i - 1][j - 1] * (n - (j - 1));
+                    dp[i][j] %= M;
+                }
+
             }
         }
 
