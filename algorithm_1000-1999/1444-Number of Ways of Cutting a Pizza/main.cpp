b75a61682c8faa5fdcaa265186a129857888eeeb
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-29 03:59:48 +0800

    1444: update solution

--- algorithm_1000-1999/1444-Number of Ways of Cutting a Pizza/main.cpp
+++ algorithm_1000-1999/1444-Number of Ways of Cutting a Pizza/main.cpp
@@ -3,52 +3,59 @@ public:
     int M = 1e9 + 7;
 
     int ways(vector<string>& pizza, int k) {
-        size_t m = pizza.size(), n = pizza.empty() ? 0 : pizza[0].size();
+        int m = pizza.size(), n = pizza.empty() ? 0 : pizza[0].size();
 
-        vector<vector<int>> sum(m, vector<int>(n, 0));
-        sum[0][0] = (pizza[0][0] == 'A' ? 1 : 0);
-        for (int r = 1; r < m; ++r) {
-            sum[r][0] = sum[r - 1][0] + (pizza[r][0] == 'A' ? 1 : 0);
-        }
-        for (int c = 1; c < n; ++c) {
-            sum[0][c] = sum[0][c - 1] + (pizza[0][c] == 'A' ? 1 : 0);
-        }
-        for (int r = 1; r < m; ++r) {
-            for (int c = 1; c < n; ++c) {
-                sum[r][c] = (pizza[r][c] == 'A' ? 1 : 0) + sum[r - 1][c] + sum[r][c - 1] - sum[r-1][c-1];
+        int presum[m][n];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                presum[r][c] = (pizza[r][c] == 'A' ? 1 : 0);
+                presum[r][c] += (0 < r ? presum[r - 1][c] : 0);
+                presum[r][c] += (0 < c ? presum[r][c - 1] : 0 );
+                presum[r][c] -= (0 < r && 0 < c ? presum[r - 1][c - 1] : 0);
             }
         }
-        std::function<bool(int, int, int, int)> hasApples = [&](int r1, int c1, int r2, int c2) -> bool {
-            return (0 < (sum[r2][c2] - (0 < r1 ? sum[r1 - 1][c2] : 0) - (0 < c1 ? sum[r2][c1 - 1] : 0) + ((0 < r1 && 0 < c1 )? sum[r1 - 1][c1 - 1] : 0)));
+        std::function<bool(int, int, int, int)> containApples = [&](int r1, int c1, int r2, int c2) {
+            int cnt = presum[r2][c2] - (0 < r1 ? presum[r1 - 1][c2] : 0) - (0  < c1 ? presum[r2][c1 - 1] : 0) + (0 < r1 && 0 < c1 ? presum[r1 - 1][c1 - 1] : 0);
+            return 0 < cnt;
         };
 
-        map<tuple<int, int, int>, int> cache;
-
-        // dp(r, c, remaining): number of ways of cutting the pizza[r:m][c:n] with remaining cut
-        std::function<int(int, int, int)> dp = [&](int r, int c, int remaining) -> int {
-            if (remaining == 0) {
-                return hasApples(r, c, m - 1, n - 1) ? 1 : 0;
-            }
-
-            if (cache.find({r, c, remaining}) != cache.end()) {
-                return cache[{r, c, remaining}];
-            }
-
-            int& ret = cache[{r, c, remaining}];
-            for (int i = r + 1; i < m; ++i) { // horizontal
-                if (hasApples(r, c, i - 1, n - 1)) {
-                    ret = (ret + dp(i, c, remaining - 1)) % M;
+        // dp[r][c][remaining]: the number of ways of cutting the pizza[r:m-1][c:n-1] with remaining cut
+        int dp[m][n][k];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                for (int remaining = 0; remaining < k; ++remaining) {
+                    dp[r][c][remaining] = -1;
                 }
             }
-            for (int j = c + 1; j < n; ++j) { // vertical
-                if (hasApples(r, c, m - 1, j - 1)) {
-                    ret = (ret + dp(r, j, remaining - 1)) % M;
+        }
+        std::function<int(int, int, int)> countWays = [&](int r, int c, int remaining) {
+            int& ret = dp[r][c][remaining];
+            if (0 <= ret) {
+                return ret;
+            }
+
+            if (remaining == 0) {
+                ret = containApples(r, c, m - 1, n - 1) ? 1 : 0;
+                return ret;
+            }
+
+            ret = 0;
+            for (int i = r; (i + 1) < m; ++i) { // horizontal
+                if (containApples(r, c, i, n - 1)) {
+                    ret += countWays(i + 1, c, remaining - 1);
+                    ret %= M;
+                }
+            }
+            for (int j = c; (j + 1) < n; ++j) { // vertical
+                if (containApples(r, c, m - 1, j)) {
+                    ret += countWays(r, j + 1, remaining - 1);
+                    ret %= M;
                 }
             }
 
             return ret;
         };
 
-        return dp(0, 0, k - 1);
+        return countWays(0, 0, k - 1);
     }
 };
