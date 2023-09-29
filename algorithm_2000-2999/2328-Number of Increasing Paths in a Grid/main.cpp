12320fc32399d3665a1fd2621b707fc08762a137
coolyuyuyu <coolyuyuyu@gmail.com>
2023-09-30 01:43:27 +0800

    2328: update solution

--- algorithm_2000-2999/2328-Number of Increasing Paths in a Grid/main.cpp
+++ algorithm_2000-2999/2328-Number of Increasing Paths in a Grid/main.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int M = 1e9 + 7;
-
-    const vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-
+    pair<int, int> dirs[4] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     int btmupDP(const vector<vector<int>>& grid) {
         int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
-        vector<tuple<int, int, int>> arr;
-        for (int r = 0; r < m; ++r) {
+        pair<int, int> pnts[m * n];
+        for (int r = 0, i = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
-                arr.emplace_back(r, c, grid[r][c]);
+                pnts[i++] = {r, c};
             }
         }
-        std::sort(arr.begin(), arr.end(), [](const auto& t1, const auto& t2) { return std::get<2>(t1) < std::get<2>(t2); });
+        std::sort(pnts, pnts + m * n, [&](const auto& pnt1, const auto& pnt2) { return grid[pnt1.first][pnt1.second] < grid[pnt2.first][pnt2.second]; });
 
-        // dp[r][c]: the number of strictly increasing paths ending at matrix (r, c)
-        vector<vector<int>> dp(m, vector<int>(n));
+        // dp[r][c]: the number of strictly increasing paths ending at (r,c)
+        int dp[m][n];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                dp[r][c] = 1;
+            }
+        }
 
         int ret = 0;
-        for (const auto& [r, c, v] : arr) {
-            dp[r][c] = 1;
+        for (const auto& [r, c] : pnts) {
             for (const auto& [dr, dc] : dirs) {
                 int x = r + dr, y = c + dc;
-                if (x < 0 || m <= x || y < 0 || n <= y) {
-                    continue;
-                }
-                if (grid[r][c] <= grid[x][y]) {
-                    continue;
-                }
-                dp[r][c] += dp[x][y];
-                dp[r][c] %= M;
+                if (x < 0 || m <= x || y < 0 || n <= y) { continue; }
+                if (grid[r][c] >= grid[x][y]) { continue; }
+                dp[x][y] += dp[r][c];
+                dp[x][y] %= M;
             }
-
             ret += dp[r][c];
             ret %= M;
         }
@@ -43,24 +40,24 @@ public:
     int topdnDFS(const vector<vector<int>>& grid) {
         int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
-        // dp[r][c]: the number of strictly increasing paths ending at matrix (r, c)
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // dp[r][c]: the number of strictly increasing paths ending at (r,c)
+        int dp[m][n];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                dp[r][c] = 0;
+            }
+        }
         std::function<int(int, int)> f = [&](int r, int c) {
-            if (0 < dp[r][c]) {
-                return dp[r][c];
+            int& ret = dp[r][c];
+            if (1 <= ret) {
+                return ret;
             }
 
-            int& ret = dp[r][c];
             ret = 1;
             for (const auto& [dr, dc] : dirs) {
                 int x = r + dr, y = c + dc;
-                if (x < 0 || m <= x || y < 0 || n <= y) {
-                    continue;
-                }
-                if (grid[r][c] <= grid[x][y]) {
-                    continue;
-                }
-
+                if (x < 0 || m <= x || y < 0 || n <= y) { continue; }
+                if (grid[r][c] >= grid[x][y]) { continue; }
                 ret += f(x, y);
                 ret %= M;
             }
@@ -69,7 +66,7 @@ public:
         };
 
         int ret = 0;
-        for (int r = 0; r < m; ++r) {
+        for (int r = 0, i = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
                 ret += f(r, c);
                 ret %= M;
