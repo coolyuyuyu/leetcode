a8847371279ffd97aeb07ef7d428df2c61c47273
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-01 01:06:26 +0800

    2870: initial solution

---
+++ algorithm_2000-2999/2870-Minimum Number of Operations to Make Array Empty/main.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> cnts;
+        for (int num : nums) {
+            ++cnts[num];
+        }
+
+        int ret = 0;
+        for (auto [_, cnt] : cnts) {
+            if (cnt == 1) {
+                return -1;
+            }
+
+            if (cnt >= 3 && cnt & 1) {
+                cnt -= 3;
+                ++ret;
+            }
+
+            ret += (cnt / 6) * 2;
+            cnt -= (cnt / 6) * 6;
+
+            ret += cnt / 2;
+        }
+
+        return ret;
+    }
+};
