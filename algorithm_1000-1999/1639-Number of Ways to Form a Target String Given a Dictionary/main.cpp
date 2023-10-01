7784b8223387c68d77720c456f22affa2f6a9e4f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-02 02:07:50 +0800

    1639: update solution

--- algorithm_1000-1999/1639-Number of Ways to Form a Target String Given a Dictionary/main.cpp
+++ algorithm_1000-1999/1639-Number of Ways to Form a Target String Given a Dictionary/main.cpp
@@ -3,30 +3,31 @@ public:
     int M = 1e9 + 7;
 
     int numWays(vector<string>& words, string target) {
-        int m = target.size();
-        int n = words.empty() ? 0 : words[0].size();
+        int m = target.size(), n = words.empty() ? 0 : words[0].size();
 
-        target.insert(target.begin(), '#');
+        target = "#" + target;
 
-        vector<array<int, 26>> count(n + 1);
+        vector<array<int, 26>> cnt(n + 1);
         for (const string& word : words) {
             for (int j = 0; j < n; ++j) {
-                ++count[j + 1][word[j] - 'a'];
+                ++cnt[j + 1][word[j] - 'a'];
             }
         }
 
-        // dp[i][j]: the number of ways to form target[0:i] from all words[0:j]
-        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1));
-        for (int j = 0; j <= n; ++j) {
+        // the number of ways to form target[0:i-1] from all j-char-long prefix of words.
+        long long dp[m + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= n; ++j) {
             dp[0][j] = 1;
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 dp[i][j] = dp[i][j - 1];
-                if (0 < count[j][target[i] - 'a']) {
-                    dp[i][j] += (dp[i - 1][j - 1] * count[j][target[i] - 'a']);
-                    dp[i][j] %= M;
-                }
+                dp[i][j] += dp[i - 1][j - 1] * cnt[j][target[i] - 'a'];
+                dp[i][j] %= M;
             }
         }
 
