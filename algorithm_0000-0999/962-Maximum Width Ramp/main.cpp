6e77daa60c0d4936cb170e6abc03259004cb4124
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-03 20:26:36 +0800

    962: initial solution

---
+++ algorithm_0000-0999/962-Maximum Width Ramp/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // Time: O(n)
+    int maxWidthRamp(vector<int>& nums) {
+        // monotonic stack
+        // farthest greater or equal element
+
+        stack<int> stk;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (stk.empty() || nums[stk.top()] >= nums[i]) {
+                stk.push(i);
+            }
+        }
+
+        int ret = 0;
+        for (int j = nums.size(); 0 < j--;) {
+            while (!stk.empty() && nums[j] >= nums[stk.top()]) {
+                ret = std::max(ret, j - stk.top());
+                stk.pop();
+            }
+        }
+
+        return ret;
+    }
+};
