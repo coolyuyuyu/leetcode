81746e03f131e46189acb4155d0450da803f9f1b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-04 02:38:30 +0800

    1575: update solution

--- algorithm_1000-1999/1575-Count All Possible Routes/main.cpp
+++ algorithm_1000-1999/1575-Count All Possible Routes/main.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int M = 1e9 + 7;
 
+    // Time: O(fuel * n * n)
     int dp1(const vector<int>& locations, int start, int finish, int fuel) {
         int n = locations.size();
 
@@ -36,11 +37,12 @@ public:
         return ret;
     }
 
+    // Time: O(nlogn + fuel * n )
     int dp2(vector<int>& locations, int start, int finish, int fuel) {
         int n = locations.size();
 
         int startLoc = locations[start], finishLoc = locations[finish];
-        std::sort(locations.begin(), locations.end());
+        sort(locations.begin(), locations.end());
         for (int i = 0; i < n; ++i) {
             if (locations[i] == startLoc) {
                 start = i;
@@ -50,21 +52,27 @@ public:
             }
         }
 
-        // dp[f][i][0]: the count of all possible routes stopping at ith city with remaining fuel f
-        // dp[f][i][1]: the count of all possible routes moving left and passing ith city with remaining fuel f
-        // dp[f][i][2]: the count of all possible routes moving right and passing ith city with remaining fuel f
-        vector<vector<vector<int>>> dp(fuel + 1, vector<vector<int>>(n, vector<int>(3, 0)));
+        // dp[f][i][0]: the count of all possible routes stpping at ith city
+        // dp[f][i][1]: the count of all possible routes moving left and passing ith city
+        // dp[f][i][2]: the count of all possible routes moving right and  passing ith city
+        int dp[fuel + 1][n][3];
+        for (int f = 0; f <= fuel; ++f) {
+            for (int i = 0; i < n; ++i) {
+                dp[f][i][0] = dp[f][i][1] = dp[f][i][2] = 0;
+            }
+        }
         dp[fuel][start][0] = 1;
         for (int f = fuel; 0 <= f; --f) {
             for (int i = 0; i < n; ++i) {
-                if (0 < i && 0 <= f - (locations[i] - locations[i - 1])) {
+                if (0 < i && (locations[i] - locations[i - 1]) <= f) { // move from left to right
                     int cost = locations[i] - locations[i - 1];
                     dp[f - cost][i][0] += (dp[f][i - 1][0] + dp[f][i - 1][2]) % M;
                     dp[f - cost][i][0] %= M;
                     dp[f - cost][i][2] += (dp[f][i - 1][0] + dp[f][i - 1][2]) % M;
                     dp[f - cost][i][2] %= M;
                 }
-                if ((i + 1) < n && 0 <= f - (locations[i + 1] - locations[i])) {
+
+                if ((i + 1) < n && (locations[i + 1] - locations[i]) <= f) { // moveing from left to right
                     int cost = locations[i + 1] - locations[i];
                     dp[f - cost][i][0] += (dp[f][i + 1][0] + dp[f][i + 1][1]) % M;
                     dp[f - cost][i][0] %= M;
@@ -75,7 +83,7 @@ public:
         }
 
         int ret = 0;
-        for (int f = fuel; 0 <= f; --f) {
+        for (int f = 0; f <= fuel; ++f) {
             ret += dp[f][finish][0];
             ret %= M;
         }
@@ -84,7 +92,7 @@ public:
     }
 
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        return dp1(locations, start, finish, fuel);
-        //return dp2(locations, start, finish, fuel);
+        //return dp1(locations, start, finish, fuel);
+        return dp2(locations, start, finish, fuel);
     }
 };
