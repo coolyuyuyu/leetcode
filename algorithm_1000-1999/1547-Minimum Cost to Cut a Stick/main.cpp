fb821b2415dcb8df55a345d3270c633832b3ff97
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-04 02:38:07 +0800

    1547: update solution

--- algorithm_1000-1999/1547-Minimum Cost to Cut a Stick/main.cpp
+++ algorithm_1000-1999/1547-Minimum Cost to Cut a Stick/main.cpp
@@ -4,19 +4,20 @@ public:
         std::sort(cuts.begin(), cuts.end());
         cuts.insert(cuts.begin(), 0);
         cuts.insert(cuts.end(), n);
-
         int m = cuts.size();
 
-        vector<vector<int>> dp(m + 1, vector<int>(m + 1, INT_MAX)); // the total cost of of the cuts for the wood stick of cuts[i] -> cuts[j]
-        for (int i = 0; (i + 1) < m; ++i) {
+        // dp[i][j]: the minimum total cost of the cuts for stick of ith cut position to jth cut position
+        int dp[m + 1][m + 1];
+        for (int i = 0; i < m; ++i) { // len = 2;
             dp[i][i + 1] = 0;
         }
         for (int len = 3; len <= m; ++len) {
-            for (int i = 0; (i + len - 1) < m; ++i) {
-                int j = i + len - 1;
+            for (int i = 0, j = i + len - 1; j < m; ++i, ++j) {
+                dp[i][j] = INT_MAX;
                 for (int k = i + 1; k < j; ++k) {
-                    dp[i][j] = std::min(dp[i][j], cuts[j] - cuts[i] + dp[i][k] + dp[k][j]);
+                    dp[i][j] = std::min(dp[i][j], dp[i][k] + dp[k][j]);;
                 }
+                dp[i][j] += (cuts[j] - cuts[i]);
             }
         }
 
