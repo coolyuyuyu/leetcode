1bfa078f4a67daeb6d5de5d556641c8ad8e08ed6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-04 17:35:43 +0800

    706: update solution

--- algorithm_0000-0999/706-Design HashMap/main.cpp
+++ algorithm_0000-0999/706-Design HashMap/main.cpp
@@ -1,131 +1,78 @@
 class MyHashMap {
 public:
-    /** Initialize your data structure here. */
-    MyHashMap(int capacity = 101)
+    MyHashMap(size_t n = 101)
         : m_size(0)
-        , m_capacity(capacity) {
-        m_buckets = new ListNode*[m_capacity];
-        memset(m_buckets, 0, sizeof(ListNode*) * m_capacity);
+        , m_buckets(n) {
     }
 
-    ~MyHashMap() {
-        for (size_t i = 0; i < m_capacity; ++i) {
-            ListNode* pNode = m_buckets[i];
-            while (pNode) {
-                ListNode* pDel = pNode;
-                pNode = pNode->pNext;
-                delete pDel;
-            }
-        }
-
-        delete[] m_buckets;
-    }
-
-    /** value will always be non-negative. */
-    void put(int key, int val) {
-        size_t index = hash(key);
-        for (ListNode* pNode = m_buckets[index]; pNode; pNode = pNode->pNext) {
-            if (pNode->key == key) {
-                pNode->val = val;
+    void put(int key, int value) {
+        for (auto& [k, v] : m_buckets[hash(key)]) {
+            if (k == key) {
+                v = value;
                 return;
             }
         }
 
-        if ((double)m_size / m_capacity >= s_threshold) {
-            rehash((double)m_capacity * s_growFactor);
+        if ((m_size + 1) / m_buckets.size() >= s_threshold) {
+            rehash(m_size * s_growfactor);
         }
 
-        ListNode *pNode = new ListNode(key, val);
-        index = hash(key);
-        ListNode** ppHead = &(m_buckets[index]);
-        pNode->pNext = *ppHead;
-        *ppHead = pNode;
-
+        m_buckets[hash(key)].emplace_front(key, value);
         ++m_size;
     }
 
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        size_t index = hash(key);
-        for (ListNode* pNode = m_buckets[index]; pNode; pNode = pNode->pNext) {
-            if (pNode->key == key) {
-                return pNode->val;
+        for (auto& [k, v] : m_buckets[hash(key)]) {
+            if (k == key) {
+                return v;
             }
         }
 
         return -1;
     }
 
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        size_t index = hash(key);
-        ListNode** ppNode = &(m_buckets[index]);
-
-        while (*ppNode) {
-            if ((*ppNode)->key == key) {
-                *ppNode = (*ppNode)->pNext;
+        auto& bucket = m_buckets[hash(key)];
+        for (auto pre = bucket.before_begin(), cur = bucket.begin(); cur != bucket.end(); pre = cur, ++cur) {
+            if (cur->first == key) {
+                bucket.erase_after(pre);
                 --m_size;
-
                 break;
             }
-            ppNode = &((*ppNode)->pNext);
         }
     }
 
 private:
-    class ListNode {
-    public:
-        ListNode(int key_, int val_)
-            : key(key_)
-            , val(val_)
-            , pNext(nullptr) {
-        }
-        int key;
-        int val;
-        ListNode* pNext;
-    };
+    static const double s_threshold;
+    static const double s_growfactor;
 
     size_t hash(int key) {
-        return key % m_capacity;
+        return key % m_buckets.size();
     }
 
-    void rehash(size_t capacity) {
-        ListNode** buckets = m_buckets;
-        swap(m_capacity, capacity);
+    void rehash(size_t n) {
+        vector<forward_list<pair<const int, int>>> buckets = std::move(m_buckets);
 
-        m_buckets = new ListNode*[m_capacity];
-        memset(m_buckets, 0, sizeof(ListNode*) * m_capacity);
-
-        for (size_t i = 0; i < capacity; ++i) {
-            ListNode* pNode = buckets[i];
-            while (pNode) {
-                ListNode* pMove = pNode;
-                pNode = pNode->pNext;
-
-                size_t index = hash(pMove->key);
-                ListNode** ppHead = &(m_buckets[index]);
-                pMove->pNext = *ppHead;
-                *ppHead = pMove;
+        m_buckets.resize(n);
+        for (const auto& bucket : buckets) {
+            for (const auto& [k, v] : bucket) {
+                size_t h = hash(k);
+                m_buckets[h].emplace_front(k, v);
             }
         }
-        delete[] buckets;
     }
 
-    static const double s_growFactor;
-    static const double s_threshold;
-
     size_t m_size;
-    size_t m_capacity;
-    ListNode** m_buckets;
+    vector<forward_list<pair<const int, int>>> m_buckets;
 };
 
-const double MyHashMap::s_growFactor = 1.5f;
-const double MyHashMap::s_threshold = 0.75f;
+const double MyHashMap::s_threshold = 1.5f;
+const double MyHashMap::s_growfactor = 0.75f;
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
  */
