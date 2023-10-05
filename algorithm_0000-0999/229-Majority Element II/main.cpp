71ff25bd0ddbf8241e6f6fb487a33bb921b5cbbe
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-05 23:05:50 +0800

    229: update solution

--- algorithm_0000-0999/229-Majority Element II/main.cpp
+++ algorithm_0000-0999/229-Majority Element II/main.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         // Boyer-Moore Majority Voting
+
+        // At most 2 elements that appears more than (n / 3) times
         int num1 = INT_MIN, num2 = INT_MIN;
         int cnt1 = 0, cnt2 = 0;
         for (int num : nums) {
@@ -27,9 +29,6 @@ public:
                 num2 = num;
                 cnt2 = 1;
             }
-            else {
-                assert(false);
-            }
         }
 
         cnt1 = 0, cnt2 = 0;
@@ -37,16 +36,16 @@ public:
             if (num == num1) {
                 ++cnt1;
             }
-            if (num == num2) {
+            else if (num == num2) {
                 ++cnt2;
             }
         }
 
         vector<int> ret;
-        if (cnt1 > nums.size() / 3) {
+        if (cnt1 > (nums.size() / 3)) {
             ret.push_back(num1);
         }
-        if (cnt2 > nums.size() / 3) {
+        if (cnt2 > (nums.size() / 3)) {
             ret.push_back(num2);
         }
 
