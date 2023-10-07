475880363a6b2d728e234d7b273cd6595e3fc365
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-08 01:38:21 +0800

    1108: initial solution

---
+++ algorithm_1000-1999/1108-Defanging an IP Address/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ret;
+        for (char c : address) {
+            if (c == '.') {
+                ret += "[.]";
+            }
+            else {
+                ret += c;
+            }
+        }
+
+        return ret;
+    }
+};
