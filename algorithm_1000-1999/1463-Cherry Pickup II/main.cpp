079a466d0d3a92ef1ab963a3a5d07047711b22e7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-07 23:35:20 +0800

    1463: initial solution

---
+++ algorithm_1000-1999/1463-Cherry Pickup II/main.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
+
+        // dp[r][i][j]: the maximum number of cherries collection which two robots reach (r, i) and (r, j)
+        int dp[n][n];
+        int tmp[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                dp[i][j] = INT_MIN / 2;
+            }
+        }
+        dp[0][n - 1] = grid[0][0] + grid[0][n - 1];
+        for (int r = 1; r < m; ++r) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = i; j < n; ++j) {
+                    tmp[i][j] = dp[i][j];
+                    dp[i][j] = INT_MIN / 2;
+                }
+            }
+
+            for (int i = 0; i < n; ++i) {
+                for (int j = i; j < n; ++j) {
+                    for (int a = i - 1; a <= i + 1; ++a) {
+                        for (int b = j - 1; b <= j + 1; ++b) {
+                            if (a < 0 || n <= a || b < 0 || n <= b) { continue; }
+
+                            auto [x, y] = std::minmax(a, b);
+                            if (i == j) {
+                                dp[i][j] = std::max(dp[i][j], tmp[x][y] + grid[r][i]);
+                            }
+                            else {
+                                dp[i][j] = std::max(dp[i][j], tmp[x][y] + grid[r][i] + grid[r][j]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int ret = INT_MIN;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                ret = std::max(ret, dp[i][j]);
+            }
+        }
+
+        return ret;
+    }
+};
