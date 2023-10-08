165f38fa278fa137a37cf5b540906fe3ec2f11d1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-08 23:36:37 +0800

    1230: update solution

--- algorithm_1000-1999/1230-Toss Strange Coins/main.cpp
+++ algorithm_1000-1999/1230-Toss Strange Coins/main.cpp
@@ -5,15 +5,18 @@ public:
 
         prob.insert(prob.begin(), 0.0);
 
-        // dp[i][j]: for the first i toss, j coins facing head
-        vector<vector<double>> dp(n + 1, vector<double>(target + 1,0.0));
+        // dp[i][j]: the probability that for the first i tosses and the j coins facing heads
+        double dp[n + 1][target + 1];
         dp[0][0] = 1.0;
         for (int i = 1; i <= n; ++i) {
             dp[i][0] = dp[i - 1][0] * (1 - prob[i]);
         }
         for (int j = 1; j <= target; ++j) {
-            for (int i = j; i <= n; ++i) {
-                dp[i][j] = dp[i - 1][j - 1] * (prob[i]) + dp[i-1][j] * (1 - prob[i]);
+            dp[j - 1][j] = 0.0;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= std::min(i, target); ++j) {
+                dp[i][j] = dp[i - 1][j] * (1.0 - prob[i]) + dp[i - 1][j - 1] * prob[i];
             }
         }
 
