944562ab3febf7feb8392ec0d0d5e08dba48e268
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-08 12:26:09 +0800

    1180: initial solution

--- algorithm_1000-1999/1180-Count Substrings with Only One Distinct Letter/main.cpp
+++ algorithm_1000-1999/1180-Count Substrings with Only One Distinct Letter/main.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    int maxNumberOfBalloons(string text) {
-        unordered_map<char, int> cnts;
-        for (char c : text) {
-            ++cnts[c];
+    int countLetters(string s) {
+        int ret = 0;
+
+        char c = '\0';
+        int cnt = 0;
+        for (int i = 0, n = s.size(); i <= n; ++i) {
+            if (i == n || c != s[i]) {
+                ret += (cnt) * (cnt + 1) / 2;
+
+                if (i < n) {
+                    c = s[i];
+                    cnt = 1;
+                }
+            }
+            else {
+                ++cnt;
+            }
         }
 
-        cnts['l'] /= 2;
-        cnts['o'] /= 2;
-
-        return std::min({cnts['b'], cnts['a'], cnts['l'], cnts['o'], cnts['n']});
+        return ret;
     }
 };
