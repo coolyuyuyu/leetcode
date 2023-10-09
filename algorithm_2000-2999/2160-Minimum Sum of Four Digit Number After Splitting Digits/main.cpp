ecec9813ff326d6d968dc100bef1e1442411e03f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-10 02:28:18 +0800

    2160: initial solution

---
+++ algorithm_2000-2999/2160-Minimum Sum of Four Digit Number After Splitting Digits/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> digits;
+        for (int i = 0; i < 4; ++i) {
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+        std::sort(digits.begin(), digits.end());
+
+        int new1 = digits[0] * 10 + digits[3];
+        int new2 = digits[1] * 10 + digits[2];
+        return new1 + new2;
+    }
+};
