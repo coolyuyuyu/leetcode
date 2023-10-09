9cda934e46b3ed3c2d4cddca544530b62d57478c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-10 02:28:05 +0800

    1844: initial solution

---
+++ algorithm_1000-1999/1844-Replace All Digits with Characters/main.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n = s.size();
+        for (int i = 0; i < n; i += 2) {
+            s[i + 1] = s[i] + (s[i + 1] - '0');
+        }
+
+        return s;
+    }
+};
