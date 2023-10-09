f71b359ac9872e8063aa94bc39fd211c385c50d1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-09 10:40:19 +0800

    034: update solution

--- algorithm_0000-0999/034-Search for a Range/main.cpp
+++ algorithm_0000-0999/034-Search for a Range/main.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    int searchFirst(vector<int> nums, int target) {
-        int lft = 0, rht = nums.size();
-        while (lft < rht) {
-            int mid = lft + (rht - lft) / 2;
-            if (target <= nums[mid]) {
-                rht = mid;
-            }
-            else {
-                lft = mid + 1;
-            }
-        }
-
-        return lft;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int index = searchFirst(nums, target);
-        if (index < nums.size() && nums[index] == target) {
-            return{index, searchFirst(nums, target + 1) - 1};
-        }
-        else {
-            return{-1, -1};
-        }
+       auto itr = std::lower_bound(nums.begin(), nums.end(), target);
+       if (itr == nums.end() || *itr != target) {
+           return {-1, -1};
+       }
+       else {
+           int l = std::distance(nums.begin(), itr);
+           int r = std::distance(nums.begin(), std::upper_bound(itr + 1, nums.end(), target)) - 1;
+           return {l, r};
+       }
     }
 };
