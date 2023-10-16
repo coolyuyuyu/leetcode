9d7f9ff35ee9f373cf48859b30ed4e199d3d51de
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-16 15:40:49 +0800

    119: update solution

--- algorithm_0000-0999/119-Pascal's Triangle II/main.cpp
+++ algorithm_0000-0999/119-Pascal's Triangle II/main.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    vector<int> dp(size_t rowIndex) {
-        if (rowIndex == 0) {
-            return {1};
-        }
-
-        vector<int> ret = {1};
-        for (size_t i = 1; i <= rowIndex; ++i) {
-            vector<int> tmp;
-            tmp.resize(i + 1);
-
-            tmp[0] = 1;
-            for (size_t j = 1; j < i; ++j) {
-                tmp[j] = ret[j - 1] + ret[j];
+    vector<int> dynamicProgramming(int rowIndex) {
+        // dp[i]: the ith elelement of Pascal's triangle with rowIndex as rowIndex
+        vector<int> dp(rowIndex + 1);
+        for (int r = 0; r <= rowIndex; ++r) {
+            dp[r] = 1;
+            for (int c = r - 1; c >= 1; --c) {
+                dp[c] = dp[c] + dp[c - 1];
             }
-            tmp[i] = 1;
-
-            std::swap(ret, tmp);
+            dp[0] = 1;
         }
 
-        return ret;
+        return dp;
     }
 
-
-    vector<int> math(size_t rowIndex) {
-        if (rowIndex == 0) {
-            return {1};
+    vector<int> math(int rowIndex) {
+        // dp[i]: the ith elelement of Pascal's triangle with rowIndex as rowIndex
+        vector<int> dp(rowIndex + 1);
+        dp[0] = 1;
+        for (int c = 1; c <= rowIndex / 2; ++c) {
+            dp[rowIndex - c] = dp[c] = (long long)dp[c - 1] * (rowIndex - c + 1) / c;
         }
+        dp[rowIndex] = 1;
 
-        vector<int> ret(rowIndex + 1);
-        ret[0] = 1;
-        for (size_t k = 1; k < rowIndex; ++k) {
-            ret[k] = ret[k - 1] * (rowIndex - k + 1) / k;
-        }
-        ret[rowIndex] = 1;
-
-        return ret;
+        return dp;
     }
 
     vector<int> getRow(int rowIndex) {
-        //return dp(rowIndex);
+        //return dynamicProgramming(rowIndex);
         return math(rowIndex);
     }
 };
