0cfaf1ab98d5b95733e137d10f6dcbda15732858
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-16 23:35:03 +0800

    1569: update solution

--- algorithm_1000-1999/1569-Number of Ways to Reorder Array to Get Same BST/main.cpp
+++ algorithm_1000-1999/1569-Number of Ways to Reorder Array to Get Same BST/main.cpp
@@ -1,67 +1,66 @@
 class Solution {
 public:
-    struct TreeNode {
-        int val;
-        TreeNode *lft, *rht;
+    class TreeNode {
+    public:
         TreeNode(int v)
-            : val(v)
-            , lft(nullptr)
-            , rht(nullptr) {
-        }
+            : lft(nullptr)
+            , rht(nullptr)
+            , val(v) {}
+        TreeNode* lft;
+        TreeNode* rht;
+        int val;
     };
 
     int M = 1e9 + 7;
 
     int numOfWays(vector<int>& nums) {
+        int n = nums.size();
+
         TreeNode* root = nullptr;
         for (int num : nums) {
             TreeNode** ppNode = &root;
             while (*ppNode) {
-                if ((*ppNode)->val < num) {
-                    ppNode = &((*ppNode)->rht);
-                }
-                else {
-                    ppNode = &((*ppNode)->lft);
-                }
+                ppNode = &(num < (*ppNode)->val ? (*ppNode)->lft : (*ppNode)->rht);
             }
             *ppNode = new TreeNode(num);
         }
 
-        int cache[nums.size()][nums.size()];
-        std::memset(cache, -1, sizeof(cache));
-        std::function<int(int, int)> comb = [&](int n, int r) {
-            if ((n - r) < r) {
-                r = n - r;
+        // dp[n][r]: combination C(n, r)
+        int dp[n + 1][n + 1];
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[i][j] = -1;
             }
-
-            if (r == 0) {
+        }
+        std::function<int(int, int)> comb = [&] (int n, int r) {
+            if (n - r < r) {
+                return comb(n, n - r);
+            }
+            else if (r == 0) {
                 return 1;
             }
             else if (r == 1) {
                 return n;
             }
 
-            int& ret = cache[n][r];
+            int& ret = dp[n][r];
             if (ret < 0) {
-                // C(m, n) = C(m-1, n) + C(m-1, n-1)
                 ret = (comb(n - 1, r - 1) + comb(n - 1, r)) % M;
             }
 
             return ret;
         };
 
-        std::function<pair<long, int>(TreeNode*)> dfs = [&](TreeNode* root) -> pair<long, int>{
-            if (!root) {
-                return {1, 0};
-            }
-
-            auto [lftSeqNum, lftNodeNum] = dfs(root->lft);
-            auto [rhtSeqNum, rhtNodeNum] = dfs(root->rht);
-            long seqNum = lftSeqNum * rhtSeqNum % M * comb(lftNodeNum + rhtNodeNum, lftNodeNum) % M;
-            int nodeNum = lftNodeNum + rhtNodeNum + 1;
-            return {seqNum, nodeNum};
+        std::function<pair<int, long>(TreeNode*)> dfs = [&](TreeNode* root) -> pair<int, long> {
+            if (!root) { return {0, 1}; }
+            auto [lftNodeCnt, lftSeqCnt] = dfs(root->lft);
+            auto [rhtNodeCnt, rhtSeqCnt] = dfs(root->rht);
+            int nodeCnt = lftNodeCnt + 1 + rhtNodeCnt;
+            long seqCnt = (lftSeqCnt * rhtSeqCnt) % M * comb(lftNodeCnt + rhtNodeCnt, lftNodeCnt) % M;
+            return {nodeCnt, seqCnt};
         };
-        auto [seqNum, nodeNum] = dfs(root);
-        return seqNum - 1;
+        auto [_, seqCnt] = dfs(root);
+
+        return seqCnt - 1;
     }
 };
