e6edf32c321df565baf5599b11f3f254979c36a6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-16 15:41:07 +0800

    1626: update solution

--- algorithm_1000-1999/1626-Best Team With No Conflicts/main.cpp
+++ algorithm_1000-1999/1626-Best Team With No Conflicts/main.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        int n = scores.size();
+        int n = ages.size();
 
-        vector<int> indexes(n);
-        std::iota(indexes.begin(), indexes.end(), 0);
-        std::sort(indexes.begin(), indexes.end(), [&](int i1, int i2) { return (ages[i1] < ages[i2]) || (ages[i1] == ages[i2] && scores[i1] < scores[i2]); });
+        int indexes[n];
+        std::iota(indexes, indexes + n, 0);
+        std::sort(indexes, indexes + n, [&](int i1, int i2) {
+            return
+                (ages[i1] < ages[i2]) ||
+                (ages[i1] == ages[i2] && scores[i1] < scores[i2]); });
 
-        int ret = 0;
-
-        //dp[i]: highest overall score of increasing subsequence[0:i-1] and scores[i]
-        vector<int> dp(n);
+        // dp[i]: the highest overall score from teams[0:i] and teams[i] is included
+        int dp[n];
         for (int i = 0; i < n; ++i) {
             dp[i] = scores[indexes[i]];
             for (int j = 0; j < i; ++j) {
@@ -18,6 +19,10 @@ public:
                     dp[i] = std::max(dp[i], dp[j] + scores[indexes[i]]);
                 }
             }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < n; ++i) {
             ret = std::max(ret, dp[i]);
         }
 
