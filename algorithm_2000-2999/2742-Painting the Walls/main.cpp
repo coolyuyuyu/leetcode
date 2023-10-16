56036a6c479d8b698098149b30f524995cb14eaa
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-16 15:41:15 +0800

    2742: update solution

--- algorithm_2000-2999/2742-Painting the Walls/main.cpp
+++ algorithm_2000-2999/2742-Painting the Walls/main.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
+    // Time: O(n^2)
     int dp1(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
-        int offset = n + 1;
 
         cost.insert(cost.begin(), 0);
         time.insert(time.begin(), 0);
 
-        // dp[i][diff]: the minimum amount of money to paint first i walls with cost difference between paid and free painter as diff
-        vector<vector<int>> dp(n + 1, vector<int>(2 * n + 2, INT_MAX / 2));
-        dp[0][0 + offset] = 0;
-
+        // dp[i][j]: the minimum amount of money required to paint walls[1:i] with time difference between paid and free printer as j
+        int OFFSET = n + 1;
+        int dp[n + 1][2 * n + 2];
+        for (int i = 0; i <= n; ++i) {
+            for (int j = -n; j <= n; ++j) {
+                dp[i][j + OFFSET] = INT_MAX / 2;
+            }
+        }
+        dp[0][0 + OFFSET] = 0;
         for (int i = 0; i < n; ++i) {
-            for (int diff = -n; diff <= n; ++diff) {
-                // free
-                dp[i + 1][diff - 1 + offset] = std::min(dp[i + 1][diff - 1 + offset], dp[i][diff  + offset]);
-
-                // paid
-                dp[i + 1][std::min(diff + time[i + 1], n) + offset] = std::min(dp[i + 1][std::min(diff + time[i + 1], n) + offset], dp[i][diff + offset] + cost[i + 1]);
+            for (int j = -n; j <= n; ++j) {
+                int jj = std::min(n, j + time[i + 1]);
+                dp[i + 1][jj + OFFSET] = std::min(dp[i + 1][jj + OFFSET], dp[i][j + OFFSET] + cost[i + 1]); // paid
+                dp[i + 1][j - 1 + OFFSET] = std::min(dp[i + 1][j - 1 + OFFSET], dp[i][j + OFFSET]); // free
             }
         }
 
         int ret = INT_MAX;
-        for (int diff = 0; diff <= n; ++diff) {
-            ret = std::min(ret, dp[n][diff + offset]);
+        for (int j = 0; j <= n; ++j) {
+            ret = std::min(ret, dp[n][j + OFFSET]);
         }
 
         return ret;
@@ -31,18 +34,24 @@ public:
 
     int dp2(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
+
         cost.insert(cost.begin(), 0);
         time.insert(time.begin(), 0);
 
-        // dp[i][j]: the minimum amount of money to paint j walls by considering the first i worker
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, INT_MAX / 2));
+        // if paid painter paints walls[i], overall 1 + times[i] walls can be painted with cost[i]
+        // dp[i][j]: the minimum amount of money required to paint j walls by considering walls[1:i]
+        int dp[n + 1][n + 1];
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                dp[i][j] = INT_MAX / 2;
+            }
+        }
         dp[0][0] = 0;
-
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j <= n; ++j) {
                 dp[i + 1][j] = std::min(dp[i + 1][j], dp[i][j]);
 
-                int jj = std::min(j + 1 + time[i + 1], n);
+                int jj = std::min(n, j + 1 + time[i + 1]);
                 dp[i + 1][jj] = std::min(dp[i + 1][jj], dp[i][j] + cost[i + 1]);
             }
         }
