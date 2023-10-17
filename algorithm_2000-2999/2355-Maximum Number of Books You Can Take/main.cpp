32167bb0ae042679cbcbb8cdb45e9b61f9cb2064
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-17 23:16:09 +0800

    2355: update solution

--- algorithm_2000-2999/2355-Maximum Number of Books You Can Take/main.cpp
+++ algorithm_2000-2999/2355-Maximum Number of Books You Can Take/main.cpp
@@ -3,27 +3,27 @@ public:
     long long maximumBooks(vector<int>& books) {
         int n = books.size();
 
-        // dp[i]: the maximum number of books you can take from the subarray ending at ith shelf
-        vector<long long> dp(n);
-        stack<int> stk;
+        // the maximum number of books you can take from the subarray ending at ith bookshelf
+        long long dp[n];
+
         long long ret = 0;
+        stack<int> stk;
         for (int i = 0; i < n; ++i) {
-            while (!stk.empty() && books[stk.top()] > (books[i] - (i - stk.top()))) {
+            while (!stk.empty() && books[stk.top()] >= books[i] - (i - stk.top())) {
                 stk.pop();
             }
-
             if (stk.empty()) {
-                int len = std::min(books[i], i + 1);
-                dp[i] = (long long)(books[i] + books[i] - len + 1) * len / 2;
+                long long len = std::min(i + 1, books[i]);
+                dp[i] = (books[i] + books[i] - len + 1) * len / 2;
             }
             else {
                 int j = stk.top();
-                int len = i - j;
-                dp[i] = dp[stk.top()] + (long long)(books[i] + books[i] - len + 1) * len / 2;
+                long long len = i - j;
+                dp[i] = dp[j] + (books[i] + books[i] - len + 1) * len / 2;
             }
-            ret = std::max(ret, dp[i]);
-
             stk.push(i);
+
+            ret = std::max(ret, dp[i]);
         }
 
         return ret;
