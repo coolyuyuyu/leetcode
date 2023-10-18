7f5f206e7bc83e0621ad448eb3b5905420e6d052
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-18 16:06:28 +0800

    1125: update solution

--- algorithm_1000-1999/1125-Smallest Sufficient Team/main.cpp
+++ algorithm_1000-1999/1125-Smallest Sufficient Team/main.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
     vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        int m = (1 << req_skills.size());
         unordered_map<string, int> skill2id;
         for (const string& skill : req_skills) {
             skill2id.emplace(skill, skill2id.size());
         }
-        int m = (1 << skill2id.size());
 
         int n = people.size();
-        vector<int> skillsets(n, 0);
+        int skillset[n];
         for (int i = 0; i < n; ++i) {
-            int& skillset = skillsets[i];
+            skillset[i] = 0;
             for (const string& skill : people[i]) {
-                skillset |= (1 << skill2id[skill]);
+                skillset[i] |= (1 << skill2id[skill]);
             }
         }
 
         // state: binary representation of skills
         // dp1[state]: the minimum number of people to cover the state of skills
         // dp2[state]: the minimum people combination to cover the state of skills
-        vector<int> dp1(m, INT_MAX);
+        int dp1[m];
+        long dp2[m];
+        for (int state = 0; state < m; ++state) {
+            dp1[state] = INT_MAX;
+            dp2[state] = 0;
+        }
         dp1[0] = 0;
-        vector<long> dp2(m, 0);
-        for (int state1 = 0; state1 < m; ++state1) {
-            if (dp1[state1] == INT_MAX) { continue; }
-            for (int j = 0; j < n; ++j) {
-                int state2 = state1 | skillsets[j];
-                if (dp1[state1] + 1 < dp1[state2]) {
-                    dp1[state2] = dp1[state1] + 1;
-                    dp2[state2] = dp2[state1] | (1L << j);
+        for (int state = 0; state < m; ++state) {
+            if (dp1[state] == INT_MAX) { continue; }
+            for (int i = 0; i < n; ++i) {
+                int nxtState = state | skillset[i];
+                if (dp1[state] + 1 < dp1[nxtState]) {
+                    dp1[nxtState] = dp1[state] + 1;
+                    dp2[nxtState] = dp2[state] | (1L << i);
+
                 }
             }
         }
 
         vector<int> ret;
-        for (int j = 0; j < n; ++j) {
-            if ((dp2[m - 1] >> j) & 1) {
-                ret.push_back(j);
+        for (int i = 0; i < n; ++i) {
+            if (dp2[m - 1] & (1L << i)) {
+                ret.push_back(i);
             }
         }
 
