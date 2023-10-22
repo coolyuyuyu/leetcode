150a376413fd9d925844ded07b00d2b03124f3a6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-22 23:35:11 +0800

    877: update solution

--- algorithm_0000-0999/877-Stone Game/main.cpp
+++ algorithm_0000-0999/877-Stone Game/main.cpp
@@ -1,95 +1,66 @@
 class Solution {
 public:
-    // Almost same as 486. Predict the Winner
+    bool topdnDFS(vector<int>& piles) {
+        int n = piles.size();
 
-    bool recursion(const vector<int>& nums) {
-        int n = nums.size();
-
-        vector<int> presum(n);
-        std::partial_sum(nums.begin(), nums.end(), presum.begin());
-        std::function<int(int, int)> sum = [&](int lo, int hi) {
-            return presum[hi] - (0 < lo ? presum[lo - 1] : 0);
+        int presum[n];
+        std::partial_sum(piles.begin(), piles.end(), presum);
+        std::function<int(int, int)> sum = [&](int lft, int rht) {
+            return presum[rht] - (0 < lft ? presum[lft - 1] : 0);
         };
 
-        std::function<int(int, int)> f = [&](int lft, int rht) {
-            if (lft == rht) {
-                return nums[lft];
-            }
-
-            // f(lft, rht) = std::max(
-            //     nums[lft] + sum(lft + 1, rht) - f(lft + 1, rht),
-            //     nums[rht] + sum(lft, rht - 1) - f(lft, rht - 1),
-            // );
-
-            return sum(lft, rht) - std::min(f(lft + 1, rht), f(lft, rht -1));
-        };
-
-        return sum(0, n - 1) <= (f(0, n - 1) * 2);
-    }
-
-    bool recursion_memo(const vector<int>& nums) {
-        int n = nums.size();
-
-        vector<int> presum(n);
-        std::partial_sum(nums.begin(), nums.end(), presum.begin());
-        std::function<int(int, int)> sum = [&](int lo, int hi) {
-            return presum[hi] - (0 < lo ? presum[lo - 1] : 0);
-        };
-
-        int cache[n][n];
+        // dp[i][j]: the maximum number of stones the player can take
+        int dp[n][n];
         for (int lft = 0; lft < n; ++lft) {
             for (int rht = lft; rht < n; ++rht) {
-                cache[lft][rht] = -1;
+                dp[lft][rht] = -1;
             }
         }
-
         std::function<int(int, int)> f = [&](int lft, int rht) {
-            int& ret = cache[lft][rht];
+            int& ret = dp[lft][rht];
             if (0 <= ret) {
                 return ret;
             }
 
             if (lft == rht) {
-                return ret = nums[lft];
+                return ret = piles[lft];
             }
-
-            return ret = sum(lft, rht) - std::min(f(lft + 1, rht), f(lft, rht -1));
+            return ret = sum(lft, rht) - std::min(f(lft + 1, rht), f(lft, rht - 1));
         };
 
-        return sum(0, n - 1) <= (f(0, n - 1) * 2);
+        return sum(0, n - 1) < f(0, n - 1) * 2;
     }
 
-    bool btmup_dp(const vector<int>& nums) {
-        int n = nums.size();
+    bool btmupDP(vector<int>& piles) {
+        int n = piles.size();
 
-        vector<int> presum(n);
-        std::partial_sum(nums.begin(), nums.end(), presum.begin());
-        std::function<int(int, int)> sum = [&](int lo, int hi) {
-            return presum[hi] - (0 < lo ? presum[lo - 1] : 0);
+        int presum[n];
+        std::partial_sum(piles.begin(), piles.end(), presum);
+        std::function<int(int, int)> sum = [&](int lft, int rht) {
+            return presum[rht] - (0 < lft ? presum[lft - 1] : 0);
         };
 
-        // dp[i][j]: the maximum score the player can get from nums[i:j]
+        // dp[i][j]: the maximum number of stones the player can take
         int dp[n][n];
         for (int i = 0; i < n; ++i) {
-            dp[i][i] = nums[i];
+            dp[i][i] = piles[i];
         }
         for (int len = 2; len <= n; ++len) {
             for (int lft = 0, rht = lft + len - 1; rht < n; ++lft, ++rht) {
-                dp[lft][rht] = sum(lft, rht) - std::min(dp[lft + 1][rht], dp[lft][rht -1]);
+                dp[lft][rht] = sum(lft, rht) - std::min(dp[lft + 1][rht], dp[lft][rht - 1]);
             }
         }
 
-        return sum(0, n - 1) <= (dp[0][n - 1] * 2);
+        return sum(0, n - 1) < dp[0][n - 1] * 2;
     }
 
-    bool math(const vector<int>& nums) {
+    bool math(vector<int>& piles) {
         return true;
     }
 
     bool stoneGame(vector<int>& piles) {
-        //return recursion(piles);
-        //return recursion_memo(piles);
-        //return btmup_dp(piles);
-        return math(piles);
+        return topdnDFS(piles);
+        //return btmupDP(piles);
+        //return math(piles);
     }
 };
