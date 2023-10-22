b8185d27725503aa67dfd9030ee6cec44415ad5a
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-23 02:46:24 +0800

    1140: update solution

--- algorithm_1000-1999/1140-Stone Game II/main.cpp
+++ algorithm_1000-1999/1140-Stone Game II/main.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
-    int stoneGameII(vector<int>& piles) {
+    int topdnDFS(vector<int>& piles) {
         int n = piles.size();
 
-        vector<int> suf(n + 1);
-        suf[n] = 0;
-        std::partial_sum(piles.rbegin(), piles.rend(), suf.rbegin() + 1);
+        int presum[n];
+        std::partial_sum(piles.begin(), piles.end(), presum);
+        std::function<int(int)> sufsum = [&](int i) {
+            if (i >= n) {
+                return 0;
+            }
+            return presum[n - 1] - (0 < i ? presum[i - 1] : 0);
+        };
 
-        vector<vector<int>> cache(n, vector<int>(n + 1, 0));
-        std::function<int(int, int)> solve = [&](int i, int M) {
-            if (piles.size() <= i) {
+        // dp[i]: the maximum number of stones the player can take from piles[i:]
+        int dp[n][n + 1];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                dp[i][j] = 0;
+            }
+        }
+        std::function<int(int, int)> f = [&](int i, int M) {
+            if (n <= i) {
                 return 0;
             }
 
-            int& ret = cache[i][M];
-            if (ret != 0) {
+            int& ret = dp[i][M];
+            if (0 < ret) {
                 return ret;
             }
 
-            int sum = 0;
-            for (int X = 1; X <= (2 * M) && (i + X - 1) < n; ++X) {
-                sum += piles[i + X - 1];
-                ret = std::max(ret, sum + suf[i + X] - solve(i + X, std::max(M, X)));
+            for (int X = 1; X <= 2 * M && i + X - 1 < n; ++X) {
+                //ret = std::max(ret, sum(i, i + X - 1) + sum(i + X, n) - f(i + X, std::max(M, X)));
+                ret = std::max(ret, sufsum(i) - f(i + X, std::max(M, X)));
             }
-
             return ret;
         };
 
-        return solve(0, 1);
+        return f(0, 1);
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        return topdnDFS(piles);
     }
 };
