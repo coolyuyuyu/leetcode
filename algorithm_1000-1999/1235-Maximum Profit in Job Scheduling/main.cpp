7bf9f437afb8e9e933c9d776dc69f5e22d13cba5
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-24 02:46:12 +0800

    1235: update solution

--- algorithm_1000-1999/1235-Maximum Profit in Job Scheduling/main.cpp
+++ algorithm_1000-1999/1235-Maximum Profit in Job Scheduling/main.cpp
@@ -3,23 +3,23 @@ public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
 
-        vector<tuple<int, int, int>> jobs(n);
+        tuple<int, int, int> job[n + 1];
+        job[0] = {0, 0, 0};
         for (int i = 0; i < n; ++i) {
-            jobs[i] = {startTime[i], endTime[i], profit[i]};
+            job[i + 1] = {startTime[i], endTime[i], profit[i]};
         }
-        jobs.insert(jobs.begin(), {-1, -1, 0});
-        std::sort(jobs.begin(), jobs.end(), [&](const auto& job1, const auto& job2) { return std::get<1>(job1) < std::get<1>(job2); });
+        std::sort(job, job + n + 1, [&](const auto& job1, const auto& job2){ return std::get<1>(job1) < std::get<1>(job2); });
 
-        // dp[i]: the maximum profit from first i jobs
-        vector<int> dp(n + 1);
+        // dp[i]: the maximum profit you can take from job[1:i]
+        int dp[n + 1];
         dp[0] = 0;
         for (int i = 1; i <= n; ++i) {
             auto itr = std::upper_bound(
-                jobs.begin(), jobs.begin() + i,
-                tuple<int, int, int>{-1, std::get<0>(jobs[i]), 0},
-                [&](const auto& job1, const auto& job2) { return std::get<1>(job1) < std::get<1>(job2); });
-            int x = itr - 1 - jobs.begin();
-            dp[i] = std::max(dp[i - 1], dp[x] + std::get<2>(jobs[i]));
+                job, job + i,
+                tuple<int, int, int>{0, std::get<0>(job[i]), 0},
+                [&](const auto& job1, const auto& job2){ return std::get<1>(job1) < std::get<1>(job2); });
+            int j = std::distance(job, itr) - 1;
+            dp[i] = std::max(dp[i - 1], dp[j] + std::get<2>(job[i]));
         }
 
         return dp[n];
