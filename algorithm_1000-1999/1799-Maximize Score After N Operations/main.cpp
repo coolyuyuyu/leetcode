da6417d9079afaee54b964c02f09e349a5b3ce56
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-25 01:20:51 +0800

    1799: update solution

--- algorithm_1000-1999/1799-Maximize Score After N Operations/main.cpp
+++ algorithm_1000-1999/1799-Maximize Score After N Operations/main.cpp
@@ -2,14 +2,52 @@ class Solution {
 public:
     int maxScore(vector<int>& nums) {
         int n = nums.size() / 2;
+        int m = n * 2;
 
-        vector<vector<int>> stateGroups(n + 1);
-        stateGroups[0].push_back(0);
-        for (int i = 1; i <= n; ++i) {
-            // gosper' hack
-            int state = (1 << (2 * i)) - 1;
-            while (state < (1 << (2 * n))) {
-                stateGroups[i].push_back(state);
+        int GCD[m][m];
+        for (int i = 0; i < m; ++i) { // 2 1-bit
+            for (int j = i + 1; j < m; ++j) {
+                GCD[i][j] = std::gcd(nums[i], nums[j]);
+            }
+        }
+
+        // state: binary representation whether nums[i] is considered
+        // dp[state]: the maximum score of state
+        int dp[1 << (n * 2)];
+        dp[0] = 0; // zero 1-bit
+        for (int i = 0; i < m; ++i) { // 2 1-bit
+            for (int j = i + 1; j < m; ++j) {
+                int state = (1 << i) | (1 << j);
+                dp[state] = GCD[i][j];
+            }
+        }
+
+        for (int i = 2; i <= n; ++i) {
+            int k = 2 * i;
+            // Gosper's hack: Iterate all the m-bit state where there are k 1-bits.
+            for (int state = (1 << k) - 1; state < (1 << m);) {
+                dp[state] = 0;
+
+                // iterate all subset, exclude 0
+                for (int subset = state; subset > 0; subset = (subset - 1) & state) {
+                    if (__builtin_popcount(subset) != (i - 1) * 2) { continue; }
+                    if (state & subset != subset) { continue; }
+
+                    int diff = state - subset;
+                    int idx1 = -1, idx2 = -1;
+                    for (int i = 0; i < m; ++i) {
+                        if ((diff >> i) & 1) {
+                            if (idx1 == -1) {
+                                idx1 = i;
+                            }
+                            else if (idx2 == -1) {
+                                idx2 = i;
+                                break;
+                            }
+                        }
+                    }
+                    dp[state] = std::max(dp[state], dp[subset] + i * GCD[idx1][idx2]);
+                }
 
                 int c = state & - state;
                 int r = state + c;
@@ -17,68 +55,6 @@ public:
             }
         }
 
-        vector<vector<int>> g(2 * n, vector<int>(2 * n));
-        for (int i = 0; i < (2 * n); ++i) {
-            for (int j = i + 1; j < (2 * n); ++j) {
-                g[i][j] = std::gcd(nums[i], nums[j]);
-            }
-        }
-
-        std::function<pair<int,int>(int)> findIndexPair = [n](int state) {
-            int idx1 = -1, idx2;
-            for (int i = 0; i < (2 * n); ++i) {
-                if (1 << i & state) {
-                    if (idx1 == -1) {
-                        idx1 = i;
-                    }
-                    else {
-                        idx2 = i;
-                        break;
-                    }
-                }
-            }
-
-            return make_pair(idx1, idx2);
-        };
-
-        vector<int> dp(1 << (2 * n), 0);
-        for (int i = 1; i <= n; ++i) {
-            for (int state : stateGroups[i]) {
-                // 1. iterate substate from state.
-                if (i == 1) {
-                    auto [idx1, idx2] = findIndexPair(state);
-                    dp[state] = std::max(dp[state], g[idx1][idx2]);
-                }
-                else {
-                    for (int substate = state; substate > 0; substate = (substate - 1) & state) {
-                        if (__builtin_popcount(substate) != 2 * (i - 1)) {
-                            continue;
-                        }
-
-                        auto [idx1, idx2] = findIndexPair(state - substate);
-                        dp[state] = std::max(dp[state], dp[substate] + i * g[idx1][idx2]);
-                    }
-                }
-
-                /*
-                // 2. iterate all possible substate and exclude non-subset.
-                for (int substate : stateGroups[i - 1]) {
-                    if ((state & substate) != substate) {
-                        continue;
-                    }
-
-                    auto [idx1, idx2] = findIndexPair(state - substate);
-                    dp[state] = std::max(dp[state], dp[substate] + i * g[idx1][idx2]);
-                }
-                */
-            }
-        }
-
-        return dp[(1 << (2 * n)) - 1];
+        return dp[(1 << m) - 1];
     }
 };
-
-// for (int i = 1; i <= n; ++i)
-//     dp[state A of 2*i 1-bits] = max({dp[substate B of 2*(i-1) 1-bits] + gcd(nums[index1], nums[index2])})
-//     substate B must be a subset of state A
-
