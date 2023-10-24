85b0ee7aa136d9f3f348974202a8e8f14556b6dc
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-24 23:13:16 +0800

    465: update solution

--- algorithm_0000-0999/465-Optimal Account Balancing/main.cpp
+++ algorithm_0000-0999/465-Optimal Account Balancing/main.cpp
@@ -6,43 +6,41 @@ public:
             balances[transaction[0]] -= transaction[2];
             balances[transaction[1]] += transaction[2];
         }
-
-        // retain non-zero
-        {
+        { // remove zero amount
             vector<int> tmp;
-            for (int b : balances) {
-                if (b != 0) {
-                    tmp.push_back(b);
+            for (int balance : balances) {
+                if (balance != 0) {
+                    tmp.push_back(balance);
                 }
             }
             balances.swap(tmp);
         }
-
         int n = balances.size();
-        vector<int> sum(1 << n, 0);
+
+        // state: binary representation whehter the ith person is considered
+        // even[state]: whether the state is break even or not
+        // dp[state]: the minimum number of transactions required to settle the debt of state
+        bool even[1 << n];
+        int dp[1 << n];
         for (int state = 0; state < (1 << n); ++state) {
+            int sum = 0;
             for (int i = 0; i < n; ++i) {
                 if ((state >> i) & 1) {
-                    sum[state] += balances[i];
+                    sum += balances[i];
                 }
             }
+            even[state] = (sum == 0);
+            dp[state] = INT_MAX;
         }
-
-        vector<int> dp(1 << n);
         dp[0] = 0;
         for (int state = 1; state < (1 << n); ++state) {
-            if (sum[state] != 0) {
-                dp[state] = INT_MAX;
+            if (!even[state]) {
                 continue;
             }
 
             dp[state] = __builtin_popcount(state) - 1;
-            for (int subset = state; 0 < subset; subset = (subset - 1) & state) {
-                if (subset == state) {
-                    continue;
-                }
-
-                if (sum[subset] == 0) {
+            for (int subset = state; subset; subset = (subset - 1) & state) {
+                if (even[subset]) {
                     dp[state] = std::min(dp[state], dp[subset] + dp[state - subset]);
                 }
             }
