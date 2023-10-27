e2125169cd68c57d3ad15fb05eb499a5db21ed9f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-27 14:34:15 +0800

    1335: update solution

--- algorithm_1000-1999/1335-Minimum Difficulty of a Job Schedule/main.cpp
+++ algorithm_1000-1999/1335-Minimum Difficulty of a Job Schedule/main.cpp
@@ -5,27 +5,26 @@ public:
         if (n < d) {
             return -1;
         }
-
         jobDifficulty.insert(jobDifficulty.begin(), 0);
 
-        // dp[i][numParts]: the minimum difficulty of a job schedule for jobDifficulty[1:i] finished in numParts days 
-        vector<vector<int>> dp(n + 1, vector<int>(d + 1));
+        // dp[i][k]: the minimum difficulty of a job schedule for jobDifficulty[1:i] finished in k days
+        int dp[n + 1][d + 1];
+        for (int i = 0; i <= n; ++i) {
+            for (int k = 0; k <= std::min(d, i); ++k) {
+                dp[i][k] = INT_MAX / 2;
+            }
+        }
         dp[0][0] = 0;
         for (int i = 1; i <= n; ++i) {
-            dp[i][0] = INT_MAX / 2;
-        }
-        for (int i = 1; i <= n; ++i) {
-            for (int numParts = 1; numParts <= std::min(i, d); ++numParts) {
-                int maxVal = INT_MIN;
-                dp[i][numParts] = INT_MAX / 2;
+            for (int numParts = 1; numParts <= std::min(d, i); ++numParts) {
+                int difficulty = INT_MIN;
                 for (int j = i; numParts <= j; --j) {
-                    maxVal = std::max(maxVal, jobDifficulty[j]);
-                    dp[i][numParts] = std::min(dp[i][numParts], dp[j - 1][numParts - 1] + maxVal);
+                    difficulty = std::max(difficulty, jobDifficulty[j]);
+                    dp[i][numParts] = std::min(dp[i][numParts], dp[j - 1][numParts - 1] + difficulty);
                 }
             }
         }
 
         return dp[n][d];
-        
     }
 };
