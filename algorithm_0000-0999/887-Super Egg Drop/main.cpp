8792f743f5f34c39c3e29d5c03faf0ed7aec77d7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-28 01:46:02 +0800

    887: initial solution

---
+++ algorithm_0000-0999/887-Super Egg Drop/main.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    // Time: O(kn^2), TLE
+    int topdnDFS1(int k, int n) {
+        // dp[i][j]: the minimum number of moves for i eggs and j floors
+        int dp[k + 1][n + 1];
+        for (int i = 0; i <= k; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                dp[i][j] = INT_MAX;
+            }
+        }
+        std::function<int(int, int)> f = [&](int k, int n) {
+            if (k == 0) { return 0;}
+            else if (k == 1) { return n; }
+            else if (n <= 1) { return n; }
+
+            int& ret = dp[k][n];
+            if (ret != INT_MAX) {
+                return ret;
+            }
+
+            for (int i = 1; i <= n; ++i) {
+                ret = std::min(ret, 1 + std::max(f(k - 1, i - 1),  f(k, n - i)));
+            }
+            return ret;
+        };
+
+        return f(k, n);
+    }
+
+    // Time: O(knlogn)
+    int topdnDFS2(int k, int n) {
+        // dp[i][j]: the minimum number of moves for i eggs and j floors
+        int dp[k + 1][n + 1];
+        for (int i = 0; i <= k; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                dp[i][j] = INT_MAX;
+            }
+        }
+        std::function<int(int, int)> f = [&](int k, int n) {
+            if (k == 0) { return 0;}
+            else if (k == 1) { return n; }
+            else if (n <= 1) { return n; }
+
+            int& ret = dp[k][n];
+            if (ret != INT_MAX) {
+                return ret;
+            }
+
+            // for (int i = 1; i <= n; ++i) {
+            //     ret = std::min(ret, 1 + std::max(f(k - 1, i - 1),  f(k, n - i)));
+            // }
+            // => assume fa(i) = f(k - 1, i - 1), fb(i): f(k, n - i)
+            // => fa is monotonically increasing, fb is monotonically decreasing
+            // => binary search find first i such that fa(i) >= fb(i)
+
+            int lo = 1, hi = n;
+            while (lo < hi) {
+                int i = lo + (hi - lo) / 2;
+                int fa = f(k - 1, i - 1), fb = f(k, n - i);
+                if (fa >= fb) {
+                    hi = i;
+                }
+                else {
+                    lo = i + 1;
+                }
+            }
+            ret = std::min(ret, 1 + std::max(f(k - 1, lo - 1),  f(k, n - lo)));
+
+            return ret;
+        };
+
+        return f(k, n);
+    }
+
+    int superEggDrop(int k, int n) {
+        //return topdnDFS1(k, n);
+        return topdnDFS2(k, n);
+    }
+};
