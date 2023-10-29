8bc81b2c15e465967a6f7c4f122f0d978c8df3d4
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-29 22:27:21 +0800

    1220: update solution

--- algorithm_1000-1999/1220-Count Vowels Permutation/main.cpp
+++ algorithm_1000-1999/1220-Count Vowels Permutation/main.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int M = 1e9 + 7;
 
-    int countVowelPermutation(int n) {
+    // 刷表法
+    int dp1(int n) {
         // dp[i][c]: the number of strings of length i and last character is c
         int dp[n + 1][5];
         for (int j = 0; j < 5; ++j) {
@@ -35,5 +36,38 @@ public:
         }
 
         return ret;
+    }
+
+    // 填表法
+    int dp2(int n) {
+        // dp[i][c]: the number of strings of length i and last character is c
+        int dp[n + 1][5];
+        for (int j = 0; j < 5; ++j) {
+            dp[1][j] = 1;
+        }
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                dp[i][j] = 0;
+            }
+        }
+        for (int i = 2; i <= n; ++i) {
+            dp[i][0] = (((dp[i - 1][1] + dp[i - 1][2]) % M) + dp[i - 1][4]) % M;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % M;
+            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % M;
+            dp[i][3] = (dp[i - 1][2]);
+            dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % M;
+        }
+
+        int ret = 0;
+        for (int j = 0; j < 5; ++j) {
+            ret = (ret + dp[n][j]) % M;
+        }
+
+        return ret;
+    }
+
+    int countVowelPermutation(int n) {
+        //return dp1(n);
+        return dp2(n);
     }
 };
