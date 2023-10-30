8eac03d90d8cf872fc056f21917c9da36480cd04
coolyuyuyu <coolyuyuyu@gmail.com>
2023-10-31 01:46:57 +0800

    058: update solution

--- algorithm_0000-0999/058-Length of Last Word/main.cpp
+++ algorithm_0000-0999/058-Length of Last Word/main.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        size_t end = s.find_last_not_of(' ');
-        if (end != string::npos) {
-            size_t bgn = s.find_last_of(' ', end);
-            if (bgn == string::npos) {
-                return end + 1;
-            }
-            else {
-                return end - bgn;
-            }
-        }
-        else {
-            return 0;
-        }
+        int rht = s.find_last_not_of(' ');
+        int lft = s.find_last_of(' ', rht);
+        lft = (lft == string::npos ? 0 : lft + 1);
+        return rht - lft + 1;
     }
 };
