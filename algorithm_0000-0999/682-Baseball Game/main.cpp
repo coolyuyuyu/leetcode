f9a2cccafd808a002396b34d3e50cc5ff4e57585
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-02 12:56:52 +0800

    682: update solution

--- algorithm_0000-0999/682-Baseball Game/main.cpp
+++ algorithm_0000-0999/682-Baseball Game/main.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int calPoints(vector<string>& ops) {
-        vector<int> scores;
-        for (const string& op : ops) {
+    int calPoints(vector<string>& operations) {
+        vector<int> stk;
+        for (const string& op : operations) {
             switch (op.front()) {
-                case 'C':
-                    scores.pop_back();
-                    break;
-                case 'D':
-                    scores.push_back(scores.back() * 2);
-                    break;
-                case '+':
-                    scores.push_back(scores[scores.size() - 1] + scores[scores.size() - 2]);
-                    break;
-                default:
-                    scores.push_back(stoi(op, nullptr));
-                    break;
+            case '+':
+                stk.push_back(stk[stk.size() - 2] + stk[stk.size() - 1]);
+                break;
+            case 'D':
+                stk.push_back(stk.back() * 2);
+                break;
+            case 'C':
+                stk.pop_back();
+                break;
+            default:
+                stk.push_back(std::stoi(op));
+                break;
             }
         }
 
-        return std::accumulate(scores.begin(), scores.end(), 0);
+        return std::accumulate(stk.begin(), stk.end(), 0);
     }
 };
