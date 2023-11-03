1e72ede4a6c5fe404df4a76710266266c748e698
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 01:07:29 +0800

    1441: update solution

--- algorithm_1000-1999/1441-Build an Array With Stack Operations/main.cpp
+++ algorithm_1000-1999/1441-Build an Array With Stack Operations/main.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> operations;
-
+        vector<string> ret;
         int i = 1;
         for (int t : target) {
             for (; i < t; ++i) {
-                operations.push_back("Push");
-                operations.push_back("Pop");
+                ret.push_back("Push");
+                ret.push_back("Pop");
             }
-            operations.push_back("Push");
+            ret.push_back("Push");
             ++i;
         }
 
-        return operations;
+        return ret;
     }
 };
