5853a86c265a33d6a1ac4d28790d8b5ae963cc53
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 01:06:00 +0800

    054: update solution

--- algorithm_0000-0999/054-Spiral Matrix/main.cpp
+++ algorithm_0000-0999/054-Spiral Matrix/main.cpp
@@ -2,41 +2,30 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
-        if (m == 0 || n == 0) {
-            return {};
+
+        vector<int> ret(m * n);
+        for (size_t lft = 0, rht = n - 1, top = 0, btm = m - 1, idx = 0; true;) {
+            for (size_t c = lft; c <= rht; ++c) {
+                ret[idx++] = matrix[top][c];
+            }
+            if (++top > btm) { break; }
+
+            for (size_t r = top; r <= btm; ++r) {
+                ret[idx++] = matrix[r][rht];
+            }
+            if (lft >= rht--) { break; }
+
+            for (size_t c = rht + 1; lft < c--;) {
+                ret[idx++] = matrix[btm][c];
+            }
+            if (btm-- <= top) { break; }
+
+            for (size_t r = btm + 1; top < r--;) {
+                ret[idx++] = matrix[r][lft];
+            }
+            if (++lft > rht) { break; }
         }
 
-        vector<int> nums(m * n);
-        for (size_t lft = 0, top = 0, rht = n - 1, btm = m - 1, i = 0; true;) {
-            for (size_t col = lft; col <= rht; ++col) {
-                nums[i++] = matrix[top][col];
-            }
-            if (btm < ++top) {
-                break;
-            }
-
-            for (size_t row = top; row <= btm; ++row) {
-                nums[i++] = matrix[row][rht];
-            }
-            if (rht-- <= lft) {
-                break;
-            }
-
-            for (size_t col = rht + 1; lft < col--;) {
-                nums[i++] = matrix[btm][col];
-            }
-            if (btm-- <= top) {
-                break;
-            }
-
-            for (size_t row = btm + 1; top < row--;) {
-                nums[i++] = matrix[row][lft];
-            }
-            if (rht < ++lft) {
-                break;
-            }
-        }
-
-        return nums;
+        return ret;
     }
 };
