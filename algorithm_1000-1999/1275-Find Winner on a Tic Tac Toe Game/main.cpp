9e66c9c9e705f7e700aa3659faa5516d4ccd2867
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 03:00:19 +0800

    1275: update solution

--- algorithm_1000-1999/1275-Find Winner on a Tic Tac Toe Game/main.cpp
+++ algorithm_1000-1999/1275-Find Winner on a Tic Tac Toe Game/main.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string tictactoe(vector<vector<int>>& moves) {
-        vector<int> aRow(3), aCol(3), bRow(3), bCol(3);
+        vector<int> aRow(3, 0), aCol(3, 0), bRow(3, 0), bCol(3, 0);
         int aD1 = 0, aD2 = 0, bD1 = 0, bD2 = 0;
         bool a = true;
-        for (auto& move : moves) {
+        for (const vector<int>& move : moves) {
             int r = move[0], c = move[1];
             if (a) {
-                if (++aRow[r] == 3 || ++aCol[c] == 3 || (r == c && ++aD1 == 3) || (r + c == 2 && ++aD2 == 3)) {
+                if ((++aRow[r] == 3) || (++aCol[c] == 3) || (r == c && ++aD1 == 3) || (r + c == 2 && ++aD2 == 3)) {
                     return "A";
                 }
             }
             else {
-                if (++bRow[r] == 3 || ++bCol[c] == 3 || (r == c && ++bD1 == 3) || (r + c == 2 && ++bD2 == 3)) {
+                if ((++bRow[r] == 3) || (++bCol[c] == 3) || (r == c && ++bD1 == 3) || (r + c == 2 && ++bD2 == 3)) {
                     return "B";
                 }
             }
             a = !a;
         }
 
-        return moves.size() < 9 ? "Pending" : "Draw";
+        return (moves.size() < 9 ? "Pending" : "Draw");
     }
 };
