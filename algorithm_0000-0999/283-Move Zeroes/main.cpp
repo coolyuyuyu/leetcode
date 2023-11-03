77bb6f662ca2f1fd9203b59a5723679b3dbdeae9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 02:59:59 +0800

    283: update solution

--- algorithm_0000-0999/283-Move Zeroes/main.cpp
+++ algorithm_0000-0999/283-Move Zeroes/main.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (size_t j = 0, i = 0; i < nums.size(); ++i) {
+        for (int i = 0, j = 0, n = nums.size(); i < n; ++i) {
             if (nums[i] != 0) {
                 std::swap(nums[j++], nums[i]);
             }
