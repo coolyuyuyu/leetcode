5cb2154c8c5df3ce3191a9e4a37c465dba1eb518
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 01:18:46 +0800

    896: update solution

--- algorithm_0000-0999/896-Monotonic Array/main.cpp
+++ algorithm_0000-0999/896-Monotonic Array/main.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
         auto itr = std::find_if(nums.begin(), nums.end(), [&](int num) { return num != nums[0]; });
-        if (itr != nums.end() && nums[0] < *itr) {
+        if (itr == nums.end()) { return true; }
+        if (nums[0] < *itr) {
             return std::is_sorted(itr, nums.end(), std::less<int>());
         }
         else {
