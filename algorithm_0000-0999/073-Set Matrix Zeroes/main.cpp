dafa12bee8e9d3addf1e1d766fd92923044f0b9d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 01:07:13 +0800

    073: update solution

--- algorithm_0000-0999/073-Set Matrix Zeroes/main.cpp
+++ algorithm_0000-0999/073-Set Matrix Zeroes/main.cpp
@@ -1,30 +1,72 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        size_t rowCnt = matrix.size();
-        size_t colCnt = matrix.empty() ? 0 : matrix.front().size();
+    // Space: O(m + n)
+    void setZeroes1(vector<vector<int>>& matrix) {
+        size_t m = matrix.size();
+        size_t n = matrix.empty() ? 0 : matrix.front().size();
 
-        unordered_set<int> rowIndexes;
-        unordered_set<int> colIndexes;
-        for (size_t row = 0; row < rowCnt; ++row) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                if (matrix[row][col] == 0) {
-                    rowIndexes.insert(row);
-                    colIndexes.insert(col);
+        vector<bool> rows(m, false), cols(n, false);
+        for (size_t r = 0; r < m; ++r) {
+            for (size_t c = 0; c < n; ++c) {
+                if (matrix[r][c] == 0) {
+                    rows[r] = true;
+                    cols[c] = true;
                 }
             }
         }
 
-        for (int row : rowIndexes) {
-            for (size_t col = 0; col < colCnt; ++col) {
-                matrix[row][col] = 0;
+        for (size_t r = 0; r < m; ++r) {
+            for (size_t c = 0; c < n; ++c) {
+                if (rows[r] || cols[c]) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+
+    // Space: O(1)
+    void setZeroes2(vector<vector<int>>& matrix) {
+        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+
+        bool zeroRow0 = false;
+        for (size_t c = 0; c < n && !zeroRow0; ++c) {
+            if (matrix[0][c] == 0) { zeroRow0 = true; }
+        }
+        bool zeroCol0 = false;
+        for (size_t r = 0; r < m && !zeroCol0; ++r) {
+            if (matrix[r][0] == 0) { zeroCol0 = true; }
+        }
+
+        for (size_t r = 1; r < m; ++r) {
+            for (size_t c = 1; c < n; ++c) {
+                if (matrix[r][c] == 0) {
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
+                }
+            }
+        }
+        for (size_t r = 1; r < m; ++r) {
+            for (size_t c = 1; c < n; ++c) {
+                if (matrix[r][0] == 0 || matrix[0][c] == 0) {
+                    matrix[r][c] = 0;
+                }
             }
         }
 
-        for (int col : colIndexes) {
-            for (size_t row = 0; row < rowCnt; ++row) {
-                matrix[row][col] = 0;
+        if (zeroRow0) {
+            for (int c = 0; c < n; ++c) {
+                matrix[0][c] = 0;
             }
         }
+        if (zeroCol0) {
+            for (int r = 0; r < m; ++r) {
+                matrix[r][0] = 0;
+            }
+        }
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        //setZeroes1(matrix);
+        setZeroes2(matrix);
     }
 };
