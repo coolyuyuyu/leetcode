fb6fa4284d7cfb0a34542988b787ebf6052e4c50
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 21:53:30 +0800

    163: update solution

--- algorithm_0000-0999/163-Missing Ranges/main.cpp
+++ algorithm_0000-0999/163-Missing Ranges/main.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
-        int n = nums.size();
-
         vector<vector<int>> ret;
-        for (int i = 0; i <= n; ++i) {
-            int pre = (i == 0 ? (lower) : nums[i - 1] + 1);
-            int cur = (i == n ? (upper) : nums[i] - 1);
-            if (pre <= cur) {
-                ret.push_back({pre, cur});
+        for (int i = 0, n = nums.size(); i <= n; ++i) {
+            int lft = (i == 0 ? lower : nums[i - 1] + 1);
+            int rht = (i == n ? upper : nums[i] - 1);
+            if (lft <= rht) {
+                ret.push_back({lft, rht});
             }
         }
 
