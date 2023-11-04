4db83987fe92d67b972160a3aa11fe2bf4491413
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-05 01:04:57 +0800

    002: update solution

--- algorithm_0000-0999/002-Add Two Numbers/main.cpp
+++ algorithm_0000-0999/002-Add Two Numbers/main.cpp
@@ -11,29 +11,28 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* pHead = nullptr;
-        ListNode** ppNode = &pHead;
+        ListNode* head = l1;
+        ListNode** ppCur = &head;
         bool carry = false;
         while (l1 || l2 || carry) {
-            int digit = 0;
-            if (carry) {
-                digit += 1;
-            }
-            if(l1) {
-                digit += l1->val;
+            int d = 0;
+            if (l1) {
+                d += l1->val;
                 l1 = l1->next;
             }
             if (l2) {
-                digit += l2->val;
+                d += l2->val;
                 l2 = l2->next;
             }
+            if (carry) {
+                d += 1;
+            }
+            carry = (d >= 10);
 
-            *ppNode = new ListNode(digit % 10);
-            ppNode = &((*ppNode)->next);
-
-            carry = (10 <= digit);
+            *ppCur = new ListNode(d % 10);
+            ppCur = &((*ppCur)->next);
         }
 
-        return pHead;
+        return head;
     }
 };
