2646eecd1405a3e58c6fadf2c920be6f5c40dfec
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-04 17:53:25 +0800

    1396: update solution

--- algorithm_1000-1999/1396-Design Underground System/main.cpp
+++ algorithm_1000-1999/1396-Design Underground System/main.cpp
@@ -1,26 +1,28 @@
 class UndergroundSystem {
 public:
-    UndergroundSystem() {}
+    UndergroundSystem() {
+
+    }
 
     void checkIn(int id, string stationName, int t) {
-        m_inRecords[id] = {stationName, t};
+        m_startRecords[id] = {stationName, t};
     }
 
     void checkOut(int id, string stationName, int t) {
-        const auto& [startStation, inTime] = m_inRecords[id];
-        auto& [travelTime, travelCnt] = m_statistics[startStation + "->" + stationName];
-        travelTime += (t - inTime);
-        travelCnt += 1;
+        const auto& [startStation, startTime] = m_startRecords[id];
+        auto& [sum, cnt] = m_statistics[startStation + ">" + stationName];
+        sum += t - startTime;
+        cnt += 1;
     }
 
     double getAverageTime(string startStation, string endStation) {
-        const auto& [travelTime, travelCnt] = m_statistics[startStation + "->" + endStation];
-        return static_cast<double>(travelTime) / travelCnt;
+        const auto& [sum, cnt] = m_statistics[startStation + ">" + endStation];
+        return sum * 1.0 / cnt;
     }
 
 private:
-    unordered_map<int, pair<string, int>> m_inRecords;
-    unordered_map<string, pair<int, int>> m_statistics;
+    unordered_map<int, pair<string, int>> m_startRecords; // id -> <in station, in time>
+    unordered_map<string, pair<int, int>> m_statistics; // in to out station -> <sum, cnt>
 };
 
 /**
