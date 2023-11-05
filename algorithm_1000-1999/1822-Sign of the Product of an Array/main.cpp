074b8ed7cac5a3362c750b8b95af54e8fd49f787
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-06 00:22:07 +0800

    1822: update solution

--- algorithm_1000-1999/1822-Sign of the Product of an Array/main.cpp
+++ algorithm_1000-1999/1822-Sign of the Product of an Array/main.cpp
@@ -3,13 +3,10 @@ public:
     int arraySign(vector<int>& nums) {
         bool plus = true;
         for (int num : nums) {
-            if (num == 0) {
-                return 0;
-            }
-
-            plus ^= !(0 < num);
+            if (num == 0) { return 0; }
+            plus ^= !(num > 0);
         }
 
-        return plus ? 1 : -1;
+        return (plus ? 1 : -1);
     }
 };
