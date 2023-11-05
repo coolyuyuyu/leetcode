4db1e83892f08195a5844a6c868fd30bca61a67d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-06 00:24:26 +0800

    445: update solution

--- algorithm_0000-0999/445-Add Two Numbers II/main.cpp
+++ algorithm_0000-0999/445-Add Two Numbers II/main.cpp
@@ -11,29 +11,23 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (!head) {
-            return nullptr;
+        ListNode* pre = nullptr;
+        for (ListNode* cur = head; cur;) {
+            ListNode* nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
         }
 
-        ListNode** ppHead = &head;
-        ListNode* pNode = *ppHead;
-        while (pNode->next) {
-            ListNode* pTmp = pNode->next;
-            pNode->next = pTmp->next;
-            pTmp->next = *ppHead;
-            *ppHead = pTmp;
-        }
-
-        return head;
+        return pre;
     }
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1 = reverseList(l1);
-        l2 = reverseList(l2);
+        l1 = reverseList(l1), l2 = reverseList(l2);
 
-        ListNode* pHead = nullptr;
-        ListNode** ppCur = &pHead;
-        for (int carry = 0; l1 || l2 || carry; ppCur = &((*ppCur)->next), carry /= 10) {
+        ListNode* head = nullptr;
+        ListNode** ppCur = &head;
+        for (int carry = 0; l1 || l2 || carry; carry /= 10, ppCur = &((*ppCur)->next)) {
             if (l1) {
                 carry += l1->val;
                 l1 = l1->next;
@@ -42,10 +36,9 @@ public:
                 carry += l2->val;
                 l2 = l2->next;
             }
-
             *ppCur = new ListNode(carry % 10);
         }
 
-        return reverseList(pHead);
+        return reverseList(head);
     }
 };
