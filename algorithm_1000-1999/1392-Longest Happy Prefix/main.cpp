8819d6bc8c76243514fddee72642e995df55fe73
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-08 00:10:45 +0800

    1392: update solution

--- algorithm_1000-1999/1392-Longest Happy Prefix/main.cpp
+++ algorithm_1000-1999/1392-Longest Happy Prefix/main.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     string longestPrefix(string s) {
+        size_t n = s.size();
+
         // lps[i]: the maximum value k such that s[0:k-1] == s[i-k+1:i]
-        vector<size_t> lps(s.size());
+        size_t lps[n];
         lps[0] = 0;
-        for (size_t i = 1; i < s.size(); ++i) {
+
+        for (size_t i = 1; i < n; ++i) {
             size_t j = lps[i - 1];
             while (0 < j && s[j] != s[i]) {
                 j = lps[j - 1];
@@ -12,6 +15,6 @@ public:
             lps[i] = j + (s[j] == s[i] ? 1 : 0);
         }
 
-        return s.substr(0, lps[s.size() - 1]);
+        return s.substr(0, lps[n - 1]);
     }
 };
