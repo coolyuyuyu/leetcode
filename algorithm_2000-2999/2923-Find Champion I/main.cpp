2172d56ee822d21b86064b3055ef89ad27f8dc47
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-07 14:21:37 +0800

    2923: initial solution

---
+++ algorithm_2000-2999/2923-Find Champion I/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = 0; j < n; ++j) {
+                if (i == j) { continue; }
+                if (grid[i][j] == 1) {
+                    ++cnt;
+                }
+            }
+            if (cnt == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
