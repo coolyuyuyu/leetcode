32f58716bae829b0eeaf4e6e4c4f4f21c0f2d043
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-09 13:21:16 +0800

    2849: update solution

--- algorithm_2000-2999/2849-Determine if a Cell Is Reachable at a Given Time/main.cpp
+++ algorithm_2000-2999/2849-Determine if a Cell Is Reachable at a Given Time/main.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int dx = abs(sx - fx), dy = abs(sy - fy);
+        int dx = abs(fx - sx), dy = abs(fy - sy);
         if (dx == 0 && dy == 0 && t == 1) {
             return false;
         }
-
         return std::max(dx, dy) <= t;
     }
 };
