10f796b2d648d38d6505c0abaa5f8c682c449d7b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-09 13:21:07 +0800

    459: update solution

--- algorithm_0000-0999/459-Repeated Substring Pattern/main.cpp
+++ algorithm_0000-0999/459-Repeated Substring Pattern/main.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        size_t n = s.size();
+        int n = s.size();
 
-        // lps[i]: the maximum value k such that s[0:k-1] == s[i-k+1:i]
-        vector<size_t> lps(n);
+        // lps[i]: the longest length k such that s[0:k-1] == s[i-k+1:i]
+        int lps[n];
         lps[0] = 0;
-        for (size_t i = 1; i < n; ++i) {
-            size_t j = lps[i - 1];
+        for (int i = 1; i < n; ++i) {
+            int j = lps[i - 1];
             while (0 < j && s[j] != s[i]) {
                 j = lps[j - 1];
             }
-            lps[i] = j + (s[i] == s[j] ? 1 : 0);
+            lps[i] = j + (s[j] == s[i] ? 1 : 0);
         }
 
-        return 0 < lps[n - 1] && (n % (n - lps[n - 1])) == 0;
+        return 0 < lps[n - 1] && n % (n - lps[n - 1]) == 0;
     }
 };
