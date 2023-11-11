cdddd5acabaa2d760ba3e2085e1d0a62b290f438
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-12 03:15:54 +0800

    1897: initial solution

---
+++ algorithm_1000-1999/1897-Redistribute Characters to Make All Strings Equal/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        int cnts[26];
+        std::fill(cnts, cnts + 26, 0);
+
+        for (const string& word : words) {
+            for (char c : word) {
+                ++cnts[c - 'a'];
+            }
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (cnts[i] % words.size() != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
