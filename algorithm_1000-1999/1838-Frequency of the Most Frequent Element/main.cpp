e25693d34ad91cbfc98b999fc915a0f0c0c70045
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-19 02:18:57 +0800

    1838: initial solution

---
+++ algorithm_1000-1999/1838-Frequency of the Most Frequent Element/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        std::sort(nums.begin(), nums.end());
+
+        int ret = 1;
+
+        long cnt = 0;
+        for (int lft = 0, rht = 1; rht < n; ++rht) {
+            cnt += (rht - lft) * 1L * (nums[rht] - nums[rht - 1]);
+            while (cnt > k) {
+                cnt -= (nums[rht] - nums[lft]);
+                ++lft;
+            }
+
+            ret = std::max(ret, rht - lft + 1);
+        }
+
+        return ret;
+    }
+};
