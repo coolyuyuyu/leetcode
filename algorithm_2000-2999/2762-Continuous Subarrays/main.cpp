68c87d528c4bbbeab17366c021b3dce916852b51
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-20 01:41:05 +0800

    2762: update solution

--- algorithm_2000-2999/2762-Continuous Subarrays/main.cpp
+++ algorithm_2000-2999/2762-Continuous Subarrays/main.cpp
@@ -3,34 +3,34 @@ public:
     long long continuousSubarrays(vector<int>& nums) {
         int n = nums.size();
 
-        deque<int> maxDeque, minDeque;
-        auto check = [&](int rht) {
-            int maxVal = std::max(nums[rht], maxDeque.empty() ? INT_MIN : nums[maxDeque.front()]);
-            int minVal = std::min(nums[rht], minDeque.empty() ? INT_MAX : nums[minDeque.front()]);
-            return (maxVal - minVal) <= 2;
+        deque<int> dqMax, dqMin;
+
+        std::function<bool(int)> check = [&](int rht) {
+            if (dqMax.empty()) { return true; }
+            return std::max(nums[dqMax.front()], nums[rht]) - std::min(nums[dqMin.front()], nums[rht]) <= 2;
         };
 
         long long ret = 0;
         for (int lft = 0, rht = 0; lft < n; ++lft) {
             for (; rht < n && check(rht); ++rht) {
-                while (!maxDeque.empty() && nums[maxDeque.back()] < nums[rht]) {
-                    maxDeque.pop_back();
+                while (!dqMax.empty() && nums[dqMax.back()] <= nums[rht]) {
+                    dqMax.pop_back();
                 }
-                maxDeque.push_back(rht);
+                dqMax.push_back(rht);
 
-                while (!minDeque.empty() &&  nums[minDeque.back()] > nums[rht]) {
-                    minDeque.pop_back();
+                while (!dqMin.empty() && nums[dqMin.back()] >= nums[rht]) {
+                    dqMin.pop_back();
                 }
-                minDeque.push_back(rht);
+                dqMin.push_back(rht);
             }
 
             ret += (rht - lft);
 
-            if (!maxDeque.empty() && maxDeque.front() <= lft) {
-                maxDeque.pop_front();
+            if (!dqMax.empty() && dqMax.front() == lft) {
+                dqMax.pop_front();
             }
-            if (!minDeque.empty() && minDeque.front() <= lft) {
-                minDeque.pop_front();
+            if (!dqMin.empty() && dqMin.front() == lft) {
+                dqMin.pop_front();
             }
         }
 
