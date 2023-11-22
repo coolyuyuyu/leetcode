6d6e7b8be241d3a737c20a6dab34354d4c02b01c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-23 01:18:03 +0800

    421: update solution

--- algorithm_0000-0999/421-Maximum XOR of Two Numbers in an Array/main.cpp
+++ algorithm_0000-0999/421-Maximum XOR of Two Numbers in an Array/main.cpp
@@ -1,54 +1,62 @@
+class Trie {
+public:
+    Trie()
+        : m_root(new Node()) {
+    }
+
+    void add(int num) {
+        Node** ppNode = &m_root;
+        for (int i = 31; 0 < i--;) {
+            ppNode = &((*ppNode)->nexts[(num >> i) & 1]);
+            if (*ppNode == nullptr) {
+                *ppNode = new Node();
+            }
+        }
+    }
+
+    int findMaximumXOR(int num) {
+        Node* node = m_root;
+
+        int ret = 0;
+        for (int i = 31; 0 < i--;) {
+            int b = (num >> i) & 1;
+            if (node->nexts[b ^ 1]) {
+                ret += (1 << i);
+                node = node->nexts[b ^ 1];
+            }
+            else {
+                node = node->nexts[b];
+            }
+        }
+
+        return ret;
+    }
+
+private:
+    struct Node {
+        Node* nexts[2];
+
+        Node() {
+            nexts[0] = nexts[1] = nullptr;
+        }
+    };
+
+    Node* m_root;
+};
+
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        TrieNode* pRoot = buildTrie(nums);
-
-        int ans = 0;
+        Trie trie;
         for (int num : nums) {
-            int val = 0;
-            TrieNode* pNode = pRoot;
-            for (int i = 31; 0 <= i; --i) {
-                int bit = (num >> i) & 1;
-                if (pNode->childs[bit ^ 1]) {
-                    pNode = pNode->childs[bit ^ 1];
-                    val += (1 << i);
-                }
-                else {
-                    pNode = pNode->childs[bit];
-                }
-            }
-
-            ans = max(ans, val);
+            trie.add(num);
         }
 
-        return ans;
-    }
-private:
-    class TrieNode {
-    public:
-        TrieNode()
-            : childs(2, nullptr) {
-        }
-
-        vector<TrieNode*> childs;
-    };
-
-    TrieNode* buildTrie(const vector<int>& nums) {
-        TrieNode* pRoot = new TrieNode();
+        int ret = 0;
         for (int num : nums) {
-            insertTrie(pRoot, num);
+            ret = std::max(ret, trie.findMaximumXOR(num));
         }
 
-        return pRoot;
-    }
-
-    void insertTrie(TrieNode* pRoot, int num) {
-        TrieNode** ppNode = &pRoot;
-        for (int i = 31; 0 <= i; --i) {
-            ppNode = &((*ppNode)->childs[(num >> i) & 1]);
-            if (*ppNode == nullptr) {
-                *ppNode = new TrieNode();
-            }
-        }
+        return ret;
     }
 };
