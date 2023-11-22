a1e97dd7762f2a9c0cca7ee149ebb061f620defc
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-22 19:39:47 +0800

    624: update solution

--- algorithm_0000-0999/624-Maximum Distance in Arrays/main.cpp
+++ algorithm_0000-0999/624-Maximum Distance in Arrays/main.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int maxDist = 0;
-        int minVal = arrays.front().front();
-        int maxVal = arrays.front().back();
-        for (size_t i = 1; i < arrays.size(); ++i) {
-            const vector<int>& vals = arrays[i];
-            maxDist = max(maxDist, max(vals.back() - minVal, maxVal - vals.front()));
-            minVal = min(minVal, vals.front());
-            maxVal = max(maxVal, vals.back());
+        int mn = INT_MAX / 2, mx = INT_MIN / 2;
+        int ret = 0;
+        for (const auto& array : arrays) {
+            ret = std::max({ret, mx - array.front(), array.back() - mn});
+            mn = std::min(mn, array.front());
+            mx = std::max(mx, array.back());
         }
 
-        return maxDist;
-
+        return ret;
     }
 };
