16af2337fecd3885602097c748e9d3bdbd53b5d3
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-25 16:26:08 +0800

    1685: update solution

--- algorithm_1000-1999/1685-Sum of Absolute Differences in a Sorted Array/main.cpp
+++ algorithm_1000-1999/1685-Sum of Absolute Differences in a Sorted Array/main.cpp
@@ -3,20 +3,11 @@ public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n = nums.size();
 
-        int presum[n];
-        std::partial_sum(nums.begin(), nums.end(), presum);
-        std::function<int(int, int)> sum = [&](int lo, int hi) {
-            if (lo > hi) {
-                return 0;
-            }
-            else {
-                return presum[hi] - (lo == 0 ? 0 : presum[lo - 1]);
-            }
-        };
-
         vector<int> ret(n);
-        for (int i = 0; i < n; ++i) {
-            ret[i] = nums[i] * i - sum(0, i - 1) + sum(i + 1, n - 1) - nums[i] * (n - i - 1);
+        ret[0] = std::accumulate(nums.begin(), nums.end(), 0) - nums[0] * n;
+        for (int i = 1; i < n; ++i) {
+            int diff = nums[i] - nums[i - 1];
+            ret[i] = ret[i - 1] + diff * i - diff * (n - i);
         }
 
         return ret;
