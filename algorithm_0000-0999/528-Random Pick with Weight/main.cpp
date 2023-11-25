955ef5403fd1ccca37170ab952b9376e2c5b1ae9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-26 00:00:23 +0800

    528: initial solution

---
+++ algorithm_0000-0999/528-Random Pick with Weight/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    Solution(vector<int>& w)
+        : m_presum(w.size()) {
+        std::partial_sum(w.begin(), w.end(), m_presum.begin());
+    }
+
+    int pickIndex() {
+        int i = std::rand() % m_presum.back();
+        return std::distance(m_presum.begin(), std::upper_bound(m_presum.begin(), m_presum.end(), i));
+    }
+
+private:
+    vector<int> m_presum;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
