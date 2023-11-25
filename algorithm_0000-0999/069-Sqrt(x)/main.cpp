c98f846d734b05085896bbfbe7be4e16708f9d5f
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-26 01:32:45 +0800

    069: update solution

--- algorithm_0000-0999/069-Sqrt(x)/main.cpp
+++ algorithm_0000-0999/069-Sqrt(x)/main.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        // Newton's method
-        long int root = x;
-        while (root * root > x) {
-            root = (root + x / root) / 2;
+    int binarySearch(int x) {
+        int lo = 0, hi = x;
+        while (lo < hi) {
+            int mid = hi - (hi - lo) / 2;
+            if (1LL * mid * mid > x) {
+                hi = mid - 1;
+            }
+            else {
+                lo = mid;
+            }
+        }
+
+        return lo;
+    }
+
+    int newtonMethod(int x) {
+        int root = x;
+        while (1LL * root * root > x) {
+            root = (0LL + root + x / root) / 2;
         }
 
         return root;
+    }
+
+    int mySqrt(int x) {
+        //return binarySearch(x);
+        return newtonMethod(x);
     }
 };
