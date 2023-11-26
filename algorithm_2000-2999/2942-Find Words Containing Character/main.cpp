835219c3eeda11c6452e6ced988023d4f77860fb
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-26 21:25:38 +0800

    2942: initial solution

---
+++ algorithm_2000-2999/2942-Find Words Containing Character/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ret;
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i].find(x) != string::npos) {
+                ret.push_back(i);
+            }
+        }
+
+        return ret;
+    }
+};
