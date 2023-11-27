a5effd1627a5837cb9a98ba6ba7c053b61a5ea9c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-27 14:59:51 +0800

    744: update solution

--- algorithm_0000-0999/744-Find Smallest Letter Greater Than Target/main.cpp
+++ algorithm_0000-0999/744-Find Smallest Letter Greater Than Target/main.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    char nextGreatestLetter_BinarySearch(vector<char>& letters, char target) {
-        size_t lo = 0;
-        size_t hi = letters.size();
+    char bsearch(vector<char>& letters, char target) {
+        int lo = 0, hi = letters.size();
         while (lo < hi) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (target < letters[mid]) {
+            int mid = lo + (hi - lo) / 2;
+            if (letters[mid] > target) {
                 hi = mid;
             }
             else {
@@ -13,20 +12,18 @@ public:
             }
         }
 
-        assert(!letters.empty());
-        return (lo == letters.size() ? letters.front() : letters[lo]);
+        return lo == letters.size() ? letters.front() : letters[lo];
     }
 
-    char nextGreatestLetter_StdLibrary(vector<char>& letters, char target) {
+    char stdlib(vector<char>& letters, char target) {
         auto itr = std::upper_bound(letters.begin(), letters.end(), target);
-        if (itr == letters.end()) {
-            itr = letters.begin();
-        }
+        if (itr == letters.end()) { itr = letters.begin(); }
+
         return *itr;
     }
 
     char nextGreatestLetter(vector<char>& letters, char target) {
-        return nextGreatestLetter_BinarySearch(letters, target);
-        //return nextGreatestLetter_StdLibrary(letters, target);
+        //return bsearch(letters, target);
+        return stdlib(letters, target);
     }
 };
