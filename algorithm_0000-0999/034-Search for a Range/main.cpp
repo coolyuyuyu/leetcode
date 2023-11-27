b8d40d3176c6bcbb5f2b1b2afbb39daf308b57e7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-28 02:51:57 +0800

    034: update solution

--- algorithm_0000-0999/034-Search for a Range/main.cpp
+++ algorithm_0000-0999/034-Search for a Range/main.cpp
@@ -1,14 +1,47 @@
 class Solution {
 public:
+    vector<int> bsearch(vector<int>& nums, int target) {
+        int lo, hi;
+
+        for (lo = 0, hi = nums.size(); lo < hi;) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid;
+            }
+        }
+        if (lo == nums.size() || nums[lo] != target) {
+            return {-1, -1};
+        }
+        int lft = lo;
+
+        for (hi = nums.size() - 1; lo < hi;) {
+            int mid = hi - (hi - lo) / 2;
+            if (nums[mid] <= target) {
+                lo = mid;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        int rht = lo;
+
+        return {lft, rht};
+    }
+
+    vector<int> bsearch_stdlib(vector<int>& nums, int target) {
+        auto itr = std::lower_bound(nums.begin(), nums.end(), target);
+        if (itr == nums.end() || *itr != target) { return {-1, -1}; };
+
+        int l = std::distance(nums.begin(), itr);
+        int r = std::distance(nums.begin(), std::prev(std::upper_bound(itr, nums.end(), target)));
+        return {l, r};
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-       auto itr = std::lower_bound(nums.begin(), nums.end(), target);
-       if (itr == nums.end() || *itr != target) {
-           return {-1, -1};
-       }
-       else {
-           int l = std::distance(nums.begin(), itr);
-           int r = std::distance(nums.begin(), std::upper_bound(itr + 1, nums.end(), target)) - 1;
-           return {l, r};
-       }
+        //return bsearch(nums, target);
+        return bsearch_stdlib(nums, target);
     }
 };
