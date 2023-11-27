d3e6b3672834c487be7db459ecff91ac1b0f9877
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-28 02:52:06 +0800

    935: update solution

--- algorithm_0000-0999/935-Knight Dialer/main.cpp
+++ algorithm_0000-0999/935-Knight Dialer/main.cpp
@@ -3,12 +3,9 @@ public:
     int M = 1e9 + 7;
     
     int knightDialer(int n) {
-        if (n == 1) { return 10; }
-
         // dp[i][j]: how many distinct phone numbers of length i and last digit is j
         int dp[10];
         std::fill(dp, dp + 10, 1);
-        dp[5] = 0;
 
         for (int i = 2; i <= n; ++i) {
             int tmp[10];
@@ -19,6 +16,7 @@ public:
             dp[2] = (tmp[7] + tmp[9]) % M;
             dp[3] = (tmp[4] + tmp[8]) % M;
             dp[4] = ((tmp[0] + tmp[3]) % M + tmp[9])% M;
+            dp[5] = 0;
             dp[6] = ((tmp[0] + tmp[1]) % M + tmp[7])% M;
             dp[7] = (tmp[2] + tmp[6]) % M;
             dp[8] = (tmp[1] + tmp[3]) % M;
