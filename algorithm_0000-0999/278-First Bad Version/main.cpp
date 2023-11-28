df1d78c8b65083f8e09fcf2f8c26f219f9324529
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-28 23:32:25 +0800

    278: update solution

--- algorithm_0000-0999/278-First Bad Version/main.cpp
+++ algorithm_0000-0999/278-First Bad Version/main.cpp
@@ -1,20 +1,20 @@
-// Forward declaration of isBadVersion API.
-bool isBadVersion(int version);
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int bgn = 0;
-        int end = n;
-        while (bgn + 1 < end) {
-            int mid = bgn + (end - bgn) / 2;
+        int lo = 1, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
             if (isBadVersion(mid)) {
-                end = mid;
+                hi = mid;
             }
             else {
-                bgn = mid;
+                lo = mid + 1;
             }
         }
-        return end;
+
+        return lo;
     }
 };
