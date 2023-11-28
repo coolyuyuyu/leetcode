69122a2d0c987772e870f1a39167b926697d568b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-11-28 23:29:02 +0800

    702: update solution

--- algorithm_0000-0999/702-Search in a Sorted Array of Unknown Size/main.cpp
+++ algorithm_0000-0999/702-Search in a Sorted Array of Unknown Size/main.cpp
@@ -1,38 +1,34 @@
-// Forward declaration of ArrayReader class.
-class ArrayReader;
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *   public:
+ *     int get(int index);
+ * };
+ */
 
 class Solution {
 public:
-    int searchHelper(const ArrayReader& reader, int target) {
-        size_t behind = 1;
-        while (reader.get(behind) < target) {
-            behind <<= 1;
+    int search(const ArrayReader& reader, int target) {
+        int hi = 1;
+        while (reader.get(hi) < target) {
+            hi <<= 1;
         }
-
-        size_t lo = behind / 2;
-        size_t hi = behind;
+        int lo = hi >> 1;
 
         while (lo <= hi) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (reader.get(mid) == target) {
+            int mid = lo + (hi - lo) / 2;
+            if (reader.get(mid) < target) {
+                lo = mid + 1;
+            }
+            else if (reader.get(mid) == target) {
                 return mid;
             }
-            else if (target < reader.get(mid)) {
-                if (mid == 0) {
-                    return -1;
-                }
-                hi = mid - 1;
-            }
             else {
-                assert(reader.get(mid) < target);
-                lo = mid + 1;
+                hi = mid - 1;
             }
         }
 
         return -1;
-    }
-
-    int search(const ArrayReader& reader, int target) {
-        return searchHelper(reader, target);
     }
 };
