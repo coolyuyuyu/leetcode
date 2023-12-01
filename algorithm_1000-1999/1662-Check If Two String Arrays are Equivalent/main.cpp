083d2e2cf2076a6f1201047dbe362bf9272e1834
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-01 16:30:20 +0800

    1662: update solution

--- algorithm_1000-1999/1662-Check If Two String Arrays are Equivalent/main.cpp
+++ algorithm_1000-1999/1662-Check If Two String Arrays are Equivalent/main.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        size_t i = 0, j = 0;
-        for (size_t a = 0, b = 0; i < word1.size() && j < word2.size();) {
-            while (i < word1.size() && word1[i].size() <= a) {
-                ++i;
-                a = 0;
-            }
-            while (j < word2.size() && word2[j].size() <= b) {
-                ++j;
-                b = 0;
-            }
-
-            if (i < word1.size() && j < word2.size() && word1[i][a] != word2[j][b]) {
+        int i1 = 0, i2 = 0;
+        for (int j1 = 0, j2 = 0; i1 < word1.size() && i2 < word2.size();) {
+            if (word1[i1][j1] != word2[i2][j2]) {
                 return false;
             }
-            ++a, ++b;
+
+            if (++j1 == word1[i1].size()) {
+                ++i1;
+                j1 = 0;
+            }
+            if (++j2 == word2[i2].size()) {
+                ++i2;
+                j2 = 0;
+            }
         }
 
-        return word1.size() <= i && word2.size() <= j;
+        return i1 == word1.size() && i2 == word2.size();
     }
 };
