84bea246f77af812ee30c3bbb71a2b686376f6b1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-02 02:02:07 +0800

    089: update soltution

--- algorithm_0000-0999/089-Gray Code/main.cpp
+++ algorithm_0000-0999/089-Gray Code/main.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
-    vector<int> grayCode(int n) {
-        vector<int> ret = {0, 1};
-        for (int i = 1; i < n; ++i) {
+    vector<int> math1(int n) {
+        vector<int> ret = {0};
+        for (int i = 0; i < n; ++i) {
             for (int j = ret.size(); 0 < j--;) {
                 ret.push_back(ret[j] | (1 << i));
             }
         }
+
         return ret;
+    }
+
+    vector<int> math2(int n) {
+        vector<int> ret(1 << n);
+        for (int i = 1; i < ret.size(); ++i) {
+            ret[i] = i ^ (i >> 1);
+        }
+
+        return ret;
+    }
+
+    vector<int> grayCode(int n) {
+        //return math1(n);
+        return math2(n);
     }
 };
