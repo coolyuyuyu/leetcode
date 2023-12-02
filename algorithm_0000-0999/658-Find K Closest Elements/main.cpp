5457613891dc3092fb0019b5f3e5873f86b54ebd
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-03 01:54:07 +0800

    658: update solution

--- algorithm_0000-0999/658-Find K Closest Elements/main.cpp
+++ algorithm_0000-0999/658-Find K Closest Elements/main.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-    size_t searchInsert(vector<int>& nums, int target) {
-        size_t lo = 0;
-        size_t hi = nums.size();
-
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // binary search the start index of k-length window
+        int lo = 0, hi = arr.size() - k;
         while (lo < hi) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (target <= nums[mid]) {
-                hi = mid;
-            }
-            else {
-                assert(nums[mid] < target);
+            int mid = lo + (hi - lo) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
                 lo = mid + 1;
             }
-        }
-
-        assert(lo == hi);
-        return hi;
-    }
-
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        size_t index = searchInsert(arr, x);
-        size_t first = (k <= index ? index - k : 0);
-        size_t last = (arr.size() <= index + k ? arr.size() : index + k);
-        while ((last - first) > k) {
-            if (x - arr[first] <= arr[last - 1] - x) {
-                last -= 1;
-            }
             else {
-                first += 1;
+                hi = mid;
             }
         }
 
-        vector<int> ans;
-        for (size_t i = first; i < last; ++i) {
-            ans.push_back(arr[i]);
-        }
-
-        return ans;
+        return vector<int>(arr.begin() + lo, arr.begin() + lo + k);
     }
 };
