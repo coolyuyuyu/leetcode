21a16ef0a20b260dbb5cadab7396d2100fe2df4d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-03 19:24:01 +0800

    1266: update solution

--- algorithm_1000-1999/1266-Minimum Time Visiting All Points/main.cpp
+++ algorithm_1000-1999/1266-Minimum Time Visiting All Points/main.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int secs = 0;
-        for (size_t i = 1; i < points.size(); ++i) {
-            secs += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
+        int ret = 0;
+        for (int i = 1; i < points.size(); ++i) {
+            int diffX = std::abs(points[i][0] - points[i - 1][0]);
+            int diffY = std::abs(points[i][1] - points[i - 1][1]);
+            ret += std::max(diffX, diffY);
         }
 
-        return secs;
+        return ret;
     }
 };
