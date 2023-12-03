0115609396e2b64a13278f7919be9e91935aaf6c
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-03 19:25:12 +0800

    2300: update solution

--- algorithm_2000-2999/2300-Successful Pairs of Spells and Potions/main.cpp
+++ algorithm_2000-2999/2300-Successful Pairs of Spells and Potions/main.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int m = spells.size(), n = potions.size();
 
-        vector<int> indexes(m);
-        std::iota(indexes.begin(), indexes.end(), 0);
-        std::sort(indexes.begin(), indexes.end(), [&spells](int i1, int i2){ return spells[i1] > spells[i2]; });
+        int indexes[m];
+        std::iota(indexes, indexes + m, 0);
+        std::sort(indexes, indexes + m, [&](int i1, int i2){ return spells[i1] > spells[i2]; });
 
         std::sort(potions.begin(), potions.end());
 
         vector<int> ret(m);
         for (int i = 0, j = 0; i < m; ++i) {
             long long spell = spells[indexes[i]];
-            while (j < n && (spell * potions[j]) < success) {
+            while (j < n && spell * potions[j] < success) {
                 ++j;
             }
             ret[indexes[i]] = n - j;
