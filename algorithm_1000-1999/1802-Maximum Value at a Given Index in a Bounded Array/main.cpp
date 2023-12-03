f07c5bd547a7f7d16d0127ebcbfcd59cd2ad76ad
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-03 19:24:37 +0800

    1802: update solution

--- algorithm_1000-1999/1802-Maximum Value at a Given Index in a Bounded Array/main.cpp
+++ algorithm_1000-1999/1802-Maximum Value at a Given Index in a Bounded Array/main.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int maxValue(int n, int index, int maxSum) {
-        std::function<long(long, long)> sumOneSide = [](long len, long lastNum) {
-            long numOne = std::max(len - lastNum, 0L);
-            if (len > lastNum) {
-                len = lastNum;
-            }
-
-            return (lastNum + (lastNum - len + 1)) * len / 2 + numOne;
+        std::function<long(int, int)> sumOneSide = [](int len, int maxNum) {
+            int numOnes = std::max(len - maxNum, 0);
+            len -= numOnes;
+            return 1L * (maxNum - len + 1 + maxNum) * len / 2 + numOnes;
         };
-
-        std::function<long(int)> sumArr = [&n, &index, &maxSum, &sumOneSide](int target) {
-            return sumOneSide(index + 1, target) + sumOneSide(n - index, target) - target;
+        std::function<bool(int)> canConstruct = [&](int target) {
+            long sum = sumOneSide(index + 1, target) + sumOneSide(n - index, target) - target;
+            return sum <= maxSum;
         };
 
         int lo = 1, hi = maxSum;
         while (lo < hi) {
             int mid = hi - (hi - lo) / 2;
-            if (sumArr(mid) <= maxSum) {
+            if (canConstruct(mid)) {
                 lo = mid;
             }
             else {
