266013fc4086c8a334627d8edc61151290ff0f81
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-05 01:18:04 +0800

    1539: update solution

--- algorithm_1000-1999/1539-Kth Missing Positive Number/main.cpp
+++ algorithm_1000-1999/1539-Kth Missing Positive Number/main.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
-    // O(n)
-    int linear(const vector<int>& arr, int k) {
-        for (int val : arr) {
-            if (val <= k) {
-                ++k;
+    // Time: O(logn)
+    int bsearch(vector<int>& arr, int k) {
+        int lo = 0, hi = arr.size();
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                lo = mid + 1;
             }
             else {
-                return k;
+                hi = mid;
             }
+        }
+
+        return lo + k;
+    }
+
+    // Time: O(n)
+    int lsearch(vector<int>& arr, int k) {
+        for (int num : arr) {
+            if (k < num) {
+                break;
+            }
+            ++k;
         }
 
         return k;
     }
 
-    // O(logn)
-    int bsearch(const vector<int>& arr, int k) {
-        // lft, ...., mid, ..., rht
-        // S: [1, 2, ..., mid-1]
-        // T: [all arr elements which is less then mid]
-        // if mid == kth missing element
-        //     => len(S) - len(T) = k - 1
-
-        int lft = 1, rht = arr.back() + k;
-        while (lft < rht) {
-            int mid = rht - (rht - lft) / 2;
-            int s = mid - 1;
-            int t = std::distance(arr.begin(), std::lower_bound(arr.begin(), arr.end(), mid));
-            if ((s - t) <= (k - 1)) {
-                lft = mid;
-            }
-            else {
-                rht = mid - 1;
-            }
-        }
-
-        return lft;
-    }
-
     int findKthPositive(vector<int>& arr, int k) {
-        //return linear(arr, k);
-        return bsearch(arr, k);
+        //return bsearch(arr, k);
+        return lsearch(arr, k);
     }
 };
