9c6057173ee25a0bf44f70f9bc928d765e12c3a1
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-05 01:17:33 +0800

    033: update solution

--- algorithm_0000-0999/033-Search in Rotated Sorted Array/main.cpp
+++ algorithm_0000-0999/033-Search in Rotated Sorted Array/main.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    int f1(const vector<int>& nums, int target) {
-        for (size_t lo = 0, hi = nums.size() - 1; lo <= hi;) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        for (int lo = 0, hi = nums.size() - 1; lo <= hi;) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) { return mid; }
             else if (nums[lo] <= nums[mid]) {
                 if (nums[lo] <= target && target < nums[mid]) {
-                    if (mid == 0) {
-                        return -1;
-                    }
                     hi = mid - 1;
                 }
                 else {
@@ -18,47 +14,15 @@ public:
                 }
             }
             else {
-                assert(nums[mid] <= nums[hi]);
                 if (nums[mid] < target && target <= nums[hi]) {
                     lo = mid + 1;
                 }
                 else {
-                    if (mid == 0) {
-                        return -1;
-                    }
                     hi = mid - 1;
                 }
             }
         }
 
         return -1;
-    }
-
-    int f2(const vector<int>& nums, int target) {
-        for (size_t lo = 0, hi = nums.size() - 1; lo <= hi;) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) { return mid; }
-            if ((nums[0] <= nums[mid]) == (nums[0] <= target)) {
-                if (nums[mid] < target) {
-                    lo = mid + 1;
-                }
-                else {
-                    hi = mid - 1;
-                }
-            }
-            else if (nums[0] <= target) {
-                hi = mid - 1;
-            }
-            else {
-                lo = mid + 1;
-            }
-        }
-
-        return -1;
-    }
-
-    int search(vector<int>& nums, int target) {
-        //return f1(nums, target);
-        return f2(nums, target);
     }
 };
