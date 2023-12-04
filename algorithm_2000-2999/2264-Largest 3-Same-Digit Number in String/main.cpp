f839921a5d9ab9549708da456c35f8140b576c91
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-04 14:16:31 +0800

    2264: update solution

--- algorithm_2000-2999/2264-Largest 3-Same-Digit Number in String/main.cpp
+++ algorithm_2000-2999/2264-Largest 3-Same-Digit Number in String/main.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        int n = num.size();
-        int ret = -1;
-        for (int i = 0; i + 2 < n; ++i) {
-            if (num[i] == num[i + 1] && num[i + 1] == num[i + 2]) {
-                ret = std::max(ret, num[i] - '0');
+        char ret = '0' - 1;
+        for (int i = 2; i < num.size();) {
+            if (num[i - 2] == num[i - 1]) {
+                if (num[i - 1] == num[i]) {
+                    ret = std::max(ret, num[i]);
+                    i += 3;
+                }
+                else {
+                    i += 2;
+                }
+            }
+            else {
+                i += 1;
             }
         }
 
-        if (ret == -1) {
-            return "";
-        }
-        else {
-            return string(3, '0' + ret);
-        }
+        return '0' <= ret ? string(3, ret) : "";
     }
 };
