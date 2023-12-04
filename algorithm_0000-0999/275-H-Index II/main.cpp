ed6d1b83dba0cd24317267a439b7f4887b467d5d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-05 01:17:43 +0800

    275: update solution

--- algorithm_0000-0999/275-H-Index II/main.cpp
+++ algorithm_0000-0999/275-H-Index II/main.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int lft = 0, rht = citations.size();
-        while (lft < rht) {
-            int mid = lft + (rht - lft) / 2;
+        int lo = 0, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
             if (citations[mid] >= n - mid) {
-                rht = mid;
+                hi = mid;
             }
             else {
-                lft = mid + 1;
+                lo = mid + 1;
             }
         }
 
-        return n - rht;
+        return n - lo;
     }
 };
