1f62e56ba53217e4122837a308f80995b3ad7f7b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-05 01:17:38 +0800

    153: update solution

--- algorithm_0000-0999/153-Find Minimum in Rotated Sorted Array/main.cpp
+++ algorithm_0000-0999/153-Find Minimum in Rotated Sorted Array/main.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        for (size_t lo = 0, hi = nums.size() - 1; lo < hi;) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (nums[lo] < nums[hi]) {
-                return nums[lo];
-            }
-            else if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            }
-            else if (nums[lo] > nums[mid]) {
-                hi = mid;
+        if (nums.front() < nums.back()) {
+            return nums[0];
+        }
+
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[0] <= nums[mid]) {
+                lo = mid + 1;
             }
             else {
-                lo = mid + 1;
+                hi = mid;
             }
         }
 
-        assert(nums.size() == 1);
-        return nums[0];
+        return nums[lo];
     }
 };
