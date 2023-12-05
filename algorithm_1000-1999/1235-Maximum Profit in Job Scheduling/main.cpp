b11d8cdb63a8e7588f3744dc71c406cf823701a6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-05 21:40:06 +0800

    1235: update solution

--- algorithm_1000-1999/1235-Maximum Profit in Job Scheduling/main.cpp
+++ algorithm_1000-1999/1235-Maximum Profit in Job Scheduling/main.cpp
@@ -8,16 +8,17 @@ public:
         for (int i = 0; i < n; ++i) {
             job[i + 1] = {startTime[i], endTime[i], profit[i]};
         }
-        std::sort(job, job + n + 1, [&](const auto& job1, const auto& job2){ return std::get<1>(job1) < std::get<1>(job2); });
+        std::sort(job + 1, job + n + 1, [&](const auto& j1, const auto& j2){ return std::get<1>(j1) < std::get<1>(j2); });
 
         // dp[i]: the maximum profit you can take from job[1:i]
         int dp[n + 1];
-        dp[0] = 0;
+        dp[0] = std::get<2>(job[0]);
         for (int i = 1; i <= n; ++i) {
             auto itr = std::upper_bound(
-                job, job + i,
+                job,
+                job + i,
                 tuple<int, int, int>{0, std::get<0>(job[i]), 0},
-                [&](const auto& job1, const auto& job2){ return std::get<1>(job1) < std::get<1>(job2); });
+                [&](const auto& j1, const auto& j2){ return std::get<1>(j1) < std::get<1>(j2); });
             int j = std::distance(job, itr) - 1;
             dp[i] = std::max(dp[i - 1], dp[j] + std::get<2>(job[i]));
         }
