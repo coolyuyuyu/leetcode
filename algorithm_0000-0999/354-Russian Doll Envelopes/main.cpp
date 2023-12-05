1d7ec907916a7bd010e7ca35d53ec8855b4a976e
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-05 21:39:35 +0800

    354: update solution

--- algorithm_0000-0999/354-Russian Doll Envelopes/main.cpp
+++ algorithm_0000-0999/354-Russian Doll Envelopes/main.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    // Time: O(n^2)
+    // Time: O(n^2), TLE
     int dynamicProgramming(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
 
-        std::sort(
-            envelopes.begin(), envelopes.end(),
-            [](const auto& e1, const auto& e2) {
-                return e1[0] < e2[0];
-            });
+        std::sort(envelopes.begin(), envelopes.end(), [&](const auto& e1, const auto& e2){ return e1[0] < e2[0]; });
 
-        // dp[i]: the maximum number of envelopes from envelopes[0:i] and envelopes[i] is the largest
+        // dp[i]: the maximum number of envelopes you can Russian doll from envelopes[0:i] and envelopes[i] is the lagrest of doll
         int dp[n];
-        for (int i = 0; i < n; ++i) {
-            dp[i] = 1;
-            for (int j = 0; j < i; ++j) {
-                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
-                    dp[i] = std::max(dp[i], dp[j] + 1);
-                }
-            }
-        }
+        std::fill(dp, dp + n, 1);
 
         int ret = 0;
         for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i && envelopes[j][0] < envelopes[i][0]; ++j) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    dp[i] = std::max(dp[i], dp[j] + 1);
+                }
+            }
             ret = std::max(ret, dp[i]);
         }
 
@@ -33,20 +27,20 @@ public:
     int greedy(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
 
-        std::sort(
-            envelopes.begin(), envelopes.end(),
-            [](const auto& e1, const auto& e2) {
-                return (e1[0] < e2[0]) || (e1[0] == e2[0] && e1[1] > e2[1]);
-            });
+        std::sort(envelopes.begin(), envelopes.end(), [&](const auto& e1, const auto& e2){ return (e1[0] < e2[0]) || (e1[0] == e2[0] && e1[1] > e2[1]); });
+
+        // dp[i]: the maximum number of envelopes you can Russian doll from envelopes[0:i] and envelopes[i] is the lagrest of doll
+        int dp[n];
+        std::fill(dp, dp + n, 1);
 
         vector<int> lis; // longest increasing subsequence
-        for (int i = 0; i < n; ++i) {
-            if (lis.empty() || lis.back() < envelopes[i][1]) {
-                lis.push_back(envelopes[i][1]);
+        for (const auto& envelope : envelopes) {
+            if (lis.empty() || lis.back() < envelope[1]) {
+                lis.push_back(envelope[1]);
             }
             else {
-                auto itr = std::lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
-                *itr = envelopes[i][1];
+                auto itr = std::lower_bound(lis.begin(), lis.end(), envelope[1]);
+                *itr = envelope[1];
             }
         }
 
@@ -54,7 +48,7 @@ public:
     }
 
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        //return dynamicProgramming(envelopes);
-        return greedy(envelopes);
+       //return dynamicProgramming(envelopes);
+       return greedy(envelopes);
     }
 };
