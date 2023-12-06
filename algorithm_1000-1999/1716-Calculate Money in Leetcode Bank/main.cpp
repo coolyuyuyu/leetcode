f1ee7d4a62ae0a57ad29379bdd2eebd1851e57a2
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-06 15:03:33 +0800

    1716: update solution

--- algorithm_1000-1999/1716-Calculate Money in Leetcode Bank/main.cpp
+++ algorithm_1000-1999/1716-Calculate Money in Leetcode Bank/main.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int week = n / 7;
-        int rem = n % 7;
+        int w = n / 7;
+        int d = n % 7;
 
-        int ret = (28 + 28 + 7 * (week - 1)) * week / 2;
-        ret += ((week + 1) + (week + 1 + rem - 1)) * rem / 2;
+        int ret = (28 + 28 + (w - 1) * 7) * w / 2; 
+        ret += (w + 1 + w + 1 + (d - 1)) * d / 2;
         return ret;
     }
 };
