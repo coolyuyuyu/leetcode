64c693fc12a1bbd40a7dcd5098654eb6b5ce8be0
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-06 15:31:38 +0800

    565: initial solution

---
+++ algorithm_0000-0999/565-Array Nesting/main.cpp
@@ -0,0 +1,62 @@
+class DisjointSets {
+public:
+    DisjointSets(int n)
+        : m_parents(n)
+        , m_cardinality(n, 1)
+        , m_size(n) {
+        std::iota(m_parents.begin(), m_parents.end(), 0);
+    }
+
+    void merge(int elem1, int elem2) {
+        int root1 = root(elem1), root2 = root(elem2);
+        if (root1 != root2) {
+            m_parents[root1] = root2;
+            m_cardinality[root2] += m_cardinality[root1];
+            --m_size;
+        }
+    }
+
+    bool connected(int elem1, int elem2) const {
+        return root(elem1) == root(elem2);
+    }
+
+    int cardinality(int elem) const {
+        return m_cardinality[root(elem)];
+    }
+
+    int size() const {
+        return m_size;
+    }
+
+private:
+    int root(int elem) const {
+        if (m_parents[elem] != elem) {
+            m_parents[elem] = root(m_parents[elem]);
+        }
+
+        return m_parents[elem];
+    }
+
+    mutable vector<int> m_parents;
+    vector<int> m_cardinality;
+    int m_size;
+};
+
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int n = nums.size();
+
+        DisjointSets ds(n);
+        for (int i = 0; i < n; ++i) {
+            ds.merge(i, nums[i]);
+        }
+
+        int ret = 0;
+        for (int i = 0; i < n; ++i) {
+            ret = std::max(ret, ds.cardinality(i));
+        }
+
+        return ret;
+    }
+};
