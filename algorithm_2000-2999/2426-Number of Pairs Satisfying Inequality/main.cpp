1d0f8616fb7c32d1adea1ffba91378132055adf6
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-08 01:27:04 +0800

    2426: update solution

--- algorithm_2000-2999/2426-Number of Pairs Satisfying Inequality/main.cpp
+++ algorithm_2000-2999/2426-Number of Pairs Satisfying Inequality/main.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    void recursion(vector<int>& diffs, size_t lo, size_t hi, int diff, long long& ret) {
-        if ((hi - lo) <= 1) {
-            return;
-        }
+    long long byDivideAndConquer(vector<int>& nums1, vector<int>& nums2, int diff) {
+        int n = nums1.size();
 
-        size_t mid = lo + (hi - lo) / 2;
-        recursion(diffs, lo, mid, diff, ret);
-        recursion(diffs, mid, hi, diff, ret);
-        for (size_t i1 = lo, i2 = mid; i2 < hi; ++i2) {
-            while (diffs[i1] <= (diffs[i2] + diff) && i1 < mid) {
-                ++i1;
-            }
-            ret += (i1 - lo);
-        }
-
-        std::inplace_merge(diffs.begin() + lo, diffs.begin() + mid, diffs.begin() + hi);
-    }
-
-    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
-        size_t n = nums1.size();
-        vector<int> diffs(n);
-        for (size_t i = 0; i < n; ++i) {
+        int diffs[n];
+        for (int i = 0; i < n; ++i) {
             diffs[i] = nums1[i] - nums2[i];
         }
 
         long long ret = 0;
-        recursion(diffs, 0, n, diff, ret);
+        std::function<void(int, int)> f = [&](int lo, int hi) {
+            if (lo >= hi) { return; }
+
+            int mid = lo + (hi - lo) / 2;
+            f(lo, mid);
+            f(mid + 1, hi);
+
+            for (int i = lo, j = mid + 1; j <= hi; ++j) {
+                while (i <= mid && (diffs[i] - diff <= diffs[j])) {
+                    ++i;
+                }
+                ret += i - lo;
+            }
+
+            std::inplace_merge(diffs + lo, diffs + mid + 1, diffs + hi + 1);
+        };
+        f(0, n - 1);
 
         return ret;
+
+    }
+
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
+        return byDivideAndConquer(nums1, nums2, diff);
     }
 };
-
-/*
-    nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff
-=>  nums1[i] - nums2[i] <= nums1[j] - nums2[j] + diff
-=>  diffs[i] <= diffs[j] + diff, diffs[m] = nums1[m] - nums2[m]
-*/
