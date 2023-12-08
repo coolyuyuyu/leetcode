059c66105c654eaa49a121091d807babf3f6bd55
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-09 00:14:58 +0800

    606: update  solution

--- algorithm_0000-0999/606-Construct String from Binary Tree/main.cpp
+++ algorithm_0000-0999/606-Construct String from Binary Tree/main.cpp
@@ -11,70 +11,63 @@
  */
 class Solution {
 public:
-    void tree2str_Recursive(TreeNode* root, ostringstream& oss) {
-        if (!root) {
-            return;
-        }
-
-        oss << root->val;
-        if (root->left || root->right) {
-            oss << "(";
-            tree2str_Recursive(root->left, oss);
-            oss << ")";
-
-            if (root->right) {
-                oss << "(";
+    string recursive(TreeNode* root) {
+        ostringstream oss;
+        std::function<void(TreeNode*)> f = [&](TreeNode* root) {
+            oss << root->val;
+            if (root->left == nullptr && root->right == nullptr) {
+                return;
             }
-            tree2str_Recursive(root->right, oss);
-            if (root->right) {
-                oss << ")";
+
+            oss << '(';
+            if(root->left) {
+                f(root->left);
             }
-        }
+            oss << ')';
+
+            if(root->right) {
+                oss << '(';
+                f(root->right);
+                oss << ')';
+            }
+        };
+        f(root);
+
+        return oss.str();
     }
 
-    void tree2str_Iterative(TreeNode* root, ostringstream& oss) {
-        if (!root) {
-            return;
-        }
+    string iterative(TreeNode* root) {
+        ostringstream oss;
+        for (stack<pair<TreeNode*, bool>> stk({{root, false}}); !stk.empty();) {
+            auto [node, visited] = stk.top();
+            stk.pop();
 
-        set<TreeNode*> visited;
-        stack<TreeNode*> stk({root});
-        while (!stk.empty()) {
-            root = stk.top();
-            auto p = visited.insert(root);
-            if (p.second) {
-                if (1 < stk.size()) {
-                    oss << "(";
-                }
-                oss << root->val;
-
-                if (root->right) {
-                    if (!root->left) {
-                        oss << "()";
-                    }
-                    stk.push(root->right);
-                }
-                if (root->left) {
-                    stk.push(root->left);
-                }
+            if (visited) {
+                oss << ")";
             }
             else {
-                stk.pop();
-                visited.erase(p.first);
+                oss << "(";
+                oss << node->val;
 
-                if (0 < stk.size()) {
-                    oss << ")";
+                stk.emplace(node, true);
+                if (node->right) {
+                    if (!node->left) {
+                        oss << "()";
+                    }
+                    stk.emplace(node->right, false);
+                }
+                if (node->left) {
+                    stk.emplace(node->left, false);
                 }
             }
         }
+
+        string ret = oss.str();
+        return ret.substr(1, ret.size() - 2);
     }
 
     string tree2str(TreeNode* root) {
-        ostringstream oss;
-
-        //tree2str_Recursive(root, oss);
-        tree2str_Iterative(root, oss);
-
-        return oss.str();
+        //return recursive(root);
+        return iterative(root);
     }
 };
