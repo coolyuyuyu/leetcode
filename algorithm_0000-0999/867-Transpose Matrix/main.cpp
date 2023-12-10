8c4a31905827197f38cbf72eae9eef63a9b60be4
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-11 00:10:18 +0800

    867: update solution

--- algorithm_0000-0999/867-Transpose Matrix/main.cpp
+++ algorithm_0000-0999/867-Transpose Matrix/main.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix.front().size();
-
+        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
         if (m < n) {
             matrix.resize(n, vector<int>(m));
         }
-        else if (n < m) {
+        else if (m > n) {
             for (size_t i = 0; i < n; ++i) {
                 matrix[i].resize(m);
             }
         }
 
-        const auto minmax = std::minmax(m, n);
-        for (size_t i = 0; i < minmax.first; ++i) {
-            for (size_t j = i + 1; j < minmax.second; ++j) {
+        auto [mn, mx] = std::minmax(m, n);
+        for (int i = 0; i < mn; ++i) {
+            for (int j = i + 1; j < mx; ++j) {
                 std::swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        matrix.resize(n);
-        for (auto& row : matrix) {
-            row.resize(m);
+        if (m < n) {
+            for (size_t i = 0; i < m; ++i) {
+                matrix[i].resize(m);
+            }
+        }
+        else if (m > n) {
+            matrix.resize(n);
         }
 
         return matrix;
