284bcb4abcd3a0fe404f2afc46ad233e7da511d4
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-11 21:03:13 +0800

    841: update solution

--- algorithm_0000-0999/841-Keys and Rooms/main.cpp
+++ algorithm_0000-0999/841-Keys and Rooms/main.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        size_t n = rooms.size();
-        vector<bool> visited(n, false);
-        for (queue<int> keys({0}); !keys.empty();) {
-            int door = keys.front();
-            keys.pop();
-            if (visited[door]) {
-                continue;
-            }
+        int n = rooms.size();
 
-            visited[door] = true;
-            --n;
+        bool visited[n];
+        std::fill(visited, visited + n, false);
 
-            for (int key : rooms[door]) {
-                keys.push(key);
+        queue<int> q;
+
+        visited[0] = true;
+        q.push(0);
+
+        while (!q.empty()) {
+            int room = q.front();
+            q.pop();
+
+            for (int key : rooms[room]) {
+                if (visited[key]) { continue; }
+
+                visited[key] = true;
+                q.push(key);
             }
         }
 
-        return n == 0;
+        return std::find(visited, visited + n, false) == visited + n;
     }
 };
