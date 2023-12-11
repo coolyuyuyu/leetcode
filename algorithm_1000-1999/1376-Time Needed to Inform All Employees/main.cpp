07e7474bca3bef327cd8170caddd90440413ba8e
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-11 21:03:24 +0800

    1376: update solution

--- algorithm_1000-1999/1376-Time Needed to Inform All Employees/main.cpp
+++ algorithm_1000-1999/1376-Time Needed to Inform All Employees/main.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<int> graph[n];
+        vector<int> next[n];
         for (int i = 0; i < n; ++i) {
-            if (0 <= manager[i]) {
-                graph[manager[i]].push_back(i);
-            }
+            if (i == headID) { continue; }
+            next[manager[i]].push_back(i);
         }
 
-        stack<pair<int, int>> stk;
-        stk.emplace(headID, 0);
         int ret = 0;
-        while (!stk.empty()) {
-            auto [employee, time] = stk.top();
-            stk.pop();
+        for (queue<pair<int, int>> q({{headID, 0}}); !q.empty();) {
+            auto [id, t] = q.front();
+            q.pop();
 
-            ret = std::max(ret, time);
+            ret = std::max(ret, t);
 
-            for (int subordinate : graph[employee]) {
-                stk.emplace(subordinate, time + informTime[employee]);
+            for (int nxt : next[id]) {
+                q.emplace(nxt, t + informTime[id]);
             }
         }
 
