a1a4c8c41db9e9785e001415a7f9ac1befa01238
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-11 21:03:18 +0800

    1287: update solution

--- algorithm_1000-1999/1287-Element Appearing More Than 25% In Sorted Array/main.cpp
+++ algorithm_1000-1999/1287-Element Appearing More Than 25% In Sorted Array/main.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
+    // Time: O(B)
+    int lsearch(vector<int>& arr) {
+        int len = arr.size() / 4;
+        for (int i = 0; i + len < arr.size(); ++i) {
+            if (arr[i] == arr[i + len]) {
+                return arr[i];
+            }
+        }
+
+        assert(false);
+        return -1;
+    }
+
     // Time: O(logN)
-    int findSpecialInteger(vector<int>& arr) {
-        // 25%, 50%, 75%
-        for (size_t i = 1; i < 4; ++i) {
-            int val = arr[arr.size() * i / 4];
-            auto p = std::equal_range(arr.begin(), arr.end(), val);
-            if ((p.second - p.first) * 4 > arr.size()) {
+    int bsearch(vector<int>& arr) {
+        for (float p : vector<float>{0.25, 0.5, 0.75}) {
+            int val = arr[arr.size() * p];
+            auto bounds = std::equal_range(arr.begin(), arr.end(), val);
+            if (std::distance(bounds.first, bounds.second) * 4 > arr.size()) {
                 return val;
             }
         }
 
         assert(false);
         return -1;
+    }
+
+    int findSpecialInteger(vector<int>& arr) {
+        //return lsearch(arr);
+        return bsearch(arr);
     }
 };
