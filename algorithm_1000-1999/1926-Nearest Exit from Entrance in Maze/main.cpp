9840361aa1955a93887a58ff49c3815986575211
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-12 16:49:47 +0800

    1926: update solution

--- algorithm_1000-1999/1926-Nearest Exit from Entrance in Maze/main.cpp
+++ algorithm_1000-1999/1926-Nearest Exit from Entrance in Maze/main.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int m = maze.size(), n = maze.empty() ? 0 : maze.front().size();
-        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        int m = maze.size(), n = maze.empty() ? 0 : maze[0].size();
 
-        queue<tuple<int, int, int>> q;
-        q.emplace(entrance[0], entrance[1], 0);
-
-        int minSteps = INT_MAX;
-        while (!q.empty()) {
-            tuple<int, int, int> t = q.front();
-            q.pop();
-
-            int r = std::get<0>(t), c = std::get<1>(t);
-            int steps = std::get<2>(t);
-
-            if (maze[r][c] == '+') {
-
-                continue;
+        bool visited[m][n];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                visited[r][c] = false;
             }
-            else if (dp[r][c] <= steps) {
-                continue;
-            }
-            dp[r][c] = steps;
+        }
+        queue<pair<int, int>> q;
 
-            if (r == 0 || (r + 1) == m || c == 0 || (c + 1) == n) {
-                if (r != entrance[0] || c != entrance[1]) {
-                    return steps;
+        visited[entrance[0]][entrance[1]] = true;
+        q.emplace(entrance[0], entrance[1]);
+
+        for(int step = 0; !q.empty(); ++step) {
+            for (int i = q.size(); 0 < i--;) {
+                auto [r, c] = q.front();
+                q.pop();
+                for (const auto& [dr, dc] : dirs) {
+                    int x = r + dr, y = c + dc;
+                    if (x < 0 || m <= x || y < 0 || n <= y) { cout << "\t1" << endl; continue; }
+                    if (maze[x][y] == '+') { cout << "\t2" << endl;  continue; }
+                    if (visited[x][y]) { cout << "\t3" << endl;  continue; }
+
+                    if ((x == 0 || x + 1 == m || y == 0 || y + 1 == n) && (x != entrance[0] || y != entrance[1])) {
+                        return step + 1;
+                    }
+
+                    visited[x][y] = true;
+                    q.emplace(x, y);
                 }
-            }
-
-            if (0 < c) { // lft
-                q.emplace(r, c - 1, steps + 1);
-            }
-            if (0 < r) { // top
-                q.emplace(r - 1, c, steps + 1);
-            }
-            if ((c + 1) < n) { // rht
-                q.emplace(r, c + 1, steps + 1);
-            }
-            if ((r + 1) < m) { // btm
-                q.emplace(r + 1, c, steps + 1);
             }
         }
 
