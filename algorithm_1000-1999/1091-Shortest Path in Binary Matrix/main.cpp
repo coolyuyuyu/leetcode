5b0d80e6e93544fe1a82a906b942603a9d8a4da5
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-12 22:20:34 +0800

    1091: update solution

--- algorithm_1000-1999/1091-Shortest Path in Binary Matrix/main.cpp
+++ algorithm_1000-1999/1091-Shortest Path in Binary Matrix/main.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    vector<pair<int, int>> dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    int byBFS(vector<vector<int>>& grid) {
+        vector<pair<int, int>> dirs = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
         int n = grid.size();
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
-            return -1;
-        }
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) { return -1; }
 
         queue<pair<int, int>> q;
+
         grid[0][0] = 1;
         q.emplace(0, 0);
-        for (int len = 1; !q.empty(); ++len) {
+        for (int dist = 1; !q.empty(); ++dist) {
             for (int i = q.size(); 0 < i--;) {
                 auto [r, c] = q.front();
                 q.pop();
 
                 if (r == n - 1 && c == n - 1) {
-                    return len;
+                    return dist ;
                 }
 
                 for (const auto& [dr, dc] : dirs) {
                     int x = r + dr, y = c + dc;
-                    if (x < 0 || n <= x || y < 0 || n <= y || grid[x][y] == 1) {
-                        continue;
-                    }
+                    if (x < 0 || n <= x || y < 0 || n <= y) { continue; }
+                    if (grid[x][y] == 1) { continue; }
+
                     grid[x][y] = 1;
                     q.emplace(x, y);
                 }
@@ -32,5 +31,9 @@ public:
         }
 
         return -1;
+    }
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        return byBFS(grid);
     }
 };
