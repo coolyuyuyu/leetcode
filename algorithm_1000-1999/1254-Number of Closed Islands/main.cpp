9586897b01a00d79fd27f04f2327cf644f2d37ff
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-15 00:35:54 +0800

    1254: update solution

--- algorithm_1000-1999/1254-Number of Closed Islands/main.cpp
+++ algorithm_1000-1999/1254-Number of Closed Islands/main.cpp
@@ -1,26 +1,21 @@
 class DisjointSets {
 public:
     DisjointSets(int n)
-        : m_roots(n, -1)
-        , m_size(0) {
-    }
-
-    void enable(int elem) {
-        if (m_roots[elem] == -1) {
-            m_roots[elem] = elem;
-            ++m_size;
-        }
+        : m_parents(n)
+        , m_size(n) {
+        std::iota(m_parents.begin(), m_parents.end(), 0);
     }
 
     void merge(int elem1, int elem2) {
-        enable(elem1);
-        enable(elem2);
-
         int root1 = root(elem1), root2 = root(elem2);
         if (root1 != root2) {
-            m_roots[root1] = root2;
+            m_parents[root1] = root2;
             --m_size;
         }
+    }
+
+    bool connected(int elem1, int elem2) const {
+        return root(elem1) == root(elem2);
     }
 
     int size() const {
@@ -29,14 +24,14 @@ public:
 
 private:
     int root(int elem) const {
-        if (m_roots[elem] != elem) {
-            m_roots[elem]  = root(m_roots[elem]);
+        if (m_parents[elem] != elem) {
+            m_parents[elem] = root(m_parents[elem]);
         }
 
-        return m_roots[elem];
+        return m_parents[elem];
     }
 
-    mutable vector<int> m_roots;
+    mutable vector<int> m_parents;
     int m_size;
 };
 
@@ -45,28 +40,27 @@ public:
     int closedIsland(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
-        int cornerId = m * n;
-        auto getId = [m, n](int r, int c) {
+        int boundaryId = m * n;
+        std::function<int(int, int)> getId = [&](int r, int c) {
             return r * n + c;
         };
 
         DisjointSets ds(m * n + 1);
-        ds.enable(cornerId);
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
                 if (grid[r][c] == 0) {
-                    int id = getId(r, c);
-                    ds.enable(id);
-
                     if (r == 0 || c == 0 || r == (m - 1) || c == (n - 1)) {
-                        ds.merge(id, cornerId);
+                        ds.merge(getId(r, c), boundaryId);
                     }
-                    if ((r + 1) < m && grid[r + 1][c] == 0) {
-                        ds.merge(id, getId(r + 1, c));
+                    if (r + 1 < m && grid[r + 1][c] == 0) {
+                        ds.merge(getId(r, c), getId(r + 1, c));
                     }
-                    if ((c + 1) < n && grid[r][c + 1] == 0) {
-                        ds.merge(id, getId(r, c + 1));
+                    if (c + 1 < n && grid[r][c + 1] == 0) {
+                        ds.merge(getId(r, c), getId(r, c + 1));
                     }
+                }
+                else {
+                    ds.merge(getId(r, c), boundaryId);
                 }
             }
         }
