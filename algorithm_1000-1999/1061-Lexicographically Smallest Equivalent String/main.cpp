220a1ea627f88cdd54c21cce4423bda12d37bb36
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-19 01:17:50 +0800

    1061: update solution

--- algorithm_1000-1999/1061-Lexicographically Smallest Equivalent String/main.cpp
+++ algorithm_1000-1999/1061-Lexicographically Smallest Equivalent String/main.cpp
@@ -1,64 +1,57 @@
+class DisjointSets {
+public:
+    DisjointSets(int n)
+        : m_parents(n)
+        , m_size(n) {
+        std::iota(m_parents.begin(), m_parents.end(), 0);
+    }
+
+    void merge(int elem1, int elem2) {
+        int root1 = root(elem1), root2 = root(elem2);
+        if (root1 != root2) {
+            if (root1 < root2) {
+                m_parents[root2] = root1;
+            }
+            else {
+                m_parents[root1] = root2;
+            }
+            --m_size;
+        }
+    }
+
+    bool connected(int elem1, int elem2) const {
+        return root(elem1) == root(elem2);
+    }
+
+    int size() const {
+        return m_size;
+    }
+
+    int root(int elem) const {
+        if (m_parents[elem] != elem) {
+            m_parents[elem] = root(m_parents[elem]);
+        }
+
+        return m_parents[elem];
+    }
+
+private:
+    mutable vector<int> m_parents;
+    int m_size;
+};
+
 class Solution {
 public:
-    class DisjointSets {
-    public:
-        DisjointSets()
-            : m_parents(26){
-            std::iota(m_parents.begin(), m_parents.end(), 'a');
-        }
-
-        void merge(char elem1, char elem2) {
-            char root1 = root(elem1);
-            char root2 = root(elem2);
-            if (root1 != root2) {
-                m_parents[root1 - 'a'] = root2;
-            }
-        }
-
-        vector<vector<char>> sets() {
-            map<char, vector<char>> groups;
-            for (char c = 'a'; c <= 'z'; ++c) {
-                groups[root(c)].push_back(c);
-            }
-
-            vector<vector<char>> ret;
-            for (auto& [_, group] : groups) {
-                ret.insert(ret.end(), std::move(group));
-            }
-
-            return ret;
-        }
-
-    private:
-        char root(char elem) {
-            if (m_parents[elem - 'a'] != elem) {
-                m_parents[elem - 'a'] = root(m_parents[elem - 'a']);
-            }
-
-            return m_parents[elem - 'a'];
-        }
-
-        vector<char> m_parents;
-    };
-
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        DisjointSets ds;
-        for (size_t i = 0; i < s1.size(); ++i) {
-            ds.merge(s1[i], s2[i]);
+        DisjointSets ds(26);
+        for (int i = 0, n = s1.size(); i < n; ++i) {
+            ds.merge(s1[i] - 'a', s2[i] - 'a');
         }
 
-        vector<char> bases(26);
-        for (const vector<char>& group : ds.sets()) {
-            for (char c : group) {
-                bases[c - 'a'] = group.front();
-            }
+        for (char& c : baseStr) {
+            c = 'a' + ds.root(c - 'a');
         }
 
-        string ret(baseStr.size(), 0);
-        for (size_t i = 0; i < ret.size(); ++i) {
-            ret[i] = bases[baseStr[i] - 'a'];
-        }
-
-        return ret;
+        return baseStr;
     }
 };
