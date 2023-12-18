df0df44d1eabdd22eeb8b9b3f67853c0260b275e
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-19 01:18:45 +0800

    1913: update solution

--- algorithm_1000-1999/1913-Maximum Product Difference Between Two Pairs/main.cpp
+++ algorithm_1000-1999/1913-Maximum Product Difference Between Two Pairs/main.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        int a(10001), b(10001), c(0), d(0);
+        int mx1 = INT_MIN, mx2 = INT_MIN, mn1 = INT_MAX, mn2 = INT_MAX;
         for (int num : nums) {
-            if (num < a) {
-                b = a;
-                a = num;
+            if (num > mx1) {
+                mx2 = mx1;
+                mx1 = num;
             }
-            else if (num < b) {
-                b = num;
+            else if (num > mx2) {
+                mx2 = num;
             }
 
-            if (d < num) {
-                c = d;
-                d = num;
+            if (num < mn1) {
+                mn2 = mn1;
+                mn1 = num;
             }
-            else if (c < num) {
-                c = num;
+            else if (num < mn2) {
+                mn2 = num;
             }
         }
 
-        return (c * d - a * b);
+        return mx1 * mx2 - mn1 * mn2;
     }
 };
