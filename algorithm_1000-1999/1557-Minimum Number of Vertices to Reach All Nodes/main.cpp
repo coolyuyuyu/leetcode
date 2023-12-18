6ee3da1614c1c64b0b5cae414d31b165ad068851
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-19 01:17:57 +0800

    1557: update solution

--- algorithm_1000-1999/1557-Minimum Number of Vertices to Reach All Nodes/main.cpp
+++ algorithm_1000-1999/1557-Minimum Number of Vertices to Reach All Nodes/main.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<bool> reachables(n, false);
+        bool in[n];
+        std::fill(in, in + n, false);
         for (const auto& edge : edges) {
-            reachables[edge[1]] = true;
+            in[edge[1]] = true;
         }
 
         vector<int> ret;
         for (int i = 0; i < n; ++i) {
-            if (!reachables[i]) {
+            if (!in[i]) {
                 ret.push_back(i);
             }
         }
