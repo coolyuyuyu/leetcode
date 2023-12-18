17d3b179809b22e476fbaeea10e239a558a14890
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-19 01:17:33 +0800

    2929: initial solution

---
+++ algorithm_2000-2999/2929-Distribute Candies Among Children II/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        // distrinute candies to a, b and c
+
+        long long ret = 0;
+
+        int aMin = std::max(0, n - 2 * limit);
+        int aMax = std::min(limit, n);
+        for (int a = aMin; a <= aMax; ++a) {
+            int bMin = std::max(0, n - a - limit);
+            int bMax = std::min(limit, n - a);
+
+            ret += bMax - bMin + 1;
+        }
+
+        return ret;
+    }
+};
