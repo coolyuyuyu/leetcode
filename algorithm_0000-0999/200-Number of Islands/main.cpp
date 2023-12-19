c2fd5c0a898aabef0c4ded98e3c8c039c5df4801
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-20 00:18:27 +0800

    200: update solution

--- algorithm_0000-0999/200-Number of Islands/main.cpp
+++ algorithm_0000-0999/200-Number of Islands/main.cpp
@@ -1,122 +1,99 @@
 class DisjointSets {
 public:
-    DisjointSets(int rowCnt, int colCnt)
-        : m_rowCnt(rowCnt)
-        , m_colCnt(colCnt)
-        , m_roots(rowCnt * colCnt, kInvalidId)
-        , m_size(0) {
+    DisjointSets(int n)
+        : m_parents(n)
+        , m_size(n) {
+        std::iota(m_parents.begin(), m_parents.end(), 0);
     }
 
-    size_t size() const {
-        return m_size;
-    }
-
-    void connect(int r1, int c1, int r2, int c2) {
-        size_t elem1 = encode(r1, c1), elem2 = encode(r2, c2);
-        if (m_roots[elem1] == kInvalidId) {
-            m_roots[elem1] = elem1;
-            ++m_size;
-        }
-        if (m_roots[elem2] == kInvalidId) {
-            m_roots[elem2] = elem2;
-            ++m_size;
-        }
-
-        size_t root1 = root(elem1), root2 = root(elem2);
+    void merge(int elem1, int elem2) {
+        int root1 = root(elem1), root2 = root(elem2);
         if (root1 != root2) {
-            m_roots[root1] = root2;
+            m_parents[root1] = root2;
             --m_size;
         }
     }
 
+    bool connected(int elem1, int elem2) const {
+        return root(elem1) == root(elem2);
+    }
+
+    int size() const {
+        return m_size;
+    }
+
 private:
-    size_t root(size_t elem) const {
-        if (m_roots[elem] != elem) {
-            m_roots[elem] = root(m_roots[elem]);
+    int root(int elem) const {
+        if (m_parents[elem] != elem) {
+            m_parents[elem] = root(m_parents[elem]);
         }
 
-        return m_roots[elem];
+        return m_parents[elem];
     }
 
-    size_t encode(size_t r, size_t c) {
-        return r * m_colCnt + c;
-    }
-
-    size_t m_rowCnt;
-    size_t m_colCnt;
-
-    static size_t kInvalidId;
-    mutable vector<size_t> m_roots;
-    size_t m_size;
+    mutable vector<int> m_parents;
+    int m_size;
 };
-
-size_t DisjointSets::kInvalidId = std::numeric_limits<size_t>::max();
 
 class Solution {
 public:
-    int union_find(const vector<vector<char>>& grid) {
-        static vector<vector<int>> dirs = {{0, 1}, {1, 0}};
-
-        int m = grid.size(), n = grid[0].size();
-        DisjointSets ds(m, n);
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                if (grid[r][c] == '0') {
-                    continue;
-                }
-                ds.connect(r, c, r, c);
-
-                static vector<vector<int>> dirs = {{0, 1}, {1, 0}};
-                for (const auto& dir : dirs) {
-                    int nr = r + dir[0], nc = c + dir[1];
-                    if (0 <= nr && nr < m && 0 <= nc && nc < n && grid[nr][nc] == '1') {
-                        ds.connect(r, c, nr, nc);
-                    }
-                }
-            }
-        }
-
-        return ds.size();
-    }
-
     int bfs(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
+
+        vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        std::function<void(int, int)> f = [&](int r, int c) {
+            grid[r][c] = '0';
+            for (const auto& [dr, dc] : dirs) {
+                int x = r + dr, y = c + dc;
+                if (x < 0 || m <= x || y < 0 || n <= y) { continue; }
+                if (grid[x][y] == '0') { continue; }
+                f(x, y);
+            }
+        };
 
         int ret = 0;
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
-                if (grid[r][c] == '0') {
-                    continue;
+                if (grid[r][c] == '1') {
+                    f(r, c);
+                    ++ret;
                 }
-
-                grid[r][c] = '0';
-
-                queue<array<int, 2>> q;
-                q.push({r, c});
-
-                while (!q.empty()) {
-                    int x = q.front()[0], y = q.front()[1];
-                    q.pop();
-
-                    static vector<vector<int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-                    for (const auto& dir : dirs) {
-                        int nx = x + dir[0], ny = y + dir[1];
-                        if (0 <= nx && nx < m && 0 <= ny && ny < n && grid[nx][ny] == '1') {
-                            grid[nx][ny] = '0';
-                            q.push({nx, ny});
-                        }
-                    }
-                }
-
-                ++ret;
             }
         }
 
         return ret;
     }
 
+    int dsu(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
+
+        std::function<int(int, int)> getId = [&](int r, int c) {
+            return r * n + c;
+        };
+
+        DisjointSets ds(m * n);
+        int seaCnt = 0;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] == '1') {
+                    if (r + 1 < m && grid[r + 1][c] == '1') {
+                        ds.merge(getId(r, c), getId(r + 1, c));
+                    }
+                    if (c + 1 < n && grid[r][c + 1] == '1') {
+                        ds.merge(getId(r, c), getId(r, c + 1));
+                    }
+                }
+                else {
+                    ++seaCnt;
+                }
+            }
+        }
+
+        return ds.size() - seaCnt;
+    }
+
     int numIslands(vector<vector<char>>& grid) {
-        //return union_find(grid);
-        return bfs(grid);
+        //return bfs(grid);
+        return dsu(grid);
     }
 };
