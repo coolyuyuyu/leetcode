ae8280aa5d4063d783216c7833022462c84026f7
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-23 18:03:59 +0800

    159: update solution

--- algorithm_0000-0999/159-Longest Substring with At Most Two Distinct Characters/main.cpp
+++ algorithm_0000-0999/159-Longest Substring with At Most Two Distinct Characters/main.cpp
@@ -4,10 +4,10 @@ public:
         vector<int> cnts(128, 0);
 
         int ret = 0;
-        for (int lft = 0, rht = 0, n = s.size(), cntDistinct = 0; lft < n; ++lft) {
-            for (;rht < n && (rht < lft || cntDistinct < 2 || (cntDistinct == 2 && 0 < cnts[s[rht]])); ++rht) {
+        for (int lft = 0, rht = 0, n = s.size(), cntDistinct = 0; rht < n; ++lft) {
+            for (;rht < n && (cntDistinct < 2 || (cntDistinct == 2 && cnts[s[rht]] > 0)); ++rht) {
                 if (cnts[s[rht]]++ == 0) {
-                    cntDistinct++;
+                    ++cntDistinct;
                 }
             }
 
