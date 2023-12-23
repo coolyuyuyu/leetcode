1fe02e2d2c31f87aff7a87cc79a382c2477e6bf9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-23 18:05:58 +0800

    1496: update solution

--- algorithm_1000-1999/1496-Path Crossing/main.cpp
+++ algorithm_1000-1999/1496-Path Crossing/main.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    unordered_map<char, pair<int, int>> dirs = {{'N', {0, 1}}, {'S', {0, -1}}, {'E', {1, 0}}, {'W', {-1, 0}}};
-
     bool isPathCrossing(string path) {
+        unordered_map<char, pair<int, int>> dirs = {{'N', {0, 1}}, {'S', {0, -1}}, {'E', {1, 0}}, {'W', {-1, 0}}};
         int x = 0, y = 0;
-        set<pair<int, int>> visited = {{x, y}};
-        for (char c : path) {
-            const auto& [diffX, diffY] = dirs[c];
-            x += diffX, y += diffY;
-            if (visited.insert({x, y}).second == false) {
+
+        set<pair<int, int>> visited;
+        visited.emplace(x, y);
+        for (char ch : path) {
+            const auto& [dx, dy] = dirs[ch];
+            x += dx, y += dy;
+            if (visited.emplace(x, y).second == false) {
                 return true;
             }
         }
