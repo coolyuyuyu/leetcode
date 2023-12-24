3e1e054b27df53af81fbac5eb819e05b46ff9284
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-25 02:13:21 +0800

    787: update solution

--- algorithm_0000-0999/787-Cheapest Flights Within K Stops/main.cpp
+++ algorithm_0000-0999/787-Cheapest Flights Within K Stops/main.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     // Time: O(N + E * K)
     int bfs(int n, const vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> nexts(n);
+        vector<pair<int, int>> graph[n];
         for (const auto& flight : flights) {
-            nexts[flight[0]].emplace_back(flight[1], flight[2]);
+            int u = flight[0], v = flight[1], price = flight[2];
+            graph[u].emplace_back(v, price);
         }
 
-        vector<int> dists(n, INT_MAX);
+        int dists[n];
+        std::fill(dists, dists + n, INT_MAX);
         dists[src] = 0;
 
-        queue<pair<int, int>> q; // <node, dist>: the distance from src to node
+        queue<pair<int, int>> q; // <city, prices>
         q.emplace(src, 0);
-        for (int numStops = 0; numStops <= k && !q.empty(); ++numStops) {
-            for (int n = q.size(); 0 < n--;) {
-                auto [node, dist] = q.front();
+        for (int stops = 0; stops <= k && !q.empty(); ++stops) {
+            for (int i = q.size(); 0 < i--;) {
+                auto [cur, prices] = q.front();
                 q.pop();
 
-                for (auto [next, cost] : nexts[node]) {
-                    if (dists[next] <= (dist + cost)) {
-                        continue;
-                    }
-                    dists[next] = dist + cost;
+                for (const auto& [nxt, price] : graph[cur]) {
+                    if (dists[nxt] <= prices + price) { continue; }
+                    dists[nxt] = prices + price;
 
-                    q.emplace(next, dist + cost);
+                    q.emplace(nxt, prices + price);
                 }
             }
         }
@@ -33,62 +33,60 @@ public:
 
     // Time: O(E * min(N, K))
     int bellmanford(int n, const vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dists(n, INT_MAX);
+        int dists[n];
+        std::fill(dists, dists + n, INT_MAX / 2);
         dists[src] = 0;
-        for (int repetitions = std::min(n - 1, k + 1); 0 < repetitions--;) {
-            vector<int> tmps(dists);
-            bool modified = false;
-            for (const auto& edge : flights) {
-                int from = edge[0], to = edge[1], cost = edge[2];
-                if (tmps[from] == INT_MAX) {
-                    continue;
-                }
 
-                if ((tmps[from] + cost) < dists[to]) {
+        for (int i = 0; i < std::min(n - 1, k + 1); ++i) {
+            bool modified = false;
+
+            int tmps[n];
+            std::copy(dists, dists + n, tmps);
+            for (const auto& flight : flights) {
+                int from = flight[0], to = flight[1], price = flight[2];
+                if (tmps[from] + price < dists[to]) {
+                    dists[to] = tmps[from] + price;
                     modified = true;
-                    dists[to] = tmps[from] + cost;
                 }
             }
-            if (!modified) {
-                break;
-            }
+
+            if (!modified) { break; }
         }
 
-        return dists[dst] == INT_MAX ? -1 : dists[dst];
+        return dists[dst] == INT_MAX / 2 ? -1 : dists[dst];
     }
 
     // Time: O(N + E * K * log(E * K))
     int dijkstra(int n, const vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> nexts(n);
+        vector<pair<int, int>> graph[n];
         for (const auto& flight : flights) {
-            nexts[flight[0]].emplace_back(flight[1], flight[2]);
+            int u = flight[0], v = flight[1], price = flight[2];
+            graph[u].emplace_back(v, price);
         }
 
-        vector<int> depths(n, INT_MAX);
+        int visited[n];
+        std::fill(visited, visited + n, INT_MAX);
 
         auto comp = [](const tuple<int, int, int>& t1, const tuple<int, int, int>& t2) {
             return std::get<1>(t1) > std::get<1>(t2);
         };
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, decltype(comp)> pq(comp); // <node, dist, depth>: the distance from src to node with depth
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, decltype(comp)> pq(comp); // <city, prices, times>
         pq.emplace(src, 0, 0);
         while (!pq.empty()) {
-            auto [node, dist, depth] = pq.top();
+            auto [cur, prices, times] = pq.top();
             pq.pop();
 
-            if ((k + 1) < depth) {
-                continue;
-            }
-            if (depths[node] <= depth) {
-                continue;
-            }
-            depths[node] = depth;
+            if (visited[cur] <= times) { continue; }
+            visited[cur] = times;
 
-            if (node == dst) {
-                return dist;
+            if (cur == dst) {
+                return prices;
             }
 
-            for (auto [next, cost] : nexts[node]) {
-                pq.emplace(next, dist + cost, depth + 1);
+            for (const auto& [nxt, price] : graph[cur]) {
+                if (times + 1 > k + 1) { continue; }
+                if (visited[nxt] <= times + 1) { continue; }
+                pq.emplace(nxt, prices + price, times + 1);
             }
         }
 
@@ -96,8 +94,8 @@ public:
     }
 
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        //return bfs(n, flights, src, dst, k);
-        return bellmanford(n, flights, src, dst, k);
+        return bfs(n, flights, src, dst, k);
+        //return bellmanford(n, flights, src, dst, k);
         //return dijkstra(n, flights, src, dst, k);
     }
 };
