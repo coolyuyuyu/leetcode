7f73862d69e66cdd000e314d6d16c9f9ce92a303
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-25 02:13:27 +0800

    864: update solution

--- algorithm_0000-0999/864-Shortest Path to Get All Keys/main.cpp
+++ algorithm_0000-0999/864-Shortest Path to Get All Keys/main.cpp
@@ -4,17 +4,18 @@ public:
 
     int shortestPathAllKeys(vector<string>& grid) {
         int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
+
+        unordered_set<uint8_t> visited[m][n];
+        queue<tuple<int, int, int>> q;
+
         int keyCnt = 0;
-        
-        vector<vector<unordered_set<int>>> visited(m, vector<unordered_set<int>>(n));
-        queue<tuple<int, int, int>> q; // <r, c, state>
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
                 if (grid[r][c] == '@') {
                     visited[r][c].insert(0);
                     q.emplace(r, c, 0);
                 }
-                else if (std::islower(grid[r][c])) {
+                else if (islower(grid[r][c])) {
                     ++keyCnt;
                 }
             }
@@ -22,26 +23,24 @@ public:
         int finalState = (1 << keyCnt) - 1;
 
         for (int step = 0; !q.empty(); ++step) {
-            for (int len = q.size(); 0 < len--;) {
+            for (int i = q.size(); 0 < i--;) {
                 auto [r, c, s] = q.front();
                 q.pop();
 
                 if (s == finalState) {
                     return step;
                 }
-                for (const auto& [dr, dc] : dirs) {
-                    int x = r + dr, y = c + dc;
-                    if (x < 0 || m <= x || y < 0 || n <= y) { continue; } // out of range
-                    if (grid[x][y] == '#') { continue; } // wall
-                    if (isupper(grid[x][y]) && (s & (1 << (grid[x][y] - 'A'))) == 0) { continue; } // lock without key
-                    
-                    int state = s;
-                    if (islower(grid[x][y])) { // pick key if necessary;
-                        state |= (1 << (grid[x][y] - 'a'));
-                    } 
-                    if (visited[x][y].insert(state).second == false) { continue; } // visited
 
-                    q.emplace(x, y, state);
+                for (const auto& [dr, dc] : dirs) {
+                    int x = r + dr, y = c + dc, t = s;
+                    if (x < 0 || m <= x || y < 0 || n <= y) { continue; } // out of range
+                    if (islower(grid[x][y])){ t |= 1 << (grid[x][y] - 'a'); } // pick key
+
+                    if (grid[x][y] == '#') { continue; } // wall
+                    if (isupper(grid[x][y]) && (s >> (grid[x][y] - 'A') & 1) == 0) { continue; } // lock without key
+                    if (visited[x][y].insert(t).second == false) { continue; } // visited
+
+                    q.emplace(x, y, t);
                 }
             }
         }
