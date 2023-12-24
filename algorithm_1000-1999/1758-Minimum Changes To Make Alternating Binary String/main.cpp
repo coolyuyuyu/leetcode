498d6021e0a157df5a31e1b69a56b4ef1143af3b
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-24 14:00:13 +0800

    1758: update solution

--- algorithm_1000-1999/1758-Minimum Changes To Make Alternating Binary String/main.cpp
+++ algorithm_1000-1999/1758-Minimum Changes To Make Alternating Binary String/main.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int numOp1 = 0; // 010101...
-        int numOp2 = 0; // 101010...
-        for (size_t i = 0; i < s.size(); ++i) {
-            if (i % 2 == 0) {
-                if (s[i] == '1') {
-                    ++numOp1;
-                }
-                else {
-                    ++numOp2;
-                }
-            }
-            else {
-                if (s[i] == '0') {
-                    ++numOp1;
-                }
-                else {
-                    ++numOp2;
-                }
-            }
+        // dp0[i]: the minimum number of operations to make s[0:i] alternating and ending with '0'
+        // dp1[i]: the minimum number of operations to make s[0:i] alternating and ending with '1'
+        int dp0 = 0, dp1 = 0;
+        for (char c : s) {
+            int tmp0 = dp0, tmp1 = dp1;
+            dp0 = tmp1 + (c == '0' ? 0 : 1);
+            dp1 = tmp0 + (c == '1' ? 0 : 1);
         }
 
-        return min(numOp1, numOp2);
+        return std::min(dp0, dp1);
     }
 };
