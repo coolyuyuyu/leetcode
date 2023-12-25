fe2b81db03100688950e25545ec4f8597e7b3bd9
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-25 23:38:45 +0800

    091: update solution

--- algorithm_0000-0999/091-Decode Ways/main.cpp
+++ algorithm_0000-0999/091-Decode Ways/main.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
+    // Time: O(n), Space: O(n)
     int dp1(string s) {
         int n = s.size();
+
         s.insert(s.begin(), '9' + 1);
 
-        // dp[i]: the number of ways to decode it s[1:i]
+        // dp[i]: the number of ways to decode s[1:i]
         int dp[n + 1];
         dp[0] = 1;
-        dp[1] = ('1' <= s[1]? 1 : 0);
+        dp[1] = ('1' <= s[1]) ? 1 : 0 ;
         for (int i = 2; i <= n; ++i) {
             dp[i] = 0;
-
-            int d1 = s[i] - '0';
-            if (1 <= d1) {
+            if ('1' <= s[i]) {
                 dp[i] += dp[i - 1];
             }
-            int d2 = (s[i - 1] - '0') * 10 + s[i] - '0';
-            if (10 <= d2 && d2 <= 26) {
+            if ('1' == s[i - 1] || (s[i - 1] == '2' && s[i] <= '6')) {
                 dp[i] += dp[i - 2];
             }
         }
@@ -24,33 +23,33 @@ public:
         return dp[n];
     }
 
+     // Time: O(n), Space: O(1)
     int dp2(string s) {
         int n = s.size();
+
         s.insert(s.begin(), '9' + 1);
 
-        // dp[i]: the number of ways to decode it s[1:i]
-        int dp[n + 1];
-        int z = 0; // dp[-1]
-        int y = 1; // dp[0]
-        int x = ('1' <= s[1]? 1 : 0); // dp[1]
+        // dp[i]: the number of ways to decode s[1:i]
+        int pre = 1;
+        int cur = ('1' <= s[1]) ? 1 : 0 ;
         for (int i = 2; i <= n; ++i) {
-            z = y, y = x, x = 0;
+            int tmp = pre;
+            pre = cur;
 
-            int d1 = s[i] - '0';
-            if (1 <= d1) {
-                x += y;
+           cur = 0;
+            if ('1' <= s[i]) {
+                cur += pre;
             }
-            int d2 = (s[i - 1] - '0') * 10 + s[i] - '0';
-            if (10 <= d2 && d2 <= 26) {
-                x += z;
+            if ('1' == s[i - 1] || (s[i - 1] == '2' && s[i] <= '6')) {
+                cur += tmp;
             }
         }
 
-        return x;
+        return cur;
     }
 
     int numDecodings(string s) {
-        return dp1(s);
-        // return dp2(s);
+        //return dp1(s);
+        return dp2(s);
     }
 };
