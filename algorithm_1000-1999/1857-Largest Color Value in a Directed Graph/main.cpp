5b732be468238a405952b9c4899b658016dea105
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-28 00:47:15 +0800

    1857: update solution

--- algorithm_1000-1999/1857-Largest Color Value in a Directed Graph/main.cpp
+++ algorithm_1000-1999/1857-Largest Color Value in a Directed Graph/main.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
-    int helper(const string& colors, const vector<vector<int>>& nexts, vector<int> inDegrees, char c) {
-        int n = nexts.size();
-
-        int ret = 0;
-
-        queue<int> q;
-        vector<int> counts(nexts.size(), 0);
-        for (int v = 0; v < nexts.size(); ++v) {
-            if (inDegrees[v] == 0) {
-                counts[v] = (colors[v] == c ? 1 : 0);
-                ret = std::max(ret, counts[v]);
-                --n;
-                q.push(v);
-            }
-        }
-
-        while (!q.empty()) {
-            int v = q.front();
-            q.pop();
-
-            for (int next : nexts[v]) {
-                counts[next] = std::max(counts[next], counts[v] + (colors[next] == c ? 1 : 0));
-                ret = std::max(ret, counts[next]);
-                if (--inDegrees[next] == 0) {
-                    --n;
-                    q.push(next);
-                }
-            }
-        }
-
-        return n == 0 ? ret : -1;
-    }
-
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.size();
 
-        vector<vector<int>> nexts(n);
-        vector<int> inDegrees(n, 0);
+        vector<int> graph[n];
+        vector<int> in(n, 0);
         for (const auto& edge : edges) {
-            nexts[edge[0]].push_back(edge[1]);
-            ++inDegrees[edge[1]];
+            int u = edge[0], v = edge[1];
+            graph[u].push_back(v);
+            ++in[v];
         }
 
+        std::function<int(char color, vector<int>)> f = [&](char color, vector<int> in) {
+            int cnts[n]; std::fill(cnts, cnts + n, 0);
+            queue<int> q;
+            for (int v = 0; v < n; ++v) {
+                if (in[v] == 0) {
+                    cnts[v] = (colors[v] == color ? 1 : 0);
+                    q.push(v);
+                }
+            }
+
+            int visited = 0;
+            for (; !q.empty(); ++visited) {
+                auto cur = q.front();
+                q.pop();
+
+                for (int nxt : graph[cur]) {
+                    cnts[nxt] = std::max(cnts[nxt], cnts[cur] + (colors[nxt] == color ? 1 : 0));
+                    if (--in[nxt] == 0) {
+                        q.push(nxt);
+                    }
+                }
+            }
+
+            return visited == n ? *std::max_element(cnts, cnts + n) : -1;
+        };
+
         int ret = 0;
-        for (char c : unordered_set<char>(colors.begin(), colors.end())) {
-            int val = helper(colors, nexts, inDegrees, c);
+        for (char color : unordered_set<char>(colors.begin(), colors.end())) {
+            int val = f(color, in);
             if (val == -1) {
-                return -1;
+               return -1;
             }
             ret = std::max(ret, val);
         }
