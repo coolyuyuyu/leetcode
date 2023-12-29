09370a15d0145d85853a6f0e6388d572a875f40d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-30 01:05:45 +0800

    1335: update solution

--- algorithm_1000-1999/1335-Minimum Difficulty of a Job Schedule/main.cpp
+++ algorithm_1000-1999/1335-Minimum Difficulty of a Job Schedule/main.cpp
@@ -7,18 +7,18 @@ public:
         }
         jobDifficulty.insert(jobDifficulty.begin(), 0);
 
-        // dp[i][k]: the minimum difficulty of a job schedule for jobDifficulty[1:i] finished in k days
+        // dp[i][k]: the minimum difficulty of a job schedule of jobs[1:i] in k days
         int dp[n + 1][d + 1];
         for (int i = 0; i <= n; ++i) {
-            for (int k = 0; k <= std::min(d, i); ++k) {
-                dp[i][k] = INT_MAX / 2;
+            for (int numParts = 0; numParts <= std::min(d, i); ++numParts) {
+                dp[i][numParts] = INT_MAX / 2;
             }
         }
         dp[0][0] = 0;
         for (int i = 1; i <= n; ++i) {
             for (int numParts = 1; numParts <= std::min(d, i); ++numParts) {
                 int difficulty = INT_MIN;
-                for (int j = i; numParts <= j; --j) {
+                for (int j = i; j >= numParts; --j) {
                     difficulty = std::max(difficulty, jobDifficulty[j]);
                     dp[i][numParts] = std::min(dp[i][numParts], dp[j - 1][numParts - 1] + difficulty);
                 }
