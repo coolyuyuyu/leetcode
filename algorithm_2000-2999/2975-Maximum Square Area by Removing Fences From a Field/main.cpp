33e93717fb4961c46880aeb600c92aef62ba055d
coolyuyuyu <coolyuyuyu@gmail.com>
2023-12-31 17:07:52 +0800

    2975: initial solution

---
+++ algorithm_2000-2999/2975-Maximum Square Area by Removing Fences From a Field/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int M = 1e9 + 7;
+
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        std::sort(hFences.begin(), hFences.end());
+        hFences.insert(hFences.begin(), 1);
+        hFences.insert(hFences.end(), m);
+
+        unordered_set<int> sides;
+        for (int i = 0; i < hFences.size(); ++i) {
+            for (int j = i + 1; j < hFences.size(); ++j) {
+                sides.insert(hFences[j] - hFences[i]);
+            }
+        }
+
+        std::sort(vFences.begin(), vFences.end());
+        vFences.insert(vFences.begin(), 1);
+        vFences.insert(vFences.end(), n);
+
+        long long ret = -1;
+        for (int i = 0; i < vFences.size(); ++i) {
+            for (int j = i + 1; j < vFences.size(); ++j) {
+                int side = vFences[j] - vFences[i];
+                if (sides.find(side) != sides.end()) {
+                    ret = std::max<long long>(ret, (1LL * (side) * (side )));
+                }
+            }
+        }
+
+        return ret % M;
+    }
+};
