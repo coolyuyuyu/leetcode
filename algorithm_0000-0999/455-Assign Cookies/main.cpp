6c795e8bc30cd4d21048c93781195ab534e2855d
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-01 12:43:25 +0800

    455: update solution

--- algorithm_0000-0999/455-Assign Cookies/main.cpp
+++ algorithm_0000-0999/455-Assign Cookies/main.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     // Time: O(MlogM + NlogN)
-    int findContentChildren_Sort(vector<int>& g, vector<int>& s) {
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int m = g.size(), n = s.size();
+
         std::sort(g.begin(), g.end());
         std::sort(s.begin(), s.end());
 
-        int cnt = 0;
-        for (size_t i = 0, j = 0; i < g.size() && j < s.size();) {
+        int ret = 0;
+        for (int i = 0, j = 0; i < m && j < n;) {
             if (g[i] <= s[j]) {
-                ++i; ++j;
-                ++cnt;
+                ++i, ++j;
+                ++ret;
             }
             else {
                 ++j;
             }
         }
 
-        return cnt;
-    }
-
-    int findContentChildren(vector<int>& g, vector<int>& s) {
-        return findContentChildren_Sort(g, s);
+        return ret;
     }
 };
