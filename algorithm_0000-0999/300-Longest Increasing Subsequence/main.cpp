ea87001a603c3670adf1466461e608f68d897c78
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-05 10:57:00 +0800

    300: update solution

--- algorithm_0000-0999/300-Longest Increasing Subsequence/main.cpp
+++ algorithm_0000-0999/300-Longest Increasing Subsequence/main.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     // Time: O(n^2), Space: O(n)
-    int dp(const vector<int>& nums) {
+    int byDp(const vector<int>& nums) {
         int n = nums.size();
 
-        // dp[i]: the length of the longest strictly increasing subsequence from nums[0:i] and ending at nums[i]
+        // dp[i]: the length of the longest strictly increasing subsequence of nums[0:i] and ending at nums[i]
         int dp[n];
         std::fill(dp, dp + n, 1);
-
-        int ret = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[j] < nums[i]) {
                     dp[i] = std::max(dp[i], dp[j] + 1);
                 }
             }
+        }
 
+        int ret = 0;
+        for (int i = 0; i < n; ++i) {
             ret = std::max(ret, dp[i]);
         }
 
@@ -23,7 +24,7 @@ public:
     }
 
     // Time: O(nlogn), Space: O(n)
-    int greedy(const vector<int>& nums) {
+    int byGreedy(const vector<int>& nums) {
         vector<int> lis;
         for (int num : nums) {
             if (lis.empty() || lis.back() < num) {
@@ -39,7 +40,7 @@ public:
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        //return dp(nums);
-        return greedy(nums);
+        //return byDp(nums);
+        return byGreedy(nums);
     }
 };
