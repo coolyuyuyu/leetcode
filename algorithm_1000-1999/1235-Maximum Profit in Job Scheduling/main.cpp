26da7ce1d5f1dc3753c4420655ba5c0763b613a3
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-06 11:38:16 +0800

    1235: update solution

--- algorithm_1000-1999/1235-Maximum Profit in Job Scheduling/main.cpp
+++ algorithm_1000-1999/1235-Maximum Profit in Job Scheduling/main.cpp
@@ -3,24 +3,26 @@ public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
 
-        tuple<int, int, int> job[n + 1];
-        job[0] = {0, 0, 0};
+        tuple<int, int, int> jobs[n + 1];
+        jobs[0] = {0, 0, 0};
         for (int i = 0; i < n; ++i) {
-            job[i + 1] = {startTime[i], endTime[i], profit[i]};
+            jobs[i + 1] = {startTime[i], endTime[i], profit[i]};
         }
-        std::sort(job + 1, job + n + 1, [&](const auto& j1, const auto& j2){ return std::get<1>(j1) < std::get<1>(j2); });
+        std::sort(jobs, jobs + n + 1, [](const auto& job1, const auto& job2){ return std::get<1>(job1) < std::get<1>(job2); });
 
-        // dp[i]: the maximum profit you can take from job[1:i]
+        // dp[i]: the maximum profit you can take from jobs[1:i]
         int dp[n + 1];
-        dp[0] = std::get<2>(job[0]);
+        dp[0] = std::get<2>(jobs[0]);
+
+        int ret = 0;
         for (int i = 1; i <= n; ++i) {
+            const auto& [startTime, _, profit] = jobs[i];
             auto itr = std::upper_bound(
-                job,
-                job + i,
-                tuple<int, int, int>{0, std::get<0>(job[i]), 0},
-                [&](const auto& j1, const auto& j2){ return std::get<1>(j1) < std::get<1>(j2); });
-            int j = std::distance(job, itr) - 1;
-            dp[i] = std::max(dp[i - 1], dp[j] + std::get<2>(job[i]));
+                jobs, jobs + i,
+                tuple<int, int, int>{0, startTime, 0},
+                [](const auto& job1, const auto& job2){ return std::get<1>(job1) < std::get<1>(job2); });
+            int j = std::distance(jobs, std::prev(itr));
+            dp[i] = std::max(dp[i - 1], dp[j] + profit);
         }
 
         return dp[n];
