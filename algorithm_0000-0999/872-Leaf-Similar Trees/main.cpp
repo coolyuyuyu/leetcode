4c5542caa11a0052ac001a12f54622f4efeae22c
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-09 19:16:03 +0800

    872: update solution

--- algorithm_0000-0999/872-Leaf-Similar Trees/main.cpp
+++ algorithm_0000-0999/872-Leaf-Similar Trees/main.cpp
@@ -9,116 +9,108 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class LeafIterator {
+public:
+    LeafIterator(TreeNode* root) {
+        if (root) {
+            m_stk.push(root);
+        }
+    }
+
+    TreeNode* next() {
+        assert(hasNext());
+
+        TreeNode* ret = m_stk.top();
+        m_stk.pop();
+
+        return ret;
+    }
+
+    bool hasNext() const {
+        while (!m_stk.empty()) {
+            TreeNode* node = m_stk.top();
+            if (node->left == nullptr && node->right == nullptr) {
+                return true;
+            }
+
+            m_stk.pop();
+
+            if (node->right) {
+                m_stk.push(node->right);
+            }
+            if (node->left) {
+                m_stk.push(node->left);
+            }
+        }
+
+        return false;
+    }
+
+private:
+    mutable stack<TreeNode*> m_stk;
+};
+
 class Solution {
 public:
-    void collectLeaf_recursive(TreeNode* root, vector<int>& vals) {
-        if (!root) {
-            return;
-        }
-
-        collectLeaf_recursive(root->left, vals);
-        if (!root->left && !root->right) {
-            vals.push_back(root->val);
-        }
-        collectLeaf_recursive(root->right, vals);
-    }
-
     bool compareAll_recursive(TreeNode* root1, TreeNode* root2) {
-        vector<int> vals1, vals2;
-        collectLeaf_recursive(root1, vals1);
-        collectLeaf_recursive(root2, vals2);
-        return vals1 == vals2;
-    }
+        vector<int> nums1, nums2;
+        std::function<void(TreeNode*, vector<int>&)> f = [&](TreeNode* root, vector<int>& nums) {
+            if (!root) { return; }
 
-    vector<int> collectLeaf_iterative(TreeNode* root) {
-        stack<TreeNode*> stk;
-        if (root) {
-            stk.push(root);
-        }
-
-        vector<int> vals;
-        while (!stk.empty()) {
-            root = stk.top();
+            f(root->left, nums);
             if (root->left == nullptr && root->right == nullptr) {
-                vals.push_back(root->val);
+                nums.push_back(root->val);
             }
-            stk.pop();
+            f(root->right, nums);
+        };
+        f(root1, nums1);
+        f(root2, nums2);
 
-            if (root->right) {
-                stk.push(root->right);
-            }
-            if (root->left) {
-                stk.push(root->left);
-            }
-        }
-
-        return vals;
+        return nums1 == nums2;
     }
 
     bool compareAll_iterative(TreeNode* root1, TreeNode* root2) {
-        return collectLeaf_iterative(root1) == collectLeaf_iterative(root2);
+        std::function<vector<int>(TreeNode*)> f = [&](TreeNode* root) {
+            vector<int> nums;
+            for (stack<TreeNode*> stk({root}); !stk.empty();) {
+                auto root = stk.top();
+                stk.pop();
+
+                if (!root) { continue; }
+                if (root->left == nullptr && root->right == nullptr) {
+                    nums.push_back(root->val);
+                }
+
+                stk.push(root->right);
+                stk.push(root->left);
+            }
+
+            return nums;
+        };
+        vector<int> nums1 = f(root1);
+        vector<int> nums2 = f(root2);
+
+        return nums1 == nums2;
     }
 
-    bool compare1by1(TreeNode* root1, TreeNode* root2) {
-        class LeafIterator {
-        public:
-            LeafIterator(TreeNode* root) {
-                if (root) {
-                    m_stk.push(root);
-                }
-            }
-
-            bool hasNext() {
-                while (!m_stk.empty()) {
-                    TreeNode* node = m_stk.top();
-                    if (node->left == nullptr && node->right == nullptr) {
-                        return true;
-                    }
-                    m_stk.pop();
-
-                    if (node->right) {
-                        m_stk.push(node->right);
-                    }
-                    if (node->left) {
-                        m_stk.push(node->left);
-                    }
-                }
-
-                return false;
-            }
-
-            int next() {
-                assert(hasNext());
-                bool b = hasNext();
-                if (!b) {
-                    assert(false);
-                }
-
-                TreeNode* node = m_stk.top();
-                assert(node->left == nullptr && node->right == nullptr);
-
-                m_stk.pop();
-
-                return node->val;
-            }
-
-        private:
-            stack<TreeNode*> m_stk;
-        };
-
+    bool compare1By1(TreeNode* root1, TreeNode* root2) {
         LeafIterator itr1(root1), itr2(root2);
         while (itr1.hasNext() && itr2.hasNext()) {
-            if (itr1.next() != itr2.next()) {
+            int num1 = itr1.next()->val, num2 = itr2.next()->val;
+            if (num1 != num2) {
                 return false;
             }
         }
+        if (itr1.hasNext() || itr2.hasNext()) {
+            return false;
+        }
 
-        return !(itr1.hasNext() || itr2.hasNext());
+        return true;
     }
 
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         //return compareAll_recursive(root1, root2);
         //return compareAll_iterative(root1, root2);
-        return compare1by1(root1, root2);
+        return compare1By1(root1, root2);
     }
 };
