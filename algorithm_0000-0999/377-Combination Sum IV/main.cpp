218225c150b828c59f8515e0ecd65a14d4c4d1a4
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-14 02:48:40 +0800

    377: update solution

--- algorithm_0000-0999/377-Combination Sum IV/main.cpp
+++ algorithm_0000-0999/377-Combination Sum IV/main.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        // dp[i]: the number of possible combinations that add up to target
-        vector<unsigned long long> dp(target + 1);
+        // dp[t]: the number of possible combinations that add up to t
+        unsigned dp[target + 1];
+        std::fill(dp, dp + target + 1, 0);
         dp[0] = 1;
-        for (int i = 1; i <= target; ++i) {
+
+        for (int i = 0; i <= target; ++i) {
             for (int num : nums) {
                 if (num <= i) {
                     dp[i] += dp[i - num];
