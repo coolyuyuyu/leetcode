9d7fd5af43308bc3033c2bae4594bf3bd7b80ac5
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-19 00:50:30 +0800

    070: update solution

--- algorithm_0000-0999/070-Climbing Stairs/main.cpp
+++ algorithm_0000-0999/070-Climbing Stairs/main.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    // Time: O(n), Space: O(n)
+    // 填表法
     int dp1(int n) {
-        // dp[i]: the number of distinct ways to climb to i
-        vector<int> dp(n + 1);
-        dp[0] = dp[1] = 1;
+        // dp[i]: the number of distinct ways to take i steps
+        int dp[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
         for (int i = 2; i <= n; ++i) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
@@ -12,16 +13,20 @@ public:
         return dp[n];
     }
 
-    // Time: O(n), Space: O(1)
+    // 刷表法
     int dp2(int n) {
-        int cur = 1, pre = 1;
-        for (int i = 2; i <= n; ++i) {
-            int tmp = cur + pre;
-            pre = cur;
-            cur = tmp;
+        // dp[i]: the number of distinct ways to take i steps
+        int dp[n + 1];
+        std::fill(dp, dp + n + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            dp[i + 1] += dp[i];
+            if (i + 2 <= n) {
+                dp[i + 2] += dp[i];
+            }
         }
 
-        return cur;
+        return dp[n];
     }
 
     int climbStairs(int n) {
