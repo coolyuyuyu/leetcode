1b30a6c2687800a457ed3e83ca6a99072f08ad50
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-20 00:59:41 +0800

    931: update solution

--- algorithm_0000-0999/931-Minimum Falling Path Sum/main.cpp
+++ algorithm_0000-0999/931-Minimum Falling Path Sum/main.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-    // Space: O(m * n)
+    // Time: O(m * n), Space: O(m * n)
     int dp1(const vector<vector<int>>& matrix) {
         size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
-        //dp[i][j]: the minimum sum of  any falling path down to (i, j);
-        vector<vector<int>> dp(m, vector<int>(n));
+        // dp[r][c]: the minimum sum of any falling path down to position (r, c)
+        int dp[m][n];
         for (size_t c = 0; c < n; ++c) {
             dp[0][c] = matrix[0][c];
         }
         for (size_t r = 1; r < m; ++r) {
             for (size_t c = 0; c < n; ++c) {
-                dp[r][c] = matrix[r][c] + std::min({0 < c ? dp[r - 1][c - 1] : INT_MAX, dp[r - 1][c], (c + 1) < n ? dp[r - 1][c + 1] : INT_MAX});
+                dp[r][c] = std::min({0 < c ? dp[r - 1][c - 1] : INT_MAX, dp[r - 1][c], c + 1 < n ? dp[r - 1][c + 1] : INT_MAX}) + matrix[r][c];
             }
         }
 
-        return *std::min_element(dp[m - 1].begin(), dp[m - 1].end());
+        return *std::min_element(dp[m - 1], dp[m - 1] + n);
     }
 
-    // Space: O(n)
+    // Time: O(m * n), Space: O(n)
     int dp2(const vector<vector<int>>& matrix) {
         size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
-        //dp[i][j]: the minimum sum of  any falling path down to (i, j);
-        vector<int> dp(n);
+        // dp[r][c]: the minimum sum of any falling path down to position (r, c)
+        int dp[n];
         for (size_t c = 0; c < n; ++c) {
             dp[c] = matrix[0][c];
         }
         for (size_t r = 1; r < m; ++r) {
-            vector<int> tmp = dp;
+            int tmp[n];
+            std::copy(dp, dp + n, tmp);
             for (size_t c = 0; c < n; ++c) {
-                dp[c] = matrix[r][c] + std::min({0 < c ? tmp[c - 1] : INT_MAX, tmp[c], (c + 1) < n ? tmp[c + 1] : INT_MAX});
+                dp[c] = std::min({0 < c ? tmp[c - 1] : INT_MAX, tmp[c], c + 1 < n ? tmp[c + 1] : INT_MAX}) + matrix[r][c];
             }
         }
 
-        return *std::min_element(dp.begin(), dp.end());
+        return *std::min_element(dp, dp + n);
     }
 
-    // Space: O(1)
+    // Time: O(m * n), Space: O(1)
     int dp3(vector<vector<int>>& matrix) {
         size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
         for (size_t r = 1; r < m; ++r) {
             for (size_t c = 0; c < n; ++c) {
-                matrix[r][c] += std::min({0 < c ? matrix[r - 1][c - 1] : INT_MAX, matrix[r - 1][c], (c + 1) < n ? matrix[r - 1][c + 1] : INT_MAX});
+                matrix[r][c] += std::min({0 < c ? matrix[r - 1][c - 1] : INT_MAX, matrix[r - 1][c], c + 1 < n ? matrix[r - 1][c + 1] : INT_MAX});
             }
         }
 
