76023b68dd95a90991dc6ce09dff0924b0e0f070
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-21 01:34:04 +0800

    907: update solution

--- algorithm_0000-0999/907-Sum of Subarray Minimums/main.cpp
+++ algorithm_0000-0999/907-Sum of Subarray Minimums/main.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-    // Time: O(n^2), TLE
-    int dp(vector<int>& arr) {
-        size_t n = arr.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n));
-        dp[0] = arr;
-
-        int ans = 0;
-        for (size_t len = 1; len <= n; ++len) {
-            for (size_t i = 0; (i + len) <= n; ++i) {
-                dp[len][i] = std::min(dp[len - 1][i], arr[i + len - 1]);
-                ans = (ans + dp[len][i]) % 1000000007;
-            }
-        }
-
-        return ans;
-    }
-
-    // Time: O(n)
-    int boundary(vector<int>& arr) {
-        size_t n = arr.size();
-
-        vector<size_t> lft(n), rht(n, n); // store the index of lft/rht less element
-        stack<size_t> stk;
-        for (size_t i = 0; i < arr.size(); ++i) {
-            while (!stk.empty() && arr[i] < arr[stk.top()]) {
-                size_t j = stk.top();
-                rht[j] = i;
-
-                stk.pop();
-            }
-            lft[i] = stk.empty() ? n : stk.top();
-            stk.push(i);
-        }
-
-        int ans = 0;
-        for (size_t i = 0; i < n; ++i) {
-            size_t lftLen = lft[i] != n ? (i - lft[i]) : i + 1;
-            size_t rhtLen = rht[i] - i;
-            ans = (ans + arr[i] * lftLen * rhtLen) % 1000000007;
-        }
-        return ans;
-    }
+    int M = 1e9 + 7;
 
     int sumSubarrayMins(vector<int>& arr) {
-        //return dp(arr);
-        return boundary(arr);
+        int n = arr.size();
+
+        int preLE[n]; // prev less and equal
+        int nxtLT[n]; // next less
+
+        stack<int> stk;
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && arr[stk.top()] > arr[i]) {
+                nxtLT[stk.top()] = i;
+                stk.pop();
+            }
+            preLE[i] = stk.empty() ? -1 : stk.top();
+            stk.push(i);
+        }
+        for (; !stk.empty(); stk.pop()) {
+            nxtLT[stk.top()] = n;
+        }
+
+        int ret = 0;
+        for (int i = 0; i < n; ++i) {
+            int lftLen = i - preLE[i];
+            int rhtLen = nxtLT[i] - i;
+            ret += (1L * arr[i] * lftLen * rhtLen) % M;
+            ret %= M;
+        }
+
+        return ret;
     }
 };
