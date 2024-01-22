56eafc6085d9d8eda8053747219c5623497247ad
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-22 17:24:13 +0800

    442: update solution

--- algorithm_0000-0999/442-Find All Duplicates in an Array/main.cpp
+++ algorithm_0000-0999/442-Find All Duplicates in an Array/main.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    // Time: O(n), Space: O(1)
-    vector<int> findDuplicates_CycleSort(vector<int>& nums) {
-        vector<int> ans;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            while (nums[i] != nums[nums[i] - 1]) {
-                std::swap(nums[i], nums[nums[i] - 1]);
-            }
-        }
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != (i + 1)) {
-                ans.push_back(nums[i]);
-            }
-        }
-
-        return ans;
-    }
-
-    // Time: O(n), Space: O(1)
-    vector<int> findDuplicates_Linear(vector<int>& nums) {
-        vector<int> ans;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            nums[abs(nums[i]) - 1] *= -1;
-            if (0 < nums[abs(nums[i]) - 1] ) {
-                ans.push_back(abs(nums[i]));
-            }
-        }
-
-        return ans;
-    }
-
+    // Time: O(N), Space: O(1), index sort
     vector<int> findDuplicates(vector<int>& nums) {
-        //return findDuplicates_CycleSort(nums);
-        return findDuplicates_Linear(nums);
+        int n = nums.size();
+        nums.insert(nums.begin(), 0);
+
+        for (int i = 1; i <= n; ++i) {
+            while (nums[i] != i && nums[i] != nums[nums[i]]) {
+                std::swap(nums[i], nums[nums[i]]);
+            }
+        }
+
+        vector<int> ret;
+        for (int i = 0; i <= n; ++i) {
+            if (nums[i] != i) {
+                ret.push_back(nums[i]);
+            }
+        }
+
+        return ret;
     }
 };
