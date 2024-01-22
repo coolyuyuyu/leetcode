3b19c247f9d214eb2ca0ea215a732456571bf481
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-23 01:22:44 +0800

    1167: update solution

--- algorithm_1000-1999/1167-Minimum Cost to Connect Sticks/main.cpp
+++ algorithm_1000-1999/1167-Minimum Cost to Connect Sticks/main.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int connectSticks(vector<int>& sticks) {
-        priority_queue<int, vector<int>, greater<int>> pq;
+        priority_queue<int, vector<int>, std::greater<int>> pq;
         for (int stick : sticks) {
             pq.push(stick);
         }
 
         int ret = 0;
-        while (2 <=  pq.size()) {
-            int x = pq.top(); pq.pop();
-            int y = pq.top(); pq.pop();
-            int z = x + y;
-            pq.push(z);
-            ret += (z);
+        while (pq.size() >= 2) {
+            int stick1 = pq.top(); pq.pop();
+            int stick2 = pq.top(); pq.pop();
+            int cost = stick1 + stick2;
+            pq.push(cost);
+            ret += cost;
         }
 
         return ret;
