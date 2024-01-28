e3445b47ce2765105847d11185d4bb1139730329
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-29 00:18:51 +0800

    1074: update solution

--- algorithm_1000-1999/1074-Number of Submatrices That Sum to Target/main.cpp
+++ algorithm_1000-1999/1074-Number of Submatrices That Sum to Target/main.cpp
@@ -3,25 +3,19 @@ public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
-        // presum[r][c]: sum(matrix[0][c], matrix[1][c], ..., matrix[r][c])
-        int presum[m][n];
-        for (int c = 0; c < n; ++c) {
-            presum[0][c] = matrix[0][c];
-        }
-        for (int r = 1; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                presum[r][c] = presum[r - 1][c] + matrix[r][c];
-            }
-        }
-
         int ret = 0;
         for (int r1 = 0; r1 < m; ++r1) {
+            int colSum[n];
+            std::fill(colSum, colSum + n, 0);
             for (int r2 = r1; r2 < m; ++r2) {
                 int sum = 0;
                 unordered_map<int, int> sum2cnt = {{0, 1}};
                 for (int c = 0; c < n; ++c) {
-                    sum += presum[r2][c] - (r1 > 0 ? presum[r1 - 1][c] : 0);
+                    colSum[c] += matrix[r2][c];
+                    sum += colSum[c];
+
                     ret += sum2cnt[sum - target];
+
                     ++sum2cnt[sum];
                 }
             }
