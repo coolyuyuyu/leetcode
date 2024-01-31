35362ff69b4cfe374d3663fbb8ab90e109f3fa21
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-01 01:04:45 +0800

    Create Prime.hpp

---
+++ utility/Prime.hpp
@@ -0,0 +1,23 @@
+vector<int> findPrinesLT(int n) {
+    vector<int> primes;
+
+    bool isPrimes[n];
+    std::fill(isPrimes, isPrimes + n, true);
+
+    for (int i = 2, limit = ceil(sqrt(n)); i < n; ++i) {
+        if (isPrimes[i]) {
+            primes.push_back(i);
+        }
+        else {
+            continue;
+        }
+
+        if (i < limit) {
+            for (int j = i * i; j < n; j += i) {
+                isPrimes[j] = false;
+            }
+        }
+    }
+
+    return primes;
+}
