5511680887b51a52fc4387d87c453c6dfb6bed12
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-31 21:36:32 +0800

    739: update solution

--- algorithm_0000-0999/739-Daily Temperatures/main.cpp
+++ algorithm_0000-0999/739-Daily Temperatures/main.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     // Time: O(n), Space: O(n)
-    vector<int> f1(const vector<int>& temperatures) {
-        vector<int> ret(temperatures.size(), 0);
+    vector<int> f1(vector<int>& temperatures) {
+        vector<int> ret(temperatures.size());
 
-        stack<int> stk; // <index>
+        stack<int> stk;
         for (int i = 0; i < temperatures.size(); ++i) {
             while (!stk.empty() && temperatures[stk.top()] < temperatures[i]) {
                 ret[stk.top()] = i - stk.top();
                 stk.pop();
             }
-            stk.emplace(i);
+            stk.push(i);
         }
 
         return ret;
@@ -18,17 +18,16 @@ public:
 
     // Time: O(n), Space: O(1);
     vector<int> f2(const vector<int>& temperatures) {
-        vector<int> ret(temperatures.size(), 0);
+        vector<int> ret(temperatures.size());
 
-        stack<int> stk; // <index>
         for (int hottest = temperatures.back(), i = temperatures.size(); 0 < i--;) {
-            if (hottest <= temperatures[i]) {
+            if (temperatures[i] >= hottest) {
                 hottest = temperatures[i];
                 continue;
             }
 
             int ans = 1;
-            while (temperatures[i + ans] <= temperatures[i]) {
+            while (temperatures[i] >= temperatures[i + ans]) {
                 ans += ret[i + ans];
             }
             ret[i] = ans;
