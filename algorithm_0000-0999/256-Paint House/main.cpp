1231fda0b1aca6f8ff4a680f165760217972c083
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-31 21:36:22 +0800

    256: update solution

--- algorithm_0000-0999/256-Paint House/main.cpp
+++ algorithm_0000-0999/256-Paint House/main.cpp
@@ -3,13 +3,12 @@ public:
     int minCost(vector<vector<int>>& costs) {
         int n = costs.size();
 
-        // dp[i][j]: the minimum cost to paint houses[0:i] and houses[i] is painted color j
+        // dp[i][j]: the minimum cost to paint house[0:i] and house[i] is painted with color j
         int dp[n][3];
         dp[0][0] = costs[0][0], dp[0][1] = costs[0][1], dp[0][2] = costs[0][2];
-
         for (int i = 1; i < n; ++i) {
             dp[i][0] = std::min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
-            dp[i][1] = std::min(dp[i - 1][2], dp[i - 1][0]) + costs[i][1];
+            dp[i][1] = std::min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
             dp[i][2] = std::min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
 
