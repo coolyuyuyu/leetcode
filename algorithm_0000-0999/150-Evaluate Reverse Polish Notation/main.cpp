96bfb6f02bd70a3b62c0d38825d58be902ab7bfe
coolyuyuyu <coolyuyuyu@gmail.com>
2024-01-31 21:36:15 +0800

    150: update solution

--- algorithm_0000-0999/150-Evaluate Reverse Polish Notation/main.cpp
+++ algorithm_0000-0999/150-Evaluate Reverse Polish Notation/main.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        unordered_map<string, function<long long(long long, long long)>> operators = {
-            {"+", std::plus<long long>()},
-            {"-", std::minus<long long>()},
-            {"*", std::multiplies<long long>()},
-            {"/", std::divides<long long>()},
+        unordered_map<string, std::function<int(int, int)>> operators = {
+            {"+", std::plus<int>()},
+            {"-", std::minus<int>()},
+            {"*", std::multiplies<int>()},
+            {"/", std::divides<int>()},
         };
 
-        stack<long long> stk;
-        for (const auto& token : tokens) {
+        stack<int> stk;
+        for (const string& token : tokens) {
             auto itr = operators.find(token);
             if (itr != operators.end()) {
                 int y = stk.top(); stk.pop();
