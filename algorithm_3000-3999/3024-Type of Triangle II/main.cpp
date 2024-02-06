2e9d4cea38a064afacdc755fe680b8b5369122a8
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-07 02:00:02 +0800

    3024: initial solution

---
+++ algorithm_3000-3999/3024-Type of Triangle II/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c) {
+            return "none";
+        }
+        else if (a == b && b == c) {
+            return "equilateral";
+        }
+        else if (a == b || b == c) {
+            return "isosceles";
+        }
+        else {
+            return "scalene";
+        }
+    }
+};
