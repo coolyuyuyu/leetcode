1148647b6620d7c148980430747c5a014701d642
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-07 01:34:35 +0800

    049: update solution

--- algorithm_0000-0999/049-Group Anagrams/main.cpp
+++ algorithm_0000-0999/049-Group Anagrams/main.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    string sortStr(const string& str) {
-        vector<int> counts(26, 0);
-        for (auto c : str) {
-            ++counts[c - 'a'];
-        }
+    int minimumTimeToInitialState(string word, int k) {
+        int n = word.size();
 
-        string result;
-        result.reserve(str.size());
-
-        for (int i = 0; i < 26; ++i) {
-            for (int j = 0; j < counts[i]; ++j) {
-                result.push_back('a' + i);
+        // lps[i]: the longest length k such that word[0:k-1] == word[i-k+1:i]
+        int lps[n];
+        lps[0] = 0;
+        for (int i = 1; i < n; ++i) {
+            int j = lps[i - 1];
+            while (j > 0 && word[j] != word[i]) {
+                j = lps[j - 1];
             }
+            lps[i] = j + (word[j] == word[i] ? 1 : 0);
         }
 
-        return result;
-    }
-
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_multimap<string, string> groups;
-        for (vector<string>::const_iterator strIt = strs.begin(); strIt != strs.end(); ++strIt) {
-            groups.emplace(sortStr(*strIt), *strIt);
+        int ret = 1;
+        int j = lps[n - 1];
+        while (j > 0 && (n - j) % k != 0) {
+            j = lps[j - 1];
         }
 
-        vector<vector<string>> result;
-        for (unordered_multimap<string, string>::const_iterator it = groups.begin(); it != groups.end(); ) {
-            const string& key = it->first;
-            result.push_back(vector<string>());
-            do {
-                result.back().push_back(it->second);
-                ++it;
-            } while (it != groups.end() && key == it->first);
-        }
-        
-        return result;
+        return j == 0 ? ((n + k - 1) / k) : ((n - j) / k);
     }
 };
