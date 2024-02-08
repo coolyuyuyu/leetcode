f63bcee6e6d83b8261d6305d03c116f9f5a65433
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-08 11:27:13 +0800

    279: update solution

--- algorithm_0000-0999/279-Perfect Squares/main.cpp
+++ algorithm_0000-0999/279-Perfect Squares/main.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    int dp_topDown(int n, vector<int>& cache) {
-        if (cache.size() <= n) {
-            cache.resize(n + 1, INT_MAX);
-        }
+    int dp_topDown(int n) {
+        int cache[n + 1];
+        std::fill(cache, cache + n + 1, INT_MAX);
+        std::function<int(int)> f = [&](int n) {
+            if (n == 0) { return 0; }
+            if (cache[n] != INT_MAX) { return cache[n]; }
 
-        if (cache[n] != INT_MAX) {
-            return cache[n];
-        }
+            int& ret = cache[n];
+            for (int i = 1; i * i <= n; ++i) {
+                ret = std::min(ret, f(n - i * i) + 1);
+            }
 
-        for (int i = 1; (i * i) <= n; ++i) {
-            cache[n] = std::min(cache[n], dp_topDown(n - i * i, cache) + 1);
-        }
+            return ret;
+        };
 
-        return cache[n];
+        return f(n);
     }
 
     int dp_btmUp(int n) {
-        vector<int> dp(n + 1, INT_MAX);
+        // dp[i]: the least number of perfect square numbers that sum to i
+        int dp[n + 1];
+        std::fill(dp, dp + n + 1, INT_MAX);
         dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 1; (j * j) <= i; ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
                 dp[i] = std::min(dp[i], dp[i - j * j] + 1);
             }
         }
+
         return dp[n];
     }
 
     int numSquares(int n) {
-        //vector<int> cache = {0, 1};
-        //return dp_topDown(n, cache);
-
-        return dp_btmUp(n);
+        return dp_topDown(n);
+        //return dp_btmUp(n);
     }
 };
