162e7603927a76fc2cb69e89346b7b9b340d7eab
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-10 12:14:23 +0800

    368: update solution

--- algorithm_0000-0999/368-Largest Divisible Subset/main.cpp
+++ algorithm_0000-0999/368-Largest Divisible Subset/main.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
         int n = nums.size();
-        vector<int> dp(n, 1); // dp[i]: considering nums[0:i], the size of largest divisible subset containing nums[i]
-        vector<int> prev(n, -1);
-        int maxSize = 0, idx = -1;
+
+        std::sort(nums.begin(), nums.end());
+
+        // dp[i]: considering nums[0:i], the size of largest subset ending with nums[i]
+        int dp[n];
+        std::fill(dp, dp + n, 1);
+        int prev[n];
+        std::fill(prev, prev + n, -1);
+        int idx = 0;
+        int maxSize = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
-                if ((nums[i] % nums[j]) == 0) {
-                    if (dp[i] < (dp[j] + 1)) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[j] + 1 > dp[i]) {
                         dp[i] = dp[j] + 1;
                         prev[i] = j;
                     }
                 }
-            }
-
-            if (maxSize < dp[i]) {
-                maxSize = dp[i];
-                idx = i;
+                if (dp[i] > maxSize) {
+                    maxSize = dp[i];
+                    idx = i;
+                }
             }
         }
 
         vector<int> ret;
-        while (0 <= idx) {
+        while (idx >= 0) {
             ret.push_back(nums[idx]);
             idx = prev[idx];
         }
+
         return ret;
     }
 };
