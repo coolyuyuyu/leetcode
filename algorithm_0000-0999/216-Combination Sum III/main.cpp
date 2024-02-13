b3d5c4c155039e517ebaad139d44e9b7b4f9621a
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-14 01:47:22 +0800

    216: update solution

--- algorithm_0000-0999/216-Combination Sum III/main.cpp
+++ algorithm_0000-0999/216-Combination Sum III/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> byDP(int k, int n) {
         // dp[i][j]: list of all possible valid combinations of j numbers that sum up to i
         vector<vector<int>> dp[n + 1][k + 1];
         dp[0][0] = {{}};
@@ -17,5 +17,33 @@ public:
         }
 
         return dp[n][k];
+    }
+
+    vector<vector<int>> byDFS(int k, int n) {
+        vector<vector<int>> ret;
+        std::function<void(vector<int>&, int, int)> f = [&](vector<int>& comb, int sum, int cur) {
+            if (sum > n) { return; }
+            if (comb.size() > k) { return; }
+            if (comb.size() == k && sum == n) {
+                ret.push_back(comb);
+                return;
+            }
+
+            for (int d = cur; d <= 9; ++d) {
+                comb.push_back(d);
+                f(comb, sum + d, d + 1);
+                comb.pop_back();
+            }
+        };
+
+        vector<int> comb;
+        f(comb, 0, 1);
+
+        return ret;
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        //return byDP(k, n);
+        return byDFS(k, n);
     }
 };
