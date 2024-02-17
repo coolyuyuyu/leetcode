72452e1bb51b024f4c4314ec4d7a93f30ec8dbdc
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-17 16:12:20 +0800

    1642: update solution

--- algorithm_1000-1999/1642-Furthest Building You Can Reach/main.cpp
+++ algorithm_1000-1999/1642-Furthest Building You Can Reach/main.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
-    int furthestBuilding_Heap(vector<int>& heights, int bricks, int ladders) {
-        long sumBricks = 0;
-        long sumBricksByLadder = 0;
-        priority_queue<long, vector<long>, greater<long>> pq; // min_heap stores top "laddeers" differnces
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int n = heights.size();
 
-        size_t i = 1;
-        for (; i < heights.size(); ++i) {
-            long diff = heights[i] - heights[i - 1];
+        priority_queue<int, vector<int>, std::greater<int>> pq;
+        int sumBricks = 0;
+        for (int i = 1; i < n; ++i) {
+            int diff = heights[i] - heights[i - 1];
             if (diff <= 0) {
                 continue;
             }
 
-            sumBricks += diff;
-            if (pq.size() < ladders) {
-                sumBricksByLadder += diff;
-
-                pq.push(diff);
-            }
-            else if (!pq.empty() && pq.top() < diff) {
-                sumBricksByLadder -= pq.top();
-                sumBricksByLadder += diff;
-
+            pq.push(diff);
+            if (pq.size() > ladders) {
+                sumBricks += pq.top();
                 pq.pop();
-                pq.push(diff);
             }
 
-            if (bricks < (sumBricks - sumBricksByLadder)) {
-                break;
+            if (sumBricks > bricks) {
+                return i - 1;
             }
         }
 
-        return (i - 1);
-    }
-
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        return furthestBuilding_Heap(heights, bricks, ladders);
+        return n - 1;
     }
 };
