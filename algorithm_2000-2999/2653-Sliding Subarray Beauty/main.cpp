15c984b1151faabd43f29b6130c6717f0f0a97f4
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-21 02:29:44 +0800

    2653: update soluton

--- algorithm_2000-2999/2653-Sliding Subarray Beauty/main.cpp
+++ algorithm_2000-2999/2653-Sliding Subarray Beauty/main.cpp
@@ -3,28 +3,28 @@ public:
     vector<int> byBucket(const vector<int>& nums, int k, int x) {
         int n = nums.size();
 
-        int cnts[101];
-        std::fill(cnts, cnts + 101, 0);
-        std::function<int()> findKthSmallest = [&]() {
+        int cnts[102];
+        std::fill(cnts, cnts + 102, 0);
+
+        std::function<int()> findXthElement = [&]() {
             int cnt = 0;
-            for (int i = 0; i < 50; ++i) {
+            for (int i = 0; i < 102; ++i) {
                 cnt += cnts[i];
-                if (x <= cnt) {
+                if (cnt >= x) {
                     return i - 50;
                 }
             }
 
-            return 0;
+            assert(false);
+            return 102;
         };
 
         vector<int> ret(n - k + 1);
         for (int i = 0; i < n; ++i) {
             ++cnts[nums[i] + 50];
-
             if (i >= k - 1) {
-                ret[i - k + 1] = findKthSmallest();
-
-                --cnts[nums[i - k + 1] + 50];
+                ret[i + 1 - k] = std::min(findXthElement(), 0);
+                --cnts[nums[i + 1 - k] + 50];
             }
         }
 
@@ -35,7 +35,6 @@ public:
         int n = nums.size();
 
         multiset<int> ms1, ms2;
-
         vector<int> ret(n - k + 1);
         for (int i = 0; i < n; ++i) {
             ms1.insert(nums[i]);
@@ -46,17 +45,16 @@ public:
             }
 
             if (i >= k - 1) {
-                ret[i - k + 1] = std::min(*ms1.rbegin(), 0);
+                ret[i + 1 - k] = std::min(*ms1.rbegin(), 0);
 
-                int toErase = nums[i - k + 1];
-                auto itr2 = ms2.find(toErase);
+                int toErased = nums[i + 1 - k];
+                auto itr2 = ms2.find(toErased);
                 if (itr2 != ms2.end()) {
                     ms2.erase(itr2);
                 }
                 else {
-                    auto itr1 = ms1.find(toErase);
-                    ms1.erase(itr1);
-                    if (ms1.size() < k && !ms2.empty()) {
+                    ms1.erase(ms1.find(toErased));
+                    if (!ms2.empty()) {
                         itr2 = ms2.begin();
                         ms1.insert(*itr2);
                         ms2.erase(itr2);
