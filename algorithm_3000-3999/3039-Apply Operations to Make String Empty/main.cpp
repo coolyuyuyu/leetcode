76f5b62d1724dbaffbb6de6b4434cf128cc68e94
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-22 02:16:40 +0800

    3039: update solution

--- algorithm_3000-3999/3039-Apply Operations to Make String Empty/main.cpp
+++ algorithm_3000-3999/3039-Apply Operations to Make String Empty/main.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     string lastNonEmptyString(string s) {
         int cnts[26];
+        int indexes[26];
         std::fill(cnts, cnts + 26, 0);
-        for (char c : s) {
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
             ++cnts[c - 'a'];
+            indexes[c - 'a'] = i;
         }
         int maxCnt = *std::max_element(cnts, cnts + 26);
 
         string ret;
-        std::fill(cnts, cnts + 26, 0);
-        for (char c : s) {
-            if (++cnts[c - 'a'] == maxCnt) {
-                ret += c;
+        for (int i = 0; i < 26; ++i) {
+            if (cnts[i] == maxCnt) {
+                ret.push_back('a' + i);
             }
         }
+        std::sort(ret.begin(), ret.end(), [&](char c1, char c2){ return indexes[c1 - 'a'] < indexes[c2 - 'a']; });
 
         return ret;
     }
