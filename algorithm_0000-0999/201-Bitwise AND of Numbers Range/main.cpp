c766231dd239701abdf036c70c771c2b26e70256
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-21 10:11:50 +0800

    201: update solution

--- algorithm_0000-0999/201-Bitwise AND of Numbers Range/main.cpp
+++ algorithm_0000-0999/201-Bitwise AND of Numbers Range/main.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
-    int rangeBitwiseAnd(int m, int n) {
-        while (m < n) {
-            n = n & (n - 1); // clear last bit
+    int f1(int left, int right) {
+        int ret = 0;
+        for (int i = 31; i >= 0; --i) {
+            int b1 = left >> i, b2 = right >> i;
+            if (b1 != b2) {
+                break;
+            }
+            ret |= b1 << i;
         }
 
-        return n;
+        return ret;
+    }
+
+    int f2(int left, int right) {
+        while (left < right) {
+            right = right & (right - 1);
+        }
+
+        return right;
+    }
+
+    int rangeBitwiseAnd(int left, int right) {
+        //return f1(left, right);
+        return f2(left, right);
     }
 };
