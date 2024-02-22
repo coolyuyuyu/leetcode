2babfa6dfe5ac424d1319372ae1dc853055cda8f
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-23 02:42:01 +0800

    1245: update solution

--- algorithm_1000-1999/1245-Tree Diameter/main.cpp
+++ algorithm_1000-1999/1245-Tree Diameter/main.cpp
@@ -1,42 +1,72 @@
 class Solution {
 public:
-    int depth_Recursive(vector<set<int>>& graph, int cur, int pre, int& diameter) {
-        int depth1st = 0, depth2nd = 0;
-        for (int neighbor : graph[cur]) {
-            if (neighbor == pre) {
-                continue;
-            }
+    int f1(vector<vector<int>>& edges) {
+        // Assume the longest path of the graph is in between node a and b.
+        // Start from any node s, the farest node against s must be a or b
 
-            int depth = depth_Recursive(graph, neighbor, cur, diameter);
-            if (depth1st < depth) {
-                depth2nd = depth1st;
-                depth1st = depth;
-            }
-            else if (depth2nd < depth){
-                depth2nd = depth;
-            }
+        int n = edges.size() + 1;
+        vector<int> graph[n];
+        for (const auto& edge : edges) {
+            int a = edge[0], b = edge[1];
+            graph[a].push_back(b);
+            graph[b].push_back(a);
         }
 
-        diameter = max(diameter, depth1st + depth2nd);
+        std::function<pair<int, int>(int, int)> f = [&](int cur, int pre) {
+            pair<int, int> ret = {-1, cur};
+            for (int nxt : graph[cur]) {
+                if (nxt == pre) { continue; }
 
-        return depth1st + 1;
+                auto [d, v] = f(nxt, cur);
+                if (d > ret.first) {
+                    ret = {d, v};
+                }
+            }
+            ret.first += 1;
+
+            return ret;
+        };
+
+        auto [_, s] = f(0, -1);
+        auto [d , t] = f(s, -1);
+        return d;
+    }
+
+    int f2(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+        vector<int> graph[n];
+        for (const auto& edge : edges) {
+            int a = edge[0], b = edge[1];
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+
+        int ret = 0;
+        std::function<int(int, int)> f = [&](int cur, int pre) {
+            int d1 = 0, d2 = 0;
+            for (int nxt : graph[cur]) {
+                if (nxt == pre) { continue; }
+
+                int d = f(nxt, cur);
+                if (d >= d1) {
+                    d2 = d1;
+                    d1 = d;
+                }
+                else if (d > d2) {
+                    d2 = d;
+                }
+            }
+            ret = std::max(ret, d1 + d2);
+
+            return d1 + 1;
+        };
+        f(0, -1);
+
+        return ret;
     }
 
     int treeDiameter(vector<vector<int>>& edges) {
-        vector<set<int>> graph;
-        for (const auto& edge : edges) {
-            if (graph.size() <= edge[0]) {
-                graph.resize(edge[0] + 1);
-            }
-            if (graph.size() <= edge[1]) {
-                graph.resize(edge[1] + 1);
-            }
-            graph[edge[0]].insert(edge[1]);
-            graph[edge[1]].insert(edge[0]);
-        }
-
-        int diameter = 0;
-        depth_Recursive(graph, 0, -1, diameter);
-        return diameter;
+        return f1(edges);
+        //return f2(edges);
     }
 };
