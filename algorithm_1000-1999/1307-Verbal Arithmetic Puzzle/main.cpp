72fe9f1644543a3ca6c6a8001c3230f7ea58f301
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-24 01:38:20 +0800

    1307: update solution

--- algorithm_1000-1999/1307-Verbal Arithmetic Puzzle/main.cpp
+++ algorithm_1000-1999/1307-Verbal Arithmetic Puzzle/main.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isSolvable(vector<string>& words, string result) {
-        int digits[26];
-        std::fill(digits, digits + 26, -1);
+        int digits[128];
+        std::fill(digits, digits + 128, -1);
 
         int m = words.size();
         int n = 0;
@@ -21,7 +21,7 @@ public:
             else if (i >= m) {
                 if (j >= result.size()) { return false; }
                 char c = result[j];
-                int d = digits[c - 'A'];
+                int d = digits[c];
                 if (d >= 0) {
                     if (d != sum % 10) { return false; }
                     if (j + 1 == result.size() && j > 0 && d == 0) { return false; }
@@ -30,14 +30,14 @@ public:
                 else {
                     d = sum % 10;
                     if (state & (1 << d)) { return false; }
-                    if (j + 1 == result.size() && j > 0 && d == 0) { return false; }
-                    digits[c - 'A'] = d;
+                    if (d == 0 && j + 1 == result.size() && j > 0 ) { return false; }
+                    digits[c] = d;
                     state |= (1 << d);
                     if (dfs(0, j + 1, sum / 10)) {
                         return true;
                     }
                     state &= ~(1 << d);
-                    digits[c - 'A'] = -1;
+                    digits[c] = -1;
                     return false;
                 }
             }
@@ -47,21 +47,21 @@ public:
 
             const string& word = words[i];
             char c = word[j];
-            int d = digits[c - 'A'];
+            int d = digits[c];
             if (d >= 0) {
-                if (j + 1 == word.size() && j > 0 && d == 0) { return false; }
-                return dfs(i + 1, j, sum + digits[c - 'A']);
+                if (d == 0 && j + 1 == word.size() && j > 0) { return false; }
+                return dfs(i + 1, j, sum + digits[c]);
             }
             else {
                 for (d = (j + 1 == word.size() && j > 0) ? 1 : 0; d < 10; ++d) {
                     if (state & (1 << d)) { continue; }
-                    digits[c - 'A'] = d;
+                    digits[c] = d;
                     state |= (1 << d);
                     if (dfs(i + 1, j, sum + d)) {
                         return true;
                     }
                     state &= ~(1 << d);
-                    digits[c - 'A'] = -1;
+                    digits[c] = -1;
                 }
                 return false;
             }
