eeab2e48ef4ecb60d3ca1b6b6c75e45f336b4bc7
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-26 21:01:21 +0800

    698: update solution

--- algorithm_0000-0999/698-Partition to K Equal Sum Subsets/main.cpp
+++ algorithm_0000-0999/698-Partition to K Equal Sum Subsets/main.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     bool dfs(vector<int>& nums, int k) {
+        int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
         int subsum = sum / k;
         if (subsum * k != sum) {
             return false;
         }
 
-        int n = nums.size();
-
-        std::sort(nums.rbegin(), nums.rend()); // optimize
+        std::sort(nums.begin(), nums.end()); // optimization 1: to avoid duplicate branch
+        std::reverse(nums.begin(), nums.end()); // optimization 2: sum is getting exceed earlier
 
         bool visited[n];
         std::fill(visited, visited + n, false);
-        std::function<bool(int, int, int)> f = [&](int group, int idx, int sum) {
-            if (group == k) {
-                return true;
-            }
-            else if (sum > subsum) {
-                return false;
-            }
-            else if (sum == subsum) {
-                return f(group + 1, 0, 0);
-            }
+        std::function<bool(int, int, int)> f = [&](int cur, int group, int sum) {
+            if (group == k) { return true; }
+            else if (sum > subsum) { return false; }
+            else if (sum == subsum) { return f(0, group + 1, 0); }
 
-            for (int i = idx, last = -1; i < n; ++i) {
+            for (int i = cur, last = -1; i < n; ++i) {
                 if (visited[i]) { continue; }
-                if (nums[i] == last) { continue; } // optimize
+                if (nums[i] == last) { continue; }
 
                 visited[i] = true;
                 last = nums[i];
-                if (f(group, i + 1, sum + nums[i])) {
+                if (f(i + 1, group, sum + nums[i])) {
                     return true;
                 }
                 visited[i] = false;
@@ -38,21 +32,20 @@ public:
 
             return false;
         };
+
         return f(0, 0, 0);
     }
 
     bool dp(vector<int>& nums, int k) {
-        int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        int m = 1 << n;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
         int subsum = sum / k;
         if (subsum * k != sum) {
             return false;
         }
 
-        int n = nums.size();
-        int m = 1 << n;
-
-        // state: the binary representation whether nums[i] is grouped
-        // dp[state]; sum of grouped numbs % subsum
+        // dp[state]: the sum of subsequence of nums presented in binary representation.
         int dp[m];
         std::fill(dp, dp + m, -1);
         dp[0] = 0;
