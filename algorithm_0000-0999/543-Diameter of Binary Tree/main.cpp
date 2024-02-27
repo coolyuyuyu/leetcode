25113f7f50e138c03837a1bbba32b51704ccd5ec
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-27 18:45:49 +0800

    543: update solution

--- algorithm_0000-0999/543-Diameter of Binary Tree/main.cpp
+++ algorithm_0000-0999/543-Diameter of Binary Tree/main.cpp
@@ -11,61 +11,19 @@
  */
 class Solution {
 public:
-    int diameterOfBinaryTree_Recursive(TreeNode* root, int& height) {
-        if (!root) {
-            height = 0;
-            return 0;
-        }
-
-        int lftHeight;
-        int lftDiameter = diameterOfBinaryTree_Recursive(root->left, lftHeight);
-        int rhtHeight;
-        int rhtDiameter = diameterOfBinaryTree_Recursive(root->right, rhtHeight);
-
-        height = max(lftHeight, rhtHeight) + 1;
-        return max(max(lftDiameter, rhtDiameter), lftHeight + rhtHeight);
-    }
-
-    int diameterOfBinaryTree_Iterative(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-
-        int diameter = 0;
-        map<TreeNode*, int> m; // <node, height>
-
-        // postorder traversal
-        stack<pair<TreeNode*, bool>> stk;
-        stk.emplace(root, false);
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            if (stk.top().second) {
-                stk.pop();
-
-                int lftHeight = (node->left ? m[node->left] : 0);
-                int rhtHeight = (node->right ? m[node->right] : 0);
-
-                m[node] = max(lftHeight, rhtHeight) + 1;
-                diameter = max(diameter, lftHeight + rhtHeight);
-            }
-            else {
-                stk.top().second = true;
-                if (node->right) {
-                    stk.emplace(node->right, false);
-                }
-                if (node->left) {
-                    stk.emplace(node->left, false);
-                }
-            }
-        }
-
-        return diameter;
-    }
-
     int diameterOfBinaryTree(TreeNode* root) {
-        //int height;
-        //return diameterOfBinaryTree_Recursive(root, height);
+        int ret = 0;
+        std::function<int(TreeNode*)> dfs = [&](TreeNode* root) {
+            if (!root) { return 0; }
 
-        return diameterOfBinaryTree_Iterative(root);
+            int lftH = dfs(root->left);
+            int rhtH = dfs(root->right);
+            ret = std::max(ret, lftH+ rhtH);
+            return std::max(lftH, rhtH) + 1;
+        };
+
+        dfs(root);
+
+        return ret;
     }
 };
