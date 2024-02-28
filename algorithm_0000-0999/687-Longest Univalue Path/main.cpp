8a1e6c54d79855426dacfafd0915add66607b13c
coolyuyuyu <coolyuyuyu@gmail.com>
2024-02-29 00:50:06 +0800

    687: update solution

--- algorithm_0000-0999/687-Longest Univalue Path/main.cpp
+++ algorithm_0000-0999/687-Longest Univalue Path/main.cpp
@@ -11,77 +11,24 @@
  */
 class Solution {
 public:
-    int longestUnivaluePath_Recursive(TreeNode* root, int& maxNumOfNodes) {
-        if (!root) {
-            return 0;
-        }
-
-        int lftNumOfNodesTopDown = longestUnivaluePath_Recursive(root->left, maxNumOfNodes);
-        if (0 < lftNumOfNodesTopDown && root->left->val != root->val) {
-            lftNumOfNodesTopDown = 0;
-        }
-
-        int rhtNumOfNodesTopDown = longestUnivaluePath_Recursive(root->right, maxNumOfNodes);
-        if (0 < rhtNumOfNodesTopDown && root->right->val != root->val) {
-            rhtNumOfNodesTopDown = 0;
-        }
-
-        int numOfNodes = lftNumOfNodesTopDown + 1 + rhtNumOfNodesTopDown;
-        if (maxNumOfNodes < numOfNodes) {
-            maxNumOfNodes = numOfNodes;
-        }
-
-        return (1 + max(lftNumOfNodesTopDown, rhtNumOfNodesTopDown));
-    }
-
-    void longestUnivaluePath_Iterative(TreeNode* root, int& maxNumOfNodes) {
-        map<TreeNode*, int> m; // <node, maxNumOfNodesTopDown>
-
-        // postorder traversal
-        stack<pair<TreeNode*, bool>> stk; // <node, visited>
-        if (root) {
-            stk.emplace(root, false);
-        }
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            bool visited = stk.top().second;
-            stk.pop();
-
-            if (visited) {
-                int lftNumOfNodesTopDown = (node->left ? m[node->left] : 0);
-                if (0 < lftNumOfNodesTopDown && node->left->val != node->val) {
-                    lftNumOfNodesTopDown = 0;
-                }
-                int rhtNumOfNodesTopDown = (node->right ? m[node->right] : 0);
-                if (0 < rhtNumOfNodesTopDown && node->right->val != node->val) {
-                    rhtNumOfNodesTopDown = 0;
-                }
-
-                int numOfNodes = lftNumOfNodesTopDown + 1 + rhtNumOfNodesTopDown;
-                if (maxNumOfNodes < numOfNodes) {
-                    maxNumOfNodes = numOfNodes;
-                }
-
-                m[node] = (1 + max(lftNumOfNodesTopDown, rhtNumOfNodesTopDown));
-            }
-            else {
-                stk.emplace(node, true);
-                if (node->right) {
-                    stk.emplace(node->right, false);
-                }
-                if (node->left) {
-                    stk.emplace(node->left, false);
-                }
-            }
-        }
-    }
-
     int longestUnivaluePath(TreeNode* root) {
-        int maxNumOfNodes = 0;
+        int ret = 0;
+        std::function<int(TreeNode*)> dfs = [&](TreeNode* root) {
+            if (!root) { return 0; }
 
-        longestUnivaluePath_Recursive(root, maxNumOfNodes);
-        //longestUnivaluePath_Iterative(root, maxNumOfNodes);
+            int lftH = dfs(root->left);
+            if (root->left && root->left->val != root->val) {
+                lftH = 0;
+            }
+            int rhtH = dfs(root->right);
+            if (root->right && root->right->val != root->val) {
+                rhtH = 0;
+            }
+            ret = std::max(ret, lftH + rhtH);
+            return std::max(lftH, rhtH) + 1;
+        };
+        dfs(root);
 
-        return (0 < maxNumOfNodes ? maxNumOfNodes - 1 : 0);
+        return ret;
     }
 };
