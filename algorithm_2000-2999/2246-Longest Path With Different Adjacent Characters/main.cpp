3753c0e8bebe500582c55d63eeaafcaf6dc92035
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-02 02:39:32 +0800

    2246: update solution

--- algorithm_2000-2999/2246-Longest Path With Different Adjacent Characters/main.cpp
+++ algorithm_2000-2999/2246-Longest Path With Different Adjacent Characters/main.cpp
@@ -2,50 +2,46 @@ class Solution {
 public:
     int longestPath(vector<int>& parent, string s) {
         int n = parent.size();
-        vector<int> degrees(n, 0);
-        vector<vector<int>> childs(n);
-        for (int i = 0; i < n; ++i) {
-            if (parent[i] == -1) {
-                continue;
-            }
 
-            ++degrees[parent[i]];
-            childs[parent[i]].push_back(i);
-        }
-
-        queue<int> nodes;
-        for (int i = 0; i < n; ++i) {
-            if (degrees[i] == 0) {
-                nodes.push(i);
+        vector<int> childs[n];
+        int out[n]; // out degree
+        std::fill(out, out + n, 0);
+        for (int node = 0; node < n; ++node) {
+            if (parent[node] >= 0) {
+                childs[parent[node]].push_back(node);
+                ++out[parent[node]];
             }
         }
 
-        vector<int> longestVerticalLens(n, 0);
+        queue<int> q;
+        for (int node = 0; node < n; ++node) {
+            if (out[node] == 0) {
+                q.push(node);
+            }
+        }
+
         int ret = 0;
-        while (!nodes.empty()) {
-            int i = nodes.front();
-            nodes.pop();
+        int len[n]; // the length of longest valid down path
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
 
-            int lenA = 0, lenB = 0;
-            for (int child : childs[i]) {
-                if (s[child] != s[i]) {
-                    if (lenA <= longestVerticalLens[child]) {
-                        lenB = lenA;
-                        lenA = longestVerticalLens[child];
-                    }
-                    else if (lenB < longestVerticalLens[child]) {
-                        lenB = longestVerticalLens[child];
-                    }
+            int len1 = 0, len2 = 0;
+            for (int child : childs[node]) {
+                if (s[node] == s[child]) { continue; }
+                if (len[child] >= len1) {
+                    len2 = len1, len1 = len[child];
+                }
+                else if (len[child] > len2) {
+                    len2 = len[child];
                 }
             }
-            longestVerticalLens[i] = 1 + lenA;
-            ret = std::max({ret, 1 + lenA, 1 + lenA + lenB});
+            len[node] = 1 + len1;
 
-            if (parent[i] == -1) {
-                continue;
-            }
-            if (--degrees[parent[i]] == 0) {
-                nodes.push(parent[i]);
+            ret = std::max(ret, len1 + 1 + len2);
+
+            if (parent[node] >= 0 && --out[parent[node]] == 0) {
+                q.push(parent[node]);
             }
         }
 
