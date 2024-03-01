03ba17b8a98e87519f3d29f9671a547e74fa3698
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-02 02:39:27 +0800

    382: update solution

--- algorithm_0000-0999/382-Linked List Random Node/main.cpp
+++ algorithm_0000-0999/382-Linked List Random Node/main.cpp
@@ -8,74 +8,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-class Strategy {
-public:
-    virtual int getRandom(ListNode* head) = 0;
-};
-
-class TwoPassSampling : public Strategy {
-public:
-    int getRandom(ListNode* head) {
-        vector<ListNode*> nodes;
-        while (head) {
-            nodes.push_back(head);
-            head = head->next;
-        }
-        return nodes[rand() % nodes.size()]->val;
-    }
-};
-
-class ReservoirSampling : public Strategy {
-public:
-    int getRandom(ListNode* head) {
-        return getRandom(head, 1)[0];
-    }
-
-private:
-    vector<int> getRandom(ListNode* head, int k) {
-        vector<int> vals(k);
-
-        for (int i = 0; i < k; ++i, head = head->next) {
-            vals[i] = head->val;
-        }
-
-        for (int i = k; head; ++i, head = head->next) {
-            int j = rand() % (i + 1);
-            if (j < k) {
-                vals[j] = head->val;
-            }
-        }
-
-        return vals;
-    }
-};
-
-// [a]
-// P1 = a
-
-// [a,b]
-// P2 = 1/2a + 1/2b
-
-// [a,b,c]
-// P3 = 1/3a + 1/3b + 1/3c
-//    = 2/3(x2) + 1/3c
-
 class Solution {
 public:
     Solution(ListNode* head)
         : m_head(head) {
-        //m_strategy = new TwoPassSampling();
-        m_strategy = new ReservoirSampling();
     }
 
     int getRandom() {
-        return m_strategy->getRandom(m_head);
+        // reservoir sampling
+
+        int ret;
+
+        int i = 0;
+        for (ListNode* node = m_head; node; node = node->next) {
+            if (std::rand() % ++i == 0) {
+                ret = node->val;
+            }
+        }
+
+        return ret;
     }
 
 private:
     ListNode* m_head;
-    Strategy* m_strategy;
 };
 
 /**
