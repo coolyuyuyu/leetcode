ead7118b39b06ed5a6ef367faf224a674ad6a2d5
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-02 02:39:42 +0800

    2864: update solution

--- algorithm_2000-2999/2864-Maximum Odd Binary Number/main.cpp
+++ algorithm_2000-2999/2864-Maximum Odd Binary Number/main.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
         int n = s.size();
-        int cnt1 = 0;
-        for (char& c : s) {
+
+        int cnt = 0;
+        for (char c : s) {
             if (c == '1') {
-                ++cnt1;
-                c = '0';
+                ++cnt;
             }
         }
 
-        if (cnt1 > 0) {
-            s.back() = '1';
+        string ret(n, '0');
+        for (int i = 0; i + 1 < cnt; ++i) {
+            ret[i] = '1';
         }
-        for (int i = 0; i + 1 < cnt1; ++i) {
-            s[i] = '1';
-        }
+        ret.back() = '1';
 
-        return s;
+        return ret;
     }
 };
