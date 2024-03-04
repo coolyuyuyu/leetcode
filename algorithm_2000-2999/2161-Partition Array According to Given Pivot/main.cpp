2c19d4fe3d18c6d4b366ea13868ea1abd21a99df
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-05 01:45:29 +0800

    2161: initial solution

---
+++ algorithm_2000-2999/2161-Partition Array According to Given Pivot/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+
+        vector<int> ret(n);
+
+        int lft = 0, rht = n - 1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < pivot) {
+                ret[lft++] = nums[i];
+            }
+        }
+        for (int i = n; 0 < i--;) {
+            if (nums[i] > pivot) {
+                ret[rht--] = nums[i];
+            }
+        }
+        for (; lft <= rht;) {
+            ret[lft++] = pivot;
+        }
+
+        return ret;
+    }
+};
