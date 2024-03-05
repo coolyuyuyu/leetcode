b62ad9874572be2c387ee476a6bcf92fcbd48ed5
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-06 02:51:31 +0800

    470: update solution

--- algorithm_0000-0999/470-Implement Rand10() Using Rand7()/main.cpp
+++ algorithm_0000-0999/470-Implement Rand10() Using Rand7()/main.cpp
@@ -4,7 +4,7 @@
 
 class Solution {
 public:
-    int rand10() {
+    int f1() {
         int group;
         do {
             group = rand7();
@@ -17,9 +17,42 @@ public:
 
         return (group - 1) * 2 + 1 + (parity & 1);
     }
+
+    int f2() {
+        int num = rand7() - 1;
+        do {
+            num %= 49;
+            num = num * 7 + rand7() - 1;
+        } while (num >= 40);
+
+        return num % 10 + 1;
+    }
+
+    std::vector<int> m_nums;
+    int f3() {
+        if (m_nums.empty()) {
+            int num = 0;
+            for (int i = 0; i < 6; ++i) {
+                num = num * 7 + rand7() - 1;
+            }
+            do {
+                num %= 823543;
+                num = num * 7 + rand7() - 1;
+            } while (num >= 800000);
+
+            for (int i = 0; i < 5; ++i, num /= 10) {
+                m_nums.push_back(num % 10 + 1);
+            }
+        }
+
+        int ret = m_nums.back();
+        m_nums.pop_back();
+        return ret;
+    }
+
+    int rand10() {
+        //return f1();
+        //return f2();
+        return f3();
+    }
 };
-
-/*
-1 2 3 4 5 6 7
-
-*/
