32338ebffe3e5da165a69a1d99d6cb47c25e06b6
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-10 23:12:32 +0800

    710: update solution

--- algorithm_0000-0999/710-Random Pick with Blacklist/main.cpp
+++ algorithm_0000-0999/710-Random Pick with Blacklist/main.cpp
@@ -3,44 +3,74 @@ public:
     virtual int pick() = 0;
 };
 
-class MapBlack : public Strategy {
+class BSearchKthWhite : public Strategy {
 public:
-    MapBlack(int n, vector<int>& blacklist)
+    BSearchKthWhite(int n, vector<int>& blacklist)
         : m_n(n)
-        , m_m(blacklist.size()){
+        , m_blacks(blacklist) {
+        sort(m_blacks.begin(), m_blacks.end());
+    }
 
-        unordered_set<int> blackNums(blacklist.begin(), blacklist.end());
-        int num = m_n - 1;
-        for (int blackNum : blackNums) {
-            if (blackNum >= m_n - m_m) { continue; }
-            while (blackNums.find(num) != blackNums.end()) {
-                num--;
+    int pick() {
+        int k = rand() % (m_n - m_blacks.size()) ;
+
+        int lo = 0, hi = m_n - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid - std::distance(m_blacks.begin(), std::upper_bound(m_blacks.begin(), m_blacks.end(), mid)) >= k) {
+                hi = mid;
             }
-            m_mappedNums[blackNum] = num--;
+            else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+private:
+    int m_n;
+    vector<int>& m_blacks;
+};
+
+class MapBlack2White : public Strategy {
+public:
+    MapBlack2White(int n, vector<int>& blacklist)
+        : m_n(n)
+        , m_m(blacklist.size()) {
+        unordered_set<int> blacks(blacklist.begin(), blacklist.end());
+        int white = m_n - 1;
+        for (int black : blacklist) {
+            if (black >= m_n - m_m) { continue; }
+            while (blacks.find(white) != blacks.end()) {
+                --white;
+            }
+            m_black2white[black] = white--;
         }
     }
 
     int pick() {
-        int idx = rand() % (m_n - m_m);
-        if (m_mappedNums.find(idx) != m_mappedNums.end()) {
-            return m_mappedNums[idx];
+        int k = rand() % (m_n - m_m);
+        if (m_black2white.find(k) == m_black2white.end()) {
+            return k;
         }
         else {
-            return idx;
+            return m_black2white[k];
         }
     }
 
 private:
     int m_n;
     int m_m;
-    unordered_map<int, int> m_mappedNums;
+    unordered_map<int, int> m_black2white;
 };
 
 
 class Solution {
 public:
     Solution(int n, vector<int>& blacklist)
-        : m_strategy(new MapBlack(n, blacklist)) {
+        //: m_strategy(new BSearchKthWhite(n, blacklist)) {
+        : m_strategy(new MapBlack2White(n, blacklist)) {
+
     }
 
     int pick() {
