70da336819385291ea279debe70f364203ed0653
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-14 10:09:13 +0800

    930: update solution

--- algorithm_0000-0999/930-Binary Subarrays With Sum/main.cpp
+++ algorithm_0000-0999/930-Binary Subarrays With Sum/main.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    // hash + prefix sum
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int, int> m; // key: presum, val: number of prefix
-        m[0] = 1;
         int ret = 0;
-        for (int presum = 0, i = 0; i < nums.size(); ++i) {
-            presum += nums[i];
-            if (goal <= presum) {
-                ret += m[presum - goal];
-            }
-            ++m[presum];
+        unordered_map<int, int> presum2cnt = {{0, 1}};
+        for (int i = 0, sum = 0; i < nums.size();; ++i) {
+            sum += nums[i];
+            ret += presum2cnt[sum - goal];
+            ++presum2cnt[sum];
         }
+
         return ret;
     }
 };
-
-/*
-X X X X X X X
-    j       i
-
-sum[j+1:i] = presum[i] - presum[j] = k
-*/
