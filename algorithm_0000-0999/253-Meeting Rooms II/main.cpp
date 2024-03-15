dd91804690bc8de837fa764f759df686e0756112
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-15 16:18:00 +0800

    253: update solution

--- algorithm_0000-0999/253-Meeting Rooms II/main.cpp
+++ algorithm_0000-0999/253-Meeting Rooms II/main.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int sweepline(const vector<vector<int>>& intervals) {
-        map<int, int> diffs; // time -> diff
+        map<int, int> diffs;
         for (const auto& interval : intervals) {
-            ++diffs[interval[0]];
-            --diffs[interval[1]];
+            int start = interval[0], end = interval[1];
+            diffs[start] += 1;
+            diffs[end] -= 1;
         }
 
         int ret = 0;
         int sum = 0;
-        for (auto [_, diff] : diffs) {
+        for (const auto& [_, diff] : diffs) {
             sum += diff;
             ret = std::max(ret, sum);
         }
@@ -18,19 +19,19 @@ public:
     }
 
     int heap(vector<vector<int>>& intervals) {
-        auto comp1 = [](const auto& interval1, const auto& interva2) {
-            return interval1[0] < interva2[0];
+        auto comp1 = [](const auto& interval1, const auto& interval2) {
+            return interval1[0] < interval2[0];
         };
         std::sort(intervals.begin(), intervals.end(), comp1);
-
-        auto comp2 = [](const auto& interval1, const auto& interva2) {
-            return interval1[1] > interva2[1];
+        auto comp2 = [](const auto& interval1, const auto& interval2) {
+            return interval1[1] > interval2[1];
         };
         priority_queue<vector<int>, vector<vector<int>>, decltype(comp2)> pq(comp2);
 
         int ret = 0;
         for (const auto& interval : intervals) {
-            while (!pq.empty() && pq.top()[1] <= interval[0]) {
+            int start = interval[0], end = interval[1];
+            while (!pq.empty() && pq.top()[1] <= start) {
                 pq.pop();
             }
             pq.push(interval);
