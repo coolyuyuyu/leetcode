a7c547fa92ac8f43e4a1b7f21e4ba5dc9ddc9480
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-15 15:01:15 +0800

    930: update solution

--- algorithm_0000-0999/930-Binary Subarrays With Sum/main.cpp
+++ algorithm_0000-0999/930-Binary Subarrays With Sum/main.cpp
@@ -1,14 +1,52 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    // hash + prefix
+    int f1(vector<int>& nums, int goal) {
         int ret = 0;
         unordered_map<int, int> presum2cnt = {{0, 1}};
-        for (int i = 0, sum = 0; i < nums.size();; ++i) {
+        for (int i = 0, sum = 0; i < nums.size(); ++i) {
             sum += nums[i];
             ret += presum2cnt[sum - goal];
             ++presum2cnt[sum];
         }
 
         return ret;
+    }
+
+    // two pointers
+    int f2(vector<int>& nums, int goal) {
+        int n = nums.size();
+
+        // dp[i]: the number of consecutive 0 from nums[i + 1];
+        int dp[n];
+        for (int i = n, cnt = 0; 0 < i--;) {
+            dp[i] = cnt;
+            if (nums[i] == 0) {
+                ++cnt;
+            }
+            else {
+                cnt = 0;
+            }
+        }
+
+        int ret = 0;
+        for (int lft = 0, rht = 0, sum = 0; lft < n; ++lft) {
+            for (; rht <= lft || (sum < goal && rht < n); ++rht) {
+                sum += nums[rht];
+            }
+
+            if (sum == goal) {
+                ret += dp[rht - 1] + 1;
+            }
+
+            sum -= nums[lft];
+        }
+
+        return ret;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        //return f1(nums, goal);
+        return f2(nums, goal);
     }
 };
