2546e2914b07a16797ecf644639e061ee7198637
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-15 16:49:15 +0800

    729: initial solution

---
+++ algorithm_0000-0999/729-My Calendar I/main.cpp
@@ -0,0 +1,33 @@
+class MyCalendar {
+public:
+    MyCalendar() {
+    }
+
+    bool book(int start, int end) {
+        auto itr1 = m_start2end.upper_bound(start);
+        if (itr1 != m_start2end.begin()) {
+            auto itr2 = std::prev(itr1);
+            if (itr2->second > start) {
+                return false;
+            }
+        }
+        if (itr1 != m_start2end.end()) {
+            if (end > itr1->first) {
+                return false;
+            }
+        }
+
+
+        m_start2end[start] = end;
+        return true;
+    }
+
+private:
+    map<int, int> m_start2end;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
