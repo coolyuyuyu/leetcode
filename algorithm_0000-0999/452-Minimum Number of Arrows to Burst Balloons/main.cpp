b57e18767f94844fd10bea0df6d9b8e1b96ad4db
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-18 19:58:44 +0800

    452: update solution

--- algorithm_0000-0999/452-Minimum Number of Arrows to Burst Balloons/main.cpp
+++ algorithm_0000-0999/452-Minimum Number of Arrows to Burst Balloons/main.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        std::sort(points.begin(), points.end());
+        std::sort(points.begin(), points.end(), [](const auto& p1, const auto& p2){ return p1[1] < p2[1]; });
 
         int ret = 1;
-        for (int coverR = points[0][1], i = 1; i < points.size(); ++i) {
-            if (coverR < points[i][0]) { // not covered
+        for (int i = 1, maxRht = points[0][1]; i < points.size(); ++i) {
+            if (points[i][0] > maxRht) {
                 ++ret;
-                coverR = points[i][1];
-            }
-            else {
-                coverR = std::min(coverR, points[i][1]);
+                maxRht = points[i][1];
             }
         }
 
