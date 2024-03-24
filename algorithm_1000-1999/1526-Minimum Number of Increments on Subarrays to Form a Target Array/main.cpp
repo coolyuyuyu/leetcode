8cedacec7840a89fcc165a2479c7947326c9f6f1
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-24 16:48:05 +0800

    1526: update solution

--- algorithm_1000-1999/1526-Minimum Number of Increments on Subarrays to Form a Target Array/main.cpp
+++ algorithm_1000-1999/1526-Minimum Number of Increments on Subarrays to Form a Target Array/main.cpp
@@ -1,6 +1,8 @@
 template<typename T, typename BinaryOperation = std::plus<T>>
 class SegmentTree {
 public:
+    typedef std::vector<T>::size_type size_type;
+
     template<typename InputIterator>
     explicit SegmentTree(InputIterator first, InputIterator last)
         : m_op()
@@ -27,7 +29,7 @@ public:
         return m_vals[0];
     }
 
-    T query(size_t lo, size_t hi) const {
+    T query(size_type lo, size_type hi) const {
         if (hi < lo || size() <= hi) {
             throw std::out_of_range("invalid range");
         }
@@ -35,19 +37,18 @@ public:
         return query(0, size() - 1, 0, lo, hi);
     }
 
-    void set(size_t idx, const T& val) {
-        if (size() <= idx) {
-            throw std::out_of_range("invalid subscript");
-        }
+    void set(size_type idx, const T& val) {
+        assert(idx < size());
+
         set(0, size() - 1, 0, idx, val);
     }
 
-    const T& operator[](size_t idx) const {
+    const T& get(size_type idx) const {
         assert(idx < size());
 
-        size_t l = 0, h = size() - 1, i = 0;
+        size_type l = 0, h = size() - 1, i = 0;
         while (l < h) {
-            size_t m = l + (h - l) / 2;
+            size_type m = l + (h - l) / 2;
             if (idx <= m) {
                 h = m;
                 i = lft(i);
@@ -61,20 +62,12 @@ public:
         return m_vals[i];
     }
 
-    const T& at(size_t idx) const {
-        if (size() <= idx) {
-            throw std::out_of_range("invalid subscript");
-        }
-
-        return (*this)[idx];
-    }
-
-    size_t size() const { return m_size; }
+    size_type size() const { return m_size; }
     bool empty() const { return m_size == 0; }
 
 private:
-    inline size_t lft(size_t i) const { return i * 2 + 1; }
-    inline size_t rht(size_t i) const { return i * 2 + 2; }
+    inline size_type lft(size_type i) const { return i * 2 + 1; }
+    inline size_type rht(size_type i) const { return i * 2 + 2; }
 
     template<typename InputIterator>
     void build(InputIterator first, InputIterator last) {
@@ -84,7 +77,7 @@ private:
     }
 
     template<typename InputIterator>
-    void build(size_t l, size_t h, size_t i, InputIterator& itr) {
+    void build(size_type l, size_type h, size_type i, InputIterator& itr) {
         assert(l <= h);
 
         if (l == h) {
@@ -94,14 +87,14 @@ private:
             m_vals[i] = *itr++;
         }
         else {
-            size_t m = l + (h - l) / 2;
+            size_type m = l + (h - l) / 2;
             build(l, m, lft(i), itr);
             build(m + 1, h, rht(i), itr);
             m_vals[i] = m_op(m_vals[lft(i)], m_vals[rht(i)]);
         }
     }
 
-    void set(size_t l, size_t h, size_t i, size_t idx, const T& val) {
+    void set(size_type l, size_type h, size_type i, size_type idx, const T& val) {
         assert(l <= h);
 
         if (idx < l || h < idx) {
@@ -112,21 +105,21 @@ private:
             m_vals[i] = val;
         }
         else {
-            size_t m = l + (h - l) / 2;
+            size_type m = l + (h - l) / 2;
             set(l, m, lft(i), idx, val);
             set(m + 1, h, rht(i), idx, val);
             m_vals[i] = m_op(m_vals[lft(i)], m_vals[rht(i)]);
         }
     }
 
-    T query(size_t l, size_t h, size_t i, size_t lo, size_t hi) const {
+    T query(size_type l, size_type h, size_type i, size_type lo, size_type hi) const {
         assert(l <= h);
 
         if (lo <= l && h <= hi) {
             return m_vals[i];
         }
         else {
-            size_t m = l + (h - l) / 2;
+            size_type m = l + (h - l) / 2;
             if (hi < l || m < lo) {
                 return query(m + 1, h, rht(i), lo, hi);
             }
@@ -141,7 +134,7 @@ private:
 
     std::vector<T> m_vals;
     BinaryOperation m_op;
-    size_t m_size;
+    size_type m_size;
 };
 
 class Solution {
@@ -174,7 +167,7 @@ public:
                 return 0;
             }
             else if (lo == hi) {
-                auto [val, pos] = st[lo];
+                auto [val, pos] = st.get(lo);
                 return val - base;
             }
 
