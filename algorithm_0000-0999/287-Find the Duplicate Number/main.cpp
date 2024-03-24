0b81a6129429fff65a9d29e3ada805c6e7e6b1bf
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-24 16:46:09 +0800

    287: update solution

--- algorithm_0000-0999/287-Find the Duplicate Number/main.cpp
+++ algorithm_0000-0999/287-Find the Duplicate Number/main.cpp
@@ -2,43 +2,46 @@ class Solution {
 public:
     // Time: O(n), Space: O(n)
     int linearScan(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> flags(n, false);
+        int n = nums.size() - 1;
+        vector<int> existed(n + 1, false);
         for (int num : nums) {
-            if (flags[num - 1]) {
+            if (existed[num]) {
                 return num;
             }
-            flags[num - 1] = true;
+            existed[num] = true;
         }
 
+        assert(false);
         return 0;
     }
 
     // Time: O(n), Space: O(1)
     int indexSort(vector<int>& nums) {
-        nums.insert(nums.begin(), 0);
-        int n = nums.size();
-
-        for (int i = 0; i < n; ++i) {
-            while (nums[i] != i && nums[i] != nums[nums[i]]) {
-                std::swap(nums[i], nums[nums[i]]);
+        int n = nums.size() - 1;
+        for (int i = 0; i <= n; ++i) {
+            while (nums[i] != i + 1 && nums[i] - 1 <= n && nums[i] != nums[nums[i] - 1]) {
+                std::swap(nums[i], nums[nums[i] - 1]);
             }
+        }
 
-            if (nums[i] != i) {
+        for (int i = 0; i <= n; ++i) {
+            if (nums[i] != i + 1) {
                 return nums[i];
             }
         }
 
+        assert(false);
         return 0;
     }
 
     // Time: O(nlogn), Space: O(1)
     int binaySearch(vector<int>& nums) {
-        int lo = 1, hi = nums.size() - 1;
+        int n = nums.size() - 1;
+        int lo = 1, hi = n;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            int cnt = std::count_if(nums.begin(), nums.end(), [&](int num) { return num <= mid; });//0;
-            if (mid < cnt) {
+            int cnt = std::count_if(nums.begin(), nums.end(), [&](int num){ return num <= mid; });
+            if (cnt > mid) {
                 hi = mid;
             }
             else {
@@ -51,8 +54,6 @@ public:
 
     // Time: O(n), Space: O(1)
     int cycleDetect(vector<int>& nums) {
-        // 142. Linked List Cycle II
-
         int slow = 0, fast = 0;
         do {
             slow = nums[slow];
