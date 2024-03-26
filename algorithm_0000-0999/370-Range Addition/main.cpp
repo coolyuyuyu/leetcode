e23cd84908a88c9e421c8ee95cd579cc6736cf18
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-27 00:02:26 +0800

    370: segment tree solution

--- algorithm_0000-0999/370-Range Addition/main.cpp
+++ algorithm_0000-0999/370-Range Addition/main.cpp
@@ -1,15 +1,133 @@
-class Solution {
+template<typename T>
+class DiffSegmentTree {
 public:
-    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
-        vector<int> nums(length, 0);
-        for (size_t i = 0; i < updates.size(); ++i) {
-            nums[updates[i][0]] += updates[i][2];
-            if (updates[i][1] + 1 < nums.size()) {
-                nums[updates[i][1] + 1] -= updates[i][2];
+    typedef typename std::vector<T>::size_type size_type;
+
+    explicit DiffSegmentTree(size_type n, const T& val = T()) {
+        build(n, val);
+    }
+
+    void update(size_type lo, size_type hi, const T& val) {
+        update(0, size() - 1, 0, lo, hi, val);
+    }
+
+    const T& get(size_type idx) const {
+        assert(idx < size());
+
+        size_type l = 0, h = size() - 1, i = 0;
+        while (l < h) {
+            pushDown(l, h, i);
+            size_type m = l + (h - l) / 2;
+            if (idx <= m) {
+                h = m;
+                i = lft(i);
+            }
+            else {
+                l = m + 1;
+                i = rht(i);
             }
         }
-        partial_sum(nums.begin(), nums.end(), nums.begin());
 
-        return nums;
+        return m_vals[i];
+    }
+
+    size_type size() const { return m_size; }
+    bool empty() const { return m_size == 0; }
+
+private:
+    inline size_type lft(size_type i) const { return i * 2 + 1; }
+    inline size_type rht(size_type i) const { return i * 2 + 2; }
+
+    void build(size_type n, const T& val) {
+        m_size = n;
+        if (m_size) {
+            build(0, m_size - 1, 0, val);
+        }
+    }
+
+    void build(size_type l, size_type h, size_type i, const T& val) {
+        assert(l <= h);
+
+        if (l == h) {
+            if (m_vals.size() <= i) {
+                m_vals.resize(i + 1);
+            }
+            m_vals[i] = val;
+        }
+        else {
+            size_type m = l + (h - l) / 2;
+            build(l, m, lft(i), val);
+            build(m + 1, h, rht(i), val);
+        }
+    }
+
+    void pushDown(size_type l, size_type h, size_type i) const {
+        if (l == h) {
+            return;
+        }
+
+        m_vals[lft(i)] += m_vals[i];
+        m_vals[rht(i)] += m_vals[i];
+        m_vals[i] = 0;
+    }
+
+    void update(size_type l, size_type h, size_type i, size_type lo, size_type hi, const T& diff) {
+        if (hi < l || h < lo) {
+            return;
+        }
+
+        if (lo <= l && h <= hi) {
+            m_vals[i] += diff;
+        }
+        else {
+            pushDown(l, h, i);
+
+            size_type m = l + (h - l) / 2;
+            update(l, m, lft(i), lo, hi, diff);
+            update(m + 1, h, rht(i), lo, hi, diff);
+        }
+    }
+
+    mutable std::vector<T> m_vals;
+    size_type m_size;
+};
+
+class Solution {
+public:
+    vector<int> byDiffArray(int length, vector<vector<int>>& updates) {
+        vector<int> diffs(length, 0);
+        for (const auto& update : updates) {
+            diffs[update[0]] += update[2];
+            if (update[1] + 1 < length) {
+                diffs[update[1] + 1] -= update[2];
+            }
+        }
+
+        vector<int> ret(length, 0);
+        for (int i = 0, sum = 0; i < length; ++i) {
+            sum += diffs[i];
+            ret[i] = sum;
+        }
+
+        return ret;
+    }
+
+    vector<int> bySegmentTree(int length, vector<vector<int>>& updates) {
+        DiffSegmentTree<int> st(length, 0);
+        for (const auto& update : updates) {
+            st.update(update[0], update[1], update[2]);
+        }
+
+        vector<int> ret(length, 0);
+        for (int i = 0; i < length; ++i) {
+            ret[i] = st.get(i);
+        }
+
+        return ret;
+    }
+
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        //return byDiffArray(length, updates);
+        return bySegmentTree(length, updates);
     }
 };
