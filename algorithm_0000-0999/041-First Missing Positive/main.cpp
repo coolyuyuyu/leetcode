61297aeb619540fed29018d586c834d46852c554
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-27 00:02:10 +0800

    041: update solution

--- algorithm_0000-0999/041-First Missing Positive/main.cpp
+++ algorithm_0000-0999/041-First Missing Positive/main.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    // cycle sort
     int firstMissingPositive(vector<int>& nums) {
+        // index sort;
+
         int n = nums.size();
         for (int i = 0; i < n; ++i) {
-            while (0 < nums[i] && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+            while (nums[i] != i + 1 && 0 < nums[i] && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                 std::swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
         for (int i = 0; i < n; ++i) {
-            if (nums[i] != (i + 1)) {
+            if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
