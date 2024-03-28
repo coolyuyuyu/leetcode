c5708afb7a9f9ddc5e07bd267268cf4c774efb29
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-29 00:03:06 +0800

    2958: two pointers solution

--- algorithm_2000-2999/2958-Length of Longest Subarray With at Most K Frequency/main.cpp
+++ algorithm_2000-2999/2958-Length of Longest Subarray With at Most K Frequency/main.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        int n = nums.size();
-
         unordered_map<int, int> cnts;
 
         int ret = 0;
-        for (int lft = 0, rht = 0; rht < n; ++lft) {
+        for (int lft = 0, rht = 0, n = nums.size(); rht < n; ++lft) {
             for (; rht < n && cnts[nums[rht]] < k; ++rht) {
                 ++cnts[nums[rht]];
             }
