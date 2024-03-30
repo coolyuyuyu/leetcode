484d57204b5d64ad50858be6c81ff9cc0e6c3b8e
coolyuyuyu <coolyuyuyu@gmail.com>
2024-03-30 14:43:41 +0800

    992: update solution

--- algorithm_0000-0999/992-Subarrays with K Different Integers/main.cpp
+++ algorithm_0000-0999/992-Subarrays with K Different Integers/main.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         int n = nums.size();
-        std::function<int(int)> cntSubarraysWithAtMostKDistinct = [&](int k) {
-            vector<int> cnts(20000 + 1, 0);
+        std::function<int(int)> subarraysWithAtmostKDistinct = [&](int k) {
+            unordered_map<int, int> freq;
 
             int ret = 0;
-            for (int lft = 0, rht = 0; lft < n; ++lft) {
-                for (; rht < n && (rht < lft || 0 < k || (k == 0 && 0 < cnts[nums[rht]])); ++rht) {
-                    if (cnts[nums[rht]]++ == 0) {
-                        --k;
+            for (int lft = 0, rht = 0, cnt = 0; rht < n; ++rht) {
+                if (++freq[nums[rht]] == 1) {
+                    ++cnt;
+                }
+
+                for (; cnt > k; ++lft) {
+                    if (--freq[nums[lft]] == 0) {
+                        --cnt;
                     }
                 }
 
-                ret += (rht - lft);
-
-                if (--cnts[nums[lft]] == 0) {
-                    ++k;
-                }
+                ret += rht - lft + 1;
             }
 
             return ret;
         };
 
-        return cntSubarraysWithAtMostKDistinct(k) - cntSubarraysWithAtMostKDistinct(k - 1);
+        return subarraysWithAtmostKDistinct(k) - subarraysWithAtmostKDistinct(k - 1);
     }
 };
