75f3c9510185445c7202980a16b14229e5329203
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-02 01:50:26 +0800

    2136: update solution

--- algorithm_2000-2999/2136-Earliest Possible Day of Full Bloom/main.cpp
+++ algorithm_2000-2999/2136-Earliest Possible Day of Full Bloom/main.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
-    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+    int byBinarySearch(vector<int>& plantTime, vector<int>& growTime) {
+        int n = plantTime.size();
+
+        vector<pair<int, int>> pairs(n);
+        for (int i = 0; i < n; ++i) {
+            pairs[i] = {plantTime[i], -growTime[i]};
+        }
+        std::sort(pairs.begin(), pairs.end(), [](const auto& p1, const auto& p2){ return p1.second < p2.second; });
+        std::function<bool(int)> checkOk = [&](int t) {
+            int x = 0;
+            for (const auto& [pTime, gTime] : pairs) {
+                x += pTime;
+                if (x > t + gTime) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        int lo = 1, hi = INT_MAX;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (checkOk(mid)) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    int byGreedy(vector<int>& plantTime, vector<int>& growTime) {
         int n = plantTime.size();
 
         vector<pair<int, int>> pairs(n);
@@ -19,5 +53,10 @@ public:
         }
 
         return ret;
+    }
+
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+        return byBinarySearch(plantTime, growTime);
+        //return byGreedy(plantTime, growTime);
     }
 };
