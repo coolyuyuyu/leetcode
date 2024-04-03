4aa8e5083459d0d1cbf063ff7b1a01ed2a5795c8
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-03 23:21:12 +0800

    079: update solution

--- algorithm_0000-0999/079-Word Search/main.cpp
+++ algorithm_0000-0999/079-Word Search/main.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool existRecv(vector<vector<char>>& board, size_t r, size_t c, const string& word, size_t i) {
-        assert(r < board.size() && c < board[0].size());
-
-        if (board[r][c] != word[i]) {
-            return false;
-        }
-        if (word.size() <= (i + 1)) {
-            return true;
-        }
-
-        char ch = board[r][c];
-        board[r][c] = '\0';
-        bool found =
-            (0 < c && existRecv(board, r, c - 1, word, i + 1)) ||
-            (0 < r && existRecv(board, r - 1, c, word, i + 1)) ||
-            (c + 1 < board[0].size() && existRecv(board, r, c + 1, word, i + 1)) ||
-            (r + 1 < board.size() && existRecv(board, r + 1, c, word, i + 1));
-        board[r][c] = ch;
-
-        return found;
-    }
-
     bool exist(vector<vector<char>>& board, string word) {
-        size_t row = board.size(), col = board[0].size();
-        for (size_t r = 0; r < row; ++r) {
-            for (size_t c = 0; c < col; ++c) {
-                if (existRecv(board, r, c, word, 0)) {
+        int m = board.size(), n = board.empty() ? 0 : board[0].size();
+        std::function<bool(int, int, int)> dfs = [&](int r, int c, int i) {
+            if (board[r][c] != word[i]) {
+                return false;
+            }
+            if (i + 1 >= word.size()) {
+                return true;
+            }
+
+            char ch = board[r][c];
+            board[r][c] = 0;
+            bool found = 
+                (c > 0 && dfs(r, c - 1, i + 1)) ||
+                (r > 0 && dfs(r - 1, c, i + 1)) ||
+                (c + 1 < n && dfs(r, c + 1, i + 1)) ||
+                (r + 1 < m && dfs(r + 1, c, i + 1));
+            board[r][c] = ch;
+            return found;
+        };
+
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (dfs(r, c, 0)) {
                     return true;
                 }
             }
