f0e1735eb6f33879b23deaf82c565605876f4694
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-06 13:03:35 +0800

    1249: update solution

--- algorithm_1000-1999/1249-Minimum Remove to Make Valid Parentheses/main.cpp
+++ algorithm_1000-1999/1249-Minimum Remove to Make Valid Parentheses/main.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        vector<size_t> stk;
-        for (size_t i = 0; i < s.size(); ++i) {
-            switch (s[i]) {
-                case '(':
-                    stk.emplace_back(i);
-                    break;
-                case ')':
-                    if (!stk.empty() && s[stk.back()] == '(') {
-                        stk.pop_back();
-                    }
-                    else {
-                        stk.emplace_back(i);
-                    }
-                    break;
-                default:
-                    break;
+        stack<int> stk;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                stk.push(i);
+            }
+            else if (s[i] == ')') {
+                if (stk.empty()) {
+                    s[i] = ' ';
+                }
+                else {
+                    stk.pop();
+                }
+            }
+        }
+        while (!stk.empty()) {
+            s[stk.top()] = ' ';
+            stk.pop();
+        }
+
+        string ret;
+        for (char c : s) {
+            if (c != ' ') {
+                ret.push_back(c);
             }
         }
 
-        string result;
-        size_t parenthesesIndex = 0;
-        for (size_t i = 0; i < s.size(); ++i) {
-            if (parenthesesIndex < stk.size() && i == stk[parenthesesIndex]) {
-                ++parenthesesIndex;
-            }
-            else {
-                result.push_back(s[i]);
-            }
-        }
-
-        return result;
+        return ret;
     }
 };
