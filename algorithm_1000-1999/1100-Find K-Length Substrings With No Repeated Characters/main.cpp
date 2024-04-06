10e2cbe5067bb9e998f1b0d479b08085f6370e0b
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-07 01:52:55 +0800

    1100: initial solution

---
+++ algorithm_1000-1999/1100-Find K-Length Substrings With No Repeated Characters/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numKLenSubstrNoRepeats(string s, int k) {
+        vector<int> freqs(26, 0);
+
+        int ret = 0;
+        for (int i = 0, n = s.size(), diffCnt = 0; i < n; ++i) {
+            if (++freqs[s[i] - 'a'] == 1) {
+                ++diffCnt;
+            }
+
+            if (i + 1 >= k) {
+                if (diffCnt == k) {
+                    ++ret;
+                }
+
+                if (--freqs[s[i - k + 1] - 'a'] == 0) {
+                --diffCnt;
+                }
+            }
+        }
+
+        return ret;
+    }
+};
