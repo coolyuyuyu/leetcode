408b9897d2cb3c6d8e4452b058da848e5544e299
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-08 01:51:51 +0800

    678: update solution

--- algorithm_0000-0999/678-Valid Parenthesis String/main.cpp
+++ algorithm_0000-0999/678-Valid Parenthesis String/main.cpp
@@ -1,66 +1,31 @@
 class Solution {
 public:
-    // Time(n): O(3^n), TLE
-    bool checkValidStringDfs(const string& str, size_t pos, size_t openCount) {
-        for (size_t i = pos; i < str.size(); ++i) {
-            switch (str[i]) {
-                case '(':
-                    ++openCount;
-                    break;
-                case ')':
-                    if (openCount == 0) {
-                        return false;
-                    }
-                    --openCount;
-                    break;
-                case '*':
-                    if (checkValidStringDfs(str, pos + 1, openCount + 1)) {
-                        return true;
-                    }
-                    if (1 <= openCount && checkValidStringDfs(str, pos + 1, openCount - 1)) {
-                        return true;
-                    }
-                    break;
+    bool checkValidString(string s) {
+        int lo = 0; // the minimal possible number of unmatched open parenthesis
+        int hi = 0; // the maximal possible number of unmatched open parenthesis
+
+        for (char c : s) {
+            if (c == '(') {
+                ++lo;
+                ++hi;
+            }
+            else if (c == ')') {
+                --lo;
+                --hi;
+            }
+            else {
+                --lo;
+                ++hi;
+            }
+
+            if (lo < 0) {
+                lo = 0;
+            }
+            if (hi < 0) {
+                return false;
             }
         }
 
-        return openCount == 0;
-    }
-
-    bool checkValidStringGreedy(const string& str) {
-        size_t openCountLower = 0, openCountUpper = 0;
-        for (size_t i = 0; i < str.size(); ++i) {
-            switch (str[i]) {
-                case '(':
-                    ++openCountLower;
-                    ++openCountUpper;
-                    break;
-                case ')':
-                    if (0 < openCountLower) {
-                        --openCountLower;
-                    }
-
-                    if (openCountUpper == 0) {
-                        return false;
-                    }
-                    --openCountUpper;
-                    break;
-                case '*':
-                    if (0 < openCountLower) {
-                        --openCountLower;
-                    }
-
-                    ++openCountUpper;
-                    break;
-            }
-        }
-
-        return openCountLower == 0;
-    }
-
-    bool checkValidString(const string& str) {
-        //return checkValidStringDfs(str, 0, 0);
-
-        return checkValidStringGreedy(str);
+        return lo == 0;
     }
 };
