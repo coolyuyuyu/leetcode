71b2821fed78d06f6eb645ad83f8a40b77c35ee9
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-09 01:10:54 +0800

    432: update solution

--- algorithm_0000-0999/432-All O`one Data Structure/main.cpp
+++ algorithm_0000-0999/432-All O`one Data Structure/main.cpp
@@ -1,38 +1,73 @@
 class AllOne {
 public:
     AllOne() {
+        m_vals.assign({0});
+        m_val2itr[0] = m_vals.begin();
     }
 
     void inc(string key) {
-        int& cnt = m_key2cnt[key];
-        if (cnt > 0) {
-            m_pairs.erase({cnt, key});
+        int& val = m_key2val[key];
+        ++val;
+
+        m_val2keys[val].insert(key);
+        if (val - 1 > 0) {
+            m_val2keys[val - 1].erase(key);
         }
-        m_pairs.insert({++cnt, key});
+
+        if (m_val2keys[val].size() == 1) {
+            m_vals.insert(std::next(m_val2itr[val - 1]), val);
+            m_val2itr[val] = std::next(m_val2itr[val - 1]);
+        }
+        if (m_val2keys[val - 1].size() == 0 && val - 1 > 0) {
+            m_vals.erase(m_val2itr[val - 1]);
+            m_val2itr.erase(val - 1);
+        }
     }
 
     void dec(string key) {
-        int& cnt = m_key2cnt[key];
-        assert(cnt > 0);
-        m_pairs.erase({cnt, key});
-        if (--cnt > 0) {
-            m_pairs.insert({cnt, key});
+
+        int& val = m_key2val[key];
+        --val;
+
+        if (val > 0) {
+            m_val2keys[val].insert(key);
+        }
+        m_val2keys[val + 1].erase(key);
+
+
+        if (m_val2keys[val].size() == 1) {
+            m_vals.insert(m_val2itr[val + 1], val);
+            m_val2itr[val] = std::prev(m_val2itr[val + 1]);
+        }
+        if (m_val2keys[val + 1].size() == 0) {
+            m_vals.erase(m_val2itr[val + 1]);
+            m_val2itr.erase(val + 1);
         }
     }
 
     string getMaxKey() {
-        if (m_pairs.empty()) { return ""; }
-        return m_pairs.rbegin()->second;
+        if (m_vals.size() > 1) {
+            return *(m_val2keys[*std::prev(m_vals.end())].begin());
+        }
+        else {
+            return "";
+        }
     }
 
     string getMinKey() {
-        if (m_pairs.empty()) { return ""; }
-        return m_pairs.begin()->second;
+        if (m_vals.size() > 1) {
+            return *(m_val2keys[*std::next(m_vals.begin())].begin());
+        }
+        else {
+            return "";
+        }
     }
 
 private:
-    unordered_map<string, int> m_key2cnt;
-    set<pair<int, string>> m_pairs;
+    unordered_map<string, int> m_key2val;
+    unordered_map<int, unordered_set<string>> m_val2keys;
+    list<int> m_vals;
+    unordered_map<int, list<int>::iterator> m_val2itr;
 };
 
 /**
