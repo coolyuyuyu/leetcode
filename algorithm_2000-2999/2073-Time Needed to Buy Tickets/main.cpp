5cdd9a3f000388c59ff890f18b4aac76ee50c803
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-09 23:56:48 +0800

    2073: update solution

--- algorithm_2000-2999/2073-Time Needed to Buy Tickets/main.cpp
+++ algorithm_2000-2999/2073-Time Needed to Buy Tickets/main.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int totalTime = 0;
-        for (size_t i = 0; i < tickets.size(); ++i) {
-            totalTime += std::min(tickets[i],  tickets[k] - (k < i ? 1 : 0));
+        int ret = 0;
+
+        for (int i = 0; i < k; ++i) {
+            ret += std::min(tickets[i], tickets[k]);
+        }
+        ret += tickets[k];
+        for (int i = k + 1; i < tickets.size(); ++i) {
+            ret += std::min(tickets[i], tickets[k] - 1);
         }
 
-        return totalTime;
+        return ret;
     }
 };
