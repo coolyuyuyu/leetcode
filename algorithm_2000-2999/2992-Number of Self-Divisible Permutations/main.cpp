4bf69af8cd48372d50e3ab8a10015a85d1e51280
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-11 02:56:11 +0800

    2992: update solution

--- algorithm_2000-2999/2992-Number of Self-Divisible Permutations/main.cpp
+++ algorithm_2000-2999/2992-Number of Self-Divisible Permutations/main.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int selfDivisiblePermutationCount(int n) {
-        // state: the binary representation of used numbers
-        // dp[i][state]: the number of self-divisible permutations of state
+        // state: the binary representation of used number
+        // dp[i][state]: the number of i-length self-divisible permutations using numbers of state
         int dp[n + 1][1 << n];
         for (int i = 0; i <= n; ++i) {
             for (int state = 0; state < (1 << n); ++state) {
                 dp[i][state] = 0;
             }
         }
+
         dp[0][0] = 1;
         for (int i = 1; i <= n; ++i) {
             for (int state = 0; state < (1 << n); ++state) {
                 for (int num = 1; num <= n; ++num) {
-                    if (gcd(num, i) != 1) { continue; }
+                    if (std::gcd(num, i) != 1) { continue; }
                     if ((state & (1 << (num - 1))) == 0) { continue; }
-                    dp[i][state] += dp[i - 1][state - (1 << (num - 1))];
+                    dp[i][state]  += dp[i - 1][state - (1 << (num - 1))];
                 }
             }
         }
