f1272c7440ca12dae9ba5a06523e241b9da293e2
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-15 02:32:11 +0800

    2334: update solution

--- algorithm_2000-2999/2334-Subarray With Elements Greater Than Varying Threshold/main.cpp
+++ algorithm_2000-2999/2334-Subarray With Elements Greater Than Varying Threshold/main.cpp
@@ -1,23 +1,59 @@
+// 84. Largest Rectangle in Histogram
+
 class Solution {
 public:
-    int validSubarraySize(vector<int>& nums, int threshold) {
-        // 84. Largest Rectangle in Histogram
-
-        nums.insert(nums.begin(), 0);
-        nums.push_back(0);
-
+    // Time: O(3N)
+    int f1(vector<int>& nums, int threshold) {
         int n = nums.size();
-
         stack<int> stk;
 
-        int ret = 0;
+        int nxtLT[n];
+        std::fill(nxtLT, nxtLT + n, n);
+        while (!stk.empty()) { stk.pop(); }
         for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && nums[stk.top()] > nums[i]) {
+                nxtLT[stk.top()] = i;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+
+        int preLT[n];
+        std::fill(preLT, preLT + n, -1);
+        while (!stk.empty()) { stk.pop(); }
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && nums[stk.top()] > nums[i]) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                preLT[i] = stk.top();
+            }
+            stk.push(i);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int w = nxtLT[i] - preLT[i] - 1;
+            int h = nums[i];
+            if (h > threshold / w) {
+                return w;
+            }
+        }
+
+        return -1;
+    }
+
+    // Time: O(N)
+    int f2(vector<int>& nums, int threshold) {
+        nums.push_back(0);
+        nums.insert(nums.begin(), 0);
+        stack<int> stk;
+
+        for (int i = 0; i < nums.size(); ++i) {
             while (!stk.empty() && nums[stk.top()] > nums[i]) {
                 int h = nums[stk.top()];
                 stk.pop();
                 int w = i - stk.top() - 1;
-
-                if (threshold < w * h) {
+                if (h > threshold / w) {
                     return w;
                 }
             }
@@ -25,5 +61,10 @@ public:
         }
 
         return -1;
+    }
+
+    int validSubarraySize(vector<int>& nums, int threshold) {
+        //return f1(nums, threshold);
+        return f2(nums, threshold);
     }
 };
