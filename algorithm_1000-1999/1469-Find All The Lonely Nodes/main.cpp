1099ff2080ba211b22677fc812be7bf8292774e5
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-15 20:03:54 +0800

    1469: update solution

--- algorithm_1000-1999/1469-Find All The Lonely Nodes/main.cpp
+++ algorithm_1000-1999/1469-Find All The Lonely Nodes/main.cpp
@@ -11,55 +11,46 @@
  */
 class Solution {
 public:
-    void getLonelyNodes_Recursive(TreeNode* root, int siblingCnt, vector<int>& vals) {
-        if (!root) {
-            return;
-        }
-        if (siblingCnt == 1) {
-            vals.push_back(root->val);
-        }
+    vector<int> recursive(TreeNode* root) {
+        vector<int> ret;
+        std::function<void(TreeNode*)> f = [&](TreeNode* root) {
+            if (!root) { return; }
+            if (root->left && !root->right) {
+                ret.push_back(root->left->val);
+            }
+            else if (!root->left && root->right) {
+                ret.push_back(root->right->val);
+            }
+            f(root->left);
+            f(root->right);
+        };
+        f(root);
 
-        siblingCnt = (root->left ? 1 : 0) + (root->right ? 1 : 0);
-        if (root->left) {
-            getLonelyNodes_Recursive(root->left, siblingCnt, vals);
-        }
-        if (root->right) {
-            getLonelyNodes_Recursive(root->right, siblingCnt, vals);
-        }
+        return ret;
     }
 
-    void getLonelyNodes_Iterative(TreeNode* root, vector<int>& vals) {
-        if (!root) {
-            return;
-        }
+    vector<int> iterative(TreeNode* root) {
+        vector<int> ret;
+        for (queue<TreeNode*> q({root}); !q.empty();) {
+            auto root = q.front();
+            q.pop();
 
-        stack<pair<TreeNode*, int>> stk; // node, siblingCnt;
-        stk.emplace(root, 0);
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            int siblingCnt = stk.top().second;
-            stk.pop();
+            if (!root) { continue; }
+            if (root->left && !root->right) {
+                ret.push_back(root->left->val);
+            }
+            else if (!root->left && root->right) {
+                ret.push_back(root->right->val);
+            }
+            q.push(root->left);
+            q.push(root->right);
+        };
 
-            if (siblingCnt == 1) {
-                vals.push_back(node->val);
-            }
-
-            siblingCnt = (node->left ? 1 : 0) + (node->right ? 1 : 0);
-            if (node->right) {
-                stk.emplace(node->right, siblingCnt);
-            }
-            if (node->left) {
-                stk.emplace(node->left, siblingCnt);
-            }
-        }
+        return ret;
     }
 
     vector<int> getLonelyNodes(TreeNode* root) {
-        vector<int> vals;
-
-        //getLonelyNodes_Recursive(root, 0, vals);
-        getLonelyNodes_Iterative(root, vals);
-
-        return vals;
+        //return recursive(root);
+        return iterative(root);
     }
 };
