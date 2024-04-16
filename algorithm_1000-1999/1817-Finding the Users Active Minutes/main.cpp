2d91a5339e42ec3c2284692467f2113f2b611c72
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-16 23:39:32 +0800

    1817: initial solution

---
+++ algorithm_1000-1999/1817-Finding the Users Active Minutes/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        int n = logs.size();
+        std::sort(logs.begin(), logs.end());
+
+        vector<int> ret(k, 0);
+        for (int i = 0; i < n;) {
+            int j = i + 1;
+            int cnt = 1;
+            for (; j < n && logs[i][0] == logs[j][0]; ++j) {
+                if (logs[j - 1][1] < logs[j][1]) {
+                    ++cnt;
+                }
+            }
+            if (0 < cnt && cnt <= k) {
+                ++ret[cnt - 1];
+            }
+            i = j;
+        }
+
+        return ret;
+    }
+};
