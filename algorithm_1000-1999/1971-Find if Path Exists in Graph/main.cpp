8c7dbe3a9c202a5243c4a26bf74ae0455fc4ca3b
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-21 23:50:49 +0800

    1971: update solution

--- algorithm_1000-1999/1971-Find if Path Exists in Graph/main.cpp
+++ algorithm_1000-1999/1971-Find if Path Exists in Graph/main.cpp
@@ -1,15 +1,14 @@
-class DisjointSet {
+class DisjointSets {
 public:
-    DisjointSet(size_t n)
-        : m_roots(n) {
-        std::iota(m_roots.begin(), m_roots.end(), 0);
+    DisjointSets(int n)
+        : m_parents(n) {
+        std::iota(m_parents.begin(), m_parents.end(), 0);
     }
 
-    void connect(int elem1, int elem2) {
-        int root1 = root(elem1);
-        int root2 = root(elem2);
+    void merge(int elem1, int elem2) {
+        int root1 = root(elem1), root2 = root(elem2);
         if (root1 != root2) {
-            m_roots[root1] = root2;
+            m_parents[root1] = root2;
         }
     }
 
@@ -19,21 +18,23 @@ public:
 
 private:
     int root(int elem) const {
-        if (m_roots[elem] != elem) {
-            m_roots[elem] = root(m_roots[elem]);
+        if (m_parents[elem] != elem) {
+            m_parents[elem] = root(m_parents[elem]);
         }
-        return m_roots[elem];
+
+        return m_parents[elem];
     }
 
-    mutable vector<int> m_roots;
+    mutable vector<int> m_parents;
 };
+
 
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        DisjointSet ds(n);
+        DisjointSets ds(n);
         for (const auto& edge : edges) {
-            ds.connect(edge[0], edge[1]);
+            ds.merge(edge[0], edge[1]);
         }
 
         return ds.connected(source, destination);
