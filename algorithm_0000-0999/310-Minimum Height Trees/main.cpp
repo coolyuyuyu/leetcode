84584952c0277bfea1a7bd2faa6dbe69788204be
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-23 17:31:57 +0800

    310: update solution

--- algorithm_0000-0999/310-Minimum Height Trees/main.cpp
+++ algorithm_0000-0999/310-Minimum Height Trees/main.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-        if (n == 1) {
-            return {0};
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<int> graph[n];
+        vector<int> degrees(n, 0);
+        for (const auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+
+            ++degrees[u];
+            ++degrees[v];
         }
 
-        vector<unordered_set<int>> adj(n);
-        for (const pair<int, int>& edge : edges) {
-            adj[edge.first].emplace(edge.second);
-            adj[edge.second].emplace(edge.first);
-        }
-
-        vector<int> leaves;
-        for (int node = 0; node < n; ++node) {
-            if (adj[node].size() == 1) {
-                leaves.emplace_back(node);
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (degrees[i] <= 1) {
+                q.push(i);
             }
         }
 
-        while (2 < n) {
-            n -= leaves.size();
-            vector<int> leavesTmp;
-            for (int leave : leaves) {
-                int parent = *(adj[leave].begin());
+        while (n > 2) {
+            n -= q.size();
+            for (int i = q.size(); 0 < i--;) {
+                int u = q.front();
+                q.pop();
 
-                adj[parent].erase(leave);
-                if (adj[parent].size() == 1) {
-                    leavesTmp.emplace_back(parent);
+                for (int v : graph[u]) {
+                    if (--degrees[v] == 1) {
+                        q.push(v);
+                    }
                 }
             }
-            leaves.swap(leavesTmp);
         }
 
-        return leaves;
+        vector<int> ret;
+        while (!q.empty()) {
+            ret.push_back(q.front());
+            q.pop();
+        }
+
+        return ret;
     }
 };
