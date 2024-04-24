f79c238667452308a3bfc5962db0279126c068a0
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-25 00:45:59 +0800

    1056: update solution

--- algorithm_1000-1999/1056-Confusing Number/main.cpp
+++ algorithm_1000-1999/1056-Confusing Number/main.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool confusingNumber(int n) {
-        int x = 0;
+        int m = 0;
         for (int tmp = n; tmp; tmp /= 10) {
-            x *= 10;
+            m *= 10;
             switch (tmp % 10) {
-                case 0: x += 0; break;
-                case 1: x += 1; break;
-                case 6: x += 9; break;
-                case 8: x += 8; break;
-                case 9: x += 6; break;
+                case 0: m += 0; break;
+                case 1: m += 1; break;
+                case 6: m += 9; break;
+                case 8: m += 8; break;
+                case 9: m += 6; break;
                 default: return false;
             }
         }
 
-        return x != n;
+        return m != n;
     }
 };
