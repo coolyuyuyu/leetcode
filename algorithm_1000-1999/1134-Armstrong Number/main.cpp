0dfc1b8669e71399229ed775d42383bffd5a52b1
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-26 00:02:14 +0800

    1134: update solution

--- algorithm_1000-1999/1134-Armstrong Number/main.cpp
+++ algorithm_1000-1999/1134-Armstrong Number/main.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     bool isArmstrong(int n) {
         int cnt = 0;
-        for (int num = n; num; num /= 10) {
+        for (int x = n; x; x /= 10) {
             ++cnt;
         }
 
-        int arm = 0;
-        for (int num = n; num; num /= 10) {
-            int d = num % 10;
-
-            int tmp = d;
-            for (int i = 1; i < cnt; ++i) {
-                tmp *= d;
-            }
-            arm += tmp;
+        int sum = 0;
+        for (int x = n; x; x /= 10) {
+            sum += pow(x % 10, cnt);
         }
 
-        return arm == n;
+        return sum == n;
     }
 };
