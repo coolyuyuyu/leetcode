402052719fc27e0343fdda9c23b8e3b426d85de6
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-26 00:02:26 +0800

    1641: update solution

--- algorithm_1000-1999/1641-Count Sorted Vowel Strings/main.cpp
+++ algorithm_1000-1999/1641-Count Sorted Vowel Strings/main.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
-    int byRecursion(int n) {
-        int cache[n + 1][5 + 1];
+    int byDP(int n) {
+        // dp[i][c]: the number of length i strings ending with character c
+        int dp[n + 1][5];
         for (int i = 0; i <= n; ++i) {
-            for (int j = 0; j <= 5; ++j) {
-                cache[i][j] = -1;
+            for (int j = 0; j < 5; ++j) {
+                dp[i][j] = 0;
             }
         }
-        std::function<int(int, int)> f = [&](int n, int k) {
-            if (n == 1) {
-                return k;
+        for (int j = 0; j < 5; ++j) {
+            dp[1][j] = 1;
+        }
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                for (int k = 0; k <= j; ++k) {
+                    dp[i][j] += dp[i - 1][k];
+                }
             }
+        }
 
-            int& ret = cache[n][k];
-            if (ret >= 0) {
-                return ret;
-            }
+        int ret = 0;
+        for (int j = 0; j < 5; ++j) {
+            ret += dp[n][j];
+        }
 
-            ret = 0;
-            for (int j = 1; j <= k; ++j) {
-                ret += f(n - 1, j);
-            }
+        return ret;
+    }
 
-            return ret;
-        };
-        return f(n, 5);
+    int byMath(int n) {
+        // Given n ball, there are n + 1 spaces to insert a bar.
+        // And there are 3 other special options: bar0==bar1, bar1==bar2, bar2==bar3
+        // => C(n + 1 + 3, 4)
+
+        n += 4;
+        int r = 4;
+
+        int ret = 1;
+        for (int r = 0; r < 4; ++r) {
+            ret *= n - r;
+            ret /= r + 1;
+        }
+
+        return ret;
     }
 
     int countVowelStrings(int n) {
-        return byRecursion(n);
+        //return byDP(n);
+        return byMath(n);
     }
 };
