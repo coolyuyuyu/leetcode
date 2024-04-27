b7cb4498d8e5548dbe4380e35d8cba6260431916
coolyuyuyu <coolyuyuyu@gmail.com>
2024-04-27 12:05:36 +0800

    2044: update solution

--- algorithm_2000-2999/2044-Count Number of Maximum Bitwise-OR Subsets/main.cpp
+++ algorithm_2000-2999/2044-Count Number of Maximum Bitwise-OR Subsets/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countMaxOrSubsets(vector<int>& nums) {
+    int byBruteForce(vector<int>& nums) {
         int n = nums.size();
         int sum = std::accumulate(nums.begin(), nums.end(), 0, std::bit_or<int>());
 
@@ -18,5 +18,33 @@ public:
         }
 
         return ret;
+    }
+
+    int byDP(vector<int>& nums) {
+        int n = nums.size();
+        int sum = std::accumulate(nums.begin(), nums.end(), 0, std::bit_or<int>());
+        int m = 1 << (sizeof(int) * CHAR_BIT - __builtin_clz(sum));
+
+        // dp[num]: the number of different non-empty subsets whose bitwise or is num
+        int dp[m], tmp[m];
+        for (int num = 0; num < m; ++num) {
+            dp[num] = 0;
+        }
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            std::copy(dp, dp + m, tmp);
+            std::fill(dp, dp + m, 0);
+            for (int num = 0; num < m; ++num) {
+                dp[num] += tmp[num];
+                dp[num | nums[i]] += tmp[num];
+            }
+        }
+
+        return dp[sum];
+    }
+
+    int countMaxOrSubsets(vector<int>& nums) {
+        //return byBruteForce(nums);
+        return byDP(nums);
     }
 };
