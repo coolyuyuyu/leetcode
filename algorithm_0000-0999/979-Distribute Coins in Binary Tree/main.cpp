0872072a519d39e7cdf38eba7ed75632cbd38849
coolyuyuyu <coolyuyuyu@gmail.com>
2024-05-19 01:00:50 +0800

    979: initial solution

--- algorithm_0000-0999/979-Distribute Coins in Binary Tree/main.cpp
+++ algorithm_0000-0999/979-Distribute Coins in Binary Tree/main.cpp
@@ -11,58 +11,21 @@
  */
 class Solution {
 public:
-    int distributeCoins_Recursive(TreeNode* root, int& numOfMoves) {
-        if (!root) {
-            return 0;
-        }
-
-        int lftRemainingCoins = distributeCoins_Recursive(root->left, numOfMoves);
-        int rhtRemainingCoins = distributeCoins_Recursive(root->right, numOfMoves);
-
-        numOfMoves += (abs(lftRemainingCoins) + abs(rhtRemainingCoins));
-
-        return (root->val + lftRemainingCoins + rhtRemainingCoins - 1);
-    }
-
-    void distributeCoins_Iterative(TreeNode* root, int& numOfMoves) {
-        map<TreeNode*, int> m; // <node, remainingCoins>
-
-        stack<pair<TreeNode*, bool>> stk; // <node, visited>;
-        if (root) {
-            stk.emplace(root, false);
-        }
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            bool visited = stk.top().second;
-            stk.pop();
-
-            if (visited) {
-                int lftRemainingCoins = (node->left ? m[node->left] : 0);
-                int rhtRemainingCoins = (node->right ? m[node->right] : 0);
-
-                numOfMoves += (abs(lftRemainingCoins) + abs(rhtRemainingCoins));
-
-                m[node] = node->val + lftRemainingCoins + rhtRemainingCoins - 1;
-            }
-            else {
-                stk.emplace(node, true);
-                if (node->right) {
-                    stk.emplace(node->right, false);
-                }
-                if (node->left) {
-                    stk.emplace(node->left, false);
-                }
-            }
-
-        }
-    }
-
     int distributeCoins(TreeNode* root) {
-        int numOfMoves = 0;
+        int ret = 0;
 
-        //distributeCoins_Recursive(root, numOfMoves);
-        distributeCoins_Iterative(root, numOfMoves);
+        std::function<int(TreeNode*)> f = [&](TreeNode* root) {
+            if (!root) { return 0; }
 
-        return numOfMoves;
+            int lftCoins = f(root->left);
+            int rhtCoins = f(root->right);
+
+            ret += abs(lftCoins) + abs(rhtCoins);
+
+            return root->val + lftCoins + rhtCoins - 1;
+        };
+        f(root);
+
+        return ret;
     }
 };
