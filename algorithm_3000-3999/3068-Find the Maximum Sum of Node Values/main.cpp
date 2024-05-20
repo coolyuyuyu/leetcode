8df484e51e5636c9f95c562b3beca70c0b150852
coolyuyuyu <coolyuyuyu@gmail.com>
2024-05-21 00:58:18 +0800

    3068: update solution

--- algorithm_3000-3999/3068-Find the Maximum Sum of Node Values/main.cpp
+++ algorithm_3000-3999/3068-Find the Maximum Sum of Node Values/main.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        priority_queue<long long> diffs;
-        for (int num : nums) {
-            diffs.push((num ^ k) - num);
-        }
-
         long long ret = 0;
-        while (diffs.size() >= 2) {
-            long long diff1 = diffs.top(); diffs.pop();
-            long long diff2 = diffs.top(); diffs.pop();
-            if (diff1 + diff2 > 0) {
-                ret += diff1 + diff2;
-            }
-            else {
-                break;
-            }
+        priority_queue<int> maxPQ;
+        for (int num : nums) {
+            ret += num;
+            maxPQ.push((num ^ k) - num);
         }
 
-        return ret + std::accumulate(nums.begin(), nums.end(), 0LL);
+        while (maxPQ.size() >= 2) {
+            int diff1 = maxPQ.top(); maxPQ.pop();
+            int diff2 = maxPQ.top(); maxPQ.pop();
+            if (diff1 + diff2 <= 0) { break; }
+
+            ret += (diff1 + diff2);
+        }
+
+        return ret;
     }
 };
