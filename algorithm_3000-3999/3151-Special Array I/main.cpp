ab2c1484f281ed8b5bd5f74f8b4d6df6a2929531
coolyuyuyu <coolyuyuyu@gmail.com>
2024-05-21 00:57:40 +0800

    3151: initial solution

---
+++ algorithm_3000-3999/3151-Special Array I/main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for (int i = 1, n = nums.size(); i < n; ++i) {
+            if ((nums[i - 1] & 1) == (nums[i] & 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
