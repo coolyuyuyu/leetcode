7661e6eedbefdc381c00928e862a4e6e2a6ced88
coolyuyuyu <coolyuyuyu@gmail.com>
2024-05-31 15:20:22 +0800

    1442: initial solution

---
+++ algorithm_1000-1999/1442-Count Triplets That Can Form Two Arrays of Equal XOR/main.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        /*
+        sum(i:j-1) == sum(j:k)
+        presum(j - 1) ^ presum(i - 1) == presum(k) ^ presum(j - 1)
+        presum(i - 1) == presum(k)
+        */
+        unordered_map<int, vector<int>> presum2indexes;
+        presum2indexes[0].push_back(-1);
+
+
+        int ret = 0;
+        for (int k = 0, sum = 0; k < arr.size(); ++k) {
+            int num = arr[k];
+
+            sum ^= num;
+            for (int t : presum2indexes[sum]) {
+                int i = t + 1;
+                ret += k - i;
+            }
+
+            presum2indexes[sum].push_back(k);
+        }
+
+        return ret;
+    }
+};
