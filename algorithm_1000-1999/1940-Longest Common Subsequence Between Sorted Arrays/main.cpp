e2575f4be58e78013c5f3ecea6de3734f5aa250c
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-04 09:07:44 +0800

    1940: update solution

--- algorithm_1000-1999/1940-Longest Common Subsequence Between Sorted Arrays/main.cpp
+++ algorithm_1000-1999/1940-Longest Common Subsequence Between Sorted Arrays/main.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> longestCommonSubsequence(vector<vector<int>>& arrays) {
-        vector<int> ret;
         vector<int> cnts(101, 0);
-        for (const auto& array : arrays) {
+
+        vector<int> ret;
+        for (const vector<int>& array : arrays) {
             for (int num : array) {
                 if (++cnts[num] == arrays.size()) {
                     ret.push_back(num);
