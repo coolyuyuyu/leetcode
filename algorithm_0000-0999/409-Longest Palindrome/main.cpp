3567b55136eb636726584adfee5f9354fc1e7a69
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-04 09:07:33 +0800

    409: update solution

--- algorithm_0000-0999/409-Longest Palindrome/main.cpp
+++ algorithm_0000-0999/409-Longest Palindrome/main.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int> counts(52, 0);
-        for (string::const_iterator iter = s.begin(); iter != s.end(); ++iter) {
-            if ('a' <= *iter) {
-                ++counts[*iter - 'a'];
-            }
-            else {
-                ++counts[*iter - 'A' + 26];
-            }
+        vector<int> cnts(256, 0);
+        for (char c : s) {
+            ++cnts[c];
         }
 
-        int sum = 0;
-        bool hasOdd = false;
-        for (vector<int>::const_iterator iter = counts.begin(); iter != counts.end(); ++iter) {
-            if (*iter) {
-                if (*iter % 2) {
-                    hasOdd = true;
-                    sum += (*iter - 1);
-                }
-                else {
-                    sum += *iter;
-                }
-            }
-        }
-        if (hasOdd) {
-            sum += 1;
-        }
+        int ret = 0;
+        bool anyOdd = false;
+        for (int cnt : cnts) {
+            anyOdd |= cnt & 1;
+            ret += cnt & ~(1);
 
-        return sum;
+        }
+        ret += anyOdd ? 1 : 0;
+
+        return ret;
     }
 };
