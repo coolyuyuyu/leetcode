775fd1a996cd8a403c061216305ca4199b93a0af
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-08 03:50:46 +0800

    1002: update solution

--- algorithm_1000-1999/1002-Find Common Characters/main.cpp
+++ algorithm_1000-1999/1002-Find Common Characters/main.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    vector<string> commonChars(vector<string>& A) {
-        auto genCounts = [](const string& str) -> vector<int> {
-            vector<int> counts(26, 0);
-            for (char c : str) {
-                ++counts[c - 'a'];
+    vector<string> commonChars(vector<string>& words) {
+        std::function<vector<int>(const string&)> computeCnt = [](const string& word) {
+            vector<int> ret(26, 0);
+            for (char c : word) {
+                ++ret[c - 'a'];
             }
-
-            return counts;
+            return ret;
         };
 
-        vector<int> commonCounts = genCounts(A[0]);
-        for (const string& str : A) {
-            vector<int> counts = genCounts(str);
-            for (size_t i = 0; i < 26; ++i) {
-                commonCounts[i] = min(commonCounts[i], counts[i]);
+        vector<int> cnts = computeCnt(words[0]);
+        for (const string& word : words) {
+            vector<int> tmps = computeCnt(word);
+            for (int i = 0; i < 26; ++i) {
+                cnts[i] = std::min(cnts[i], tmps[i]);
             }
         }
 
         vector<string> ret;
-        for (size_t i = 0; i < 26; ++i) {
-            for (int j = 0; j < commonCounts[i]; ++j) {
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < cnts[i]; ++j) {
                 ret.emplace_back(1, 'a' + i);
             }
         }
+
         return ret;
     }
 };
