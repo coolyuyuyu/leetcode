419d488774ec1a7427dfa14fbb4a6729760988fd
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-12 09:24:18 +0800

    523: update solution

--- algorithm_0000-0999/523-Continuous Subarray Sum/main.cpp
+++ algorithm_0000-0999/523-Continuous Subarray Sum/main.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
+        // X X X X X X X X
+        //         j     i
+        // sum(j+1, i) = presum(i) - presum(j) = n * k
+        // => presum(i) % k == presum(j) % k
+
         unordered_set<int> reminders({0});
         for (int i = 1, presum = nums[0]; i < nums.size(); ++i) {
             presum += nums[i];
