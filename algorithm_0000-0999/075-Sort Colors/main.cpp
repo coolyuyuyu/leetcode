9f99c752869db263c2a7326345ab319a3c001de8
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-12 09:24:12 +0800

    075: update solution

--- algorithm_0000-0999/075-Sort Colors/main.cpp
+++ algorithm_0000-0999/075-Sort Colors/main.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         // quick select
-        int i = 0, j = nums.size() - 1, x = 0;
-        while (x <= j) {
-            if (nums[x] == 0) {
-                std::swap(nums[i], nums[x]);
-                ++i;
-                ++x;
-            }
-            else if (nums[x] == 1) {
-                ++x;
-            }
-            else {
-                std::swap(nums[x], nums[j]);
-                --j;
+        for (int i = 0, j = nums.size() - 1, x = 0; x <= j;) {
+            switch (nums[x]) {
+                case 0:
+                    std::swap(nums[i], nums[x]);
+                    ++i, ++x;
+                    break;
+                case 1:
+                    ++x;
+                    break;
+                case 2:
+                    std::swap(nums[x], nums[j]);
+                    --j;
+                    break;
             }
         }
     }
