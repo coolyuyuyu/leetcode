42764f9ced45f71fd2612f2acd86436f679a53cd
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-12 09:24:35 +0800

    1122: update solution

--- algorithm_1000-1999/1122-Relative Sort Array/main.cpp
+++ algorithm_1000-1999/1122-Relative Sort Array/main.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> indexes(1002, -1);
+        vector<int> num2idx(1002, -1);
         for (int i = 0; i < arr2.size(); ++i) {
-            indexes[arr2[i]] = i;
+            num2idx[arr2[i]] = i;
         }
 
-        auto comp = [&indexes](int num1, int num2) -> bool {
-            if (0 <= indexes[num1]) {
-                if (0 <= indexes[num2]) {
-                    return indexes[num1] < indexes[num2];
-                }
-                else {
-                    return true;
-                }
+        auto comp = [&](int a, int b) {
+            if (num2idx[a] == -1 && num2idx[b] == -1 ) {
+                return a < b;
+            }
+            else if (num2idx[a] == -1) {
+                return false;
+            }
+            else if (num2idx[b] == -1)  {
+                return true;
             }
             else {
-                if (0 <= indexes[num2]) {
-                    return false;
-                }
-                else {
-                    return num1 < num2;
-                }
+                return num2idx[a] < num2idx[b];
             }
         };
-        sort(arr1.begin(), arr1.end(), comp);
+        std::sort(arr1.begin(), arr1.end(), comp);
 
         return arr1;
     }
