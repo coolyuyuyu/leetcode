be087b5db4e18595b139d839d458ac56b5b0d78e
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-18 00:11:17 +0800

    2037: update solution

--- algorithm_2000-2999/2037-Minimum Number of Moves to Seat Everyone/main.cpp
+++ algorithm_2000-2999/2037-Minimum Number of Moves to Seat Everyone/main.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        std::sort(seats.begin(), seats.end());
-        std::sort(students.begin(), students.end());
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
 
-        int move = 0;
-        for (size_t i = 0; i < seats.size(); ++i) {
-            move += abs(seats[i] - students[i]);
+        int ret = 0;
+        for (int i = 0, n = seats.size(); i < n; ++i) {
+            ret += abs(seats[i] - students[i]);
         }
-        return move;
+
+        return ret;
     }
 };
