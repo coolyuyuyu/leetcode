31dcb6fe57f006856c231ced4665f498319d01ad
coolyuyuyu <coolyuyuyu@gmail.com>
2024-06-24 23:03:58 +0800

    2743: update solution

--- algorithm_2000-2999/2743-Count Substrings Without Repeating Character/main.cpp
+++ algorithm_2000-2999/2743-Count Substrings Without Repeating Character/main.cpp
@@ -4,12 +4,12 @@ public:
         vector<bool> repeated(26, false);
 
         int ret = 0;
-        for (int lft = 0, rht = 0; lft < s.size(); ++lft) {
-            for (; rht < s.size() && !repeated[s[rht] - 'a']; ++rht) {
+        for (int lft = 0, rht = 0, n = s.size(); lft < n; ++lft) {
+            for (; rht < n && repeated[s[rht] - 'a'] == false; ++rht) {
                 repeated[s[rht] - 'a'] = true;
             }
 
-            ret += (rht - lft);
+            ret += rht - lft;
 
             repeated[s[lft] - 'a'] = false;
         }
