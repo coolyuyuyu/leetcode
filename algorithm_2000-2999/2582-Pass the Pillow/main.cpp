bdf3f6702106d31b672847ebb5d22b8013c4db8c
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-07 11:58:49 +0800

    2582: update solution

--- algorithm_2000-2999/2582-Pass the Pillow/main.cpp
+++ algorithm_2000-2999/2582-Pass the Pillow/main.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int index = 1;
-        int diff = 1;
-        for (int t = 0; t < time; ++t) {
-            if (n < index + diff) {
+        int ret = 1;
+        for (int t = 0, diff = 1; t < time; ++t) {
+            ret += diff;
+            if (ret == n) {
                 diff = -1;
             }
-            else if (index + diff == 0) {
+            else if (ret == 1) {
                 diff = 1;
             }
-            index += diff;
         }
 
-        return index;
+        return ret;
     }
 };
