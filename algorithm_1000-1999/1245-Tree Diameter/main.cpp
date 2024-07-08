fbe0bc53a1019e94f4130c2a0b2b7765b98d706b
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-09 00:20:52 +0800

    1245: update solution

--- algorithm_1000-1999/1245-Tree Diameter/main.cpp
+++ algorithm_1000-1999/1245-Tree Diameter/main.cpp
@@ -1,72 +1,46 @@
 class Solution {
 public:
-    int f1(vector<vector<int>>& edges) {
-        // Assume the longest path of the graph is in between node a and b.
-        // Start from any node s, the farest node against s must be a or b
-
-        int n = edges.size() + 1;
-        vector<int> graph[n];
-        for (const auto& edge : edges) {
-            int a = edge[0], b = edge[1];
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-        }
-
-        std::function<pair<int, int>(int, int)> f = [&](int cur, int pre) {
-            pair<int, int> ret = {-1, cur};
-            for (int nxt : graph[cur]) {
-                if (nxt == pre) { continue; }
-
-                auto [d, v] = f(nxt, cur);
-                if (d > ret.first) {
-                    ret = {d, v};
-                }
-            }
-            ret.first += 1;
-
-            return ret;
-        };
-
-        auto [_, s] = f(0, -1);
-        auto [d , t] = f(s, -1);
-        return d;
-    }
-
-    int f2(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        vector<int> graph[n];
-        for (const auto& edge : edges) {
-            int a = edge[0], b = edge[1];
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-        }
-
-        int ret = 0;
-        std::function<int(int, int)> f = [&](int cur, int pre) {
-            int d1 = 0, d2 = 0;
-            for (int nxt : graph[cur]) {
-                if (nxt == pre) { continue; }
-
-                int d = f(nxt, cur);
-                if (d >= d1) {
-                    d2 = d1;
-                    d1 = d;
-                }
-                else if (d > d2) {
-                    d2 = d;
-                }
-            }
-            ret = std::max(ret, d1 + d2);
-
-            return d1 + 1;
-        };
-        f(0, -1);
-
-        return ret;
-    }
-
     int treeDiameter(vector<vector<int>>& edges) {
-        return f1(edges);
-        //return f2(edges);
+        // Assume the longest path of the graph is in between node a and b.
+        // Start from any node s, the farest node against s must be a or b.
+
+        int n = edges.size() + 1;
+        vector<int> graph[n];
+        for (const auto& edge : edges) {
+            int a = edge[0], b = edge[1];
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+
+        std::function<pair<int, int>(int)> bfs = [&](int src) {
+            bool visited[n];
+            std::fill(visited, visited + n, false);
+
+            queue<int> q;
+            visited[src] = true;
+            q.push(src);
+
+            int leaf = src, depth = -1;
+            for (; !q.empty(); ++depth) {
+                leaf = q.front();
+                for (int k = q.size(); 0 < k--;) {
+                    int cur = q.front();
+                    q.pop();
+                    for (int nxt : graph[cur]) {
+                        if (visited[nxt]) { continue; }
+
+                        visited[nxt] = true;
+                        q.push(nxt);
+                    }
+                }
+            }
+
+            return pair<int, int>{leaf, depth};
+        };
+
+        auto [s, maxDistfromS] = bfs(0);
+        auto [t, maxDistfromT] = bfs(s);
+
+        return maxDistfromT;
     }
 };
