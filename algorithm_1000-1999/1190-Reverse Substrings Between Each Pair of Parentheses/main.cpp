00da9f539cc96ba512c0d2a151b12007e15e1205
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-13 02:35:32 +0800

    1190: update solution

--- algorithm_1000-1999/1190-Reverse Substrings Between Each Pair of Parentheses/main.cpp
+++ algorithm_1000-1999/1190-Reverse Substrings Between Each Pair of Parentheses/main.cpp
@@ -1,28 +1,68 @@
 class Solution {
 public:
-    string reverseParentheses(string s) {
-        stack<string> stk;
-        stk.push("");
+    // Time: O(n^2)
+    string byStack(string s) {
+        string ret;
 
-        string tmp;
+        stack<int> stk;
         for (char c : s) {
             switch (c) {
             case '(':
-                stk.push("");
+                stk.push(ret.size());
                 break;
             case ')':
-                tmp = stk.top();
+                std::reverse(ret.begin() + stk.top(), ret.end());
                 stk.pop();
-
-                reverse(tmp.begin(), tmp.end());
-                stk.top() += tmp;
                 break;
             default:
-                stk.top() += c;
+                ret.push_back(c);
                 break;
             }
         }
 
-        return stk.top();
+        return ret;
+    }
+
+    // Time: O(n)
+    string byLinear(string s) {
+        int n = s.size();
+
+        vector<int> mapping(n);
+        stack<int> stk;
+        for (int i = 0; i < n; ++i) {
+            switch (s[i]) {
+            case '(':
+                stk.push(i);
+                break;
+            case ')':
+                mapping[i] = stk.top();
+                mapping[stk.top()] = i;
+                stk.pop();
+                break;
+            default:
+                break;
+            }
+        }
+
+        string ret;
+        for (int i = 0, dir = 1; i < n; i += dir) {
+            switch (s[i]) {
+            case '(':
+            case ')':
+                i = mapping[i];
+                dir = -dir;
+                break;
+            default:
+                ret.push_back(s[i]);
+                break;
+            }
+        }
+
+        return ret;
+    }
+
+    string reverseParentheses(string s) {
+        //return byStack(s);
+        return byLinear(s);
     }
 };
