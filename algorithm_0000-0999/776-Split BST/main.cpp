22f85c46ea5f41f33b8ebf4cf2c23e8e13e49e74
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-13 02:35:25 +0800

    776: update solution

--- algorithm_0000-0999/776-Split BST/main.cpp
+++ algorithm_0000-0999/776-Split BST/main.cpp
@@ -11,55 +11,50 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> splitBST_Recursive(TreeNode* root, int V) {
-        if (!root) {
-            return {nullptr, nullptr};
-        }
+    vector<TreeNode*> recursive(TreeNode* root, int target) {
+        std::function<pair<TreeNode*, TreeNode*>(TreeNode*)> f = [&](TreeNode* root) -> pair<TreeNode*, TreeNode*> {
+            if (!root) { return {nullptr, nullptr}; }
 
-        vector<TreeNode*> ans;
-        if (root->val <= V){
-            ans = splitBST_Recursive(root->right, V);
-            root->right = ans[0];
-            ans[0] = root;
-        }
-        else {
-            ans = splitBST_Recursive(root->left, V);
-            root->left = ans[1];
-            ans[1] = root;
-        }
-
-        return ans;
-    }
-
-    vector<TreeNode*> splitBST_Iterative(TreeNode* root, int V) {
-        TreeNode* pLftRoot = nullptr;
-        TreeNode* pRhtRoot = nullptr;
-        TreeNode** ppLftNode = &pLftRoot;
-        TreeNode** ppRhtNode = &pRhtRoot;
-        while (root) {
-            if (root->val <= V) {
-                *ppLftNode = root;
-                root = root->right;
-                ppLftNode = &((*ppLftNode)->right);\
-
-                *ppLftNode = nullptr;
-                *ppRhtNode = root;
+            if (root->val <= target) {
+                auto [childRoot1, childRoot2] = f(root->right);
+                root->right = childRoot1;
+                return {root, childRoot2};
             }
             else {
-                *ppRhtNode = root;
-                root = root->left;
-                ppRhtNode = &((*ppRhtNode)->left);
+                auto [childRoot1, childRoot2] = f(root->left);
+                root->left = childRoot2;
+                return {childRoot1, root};
+            }
+        };
 
-                *ppRhtNode = nullptr;
-                *ppLftNode = root;
+        auto [ret1, ret2] = f(root);
+        return {ret1, ret2};
+    }
+
+    vector<TreeNode*> iterative(TreeNode* root, int target) {
+        TreeNode *pRoot1 = nullptr, *pRoot2 = nullptr;
+        for (TreeNode **ppNode1 = &pRoot1, **ppNode2 = &pRoot2; root;) {
+            if (root->val <= target) {
+                *ppNode1 = root;
+                *ppNode2 = root->right;
+                ppNode1 = &((*ppNode1)->right);
+                root = root->right;
+                *ppNode1 = nullptr;
+            }
+            else {
+                *ppNode2 = root;
+                *ppNode1 = root->left;
+                ppNode2 = &((*ppNode2)->left);
+                root = root->left;
+                *ppNode2 = nullptr;
             }
         }
 
-        return {pLftRoot,  pRhtRoot};
+        return {pRoot1, pRoot2};
     }
 
-    vector<TreeNode*> splitBST(TreeNode* root, int V) {
-        //return splitBST_Recursive(root, V);
-        return splitBST_Iterative(root, V);
+    vector<TreeNode*> splitBST(TreeNode* root, int target) {
+        //return recursive(root, target);
+        return iterative(root, target);
     }
 };
