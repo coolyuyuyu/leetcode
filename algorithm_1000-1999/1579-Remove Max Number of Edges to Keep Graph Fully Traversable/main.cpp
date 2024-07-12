0f03b2450458dadc51d4fb7782c5cb1d1d7fe003
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-13 02:35:42 +0800

    1579: update solution

--- algorithm_1000-1999/1579-Remove Max Number of Edges to Keep Graph Fully Traversable/main.cpp
+++ algorithm_1000-1999/1579-Remove Max Number of Edges to Keep Graph Fully Traversable/main.cpp
@@ -6,16 +6,15 @@ public:
         std::iota(m_parents.begin(), m_parents.end(), 0);
     }
 
-    DisjointSets(const DisjointSets& ds)
-        : m_parents(ds.m_parents)
-        , m_size(ds.m_size) {
-    }
-
-    void merge(int elem1, int elem2) {
+    bool merge(int elem1, int elem2) {
         int root1 = root(elem1), root2 = root(elem2);
         if (root1 != root2) {
             m_parents[root1] = root2;
             --m_size;
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
@@ -45,55 +44,50 @@ public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
         vector<pair<int, int>> edges1, edges2, edges3;
         for (const auto& edge : edges) {
-            switch (edge[0]) {
+            int type = edge[0], u = edge[1] - 1, v = edge[2] - 1;
+            switch (type) {
             case 1:
-                edges1.emplace_back(edge[1] - 1, edge[2] - 1);
+                edges1.emplace_back(u, v);
                 break;
             case 2:
-                edges2.emplace_back(edge[1] - 1, edge[2] - 1);
+                edges2.emplace_back(u, v);
                 break;
             case 3:
-                edges3.emplace_back(edge[1] - 1, edge[2] - 1);
-                break;
-            default:
+                edges3.emplace_back(u, v);
                 break;
             }
         }
 
         DisjointSets ds(n);
-
         int cnt3 = 0;
-        for (int i = 0; i < edges3.size() && 1 < ds.size(); ++i) {
-            auto [v1, v2] = edges3[i];
-            if (!ds.connected(v1, v2)) {
-                ds.merge(v1, v2);
+        for (const auto& [u, v] : edges3) {
+            if (ds.merge(u, v)) {
                 ++cnt3;
             }
         }
+        if (ds.size() == 1) {
+            return edges.size() - (n - 1);
+        }
 
-        int cnt1 = 0;
         DisjointSets ds1(ds);
-        for (int i = 0; i < edges1.size() && 1 < ds1.size(); ++i) {
-            auto [v1, v2] = edges1[i];
-            if (!ds1.connected(v1, v2)) {
-                ds1.merge(v1, v2);
+        int cnt1 = 0;
+        for (const auto& [u, v] : edges1) {
+            if (ds1.merge(u, v)) {
                 ++cnt1;
             }
         }
-        if (1 < ds1.size()) {
+        if (ds1.size() > 1) {
             return -1;
         }
 
-        int cnt2 = 0;
         DisjointSets ds2(ds);
-        for (int i = 0; i < edges2.size() && 1 < ds2.size(); ++i) {
-            auto [v1, v2] = edges2[i];
-            if (!ds2.connected(v1, v2)) {
-                ds2.merge(v1, v2);
+        int cnt2 = 0;
+        for (const auto& [u, v] : edges2) {
+            if (ds2.merge(u, v)) {
                 ++cnt2;
             }
         }
-        if (1 < ds2.size()) {
+        if (ds2.size() > 1) {
             return -1;
         }
 
