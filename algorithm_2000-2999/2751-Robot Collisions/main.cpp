6458792b0c717fce299f6042b20ea9ceb23bf58e
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-14 02:03:46 +0800

    2751: update solution

--- algorithm_2000-2999/2751-Robot Collisions/main.cpp
+++ algorithm_2000-2999/2751-Robot Collisions/main.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        vector<int> indexes(positions.size());
-        std::iota(indexes.begin(), indexes.end(), 0);
-        std::sort(indexes.begin(), indexes.end(), [&](int i1, int i2) { return positions[i1] < positions[i2]; });
+        int n = positions.size();
+
+        int indexes[n];
+        std::iota(indexes, indexes + n, 0);
+        std::sort(indexes, indexes + n, [&](int idx1, int idx2){ return positions[idx1] < positions[idx2]; });
 
         stack<int> stk;
-        for (int index : indexes) {
-            while (!stk.empty() && directions[stk.top()] == 'R' && directions[index] == 'L') {
-                if (healths[stk.top()] < healths[index]) {
+        for (int idx : indexes) {
+            while (!stk.empty() && directions[stk.top()] == 'R' && directions[idx] == 'L' && healths[idx] > 0) {
+                if (healths[stk.top()] < healths[idx]) {
                     healths[stk.top()] = 0;
-                    healths[index] -= 1;
+                    healths[idx] -= 1;
                     stk.pop();
                 }
-                else if (healths[stk.top()] == healths[index]) {
+                else if (healths[stk.top()] == healths[idx]) {
                     healths[stk.top()] = 0;
-                    healths[index] = 0;
+                    healths[idx] = 0;
                     stk.pop();
-                    break;
                 }
                 else {
-                    healths[stk.top()] -= 1;
-                    if (healths[stk.top()] == 0) {
+                    if (--healths[stk.top()] == 0) {
                         stk.pop();
                     }
-                    healths[index] = 0;
-                    break;
+                    healths[idx] = 0;
                 }
             }
 
-            if (0 < healths[index]) {
-                stk.push(index);
+            if (healths[idx] > 0) {
+                stk.push(idx);
             }
         }
 
         vector<int> ret;
         for (int health : healths) {
-            if (0 < health) {
+            if (health) {
                 ret.push_back(health);
             }
         }
