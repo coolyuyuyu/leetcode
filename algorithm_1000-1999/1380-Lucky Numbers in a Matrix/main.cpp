15a4b2c15ceb33a9095fff2329099c0b34e06f94
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-23 01:04:01 +0800

    1380: update solution

--- algorithm_1000-1999/1380-Lucky Numbers in a Matrix/main.cpp
+++ algorithm_1000-1999/1380-Lucky Numbers in a Matrix/main.cpp
@@ -1,23 +1,59 @@
 class Solution {
 public:
-    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix.front().size();
-        vector<int> rowMinNums(m, INT_MAX), colMaxNums(n, INT_MIN);
-        for (size_t i = 0; i < m; ++i) {
-            for (size_t j = 0; j < n; ++j) {
-                rowMinNums[i] = std::min(rowMinNums[i], matrix[i][j]);
-                colMaxNums[j] = std::max(colMaxNums[j], matrix[i][j]);
+    vector<int> bruteforce(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+
+        vector<int> rowMins(m, INT_MAX), colMaxs(n, INT_MIN);
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                rowMins[r] = std::min(rowMins[r], matrix[r][c]);
+                colMaxs[c] = std::max(colMaxs[c], matrix[r][c]);
             }
         }
 
-        for (size_t i = 0; i < m; ++i) {
-            for (size_t j = 0; j < n; ++j) {
-                if (rowMinNums[i] == colMaxNums[j]) {
-                    return {rowMinNums[i]};
+        vector<int> ret;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (matrix[r][c] == rowMins[r] && matrix[r][c] == colMaxs[c]) {
+                    ret.push_back(matrix[r][c]);
                 }
             }
         }
 
-        return {};
+        return ret;
+    }
+
+    vector<int> greedy(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+
+        int rowMinMax = INT_MIN;
+        for (int r = 0; r < m; ++r) {
+            int rowMin = INT_MAX;
+            for (int c = 0; c < n; ++c) {
+                rowMin = std::min(rowMin, matrix[r][c]);
+            }
+            rowMinMax = std::max(rowMinMax, rowMin);
+        }
+
+        int colMaxMin = INT_MAX;
+        for (int c = 0; c < n; ++c) {
+            int colMax = INT_MIN;
+            for (int r = 0; r < m; ++r) {
+                colMax = std::max(colMax, matrix[r][c]);
+            }
+            colMaxMin = std::min(colMaxMin, colMax);
+        }
+
+        vector<int> ret;
+        if (rowMinMax == colMaxMin) {
+            ret.push_back(rowMinMax);
+        }
+
+        return ret;
+    }
+
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        //return bruteforce(matrix);
+        return greedy(matrix);
     }
 };
