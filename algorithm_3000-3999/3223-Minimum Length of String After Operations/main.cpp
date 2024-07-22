368faccdae565c2443254a04d903e66155f2e6b4
coolyuyuyu <coolyuyuyu@gmail.com>
2024-07-23 01:03:32 +0800

    3223: initial solution

---
+++ algorithm_3000-3999/3223-Minimum Length of String After Operations/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> cnts(26, 0);
+        for (char c : s) {
+            ++cnts[c - 'a'];
+        }
+
+        int ret = 0;
+        for (int cnt : cnts) {
+            if (cnt <= 2) {
+                ret += cnt;
+            }
+            else {
+                ret += cnt & 1 ? 1 : 2;
+            }
+        }
+
+        return ret;
+    }
+};
