34df7ec58d9104dfcf6227f6b1dbe68e3b5b0770
coolyuyuyu <coolyuyuyu@gmail.com>
2024-08-01 01:07:13 +0800

    1105: update solution

--- algorithm_1000-1999/1105-Filling Bookcase Shelves/main.cpp
+++ algorithm_1000-1999/1105-Filling Bookcase Shelves/main.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
         int n = books.size();
+
         books.insert(books.begin(), {0, 0});
 
-        //dp[i]: the minimum height of bookshelf can be after placing books[1:i]
+        // dp[i]: the minimum height of bookshelf from books[1:i]
         int dp[n + 1];
         std::fill(dp, dp + n + 1, INT_MAX);
         dp[0] = 0;
+
         for (int i = 1; i <= n; ++i) {
-            int width = 0, maxHeight = 0;
+            int totalW = 0, maxH = 0;
             for (int j = i; j >= 1; --j) {
-                width += books[j][0];
-                if (width > shelfWidth) {
+                int w = books[j][0], h = books[j][1];
+                totalW += w;
+                if (totalW > shelfWidth) {
                     break;
                 }
-                maxHeight = std::max(maxHeight, books[j][1]);
-                dp[i] = std::min(dp[i], dp[j - 1] + maxHeight);
+                maxH = std::max(maxH, h);
+                dp[i] = std::min(dp[i], dp[j - 1] + maxH);
             }
         }
 
