3791d4baab619cbcaea4a04323c669bee4972336
coolyuyuyu <coolyuyuyu@gmail.com>
2024-08-20 21:31:07 +0800

    1140: update solution

--- algorithm_1000-1999/1140-Stone Game II/main.cpp
+++ algorithm_1000-1999/1140-Stone Game II/main.cpp
@@ -3,40 +3,40 @@ public:
     int topdnDFS(vector<int>& piles) {
         int n = piles.size();
 
-        int presum[n];
-        std::partial_sum(piles.begin(), piles.end(), presum);
-        std::function<int(int)> sufsum = [&](int i) {
-            if (i >= n) {
+        int presums[n];
+        std::partial_sum(piles.begin(), piles.end(), presums);
+        std::function<int(int, int)> sum = [&](int lo, int hi) {
+            if (lo > hi) {
                 return 0;
             }
-            return presum[n - 1] - (0 < i ? presum[i - 1] : 0);
+            return presums[hi] - (lo > 0 ? presums[lo - 1] : 0);
         };
 
-        // dp[i]: the maximum number of stones the player can take from piles[i:]
+        // dp[i][j]: the maximum number of stones the player can take from piles[i:] where M = j
         int dp[n][n + 1];
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j <= n; ++j) {
                 dp[i][j] = 0;
             }
         }
-        std::function<int(int, int)> f = [&](int i, int M) {
-            if (n <= i) {
+        std::function<int(int, int)> solve = [&](int i, int M) {
+            if (i >= n) {
                 return 0;
             }
 
             int& ret = dp[i][M];
-            if (0 < ret) {
+            if (ret > 0) {
                 return ret;
             }
 
             for (int X = 1; X <= 2 * M && i + X - 1 < n; ++X) {
-                //ret = std::max(ret, sum(i, i + X - 1) + sum(i + X, n) - f(i + X, std::max(M, X)));
-                ret = std::max(ret, sufsum(i) - f(i + X, std::max(M, X)));
+                ret = std::max(ret, sum(i, i + X - 1) + sum(i + X, n - 1) - solve(i + X, std::max(X, M)));
             }
+
             return ret;
         };
 
-        return f(0, 1);
+        return solve(0, 1);
     }
 
     int stoneGameII(vector<int>& piles) {
