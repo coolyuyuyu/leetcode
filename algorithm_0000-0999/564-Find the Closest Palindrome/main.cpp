b6a0b2f30d8494d5e84bda68ac107336a60fd0b4
coolyuyuyu <coolyuyuyu@gmail.com>
2024-08-25 11:48:23 +0800

    564: update solution

--- algorithm_0000-0999/564-Find the Closest Palindrome/main.cpp
+++ algorithm_0000-0999/564-Find the Closest Palindrome/main.cpp
@@ -3,27 +3,27 @@ public:
     string nearestPalindromic(string n) {
         std::function<string(const string&)> nextSmallerPalindrom = [](const string& s) {
             int n = s.size();
-            string t = s;
+            string t(s);
 
-            for (int i = (n - 1) / 2; 0 <= i; --i) {
-                t[n - 1 - i] = t[i];
+            for (int l = 0, r = n - 1; l < r; ++l, --r) {
+                t[r] = t[l];
             }
             if (t < s) {
                 return t;
             }
 
             int carry = -1;
-            for (int i = (n - 1) / 2; 0 <= i; --i) {
+            for (int i = (n - 1) / 2; i >= 0; --i) {
                 t[i] += carry;
                 if (isdigit(t[i])) {
                     carry = 0;
                 }
                 else {
                     t[i] = '9';
+                    carry = -1;
                 }
-                t[n - 1 - i] = t[i];
+                t[n - i - 1] = t[i];
             }
-
             if (t[0] == '0' && n > 1) {
                 return string(n - 1, '9');
             }
@@ -33,44 +33,42 @@ public:
         };
         std::function<string(const string&)> nextGreaterPalindrom = [](const string& s) {
             int n = s.size();
-            string t = s;
+            string t(s);
 
-            for (int i = (n - 1) / 2; 0 <= i; --i) {
-                t[n - 1 - i] = t[i];
+            for (int l = 0, r = n - 1; l < r; ++l, --r) {
+                t[r] = t[l];
             }
             if (t > s) {
                 return t;
             }
 
             int carry = 1;
-            for (int i = (n - 1) / 2; 0 <= i; --i) {
+            for (int i = (n - 1) / 2; i >= 0; --i) {
                 t[i] += carry;
                 if (isdigit(t[i])) {
                     carry = 0;
                 }
                 else {
                     t[i] = '0';
+                    carry = 1;
                 }
                 t[n - 1 - i] = t[i];
             }
-
             if (carry) {
                 t.assign(n + 1, '0');
                 t[0] = t[n] = '1';
-                return t;
             }
-            else {
-                return t;
-            }
+
+            return t;
         };
 
-        string a = nextSmallerPalindrom(n);
-        string b = nextGreaterPalindrom(n);
-        if (stoll(n) - stoll(a) <= stoll(b) - stoll(n)) {
-            return a;
+        string nxt = nextGreaterPalindrom(n);
+        string pre = nextSmallerPalindrom(n);
+        if (stoll(n) - stoll(pre) <= stoll(nxt) - stoll(n)) {
+            return pre;
         }
         else {
-            return b;
+            return nxt;
         }
     }
 };
