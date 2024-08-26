620845c72f7aedec9b0aa02be8aa21aa1282bf91
coolyuyuyu <coolyuyuyu@gmail.com>
2024-08-26 21:50:35 +0800

    320: update solution

--- algorithm_0000-0999/320-Generalized Abbreviation/main.cpp
+++ algorithm_0000-0999/320-Generalized Abbreviation/main.cpp
@@ -1,97 +1,60 @@
 class Solution {
 public:
-    void generateAbbreviationsbBackTrackV1(const string& word, vector<bool> used, size_t index, vector<string>& abbrs) {
-        if (word.size() <= index) {
-            string abbr;
-            for (size_t index = 0, preIndex = 0; index <= used.size(); ++index) {
-                if (index == used.size() || used[index]) {
-                    if (preIndex < index) {
-                        abbr += to_string(index - preIndex);
-                    }
+    vector<string> recursive(string word) {
+        int n = word.size();
 
-                    if (index < used.size()) {
-                        abbr += word[index];
-                    }
-                    preIndex = index + 1;
+        string abbr;
+        vector<string> ret;
+        std::function<void(int)> f = [&](int start) {
+            if (start >= n) {
+                ret.push_back(abbr);
+                return;
+            }
+
+            abbr += word[start];
+            f(start + 1);
+            abbr.pop_back();
+
+            if (abbr.empty() || !std::isdigit(abbr.back())) {
+                for (int i = start; i < n; ++i) {
+                    string substr = std::to_string(i - start + 1);
+                    abbr += substr;
+                    f(i + 1);
+                    abbr.resize(abbr.size() - substr.size());
                 }
             }
-            abbrs.emplace_back(abbr);
-            return;
-        }
+        };
+        f(0);
 
-        generateAbbreviationsbBackTrackV1(word, used, index + 1, abbrs);
-        used[index] = true;
-        generateAbbreviationsbBackTrackV1(word, used, index + 1, abbrs);
+        return ret;
     }
 
-    void generateAbbreviationsbBackTrackV2(const string& word, size_t pos, size_t count, string& abbr, vector<string>& abbrs) {
-        if (word.size() <= pos) {
-            abbrs.emplace_back(abbr + (0 < count ? to_string(count) : ""));
-            return;
-        }
+    vector<string> iterative(string word) {
+        int n = word.size();
 
-        generateAbbreviationsbBackTrackV2(word, pos + 1, count + 1, abbr, abbrs);
-
-        size_t oldSize = abbr.size();
-        if (0 < count) {
-            abbr += to_string(count);
-        }
-        abbr += word[pos];
-        generateAbbreviationsbBackTrackV2(word, pos + 1, 0, abbr, abbrs);
-        abbr.resize(oldSize);
-    }
-
-    vector<string> generateAbbreviationsTopDownV1(const string& word) {
-        vector<string> abbrs;
-        abbrs.emplace_back(word.empty() ? "" : to_string(word.size()));
-        for (size_t i = 0; i < word.size(); ++i) {
-            string lft = (0 < i ? to_string(i) : "") + word[i];
-            for (const string& rht : generateAbbreviationsTopDownV1(word.substr(i + 1))) {
-                abbrs.emplace_back(lft + rht);
+        vector<string> ret;
+        for (int state = 0; state < (1 << n); ++state) {
+            string abbr;
+            for (int i = 0; i < n;) {
+                if ((state >> i) & 1) {
+                    int j = i + 1;
+                    for (; j < n && (state >> j) & 1; ++j)
+                        ;
+                    abbr += std::to_string(j - i);
+                    i = j;
+                }
+                else {
+                    abbr.push_back(word[i++]);
+                }
             }
-        }
-        return abbrs;
-    }
-
-    const vector<string>& generateAbbreviationsTopDownV2(const string& word, unordered_map<string, vector<string>>& cache) {
-        auto p = cache.emplace(word, vector<string>{});
-        if (p.second == false) {
-            return (p.first)->second;
+            ret.push_back(abbr);
         }
 
-        vector<string>& abbrs = (p.first)->second;
-        abbrs.reserve(pow(2, word.size()));
-        abbrs.emplace_back(word.empty() ? "" : to_string(word.size()));
-        for (size_t i = 0; i < word.size(); ++i) {
-            string lft = (0 < i ? to_string(i) : "") + word[i];
-            for (const string& rht : generateAbbreviationsTopDownV2(word.substr(i + 1), cache)) {
-                abbrs.emplace_back(lft + rht);
-            }
-        }
-
-        return abbrs;
+        return ret;
     }
 
     vector<string> generateAbbreviations(string word) {
-        /*
-        vector<string> abbrs;
-        vector<bool> used(word.size(), false);
-        generateAbbreviationsbBackTrackV1(word, used, 0, abbrs);
-        return abbrs;
-        */
-
-        /*
-        string abbr;
-        vector<string> abbrs;
-        generateAbbreviationsbBackTrackV2(word, 0, 0, abbr, abbrs);
-        return abbrs;
-        */
-
-        /*
-        return generateAbbreviationsTopDownV1(word);
-        */
-
-        unordered_map<string, vector<string>> cache;
-        return generateAbbreviationsTopDownV2(word, cache);
+        //return recursive(word);
+        return iterative(word);
     }
 };
