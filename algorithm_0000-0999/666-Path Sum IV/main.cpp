276b008828c32179557dbd0659864234cc39985a
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-02 02:07:59 +0800

    666: update solution

--- algorithm_0000-0999/666-Path Sum IV/main.cpp
+++ algorithm_0000-0999/666-Path Sum IV/main.cpp
@@ -1,69 +1,25 @@
 class Solution {
 public:
-    void pathSum_Recursive(map<pair<int, int>, int>& nodes, int& totalSum, int sum = 0, int depth = 1, int pos = 1) {
-        auto itr = nodes.find({depth, pos});
-        if (itr == nodes.end()) {
-            return;
-        }
-
-        sum += itr->second;
-
-        int lftPos = (pos - 1) * 2 + 1;
-        int rhtPos = (pos - 1) * 2 + 2;
-        if (nodes.find({depth + 1, lftPos}) == nodes.end() &&
-            nodes.find({depth + 1, rhtPos}) == nodes.end()) {
-            totalSum += sum;
-        }
-        else {
-            pathSum_Recursive(nodes, totalSum, sum, depth + 1, lftPos);
-            pathSum_Recursive(nodes, totalSum, sum, depth + 1, rhtPos);
-        }
-    }
-
-    void pathSum_Iterative(map<pair<int, int>, int>& nodes, int& totalSum) {
-        queue<tuple<int, int, int>> q; // <depth, pos, sum>
-        if (nodes.find({1, 1}) != nodes.end()) {
-            q.emplace(1, 1, 0);
-        }
-        while (!q.empty()) {
-            int depth = get<0>(q.front());
-            int pos = get<1>(q.front());
-            int sum = get<2>(q.front());
-            q.pop();
-
-            sum += nodes[{depth, pos}];
-
-            int lftPos = (pos - 1) * 2 + 1;
-            int rhtPos = (pos - 1) * 2 + 2;
-            if (nodes.find({depth + 1, lftPos}) == nodes.end() &&
-                nodes.find({depth + 1, rhtPos}) == nodes.end()) {
-                totalSum += sum;
-            }
-            else {
-                if (nodes.find({depth + 1, lftPos}) != nodes.end()) {
-                    q.emplace(depth + 1, lftPos, sum);
-                }
-                if (nodes.find({depth + 1, rhtPos}) != nodes.end()) {
-                    q.emplace(depth + 1, rhtPos, sum);
-                }
-            }
-        }
-    }
-
     int pathSum(vector<int>& nums) {
-        map<pair<int, int>, int> nodes; // <<depth, pos>, val>;
+        int arr[5][8];
+        std::memset(arr, 0, sizeof(arr));
+
         for (int num : nums) {
             int depth = num / 100;
-            int pos = (num % 100) / 10;
-            int val = num % 10;
-            nodes[{depth, pos}] = val;
+            int index = (num / 10) % 10 - 1;
+            int value = num % 10;
+            arr[depth][index] = value + arr[depth - 1][index / 2];
         }
 
-        int totalSum = 0;
+        int ret = 0;
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (i == 4 || (arr[i][j] != 0 && arr[i + 1][j * 2] == 0 && arr[i + 1][j * 2 + 1] == 0)) {
+                    ret += arr[i][j];
+                }
+            }
+        }
 
-        //pathSum_Recursive(nodes, totalSum);
-        pathSum_Iterative(nodes, totalSum);
-
-        return totalSum;
+        return ret;
     }
 };
