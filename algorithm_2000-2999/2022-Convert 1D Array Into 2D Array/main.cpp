461d9f82595ff5a3ea1ca56ac589e3649ddd252c
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-02 02:08:04 +0800

    2022: update solution

--- algorithm_2000-2999/2022-Convert 1D Array Into 2D Array/main.cpp
+++ algorithm_2000-2999/2022-Convert 1D Array Into 2D Array/main.cpp
@@ -5,11 +5,11 @@ public:
             return {};
         }
 
-        vector<vector<int>> converted;
-        for (auto itr = original.begin(); itr != original.end(); itr += n) {
-            converted.emplace_back(itr, itr + n);
-
+        vector<vector<int>> ret(m, vector<int>(n));
+        for (int i = 0; i < original.size(); ++i) {
+            ret[i / n][i % n] = original[i];
         }
-        return converted;
+
+        return ret;
     }
 };
