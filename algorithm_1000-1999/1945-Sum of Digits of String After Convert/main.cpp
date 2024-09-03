d87d305bb22998bb9464d79df20dc95a6aa54074
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-03 11:13:34 +0800

    1945: update solution

--- algorithm_1000-1999/1945-Sum of Digits of String After Convert/main.cpp
+++ algorithm_1000-1999/1945-Sum of Digits of String After Convert/main.cpp
@@ -3,18 +3,17 @@ public:
     int getLucky(string s, int k) {
         int ret = 0;
         for (char c : s) {
-            int num = 1 + c - 'a';
-            ret += num / 10;
-            ret += num % 10;
+            int x = 1 + c - 'a';
+            ret += x / 10;
+            ret += x % 10;
         }
 
-        for (int i = 1; i < k; ++i) {
-            int num = ret;
-            ret = 0;
-            while (num) {
-                ret += num %10;
-                num /= 10;
+        while (--k > 0) {
+            int sum = 0;
+            for (; ret; ret /= 10) {
+                sum += ret % 10;
             }
+            ret = sum;
         }
 
         return ret;
