a7bebf1fd6f1fab6ae687bfb39ba8070313164c2
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-12 14:31:03 +0800

    2220: update solution

--- algorithm_2000-2999/2220-Minimum Bit Flips to Convert Number/main.cpp
+++ algorithm_2000-2999/2220-Minimum Bit Flips to Convert Number/main.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {
+    // Time: O(32)
+    int f1(int start, int goal) {
         int ret = 0;
         for (int i = 0; i < 31; ++i) {
             if ((start & 1) != (goal & 1)) {
@@ -11,5 +12,15 @@ public:
         }
 
         return ret;
+    }
+
+    // Time: O(1)
+    int f2(int start, int goal) {
+        return __builtin_popcount(start ^ goal);
+    }
+
+    int minBitFlips(int start, int goal) {
+        //return f1(start, goal);
+        return f2(start, goal);
     }
 };
