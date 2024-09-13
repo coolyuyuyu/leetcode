e4fcb341f0945c2b9c8978101b130f4162a564a1
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-13 17:29:01 +0800

    1310: update solution

--- algorithm_1000-1999/1310-XOR Queries of a Subarray/main.cpp
+++ algorithm_1000-1999/1310-XOR Queries of a Subarray/main.cpp
@@ -4,9 +4,8 @@ public:
         vector<int> presum(arr.size());
         std::partial_sum(arr.begin(), arr.end(), presum.begin(), std::bit_xor<int>());
 
-        int n = queries.size();
-        vector<int> ret(n);
-        for (int i = 0; i < n; ++i) {
+        vector<int> ret(queries.size());
+        for (int i = 0; i < queries.size(); ++i) {
             int lft = queries[i][0], rht = queries[i][1];
             ret[i] = presum[rht] ^ (lft > 0 ? presum[lft - 1] : 0);
         }
