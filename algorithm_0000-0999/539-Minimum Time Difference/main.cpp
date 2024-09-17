3a3caaeb096c93e52461733611d9b630b66231fb
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-17 15:35:46 +0800

    539: update solution

--- algorithm_0000-0999/539-Minimum Time Difference/main.cpp
+++ algorithm_0000-0999/539-Minimum Time Difference/main.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    size_t findMinDifference(vector<string>& timePoints) {
-        vector<size_t> mins(timePoints.size());
-        for (size_t i = 0; i < mins.size(); ++i) {
-            mins[i] = (timePoints[i][0] - '0') * 600 + (timePoints[i][1] - '0') * 60 + (timePoints[i][3] - '0') * 10 + (timePoints[i][4] - '0') * 1;
+    int findMinDifference(vector<string>& timePoints) {
+        int n = timePoints.size();
+        int times[n];
+        for (int i = 0; i < n; ++i) {
+            times[i] = (timePoints[i][0] - '0') * 600 + (timePoints[i][1] - '0') * 60 + (timePoints[i][3] - '0') * 10 + (timePoints[i][4] - '0') * 1;
         }
-        sort(mins.begin(), mins.end());
+        std::sort(times, times + n);
 
-        size_t minDiff = numeric_limits<size_t>::max();
-        for (size_t i = 1; i < mins.size(); ++i) {
-            minDiff = min(minDiff, mins[i] - mins[i - 1]);
+        int ret = times[0] + 1440 - times[n - 1];
+        for (int i = 1; i < n; ++i) {
+            ret = std::min(ret, times[i] - times[i - 1]);
         }
-        minDiff = min(minDiff, mins.front() + 1440 - mins.back());
 
-        return minDiff;
+        return ret;
     }
 };
