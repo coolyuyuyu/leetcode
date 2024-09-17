89172058382a9221aa06ea06019ba4b0cd783721
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-17 15:35:56 +0800

    1371: update solution

--- algorithm_1000-1999/1371-Find the Longest Substring Containing Vowels in Even Counts/main.cpp
+++ algorithm_1000-1999/1371-Find the Longest Substring Containing Vowels in Even Counts/main.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    int getBitIdx(char c) {
+    bool isVowel(char c) {
         switch (c) {
-        case 'a': return 0;
-        case 'e': return 1;
-        case 'i': return 2;
-        case 'o': return 3;
-        case 'u': return 4;
-        default: return -1;
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            return true;
+        default:
+            return false;
         }
     };
 
     int findTheLongestSubstring(string s) {
         int n = s.size();
 
-        // state: binary representation of the parity of number of occurence of each vowel character
+        // state: binary representation of the parity of number of occurence of character
         // state2idx[state]: the least index of state of prefix
         unordered_map<int, int> state2idx;
         state2idx[0] = -1;
 
         int ret = 0;
         for (int i = 0, state = 0; i < n; ++i) {
-            int bidx = getBitIdx(s[i]);
-            if (bidx >= 0) {
-                state ^= (1 << bidx);
+            if (isVowel(s[i])) {
+                state ^= (1 << (s[i] - 'a'));
             }
 
             // All vowel characters of substring appear even number of times
             if (state2idx.find(state) != state2idx.end()) {
                 ret = std::max(ret, i - state2idx[state]);
             }
-
-            state2idx.emplace(state, i);
+            else {
+                state2idx.emplace(state, i);
+            }
         }
 
         return ret;
