dde070f62d2ef777c189738d4e56c4141d2f37f2
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-25 01:16:49 +0800

    440: update solution

--- algorithm_0000-0999/440-K-th Smallest in Lexicographical Order/main.cpp
+++ algorithm_0000-0999/440-K-th Smallest in Lexicographical Order/main.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
-    int countNumbersBeginWith(int prefix, int n) {
-        int cnt = 0;
-        for (long exp = 10; true; exp *= 10) {
-            long lower = prefix * exp;
-            long upper = prefix * exp + exp - 1;
-            if (n < lower) {
-                break;
-            }
-
-            if (lower <= n && n <= upper) {
-                cnt += (n - lower + 1);
-            }
-            else {
-                cnt += exp;
-            }
-        }
-
-        return cnt;
-    }
-
-    int findKthNumberBeginWith(int prefix, int n, int k) {
-        if (k == 0) {
-            return prefix;
-        }
-
-        for (int i = (prefix == 0 ? 1 : 0); i <= 9; ++i) {
-            int cnt = 1 + countNumbersBeginWith(prefix * 10 + i, n);
-            if (cnt < k) {
-                k -= cnt;
-            }
-            else {
-                return findKthNumberBeginWith(prefix * 10 + i, n, k - 1);
-            }
-        }
-
-        return -1;
-    }
-
     int findKthNumber(int n, int k) {
-        return findKthNumberBeginWith(0, n, k);
+        std::function<int(int)> countNumberBeginWith = [&](int prefix){
+            int cnt = 0;
+            for (int exp = 10; true; exp *= 10) {
+                long lower = 1L * prefix * exp, upper = 1L * prefix * exp + (exp - 1);
+                if (lower > n) {
+                    break;
+                }
+
+                if (lower <= n && n < upper) {
+                    cnt += n - lower + 1;
+                }
+                else {
+                    cnt += exp;
+                }
+            }
+            return cnt;
+        };
+
+        std::function<int(int, int)> findKthNumberBeginWith = [&](int prefix, int k) {
+            if (k == 0) {
+                return prefix;
+            }
+
+            int i = (prefix == 0 ? 1 : 0);
+            for (; i <= 9; ++i) {
+                int cnt = 1 + countNumberBeginWith(prefix * 10 + i);
+                if (cnt < k) {
+                    k -= cnt;
+                }
+                else {
+                    break;
+                }
+            }
+
+            return findKthNumberBeginWith(prefix * 10 + i, k - 1);
+        };
+
+        return findKthNumberBeginWith(0, k);
     }
 };
