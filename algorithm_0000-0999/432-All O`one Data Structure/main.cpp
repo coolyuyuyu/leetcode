86d8c41209d43a0cb343f2786594e6b05cd8ad5f
coolyuyuyu <coolyuyuyu@gmail.com>
2024-09-30 02:43:17 +0800

    432: update solution

--- algorithm_0000-0999/432-All O`one Data Structure/main.cpp
+++ algorithm_0000-0999/432-All O`one Data Structure/main.cpp
@@ -1,66 +1,62 @@
 class AllOne {
 public:
-    AllOne() {
-        m_vals.assign({0});
-        m_val2itr[0] = m_vals.begin();
+    AllOne()
+        : m_vals({0})
+        , m_val2itr({{0, m_vals.begin()}}) {
     }
 
     void inc(string key) {
-        int& val = m_key2val[key];
-        ++val;
+        int val = m_key2val[key];
+        m_key2val[key] = val + 1;
 
-        m_val2keys[val].insert(key);
-        if (val - 1 > 0) {
-            m_val2keys[val - 1].erase(key);
+        m_val2keys[val + 1].insert(key);
+        if (val > 0) {
+            m_val2keys[val].erase(key);
         }
 
-        if (m_val2keys[val].size() == 1) {
-            m_vals.insert(std::next(m_val2itr[val - 1]), val);
-            m_val2itr[val] = std::next(m_val2itr[val - 1]);
+        if (m_val2keys[val + 1].size() == 1) {
+            m_val2itr[val + 1] = m_vals.insert(std::next(m_val2itr[val]), val + 1);
         }
-        if (m_val2keys[val - 1].size() == 0 && val - 1 > 0) {
-            m_vals.erase(m_val2itr[val - 1]);
-            m_val2itr.erase(val - 1);
+        if (val > 0 && m_val2keys[val].empty()) {
+            m_vals.erase(m_val2itr[val]);
+            m_val2itr.erase(val);
         }
     }
 
     void dec(string key) {
+        int val = m_key2val[key];
+        m_key2val[key] = val - 1;
 
-        int& val = m_key2val[key];
-        --val;
-
-        if (val > 0) {
-            m_val2keys[val].insert(key);
+        if (val - 1 > 0) {
+            m_val2keys[val - 1].insert(key);
         }
-        m_val2keys[val + 1].erase(key);
+        m_val2keys[val].erase(key);
 
-
-        if (m_val2keys[val].size() == 1) {
-            m_vals.insert(m_val2itr[val + 1], val);
-            m_val2itr[val] = std::prev(m_val2itr[val + 1]);
+        if (m_val2keys[val - 1].size() == 1) {
+            m_val2itr[val - 1] = m_vals.insert(m_val2itr[val], val - 1);
         }
-        if (m_val2keys[val + 1].size() == 0) {
-            m_vals.erase(m_val2itr[val + 1]);
-            m_val2itr.erase(val + 1);
+        if (m_val2keys[val].empty()) {
+            m_vals.erase(m_val2itr[val]);
+            m_val2itr.erase(val);
         }
     }
 
     string getMaxKey() {
-        if (m_vals.size() > 1) {
-            return *(m_val2keys[*std::prev(m_vals.end())].begin());
-        }
-        else {
+        if (m_vals.size() <= 1) {
             return "";
         }
+
+        int val = *std::prev(m_vals.end());
+        return *(m_val2keys[val].begin());
     }
 
     string getMinKey() {
-        if (m_vals.size() > 1) {
-            return *(m_val2keys[*std::next(m_vals.begin())].begin());
-        }
-        else {
+        if (m_vals.size() <= 1) {
             return "";
         }
+
+        int val = *std::next(m_vals.begin());
+        return *(m_val2keys[val].begin());
     }
 
 private:
