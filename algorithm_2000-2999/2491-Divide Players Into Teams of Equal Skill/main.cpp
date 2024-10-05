32174c438e5321f0e267e4425f9d47dc772d3b1f
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-06 00:05:48 +0800

    2491: update solution

--- algorithm_2000-2999/2491-Divide Players Into Teams of Equal Skill/main.cpp
+++ algorithm_2000-2999/2491-Divide Players Into Teams of Equal Skill/main.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         int teamCnt = skill.size() / 2;
-        map<int, int> cnts;
-        int sum = 0;
-        for (int num : skill) {
-            sum += num;
-            ++cnts[num];
+
+        long long sum = 0;
+        unordered_map<int, int> cnts;
+        for (int s : skill) {
+            sum += s;
+            ++cnts[s];
         }
-        if ((sum % teamCnt) != 0) {
-            return -1;
-        }
+        if (sum % teamCnt != 0) { return -1; }
+        int teamSum = sum / teamCnt;
 
         long long ret = 0;
-        for (int teamSkill = sum / teamCnt; !cnts.empty();) {
-            int skill1 = cnts.begin()->first;
-            if (--cnts.begin()->second == 0) {
-                cnts.erase(cnts.begin());
-            }
-
-            int skill2 = teamSkill - skill1;
-            if (cnts[skill2] == 0) {
+        for (auto itr1 = cnts.begin(); itr1 != cnts.end(); ++itr1) {
+            const auto& [s1, cnt1] = *itr1;
+            if (s1 > teamSum / 2) { continue; }
+            int s2 = teamSum - s1;
+            auto itr2 = cnts.find(s2);
+            if (itr2 == cnts.end() || itr2->second != cnt1) {
                 return -1;
             }
-            if (--cnts[skill2] == 0) {
-                cnts.erase(skill2);
-            }
 
-            ret += (skill1 * skill2);
+            if (s1 * 2 ==  teamSum) {
+                ret += 1LL* s1 * s2 * cnt1 / 2;
+            }
+            else {
+                ret += 1LL* s1 * s2 * cnt1;
+            }
         }
 
         return ret;
