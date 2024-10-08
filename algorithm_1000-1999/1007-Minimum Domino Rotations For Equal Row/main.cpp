23f89cf8f7648a462aab258a2b3bded6ebbbe1d6
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-09 01:10:53 +0800

    1007: update solution

--- algorithm_1000-1999/1007-Minimum Domino Rotations For Equal Row/main.cpp
+++ algorithm_1000-1999/1007-Minimum Domino Rotations For Equal Row/main.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         std::function<int(int, const vector<int>&, const vector<int>&)> f = [](int target, const vector<int>& row1, const vector<int>& row2) {
-            int n = row1.size();
-
             int ret = 0;
-            for (int i = 0; i < n; ++i) {
+            for (int i = 0; i < row1.size(); ++i) {
                 if (row1[i] == target) { continue; }
                 if (row2[i] == target) { ++ret; }
                 else { return INT_MAX; }
