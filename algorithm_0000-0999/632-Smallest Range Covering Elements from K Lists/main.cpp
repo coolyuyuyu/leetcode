50fb87bd2c7bd52a94f5a22e4ebb70bda8b32f00
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-14 00:56:24 +0800

    632: update solution

--- algorithm_0000-0999/632-Smallest Range Covering Elements from K Lists/main.cpp
+++ algorithm_0000-0999/632-Smallest Range Covering Elements from K Lists/main.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        set<tuple<int, int, int>> s; // num, row, col
-        for (int i = 0; i < nums.size(); ++i) {
-            s.emplace(nums[i][0], i, 0);
+        int k = nums.size();
+
+        multiset<tuple<int, int, int>> ms; // num, row, col
+        for (int r = 0; r < k; ++r) {
+            ms.emplace(nums[r][0], r, 0);
         }
 
-        int minDiff = INT_MAX;
-        vector<int> ret;
-        while (true) {
-            auto [num, row, col] = *s.begin();
-            int newDiff = std::get<0>(*s.rbegin()) - num;
-            if (newDiff < minDiff) {
-                minDiff = newDiff;
-                ret = {num, std::get<0>(*s.rbegin())};
+        vector<int> ret = {0, INT_MAX};
+        while (ms.size() == k) {
+            const auto& [minNum, _1, _2] = *ms.begin();
+            const auto& [maxNum, _3, _4] = *ms.rbegin();
+            if (maxNum - minNum < ret[1] - ret[0]) {
+                ret = {minNum, maxNum};
             }
 
-            s.erase(s.begin());
-            if (nums[row].size() <= (col + 1)) {
-                break;
+            auto [_, r, c] = *ms.begin();
+            ms.erase(ms.begin());
+            ++c;
+            if (c < nums[r].size()) {
+                ms.emplace(nums[r][c], r, c);
             }
-
-            s.emplace(nums[row][col + 1], row, col + 1);
         }
 
         return ret;
