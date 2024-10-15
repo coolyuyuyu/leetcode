d37563daff809a72b036d7c0b9c89987ab1730aa
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-15 23:54:13 +0800

    2530: update solution

--- algorithm_2000-2999/2530-Maximal Score After Applying K Operations/main.cpp
+++ algorithm_2000-2999/2530-Maximal Score After Applying K Operations/main.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
         long long ret = 0;
-
-        priority_queue<int> pq(nums.begin(), nums.end());
-        for (int i = 0; i < k; ++i) {
-            int num = pq.top();
-            pq.pop();
+        for (priority_queue<int> maxPQ(nums.begin(), nums.end()); k--;) {
+            int num = maxPQ.top();
+            maxPQ.pop();
 
             ret += num;
 
-            pq.push(ceil(static_cast<double>(num) / 3));
+            maxPQ.emplace(ceil(1.0 * num / 3));
         }
 
         return ret;
