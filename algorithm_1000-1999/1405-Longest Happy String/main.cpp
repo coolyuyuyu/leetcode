ad99ddd1ac70486b0737d232a412fac4dfd30b86
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-21 03:03:04 +0800

    1405: update solution

--- algorithm_1000-1999/1405-Longest Happy String/main.cpp
+++ algorithm_1000-1999/1405-Longest Happy String/main.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    string byPQ(int a, int b, int c) {
+    string longestDiverseString(int a, int b, int c) {
         priority_queue<pair<int, char>> pq;
-        if (a > 0) { pq.emplace(a, 'a'); }
-        if (b > 0) { pq.emplace(b, 'b'); }
-        if (c > 0) { pq.emplace(c, 'c'); }
+        if (a) { pq.emplace(a, 'a'); }
+        if (b) { pq.emplace(b, 'b'); }
+        if (c) { pq.emplace(c, 'c'); }
 
         string ret;
-        int idx = 0;
         while (!pq.empty()) {
-            if (pq.size() == 1) {
-                auto [cnt, c] = pq.top(); pq.top();
-                for (int i = std::min(cnt, 2); 0 < i--;) {
-                    ret.push_back(c);
+            auto [cnt1, ch1] = pq.top(); pq.pop();
+            if (pq.empty()) {
+                for (int n = std::min(cnt1, 2); 0 < n--;) {
+                    ret += ch1;
                 }
                 break;
             }
+            auto [cnt2, ch2] = pq.top(); pq.pop();
 
-            auto [cnt1, c1] = pq.top(); pq.pop();
-            auto [cnt2, c2] = pq.top(); pq.pop();
-
-            int k = (cnt1 == cnt2 ? 1 : 2);
-            for (int i = 0; i < k; ++i) {
-                ret.push_back(c1);
+            for (int n = std::min(1 + cnt1 - cnt2, 2); 0 < n--;) {
+                ret += ch1;
+                --cnt1;
             }
-            ret.push_back(c2);
+            ret += ch2;
+            --cnt2;
 
-            if (cnt1 - k > 0) { pq.emplace(cnt1 - k, c1); }
-            if (cnt2 - 1 > 0) { pq.emplace(cnt2 - 1, c2); }
+            if (cnt1) { pq.emplace(cnt1, ch1); }
+            if (cnt2) { pq.emplace(cnt2, ch2); }
         }
 
         return ret;
-    }
-
-    string longestDiverseString(int a, int b, int c) {
-        return byPQ(a, b, c);
     }
 };
