357e47676bab74b76fcf1a675a299ac29f7102aa
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-22 10:03:05 +0800

    1593: update solution

--- algorithm_1000-1999/1593-Split a String Into the Max Number of Unique Substrings/main.cpp
+++ algorithm_1000-1999/1593-Split a String Into the Max Number of Unique Substrings/main.cpp
@@ -1,43 +1,65 @@
 class Solution {
 public:
-    class TrieNode {
-    public:
-        std::array<TrieNode*, 26> childs;
-        bool end;
-
-        TrieNode() {
-            childs.fill(nullptr);
-            end = false;
-        }
-    };
-
-    int maxUniqueSplit(string s) {
-        int n = s.size();
-
-        TrieNode* root = new TrieNode();
-
-        int ret = 0;
-        std::function<void(int, int)> dfs = [&](int start, int level) {
-            if (start >= n) {
-                ret = std::max(ret, level);
+    int bySet(const string& s) {
+        unordered_set<string> uniques;
+        std::function<int(int)> dfs = [&](int i) {
+            if (i >= s.size()) {
+                return 0;
             }
 
-            TrieNode* node = root;
-            for (int i = start; i < n; ++i) {
+            int ret = 0;
+            for (string part; i < s.size(); ++i) {
+                part += s[i];
+                if (uniques.find(part) == uniques.end()) {
+                    uniques.insert(part);
+                    ret = std::max(ret, 1 + dfs(i + 1));
+                    uniques.erase(part);
+                }
+            }
+
+            return ret;
+        };
+
+        return dfs(0);
+    }
+
+    int byTrie(const string& s) {
+        struct Node {
+            std::array<Node*, 26> childs;
+            bool end;
+            Node() {
+                childs.fill(nullptr);
+                end = false;
+            }
+        };
+
+        Node* root = new Node();
+        std::function<int(int)> dfs = [&](int i) {
+            if (i >= s.size()) {
+                return 0;
+            }
+
+            int ret = 0;
+            for (Node* node = root; i < s.size(); ++i) {
                 if (node->childs[s[i] - 'a'] == nullptr) {
-                    node->childs[s[i] - 'a'] = new TrieNode();
+                    node->childs[s[i] - 'a'] = new Node();
                 }
                 node = node->childs[s[i] - 'a'];
-
                 if (node->end == false) {
                     node->end = true;
-                    dfs(i + 1, level + 1);
+                    ret = std::max(ret, 1 + dfs(i + 1));
                     node->end = false;
                 }
             }
-        };
-        dfs(0, 0);
 
-        return ret;
+            return ret;
+        };
+
+        return dfs(0);
+    }
+
+    int maxUniqueSplit(string s) {
+        //return bySet(s);
+        return byTrie(s);
     }
 };
