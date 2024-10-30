716076190b715a638596c4bc1603392d67488ca9
coolyuyuyu <coolyuyuyu@gmail.com>
2024-10-31 04:33:46 +0800

    221: update solution

--- algorithm_0000-0999/221-Maximal Square/main.cpp
+++ algorithm_0000-0999/221-Maximal Square/main.cpp
@@ -3,26 +3,23 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
-        // dp[i][j]: the length of largest square containing only 1's and its bottom-right corner is (i, j)
-        vector<vector<int>> dp(m, vector<int>(n));
+        // dp[r][c]: the length of maximal squre where bottom-right cornert at (r, c);
+        int dp[m][n];
         int len = 0;
         for (int r = 0; r < m; ++r) {
-            dp[r][0] = matrix[r][0] - '0';
-            len = std::max(len, dp[r][0]);
-        }
-        for (int c = 0; c < n; ++c) {
-            dp[0][c] = matrix[0][c] - '0';
-            len = std::max(len, dp[0][c]);
-        }
-        for (int r = 1; r < m; ++r) {
-            for (int c = 1; c < n; ++c) {
-                if (matrix[r][c] == '1') {
-                    dp[r][c] = std::min({dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]}) + 1;
-                    len = std::max(len, dp[r][c]);
+            for (int c = 0; c < n; ++c) {
+                if (matrix[r][c] == '0') {
+                    dp[r][c] = 0;
+                    continue;
+                }
+
+                if (r == 0 || c == 0) {
+                    dp[r][c] = 1;
                 }
                 else {
-                    dp[r][c] = 0;
+                    dp[r][c] = std::min({dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]}) + 1;
                 }
+                len = std::max(len, dp[r][c]);
             }
         }
 
