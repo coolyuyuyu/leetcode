8dff3a1c2ced7dbb95199d5cd80ba8a9d8494199
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-11 02:36:19 +0800

    3133: update solution

--- algorithm_3000-3999/3133-Minimum Array End/main.cpp
+++ algorithm_3000-3999/3133-Minimum Array End/main.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     long long minEnd(int n, int x) {
         --n;
-        long long ret = x;
 
+        long long ret = x;
         for (int i = 0; n; ++i) {
             if (((ret >> i) & 1) == 0) {
-                ret |= (1LL << i) * (n & 1);
+                ret |= (n & 1) * (1LL << i);
                 n >>= 1;
             }
         }
