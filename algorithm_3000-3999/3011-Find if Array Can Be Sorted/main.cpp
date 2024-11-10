328bf45a4de6c6eda25ffe8f64ec1b0d7a74e28c
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-11 02:36:07 +0800

    3011: update solution

--- algorithm_3000-3999/3011-Find if Array Can Be Sorted/main.cpp
+++ algorithm_3000-3999/3011-Find if Array Can Be Sorted/main.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
-        for (int lft = 0, rht = 0; rht < n;) {
-            int cnt = __builtin_popcount(nums[lft]);
-            while (rht < n && __builtin_popcount(nums[rht]) == cnt) {
-                ++rht;
+        for (int i = 0, n = nums.size(), preMax = INT_MIN; i < n;) {
+            int cnt = __builtin_popcount(nums[i]);
+            int curMin = nums[i], curMax = nums[i];
+            int j;
+            for (j = i + 1; j < n && __builtin_popcount(nums[j]) == cnt; ++j) {
+                curMin = std::min(curMin, nums[j]);
+                curMax = std::max(curMax, nums[j]);
             }
-
-            std::sort(nums.begin() + lft, nums.begin() + rht);
-            if (lft > 0 && nums[lft - 1] > nums[lft]) {
+            if (preMax > curMin) {
                 return false;
             }
+            preMax = curMax;
 
-            lft = rht;
+            i = j;
         }
 
         return true;
