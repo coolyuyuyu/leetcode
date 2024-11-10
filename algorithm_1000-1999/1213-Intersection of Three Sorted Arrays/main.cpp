8460f6fe36994bf6fc2498380acdf18255bfebd0
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-11 02:36:02 +0800

    1213: update solution

--- algorithm_1000-1999/1213-Intersection of Three Sorted Arrays/main.cpp
+++ algorithm_1000-1999/1213-Intersection of Three Sorted Arrays/main.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
-        vector<int> ans;
-        for (size_t i = 0, j = 0, k = 0; i < arr1.size() && j < arr2.size() && k < arr3.size();) {
-            int m = std::max({arr1[i], arr2[j], arr3[k]});
-            if (arr1[i] < m) {
+        vector<int> ret;
+        for (int i = 0, j = 0, k = 0; i < arr1.size() && j < arr2.size() && k < arr3.size();) {
+            int mn = std::max({arr1[i], arr2[j], arr3[k]});
+            if (arr1[i] < mn) {
                 ++i;
             }
-            if (arr2[j] < m) {
+            if (arr2[j] < mn) {
                 ++j;
             }
-            if (arr3[k] < m) {
+            if (arr3[k] < mn) {
                 ++k;
             }
 
-            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
-                ans.push_back(arr1[i]);
+            if (arr1[i] == mn && arr2[j] == mn && arr3[k] == mn) {
+                ret.push_back(arr1[i]);
                 ++i, ++j, ++k;
             }
         }
 
-        return ans;
+        return ret;
     }
 };
