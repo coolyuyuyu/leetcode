83e1c30bb2dc5459aa47360ec85ab03602f55e1a
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-20 03:40:48 +0800

    862: update solution

--- algorithm_0000-0999/862-Shortest Subarray with Sum at Least K/main.cpp
+++ algorithm_0000-0999/862-Shortest Subarray with Sum at Least K/main.cpp
@@ -3,27 +3,26 @@ public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
 
-        long presum[n + 1];
-        presum[0] = 0;
-        for (int i = 1; i <= n; ++i) {
-            presum[i] = presum[i - 1] + nums[i - 1];
+        long presums[n + 1];
+        presums[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            presums[i + 1] = presums[i] + nums[i];
         }
 
-        deque<int> dq;
-
         int ret = INT_MAX;
+
+        deque<int> dq;
         for (int i = 0; i <= n; ++i) {
-            while (!dq.empty() && presum[dq.back()] >= presum[i]) {
+            while (!dq.empty() && presums[dq.back()] >= presums[i]) {
                 dq.pop_back();
             }
-            while (!dq.empty() && presum[i] - presum[dq.front()] >= k) {
+            while (!dq.empty() && presums[i] - presums[dq.front()] >= k) {
                 ret = std::min(ret, i - dq.front());
                 dq.pop_front();
             }
-
             dq.push_back(i);
         }
 
-        return ret == INT_MAX ? -1 : ret;
+        return ret == INT_MAX ? -1: ret;
     }
 };
