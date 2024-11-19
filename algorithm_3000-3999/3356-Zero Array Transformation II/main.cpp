be0c684dee861c0808e9c140f505aaffbb7bd475
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-20 03:39:59 +0800

    3356: initial solution

---
+++ algorithm_3000-3999/3356-Zero Array Transformation II/main.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+
+        std::function<bool(int)> check = [&](int k) {
+            int diffs[n];
+            std::fill(diffs, diffs + n, 0);
+
+            for (int i = 0; i < k; ++i) {
+                int l = queries[i][0], r = queries[i][1], val = queries[i][2];
+                diffs[l] += val;
+                if (r + 1 < n) {
+                    diffs[r + 1] -= val;
+                }
+            }
+
+            for (int i = 0, sum = 0; i < n; ++i) {
+                sum += diffs[i];
+                if (sum < nums[i]) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        int lo = 0, hi = queries.size();
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (check(mid)) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo < queries.size() ? lo : (check(lo) ? lo : -1);
+    }
+};
