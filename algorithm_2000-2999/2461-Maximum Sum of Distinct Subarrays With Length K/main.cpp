eb7321ffa4a1e407541e06b712597678b43d94cd
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-20 03:40:34 +0800

    2461: update solution

--- algorithm_2000-2999/2461-Maximum Sum of Distinct Subarrays With Length K/main.cpp
+++ algorithm_2000-2999/2461-Maximum Sum of Distinct Subarrays With Length K/main.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+
+
+        int uniqueCnt = 0;
         unordered_map<int, int> cnts;
         long sum = 0;
-        for (int i = 0; i < k; ++i) {
-            sum += nums[i];
-            ++cnts[nums[i]];
-        }
 
         long ret = 0;
-        if (cnts.size() == k) {
-            ret = sum;
-        }
-        for (int i = k; i < nums.size(); ++i) {
-            sum -= nums[i - k];
-            if (--cnts[nums[i - k]] == 0) {
-                cnts.erase(nums[i - k]);
-            }
+        for (int i = 0; i < n; ++i) {
             sum += nums[i];
-            ++cnts[nums[i]];
+            if (++cnts[nums[i]] == 1) {
+                ++uniqueCnt;
+            }
 
-            if (cnts.size() == k) {
-                ret = std::max(ret, sum);
+            if (i + 1 >= k) {
+                if (uniqueCnt == k) {
+                    ret = std::max(ret, sum);
+                }
+
+                sum -= nums[i + 1 - k];
+                if (--cnts[nums[i + 1 - k]] == 0) {
+                    --uniqueCnt;
+                }
             }
         }
 
         return ret;
+
     }
 };
