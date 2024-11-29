cb601f68d18a9ef04482597ab8b53f47a19daee9
coolyuyuyu <coolyuyuyu@gmail.com>
2024-11-29 17:28:54 +0800

    2204: update solution

--- algorithm_2000-2999/2204-Distance to a Cycle in Undirected Graph/main.cpp
+++ algorithm_2000-2999/2204-Distance to a Cycle in Undirected Graph/main.cpp
@@ -1,70 +1,49 @@
 class Solution {
 public:
-    vector<int> detectCycles(int n, vector<int> graph[]) {
-        vector<int> degrees(n, 0);
-        for (int u = 0; u < n; ++u) {
-            for (int v : graph[u]) {
-                ++degrees[v];
-            }
-        }
-
-        queue<int> q;
-        vector<bool> visited(n, false);
-        for (int i = 0; i < n; ++i) {
-            if (degrees[i] == 1) {
-                visited[i] = true;
-                q.push(i);
-            }
-        }
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for (int v : graph[u]) {
-                if (visited[v]) { continue; }
-                if (--degrees[v] == 1) {
-                    visited[v] = true;
-                    q.push(v);
-                }
-            }
-        }
-
-        vector<int> cycles;
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                cycles.push_back(i);
-            }
-        }
-
-        return cycles;
-    }
-
     vector<int> distanceToCycle(int n, vector<vector<int>>& edges) {
         vector<int> graph[n];
+        vector<int> degrees(n, 0);
         for (const auto& edge : edges) {
             int node1 = edge[0], node2 = edge[1];
             graph[node1].push_back(node2);
             graph[node2].push_back(node1);
+            ++degrees[node1], ++degrees[node2];
         }
-        vector<int> cycles = detectCycles(n, graph);
 
-        vector<int> ret(n, -1);
+        vector<int> ret(n, 0);
 
         queue<int> q;
-        for (int cycle : cycles) {
-            ret[cycle] = 0;
-            q.push(cycle);
+        for (int u = 0; u < n; ++u) {
+            if (degrees[u] == 1) {
+                q.push(u);
+            }
         }
-        for (int dist = 0; !q.empty(); ++dist) {
-            for (int i = q.size(); 0 < i--;) {
-                int u = q.front();
-                q.pop();
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
 
-                ret[u] = dist;
+            ret[cur] = INT_MAX;
 
-                for (int v : graph[u]) {
-                    if (ret[v] != -1) { continue; }
-                    q.push(v);
+            for (int nxt : graph[cur]) {
+                if (--degrees[nxt] == 1) {
+                    q.push(nxt);
                 }
+            }
+        }
+
+        for (int u = 0; u < n; ++u) {
+            if (ret[u] == 0) {
+                q.push(u);
+            }
+        }
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+
+            for (int nxt : graph[cur]) {
+                if (ret[nxt] < INT_MAX) { continue; }
+                ret[nxt] = ret[cur] + 1;
+                q.push(nxt);
             }
         }
 
