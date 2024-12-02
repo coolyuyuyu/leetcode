93d5f801cb81f4322c21dcb90644b6ff369e9cde
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-03 00:18:05 +0800

    1346: update solution

--- algorithm_1000-1999/1346-Check If N and Its Double Exist/main.cpp
+++ algorithm_1000-1999/1346-Check If N and Its Double Exist/main.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        set<int> s;
+        unordered_map<int, int> cnts;
         for (int num : arr) {
-            if (num == 0) {
-                if (s.find(0) != s.end()) {
-                    return true;
-                }
-            }
-            else {
-                if (s.find(num * 2) != s.end()) {
-                    return true;
-                }
-                if (num % 2 == 0 && s.find(num / 2) != s.end()) {
-                    return true;
-                }
+            ++cnts[num];
+        }
+
+        for (int num : arr) {
+            --cnts[num];
+
+            if (cnts[num * 2] > 0) {
+                return true;
             }
 
-            s.insert(num);
+            ++cnts[num];
         }
 
         return false;
