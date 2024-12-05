2d5aa6ca357551bfd76cae98fb47526cc20fdc91
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-06 00:01:24 +0800

    277: update solution

--- algorithm_0000-0999/277-Find the Celebrity/main.cpp
+++ algorithm_0000-0999/277-Find the Celebrity/main.cpp
@@ -1,26 +1,23 @@
-// Forward declaration of the knows API.
-bool knows(int a, int b);
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
 
 class Solution {
 public:
     int findCelebrity(int n) {
-        int celebrity = 0;
+        int ret = 0;
         for (int i = 1; i < n; ++i) {
-            if (knows(celebrity, i)) {
-                celebrity = i;
+            if (knows(ret, i)) {
+                ret = i;
             }
         }
 
         for (int i = 0; i < n; ++i) {
-            if (i == celebrity) {
-                continue;
-            }
-
-            if (knows(celebrity, i) || !knows(i, celebrity)) {
+            if (i == ret) { continue; }
+            if (knows(ret, i) || !knows(i, ret)) {
                 return -1;
             }
         }
 
-        return celebrity;
+        return ret;
     }
 };
