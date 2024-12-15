54841cd41bd5a65e8f270d67aeb2a24b459bb2ef
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-15 23:39:44 +0800

    1792: update solution

--- algorithm_1000-1999/1792-Maximum Average Pass Ratio/main.cpp
+++ algorithm_1000-1999/1792-Maximum Average Pass Ratio/main.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
-    typedef pair<int, int> Class;
-
-    static double passRate(int pass, int total) {
-        return (static_cast<double>(pass) / total);
-    };
-
-    static double passRate(const Class& c) {
-        return passRate(c.first, c.second);
-    };
-
-    static double benefit(const Class& c) {
-        return (passRate(c.first + 1, c.second + 1) - passRate(c));
-    };
-
-    double maxAverageRatio_Heap(vector<vector<int>>& classes, int extraStudents) {
-        auto comp = [&](const Class& c1, const Class& c2) {
-            return (benefit(c1) < benefit(c2));
-
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        std::function<bool(const pair<int, int>&, const pair<int, int>&)> cmp = [](const pair<int, int>& p1, const pair<int, int>& p2) {
+            const auto& [pass1, total1] = p1;
+            const auto& [pass2, total2] = p2;
+            return std::less<>()((1.0 * pass1 + 1) / (total1 + 1) - (1.0 * pass1 / total1), (1.0 * pass2 + 1) / (total2 + 1) - 1.0 * pass2 / total2);
         };
-        priority_queue<Class, vector<Class>, decltype(comp)> pq(comp); // max_heap
-        for (const vector<int>& c : classes) {
-            pq.emplace(c[0], c[1]);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
+        for (const auto& c : classes) {
+            int pass = c[0], total = c[1];
+            pq.emplace(pass, total);
         }
 
-        for (size_t i = 0; i < extraStudents; ++i) {
-            Class c = pq.top();
+        for (int i = 0; i < extraStudents; ++i) {
+            auto [pass, total] = pq.top();
             pq.pop();
-            pq.emplace(c.first + 1, c.second + 1);
+            pq.emplace(pass + 1, total + 1);
         }
-
 
         double sum = 0;
-        for (; !pq.empty(); pq.pop()) {
-            sum += passRate(pq.top());
+        while (!pq.empty()) {
+            auto [pass, total] = pq.top();
+            pq.pop();
+            sum += (1.0 * pass / total);
         }
-        return (sum / classes.size());
-    }
 
-    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        return maxAverageRatio_Heap(classes, extraStudents);
+        return sum / classes.size();
     }
 };
