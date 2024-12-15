72c0017cf208faf58cdb98fb69078bcb256c6657
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-15 23:40:09 +0800

    2762: update solution

--- algorithm_2000-2999/2762-Continuous Subarrays/main.cpp
+++ algorithm_2000-2999/2762-Continuous Subarrays/main.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    long long continuousSubarrays(vector<int>& nums) {
-        int n = nums.size();
-
+    long long f1(vector<int>& nums) {
         deque<int> dqMax, dqMin;
 
-        std::function<bool(int)> check = [&](int rht) {
-            if (dqMax.empty()) { return true; }
-            return std::max(nums[dqMax.front()], nums[rht]) - std::min(nums[dqMin.front()], nums[rht]) <= 2;
+        std::function<bool(int)> check = [&](int i) {
+            int maxVal = std::max((dqMax.empty() ? INT_MIN : nums[dqMax.front()]), nums[i]);
+            int minVal = std::min((dqMin.empty() ? INT_MAX : nums[dqMin.front()]), nums[i]);
+            return maxVal - minVal <= 2;
         };
 
         long long ret = 0;
-        for (int lft = 0, rht = 0; lft < n; ++lft) {
+        for (int lft = 0, rht = 0, n = nums.size(); lft < n; ++lft) {
             for (; rht < n && check(rht); ++rht) {
-                while (!dqMax.empty() && nums[dqMax.back()] <= nums[rht]) {
+                while (!dqMax.empty() && nums[dqMax.back()] < nums[rht]) {
                     dqMax.pop_back();
                 }
                 dqMax.push_back(rht);
-
-                while (!dqMin.empty() && nums[dqMin.back()] >= nums[rht]) {
+                while (!dqMin.empty() && nums[dqMin.back()] > nums[rht]) {
                     dqMin.pop_back();
                 }
                 dqMin.push_back(rht);
@@ -26,14 +24,48 @@ public:
 
             ret += (rht - lft);
 
-            if (!dqMax.empty() && dqMax.front() == lft) {
+            if (dqMax.front() == lft) {
                 dqMax.pop_front();
             }
-            if (!dqMin.empty() && dqMin.front() == lft) {
+            if (dqMin.front() == lft) {
                 dqMin.pop_front();
             }
         }
 
         return ret;
+    }
+
+    long long f2(vector<int>& nums) {
+        deque<int> dqMax, dqMin;
+
+        long long ret = 0;
+        for (int lft = 0, rht = 0, n = nums.size(); rht < n; ++rht) {
+            while (!dqMax.empty() && nums[dqMax.back()] < nums[rht]) {
+                dqMax.pop_back();
+            }
+            dqMax.push_back(rht);
+            while (!dqMin.empty() && nums[dqMin.back()] > nums[rht]) {
+                dqMin.pop_back();
+            }
+            dqMin.push_back(rht);
+
+            for (; nums[dqMax.front()] - nums[dqMin.front()] > 2; ++lft) {
+                if (dqMax.front() == lft) {
+                    dqMax.pop_front();
+                }
+                if (dqMin.front() == lft) {
+                    dqMin.pop_front();
+                }
+            }
+
+            ret += (rht - lft + 1);
+        }
+
+        return ret;
+    }
+
+    long long continuousSubarrays(vector<int>& nums) {
+        return f1(nums);
+        //return f2(nums);
     }
 };
