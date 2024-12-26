7822ec7bffb052cd052e1f034282859b16122670
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-26 23:55:51 +0800

    494: update solution

--- algorithm_0000-0999/494-Target Sum/main.cpp
+++ algorithm_0000-0999/494-Target Sum/main.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
+        int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        if (target < -sum || target > sum) {
+            return 0;
+        }
 
         nums.insert(nums.begin(), 0);
 
-        int offset = 1000;
+        int offset = sum;
 
         // dp[i][s]: the number of different expressions that you can build from nums[1:i], which evaluates to s
-        int dp[n + 1][2001];
-        for (int s = -1000; s <= 1000; ++s) {
+        int dp[n + 1][sum * 2 + 1];
+        for (int s = -sum; s <= sum; ++s) {
             dp[0][s + offset] = 0;
         }
         dp[0][0 + offset] = 1;
         for (int i = 1; i <= n; ++i) {
-            for (int s = -1000; s <= 1000; ++s) {
+            for (int s = -sum; s <= sum; ++s) {
                 dp[i][s + offset] = 0;
-                if (-1000 <= (s - nums[i])) {
+                if (s - nums[i] >= -sum) {
                     dp[i][s + offset] += dp[i - 1][s - nums[i] + offset];
                 }
-                if ((s + nums[i]) <= 1000) {
+                if (s + nums[i] <= sum) {
                     dp[i][s + offset] += dp[i - 1][s + nums[i] + offset];
                 }
             }
