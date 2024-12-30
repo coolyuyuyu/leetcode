96d7407f2b3da079e290704786da5bfce9b5b88a
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-31 00:48:42 +0800

    2466: update solution

--- algorithm_2000-2999/2466-Count Ways To Build Good Strings/main.cpp
+++ algorithm_2000-2999/2466-Count Ways To Build Good Strings/main.cpp
@@ -3,15 +3,16 @@ public:
     int M = 1e9 + 7;
 
     int countGoodStrings(int low, int high, int zero, int one) {
-        // dp[i]: number of different good strings and its length = i
+        // dp[i]: number of different length i good strings
         int dp[high + 1];
         dp[0] = 1;
         for (int i = 1; i <= high; ++i) {
             dp[i] = 0;
-            if (zero <= i) {
-                dp[i] = dp[i - zero];
+            if (i >= zero) {
+                dp[i] += dp[i - zero];
+                dp[i] %= M;
             }
-            if (one <= i) {
+            if (i >= one) {
                 dp[i] += dp[i - one];
                 dp[i] %= M;
             }
