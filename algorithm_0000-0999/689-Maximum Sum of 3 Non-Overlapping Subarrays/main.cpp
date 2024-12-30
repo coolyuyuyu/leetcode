b9648292a95e237e49ccfabf328184a924deff3a
coolyuyuyu <coolyuyuyu@gmail.com>
2024-12-31 00:48:25 +0800

    689: update solution

--- algorithm_0000-0999/689-Maximum Sum of 3 Non-Overlapping Subarrays/main.cpp
+++ algorithm_0000-0999/689-Maximum Sum of 3 Non-Overlapping Subarrays/main.cpp
@@ -3,39 +3,38 @@ public:
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<int>& presum = nums;
-        std::partial_sum(presum.begin(), presum.end(), presum.begin());
+        vector<int>& presums = nums;
+        std::partial_sum(nums.begin(), nums.end(), presums.begin());
+        std::function<int(int, int)> sum = [&](int l, int r) {
+            return presums[r] - (l > 0 ? presums[l - 1] : 0);
+        };
 
-        // dpL[i]: (S, I) such that the max sum S of k-element-subarray with lft end index I in nums[0:i]
-        vector<array<int, 2>> dpL(n);
-        for (int maxSum = INT_MIN, maxIdx, i = k - 1; i < n; ++i) {
-            int sum = presum[i] - (k <= i ? presum[i - k] : 0);
-            if (maxSum < sum) {
-                maxSum = sum;
-                maxIdx = i - k + 1;
-            } 
-
-            dpL[i] = {maxSum, maxIdx};
+        // dpL[i]=(sum, idx): the maximal sum of legnth-k-subarrays from nums[0:i] and it's lft end index is idx.
+        pair<int, int> dpL[n];
+        for (int l = 0, r = k - 1, mx = INT_MIN, idx; r < n; ++l, ++r) {
+            int s = sum(l, r);
+            if (s > mx) {
+                mx = s, idx = l;
+            }
+            dpL[r] = {mx, idx};
         }
 
-        // dpR[i]: (S, I) such that the max sum S of k-element-subarray with lft end index I in nums[i:n-1]
-        vector<array<int, 2>> dpR(n);
-        for (int maxSum = INT_MIN, maxIdx, i = n - k; 0 <= i; --i) {
-            int sum = presum[i + k - 1] - (0 < i ? presum[i - 1] : 0);
-            if (maxSum <= sum) {
-                maxSum = sum;
-                maxIdx = i;
-            } 
-
-            dpR[i] = {maxSum, maxIdx};
+        // dpR[i]=(sum, idx): the maximal sum of legnth-k-subarrays from nums[i:] and it's lft end index is idx.
+        pair<int, int> dpR[n];
+        for (int l = n - k, r = n - 1, mx = INT_MIN, idx; l >= 0; --l, --r) {
+            int s = sum(l, r);
+            if (s >= mx) {
+                mx = s, idx = l;
+            }
+            dpR[l] = {mx, idx};
         }
 
         vector<int> ret;
-        for (int maxSum = INT_MIN, i = k; i < (n - 2 * k + 1); ++i) {
-            int sum = dpL[i - 1][0] + (presum[i + k - 1] - presum[i - 1]) + dpR[i + k][0];
-            if (maxSum < sum) {
-                maxSum = sum;
-                ret = {dpL[i - 1][1], i, dpR[i + k][1]};
+        for (int l = k, r = k + k - 1, mx = INT_MIN; r + k < n; ++l, ++r) {
+            int s = dpL[l - 1].first + sum(l, r) + dpR[r + 1].first;
+            if (s > mx) {
+                mx = s;
+                ret = {dpL[l - 1].second, l, dpR[r + 1].second};
             }
         }
 
