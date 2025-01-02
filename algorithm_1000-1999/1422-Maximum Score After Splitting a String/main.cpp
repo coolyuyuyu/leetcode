ed56131fc6b6c1c5048cadf76c48c01ce202fa9d
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-03 01:31:52 +0800

    1422: update solution

--- algorithm_1000-1999/1422-Maximum Score After Splitting a String/main.cpp
+++ algorithm_1000-1999/1422-Maximum Score After Splitting a String/main.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.size();
-
-        int cnt1 = 0;
-        for (char c : s) {
-            cnt1 += (c == '1' ? 1 : 0);
-        }
-
         int ret = 0;
-        for (int i = 0, lft0 = 0, lft1 = 0; i + 1 < n; ++i) {
-            ++(s[i] == '0' ? lft0 : lft1);
-            ret = std::max(ret, lft0 + cnt1 - lft1);
+        for (int i = 0, cntLft0 = 0, cntRht1 = std::count(s.begin(), s.end(), '1'); i + 1 < s.size(); ++i) {
+            cntLft0 += (s[i] == '0' ? 1 : 0);
+            cntRht1 -= (s[i] == '1' ? 1 : 0);
+            ret = std::max(ret, cntLft0 + cntRht1);
         }
 
         return ret;
