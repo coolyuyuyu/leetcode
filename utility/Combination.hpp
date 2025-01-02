639fb730b6ae4c727b44c3b4520b183811ee69e3
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-03 01:31:26 +0800

    Update Combination.hpp

--- utility/Combination.hpp
+++ utility/Combination.hpp
@@ -21,3 +21,32 @@ int comb(int n, int r) {
 
     return ret;
 };
+
+int M = 1e9 + 7;
+vector<int> factorial(n);
+factorial[0] = 1;
+for (int i = 1; i < n; ++i) {
+    factorial[i] = 1L * factorial[i - 1] * i % M;
+}
+
+std::function<int(int, int)> quickPow = [&](int base, int exp) {
+    int ret = 1;
+    for (; exp; exp >>= 1) {
+        if (exp & 1) {
+            ret = 1L * ret * base % M;
+        }
+        base = 1L * base * base % M;
+    }
+
+    return ret;
+};
+
+std::function<int(int, int)> comb = [&](int m, int n) {
+    if (m < n) { return 0; }
+    int a = factorial[m];
+    int b = 1L * factorial[n] * factorial[m - n] % M;
+    int invB = quickPow(b, M - 2);
+
+    return 1L * a * invB % M;
+};
+
