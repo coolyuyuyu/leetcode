57ff1ed566b6581a0f92d8325fb5edf4d3876410
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-06 02:48:31 +0800

    1930: update solution

--- algorithm_1000-1999/1930-Unique Length-3 Palindromic Subsequences/main.cpp
+++ algorithm_1000-1999/1930-Unique Length-3 Palindromic Subsequences/main.cpp
@@ -3,30 +3,27 @@ public:
     int countPalindromicSubsequence(string s) {
         int n = s.size();
 
-        size_t lft[26];
-        std::fill(lft, lft + 26, string::npos);
+        int lftMost[26];
+        std::fill(lftMost, lftMost + 26, -1);
         for (int i = 0; i < n; ++i) {
-            if (lft[s[i] - 'a'] == string::npos) {
-                lft[s[i] - 'a'] = i;
-            }
+            if (lftMost[s[i] - 'a'] != string::npos) { continue; }
+            lftMost[s[i] - 'a'] = i;
         }
 
-        size_t rht[26];
-        std::fill(rht, rht + 26, string::npos);
+        int rhtMost[26];
+        std::fill(rhtMost, rhtMost + 26, -1);
         for (int i = n; 0 < i--;) {
-            if (rht[s[i] - 'a'] == string::npos) {
-                rht[s[i] - 'a'] = i;
-            }
+            if (rhtMost[s[i] - 'a'] != string::npos) { continue; }
+            rhtMost[s[i] - 'a'] = i;
         }
 
         int ret = 0;
         for (char c = 'a'; c <= 'z'; ++c) {
-            if (lft[c - 'a'] == string::npos) { continue; }
-
             bitset<26> flags;
-            for (int i = lft[c - 'a'] + 1; i < rht[c - 'a']; ++i) {
-                flags.set(s[i] - 'a');
+            for (int i = lftMost[c - 'a'] + 1; i < rhtMost[c - 'a']; ++i) {
+                flags[s[i] - 'a'] = true;
             }
+
             ret += flags.count();
         }
 
