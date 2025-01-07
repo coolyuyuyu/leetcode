4c9f3a83b335be9cc938120eac151367b23c2836
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-08 00:27:39 +0800

    1408: update solution

--- algorithm_1000-1999/1408-String Matching in an Array/main.cpp
+++ algorithm_1000-1999/1408-String Matching in an Array/main.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        std::sort(words.begin(), words.end(), [](const string& w1, const string& w2) { return w1.size() < w2.size(); });
+        std::sort(words.begin(), words.end(), [](const string& s1, const string& s2) { return s1.size() < s2.size(); });
 
-        vector<string> ans;
-        for (size_t i = 0; i < words.size(); ++i) {
-            for (size_t j = i + 1; j < words.size(); ++j) {
+        vector<string> ret;
+        for (int i = 0; i < words.size(); ++i) {
+            for (int j = i + 1; j < words.size(); ++j) {
                 if (words[j].find(words[i]) != string::npos) {
-                    ans.push_back(words[i]);
+                    ret.push_back(words[i]);
                     break;
                 }
             }
         }
-        return ans;
+
+        return ret;
     }
 };
