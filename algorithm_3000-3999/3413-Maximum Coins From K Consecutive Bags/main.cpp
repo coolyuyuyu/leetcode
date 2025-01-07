a5653db2a1c04d171258d6fa69216062269340f4
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-08 00:49:02 +0800

    3413: update solution

--- algorithm_3000-3999/3413-Maximum Coins From K Consecutive Bags/main.cpp
+++ algorithm_3000-3999/3413-Maximum Coins From K Consecutive Bags/main.cpp
@@ -6,47 +6,37 @@ public:
         std::sort(coins.begin(), coins.end());
 
         long long ret = 0;
-
-        long long presums[n];
-        for (int i = 0; i < n; ++i) {
-            presums[i] = (i > 0 ? presums[i - 1] : 0) + 1LL * coins[i][2] * (coins[i][1] - coins[i][0] + 1);
-        }
-        for (int i = 0, j = 0; i < n; ++i) {
-            while (j < n && coins[i][0] + k - 1 >= coins[j][1]) {
-                ++j;
+        for (long i = 0, j = 0, sum = 0; i < n; ++i) {
+            int end = coins[i][0] + k - 1;
+            for (; j < n && end >= coins[j][1]; ++j) {
+                sum += 1LL * coins[j][2] * (coins[j][1] - coins[j][0] + 1);
             }
 
-            long long cntCoins = 0;
-            if (j > i) {
-                cntCoins += presums[j - 1] - (i > 0 ? presums[i - 1] : 0);
-            }
-            if (j < n) {
-                cntCoins += std::max(0LL, 1LL * coins[j][2] * (coins[i][0] + k - 1 - coins[j][0] + 1));
+            long long extra = 0;
+            if (j < n && end >= coins[j][0]) {
+                extra += 1LL * coins[j][2] * (end - coins[j][0] + 1);
             }
 
-            ret = std::max(ret, cntCoins);
+            ret = std::max(ret, sum + extra);
+
+            sum -= 1LL * coins[i][2] * (coins[i][1] - coins[i][0] + 1);
         }
 
-        long long postsums[n];
-        for (int i = n - 1; i >= 0; --i) {
-            postsums[i] = ((i + 1 < n) ? postsums[i + 1] : 0) + 1LL * coins[i][2] * (coins[i][1] - coins[i][0] + 1);
-        }
-        for (int i = n - 1, j = n - 1; i >= 0; --i) {
-            while (j >= 0 && coins[i][1] - k + 1 <= coins[j][0]) {
-                --j;
+        for (long long i = n - 1, j = n - 1, sum = 0; i >= 0; --i) {
+            int end = coins[i][1] - k + 1;
+            for (; j >= 0 && end <= coins[j][0]; --j) {
+                sum += 1LL * coins[j][2] * (coins[j][1] - coins[j][0] + 1);
             }
 
-            long long cntCoins = 0;
-            if (j < i) {
-                cntCoins += postsums[j + 1] - ((i + 1 < n) ? postsums[i + 1] : 0);
-            }
-            if (j >= 0) {
-                cntCoins += std::max(0LL, 1LL * coins[j][2] * (coins[j][1] - (coins[i][1] - k + 1) + 1));
+            long long extra = 0;
+            if (j >= 0 && end <= coins[j][1]) {
+                extra += 1LL * coins[j][2] * (coins[j][1] - end + 1);
             }
 
-            ret = std::max(ret, cntCoins);
+            ret = std::max(ret, sum + extra);
+
+            sum -= 1LL * coins[i][2] * (coins[i][1] - coins[i][0] + 1);
         }
-        
 
         return ret;
     }
