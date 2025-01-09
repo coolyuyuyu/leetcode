821499383b2a380d454d9cb3454f276548b1088d
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-10 01:25:07 +0800

    2185: update solution

--- algorithm_2000-2999/2185-Counting Words With a Given Prefix/main.cpp
+++ algorithm_2000-2999/2185-Counting Words With a Given Prefix/main.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int cnt = 0;
+        int ret = 0;
         for (const string& word : words) {
-            if (word.size() < pref.size()) {
-                continue;
-            }
             if (word.compare(0, pref.size(), pref) == 0) {
-                ++cnt;
+                ++ret;
             }
         }
 
-        return cnt;
+        return ret;
     }
 };
