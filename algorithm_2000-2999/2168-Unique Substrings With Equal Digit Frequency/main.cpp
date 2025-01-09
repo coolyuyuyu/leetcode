223355abdf6f35e8864e12ac692f5dd168e4a291
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-10 01:25:02 +0800

    2168: update solution

--- algorithm_2000-2999/2168-Unique Substrings With Equal Digit Frequency/main.cpp
+++ algorithm_2000-2999/2168-Unique Substrings With Equal Digit Frequency/main.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int M = 1e9 + 7;
-
     int equalDigitFrequency(string s) {
         int n = s.size();
 
-        unordered_set<int> hashs;
-        int cnts[10];
-        for (int i = 0; i < n; ++i) {
-            std::fill(cnts, cnts + 10, 0);
-            long long hash = 0;
+        std::array<int, 10> cnts;
+        unordered_set<unsigned long long> hashs;
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            cnts.fill(0);
+            unsigned long long hash = 0;
             for (int j = i; j < n; ++j) {
-                hash = (hash * 11 + s[j] - '0' + 1) % M;
-
                 ++cnts[s[j] - '0'];
+                hash = hash * 11 + (s[j] - '0' + 1);
 
-                bool sameFreq = true;
-                for (int k = 0, cnt = -1; k < 10 && sameFreq; ++k) {
-                    if (cnts[k] > 0) {
-                        if (cnt == -1) {
-                            cnt = cnts[k];
-                        }
-                        else if (cnt != cnts[k]) {
-                            sameFreq = false;
-                        }
-                    }
+                bool same = true;
+                for (int k = 0, cnt = -1; k < cnts.size() && same; ++k) {
+                    if (cnts[k] == 0) { continue; }
+                    if (cnt == -1) { cnt = cnts[k]; }
+                    else if (cnt != cnts[k]) { same = false; }
                 }
-                if (sameFreq) {
+
+                if (same) {
                     hashs.insert(hash);
                 }
             }
