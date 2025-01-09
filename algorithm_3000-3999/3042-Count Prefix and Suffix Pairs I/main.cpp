da7d338e2583f1c176e8fc9f02d43f90cef01171
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-10 01:25:13 +0800

    3042: update solution

--- algorithm_3000-3999/3042-Count Prefix and Suffix Pairs I/main.cpp
+++ algorithm_3000-3999/3042-Count Prefix and Suffix Pairs I/main.cpp
@@ -4,12 +4,12 @@ public:
         int n = words.size();
 
         int ret = 0;
-        for (int j = 0; j < n; ++j) {
-            const string& wordJ = words[j];
-            for (int i = 0; i < j; ++i) {
-                const string& wordI = words[i];
-                if (wordJ.compare(0, wordI.size(), wordI) == 0 &&
-                    wordJ.compare(wordJ.size() - wordI.size(), wordI.size(), wordI) == 0) {
+        for (int i = 0; i < n; ++i) {
+            const auto& str1 = words[i];
+            for (int j = i + 1; j < n; ++j) {
+                const auto& str2 = words[j];
+                if (str2.compare(0, str1.size(), str1) == 0 &&
+                    str2.compare(str2.size() - str1.size(), str1.size(), str1) == 0) {
                     ++ret;
                 }
             }
