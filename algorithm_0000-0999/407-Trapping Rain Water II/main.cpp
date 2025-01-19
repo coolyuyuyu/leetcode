630c00f74fa96bb16d2cf9eba5bb2adf879a89f9
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-20 01:09:23 +0800

    407: update solution

--- algorithm_0000-0999/407-Trapping Rain Water II/main.cpp
+++ algorithm_0000-0999/407-Trapping Rain Water II/main.cpp
@@ -6,37 +6,31 @@ public:
         int m = heightMap.size(), n = heightMap.empty() ? 0 : heightMap[0].size();
 
         bool visited[m][n];
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                visited[r][c] = false;
-            }
-        }
-
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, std::greater<tuple<int, int, int>>> pq;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,std::greater<tuple<int, int, int>>> pq;
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
                 if (r == 0 || r == m - 1 || c == 0 || c == n - 1) {
                     visited[r][c] = true;
                     pq.emplace(heightMap[r][c], r, c);
                 }
+                else {
+                    visited[r][c] = false;
+                }
             }
         }
 
         int ret = 0;
-
-        int curH = INT_MIN;
-        while (!pq.empty()) {
+        for (int curH = INT_MIN; !pq.empty();) {
             auto [h, r, c] = pq.top();
             pq.pop();
 
-            curH = std::max(curH, heightMap[r][c]);
-            ret += curH - heightMap[r][c];
+            if (h < curH) { ret += curH - h; }
+            else { curH = h; }
 
             for (const auto& [dr, dc] : dirs) {
                 int x = r + dr, y = c + dc;
                 if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
                 if (visited[x][y]) { continue; }
-
                 visited[x][y] = true;
                 pq.emplace(heightMap[x][y], x, y);
             }
