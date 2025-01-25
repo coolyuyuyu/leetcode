e5fa21ce6612fe25dd3dfdead11683793b7c4000
coolyuyuyu <coolyuyuyu@gmail.com>
2025-01-26 01:45:04 +0800

    2948: update solution

--- algorithm_2000-2999/2948-Make Lexicographically Smallest Array by Swapping Elements/main.cpp
+++ algorithm_2000-2999/2948-Make Lexicographically Smallest Array by Swapping Elements/main.cpp
@@ -5,11 +5,11 @@ public:
 
         int indexes[n];
         std::iota(indexes, indexes + n, 0);
-        std::sort(indexes, indexes + n, [&](const int i1, const int i2){ return nums[i1] < nums[i2]; });
+        std::sort(indexes, indexes + n, [&](int idx1, int idx2){ return nums[idx1] < nums[idx2]; });
 
         vector<int> ret(n);
-        for (int lft = 0, rht = 0; rht < n; lft = rht) {
-            while (lft == rht || (rht < n  && nums[indexes[rht]] - nums[indexes[rht - 1]] <= limit)) {
+        for (int lft = 0, rht = 0; lft < n; lft = rht) {
+            while (lft == rht || (rht < n && nums[indexes[rht]] - nums[indexes[rht - 1]] <= limit)) {
                 ++rht;
             }
 
