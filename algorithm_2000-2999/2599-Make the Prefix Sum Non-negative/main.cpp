7cd0104868aa72ce882378c293d1e6bf23a19d96
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-11 01:53:31 +0800

    2599: update solution

--- algorithm_2000-2999/2599-Make the Prefix Sum Non-negative/main.cpp
+++ algorithm_2000-2999/2599-Make the Prefix Sum Non-negative/main.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int makePrefSumNonNegative(vector<int>& nums) {
         int ret = 0;
-        
-        priority_queue<int, vector<int>, greater<int>> pq;
-        long long presum = 0;
-        for (int num : nums) {
-            if (num < 0) {
-                pq.push(num);
+        priority_queue<int, vector<int>, std::greater<int>> pq;
+        for (long long sum = 0, i = 0; i < nums.size(); ++i) {
+            if (nums[i] < 0) {
+                pq.push(nums[i]);
             }
-            
-            presum += num;
-            if (presum < 0) {
-                presum -= pq.top();
+            sum += nums[i];
+            if (sum < 0) {
+                sum -= pq.top();
                 pq.pop();
-
                 ++ret;
             }
         }
