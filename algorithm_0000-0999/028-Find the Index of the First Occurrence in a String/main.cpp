7ec184ef107b777f852c69500b30d570619fb477
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-12 01:59:29 +0800

    028: update solution

--- algorithm_0000-0999/028-Find the Index of the First Occurrence in a String/main.cpp
+++ algorithm_0000-0999/028-Find the Index of the First Occurrence in a String/main.cpp
@@ -3,20 +3,19 @@ public:
     int strStr(string haystack, string needle) {
         int m = haystack.size(), n = needle.size();
 
-        // lps[i] the maximum value k such that needle[0:k-1] == needle[i-k+1:i]
+        // lps[i]: the maximum value k such that needle[0:k-1] == needle[i-k+1:i]
         int lps[n];
         lps[0] = 0;
         for (int i = 1; i < n; ++i) {
             int j = lps[i - 1];
-            while (0 < j && needle[j] != needle[i]) {
+            while (j > 0 && needle[j] != needle[i]) {
                 j = lps[j - 1];
             }
             lps[i] = j + (needle[j] == needle[i] ? 1 : 0);
         }
 
-        int j = 0;
-        for (int i = 0; i < m; ++i) {
-            while (0 < j && haystack[i] != needle[j]) {
+        for (int i = 0, j = 0; i < m; ++i) {
+            while ((j > 0 && haystack[i] != needle[j]) || j == n) {
                 j = lps[j - 1];
             }
             j = j + (haystack[i] == needle[j] ? 1 : 0);
