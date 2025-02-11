1b94c8ee7d39dd5e6718897a3dfd101cab60a0df
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-12 01:59:38 +0800

    1910: update solution

--- algorithm_1000-1999/1910-Remove All Occurrences of a Substring/main.cpp
+++ algorithm_1000-1999/1910-Remove All Occurrences of a Substring/main.cpp
@@ -3,34 +3,31 @@ public:
     string removeOccurrences(string s, string part) {
         int m = s.size(), n = part.size();
 
-        // lps[i] the maximum value k such that part[0:k-1] == part[i-k+1:i]
+        // lps[i]: the maximum value k such that part[0:k-1] == part[i-k+1:i]
         int lps[n];
         lps[0] = 0;
         for (int i = 1; i < n; ++i) {
             int j = lps[i - 1];
-            while (0 < j && part[j] != part[i]) {
+            while (j > 0 && part[j] != part[i]) {
                 j = lps[j - 1];
             }
             lps[i] = j + (part[j] == part[i] ? 1 : 0);
         }
 
         string ret;
-        vector<int> dp; // dp[i]: the maximum value k such that s[i-k+1:i]==part[0:k-1]
-        for (char c : s) {
-            ret.push_back(c);
-
-            int j = dp.empty() ? 0 : dp.back();
-            while (0 < j && c != part[j]) {
+        vector<int> dp;
+        for (int i = 0; i < m; ++i) {
+            ret.push_back(s[i]);
+            int j = dp.empty() ? 0 : dp.back() ;
+            while ((j > 0 && s[i] != part[j]) || j == n) {
                 j = lps[j - 1];
             }
-            j = j + (c == part[j] ? 1 : 0);
+            j += (s[i] == part[j] ? 1 : 0);
             dp.push_back(j);
 
             if (dp.back() == n) {
-                for (int k = 0; k < n; ++k) {
-                    ret.pop_back();
-                    dp.pop_back();
-                }
+                dp.erase(dp.end() - n, dp.end());
+                ret.erase(ret.end() - n, ret.end());
             }
         }
 
