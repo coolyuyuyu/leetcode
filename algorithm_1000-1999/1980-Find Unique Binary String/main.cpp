9daa2b0fd881d7d64540d9d8582e1e197c2dba25
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-20 13:23:29 +0800

    1980: update solution

--- algorithm_1000-1999/1980-Find Unique Binary String/main.cpp
+++ algorithm_1000-1999/1980-Find Unique Binary String/main.cpp
@@ -1,44 +1,63 @@
 class Solution {
 public:
-    string findLowestDifferentBinaryString(vector<string>& nums) {
+    string byRecursion(vector<string>& nums) {
         int n = nums.size();
 
-        priority_queue<string, vector<string>, std::greater<string>> pq;
-        for (const string& num : nums) {
-            pq.push(num);
+        unordered_set<int> numSet;
+        for (const auto& s : nums) {
+            numSet.insert(std::stoi(s, nullptr, 2));
         }
 
-        std::function<void(string&)> increment = [](string& num) {
-            for (int i = num.size(), carry = 1; 0 < i--;) {
-                num[i] += carry;
-                carry = (num[i] - '0') / 2;
-                num[i] = '0' + (num[i] - '0') % 2;
+        string s;
+        std::function<bool()> f = [&]() {
+            if (s.size() >= n) {
+                return numSet.find(std::stoi(s, nullptr, 2)) == numSet.end();
             }
+
+            for (char c : {'0', '1'}) {
+                s.push_back(c);
+                if (f()) {
+                    return true;
+                }
+                s.pop_back();
+            }
+            return false;
         };
+        f();
 
-        string cur(n, '1');
-        for (; !pq.empty(); pq.pop()) {
-            increment(cur);
-            if (cur != pq.top()) {
-                return cur;
-            }
-        }
-
-        increment(cur);
-        return cur;
+        return s;
     }
 
-    string CantorDiadonalArgument(vector<string>& nums) {
-        string ret(nums.size(), '\0');
-        for (int i = 0; i < nums.size(); ++i) {
-            ret[i] = (nums[i][i] == '1' ? '0' : '1');
+    string byRandom(vector<string>& nums) {
+        int n = nums.size();
+
+        unordered_set<int> numSet;
+        for (const auto& s : nums) {
+            numSet.insert(std::stoi(s, nullptr, 2));
+        }
+
+        int num;
+        do {
+            num = std::rand() % (int)pow(2, n);
+        } while (numSet.find(num) != numSet.end());
+
+        return std::bitset<16>(num).to_string().substr(16 - n);
+    }
+
+    string byCantorDiagonalArgument(vector<string>& nums) {
+        int n = nums.size();
+
+        string ret(n, '\0');
+        for (int i = 0; i < n; ++i) {
+            ret[i] = '1' - nums[i][i] + '0';
         }
 
         return ret;
     }
 
     string findDifferentBinaryString(vector<string>& nums) {
-        //return findLowestDifferentBinaryString(nums);
-        return CantorDiadonalArgument(nums);
+        //return byRecursion(nums);
+        //return byRandom(nums);
+        return byCantorDiagonalArgument(nums);
     }
 };
