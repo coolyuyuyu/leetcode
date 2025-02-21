fed768cb97e51396ca6f1bc3260ec2dc1399a35b
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-22 01:32:34 +0800

    186: update solution

--- algorithm_0000-0999/186-Reverse Words in a String II/main.cpp
+++ algorithm_0000-0999/186-Reverse Words in a String II/main.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    inline void reverse(vector<char>& str, size_t first, size_t last) {
-        while (first + 1 < last) {
-            swap(str[first], str[last - 1]);
-            ++first, --last;
-        }
-    }
-
-    void reverseWords(vector<char>& str) {
-        reverse(str, 0, str.size());
-
-        size_t nonSpaceStart = 0;
-        for (size_t i = 0; i <= str.size(); ++i) {
-            if (str.size() <= i || isblank(str[i])) {
-                reverse(str, nonSpaceStart, i);
-                nonSpaceStart = i + 1;
+    void reverseWords(vector<char>& s) {
+        std::reverse(s.begin(), s.end());
+        for (int i = 0, pre = 0, n = s.size(); i <= n; ++i) {
+            if (i == n || s[i] == ' ') {
+                std::reverse(s.begin() + pre, s.begin() + i);
+                pre =  i + 1;
             }
         }
     }
