bc8daf30e75296a4b2152d08fb84a94b22c0366e
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-24 01:48:41 +0800

    340: update solution

--- algorithm_0000-0999/340-Longest Substring with At Most K Distinct Characters/main.cpp
+++ algorithm_0000-0999/340-Longest Substring with At Most K Distinct Characters/main.cpp
@@ -4,17 +4,17 @@ public:
         vector<int> freq(256, 0);
 
         int ret = 0;
-        for (int lft = 0, rht = 0, n = s.size(), cnt = 0; lft < n; ++lft) {
-            for (; rht < n && (cnt < k || cnt == k && freq[s[rht]] > 0); ++rht) {
-                if (freq[s[rht]]++ == 0) {
-                    ++cnt;
+        for (int lft = 0, rht = 0, n = s.size(), diffCnt = 0; lft < n; ++lft) {
+            for (; rht < n && (diffCnt < k || (diffCnt == k && freq[s[rht]] > 0)); ++rht) {
+                if (++freq[s[rht]] == 1) {
+                    ++diffCnt;
                 }
             }
 
             ret = std::max(ret, rht - lft);
 
             if (--freq[s[lft]] == 0) {
-                --cnt;
+                --diffCnt;
             }
         }
 
