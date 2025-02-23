6bff0e9a2af48c4f8e212b634c90fac6241908c4
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-24 01:47:11 +0800

    159: update solution

--- algorithm_0000-0999/159-Longest Substring with At Most Two Distinct Characters/main.cpp
+++ algorithm_0000-0999/159-Longest Substring with At Most Two Distinct Characters/main.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
         vector<int> freq(256, 0);
-        int distinctCnt = 0;
 
         int ret = 0;
-        for (int lft = 0, rht = 0, n = s.size(), distinctCnt = 0; rht < n; ++lft) {
-            for (; rht < n && (distinctCnt < 2 || (distinctCnt == 2 && freq[s[rht]] > 0)); ++rht) {
+        for (int lft = 0, rht = 0, n = s.size(), diffCnt = 0; lft < n; ++lft) {
+            for (; rht < n && (diffCnt < 2 || (diffCnt == 2 && freq[s[rht]] > 0)); ++rht) {
                 if (++freq[s[rht]] == 1) {
-                    ++distinctCnt;
+                    ++diffCnt;
                 }
             }
 
             ret = std::max(ret, rht - lft);
 
             if (--freq[s[lft]] == 0) {
-                --distinctCnt;
+                --diffCnt;
             }
         }
 
