a49ad3fdf82ba2764d5400a5aaf275b1ac82f2b5
coolyuyuyu <coolyuyuyu@gmail.com>
2025-02-24 01:47:33 +0800

    280: update solution

--- algorithm_0000-0999/280-Wiggle Sort/main.cpp
+++ algorithm_0000-0999/280-Wiggle Sort/main.cpp
@@ -3,26 +3,27 @@ public:
     // Time: O(NlogN)
     void bySort(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
-        for (size_t i = 2; i < nums.size(); i += 2) {
+        for (int i = 2, n = nums.size(); i < n; i += 2) {
             std::swap(nums[i - 1], nums[i]);
         }
     }
 
     // Time: O(N)
     void byGreedy(vector<int>& nums) {
-        for (size_t i = 1; i < nums.size(); ++i) {
-            if (i % 2) { // odd index
-                if (nums[i] < nums[i - 1]) {
-                    std::swap(nums[i - 1], nums[i]);
+        for (int i = 0, n = nums.size(); i + 1 < n; ++i) {
+            if (i & 1) {
+                if (nums[i] < nums[i + 1]) {
+                    std::swap(nums[i], nums[i + 1]);
                 }
             }
-            else { // even index
-                if (nums[i - 1] < nums[i]) {
-                    std::swap(nums[i - 1], nums[i]);
+            else {
+                if (nums[i] > nums[i + 1]) {
+                    std::swap(nums[i], nums[i + 1]);
                 }
             }
         }
     }
+
     void wiggleSort(vector<int>& nums) {
         //bySort(nums);
         byGreedy(nums);
