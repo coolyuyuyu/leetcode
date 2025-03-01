17519ac0baac0ce46961cc45cc0910c7e56ef688
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-02 03:35:20 +0800

    1092: update solution

--- algorithm_1000-1999/1092-Shortest Common Supersequence/main.cpp
+++ algorithm_1000-1999/1092-Shortest Common Supersequence/main.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    string dp_scs(string str1, string str2) {
-        int m = str1.size(), n = str2.size();
+    string dp_scs(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        s1 = "#" + s1;
+        s2 = "#" + s2;
 
-        str1 = "#" + str1;
-        str2 = "#" + str2;
-
-        // dp[i][j]: the length of shortest common supersequence of str1[1:i] and str2[1:j]
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        // dp[i][j]: the length of shortest common supersequence of s1[] and s2[]
+        int dp[m + 1][n + 1];
         dp[0][0] = 0;
         for (int i = 1; i <= m; ++i) {
             dp[i][0] = i;
@@ -17,7 +16,7 @@ public:
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (str1[i] == str2[j]) {
+                if (s1[i] == s2[j]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else {
@@ -26,40 +25,37 @@ public:
             }
         }
 
-        string ret(dp[m][n], '\0');
+        string ret(dp[m][n], '_');
         int i = m, j = n, k = ret.size() - 1;
-        while (1 <= i && 1 <= j) {
-            if (str1[i] == str2[j]) {
-                ret[k--] = str1[i];
+        while (i > 0 && j > 0) {
+            if (s1[i] == s2[j]) {
+                ret[k--] = s1[i];
                 --i, --j;
             }
-            else if (dp[i][j] == (dp[i - 1][j] + 1)) {
-                ret[k--] = str1[i];
-                --i;
+            else if (dp[i][j] == dp[i - 1][j] + 1) {
+                ret[k--] = s1[i--];
             }
             else {
-                ret[k--] = str2[j];
-                --j;
+                ret[k--] = s2[j--];
             }
         }
-        while (1 <= i) {
-            ret[k--] = str1[i--];
+        while (i > 0) {
+            ret[k--] = s1[i--];
         }
-        while (1 <= j) {
-            ret[k--] = str2[j--];
+        while (j > 0) {
+            ret[k--] = s2[j--];
         }
 
         return ret;
     }
 
-    string dp_lcs(string str1, string str2) {
-        int m = str1.size(), n = str2.size();
+    string dp_lcs(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        s1 = "#" + s1;
+        s2 = "#" + s2;
 
-        str1 = "#" + str1;
-        str2 = "#" + str2;
-
-        // dp[i][j]: the length of longest common subsequence of str1[1:i] and str2[1:j]
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        // dp[i][j]: the length of longest common subsequence of s1[] and s2[]
+        int dp[m + 1][n + 1];
         dp[0][0] = 0;
         for (int i = 1; i <= m; ++i) {
             dp[i][0] = 0;
@@ -69,7 +65,7 @@ public:
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (str1[i] == str2[j]) {
+                if (s1[i] == s2[j]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else {
@@ -78,34 +74,32 @@ public:
             }
         }
 
-        string ret(m + n - dp[m][n], '\0');
+        string ret(m + n - dp[m][n], '_');
         int i = m, j = n, k = ret.size() - 1;
-        while (1 <= i && 1 <= j) {
-            if (str1[i] == str2[j]) {
-                ret[k--] = str1[i];
+        while (i > 0 && j > 0) {
+            if (s1[i] == s2[j]) {
+                ret[k--] = s1[i];
                 --i, --j;
             }
             else if (dp[i][j] == dp[i - 1][j]) {
-                ret[k--] = str1[i];
-                --i;
+                ret[k--] = s1[i--];
             }
             else {
-                ret[k--] = str2[j];
-                --j;
+                ret[k--] = s2[j--];
             }
         }
-        while (1 <= i) {
-            ret[k--] = str1[i--];
+        while (i > 0) {
+            ret[k--] = s1[i--];
         }
-        while (1 <= j) {
-            ret[k--] = str2[j--];
+        while (j > 0) {
+            ret[k--] = s2[j--];
         }
 
         return ret;
     }
 
     string shortestCommonSupersequence(string str1, string str2) {
-        return dp_scs(str1, str2);
-        //return dp_lcs(str1, str2);
+        //return dp_scs(str1, str2);
+        return dp_lcs(str1, str2);
     }
 };
