f46d0cfc0ea6294a28b97a222130a3340c254e97
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-03 02:01:51 +0800

    1165: update solution

--- algorithm_1000-1999/1165-Single-Row Keyboard/main.cpp
+++ algorithm_1000-1999/1165-Single-Row Keyboard/main.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int calculateTime(string keyboard, string word) {
-        int indexes[26];
+        vector<int> indexes(26);
         for (int i = 0; i < 26; ++i) {
             indexes[keyboard[i] - 'a'] = i;
         }
 
         int ret = 0;
-        for (int pre = 0, i = 0; i < word.size(); ++i) {
+        for (int i = 0, pre = 0; i < word.size(); ++i) {
             int cur = indexes[word[i] - 'a'];
             ret += std::abs(cur - pre);
             pre = cur;
         }
-
         return ret;
     }
 };
