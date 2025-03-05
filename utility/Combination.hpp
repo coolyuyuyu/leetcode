ac8ff62fe2d6324de416afe51c8121051b4b68ad
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-05 22:09:23 +0800

    Update Combination.hpp

--- utility/Combination.hpp
+++ utility/Combination.hpp
@@ -1,6 +1,8 @@
+int M = 1e9 + 7;
+
+// Version 1: C(n, r) = C(n - 1, r) + C(n - 1, r - 1)
 int cache[nums.size()][nums.size()];
 std::memset(cache, -1, sizeof(cache));
-
 int comb(int n, int r) {
     if ((n - r) < r) {
         r = n - r;
@@ -22,7 +24,17 @@ int comb(int n, int r) {
     return ret;
 };
 
-int M = 1e9 + 7;
+// Version 2: C(n, r) = (n)*(n-1)*...*(n - r + 1) / (1*2*...*(r))
+int comb(int n, int r) {
+    int ret = 1;
+    for (int i = 0; i < r; ++i) {
+        ret *= n - i;
+        ret /= i + 1
+    }
+    
+    return ret;
+}
+
 vector<int> factorial(n);
 factorial[0] = 1;
 for (int i = 1; i < n; ++i) {
@@ -41,6 +53,17 @@ std::function<int(int, int)> quickPow = [&](int base, int exp) {
     return ret;
 };
 
+// (a / b) % p != (a % p) / (b % p)
+
+// Fermat's little Therom
+// (a / b) % p = a * inv(b) % p
+// inv(b) = b ^ (p - 2)
+// 1. p must be a prime
+// 2. b cannot be divisible by p
+
+
+
+// Version 3: C(n, r) = n! / (r! * (n - r)!)
 std::function<int(int, int)> comb = [&](int m, int n) {
     if (m < n) { return 0; }
     int a = factorial[m];
