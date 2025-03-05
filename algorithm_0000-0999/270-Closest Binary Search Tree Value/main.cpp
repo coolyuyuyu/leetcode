f23646843b7a6cd514456cf7350de150de3aaed2
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-05 22:10:01 +0800

    270: update solution

--- algorithm_0000-0999/270-Closest Binary Search Tree Value/main.cpp
+++ algorithm_0000-0999/270-Closest Binary Search Tree Value/main.cpp
@@ -11,58 +11,25 @@
  */
 class Solution {
 public:
-    int closestValue_Recursive(TreeNode* root, double target) {
-        assert(root);
-
-        int closestVal = root->val;
-        double minDiff = abs(closestVal - target);
-        if (target < root->val) {
-            if (root->left) {
-                int val = closestValue_Recursive(root->left, target);
-                if (abs(val - target) < minDiff) {
-                    closestVal = val;
-                }
-            }
-        }
-        else if (root->val < target) {
-            if (root->right) {
-                int val = closestValue_Recursive(root->right, target);
-                if (abs(val - target) < minDiff) {
-                    closestVal = val;
-                }
-            }
-        }
-
-        return closestVal;
-    }
-
-    int closestValue_Iterative(TreeNode* root, double target) {
-        assert(root);
-
-        int closestVal = root->val;
-        double minDiff = abs(root->val - target);
+    int closestValue(TreeNode* root, double target) {
+        int ret = root->val;
         while (root) {
-            double diff = abs(root->val - target);
-            if (diff < minDiff) {
-                closestVal = root->val;
-                minDiff = abs(root->val - target);
+            double newDiff = std::abs(root->val - target), oldDiff = std::abs(ret - target);
+            if (newDiff < oldDiff || (newDiff == oldDiff && root->val < ret)) {
+                ret = root->val;
             }
-            if (target < root->val) {
-                root = root->left;
-            }
-            else if (root->val < target) {
+
+            if (root->val < target) {
                 root = root->right;
             }
-            else {
+            else if (root->val == target) {
                 break;
+            }
+            else {
+                root = root->left;
             }
         }
 
-        return closestVal;
-    }
-
-    int closestValue(TreeNode* root, double target) {
-        //return closestValue_Recursive(root, target);
-        return closestValue_Iterative(root, target);
+        return ret;
     }
 };
