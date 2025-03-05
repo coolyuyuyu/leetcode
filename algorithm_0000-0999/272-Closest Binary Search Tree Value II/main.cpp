767385b6a744701d92fff94f2fb9cbe40fe7abe2
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-06 00:56:58 +0800

    272: update solution

--- algorithm_0000-0999/272-Closest Binary Search Tree Value II/main.cpp
+++ algorithm_0000-0999/272-Closest Binary Search Tree Value II/main.cpp
@@ -4,73 +4,66 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root, bool forward = true)
+        : m_forward(forward) {
+        for (; root; root = (m_forward ? root->left : root->right)) {
+            m_stk.emplace(root);
+        }
+    }
 
+    bool hasNext() const {
+        return !m_stk.empty();
+    }
+
+    TreeNode* next() {
+        if (!hasNext()) {
+            return nullptr;
+        }
+
+        TreeNode* ret = m_stk.top();
+        m_stk.pop();
+
+        for (TreeNode* root = (m_forward ? ret->right : ret->left); root; root = (m_forward ? root->left : root->right)) {
+            m_stk.push(root);
+        }
+
+        return ret;
+    }
+
+private:
+    bool m_forward;
+    stack<TreeNode*> m_stk;
+};
 
 class Solution {
 public:
-    template<class T>
-    void dfs(TreeNode* root, double target, int k, T& pq) {
-        if (!root) {
-            return;
-        }
-
-        double distance = fabs(target - root->val);
-        if (pq.size() < k) {
-            pq.emplace(distance, root->val);
-        }
-        else {
-            if (distance < pq.top().first) {
-                pq.pop();
-                pq.emplace(distance, root->val);
+    vector<int> closestKValues(TreeNode* root, double target, int k) {
+        queue<int> q;
+        for (BSTIterator itr(root); itr.hasNext();) {
+            int v = itr.next()->val;
+            if (q.size() < k) {
+                q.push(v);
             }
-        }
-
-        dfs(root->left, target, k, pq);
-        dfs(root->right, target, k, pq);
-    }
-
-    template<class T>
-    void inorder(TreeNode* root, double target, int k, T& pq) {
-        if (!root) {
-            return;
-        }
-
-        inorder(root->left, target, k, pq);
-
-        double distance = fabs(target - root->val);
-        if (pq.size() < k) {
-            pq.emplace(distance, root->val);
-        }
-        else {
-            if (distance < pq.top().first) {
-                pq.pop();
-                pq.emplace(distance, root->val);
+            else if (std::abs(v - target) < std::abs(q.front() - target)) {
+                q.pop();
+                q.push(v);
             }
             else {
-                return;
+                break;
             }
         }
 
-        inorder(root->right, target, k, pq);
-    }
-
-    vector<int> closestKValues(TreeNode* root, double target, int k) {
-        auto comp = [](const pair<double, int>& lft, const pair<double, int>& rht) {
-            return lft.first < rht.first;
-        };
-        priority_queue<pair<double, int>, vector<pair<double, int>>, decltype(comp)> pq(comp);
-
-        //dfs(root, target, k, pq);
-        inorder(root, target, k, pq);
-
-        vector<int> nums(pq.size());
-        for (size_t i = 0; i < nums.size(); ++i) {
-            nums[i] = pq.top().second;
-            pq.pop();
+        vector<int> ret;
+        for (; !q.empty(); q.pop()) {
+            ret.push_back(q.front());
         }
-        return nums;
+        return ret;
     }
 };
