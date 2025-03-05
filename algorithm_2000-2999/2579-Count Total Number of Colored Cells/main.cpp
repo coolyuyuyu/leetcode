206c2ac4d3ca19c0dac579a46d97f3eddccb7a72
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-05 22:10:25 +0800

    2579: update solution

--- algorithm_2000-2999/2579-Count Total Number of Colored Cells/main.cpp
+++ algorithm_2000-2999/2579-Count Total Number of Colored Cells/main.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long a = 4;
-        long long b = (long long)(n - 1) * 4;
-        long long c = (a + b) * (n - 1) / 2;
-        return c + 1;
+        // 1 + (4 + 8 + 12 + ... 4(n-1))
+        // 1 + (a0 + a1 + ... + an-2)
+
+        return 1 + ((4LL) + (4LL * (n - 1))) * (n - 1) / 2;
     }
 };
