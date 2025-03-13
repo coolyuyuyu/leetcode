dfdfd8c4823c2541dbb107ed5f4c671160965302
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-13 19:46:08 +0800

    2529: update solution

--- algorithm_2000-2999/2529-Maximum Count of Positive Integer and Negative Integer/main.cpp
+++ algorithm_2000-2999/2529-Maximum Count of Positive Integer and Negative Integer/main.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int p = 0, n = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] < 0) {
-                ++n;
-            }
-            else if (nums[i] > 0) {
-                ++p;
-            }
-        }
-
-        return std::max(n, p);
+        int cntN = std::distance(nums.begin(), std::lower_bound(nums.begin(), nums.end(), 0));
+        int cntP = std::distance(std::upper_bound(nums.begin(), nums.end(), 0), nums.end());
+        return std::max(cntN, cntP);
     }
 };
