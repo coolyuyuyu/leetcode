b396c4fe4b1142c038e26615e31434d726054584
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-17 02:20:49 +0800

    2560: update solution

--- algorithm_2000-2999/2560-House Robber IV/main.cpp
+++ algorithm_2000-2999/2560-House Robber IV/main.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    // check if the rubber can rub at least k houses with capability <= maxCapability
-    // dp approach
-    // Time: O(n), Space: O(n))
-    bool dp1(const vector<int>& nums, int maxCapability, int k) {
-        size_t n = nums.size();
+    // Time: O(n), Space: O(n)
+    bool dp1(vector<int>& nums, int k, int capability) {
+        int n = nums.size();
 
-        vector<array<int, 2>> dp(n);
-        // dp[i][0]: the max number of rubbed house from house[0:i-1] and do not rub house[i]
-        // dp[i][1]: the max number of rubbed house from house[0:i-1] and do rub house[i]
+        // dp[i][0]: how many houses can be rubbed from house[0:i] and house[i] is NOT rubbed.
+        // dp[i][0]: how many houses can be rubbed from house[0:i] and house[i] is rubbed.
+        int dp[n][2];
         dp[0][0] = 0;
-        dp[0][1] = (nums[0] <= maxCapability ? 1 : INT_MIN);
-        for (size_t i = 1; i < nums.size(); ++i) {
-            if (nums[i] <= maxCapability) {
+        dp[0][1] = (nums[0] <= capability ? 1 : INT_MIN);
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] <= capability) {
                 dp[i][0] = std::max(dp[i - 1][0], dp[i - 1][1]);
                 dp[i][1] = dp[i - 1][0] + 1;
             }
@@ -22,37 +20,37 @@ public:
             }
         }
 
-        return k <= std::max(dp[n - 1][0], dp[n - 1][1]);
+        return std::max(dp[n - 1][0], dp[n - 1][1]) >= k;
     }
 
-    // check if the rubber can rub at least k houses with capability <= maxCapability
-    // dp approach
-    // Time: O(n), Space: O(1))
-    bool dp2(const vector<int>& nums, int maxCapability, int k) {
-        int rubN = 0;
-        int rubY = (nums[0] <= maxCapability ? 1 : INT_MIN);
-        for (size_t i = 1; i < nums.size(); ++i) {
-            if (nums[i] <= maxCapability) {
-                int tmpN = std::max(rubN, rubY);
-                int tmpY = rubN + 1;
-                rubN = tmpN, rubY = tmpY;
+    // Time: O(n), Space: O(1)
+    bool dp2(vector<int>& nums, int k, int capability) {
+        int n = nums.size();
+
+        // dpN[i]: how many houses can be rubbed from house[0:i] and house[i] is NOT rubbed.
+        // dpY[i]: how many houses can be rubbed from house[0:i] and house[i] is rubbed.
+        int dpN = 0;
+        int dpY = (nums[0] <= capability ? 1 : INT_MIN);
+        for (int i = 1; i < n; ++i) {
+            int tmpN = dpN, tmpY = dpY;
+            if (nums[i] <= capability) {
+                dpN = std::max(tmpN, tmpY);
+                dpY = tmpN + 1;
             }
             else {
-                rubN = std::max(rubN, rubY);
-                rubY = INT_MIN;
+                dpN = std::max(tmpN, tmpY);
+                dpY = INT_MIN;
             }
         }
 
-        return k <= std::max(rubN, rubY);
+        return std::max(dpN, dpY) >= k;
     }
 
-    // check if the rubber can rub at least k houses with capability <= maxCapability
-    // greedy approach
-    // Time: O(n), Space: O(1))
-    bool greedy(const vector<int>& nums, int maxCapability, int k) {
-        for (size_t i = 0; i < nums.size(); ++i) {
-            if (nums[i] <= maxCapability) {
-                if (--k == 0) {
+    // Time: O(n), Space: O(1)
+    bool greedy(vector<int>& nums, int k, int capability) {
+        for (int i = 0, n = nums.size(); i < n; ++i) {
+            if (nums[i] <= capability) {
+                if (--k <= 0) {
                     return true;
                 }
                 ++i;
@@ -63,13 +61,13 @@ public:
     }
 
     int minCapability(vector<int>& nums, int k) {
-        const auto [minItr, maxItr] = std::minmax_element(nums.begin(), nums.end());
+        auto [minItr, maxItr] = std::minmax_element(nums.begin(), nums.end());
         int lo = *minItr, hi = *maxItr;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            //if (dp1(nums, mid, k)) {
-            //if (dp2(nums, mid, k)) {
-            if (greedy(nums, mid, k)) {
+            //if (dp1(nums, k, mid)) {
+            //if (dp2(nums, k, mid)) {
+            if (greedy(nums, k, mid)) {
                 hi = mid;
             }
             else {
