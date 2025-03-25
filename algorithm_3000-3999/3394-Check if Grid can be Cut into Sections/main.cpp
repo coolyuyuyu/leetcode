d3fc62d382cc3465341a0125b11b8cb009a9b3ce
coolyuyuyu <coolyuyuyu@gmail.com>
2025-03-26 00:02:00 +0800

    3394: update solution

--- algorithm_3000-3999/3394-Check if Grid can be Cut into Sections/main.cpp
+++ algorithm_3000-3999/3394-Check if Grid can be Cut into Sections/main.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        std::function<bool(const vector<pair<int, int>>&)> checkOk = [](const vector<pair<int, int>>& intervals) {
-            int cnt = 0, rhtMost = INT_MIN;
+        std::function<bool(vector<pair<int, int>>&)> f = [](vector<pair<int, int>>& intervals) {
+            std::sort(intervals.begin(), intervals.end());
+
+            int cnt = 0, most = INT_MIN;
             for (const auto& [bgn, end] : intervals) {
-                if (rhtMost <= bgn) {
-                    if (++cnt >= 3) {
+                if (bgn >= most) {
+                    if (++cnt > 2) {
                         return true;
                     }
-                    rhtMost = end;
+                    most = end;
                 }
                 else {
-                    rhtMost = std::max(rhtMost, end);
+                    most = std::max(most, end);
                 }
             }
+
             return false;
         };
 
         vector<pair<int, int>> intervals;
-        std:function<bool(const pair<int, int>& p1, const pair<int, int>& p2)> cmp = [](const pair<int, int>& p1, const pair<int, int>& p2) {
-            return p1.first < p2.first;
-        };
 
         intervals.clear();
         for (const auto& rect : rectangles) {
-            int lft = rect[0], btm = rect[1], rht = rect[2], top = rect[3];
+            int lft = rect[0], rht = rect[2];
             intervals.emplace_back(lft, rht);
         }
-        std::sort(intervals.begin(), intervals.end(), cmp);
-        if (checkOk(intervals)) {
+        if (f(intervals)) {
             return true;
         }
-        
+
         intervals.clear();
         for (const auto& rect : rectangles) {
-            int lft = rect[0], btm = rect[1], rht = rect[2], top = rect[3];
+            int btm = rect[1], top = rect[3];
             intervals.emplace_back(btm, top);
         }
-        std::sort(intervals.begin(), intervals.end(), cmp);
-        return checkOk(intervals);
+        if (f(intervals)) {
+            return true;
+        }
+
+        return false;
     }
 };
