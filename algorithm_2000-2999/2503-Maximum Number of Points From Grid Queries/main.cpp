1d13df378301f03cf41f6756c5cb1c35b8a06d5c
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-03 02:22:24 +0800

    2503: update solution

--- algorithm_2000-2999/2503-Maximum Number of Points From Grid Queries/main.cpp
+++ algorithm_2000-2999/2503-Maximum Number of Points From Grid Queries/main.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        vector<array<int, 2>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
-        vector<size_t> indexes(queries.size());
-        iota(indexes.begin(), indexes.end(), 0);
-        sort(indexes.begin(), indexes.end(), [&queries](size_t i, size_t j) { return queries[i] < queries[j]; });
+        bool visited[m][n];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                visited[r][c] = false;
+            }
+        }
 
-        int m = grid.size(), n = grid[0].size();
-        auto comp = [&grid](const array<int, 2>& p1, const array<int, 2>& p2) {
-            return grid[p1[0]][p1[1]] > grid[p2[0]][p2[1]];
-        };
-        priority_queue<array<int, 2>, vector<array<int, 2>>, decltype(comp)> minHeap(comp);
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int k = queries.size();
+        int indexes[k];
+        std::iota(indexes, indexes + k, 0);
+        std::sort(indexes, indexes + k, [&](int idx1, int idx2) { return queries[idx1] < queries[idx2]; });
 
-        minHeap.push({0, 0});
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, std::greater<tuple<int, int, int>>> pq;
         visited[0][0] = true;
+        pq.emplace(grid[0][0], 0, 0);
 
-        vector<int> ret(indexes.size(), 0);
-        for (int cnt = 0,  i = 0; i < indexes.size(); ++i) {
-            int barrier = queries[indexes[i]];
-            while (!minHeap.empty()) {
-                array<int, 2> p = minHeap.top();
-                if (grid[p[0]][p[1]] >= barrier) {
-                    break;
-                }
+        vector<int> ret(k);
+        for (int i = 0, cnt = 0; i < k; ++i) {
+            int query = queries[indexes[i]];
+            while (!pq.empty() && query > std::get<0>(pq.top())) {
+                auto [val, r, c] = pq.top();
+                pq.pop();
 
-                minHeap.pop();
                 ++cnt;
 
-                for (const auto& dir : dirs) {
-                    int r = p[0] + dir[0], c = p[1] + dir[1];
-                    if (0 <= r && r < m && 0 <= c && c < n && !visited[r][c]) {
-                        minHeap.push({r, c});
-                        visited[r][c] = true;
-                    }
+                for (const auto& [dr, dc] : dirs) {
+                    int x = r + dr, y = c + dc;
+                    if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
+                    if (visited[x][y]) { continue; }
+                    visited[x][y] = true;
+                    pq.emplace(grid[x][y], x, y);
                 }
             }
 
