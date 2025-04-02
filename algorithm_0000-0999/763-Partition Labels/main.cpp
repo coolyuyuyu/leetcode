decb5845e47a077217ec7faa88ed798501d9372a
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-03 02:22:54 +0800

    763: update solution

--- algorithm_0000-0999/763-Partition Labels/main.cpp
+++ algorithm_0000-0999/763-Partition Labels/main.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int n = s.size();
         vector<int> rhts(26, -1);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < s.size(); ++i) {
             rhts[s[i] - 'a'] = i;
         }
 
         vector<int> ret;
-        for (int i = 0; i < n;) {
+        for (int i = 0, n = s.size(); i < n;) {
             int rhtMost = rhts[s[i] - 'a'];
-            for (int j = i; j <= rhtMost; ++j) {
+            for (int j = i; j < rhtMost; ++j) {
                 rhtMost = std::max(rhtMost, rhts[s[j] - 'a']);
             }
 
