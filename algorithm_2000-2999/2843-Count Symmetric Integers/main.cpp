9267797ea34de03092965e990e703c00b85396a6
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-14 02:15:45 +0800

    2843: update solution

--- algorithm_2000-2999/2843-Count Symmetric Integers/main.cpp
+++ algorithm_2000-2999/2843-Count Symmetric Integers/main.cpp
@@ -4,14 +4,12 @@ public:
         int ret = 0;
         for (int num = low; num <= high; ++num) {
             string s = std::to_string(num);
-            if (s.size() & 1) {
-                continue;
-            }
+            if ((s.size() % 2) == 1) { continue; }
 
             int sum = 0;
-            for (int i = 0; (i * 2) < s.size(); ++i) {
-                sum += s[i];
-                sum -= s[s.size() - i - 1];
+            for (int i = 0; i * 2 < s.size(); ++i) {
+                sum += s[i] - '0';
+                sum -= s[s.size() - i - 1] - '0';
             }
             if (sum == 0) {
                 ++ret;
