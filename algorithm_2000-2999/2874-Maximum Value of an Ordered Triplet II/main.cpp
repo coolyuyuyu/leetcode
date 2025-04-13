ce29e4b52c943daf1fbe66a99f27ef3de6dd3eea
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-14 02:15:52 +0800

    2874: update solution

--- algorithm_2000-2999/2874-Maximum Value of an Ordered Triplet II/main.cpp
+++ algorithm_2000-2999/2874-Maximum Value of an Ordered Triplet II/main.cpp
@@ -3,18 +3,23 @@ public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
 
+        int lftMax[n];
+        lftMax[0] = INT_MIN;
+        for (int i = 1; i < n; ++i) {
+            lftMax[i] = std::max(lftMax[i - 1], nums[i - 1]);
+        }
+
         int rhtMax[n];
         rhtMax[n - 1] = INT_MIN;
-        for (int i = n - 2; 0 <= i; --i) {
+        for (int i = n - 2; i >= 0; --i) {
             rhtMax[i] = std::max(rhtMax[i + 1], nums[i + 1]);
         }
 
-        long long ret = LLONG_MIN;
-        for (int j = 1, lftMax = nums[0]; (j + 1) < n; ++j) {
-            ret = std::max(ret, (long long)(lftMax - nums[j]) * rhtMax[j]);
-            lftMax = std::max(lftMax, nums[j]);
+        long long ret = 0;
+        for (int j = 1; j + 1 < n; ++j) {
+            ret = std::max(ret, 1LL * (lftMax[j] - nums[j]) * rhtMax[j]);
         }
 
-        return std::max(ret, 0LL);
+        return ret;
     }
 };
