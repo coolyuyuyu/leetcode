8c1507820a64e0380057ffd0faaa4365b056a6e7
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-14 02:15:59 +0800

    2999: update solution

--- algorithm_2000-2999/2999-Count the Number of Powerful Integers/main.cpp
+++ algorithm_2000-2999/2999-Count the Number of Powerful Integers/main.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    long long numberOfPowerfulInt(const string& maxnum, int limit, const string& suffix) {
-        if (maxnum.size() < suffix.size()) {
-            return 0;
-        }
-
-        bool rhtGE = !std::lexicographical_compare(maxnum.end() - suffix.size(), maxnum.end(), suffix.begin(), suffix.end());
-        std::function<long long(int, bool)> dfs = [&](int i, bool same) {
-            if (i + suffix.size() >= maxnum.size()) {
-                return (rhtGE || !same) ? 1LL : 0LL;
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        std::function<long long(string)> cntLE = [&](string num) {
+            if (num.size() < s.size()) {
+                return 0LL;
             }
 
-            long long ret;
-            if (same) {
-                ret = 0;
-                for (int d = 0; d <= std::min(maxnum[i] - '0', limit); ++d) {
-                    ret += dfs(i + 1, d == maxnum[i] - '0');
+            std::function<long long(int, bool)> f = [&](int pos, bool samePrefix) {
+                if (pos + s.size() >= num.size()) {
+                    return (!samePrefix || num.substr(num.size() - s.size()) >= s) ? 1LL : 0LL;;
                 }
-            }
-            else {
-                ret = 1;
-                for (int exp = maxnum.size() - i - suffix.size(); 0 < exp--;) {
-                    ret *= (limit + 1);
-                }
-            }
 
-            return ret;
+                long long ret = 0;
+                if (samePrefix) {
+                    for (int d = 0; d <= (std::min(limit, (num[pos] - '0'))); ++d) {
+                        ret += f(pos + 1, d == (num[pos] - '0'));
+                    }
+                }
+                else {
+                    ret = 1;
+                    for (int i = pos; i + s.size() < num.size(); ++i) {
+                        ret *= (limit + 1);
+                    }
+                }
+
+                return ret;
+            };
+
+            return f(0, true);
         };
 
-        return dfs(0, true);
-    }
-
-    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
-        return numberOfPowerfulInt(std::to_string(finish), limit, s) - numberOfPowerfulInt(std::to_string(start - 1), limit, s);
+        return cntLE(std::to_string(finish)) - cntLE(std::to_string(start - 1));
     }
 };
