de4d493b506cad27b2132d129f6b31bfe834cbd0
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:33:23 +0800

    1534: update solution

--- algorithm_1000-1999/1534-Count Good Triplets/main.cpp
+++ algorithm_1000-1999/1534-Count Good Triplets/main.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int cnt = 0;
-        for (size_t i = 0; (i + 2) < arr.size(); ++i) {
-            for (size_t j = i + 1; (j + 1) < arr.size(); ++j) {
-                if (a < abs(arr[i] - arr[j])) {
-                    continue;
-                }
-                for (size_t k = j + 1; k < arr.size(); ++k) {
-                    if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
-                        ++cnt;
-                    }
+        int ret = 0;
+        for (int i = 0, n = arr.size(); i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (std::abs(arr[i] - arr[j]) > a) { continue; }
+                for (int k = j + 1; k < n; ++k) {
+                    if (std::abs(arr[j] - arr[k]) > b) { continue; }
+                    if (std::abs(arr[i] - arr[k]) > c) { continue; }
+                    ++ret;
                 }
             }
         }
 
-        return cnt;
+        return ret;
     }
 };
