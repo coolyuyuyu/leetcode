07f84c8647ba7ddb8f4d2b728aeaa472dc444c76
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:32:49 +0800

    311: update solution

--- algorithm_0000-0999/311-Sparse Matrix Multiplication/main.cpp
+++ algorithm_0000-0999/311-Sparse Matrix Multiplication/main.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> f1(const vector<vector<int>>& mat1, const vector<vector<int>>& mat2) {
-        int m = mat1.size(), k = mat1[0].size(), n = mat2[0].size();
 
-        vector<vector<int>> ret(m, vector<int>(n, 0));
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                for (int i = 0; i < k; ++i) {
-                    ret[r][c] += mat1[r][i] * mat2[i][c];
+    vector<vector<int>> f1(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+        int m = mat1.size(), p = mat1[0].size(), n = mat2[0].size();
+
+        vector<vector<int>> ret(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < p; ++k) {
+                    ret[i][j] += mat1[i][k] * mat2[k][j];
                 }
             }
         }
@@ -15,17 +16,15 @@ public:
         return ret;
     }
 
-    vector<vector<int>> f2(const vector<vector<int>>& mat1, const vector<vector<int>>& mat2) {
-        int m = mat1.size(), k = mat1[0].size(), n = mat2[0].size();
+    vector<vector<int>> f2(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+        int m = mat1.size(), p = mat1[0].size(), n = mat2[0].size();
 
-        vector<vector<int>> ret(m, vector<int>(n, 0));
-        for (int r = 0; r < m; ++r) {
-            for (int i = 0; i < k; ++i) {
-                if (mat1[r][i] == 0) {
-                    continue;
-                }
-                for (int c = 0; c < n; ++c) {
-                    ret[r][c] += mat1[r][i] * mat2[i][c];
+        vector<vector<int>> ret(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int k = 0; k < p; ++k) {
+                if (mat1[i][k] == 0) { continue; }
+                for (int j = 0; j < n; ++j) {
+                    ret[i][j] += mat1[i][k] * mat2[k][j];
                 }
             }
         }
