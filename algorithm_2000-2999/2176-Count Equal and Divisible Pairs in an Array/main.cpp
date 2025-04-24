712d8fcba459a2f3d1501767b7e4655adca94205
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:33:32 +0800

    2176: update solution

--- algorithm_2000-2999/2176-Count Equal and Divisible Pairs in an Array/main.cpp
+++ algorithm_2000-2999/2176-Count Equal and Divisible Pairs in an Array/main.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        int cnt = 0;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            for (size_t j = i + 1; j < nums.size(); ++j) {
-                if (nums[i] == nums[j] && (i * j) % k == 0) {
-                    ++cnt;
+        int ret = 0;
+        for (int i = 0, n = nums.size(); i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] == nums[j] && i * j % k == 0) {
+                    ++ret;
                 }
             }
         }
 
-        return cnt;
+        return ret;
     }
 };
