9820fc5aef61194041daacf649a56d9c03086f3a
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:33:08 +0800

    1228: update solution

--- algorithm_1000-1999/1228-Missing Number In Arithmetic Progression/main.cpp
+++ algorithm_1000-1999/1228-Missing Number In Arithmetic Progression/main.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& arr) {
-        int diff = (arr.back() - arr.front()) / int(arr.size());
-        int x = arr.front();
-        for (int num : arr) {
-            if (num != x) {
-                break;
+    // Time: O(N)
+    int f1(vector<int>& arr) {
+        int d = (arr.back() - arr.front()) / (int)arr.size();
+        for (int i = 1, v = arr.front() + d; i < arr.size(); ++i, v += d) {
+            if (v != arr[i]) {
+                return v;
             }
-            x += diff;
         }
 
-        return x;
+        return arr.front();
+    }
+
+    // Time: O(logN)
+    int f2(vector<int>& arr) {
+        int d = (arr.back() - arr.front()) / (int)arr.size();
+        int lo = 0, hi = arr.size() - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr.front() + mid * d != arr[mid]) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+
+        return arr.front() + d * lo;
+    }
+
+    int missingNumber(vector<int>& arr) {
+        return f1(arr);
+        //return f2(arr);
     }
 };
