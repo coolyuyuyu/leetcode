de627877c6b8adc81345d5351418fbb885eb0eb5
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:32:54 +0800

    325: update solution

--- algorithm_0000-0999/325-Maximum Size Subarray Sum Equals k/main.cpp
+++ algorithm_0000-0999/325-Maximum Size Subarray Sum Equals k/main.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maxSubArrayLen(vector<int>& nums, int k) {
-        int n = nums.size();
-
-        unordered_map<long, int> presum2idx = {{0, -1}};
+        unordered_map<int, int> m = {{0, -1}}; // <presum, idx>
 
         int ret = 0;
-        long presum = 0;
-        for (int i = 0; i < n; ++i) {
-            presum += nums[i];
-            if (presum2idx.find(presum - k) != presum2idx.end()) {
-                ret = std::max(ret, i - presum2idx[presum - k]);
+        long sum = 0;
+        for (int i = 0, n = nums.size(); i < n; ++i) {
+            sum += nums[i];
+
+            if (m.find(sum - k) != m.end()) {
+                ret = std::max(ret, i - m[sum - k]);
             }
 
-            presum2idx.emplace(presum, i);
+            m.emplace(sum, i);
         }
 
         return ret;
