87f01785fd474761de754c645a127fce344747f3
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:33:16 +0800

    1399: update solution

--- algorithm_1000-1999/1399-Count Largest Group/main.cpp
+++ algorithm_1000-1999/1399-Count Largest Group/main.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int countLargestGroup(int n) {
-        vector<int> counts(37, 0);
+        vector<int> cnts(37, 0);
         for (int i = 1; i <= n; ++i) {
             int sum = 0;
-            for (int num = i; 0 < num; num /= 10) {
-                sum += (num % 10);
+            for (int num = i; num; num /= 10) {
+                sum += num % 10;
             }
-            ++counts[sum];
+            ++cnts[sum];
         }
 
-        int maxVal = *max_element(counts.begin(), counts.end());
-        return count(counts.begin(), counts.end(), maxVal);
+        int maxCnt = *std::max_element(cnts.begin(), cnts.end());
+        return std::count(cnts.begin(), cnts.end(), maxCnt);
     }
 };
