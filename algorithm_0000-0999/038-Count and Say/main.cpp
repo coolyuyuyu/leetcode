423008a721356ac139e22bcf16a746051ce03395
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-24 11:32:32 +0800

    038: update solution

--- algorithm_0000-0999/038-Count and Say/main.cpp
+++ algorithm_0000-0999/038-Count and Say/main.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string s = "1";
-        for (int i = 1; i < n; ++i) {
-            vector<char> chars;
-            vector<int> counts;
-
-            char c = s[0];
-            int count = 1;
-            for (string::const_iterator iter = s.begin() + 1; iter != s.end(); ++iter) {
-                if (*iter == c) {
-                    ++count;
+        string ret = "1";
+        for (; 1 < n; --n) {
+            string s;
+            for (int i = 0, cnt = 0; i <= ret.size(); ++i) {
+                if (i > 0 && (ret[i - 1] != ret[i] || i == ret.size())) {
+                    s += std::to_string(cnt);
+                    s += ret[i - 1];
+                    cnt = 0;
                 }
-                else {
-                    chars.push_back(c);
-                    counts.push_back(count);
-
-                    c = *iter;
-                    count = 1;
-                }
+                ++cnt;
             }
-            chars.push_back(c);
-            counts.push_back(count);
-            assert(chars.size() == counts.size());
-
-            string target;
-            target.reserve(accumulate(counts.begin(), counts.end(), (size_t)0));
-            for (size_t i = 0; i < chars.size(); ++i) {
-                target.push_back(counts[i] + '0');
-                target.push_back(chars[i]);
-            }
-            s = target;
+            ret = s;
         }
-        return s;
+
+        return ret;
     }
 };
