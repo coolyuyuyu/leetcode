85d310cb342524290d1f382e34dc65d20008c25f
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-27 02:46:42 +0800

    2444: update solution

--- algorithm_2000-2999/2444-Count Subarrays With Fixed Bounds/main.cpp
+++ algorithm_2000-2999/2444-Count Subarrays With Fixed Bounds/main.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        // For each index i, count number of subarray ending at i.
-
         long long ret = 0;
-        for (int i = 0, preMin = -1, preMax = -1, bound = -1; i < nums.size(); ++i) {
-            if (nums[i] < minK || maxK < nums[i]) {
+        for (int i = 0, minIdx = -1, maxIdx = -1, bound = -1; i < nums.size(); ++i) {
+            if (nums[i] < minK || nums[i] > maxK) {
                 bound = i;
                 continue;
             }
 
             if (nums[i] == minK) {
-                preMin = i;
+                minIdx = i;
             }
             if (nums[i] == maxK) {
-                preMax = i;
+                maxIdx = i;
             }
 
-            ret += std::max(std::min(preMin, preMax) - bound, 0);
+            ret += std::max(std::min(minIdx, maxIdx) - bound, 0);
         }
 
         return ret;
+
     }
 };
