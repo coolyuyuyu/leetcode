24ef994e38f7c875bce1eeaf21cf4541eaa93efa
coolyuyuyu <coolyuyuyu@gmail.com>
2025-04-27 02:46:48 +0800

    2845: update solution

--- algorithm_2000-2999/2845-Count of Interesting Subarrays/main.cpp
+++ algorithm_2000-2999/2845-Count of Interesting Subarrays/main.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-        unordered_map<int, int> m;
-        m[0] = 1;
+        unordered_map<int, int> m = {{0, 1}};
 
         long long ret = 0;
         for (int i = 0, cnt = 0; i < nums.size(); ++i) {
-            cnt += ((nums[i] % modulo) == k ? 1 : 0);
-            ret +=  m[((cnt % modulo - k) % modulo + modulo) % modulo];
+            cnt += (nums[i] % modulo == k ? 1 : 0);
+            ret += m[((cnt - k) % modulo + modulo) % modulo];
             ++m[cnt % modulo];
         }
 
