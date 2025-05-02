0b56e1bdbe6263a95a2f99ae1ca15b7919a3ac59
coolyuyuyu <coolyuyuyu@gmail.com>
2025-05-02 11:13:56 +0800

    838: initial solution

---
+++ algorithm_0000-0999/838-Push Dominoes/main.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        dominoes = "L" + dominoes + "R";
+
+        string ret;
+        for (int i = 0; i + 1< dominoes.size(); ) {
+            int j = i + 1;
+            while (j < dominoes.size() && dominoes[j] == '.') {
+                ++j;
+            }
+
+            int inLen = j - i - 1;
+            ret.push_back(dominoes[i]);
+            if (dominoes[i] == 'L' && dominoes[j] == 'L') {
+                for (int k = 0; k < inLen; ++k) {
+                    ret += 'L';
+                }
+            }
+            else if (dominoes[i] == 'L' && dominoes[j] == 'R') {
+                for (int k = 0; k < inLen; ++k) {
+                    ret += '.';
+                }
+            }
+            else if (dominoes[i] == 'R' && dominoes[j] == 'L') {
+                for (int k = 0; k < inLen/2; ++k) {
+                    ret += 'R';
+                }
+                if (inLen & 1) {
+                    ret += '.';
+                }
+                for (int k = 0; k < inLen/2; ++k) {
+                    ret += 'L';
+                }
+            }
+            else if (dominoes[i] == 'R' && dominoes[j] == 'R') {
+                for (int k = 0; k < inLen; ++k) {
+                    ret += 'R';
+                }
+            }
+
+            i = j;
+        }
+        ret = ret.substr(1);
+
+        return ret;
+    }
+};
