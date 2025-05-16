641cdf1bc0c0f14a6068554f2825701248ad2f23
coolyuyuyu <coolyuyuyu@gmail.com>
2025-05-16 19:42:39 +0800

    3342: update solution

--- algorithm_3000-3999/3342-Find Minimum Time to Reach Last Room II/main.cpp
+++ algorithm_3000-3999/3342-Find Minimum Time to Reach Last Room II/main.cpp
@@ -5,23 +5,21 @@ public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int m = moveTime.size(), n = moveTime.empty() ? 0 : moveTime[0].size();
 
-        bool dones[m][n];
+        bool visited[m][n];
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
-                dones[r][c] = false;
+                visited[r][c] = false;
             }
         }
 
-        priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, std::greater<tuple<int, int, int, int>>> pq; // <time, r, c>
+        priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, std::greater<tuple<int, int, int, int>>> pq;
         pq.emplace(0, 0, 0, 2);
         while (!pq.empty()) {
             auto [t, r, c, cost] = pq.top();
             pq.pop();
 
-            if (dones[r][c]) {
-                continue;
-            }
-            dones[r][c] = true;
+            if (visited[r][c]) { continue; }
+            visited[r][c] = true;
 
             if (r == m - 1 && c == n - 1) {
                 return t;
@@ -30,9 +28,9 @@ public:
             for (const auto& [dr, dc] : dirs) {
                 int x = r + dr, y = c + dc;
                 if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
-                if (dones[x][y]) { continue; }
-                int nxtCost = (cost == 1 ? 2 : 1);
-                pq.emplace(std::max(t + nxtCost, moveTime[x][y] + nxtCost), x, y, nxtCost);
+                if (visited[x][y]) { continue; }
+                int nxtCost = 3 - cost; 
+                pq.emplace(std::max(moveTime[x][y] + nxtCost, t + nxtCost), x, y, nxtCost);
             }
         }
         __builtin_unreachable();
