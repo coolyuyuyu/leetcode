4312f4ad825012a326ba08c027315517fa39548f
coolyuyuyu <coolyuyuyu@gmail.com>
2025-05-16 19:42:26 +0800

    2901: update solution

--- algorithm_2000-2999/2901-Longest Unequal Adjacent Groups Subsequence II/main.cpp
+++ algorithm_2000-2999/2901-Longest Unequal Adjacent Groups Subsequence II/main.cpp
@@ -1,49 +1,44 @@
 class Solution {
 public:
-    vector<string> getWordsInLongestSubsequence(int n, vector<string>& words, vector<int>& groups) {
-        // adjacent[j][i]: whether index i can be a successor of j in a qualified subsequence
-        bool adjacent[n][n];
+    vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+
+        bool connected[n][n];
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (groups[j] == groups[i]) {
-                    adjacent[j][i] = false;
+            for (int j = i + 1; j < n; ++j) {
+                if (groups[i] == groups[j]) {
+                    connected[i][j] = false;
                 }
-                else if (words[j].size() != words[i].size()) {
-                    adjacent[j][i] = false;
+                else if (words[i].size() != words[j].size()) {
+                    connected[i][j] = false;
                 }
                 else {
                     int cnt = 0;
-                    for (int k = 0, len = words[j].size(); k < len; ++k) {
-                        if (words[j][k] != words[i][k]) {
-                            if (++cnt > 1) {
-                                break;
-                            }
-                        }
+                    for (int k = 0, m = words[i].size(); k < m && cnt <= 1; ++k) {
+                        cnt += (words[i][k] != words[j][k] ? 1 : 0);
                     }
-                    adjacent[j][i] = (cnt == 1);
+                    connected[i][j] = (cnt == 1);
                 }
             }
         }
 
-        // dp[i]: the length of the longest qualified subsequence from words/groups[0:i] ending at index i
+        // the length of the longest subsequence from words[0:i] ending at words[i]
         int dp[n];
+        std::fill(dp, dp + n, 1);
 
-        // prev[i]: the previous index of the longest qualified subsequence from words/groups[0:i] ending at index i
+        // prev[i]: the previous index of i from the longest subsequence ending at words[i]
         int prev[n];
         std::fill(prev, prev + n, -1);
-
         for (int i = 0; i < n; ++i) {
-            dp[i] = 1;
             for (int j = 0; j < i; ++j) {
-                if (adjacent[j][i] && dp[j] + 1 > dp[i]) {
+                if (connected[j][i] && dp[j] + 1 > dp[i]) {
                     dp[i] = dp[j] + 1;
                     prev[i] = j;
                 }
             }
         }
 
-        int retLen = 0;
-        int retIdx = -1;
+        int retLen = 0, retIdx = -1;
         for (int i = 0; i < n; ++i) {
             if (dp[i] > retLen) {
                 retLen = dp[i];
@@ -52,9 +47,8 @@ public:
         }
 
         vector<string> ret;
-        while (retIdx != -1) {
-            ret.push_back(words[retIdx]);
-            retIdx = prev[retIdx];
+        for (int i = retIdx; i != -1; i = prev[i]) {
+            ret.push_back(words[i]);
         }
         std::reverse(ret.begin(), ret.end());
 
