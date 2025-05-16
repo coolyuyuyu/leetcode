4e72601110d9597e8124d8be292f4230768f87ba
coolyuyuyu <coolyuyuyu@gmail.com>
2025-05-16 19:41:30 +0800

    359: update solution

--- algorithm_0000-0999/359-Logger Rate Limiter/main.cpp
+++ algorithm_0000-0999/359-Logger Rate Limiter/main.cpp
@@ -1,20 +1,15 @@
 class Logger {
 public:
-    /** Initialize your data structure here. */
     Logger() {
     }
 
-    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity. */
     bool shouldPrintMessage(int timestamp, string message) {
-        auto p = m_timestamps.emplace(message, timestamp);
+        auto p = m_msg2time.emplace(message, timestamp);
         if (p.second) {
             return true;
         }
         else {
-            int lastTimestamp = p.first->second;
-            if (timestamp >= (lastTimestamp + 10)) {
+            if (timestamp - p.first->second >= 10) {
                 p.first->second = timestamp;
                 return true;
             }
@@ -25,11 +20,11 @@ public:
     }
 
 private:
-    unordered_map<string, int> m_timestamps;
+    unordered_map<string, int> m_msg2time;
 };
 
 /**
  * Your Logger object will be instantiated and called as such:
- * Logger obj = new Logger();
- * bool param_1 = obj.shouldPrintMessage(timestamp,message);
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
  */
