03d0b2ff8d072d422d9b0b4f60d163a052f264f5
coolyuyuyu <coolyuyuyu@gmail.com>
2025-05-23 16:31:24 +0800

    3355: update solution

--- algorithm_3000-3999/3355-Zero Array Transformation I/main.cpp
+++ algorithm_3000-3999/3355-Zero Array Transformation I/main.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        int diffs[n];
-        std::fill(diffs, diffs + n, 0);
+        int diffs[n + 1];
+        std::fill(diffs, diffs + n + 1, 0);
 
         for (const auto& query : queries) {
             int l = query[0], r = query[1];
-            diffs[l] += 1;
-            if (r + 1 < n) {
-                diffs[r + 1] -= 1;
-            }
+            ++diffs[l];
+            --diffs[r + 1];
         }
 
         for (int i = 0, sum = 0; i < n; ++i) {
