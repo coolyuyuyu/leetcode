d9d125e5e179b8e57315c908af8a693a1d0086a6
coolyuyuyu <coolyuyuyu@gmail.com>
2025-05-23 16:31:08 +0800

    073: update solution

--- algorithm_0000-0999/073-Set Matrix Zeroes/main.cpp
+++ algorithm_0000-0999/073-Set Matrix Zeroes/main.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     // Space: O(m + n)
     void setZeroes1(vector<vector<int>>& matrix) {
-        size_t m = matrix.size();
-        size_t n = matrix.empty() ? 0 : matrix.front().size();
+        int m = matrix.size();
+        int n = matrix.empty() ? 0 : matrix.front().size();
 
         vector<bool> rows(m, false), cols(n, false);
-        for (size_t r = 0; r < m; ++r) {
-            for (size_t c = 0; c < n; ++c) {
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
                 if (matrix[r][c] == 0) {
                     rows[r] = true;
                     cols[c] = true;
@@ -15,8 +15,8 @@ public:
             }
         }
 
-        for (size_t r = 0; r < m; ++r) {
-            for (size_t c = 0; c < n; ++c) {
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
                 if (rows[r] || cols[c]) {
                     matrix[r][c] = 0;
                 }
@@ -26,27 +26,26 @@ public:
 
     // Space: O(1)
     void setZeroes2(vector<vector<int>>& matrix) {
-        size_t m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
+        int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
         bool zeroRow0 = false;
-        for (size_t c = 0; c < n && !zeroRow0; ++c) {
+        for (int c = 0; c < n && !zeroRow0; ++c) {
             if (matrix[0][c] == 0) { zeroRow0 = true; }
         }
         bool zeroCol0 = false;
-        for (size_t r = 0; r < m && !zeroCol0; ++r) {
+        for (int r = 0; r < m && !zeroCol0; ++r) {
             if (matrix[r][0] == 0) { zeroCol0 = true; }
         }
 
-        for (size_t r = 1; r < m; ++r) {
-            for (size_t c = 1; c < n; ++c) {
+        for (int r = 1; r < m; ++r) {
+            for (int c = 1; c < n; ++c) {
                 if (matrix[r][c] == 0) {
-                    matrix[r][0] = 0;
-                    matrix[0][c] = 0;
+                    matrix[r][0] = matrix[0][c] = 0;
                 }
             }
         }
-        for (size_t r = 1; r < m; ++r) {
-            for (size_t c = 1; c < n; ++c) {
+        for (int r = 1; r < m; ++r) {
+            for (int c = 1; c < n; ++c) {
                 if (matrix[r][0] == 0 || matrix[0][c] == 0) {
                     matrix[r][c] = 0;
                 }
