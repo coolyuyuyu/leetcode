84a2e9cd78fdb636c1564d85e57399aabc07a09c
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-04 16:29:12 +0800

    909: update solution

--- algorithm_0000-0999/909-Snakes and Ladders/main.cpp
+++ algorithm_0000-0999/909-Snakes and Ladders/main.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size();
-        int src = 1, dst = n * n;
 
-        auto computePos = [&](int idx) -> pair<int, int> {
+        std::function<pair<int, int>(int)> idx2pos = [&](int idx) {
             --idx;
             int r = n - (idx / n) - 1;
-            int c = ((idx / n) % 2) == 0 ? (idx % n) : (n - (idx % n) - 1);
-            return {r, c};
+            int c = (n - r - 1) & 1 ? (n - idx % n - 1): (idx % n);
+            return std::make_pair(r, c);
         };
+
+        int src = 1, dst = n * n;
 
         auto comp = [&](const pair<int, int>& p1, const pair<int, int>& p2) -> bool {
             return p1.first > p2.first || (p1.first == p2.first && p1.second < p2.second);
@@ -17,31 +18,22 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
         pq.emplace(0, src);
 
-        vector<bool> visited(dst + 1, false);
+        vector<bool> visited(n * n + 1, false);
         visited[src] = true;
         while (!pq.empty()) {
-            auto [move, idx] = pq.top();
+            auto [move, cur] = pq.top();
             pq.pop();
 
-            if (dst == idx) {
+            if (cur == dst) {
                 return move;
             }
-            else if (dst < (idx + 6)) {
-                return move + 1;
-            }
 
-            for (int diff = 1; diff <= 6; ++diff) {
-                int nxt = idx + diff;
-                auto [r, c] = computePos(nxt);
-                if (visited[nxt]) {
-                    continue;
-                }
+            for (int nxt = cur + 1; nxt <= cur + 6 && nxt <= dst; ++nxt) {
+                if (visited[nxt]) { continue; }
                 visited[nxt] = true;
 
-                if (board[r][c] != -1 ) {
-                    nxt = board[r][c];
-                }
-                pq.emplace(move + 1, nxt);
+                auto [r, c] = idx2pos(nxt);
+                pq.emplace(move + 1, (board[r][c] != -1 ? board[r][c] : nxt));
             }
         }
 
