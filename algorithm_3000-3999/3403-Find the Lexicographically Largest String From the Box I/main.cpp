c8c5860d4c94decc8628ddc1ff16afceb04198a5
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-04 16:29:41 +0800

    3403: update solution

--- algorithm_3000-3999/3403-Find the Lexicographically Largest String From the Box I/main.cpp
+++ algorithm_3000-3999/3403-Find the Lexicographically Largest String From the Box I/main.cpp
@@ -5,11 +5,11 @@ public:
             return word;
         }
 
-        char targetChar = *std::max_element(word.begin(), word.end());
+        char target = *std::max_element(word.begin(), word.end());
 
-        string ret = "";
+        string ret;
         for (int i = 0, n = word.size(); i < n; ++i) {
-            if (word[i] != targetChar) { continue; }
+            if (word[i] != target) { continue; }
             ret = std::max(ret, word.substr(i, n - numFriends + 1));
         }
 
