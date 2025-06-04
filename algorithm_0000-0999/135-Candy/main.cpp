70a301cd5504a42de6affb3226f0a54f5a442915
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-04 16:29:07 +0800

    135: update solution

--- algorithm_0000-0999/135-Candy/main.cpp
+++ algorithm_0000-0999/135-Candy/main.cpp
@@ -3,24 +3,18 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
 
-        int cnts[n];
-
-        cnts[0] = 1;
-        for (int i = 1; i < n; ++i) { // compare left
+        vector<int> candies(n, 1);
+        for (int i = 1; i < n; ++i) {
             if (ratings[i - 1] < ratings[i]) {
-                cnts[i] = cnts[i - 1] + 1;
-            }
-            else {
-                cnts[i] = 1;
+                candies[i] = candies[i - 1] + 1;
             }
         }
-
-        for (int i = n - 1; 0 < i--;) { // compare right
+        for (int i = n - 2; i >= 0; --i) {
             if (ratings[i] > ratings[i + 1]) {
-                cnts[i] = std::max(cnts[i], cnts[i + 1] + 1);
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
             }
         }
 
-        return std::accumulate(cnts, cnts + n, 0);
+        return std::accumulate(candies.begin(), candies.end(), 0);
     }
 };
