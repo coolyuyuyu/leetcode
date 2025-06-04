0259aa13e84a186dbe2e924f2ee2786881ad5873
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-04 16:29:24 +0800

    2359: update solution

--- algorithm_2000-2999/2359-Find Closest Node to Given Two Nodes/main.cpp
+++ algorithm_2000-2999/2359-Find Closest Node to Given Two Nodes/main.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    vector<int> computeDistances(const vector<int>& edges, int src) {
-        int n = edges.size();
-        vector<int> dists(n, INT_MAX);
-        for (int dist = 0; src != -1 && dists[src] == INT_MAX; ++dist) {
-            dists[src] = dist;
-            src = edges[src];
-        }
-
-        return dists;
-    }
-
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        if (node1 == node2) {
-            return node1;
-        }
-
         int n = edges.size();
-        vector<int> dists1 = computeDistances(edges, node1), dists2 = computeDistances(edges, node2);
+        int dists1[n], dists2[n];
+        std::fill(dists1, dists1 + n, INT_MAX);
+        std::fill(dists2, dists2 + n, INT_MAX);
+        std::function<void(int, int[])> computeDists = [&](int node, int dists[]) {
+            for (int dist = 0; node != -1 && dists[node] == INT_MAX; node = edges[node], ++dist) {
+                dists[node] = dist;
+            }
+        };
+        computeDists(node1, dists1);
+        computeDists(node2, dists2);
 
-        int target = -1;
         int minDist = INT_MAX;
+        int ret = -1;
         for (int i = 0; i < n; ++i) {
             int dist = std::max(dists1[i], dists2[i]);
-            if (dist == INT_MAX) {
-                continue;
-            }
-
             if (dist < minDist) {
-                target = i;
                 minDist = dist;
+                ret = i;
             }
         }
 
-        return target;
+        return ret;
     }
 };
-
-/*
-[9,8,7,0,5,6,1,3,2,2]
- 0 1 2 3 4 5 6 7 8 9
- 1 -> 8 -> 2 -> 7 -> 3 -> 0 -> 9 -> 2
-*/
