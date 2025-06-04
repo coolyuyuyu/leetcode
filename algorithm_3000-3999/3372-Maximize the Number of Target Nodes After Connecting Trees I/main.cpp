6c797d0395b4413df006c138ce83566034b8a26d
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-04 16:29:30 +0800

    3372: update solution

--- algorithm_3000-3999/3372-Maximize the Number of Target Nodes After Connecting Trees I/main.cpp
+++ algorithm_3000-3999/3372-Maximize the Number of Target Nodes After Connecting Trees I/main.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        int n = edges1.size() + 1, m = edges2.size() + 1;
-        vector<vector<int>> graph1(n, vector<int>()), graph2(m, vector<int>());
-        for (const auto& edge : edges1) {
-            graph1[edge[0]].push_back(edge[1]);
-            graph1[edge[1]].push_back(edge[0]);
-        }
-        for (const auto& edge : edges2) {
-            graph2[edge[0]].push_back(edge[1]);
-            graph2[edge[1]].push_back(edge[0]);
-        }
+        std::function<vector<vector<int>>(const vector<vector<int>>&)> buildGraph = [](const vector<vector<int>>& edges) {
+            vector<vector<int>> graph(edges.size() + 1);
+            for (const auto& edge : edges) {
+                graph[edge[0]].push_back(edge[1]);
+                graph[edge[1]].push_back(edge[0]);
+            }
+
+            return graph;
+        };
+        vector<vector<int>> graph1(buildGraph(edges1)), graph2(buildGraph(edges2));
+        int n = graph1.size(), m = graph2.size();
 
         std::function<int(const vector<vector<int>>&, int, int)> countCovered = [](const vector<vector<int>>& graph, int src, int k) {
             int ret = 0;
-            for (queue<pair<int, int>> q({{src, -1}}); k >= 0; --k) {
+            for (queue<pair<int, int>> q({{src, -1}});!q.empty() && k >= 0; k--) {
                 ret += q.size();
-
                 for (int i = q.size(); 0 < i--;) {
                     auto [cur, pre] = q.front();
                     q.pop();
@@ -31,14 +31,14 @@ public:
             return ret;
         };
 
-        int maxG2Covered = 0;
-        for (int i = 0; i < m; ++i) {
-            maxG2Covered = std::max(maxG2Covered, countCovered(graph2, i, k - 1));
+        int maxG2CoveredCnt = 0;
+        for (int j = 0; j < m; ++j) {
+            maxG2CoveredCnt = std::max(maxG2CoveredCnt, countCovered(graph2, j, k - 1));
         }
 
         vector<int> ret(n);
         for (int i = 0; i < n; ++i) {
-            ret[i] = countCovered(graph1, i, k) + maxG2Covered;
+            ret[i] = countCovered(graph1, i, k) + maxG2CoveredCnt;
         }
 
         return ret;
