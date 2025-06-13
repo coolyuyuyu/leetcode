e5652fbd7a9e181f88dc0c9ef469507a9476f07b
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-14 01:14:52 +0800

    3170: update solution

--- algorithm_3000-3999/3170-Lexicographically Minimum String After Removing Stars/main.cpp
+++ algorithm_3000-3999/3170-Lexicographically Minimum String After Removing Stars/main.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     string clearStars(string s) {
-        auto comp = [](const pair<char, int>& p1, const pair<char, int>& p2) {
-            const auto& [c1, idx1] = p1;
-            const auto& [c2, idx2] = p2;
-            return c1 > c2 || (c1 == c2 && idx1 < idx2);
+        auto comp = [&](int i1, int i2) {
+            return s[i1] > s[i2] || (s[i1] == s[i2] && i1 < i2);
         };
-        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(comp)> pq(comp);
-        for (int i = 0; i < s.size(); ++i) {
-            char c = s[i];
-            if (c == '*' && !pq.empty()) {
-                auto [_, idx] = pq.top();
+        priority_queue<int, vector<int>, decltype(comp)> pq(comp);
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            if (s[i] == '*') {
+                s[pq.top()] = '*';
                 pq.pop();
-                s[idx] = '\0';
             }
-            else if (isalpha(c)) {
-                pq.emplace(c, i);
+            else {
+                pq.push(i);
             }
         }
 
         string ret;
-        for (char c : s) {
-            if (isalpha(c)) {
-                ret += c;
-            }
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            if (s[i] == '*') { continue; }
+            ret.push_back(s[i]);
         }
 
         return ret;
