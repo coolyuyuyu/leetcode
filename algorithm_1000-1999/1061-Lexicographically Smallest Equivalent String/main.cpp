ff17efd379367893a53e4ce3dc6032a7e3903926
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-14 01:14:18 +0800

    1061: update solution

--- algorithm_1000-1999/1061-Lexicographically Smallest Equivalent String/main.cpp
+++ algorithm_1000-1999/1061-Lexicographically Smallest Equivalent String/main.cpp
@@ -1,30 +1,22 @@
 class DisjointSets {
 public:
     DisjointSets(int n)
-        : m_parents(n)
-        , m_size(n) {
+        : m_parents(n) {
         std::iota(m_parents.begin(), m_parents.end(), 0);
     }
 
     void merge(int elem1, int elem2) {
         int root1 = root(elem1), root2 = root(elem2);
-        if (root1 != root2) {
-            if (root1 < root2) {
-                m_parents[root2] = root1;
-            }
-            else {
-                m_parents[root1] = root2;
-            }
-            --m_size;
+        if (root1 < root2) {
+            m_parents[root2] = root1;
+        }
+        else if (root1 > root2){
+            m_parents[root1] = root2;
         }
     }
 
     bool connected(int elem1, int elem2) const {
         return root(elem1) == root(elem2);
-    }
-
-    int size() const {
-        return m_size;
     }
 
     int root(int elem) const {
@@ -37,7 +29,6 @@ public:
 
 private:
     mutable vector<int> m_parents;
-    int m_size;
 };
 
 class Solution {
@@ -48,10 +39,11 @@ public:
             ds.merge(s1[i] - 'a', s2[i] - 'a');
         }
 
-        for (char& c : baseStr) {
-            c = 'a' + ds.root(c - 'a');
+        string ret(baseStr.size(), '\0');
+        for (int i = 0, n = baseStr.size(); i < n; ++i) {
+            ret[i] = 'a' + ds.root(baseStr[i] - 'a');
         }
 
-        return baseStr;
+        return ret;
     }
 };
