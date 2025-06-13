28a9254d212d385efa7e55884e10bfcb00307c6b
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-14 01:14:26 +0800

    2434: update solution

--- algorithm_2000-2999/2434-Using a Robot to Print the Lexicographically Smallest String/main.cpp
+++ algorithm_2000-2999/2434-Using a Robot to Print the Lexicographically Smallest String/main.cpp
@@ -3,26 +3,25 @@ public:
     string robotWithString(string s) {
         int n = s.size();
 
-        vector<char> suffix(n);
-        suffix[n - 1] = s[n - 1];
-        for (int i = n - 1; 0 < i--;) {
-            suffix[i] = std::min(s[i], suffix[i + 1]);
+        char sufMin[n];
+        sufMin[n - 1] = s[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            sufMin[i] = std::min(s[i], sufMin[i + 1]);
         }
 
-        string t;
+        stack<char> t;
 
-        stack<char> stk;
-        for (size_t i = 0; i < n || !stk.empty();) {
-            if (i < n && (stk.empty() || suffix[i] < stk.top())) {
-                stk.push(s[i]);
-                ++i;
+        string ret;
+        for (int i = 0; i < n || !t.empty();) {
+            if (i < n && (t.empty() || t.top() > sufMin[i])) {
+                t.push(s[i++]);
             }
             else {
-                t.push_back(stk.top());
-                stk.pop();
+                ret.push_back(t.top());
+                t.pop();
             }
         }
 
-        return t;
+        return ret;
     }
 };
