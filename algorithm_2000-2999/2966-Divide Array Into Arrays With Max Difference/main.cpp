defc59d9169d9ade4f8713975bc535c69f37ab1c
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-21 11:48:51 +0800

    2966: update solution

--- algorithm_2000-2999/2966-Divide Array Into Arrays With Max Difference/main.cpp
+++ algorithm_2000-2999/2966-Divide Array Into Arrays With Max Difference/main.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        int n = nums.size();
-
         std::sort(nums.begin(), nums.end());
 
-        vector<vector<int>> ret(n / 3, vector<int>(3));
+        int n = nums.size();
+        vector<vector<int>> ret;
         for (int i = 0; i < n; i += 3) {
             if (nums[i + 2] - nums[i] > k) {
                 return {};
             }
-
-            vector<int>& triplet = ret[i / 3];
-            std::copy(nums.begin() + i, nums.begin() + i + 3, triplet.begin());
+            ret.emplace_back(nums.begin() + i, nums.begin() + i + 3);
         }
 
         return ret;
