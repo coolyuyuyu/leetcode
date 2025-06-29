b2783799c2d269c8f83a88b9b76987bcafda4538
coolyuyuyu <coolyuyuyu@gmail.com>
2025-06-29 13:51:59 +0800

    1498: update solution

--- algorithm_1000-1999/1498-Number of Subsequences That Satisfy the Given Sum Condition/main.cpp
+++ algorithm_1000-1999/1498-Number of Subsequences That Satisfy the Given Sum Condition/main.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int M = 1e9 + 7;
+
     int numSubseq(vector<int>& nums, int target) {
         int n = nums.size();
 
         std::sort(nums.begin(), nums.end());
+
+        int power2[n];
+        power2[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            power2[i] = 1L * power2[i - 1] * 2 % M;
+        }
 
         int ret = 0;
         for (int i = 0, j = n - 1; i <= j; ++i) {
             while (i <= j && nums[i] + nums[j] > target) {
                 --j;
             }
-            if (i > j) { break; }
+            if (i > j) break;
 
-            // QuickPow
-            int cnt = 1;
-            for (long exp = j - i, base = 2; exp; exp >>= 1) {
-                if (exp & 1) {
-                    cnt = (cnt * base) % M;
-                }
-                base = (base * base) % M;
-            }
-
-            ret = (ret +  cnt) % M;
+            ret = (ret + power2[j - i]) % M;
         }
 
         return ret;
