31ab6fabc14d79cf94ed5a164b517efd010fa3e5
coolyuyuyu <coolyuyuyu@gmail.com>
2025-07-04 01:25:32 +0800

    3304: update solution

--- algorithm_3000-3999/3304-Find the K-th Character in String Game I/main.cpp
+++ algorithm_3000-3999/3304-Find the K-th Character in String Game I/main.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        --k;
-
         string s = "a";
-        while (k >= s.size()) {
+        while (s.size() < k) {
             for (int i = 0, n = s.size(); i < n; ++i) {
-                s += 'a' + ((s[i] - 'a') + 1) % 26;
+                s += ('a' + (s[i] - 'a' + 1) % 26);
             }
         }
 
-        return s[k];
+        return s[k - 1];
     }
 };
