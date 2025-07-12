f9504311a4ac53bb66a895f721f91b38c3a8332b
coolyuyuyu <coolyuyuyu@gmail.com>
2025-07-13 02:08:19 +0800

    1751: update solution

--- algorithm_1000-1999/1751-Maximum Number of Events That Can Be Attended II/main.cpp
+++ algorithm_1000-1999/1751-Maximum Number of Events That Can Be Attended II/main.cpp
@@ -3,10 +3,10 @@ public:
     int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
 
-        std::sort(events.begin(), events.end(), [](const auto& e1, const auto& e2) { return e1[1] < e2[1]; });
+        std::sort(events.begin(), events.end(), [](const auto& e1, const auto& e2){ return e1[1] < e2[1]; });
         events.insert(events.begin(), {0, 0, 0});
 
-        // dp[i][j]: the maximum sum of values such that attending j events from events[1:i]
+        // dp[i][j]: the maximum sum of values by attending j events from events[:i];
         int dp[n + 1][k + 1];
         dp[0][0] = 0;
         for (int i = 1; i <= n; ++i) {
@@ -19,10 +19,10 @@ public:
             auto itr = std::lower_bound(
                 events.begin(), events.begin() + i,
                 vector<int>{0, events[i][0], 0},
-                [](const auto& e1, const auto& e2) { return e1[1] < e2[1]; });
-            int x = std::distance(events.begin(), itr) - 1;
+                [](const auto& e1, const auto& e2){ return e1[1] < e2[1]; });
+            int x = std::distance(events.begin(), itr) - 1;;
             for (int j = 1; j <= k; ++j) {
-                // not attending events[i] V.S. attending events[i]
+                // do not attend events[i] V.S. do attend events[i]
                 dp[i][j] = std::max(dp[i - 1][j], dp[x][j - 1] + events[i][2]);
             }
         }
