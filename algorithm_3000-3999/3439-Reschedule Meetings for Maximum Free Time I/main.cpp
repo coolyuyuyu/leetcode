ca038188338f506849bfef470479f22fcf789894
coolyuyuyu <coolyuyuyu@gmail.com>
2025-07-13 02:08:37 +0800

    3439: update solution

--- algorithm_3000-3999/3439-Reschedule Meetings for Maximum Free Time I/main.cpp
+++ algorithm_3000-3999/3439-Reschedule Meetings for Maximum Free Time I/main.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
-        int n = startTime.size();
+        vector<int> frees;
+        frees.push_back(startTime.front());
+        for (int i = 1; i < startTime.size(); ++i) {
+            frees.push_back(startTime[i] - endTime[i - 1]);
+        }
+        frees.push_back(eventTime - endTime.back());
 
         int ret = 0;
 
-        vector<int> frees;
-        frees.push_back(startTime[0]);
-        for (int i = 1; i < n; ++i) {
-            frees.push_back(startTime[i] - endTime[i - 1]);
-        }
-        frees.push_back(eventTime - endTime[n - 1]);
-
         ++k;
-        for (int i = 0, sum = 0; i < frees.size(); ++i) {
+        for (int i = 0, n = frees.size(), sum = 0; i < n; ++i) {
             sum += frees[i];
-            if (i + 1 >= k) {
-                ret = std::max(ret, sum);
-                sum -= frees[i + 1 - k];
+            if (i >= k) {
+                sum -= frees[i - k];
             }
+
+            ret = std::max(ret, sum);
         }
 
         return ret;
