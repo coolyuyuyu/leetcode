12a0a43b9087c4f3ff59dd121734ee61b6b0c6a8
coolyuyuyu <coolyuyuyu@gmail.com>
2025-07-19 19:36:43 +0800

    1233: update solution

--- algorithm_1000-1999/1233-Remove Sub-Folders from the Filesystem/main.cpp
+++ algorithm_1000-1999/1233-Remove Sub-Folders from the Filesystem/main.cpp
@@ -1,41 +1,21 @@
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        vector<vector<string>> paths;
-        for (const string& s: folder) {
-            vector<string> names;
-            string name;
-            for (int i = 1; i <= s.size(); ++i) {
-                if (i == s.size() || s[i] == '/') {
-                    names.push_back(name);
-                    name.clear();
-                }
-                else {
-                    name += s[i];
-                }
-            }
-            paths.push_back(names);
-        }
-        std::sort(paths.begin(), paths.end());
-
+    vector<string> removeSubfolders(vector<string>& folders) {
         vector<string> ret;
-        for (int i = 0, n = paths.size(); i < n;) {
-            int j = i + 1;
-            for (; j < n && std::equal(paths[i].begin(), paths[i].end(), paths[j].begin()); ++j) {
-                ;
+
+        unordered_set<string_view> s(folders.begin(), folders.end());
+        for (const string& folder : folders) {
+            bool isSub = false;
+            for (int i = 1; i < folder.size(); ++i) {
+                    if (folder[i] == '/' && s.find(string_view(folder.c_str(), i)) != s.end()) {
+                    isSub = true;
+                }
             }
 
-            string s;
-            for (const string name : paths[i]) {
-                s += "/";
-                s += name;
+            if (!isSub) {
+                ret.push_back(folder);
             }
-            ret.push_back(s);
-
-            i = j;
         }
-
-
 
         return ret;
     }
