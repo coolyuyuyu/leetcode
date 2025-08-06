ee0e59ca2f90626de38a4c511d87046ccc1386b3
coolyuyuyu <coolyuyuyu@gmail.com>
2025-08-07 01:44:24 +0800

    3440: update solution

--- algorithm_3000-3999/3440-Reschedule Meetings for Maximum Free Time II/main.cpp
+++ algorithm_3000-3999/3440-Reschedule Meetings for Maximum Free Time II/main.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    // Time: O(NlogN), Space: O(N)
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-        int n = startTime.size();
+        int busyCnt = startTime.size(), freeCnt = startTime.size() + 1;
 
-        multiset<int> frees;
-        frees.insert(startTime.front());
-        for (int i = 1; i < n; ++i) {
-            frees.insert(startTime[i] - endTime[i - 1]);
+        int frees[freeCnt], freesMx2Rht[freeCnt];
+        for (int i = freeCnt; 0 < i--;) {
+            frees[i] = (i < busyCnt ? startTime[i] : eventTime) - (i > 0 ? endTime[i - 1] : 0);
+            freesMx2Rht[i] = std::max(i + 1 < freeCnt ? freesMx2Rht[i + 1] : 0, frees[i]);
         }
-        frees.insert(eventTime - endTime.back());
 
         int ret = 0;
-        for (int i = 0; i < n; ++i) {
-            int lft = startTime[i] - (i > 0 ? endTime[i - 1] : 0);
-            int rht = (i + 1 < n ? startTime[i + 1] : eventTime) - endTime[i];
-            int t = endTime[i] - startTime[i];
+        for (int i = 0, lftFreeMx = 0, rhtFreeMx; i < busyCnt; ++i) {
+            int busy = endTime[i] - startTime[i];
 
-            frees.erase(frees.find(lft));
-            frees.erase(frees.find(rht));
-            if (frees.lower_bound(t) != frees.end()) {
-                ret = std::max(ret, lft + t + rht);
+            lftFreeMx = std::max(lftFreeMx, i > 0 ? frees[i - 1] : 0);
+            rhtFreeMx = i + 2 < freeCnt ? freesMx2Rht[i + 2] : 0;
+            int moveoutFree = (i + 1 < busyCnt ? startTime[i + 1] : eventTime) - (i > 0 ? endTime[i - 1] : 0);
+            if (busy <= std::max(lftFreeMx, rhtFreeMx)) {
+                ret = std::max(ret, moveoutFree);
             }
-            else {
-                ret = std::max(ret, lft  + rht);
-            }
-            frees.insert(lft);
-            frees.insert(rht);
+
+            int pulloffFree = moveoutFree - busy;
+            ret = std::max(ret, pulloffFree);
         }
 
         return ret;
