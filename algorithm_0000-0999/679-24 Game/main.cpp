a689d152a81fa4a5f30426d220ff5f1a7d0667c5
coolyuyuyu <coolyuyuyu@gmail.com>
2025-08-20 00:05:23 +0800

    679: update solution

--- algorithm_0000-0999/679-24 Game/main.cpp
+++ algorithm_0000-0999/679-24 Game/main.cpp
@@ -8,18 +8,18 @@ public:
 
         std::function<unordered_set<double>(int, int)> f = [&](int i, int j) {
             if (i == j) {
-                return unordered_set<double>{nums[i]};
+                return unordered_set<double>({nums[i]});
             }
 
             unordered_set<double> ret;
             for (int k = i; k < j; ++k) {
-                for (double opd1 : f(i, k)) {
-                    for (double opd2 : f(k + 1, j)) {
-                        ret.insert(opd1 + opd2);
-                        ret.insert(opd1 - opd2);
-                        ret.insert(opd1 * opd2);
-                        if (opd2 != 0) {
-                            ret.insert(opd1 / opd2);
+                for (double a : f(i, k)) {
+                    for (double b : f(k + 1, j)) {
+                        ret.insert(a + b);
+                        ret.insert(a - b);
+                        ret.insert(a * b);
+                        if (b != 0.0) {
+                            ret.insert(a / b);
                         }
                     }
                 }
@@ -30,13 +30,12 @@ public:
 
         std::sort(nums.begin(), nums.end());
         do {
-            unordered_set<double> results = f(0, 3);
-            for (double result : results) {
-                if (std::abs(result - 24) <= 1e-10) {
+            for (double result : f(0, nums.size() - 1)) {
+                if (std::abs(result - 24) < 1e-10) {
                     return true;
                 }
             }
-        } while (next_permutation(nums.begin(), nums.end()));
+        } while (std::next_permutation(nums.begin(), nums.end()));
 
         return false;
     }
