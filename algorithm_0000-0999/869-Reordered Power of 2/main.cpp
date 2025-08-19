217aab2c7c1988f71427299636184447ec2bde1f
coolyuyuyu <coolyuyuyu@gmail.com>
2025-08-20 00:05:45 +0800

    869: update solution

--- algorithm_0000-0999/869-Reordered Power of 2/main.cpp
+++ algorithm_0000-0999/869-Reordered Power of 2/main.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    Solution() {
-        for (int n = 1; 0 <= n; n <<= 1) {
-            m_results.insert(count(n));
-        }
-    }
+    bool reorderedPowerOf2(int n) {
+        string y = std::to_string(n);
+        std::sort(y.begin(), y.end());
 
-    bool reorderedPowerOf2(int N) {
-        return m_results.find(count(N)) != m_results.end();
-    };
-
-private:
-    array<int, 10> count(int n) {
-        array<int, 10> result;
-        result.fill(0);
-
-        while (n) {
-            ++result[n % 10];
-            n /= 10;
+        for (int i = 0; i < 31; ++i) {
+            string x = std::to_string(1 << i);
+            std::sort(x.begin(), x.end());
+            if (x == y) {
+                return true;
+            }
         }
 
-        return result;
+        return false;
     }
-
-    set<array<int, 10>> m_results;
 };
