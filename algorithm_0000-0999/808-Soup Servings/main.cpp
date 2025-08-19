2c9844021389f14dd4b90c070463c6bf559d6dfc
coolyuyuyu <coolyuyuyu@gmail.com>
2025-08-20 00:05:34 +0800

    808: update solution

--- algorithm_0000-0999/808-Soup Servings/main.cpp
+++ algorithm_0000-0999/808-Soup Servings/main.cpp
@@ -1,74 +1,72 @@
 class Solution {
 public:
     double btmupDP(int n) {
-        if (5000 < n) {
-           return 1.0;
+        if (n > 5000) {
+            return 1.0;
         }
 
-        n = (n + 24) / 25;
+        n = (n + 25 - 1) / 25;
 
-        // dp[i][j]: the probability that soup A is (n - i) units and soup B is (n - j) units
+        // dp[a][b]: the probability that A is poured a units and B is poured b units.
         double dp[n + 1][n + 1];
-        for (int i = 0; i <= n; ++i) {
-            for (int j = 0; j <= n; ++j) {
-                dp[i][j] = 0.0;
-            }
-        }
-        dp[0][0] = 1.0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                dp[std::min(n, i + 4)][std::min(n, j + 0)] += dp[i][j] * 0.25;
-                dp[std::min(n, i + 3)][std::min(n, j + 1)] += dp[i][j] * 0.25;
-                dp[std::min(n, i + 2)][std::min(n, j + 2)] += dp[i][j] * 0.25;
-                dp[std::min(n, i + 1)][std::min(n, j + 3)] += dp[i][j] * 0.25;
+        for (int a = 0; a <= n; ++a) {
+            for (int b = 0; b <= n; ++b) {
+                dp[a][b] = 0;
             }
         }
 
-        double ret = 0.0;
-        for (int j = 0; j < n; ++j) {
-            ret += dp[n][j];
+        dp[0][0] = 1.0;
+        for (int a = 0; a < n; ++a) {
+            for (int b = 0; b < n; ++b) {
+                dp[std::min(a + 4, n)][std::min(b + 0, n)] += dp[a][b] * 0.25;
+                dp[std::min(a + 3, n)][std::min(b + 1, n)] += dp[a][b] * 0.25;
+                dp[std::min(a + 2, n)][std::min(b + 2, n)] += dp[a][b] * 0.25;
+                dp[std::min(a + 1, n)][std::min(b + 3, n)] += dp[a][b] * 0.25;
+            }
         }
-        ret += (dp[n][n]/ 2);
+
+        double ret = 0;
+        for (int b = 0; b < n; ++b) {
+            ret += dp[n][b];
+        }
+        ret += dp[n][n] * 0.5;
 
         return ret;
     }
 
-    double topdnRecursion(int n) {
-        if (5000 < n) {
-           return 1.0;
+    double topdnDP(int n) {
+        if (n > 5000) {
+            return 1.0;
         }
 
-        n = (n + 24) / 25;
-
-        // dp[i][j]: the probability that initially we have i ml of A and j ml of B
+        n = (n + 25 - 1) / 25;
         double dp[n + 1][n + 1];
-        for (int i = 0; i <= n; ++i) {
-            for (int j = 0; j <= n; ++j) {
-                dp[i][j] = -1.0;
+        for (int a = 0; a <= n; ++a) {
+            for (int b = 0; b <= n; ++b) {
+                dp[a][b] = -1.0;
             }
         }
         std::function<double(int, int)> f = [&](int a, int b) {
             if (a <= 0 && b > 0) {
                 return 1.0;
             }
-            else if (a > 0 && b <= 0) {
-                return 0.0;
-            }
-            else if (a <= 0 && b <= 0){
+            else if (a <= 0 && b <= 0) {
                 return 0.5;
+            }
+            else if (a >= 0 && b <= 0) {
+                return 0.0;
             }
 
             double& ret = dp[a][b];
-            if (ret >= 0.0) {
+            if (ret >= 0) {
                 return ret;
             }
 
-            ret = 0;
+            ret = 0.0;
             ret += f(a - 4, b - 0) * 0.25;
             ret += f(a - 3, b - 1) * 0.25;
             ret += f(a - 2, b - 2) * 0.25;
             ret += f(a - 1, b - 3) * 0.25;
-
             return ret;
         };
 
@@ -76,7 +74,7 @@ public:
     }
 
     double soupServings(int n) {
-        //return btmupDP(n);
-        return topdnRecursion(n);
+        return btmupDP(n);
+        //return topdnDP(n);
     }
 };
