1a74fba4f5a219afbeb15e0096a12ad9922c5825
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:19:36 +0800

    1504: update solution

--- algorithm_1000-1999/1504-Count Submatrices With All Ones/main.cpp
+++ algorithm_1000-1999/1504-Count Submatrices With All Ones/main.cpp
@@ -4,57 +4,57 @@ public:
     int bruteForce(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat.empty() ? 0 : mat[0].size();
 
-        auto numOneRow = [&n](int h[]) -> int {
+        auto countContinuousOne = [&n](int h[n]) {
             int ret = 0;
-            for (int i = 0, cnt = 0; i < n; ++i) {
-                cnt = (h[i] ? cnt + 1 : 0);
+            for (int c = 0, cnt = 0; c < n; ++c) {
+                cnt = h[c] == 1 ? (cnt + 1) : 0;
                 ret += cnt;
             }
-
             return ret;
         };
 
         int ret = 0;
         for (int top = 0; top < m; ++top) {
             int h[n];
-            for (int col = 0; col < n; ++col) {
-                h[col] = 1;
+            for (int c = 0; c < n; ++c) {
+                h[c] = 1;
             }
             for (int btm = top; btm < m; ++btm) {
-                for (int col = 0; col < n; ++col) {
-                    h[col] &= mat[btm][col];
+                for (int c = 0; c < n; ++c) {
+                    h[c] &= mat[btm][c];
                 }
-                ret += numOneRow(h);
+                ret += countContinuousOne(h);
             }
         }
 
         return ret;
     }
 
+    // Time: O(m * n)
     int monotonicStack(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat.empty() ? 0 : mat[0].size();
 
-        int h[n + 1];
-        for (int c = 0; c <= n; ++c) {
+        int h[n];
+        for (int c = 0; c < n; ++c) {
             h[c] = 0;
         }
 
         int ret = 0;
         for (int r = 0; r < m; ++r) {
             int cnt = 0;
-            stack<int> stk({0});
+            stack<int> stk({-1});
             for (int c = 0; c < n; ++c) {
-                h[c + 1] = (mat[r][c] == 1 ? h[c + 1] + 1 : 0);
-                while (!stk.empty() && h[stk.top()] > h[c + 1]) {
+                h[c] = mat[r][c] == 1 ? (h[c] + 1) : 0;
+
+                while (!stk.empty() && (stk.top() >= 0 ? h[stk.top()] : 0) > h[c]) {
                     int p1 = stk.top();
                     stk.pop();
                     int p2 = stk.top();
-
-                    cnt -= (h[p1] - h[c + 1]) * (p1 - p2);
+                    cnt -= (h[p1] - h[c]) * (p1 - p2);
                 }
-                stk.push(c + 1);
+                stk.push(c);
 
-                cnt += h[c + 1];
+                cnt += h[c];
                 ret += cnt;
             }
         }
@@ -63,7 +63,7 @@ public:
     }
 
     int numSubmat(vector<vector<int>>& mat) {
-        //return bruteForce(mat);
-        return monotonicStack(mat);
+        return bruteForce(mat);
+        //return monotonicStack(mat);
     }
 };
