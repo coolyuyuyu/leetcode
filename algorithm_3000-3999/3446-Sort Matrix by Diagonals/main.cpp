987a51e28c8785b4da355733e095c3af2105027d
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:20:15 +0800

    3446: update solution

--- algorithm_3000-3999/3446-Sort Matrix by Diagonals/main.cpp
+++ algorithm_3000-3999/3446-Sort Matrix by Diagonals/main.cpp
@@ -4,23 +4,21 @@ public:
         int n = grid.size();
         int nums[n];
 
-        for (int k = 1; k < n; ++k) {
-            int len = 0;
-            for (int r = 0, c = k;  c < n; ++r, ++c) {
-                nums[len++] = grid[r][c];
+        for (int k = 0; k < n; ++k) {
+            for (int r = k, c = 0, i = 0; r < n; ++r, ++c) {
+                nums[i++] = grid[r][c];
             }
-            sort(nums, nums + len);
-            for (int r = 0, c = k, i = 0; i < len; ++r, ++c, ++i) {
+            std::sort(nums, nums + (n - k), std::greater<int>());
+            for (int r = k, c = 0, i = 0; r < n; ++r, ++c, ++i) {
                 grid[r][c] = nums[i];
             }
         }
-        for (int k = 0; k < n; ++k) {
-            int len = 0;
-            for (int r = k, c = 0;  r < n; ++r, ++c) {
-                nums[len++] = grid[r][c];
+        for (int k = 1; k < n; ++k) {
+            for (int r = 0, c = k, i = 0; c < n; ++r, ++c) {
+                nums[i++] = grid[r][c];
             }
-            sort(nums, nums + len, std::greater<int>());
-            for (int r = k, c = 0, i = 0; i < len; ++r, ++c, ++i) {
+            std::sort(nums, nums + (n - k), std::less<int>());
+            for (int r = 0, c = k, i = 0; c < n; ++r, ++c, ++i) {
                 grid[r][c] = nums[i];
             }
         }
