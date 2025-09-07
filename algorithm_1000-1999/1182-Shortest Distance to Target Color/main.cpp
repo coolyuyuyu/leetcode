e26017c4277914b05974c8a1e45245327d3cb60f
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:18:55 +0800

    1182: update solution

--- algorithm_1000-1999/1182-Shortest Distance to Target Color/main.cpp
+++ algorithm_1000-1999/1182-Shortest Distance to Target Color/main.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
-        vector<vector<int>> distances(3, vector<int>(colors.size(), -1));
+        int n = colors.size();
 
-        // scan forward
-        size_t rhtMost[3] = {0, 0, 0};
-        for (size_t i = 0; i < colors.size(); ++i) {
-            int color = colors[i] - 1;
-            for (size_t j = rhtMost[color]; j <= i ; ++j) {
+        vector<vector<int>> distances(4, vector<int>(n, -1));
+
+        int rhtMost[] = {-1, 0, 0, 0};
+        for (int i = 0; i < n; ++i) {
+            int color = colors[i];
+            for (int j = rhtMost[color]; j <= i; ++j) {
                 distances[color][j] = i - j;
             }
             rhtMost[color] = i + 1;
         }
 
-        // scan backward
-        size_t lftMost[3] = {colors.size(), colors.size(), colors.size()};
-        for (size_t i = colors.size(); 0 < i--;) {
-            int color = colors[i] - 1;
-            for (size_t j = lftMost[color]; i < j--;) {
-                if (distances[color][j] == -1 || (j - i) < distances[color][j]) {
-                    distances[color][j] = j - i;
-                }
+        int lftMost[] = {-1, n - 1, n - 1, n - 1};
+        for (int i = n - 1; i >= 0; --i) {
+            int color = colors[i];
+            for (int j = lftMost[color]; j >= i; --j) {
+                if (distances[color][j] == -1 || (j - i) < distances[color][j])
+                distances[color][j] = j - i;
             }
-            lftMost[color] = i;
+            lftMost[color] = i - 1;
         }
 
-        vector<int> ans;
-        for (const vector<int>& query : queries) {
-            ans.push_back(distances[query[1] - 1][query[0]]);
+        int m = queries.size();
+        vector<int> ret(m);
+        for (int k = 0; k < m; ++k) {
+            int i = queries[k][0], c = queries[k][1];
+            ret[k] = distances[c][i];
         }
 
-        return ans;
+        return ret;
     }
 };
