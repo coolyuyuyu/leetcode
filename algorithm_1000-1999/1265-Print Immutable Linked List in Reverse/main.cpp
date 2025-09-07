6432c21496e76c07d294514a742ed6c85ae9402e
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:19:02 +0800

    1265: update solution

--- algorithm_1000-1999/1265-Print Immutable Linked List in Reverse/main.cpp
+++ algorithm_1000-1999/1265-Print Immutable Linked List in Reverse/main.cpp
@@ -10,13 +10,46 @@
 
 class Solution {
 public:
-    // Time: O(N), Space: O(N)
-    void printLinkedListInReverse(ImmutableListNode* head) {
-        if (!head) {
-            return;
+    // Time: O(n), Space: O(n)
+    void byDFS(ImmutableListNode* head) {
+        std::function<void(ImmutableListNode*)> f = [&](ImmutableListNode* head) {
+            if (!head) { return; }
+            f(head->getNext());
+            head->printValue();
+        };
+        f(head);
+    }
+
+    // Time: O(n), Space: O(sqrt(n))
+    void bySquareRootDecomposition(ImmutableListNode* head) {
+        int len = 0;
+        for (ImmutableListNode* p = head; p; p = p->getNext()) {
+            ++len;
         }
 
-        printLinkedListInReverse(head->getNext());
-        head->printValue();
+        std::function<void(ImmutableListNode*, int)> f = [&](ImmutableListNode* head, int len) {
+            if (!head || len == 0) { return; }
+            f(head->getNext(), --len);
+            head->printValue();
+        };
+
+        int bktSize = std::sqrt(len);
+        stack<ImmutableListNode*> stk;
+        ImmutableListNode* p = head;
+        for (int i = 0; i < len; ++i, p = p->getNext()) {
+            if ((i % bktSize)== 0) {
+                cout << i << endl;
+                stk.push(p);
+            }
+        }
+
+        for (; !stk.empty(); stk.pop()) {
+            f(stk.top(), bktSize);
+        }
+    }
+
+    void printLinkedListInReverse(ImmutableListNode* head) {
+        //return byDFS(head);
+        return bySquareRootDecomposition(head);
     }
 };
