8f461f658227a722954c6351c3b8fd574c656a84
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:19:42 +0800

    1792: update solution

--- algorithm_1000-1999/1792-Maximum Average Pass Ratio/main.cpp
+++ algorithm_1000-1999/1792-Maximum Average Pass Ratio/main.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        std::function<bool(const pair<int, int>&, const pair<int, int>&)> cmp = [](const pair<int, int>& p1, const pair<int, int>& p2) {
+        std::function<bool(const pair<int, int>&, const pair<int, int>&)> comp = [](const pair<int, int>& p1, const pair<int, int>& p2) {
             const auto& [pass1, total1] = p1;
             const auto& [pass2, total2] = p2;
-            return std::less<>()((1.0 * pass1 + 1) / (total1 + 1) - (1.0 * pass1 / total1), (1.0 * pass2 + 1) / (total2 + 1) - 1.0 * pass2 / total2);
+            return (1.0 * (pass1 + 1) / (total1 + 1)) - (1.0 * pass1 / total1) < (1.0 * (pass2 + 1) / (total2 + 1)) - (1.0 * pass2 / total2);
         };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-        for (const auto& c : classes) {
-            int pass = c[0], total = c[1];
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+        for (const auto& cls : classes) {
+            int pass = cls[0], total = cls[1];
             pq.emplace(pass, total);
         }
 
+        double sum = 0.0;
         for (int i = 0; i < extraStudents; ++i) {
             auto [pass, total] = pq.top();
             pq.pop();
             pq.emplace(pass + 1, total + 1);
         }
 
-        double sum = 0;
-        while (!pq.empty()) {
-            auto [pass, total] = pq.top();
-            pq.pop();
-            sum += (1.0 * pass / total);
+        for (; !pq.empty(); pq.pop()) {
+            const auto& [pass, total] = pq.top();
+            sum += 1.0 * pass / total;
         }
 
         return sum / classes.size();
