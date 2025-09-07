0c4ee183b643e5368ec471ffe0d968271e126df8
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:19:08 +0800

    1277: update solution

--- algorithm_1000-1999/1277-Count Square Submatrices with All Ones/main.cpp
+++ algorithm_1000-1999/1277-Count Square Submatrices with All Ones/main.cpp
@@ -3,23 +3,25 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix.empty() ? 0 : matrix[0].size();
 
-        // dp[r][c]: the length of maximal squre where bottom-right cornert at (r, c);
+        // dp[r][c]: the length of maximal square whose bottom-right corner at (r,c)
         int dp[m][n];
         int ret = 0;
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
+        ret += (dp[0][0] = matrix[0][0]);
+        for (int c = 1; c < n; ++c) {
+            ret += (dp[0][c] = matrix[0][c]);
+        }
+        for (int r = 1; r < m; ++r) {
+            ret += (dp[r][0] = matrix[r][0]);
+        }
+        for (int r = 1; r < m; ++r) {
+            for (int c = 1; c < n; ++c) {
                 if (matrix[r][c] == 0) {
                     dp[r][c] = 0;
-                    continue;
-                }
-
-                if (r == 0 || c == 0) {
-                    dp[r][c] = 1;
                 }
                 else {
                     dp[r][c] = std::min({dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]}) + 1;
+                    ret += dp[r][c];
                 }
-                ret += dp[r][c];
             }
         }
 
