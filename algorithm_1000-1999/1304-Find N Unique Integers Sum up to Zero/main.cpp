9a468b8c2d420cfc50fdfad9ae0af20e7f0b3971
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-08 01:19:14 +0800

    1304: update solution

--- algorithm_1000-1999/1304-Find N Unique Integers Sum up to Zero/main.cpp
+++ algorithm_1000-1999/1304-Find N Unique Integers Sum up to Zero/main.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     vector<int> sumZero(int n) {
-        if (n == 2) {
-            return {1, -1};
+        vector<int> ret;
+        for (int i = 1; i < n; ++i) {
+            ret.push_back(i);
         }
+        ret.push_back(- (n * (n - 1) / 2));
 
-        vector<int> nums;
-
-        int sum = 0;
-        for (int i = 0; (i + 1) < n; ++i) {
-            nums.push_back(i);
-            sum += i;
-        }
-        nums.push_back(-sum);
-
-        return nums;
+        return ret;
     }
 };
