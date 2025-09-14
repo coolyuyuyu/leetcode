c489a045c97a98c8639277d56ab3677ffc55b456
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-15 01:47:51 +0800

    966: update solution

--- algorithm_0000-0999/966-Vowel Spellchecker/main.cpp
+++ algorithm_0000-0999/966-Vowel Spellchecker/main.cpp
@@ -1,53 +1,48 @@
 class Solution {
 public:
     vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+        std::function<char(char)> f1 = [](char c) {
+            return toupper(c);
+        };
+        std::function<char(char)> f2 = [](char c) {
+            return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') ? '*' : c;
+        };
+
         unordered_set<string> words(wordlist.begin(), wordlist.end());
-        unordered_map<string, string> capitlization;
+        unordered_map<string, string> capitalization;
         unordered_map<string, string> vowelerror;
         for (const string& word : wordlist) {
-            string key = word;
-            for (auto& c : key) {
-                if (std::isupper(c)) {
-                    c = std::tolower(c);
-                }
-            }
-            capitlization.emplace(key, word);
+            words.insert(word);
 
-            for (auto& c : key) {
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    c = '*';
-                }
-            }
-            vowelerror.emplace(key, word);
+            string key1 = word;
+            std::transform(key1.begin(), key1.end(), key1.begin(), f1);
+            capitalization.emplace(key1, word);
+
+            string key2 = key1;
+            std::transform(key2.begin(), key2.end(), key2.begin(), f2);
+            vowelerror.emplace(key2, word);
         }
 
         int n = queries.size();
         vector<string> ret(n);
         for (int i = 0; i < n; ++i) {
-            string query = queries[i];
-
-            if (words.find(query) != words.end()) {
-                ret[i] = query;
+            if (words.find(queries[i]) != words.end()) {
+                ret[i] = queries[i];
                 continue;
             }
 
-            for (auto& c : query) {
-                if (std::isupper(c)) {
-                    c = std::tolower(c);
-                }
-            }
-            if (capitlization.find(query) != capitlization.end()) {
-                ret[i] = capitlization[query];
+            string key1 = queries[i];
+            std::transform(key1.begin(), key1.end(), key1.begin(), f1);
+            if (capitalization.find(key1) != capitalization.end()) {
+                ret[i] = capitalization[key1];
                 continue;
             }
 
-            for (auto& c : query) {
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    c = '*';
-                }
-            }
-            if (vowelerror.find(query) != vowelerror.end()) {
-                ret[i] = vowelerror[query];
+            string key2 = key1;
+            std::transform(key2.begin(), key2.end(), key2.begin(), f2);
+            if (vowelerror.find(key2) != vowelerror.end()) {
+                ret[i] = vowelerror[key2];
+                continue;
             }
         }
 
