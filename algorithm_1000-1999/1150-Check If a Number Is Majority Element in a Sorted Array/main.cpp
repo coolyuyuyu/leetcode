553f3328a20b41548628eaf8a918e42486cca060
coolyuyuyu <coolyuyuyu@gmail.com>
2025-09-24 02:28:05 +0800

    1150: update solution

--- algorithm_1000-1999/1150-Check If a Number Is Majority Element in a Sorted Array/main.cpp
+++ algorithm_1000-1999/1150-Check If a Number Is Majority Element in a Sorted Array/main.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool isMajorityElement(vector<int>& nums, int target) {
-        auto itr = std::lower_bound(nums.begin(), nums.end(), target);
-        size_t count = std::distance(itr, std::upper_bound(itr, nums.end(), target));
+        auto itrLo = std::lower_bound(nums.begin(), nums.end(), target);
+        auto itrHi = std::upper_bound(itrLo, nums.end(), target);
+        int count = std::distance(itrLo, itrHi);
         return nums.size() < (count * 2);
     }
 };
