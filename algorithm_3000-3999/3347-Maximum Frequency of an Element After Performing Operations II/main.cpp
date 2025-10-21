c5d6316276f6912496daec1a2e0d4bc30b452aeb
coolyuyuyu <coolyuyuyu@gmail.com>
2025-10-22 02:10:47 +0800

    3347: update solution

--- algorithm_3000-3999/3347-Maximum Frequency of an Element After Performing Operations II/main.cpp
+++ algorithm_3000-3999/3347-Maximum Frequency of an Element After Performing Operations II/main.cpp
@@ -7,25 +7,22 @@ public:
 
         // change num into existing element of nums
         unordered_map<int, int> cnts;
-        for (int i = 0, lft = 0, rht = 0, n = nums.size(); i < n; ++i) {
+        for (int i = 0, n = nums.size(), lft = 0, rht = 0; i < n; ++i) {
             while (rht < n && nums[i] + k >= nums[rht]) {
                 ++cnts[nums[rht]];
                 ++rht;
-
             }
-            while (lft < n && nums[lft] + k < nums[i]) {
+            while (nums[lft] + k < nums[i]) {
                 ++lft;
             }
-
-            ret = std::max(ret, std::min(rht - lft, cnts[nums[i]] +numOperations));
+            ret = std::max(ret, std::min(rht - lft, cnts[nums[i]] + numOperations));
         }
 
         // change num into non-existing element of nums
         for (int i = 0, j = 0, n = nums.size(); i < n; ++i) {
-            while (j < n && (nums[j] - nums[i] <= 2 * k)) {
+            while (j < n && nums[j] - nums[i] <= 2 * k) {
                 ++j;
             }
-
             ret = std::max(ret, std::min(j - i, numOperations));
         }
 
