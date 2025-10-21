39ffb05c697a6360c60b3250c8069212c9b513fb
coolyuyuyu <coolyuyuyu@gmail.com>
2025-10-22 02:10:20 +0800

    2273: update solution

--- algorithm_2000-2999/2273-Find Resultant Array After Removing Anagrams/main.cpp
+++ algorithm_2000-2999/2273-Find Resultant Array After Removing Anagrams/main.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        vector<string> ans;
+        std::function<vector<int>(const string&)> f = [](const string& word) {
+            vector<int> cnts(26, 0);
+            for (char c : word) {
+                ++cnts[c - 'a'];
+            }
+            return cnts;
+        };
 
-        string prev;
-        for (const auto& word : words) {
-            string tmp = word;
-            std::sort(tmp.begin(), tmp.end());
-            if (tmp != prev) {
-               ans.push_back(word);
-               prev = tmp;
-           }
+        vector<string> ret;
+
+        vector<int> preCnts;
+        for (int i = 0, n = words.size(); i < n; ++i) {
+            vector<int> curCnts = f(words[i]);
+            if (curCnts != preCnts) {
+                ret.push_back(words[i]);
+                preCnts = curCnts;
+            }
         }
 
-        return ans;
+        return ret;
     }
 };
