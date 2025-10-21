88084a03a3b417d723ed5d1a78711d4015dd2879
coolyuyuyu <coolyuyuyu@gmail.com>
2025-10-22 02:09:54 +0800

    778: update solution

--- algorithm_0000-0999/778-Swim in Rising Water/main.cpp
+++ algorithm_0000-0999/778-Swim in Rising Water/main.cpp
@@ -1,162 +1,42 @@
 class Solution {
 public:
-    class DisjointSet {
-    public:
-        DisjointSet(int n)
-            : m_len(n)
-            , m_parents(m_len * m_len) {
-            for (int i = 0; i < m_len * m_len; ++i) {
-                m_parents[i] = i;
-            }
-        }
-
-        inline int findByPos(int r, int c) {
-            return find(getIndex(r, c));
-        }
-
-        inline void mergeByPos(int r1, int c1, int r2, int c2) {
-            merge(getIndex(r1, c1), getIndex(r2, c2));
-        }
-
-    private:
-        inline int getIndex(int r, int c) {
-            return r * m_len + c;
-        }
-
-        int find(int i) {
-            if (m_parents[i] != i) {
-                m_parents[i] = find(m_parents[i]);
-            }
-
-            return m_parents[i];
-        }
-
-        void merge(int i, int j) {
-            int rootI = find(i);
-            int rootJ = find(j);
-            if (rootI != rootJ) {
-                m_parents[rootI] = rootJ;
-            }
-        }
-
-        int m_len;
-        vector<int> m_parents;
-    };
-
-    class Elem {
-    public:
-        Elem() {
-        }
-
-        Elem(int t, int r, int c)
-            : time(t)
-            , row(r)
-            , col(c) {
-        }
-
-        bool operator<(const Elem& elem) {
-            return time < elem.time;
-        }
-
-        int time;
-        int row;
-        int col;
-    };
-
-    struct ElemComp{
-        bool operator()(const Elem& a, const Elem& b){
-            return a.time > b.time;
-        }
-    };
-
-    int swimInWaterDisjointSet(vector<vector<int>>& grid) {
-        priority_queue<Elem, vector<Elem>, ElemComp> pq;
-        size_t n = grid.size();
-        for (int r = 0; r < n; ++r) {
-            for (int c = 0; c < n; ++c) {
-                pq.emplace(grid[r][c], r, c);
-            }
-        }
-
-        int t = 0;
-        DisjointSet ds(n * n);
-        while (ds.findByPos(0, 0) != ds.findByPos(n - 1, n - 1)) {
-            Elem elem = pq.top();
-            pq.pop();
-
-            t = elem.time;
-            int r = elem.row, c = elem.col;
-
-            //ltf
-            if (0 < c && grid[r][c - 1] <= t) {
-                ds.mergeByPos(r, c, r, c - 1);
-            }
-
-            //top
-            if (0 < r && grid[r - 1][c] <= t) {
-                ds.mergeByPos(r, c, r - 1, c);
-            }
-
-            //rht
-            if (c + 1 < n && grid[r][c + 1] <= t) {
-                ds.mergeByPos(r, c, r, c + 1);
-            }
-
-            //btm
-            if (r + 1 < n && grid[r + 1][c] <= t) {
-                ds.mergeByPos(r, c, r + 1, c);
-            }
-        }
-
-        return t;
-    }
-
-    int swimInWaterTraversal(vector<vector<int>>& grid) {
-        size_t n = grid.size();
-        vector<vector<bool>> seen(n, vector<bool>(n, false));
-
-        int t = 0;
-        priority_queue<Elem, vector<Elem>, ElemComp> pq;
-        pq.emplace(grid[0][0], 0, 0);
-        while (!pq.empty() && !seen[n - 1][n - 1]) {
-            Elem elem = pq.top();
-            pq.pop();
-
-            int r = elem.row, c = elem.col;
-            if (seen[r][c]) {
-                continue;
-            }
-
-            seen[r][c] = true;
-            t = max(t, elem.time);
-
-            //ltf
-            if (0 < c) {
-                pq.emplace(grid[r][c - 1], r, c - 1);
-            }
-
-            //top
-            if (0 < r) {
-                pq.emplace(grid[r - 1][c], r - 1, c);
-            }
-
-            //rht
-            if (c + 1 < n) {
-                pq.emplace(grid[r][c + 1], r, c + 1);
-            }
-
-            //btm
-            if (r + 1 < n) {
-                pq.emplace(grid[r + 1][c], r + 1, c);
-            }
-        }
-
-        return t;
-    }
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 
     int swimInWater(vector<vector<int>>& grid) {
-        //return swimInWaterDisjointSet(grid);
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
-        return swimInWaterTraversal(grid);
+        bool visited[m][n];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                visited[r][c] = false;
+            }
+        }
+
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, std::greater<>> minPQ;
+        minPQ.emplace(grid[0][0], 0, 0);
+
+        int ret = INT_MIN;
+        while (!minPQ.empty()) {
+            auto [t, r, c] = minPQ.top();
+            minPQ.pop();
+
+            if (visited[r][c]) { continue; }
+            visited[r][c] = true;
+
+            ret = std::max(ret, t);
+            if (r == m - 1 && c == n - 1) {
+                break;
+            }
+
+            for (const auto& [dr, dc] : dirs) {
+                int x = r + dr, y = c + dc;
+                if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
+                if (visited[x][y]) { continue; }
+
+                minPQ.emplace(grid[x][y], x, y);
+            }
+        }
+
+        return ret;
     }
 };
