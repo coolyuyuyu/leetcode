4cae22c5bbe5695080502a2109cb01e8933a0460
coolyuyuyu <coolyuyuyu@gmail.com>
2025-10-22 02:10:07 +0800

    1518: update solution

--- algorithm_1000-1999/1518-Water Bottles/main.cpp
+++ algorithm_1000-1999/1518-Water Bottles/main.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int ret = 0;
-        for (int numEmpties = 0; numBottles; numEmpties = numEmpties % numExchange) {
+        for (int numEmpties = 0; numBottles; numBottles = numEmpties / numExchange, numEmpties = numEmpties % numExchange) {
             ret += numBottles;
 
             numEmpties += numBottles;
-            numBottles = numEmpties / numExchange;
+            numBottles = 0;
         }
 
         return ret;
