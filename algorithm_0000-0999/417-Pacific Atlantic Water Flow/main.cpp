29d35e9f7e52284810155ff2fa068939e2e0776f
coolyuyuyu <coolyuyuyu@gmail.com>
2025-10-22 02:09:45 +0800

    417: update solution

--- algorithm_0000-0999/417-Pacific Atlantic Water Flow/main.cpp
+++ algorithm_0000-0999/417-Pacific Atlantic Water Flow/main.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
         int m = heights.size(), n = heights.empty() ? 0 : heights[0].size();
-        vector<vector<bool>> pac(m, vector<bool>(n, false)), atl(m, vector<bool>(n, false));
-        std::function<void(vector<vector<bool>>&, int, int)> bfs = [&](vector<vector<bool>>& sea, int r, int c) {
+
+        std::function<void(int, int, vector<vector<bool>>&)> bfs = [&](int r, int c, vector<vector<bool>>& sea) {
             if (sea[r][c]) { return; }
 
-            queue<pair<int, int>> q;
-            sea[r][c] = true;
-            q.emplace(r, c);
+            queue<pair<int, int>> q({{r, c}});
             while (!q.empty()) {
-                const auto [r, c] = q.front();
+                auto [r, c] = q.front();
                 q.pop();
+
+                if (sea[r][c]) { continue; }
+                sea[r][c] = true;
 
                 for (const auto& [dr, dc] : dirs) {
                     int x = r + dr, y = c + dc;
-                    if (x < 0 || m <= x || y < 0 || n <= y) { continue; }
-                    if (sea[x][y]) { continue; }
+                    if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
                     if (heights[x][y] < heights[r][c]) { continue; }
+                    if (sea[x][y]) { continue; }
 
-                    sea[x][y] = true;
                     q.emplace(x, y);
                 }
             }
+
         };
+
+        vector<vector<bool>> pac(m, vector<bool>(n, false)), atl(m, vector<bool>(n, false));
         for (int r = 0; r < m; ++r) {
-            bfs(pac, r, 0);
-            bfs(atl, r, n - 1);
+            bfs(r, 0, pac);
+            bfs(r, n - 1, atl);
         }
         for (int c = 0; c < n; ++c) {
-            bfs(pac, 0, c);
-            bfs(atl, m - 1, c);
+            bfs(0, c, pac);
+            bfs(m - 1, c, atl);
         }
 
         vector<vector<int>> ret;
