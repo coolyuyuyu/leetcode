5d434a4e57c2c12d0fefbbc932054b20d7c764cb
coolyuyuyu <coolyuyuyu@gmail.com>
2025-11-05 18:12:51 +0800

    1578: update solution

--- algorithm_1000-1999/1578-Minimum Time to Make Rope Colorful/main.cpp
+++ algorithm_1000-1999/1578-Minimum Time to Make Rope Colorful/main.cpp
@@ -6,20 +6,18 @@ public:
         colors.insert(colors.begin(), '#');
         neededTime.insert(neededTime.begin(), 0);
 
-        // dp[i][j]: minimum time needed to make the colors[1:i] colorful and the remaining ending color is j
+        // dp[i][c]: the minimum needed to make the rope[1:i] colorful and ending color is c.
         int dp[n + 1][27];
-        dp[0][26] = 0;
-        for (int j = 1; j <= 26; ++j) {
-            dp[0][j] = INT_MAX / 2;
+        for (int c = 0; c < 26; ++c) {
+            dp[0][c] = INT_MAX / 2;
         }
+        dp[0][26] = 0;
         for (int i = 1; i <= n; ++i) {
             for (int c = 0; c <= 26; ++c) {
-                dp[i][c] = INT_MAX / 2;
-
-                dp[i][c] = std::min(dp[i][c], dp[i - 1][c] + neededTime[i]); // delete colors[i]
+                dp[i][c] = dp[i - 1][c] + neededTime[i]; // delete colors[i]
                 if (colors[i] == 'a' + c) { // keep colors[i]
                     for (int d = 0; d <= 26; ++d) {
-                        if (c == d) { continue; }
+                        if (d == c) { continue; }
                         dp[i][c] = std::min(dp[i][c], dp[i - 1][d]);
                     }
                 }
@@ -27,27 +25,22 @@ public:
         }
 
         int ret = INT_MAX;
-        for (char j = 0; j <= 26; ++j) {
-            ret = std::min(ret, dp[n][j]);
+        for (int c = 0; c < 26; ++c) {
+            ret = std::min(ret, dp[n][c]);
         }
 
         return ret;
     }
 
     int byGreedy(string colors, vector<int>& neededTime) {
-        int n = colors.size();
-
         int ret = 0;
-        for (int i = 0; i < n;) {
-            int sum = 0, mx = 0;
-            int j = i;
-            while (j < n && colors[i] == colors[j]) {
-                sum += neededTime[j];
+        for (int i = 0, n = colors.size(); i < n;) {
+            int j = i, mx = 0;
+            for (; j < n && colors[i] == colors[j]; ++j) {
+                ret += neededTime[j];
                 mx = std::max(mx, neededTime[j]);
-                ++j;
             }
-
-            ret += sum - mx;
+            ret -= mx;
 
             i = j;
         }
@@ -56,7 +49,7 @@ public:
     }
 
     int minCost(string colors, vector<int>& neededTime) {
-        //return byDP(colors, neededTime);
-        return byGreedy(colors, neededTime);
+        return byDP(colors, neededTime);
+        //return byGreedy(colors, neededTime);
     }
 };
