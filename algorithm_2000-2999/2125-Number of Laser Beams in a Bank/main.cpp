a9817828b2684f693dbc745cef78d60bb0f07353
coolyuyuyu <coolyuyuyu@gmail.com>
2025-11-05 18:13:01 +0800

    2125: update solution

--- algorithm_2000-2999/2125-Number of Laser Beams in a Bank/main.cpp
+++ algorithm_2000-2999/2125-Number of Laser Beams in a Bank/main.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int n = bank.size();
-
         int ret = 0;
-        for (int i = 0, preNonZeroCnt = 0; i < n; ++i) {
-            int curNonZeroCnt = std::count(bank[i].begin(), bank[i].end(), '1');
-            if (curNonZeroCnt > 0 ) {
-                ret += curNonZeroCnt * preNonZeroCnt;
-                preNonZeroCnt = curNonZeroCnt;
+        for (int i = 0, n = bank.size(), preCnt = 0; i < n; ++i) {
+            int curCnt = std::count(bank[i].begin(), bank[i].end(), '1');
+            if (curCnt) {
+                ret += preCnt * curCnt;
+                preCnt = curCnt;
             }
         }
 
