fdd2b569b56ee402c76ade60735a783c311631fc
coolyuyuyu <coolyuyuyu@gmail.com>
2025-11-25 01:59:35 +0800

    2536: update solution

--- algorithm_2000-2999/2536-Increment Submatrices by One/main.cpp
+++ algorithm_2000-2999/2536-Increment Submatrices by One/main.cpp
@@ -1,126 +1,42 @@
 class Solution {
 public:
-    template<typename T>
-    class Diff1D {
-    public:
-        Diff1D(size_t count)
-            : diffs(count, 0) {
-        }
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        int m = n;
 
-        void update(size_t lft, size_t rht, T delta) {
-            diffs[lft] += delta;
-            if ((rht + 1) < size()) {
-                diffs[rht + 1] -= delta;
-            }
-        }
+        int diffs[m][n];
+        std::fill(&diffs[0][0], &diffs[0][0] + m * n, 0);
 
-        size_t size() const {
-            return diffs.size();
-        }
-
-        template<typename OutputIterator>
-        void construct(OutputIterator itr) {
-            T sum = 0;
-            for (T diff : diffs) {
-                sum += diff;
-                *itr++ = sum;
-            }
-        }
-
-    private:
-        vector<T> diffs;
-    };
-
-    vector<vector<int>> byDiff1D(int n, vector<vector<int>>& queries) {
-        vector<Diff1D<int>> diffs(n, Diff1D<int>(n));
         for (const auto& query : queries) {
-            for (int r = query[0]; r <= query[2]; ++r) {
-                diffs[r].update(query[1], query[3], 1);
+            int t = query[0], l = query[1], b = query[2], r = query[3];
+            diffs[t][l] += 1;
+            if (b + 1 < m) {
+                diffs[b + 1][l] -= 1;
+            }
+            if (r + 1 < m) {
+                diffs[t][r + 1] -= 1;
+            }
+            if (b + 1 < m && r + 1 < n) {
+                diffs[b + 1][r + 1] += 1;
             }
         }
 
-        vector<vector<int>> nums(n, vector<int>(n));
-        for (int r = 0; r < n; ++r) {
-            diffs[r].construct(nums[r].begin());
-        }
-        return nums;
-    }
-
-    template<typename T>
-    class Diff2D {
-    public:
-        Diff2D(size_t rowCount, size_t colCount)
-            : diffs(rowCount, vector<T>(colCount, 0)) {
-        }
-
-        void update(size_t lft, size_t rht, T delta) {
-            diffs[lft] += delta;
-            if ((rht + 1) < size()) {
-                diffs[rht + 1] -= delta;
-            }
-        }
-
-        void update(size_t rowTL, size_t colTL, size_t rowBR, size_t colBR, T delta) {
-            diffs[rowTL][colTL] += delta;
-            if ((rowBR + 1) < rowCount()) {
-                diffs[rowBR + 1][colTL] -= delta;
-            }
-            if ((colBR + 1) < colCount()) {
-                diffs[rowTL][colBR + 1] -= delta;
-            }
-            if ((rowBR + 1) < rowCount() && (colBR + 1) < colCount()) {
-                diffs[rowBR + 1][colBR + 1] += delta;
-            }
-        }
-
-        size_t rowCount() const {
-            return diffs.size();
-        }
-
-        size_t colCount() const {
-            return diffs.empty() ? 0 : diffs[0].size();
-        }
-
-        size_t size() const {
-            return rowCount() * colCount();
-        }
-
-        template<typename OutputIterator>
-        void construct(OutputIterator itr) {
-            for (size_t r = 0; r < rowCount(); ++r) {
-                for (size_t c = 0; c < colCount(); ++c) {
-                    auto itrNum = (itr + r)->begin() + (c);
-                    *itrNum = diffs[r][c];
-                    if (0 < r) {
-                        *itrNum += (itr + r - 1)->at(c);
-                    }
-                    if (0 < c) {
-                        *itrNum += (itr + r)->at(c - 1);
-                    }
-                    if (0 < r && 0 < c) {
-                        *itrNum -= (itr + r - 1)->at(c - 1);
-                    }
+        vector<vector<int>> ret(m, vector<int>(n));
+        ret[0][0] = diffs[0][0];
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                ret[r][c] = diffs[r][c];
+                if (r > 0) {
+                    ret[r][c] += ret[r - 1][c];
+                }
+                if (c > 0) {
+                    ret[r][c] += ret[r][c - 1];
+                }
+                if (r > 0 && c > 0) {
+                    ret[r][c] -= ret[r - 1][c - 1];
                 }
             }
         }
 
-    private:
-        vector<vector<T>> diffs;
-    };
-
-    vector<vector<int>> byDiff2D(int n, vector<vector<int>>& queries) {
-        Diff2D<int> diffs(n, n);
-        for (const auto& query : queries) {
-            diffs.update(query[0], query[1], query[2], query[3], 1);
-        }
-
-        vector<vector<int>> nums(n, vector<int>(n));
-        diffs.construct(nums.begin());
-        return nums;
-    }
-
-    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        //return byDiff1D(n, queries);
-        return byDiff2D(n, queries);
+        return ret;
     }
 };
