72002a05b9b7bda7bf7cb9ce1754e9d0a7f29b9c
coolyuyuyu <coolyuyuyu@gmail.com>
2025-11-25 01:59:51 +0800

    3234: update solution

--- algorithm_3000-3999/3234-Count the Number of Substrings With Dominant Ones/main.cpp
+++ algorithm_3000-3999/3234-Count the Number of Substrings With Dominant Ones/main.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // the length of s <= 4 * 10^4
-        // => the maximum number of 0s of a valid substring is 200
+        // s.length <= 4 * 1e4
+        // => the maximum number of 0 is 200
 
         int n = s.size();
+        int total0 = std::count(s.begin(), s.end(), '0'), total1 = n - total0;
 
         int cntRhtConsec1[n + 1];
         cntRhtConsec1[n] = 0;
         for (int i = n; 0 < i--;) {
-            if (s[i] == '1') {
-                cntRhtConsec1[i] = cntRhtConsec1[i + 1] + 1;
-            }
-            else {
-                cntRhtConsec1[i] = 0;
-            }
+            cntRhtConsec1[i] = s[i] == '0' ? 0 : (cntRhtConsec1[i + 1] + 1);
         }
 
         int ret = 0;
-        for (int m = 1; m <= 200; ++m) {
-            for (int lft = 0, rht = 0, cnt0 = 0; lft < n; ++lft) {
-                for (; rht < n && cnt0 < m; ++rht) {
-                    cnt0 += (s[rht] == '0' ? 1 : 0);
+
+        // substring of all 1's
+        for (int i = 0; i < n; ++i) {
+            ret += cntRhtConsec1[i];
+        }
+
+        for (int m = 1; m <= std::min(200, total0); ++m) {
+            for (int l = 0, r = 0, cnt0 = 0; l < n; ++l) {
+                for (; r < n && cnt0 < m; ++r) {
+                    cnt0 += (s[r] == '0' ? 1 : 0);
                 }
                 if (cnt0 < m) { break; }
 
                 // ... [0 ... 0] 1 1 1 ...
                 //      L        R
 
-                int len = rht - lft;
-                int cnt1 = len - m;
-                if (cntRhtConsec1[rht] + cnt1 >= cnt0 * cnt0) {
-                    int extra = cntRhtConsec1[rht] - std::max(cnt0 * cnt0 - cnt1, 0);
+                int len = r - l;
+                int cnt1 = len - cnt0;
+                if (cntRhtConsec1[r] + cnt1 >= cnt0 * cnt0) {
+                    int extra = cntRhtConsec1[r] - std::max(cnt0 * cnt0 - cnt1, 0);
                     ret += std::max(extra + 1, 0);
                 }
 
-                cnt0 -= (s[lft] == '0' ? 1 : 0);
+                cnt0 -= (s[l] == '0' ? 1 : 0);
             }
         }
 
-        for (int i = 0; i < n; ++i) {
-            ret += cntRhtConsec1[i];
-        }
 
         return ret;
     }
