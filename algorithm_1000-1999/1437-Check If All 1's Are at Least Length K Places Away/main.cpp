d3b7abb8b5308f7525632f53c7dc4862fe7147c7
coolyuyuyu <coolyuyuyu@gmail.com>
2025-11-25 01:59:27 +0800

    1437: update solution

--- algorithm_1000-1999/1437-Check If All 1's Are at Least Length K Places Away/main.cpp
+++ algorithm_1000-1999/1437-Check If All 1's Are at Least Length K Places Away/main.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        if (k == 0) {
-            return true;
-        }
-
-        size_t pre = std::find(nums.begin(), nums.end(), 1) - nums.begin();
-        for (size_t i = pre + 1; i < nums.size(); ++i) {
+        for (int i = 0, n = nums.size(), pre = -k - 1; i < n; ++i) {
             if (nums[i] == 1) {
-                if ((i - pre - 1) < k) {
+                if (i - pre - 1 < k) {
                     return false;
-                }
+                } 
                 pre = i;
             }
         }
