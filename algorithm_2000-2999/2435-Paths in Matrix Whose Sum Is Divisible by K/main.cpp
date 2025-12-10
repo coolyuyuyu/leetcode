efd00928ca94e1815d4bb435c100d28f6372761b
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-11 01:18:02 +0800

    2435: update solution

--- algorithm_2000-2999/2435-Paths in Matrix Whose Sum Is Divisible by K/main.cpp
+++ algorithm_2000-2999/2435-Paths in Matrix Whose Sum Is Divisible by K/main.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int M = 1e9 + 7;
 
-    int numberOfPaths(vector<vector<int>>& grid, int K) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<long long>>> dp(m, vector<vector<long long>>(n, vector<long long>(K, 0)));
-        dp[0][0][grid[0][0] % K] = 1;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                for (int k = 0; k < K; ++k) {
-                    int t = ((k - grid[i][j]) % K + K) % K;
-                    if (0 < i) {
-                        dp[i][j][k] += dp[i-1][j][t];
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
+
+        int dp[m][n][k];
+        std::fill(&dp[0][0][0], &dp[0][0][0] + m * n * k, 0);
+        dp[0][0][grid[0][0] % k] = 1;
+        for (int x = 0; x < m; ++x) {
+            for (int y = 0; y < n; ++y) {
+                for (int z = 0; z < k; ++z) {
+                    int r = ((z - grid[x][y]) % k + k) % k;
+                    if (x > 0) {
+                        dp[x][y][z] += dp[x - 1][y][r];
+                        dp[x][y][z] %= M;
                     }
-                    if (0 < j) {
-                        dp[i][j][k] += dp[i][j-1][t];
+                    if (y > 0) {
+                        dp[x][y][z] += dp[x][y - 1][r];
+                        dp[x][y][z] %= M;
                     }
-                    dp[i][j][k] %= M;
                 }
             }
         }
@@ -24,9 +27,3 @@ public:
         return dp[m - 1][n - 1][0];
     }
 };
-
-// dp[i][j][k] = dp[i-1][j][t] + dp[i][j-1][t]
-// t = ((k - grid[i][j]) % K + K) % K
-
-// assume K = 5, grid[i][j] = 3, we are calculating we calculate dp[i][j][2], 
-// => we need dp[i-1][j][4] and dp[i][j-1][4]
