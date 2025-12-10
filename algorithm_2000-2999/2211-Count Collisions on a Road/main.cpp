7697683311c56268c6f1258b758bb78ef6337c43
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-11 01:17:52 +0800

    2211: update solution

--- algorithm_2000-2999/2211-Count Collisions on a Road/main.cpp
+++ algorithm_2000-2999/2211-Count Collisions on a Road/main.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int n = directions.size();
-
         int ret = 0;
-        for (int i = 0, lftBound = 0; i < n; ++i) {
-            if (lftBound == 0) {
-                if (directions[i] == 'R' || directions[i] == 'S') {
-                    lftBound = 1;
-                }
+        for (int i = 0, lftBound = 0; i < directions.size(); ++i) {
+            if (lftBound == 0 && (directions[i] == 'R' || directions[i] == 'S')) {
+                lftBound = 1;
             }
-            else {
-                if (directions[i] == 'L') {
-                    ++ret;
-                }
+            else if (lftBound == 1 && directions[i] == 'L') {
+                ++ret;
             }
         }
-        for (int i = n, rhtBound = 0; 0 < i--;) {
-            if (rhtBound == 0) {
-                if (directions[i] == 'L' || directions[i] == 'S') {
-                    rhtBound = 1;
-                }
+        for (int i = directions.size(), rhtBound = 0; 0 < i--;) {
+            if (rhtBound == 0 && (directions[i] == 'L' || directions[i] == 'S')) {
+                rhtBound = 1;
             }
-            else {
-                if (directions[i] == 'R') {
-                    ++ret;
-                }
+            else if (rhtBound == 1 && directions[i] == 'R') {
+                ++ret;
             }
         }
 
