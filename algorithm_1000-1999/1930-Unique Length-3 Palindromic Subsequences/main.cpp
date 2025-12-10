e2a59b43d6a45f1e83a3d922dcf74e3ec6bc4e15
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-11 01:17:39 +0800

    1930: update solution

--- algorithm_1000-1999/1930-Unique Length-3 Palindromic Subsequences/main.cpp
+++ algorithm_1000-1999/1930-Unique Length-3 Palindromic Subsequences/main.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int n = s.size();
-
-        int lftMost[26];
-        std::fill(lftMost, lftMost + 26, -1);
-        for (int i = 0; i < n; ++i) {
-            if (lftMost[s[i] - 'a'] != string::npos) { continue; }
-            lftMost[s[i] - 'a'] = i;
+        int lftMosts[26];
+        std::fill(lftMosts, lftMosts + 26, -1);
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            if (lftMosts[s[i] - 'a'] == -1) {
+                lftMosts[s[i] - 'a'] = i;
+            }
         }
 
-        int rhtMost[26];
-        std::fill(rhtMost, rhtMost + 26, -1);
-        for (int i = n; 0 < i--;) {
-            if (rhtMost[s[i] - 'a'] != string::npos) { continue; }
-            rhtMost[s[i] - 'a'] = i;
+        int rhtMosts[26];
+        std::fill(rhtMosts, rhtMosts + 26, -1);
+        for (int i = s.size(); 0 < i--;) {
+            if (rhtMosts[s[i] - 'a'] == -1) {
+                rhtMosts[s[i] - 'a'] = i;
+            }
         }
 
         int ret = 0;
-        for (char c = 'a'; c <= 'z'; ++c) {
+        for (int i = 0; i < 26; ++i) {
             bitset<26> flags;
-            for (int i = lftMost[c - 'a'] + 1; i < rhtMost[c - 'a']; ++i) {
-                flags[s[i] - 'a'] = true;
+            for (int j = lftMosts[i] + 1; j < rhtMosts[i]; ++j) {
+                flags.set(s[j] - 'a');
             }
 
             ret += flags.count();
