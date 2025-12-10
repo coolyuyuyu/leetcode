761db76da2dda54a1c493002e1284862c1283999
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-11 01:17:28 +0800

    1590: update solution

--- algorithm_1000-1999/1590-Make Sum Divisible by P/main.cpp
+++ algorithm_1000-1999/1590-Make Sum Divisible by P/main.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        // X X X X X X X X X X X X
-        //         i   j         n
+        int n = nums.size();
+        // X X X X [X X X] X X X X X
+        // 0        i   j         n
 
         // given (sum(0:i-1) + sum(j+1, n)) % p == 0
-        // let remAll = sum(0:n) % p
-        // => remAll = (sum(0:i-1) + sum(i,j) + sum(j+1, n)) % p
-        // => remAll = sum(i,j) % p
-        // => remAll = (presum(j) - presum(i-1)) % p
+        // let rem = sum(0:n) % p
+        // => rem = (sum(0:i-1) + sum(i,j) + sum(j+1, n)) % p
+        // => rem = sum(i,j) % p
+        // => rem = (presum(j) - presum(i-1)) % p
 
         // let remJ = presum(j) % p
-        // => presum(i-1) % p = remJ - remAll
-
-        int n = nums.size();
+        // => presum(i-1) % p = remJ - rem
 
         int rem = 0;
         for (int num : nums) {
             rem = (rem + num) % p;
         }
-        if (rem == 0) { return 0; }
-
-        unordered_map<int, int> rem2idx = {{0, -1}};
+        if (rem == 0) {
+            return 0;
+        }
 
         int ret = n;
-        for (int j = 0, preRem = 0; j < nums.size(); ++j) {
-            preRem = (preRem + nums[j]) % p;
-
-            int x = ((preRem - rem) % p + p) % p;
-            if (rem2idx.find(x) != rem2idx.end()) {
-                int i = rem2idx[x] + 1;
+        unordered_map<int, int> rem2idx = {{0, -1}};
+        for (int j = 0, curRem = 0; j < n; ++j) {
+            curRem = (curRem + nums[j]) % p;
+            int preRem = ((curRem - rem) % p + p) % p;
+            if (rem2idx.find(preRem) != rem2idx.end()) {
+                int i = rem2idx[preRem] + 1;
                 ret = std::min(ret, j - i + 1);
             }
-
-            rem2idx[preRem] = j;
-        }
-        if (ret == n) {
-            ret = -1;
+            rem2idx[curRem] = j;
         }
 
-        return ret;
+        return ret < n ? ret : -1;
     }
 };
