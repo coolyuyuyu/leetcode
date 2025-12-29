6983a4f9f9cb4dc1090484138e05577110e05880
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-30 02:17:51 +0800

    298: update solution

--- algorithm_0000-0999/298-Binary Tree Longest Consecutive Sequence/main.cpp
+++ algorithm_0000-0999/298-Binary Tree Longest Consecutive Sequence/main.cpp
@@ -11,73 +11,25 @@
  */
 class Solution {
 public:
-    int longestConsecutive_Recursive(TreeNode* root, int& maxLcs) {
-        if (!root) {
-            return 0;
-        }
+    int longestConsecutive(TreeNode* root) {
+        int ret = 0;
+        std::function<void(TreeNode*, int, TreeNode*)> dfs = [&](TreeNode* cur, int lcs, TreeNode* pre) {
+            if (!cur) { return; }
 
-        int lcs = 1;
-        int lftLcs = longestConsecutive_Recursive(root->left, maxLcs);
-        int rhtLcs = longestConsecutive_Recursive(root->right, maxLcs);
-        if (root->left && (root->val + 1) == root->left->val) {
-            lcs = max(lcs, lftLcs + 1);
-        }
-        if (root->right && (root->val + 1) == root->right->val) {
-            lcs = max(lcs, rhtLcs + 1);
-        }
-
-        if (maxLcs < lcs) {
-            maxLcs = lcs;
-        }
-
-        return lcs;
-    }
-
-    int longestConsecutive_Iterative(TreeNode* root) {
-        int maxLcs = 0;
-
-        map<TreeNode*, int> m; // <node, lcs>
-        stack<pair<TreeNode*, bool>> stk; // <node, visited>
-        if (root) {
-            stk.emplace(root, false);
-        }
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            bool visited = stk.top().second;
-            stk.pop();
-
-            if (visited) {
-                int lcs = 1;
-                if (node->left && (node->val + 1) == node->left->val) {
-                    lcs = m[node->left] + 1;
-                }
-                if (node->right && (node->val + 1) == node->right->val) {
-                    lcs = max(lcs, m[node->right] + 1);
-                }
-                if (maxLcs < lcs) {
-                    maxLcs = lcs;
-                }
-                m[node] = lcs;
+            if (pre && pre->val + 1 == cur->val) {
+                ++lcs;
             }
             else {
-                stk.emplace(node, true);
-                if (node->right) {
-                    stk.emplace(node->right, false);
-                }
-                if (node->left) {
-                    stk.emplace(node->left, false);
-                }
+                lcs = 1;
             }
-        }
 
-        return maxLcs;
-    }
+            ret = std::max(ret, lcs);
 
-    int longestConsecutive(TreeNode* root) {
-        //int maxLcs = 0;
-        //longestConsecutive_Recursive(root, maxLcs);
-        //return maxLcs;
+            dfs(cur->left, lcs, cur);
+            dfs(cur->right, lcs, cur);
+        };
+        dfs(root, 0, nullptr);
 
-        return longestConsecutive_Iterative(root);
+        return ret;
     }
 };
