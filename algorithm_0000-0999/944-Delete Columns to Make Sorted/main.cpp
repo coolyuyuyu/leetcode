df00451e47796c7b4f94bafa766a6d7fd71949c1
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-30 02:18:06 +0800

    944: update solution

--- algorithm_0000-0999/944-Delete Columns to Make Sorted/main.cpp
+++ algorithm_0000-0999/944-Delete Columns to Make Sorted/main.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        size_t m = strs.size(), n = strs[0].size();
+        int m = strs.size(), n = strs.empty() ? 0 : strs[0].size();
 
-        size_t ret = 0;
-        for (size_t j = 0; j < n; ++j) {
-            for(size_t i = 1; i < m; ++i) {
-                if (strs[i - 1][j] > strs[i][j]) {
+        int ret = 0;
+        for (int c = 0; c < n; ++c) {
+            for (int r = 1; r < m; ++r) {
+                if (strs[r - 1][c] > strs[r][c]) {
                     ++ret;
                     break;
                 }
@@ -14,6 +14,5 @@ public:
         }
 
         return ret;
-
     }
 };
