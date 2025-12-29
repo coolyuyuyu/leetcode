868819a39c0b61b13fdb27a983d910ecd2f90203
coolyuyuyu <coolyuyuyu@gmail.com>
2025-12-30 02:18:27 +0800

    2054: update solution

--- algorithm_2000-2999/2054-Two Best Non-Overlapping Events/main.cpp
+++ algorithm_2000-2999/2054-Two Best Non-Overlapping Events/main.cpp
@@ -6,26 +6,23 @@ public:
         std::sort(events.begin(), events.end(), [](const auto& e1, const auto& e2){ return e1[1] < e2[1]; });
 
         // maxs[i]: the max val of values from events[0:i]
-        int maxs[n];
-        for (int i = 0, mx = 0; i < n; ++i) {
-            mx = std::max(mx, events[i][2]);
-            maxs[i] = mx;
-        }
+        // ends[i]: the endTime of events[i]
+        vector<int> maxs(n), ends(n);
 
         int ret = 0;
-
-        vector<int> ends;
         for (int i = 0; i < events.size(); ++i) {
             int bgn = events[i][0], end = events[i][1], val = events[i][2];
             ret = std::max(ret, val);
 
-            auto itr = std::upper_bound(ends.begin(), ends.end(), bgn - 1);
+            auto itr = std::lower_bound(ends.begin(), ends.begin() + i, bgn);
             if (itr != ends.begin()) {
                 itr = std::prev(itr);
                 int idx = std::distance(ends.begin(), itr);
                 ret = std::max(ret, maxs[idx] + val);
             }
-            ends.push_back(end);
+
+            ends[i] = end;
+            maxs[i] = std::max(i > 0 ? maxs[i - 1] : 0, val);
         }
 
         return ret;
