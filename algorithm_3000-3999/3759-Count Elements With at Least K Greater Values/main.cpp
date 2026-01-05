96a9640353d1bc4ff155537d9eb81d224cb547ed
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-06 02:26:17 +0800

    3759: update solution

--- algorithm_3000-3999/3759-Count Elements With at Least K Greater Values/main.cpp
+++ algorithm_3000-3999/3759-Count Elements With at Least K Greater Values/main.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
-        priority_queue<int> maxPQ(nums.begin(), nums.end());
+        if (k == 0) { return nums.size(); }
 
-        int cnt = 0;
-        while (cnt < k && !maxPQ.empty()) {
-            ++cnt;
-            int val = maxPQ.top();
-            maxPQ.pop();
-
-            while (!maxPQ.empty() && maxPQ.top() == val) {
-                ++cnt;
-                maxPQ.pop();
-            }
+        std::sort(nums.begin(), nums.end());
+        int i = nums.size() - k;
+        while (i - 1 >= 0 && nums[i - 1] == nums[i]) {
+            --i;
         }
 
-        return nums.size() - cnt;
+        return i;
     }
 };
