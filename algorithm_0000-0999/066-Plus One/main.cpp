ea6ca86b4365f537ce9caca60f67786ca3a18b71
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-06 02:25:42 +0800

    066: update solution

--- algorithm_0000-0999/066-Plus One/main.cpp
+++ algorithm_0000-0999/066-Plus One/main.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         bool carry = true;
-        for (size_t i = digits.size(); 0 < i-- && carry;) {
-            if (++digits[i] == 10) {
-                digits[i] = 0;
-            }
-            else {
-                carry = false;
-            }
+        for (int i = digits.size() - 1; i >= 0 && carry; --i) {
+            carry = (digits[i] == 9);
+            digits[i] = (digits[i] + 1) % 10;
         }
         if (carry) {
             digits.insert(digits.begin(), 1);
