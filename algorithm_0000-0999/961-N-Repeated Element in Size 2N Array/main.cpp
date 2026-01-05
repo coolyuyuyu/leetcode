dbfd5ea05b323b4ec1e52caef346d9edcbe17de3
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-06 02:25:50 +0800

    961: update solution

--- algorithm_0000-0999/961-N-Repeated Element in Size 2N Array/main.cpp
+++ algorithm_0000-0999/961-N-Repeated Element in Size 2N Array/main.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        if (nums.size() == 4 && nums[0] == nums[3]) {
-            return nums[0];
-        }
-        for (size_t i = 0; (i + 1) < nums.size(); ++i) {
-            if (nums[i] == nums[i + 1]) {
-                return nums[i];
-            }
-            else if ((i + 2) < nums.size() && nums[i] == nums[i + 2]) {
-                return nums[i];
+        unordered_set<int> s;
+        for (int num : nums) {
+            if (s.insert(num).second == false) {
+                return num;
             }
         }
+        __builtin_unreachable();
 
         return -1;
     }
