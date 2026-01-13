cf3f9b835204182d9be9bc5cb9b3bdb3c30a94f9
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-14 01:52:41 +0800

    1458: update solution

--- algorithm_1000-1999/1458-Max Dot Product of Two Subsequences/main.cpp
+++ algorithm_1000-1999/1458-Max Dot Product of Two Subsequences/main.cpp
@@ -8,7 +8,8 @@ public:
 
         // dp[i][j]: the maximum dot product between non-empty subsequences of nums1 and nums2
         int dp[m + 1][n + 1];
-        dp[0][0] = 0;
+
+        dp[0][0] = INT_MIN / 2;
         for (int i = 1; i <= m; ++i) {
             dp[i][0] = INT_MIN / 2;
         }
@@ -17,9 +18,12 @@ public:
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                dp[i][j] = std::max({dp[i - 1][j], dp[i][j - 1], std::max(0, dp[i - 1][j - 1]) + nums1[i] * nums2[j]});
+                int a = std::max(0, dp[i - 1][j - 1]) + nums1[i] * nums2[j]; // pair nums1[i] and nums2[j]
+                int b = std::max(dp[i - 1][j], dp[i][j - 1]); // do not pair nums1[i] and nums2[j]
+                dp[i][j] = std::max(a, b);
             }
         }
+
         return dp[m][n];
     }
 };
