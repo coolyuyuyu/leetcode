42222fd870fbbadbc129fd44846574cba2024510
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-14 01:52:01 +0800

    490: update solution

--- algorithm_0000-0999/490-The Maze/main.cpp
+++ algorithm_0000-0999/490-The Maze/main.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
+    vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
         int m = maze.size(), n = maze.empty() ? 0 : maze[0].size();
-
-        vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-
-        auto nextPos = [&](int r, int c, const pair<int, int>& dir) {
-            while (0 <= r && r < m && 0 <= c && c < n && maze[r][c] == 0) {
-                r += dir.first;
-                c += dir.second;
+        std::function<pair<int, int>(int, int, int, int)> rollAtMost = [&](int r, int c, int dr, int dc) {
+            for (int x, y; true; r = x, c = y) {
+                x = r + dr, y = c + dc;
+                if (x < 0 || x >= m || y < 0 || y >= n) { break; }
+                if (maze[x][y] == 1) { break; }
             }
- 
-            return std::make_pair(r - dir.first, c - dir.second);
+            return make_pair(r, c);
         };
 
-        queue<pair<int, int>> q;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        bool visited[m][n];
+        std::fill(&visited[0][0], &visited[0][0] + m * n, false);
 
-        q.emplace(start[0], start[1]);
+        queue<pair<int, int>> q;
         visited[start[0]][start[1]] = true;
+        q.emplace(start[0], start[1]);
         while (!q.empty()) {
-            auto [r1, c1] = q.front();
+            auto [r, c] = q.front();
             q.pop();
 
-            for (const auto& dir : dirs) {
-                auto [r2, c2] = nextPos(r1, c1, dir);
-                if (r2 == destination[0] && c2 == destination[1]) { return true; }
-                if (visited[r2][c2]) { continue; }
-
-                q.emplace(r2, c2);
-                visited[r2][c2] = true;
+            for (const auto& [dr, dc] : dirs) {
+                auto [x, y] = rollAtMost(r, c, dr, dc);
+                if (visited[x][y]) { continue; }
+                if (x == destination[0] && y == destination[1]) { return true; }
+                visited[x][y] = true;
+                q.emplace(x, y);
             }
         }
 
