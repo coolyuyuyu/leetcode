079a894f73f86d511518eb918128586674cc4011
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-19 01:46:44 +0800

    499: update solution

--- algorithm_0000-0999/499-The Maze III/main.cpp
+++ algorithm_0000-0999/499-The Maze III/main.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
+    vector<tuple<char, int, int>> dirs = {{'l', 0, -1}, {'u', -1, 0}, {'r', 0, 1}, {'d', 1, 0}};
+
     string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
         int m = maze.size(), n = maze.empty() ? 0 : maze[0].size();
-        vector<tuple<char, int, int>> dirs = {{'l', 0, -1}, {'u', -1, 0}, {'r', 0, 1}, {'d', 1, 0}};
+        std::function<tuple<int, int, int>(int, int, int, int)> rollAtMost = [&](int r, int c, int dr, int dc) {
+            int cost = 0;
+            for (int x, y; r != hole[0] || c != hole[1]; r = x, c = y) {
+                x = r + dr, y = c + dc;
+                if (x < 0 || x >= m || y < 0 || y >= n) { break; }
+                if (maze[x][y] == 1) { break; }
+                ++cost;
+            }
+            return make_tuple(r, c, cost);
+        };
 
         bool visited[m][n];
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                visited[r][c] = false;
-            }
-        }
+        std::fill(&visited[0][0], &visited[0][0] + m * n, false);
 
-        priority_queue<tuple<int, string, int, int>, vector<tuple<int, string, int, int>>, std::greater<tuple<int, string, int, int>>> pq;
+        priority_queue<tuple<int, string, int, int>, vector<tuple<int, string, int, int>>, std::greater<>> pq; // <dist, instructions, r, c>
         pq.emplace(0, "", ball[0], ball[1]);
         while (!pq.empty()) {
-            auto [d, i, r, c] = pq.top();
+            auto [dist, instructions, r, c] = pq.top();
             pq.pop();
 
-            if (visited[r][c]) { continue;}
+            if (visited[r][c]) { continue; }
             visited[r][c] = true;
 
             if (r == hole[0] && c == hole[1]) {
-                return i;
+                return instructions;
             }
 
-            for (const auto& [mark, dr, dc] : dirs) {
-                int z = d;
-                string t = i + mark;
-                int x = r, y = c;
-                while (
-                    !(x == hole[0] && y == hole[1]) &&
-                    0 <= x + dr &&
-                    x + dr < m&&
-                    0 <= y + dc &&
-                    y + dc < n &&
-                    maze[x + dr][y + dc] == 0) {
-                    z += 1;
-                    x += dr, y += dc;
-                }
+            for (const auto& [instruction, dr, dc] : dirs) {
+                auto [x, y, cost] = rollAtMost(r, c, dr, dc);
                 if (visited[x][y]) { continue; }
-
-                pq.emplace(z, t, x, y);
+                pq.emplace(dist + cost, instructions + instruction, x, y);
             }
         }
 
