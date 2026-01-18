8a04b40d2400b83ca28d3bd041d6e65a6d1836e6
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-19 01:47:03 +0800

    2943: update solution

--- algorithm_2000-2999/2943-Maximize Area of Square Hole in Grid/main.cpp
+++ algorithm_2000-2999/2943-Maximize Area of Square Hole in Grid/main.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        std::function<int(vector<int>&)> f = [](vector<int>& bars) {
-            std::sort(bars.begin(), bars.end());
-
-            int maxCnt = 1;
-            for (int i = 0, cnt; i < bars.size(); ++i) {
-                if (i == 0 || bars[i - 1] + 1 != bars[i]) {
-                    cnt = 1;
+        std::function<int(const vector<int>&)> f = [](const vector<int>& bars) {
+            int maxCnt = 0;
+            for (int i = 0, cnt = 0; i < bars.size(); ++i) {
+                if (i > 0 && bars[i - 1] + 1 == bars[i]) {
+                    ++cnt;
                 }
                 else {
-                    ++cnt;
+                    cnt = 1;
                 }
                 maxCnt = std::max(maxCnt, cnt);
             }
@@ -18,7 +16,9 @@ public:
             return maxCnt + 1;
         };
 
-        int x = std::min(f(hBars), f(vBars));
-        return x * x;
+        std::sort(hBars.begin(), hBars.end());
+        std::sort(vBars.begin(), vBars.end());
+        int s = std::min(f(hBars), f(vBars));
+        return s * s;
     }
 };
