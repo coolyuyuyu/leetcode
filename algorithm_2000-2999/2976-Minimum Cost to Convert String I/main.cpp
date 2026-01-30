196a1de8d69cd557c97c821af57e8b4cdd8ad7d8
coolyuyuyu <coolyuyuyu@gmail.com>
2026-01-30 17:43:56 +0800

    2976: update solution

--- algorithm_2000-2999/2976-Minimum Cost to Convert String I/main.cpp
+++ algorithm_2000-2999/2976-Minimum Cost to Convert String I/main.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        int n = source.size(), m = original.size();
-
         int costs[26][26];
-        for (int u = 0; u < 26; ++u) {
-            for (int v = 0; v < 26; ++v) {
-                costs[u][v] = INT_MAX / 2;
-            }
-        }
+        std::fill(&costs[0][0], &costs[0][0] + 26 * 26, INT_MAX / 2);
+
         for (int u = 0; u < 26; ++u) {
             costs[u][u] = 0;
         }
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < original.size(); ++i) {
             int u = original[i] - 'a', v = changed[i] - 'a', w = cost[i];
             costs[u][v] = std::min(costs[u][v], w);
         }
-        for (int k = 0; k < 26; ++k) {
+
+        for (int i = 0; i < 26; ++i) {
             for (int u = 0; u < 26; ++u) {
                 for (int v = 0; v < 26; ++v) {
-                    costs[u][v] = std::min(costs[u][v], costs[u][k] + costs[k][v]);
+                    costs[u][v] = std::min(costs[u][v], costs[u][i] + costs[i][v]);
                 }
             }
         }
 
         long long ret = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < source.size(); ++i) {
             int u = source[i] - 'a', v = target[i] - 'a';
             if (costs[u][v] >= INT_MAX / 2) {
                 return -1;
