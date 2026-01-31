9cab4330dae40c36beca2a7f6380b98bcfcff01a
coolyuyuyu <coolyuyuyu@gmail.com>
2026-02-01 05:07:51 +0800

    2977: update soluton

--- algorithm_2000-2999/2977-Minimum Cost to Convert String II/main.cpp
+++ algorithm_2000-2999/2977-Minimum Cost to Convert String II/main.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    struct TrieNode {
-        std::array<TrieNode*, 26> childs;
-        int id;
-        TrieNode() {
-            childs.fill(nullptr);
-            id = -1;
-        }
-    };
-
     long long minimumCost(string source, string target, vector<string>& original, vector<string>& changed, vector<int>& cost) {
         int n = source.size(), m = original.size();
         unordered_map<string, int> str2id;
@@ -17,57 +8,63 @@ public:
             str2id.emplace(changed[j], str2id.size());
         }
 
-        TrieNode* root = new TrieNode();
-        for (const auto& [str, id] : str2id) {
-            TrieNode* node = root;
-            for (int j = str.size(); 0 < j--;) {
-                if (node->childs[str[j] - 'a'] == nullptr) {
-                    node->childs[str[j] - 'a'] = new TrieNode();
-                }
-                node = node->childs[str[j] - 'a'];
-            }
-            node->id = id;
-        }
-
-        long long dists[str2id.size()][str2id.size()];
+        long long costs[str2id.size()][str2id.size()];
+        std::fill(&costs[0][0], &costs[0][0] + str2id.size() * str2id.size(), LLONG_MAX / 2);
         for (int u = 0; u < str2id.size(); ++u) {
-            for (int v = 0; v < str2id.size(); ++v) {
-                if (u == v) { dists[u][v] = 0;}
-                else { dists[u][v] = LLONG_MAX / 2; }
-            }
+            costs[u][u] = 0;
         }
         for (int j = 0; j < m; ++j) {
             int u = str2id[original[j]], v = str2id[changed[j]];
-            dists[u][v] = std::min<long long>(dists[u][v], cost[j]);
+            long long w = cost[j];
+            costs[u][v] = std::min(costs[u][v], w);
         }
         for (int x = 0; x < str2id.size(); ++x) {
             for (int u = 0; u < str2id.size(); ++u) {
                 for (int v = 0; v < str2id.size(); ++v) {
-                    dists[u][v] = std::min(dists[u][v], dists[u][x] + dists[x][v]);
+                    costs[u][v] = std::min(costs[u][v], costs[u][x] + costs[x][v]);
                 }
             }
         }
 
-        source.insert(source.begin(), '#');
-        target.insert(target.begin(), '#');
+        struct TrieNode {
+            array<TrieNode*, 26> nxts;
+            int id;
+            TrieNode() {
+                nxts.fill(nullptr);
+                id = -1;
+            }
+        };
+        TrieNode* root = new TrieNode();
+        for (const auto& [str, id] : str2id) {
+            TrieNode* node = root;
+            for (int i = str.size(); 0 < i--;) {
+                if (node->nxts[str[i] - 'a'] == nullptr) {
+                    node->nxts[str[i] - 'a'] = new TrieNode();
+                }
+                node = node->nxts[str[i] - 'a'];
+            }
+            node->id = id;
+        }
 
-        // dp[i]: the minimum cost to convert source[1:i] to target[1:i]
+        source = "#" + source;
+        target = "#" + target;
+
+        // dp[i]: the minimum cost to convert the string source[1:i] to the string target[1:j]
         long long dp[n + 1];
         dp[0] = 0;
         for (int i = 1; i <= n; ++i) {
-            dp[i] = source[i] == target[i] ? dp[i - 1] : LLONG_MAX / 2;
+            dp[i] = (source[i] == target[i] ? dp[i - 1] : LLONG_MAX / 2);
             TrieNode *node1 = root, *node2 = root;
-            for (int j = i; j >= 1; --j) {
-                node1 = node1->childs[source[j] - 'a'];
-                node2 = node2->childs[target[j] - 'a'];
-                if (node1 == nullptr || node2 == nullptr) { break; }
-                int id1 = node1->id, id2 = node2->id;
-                if (id1 >= 0 && id2 >= 0) {
-                    dp[i] = std::min(dp[i], dp[j - 1] + dists[id1][id2]);
+            for (int j = i; j >= 1; j--) {
+                node1 = node1->nxts[source[j] - 'a'];
+                node2 = node2->nxts[target[j] - 'a'];
+                if (!node1 || !node2) { break; }
+                if (node1->id >= 0 && node2->id >= 0) {
+                    dp[i] = std::min(dp[i], dp[j - 1] + costs[node1->id][node2->id]);
                 }
             }
         }
 
-        return dp[n] != LLONG_MAX / 2? dp[n] : -1;
+        return dp[n] != LLONG_MAX / 2 ? dp[n] : -1;
     }
 };
