747334fba14f5053f6dcaa8d20d8a436286d8e4a
coolyuyuyu <coolyuyuyu@gmail.com>
2026-02-05 22:23:18 +0800

    362: update solution

--- algorithm_0000-0999/362-Design Hit Counter/main.cpp
+++ algorithm_0000-0999/362-Design Hit Counter/main.cpp
@@ -1,31 +1,16 @@
 class HitCounter {
 public:
-    /** Initialize your data structure here. */
     HitCounter() {
     }
 
-    /** Record a hit.
-        @param timestamp - The current timestamp (in seconds granularity). */
     void hit(int timestamp) {
-        m_timestamps.emplace_back(timestamp);
+        m_timestamps.push_back(timestamp);
     }
 
-    /** Return the number of hits in the past 5 minutes.
-        @param timestamp - The current timestamp (in seconds granularity). */
     int getHits(int timestamp) {
-        size_t lo = 0, hi = m_timestamps.size();
-        int target = timestamp - 300 + 1;
-        while (lo < hi) {
-            size_t mid = lo + (hi - lo) / 2;
-            if (target <= m_timestamps[mid]) {
-                hi = mid;
-            }
-            else {
-                lo = mid + 1;
-            }
-        }
-
-        return m_timestamps.size() - lo;
+        auto lo = std::lower_bound(m_timestamps.begin(), m_timestamps.end(), timestamp - 300 + 1);
+        auto hi = std::upper_bound(m_timestamps.begin(), m_timestamps.end(), timestamp);
+        return std::distance(lo, hi);
     }
 
 private:
@@ -34,7 +19,7 @@ private:
 
 /**
  * Your HitCounter object will be instantiated and called as such:
- * HitCounter obj = new HitCounter();
- * obj.hit(timestamp);
- * int param_2 = obj.getHits(timestamp);
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
  */
