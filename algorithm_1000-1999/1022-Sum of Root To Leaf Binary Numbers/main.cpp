77555f50ccf7179c9b79d53b925e0dc43eeda445
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-09 00:03:39 +0800

    1022: update solution

--- algorithm_1000-1999/1022-Sum of Root To Leaf Binary Numbers/main.cpp
+++ algorithm_1000-1999/1022-Sum of Root To Leaf Binary Numbers/main.cpp
@@ -11,57 +11,21 @@
  */
 class Solution {
 public:
-    void sumRootToLeaf_Recursive(TreeNode* root, int val, int& sum) {
-        if (!root) {
-            return;
-        }
-
-        val = val * 2 + root->val;
-        if (!root->left && !root->right) {
-            sum += val;
-        }
-        else {
-            if (root->left) {
-                sumRootToLeaf_Recursive(root->left, val, sum);
+    int sumRootToLeaf(TreeNode* root) {
+        std::function<int(TreeNode*, int)> f = [&](TreeNode* root, int num) {
+            if (!root) {
+                return 0;
             }
-            if (root->right) {
-                sumRootToLeaf_Recursive(root->right, val, sum);
-            }
-        }
-    }
 
-    void sumRootToLeaf_Iterative(TreeNode* root, int& sum) {
-        if (!root) {
-            return;
-        }
-
-        stack<pair<TreeNode*, int>> stk;
-        stk.emplace(root, root->val);
-        while (!stk.empty()) {
-            TreeNode* node = stk.top().first;
-            int val = stk.top().second;
-            stk.pop();
-
-            if (!node->left && !node->right) {
-                sum += val;
+            num = (num << 1) + root->val;
+            if (!root->left && !root->right) {
+                return num;
             }
             else {
-                if (node->right) {
-                    stk.emplace(node->right, val * 2 + node->right->val);
-                }
-                if (node->left) {
-                    stk.emplace(node->left, val * 2 + node->left->val);
-                }
+                return f(root->left, num) + f(root->right, num);
             }
-        }
-    }
+        };
 
-    int sumRootToLeaf(TreeNode* root) {
-        int sum = 0;
-
-        //sumRootToLeaf_Recursive(root, 0, sum);
-        sumRootToLeaf_Iterative(root, sum);
-
-        return sum;
+        return f(root, 0);
     }
 };
