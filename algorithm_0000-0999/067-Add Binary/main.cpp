1c269cf05dd4be4dade5a3ef21c2d02754facdec
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-09 00:02:08 +0800

    067: update solution

--- algorithm_0000-0999/067-Add Binary/main.cpp
+++ algorithm_0000-0999/067-Add Binary/main.cpp
@@ -5,16 +5,17 @@ public:
             std::swap(a, b);
         }
 
-        bool carry = false;
-        for (int i = a.size() - 1, j = b.size() - 1; 0 <= i && (0 <= j || carry); --i, --j) {
-            int v = (a[i] - '0') + (0 <= j ? b[j] - '0' : 0) + (carry ? 1 : 0);
-            carry = (1 < v);
-            a[i] = '0' + (v & 1);
+        string ret(a.size(), 0);
+        int carry = 0;
+        for (int i = a.size() - 1, j = b.size() - 1; i >= 0; --i, --j) {
+            int v = (a[i] - '0') + (j >= 0 ? b[j] - '0' : 0) + carry;
+            carry = (v > 1 ? 1 : 0);
+            ret[i] = '0' + (v & 1);
         }
         if (carry) {
-            a.insert(a.begin(), '1');
+            ret.insert(ret.begin(), '1');
         }
 
-        return a;
+        return ret;
     }
 };
