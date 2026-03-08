fbaedcb781ad9b3f554b85d0a7592019a309e17d
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-09 00:03:29 +0800

    868: update solution

--- algorithm_0000-0999/868-Binary Gap/main.cpp
+++ algorithm_0000-0999/868-Binary Gap/main.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int binaryGap(int N) {
-        int maxGap = 0;
-        for (int lastIndex = -1, i = 0; i < 32; ++i, N >>= 1) {
-            if (N & 1) {
-                if (lastIndex >= 0) {
-                    int gap = i - lastIndex;
-                    if (maxGap < gap) {
-                        maxGap = gap;
-                    }
-                }
-                lastIndex = i;
+    int binaryGap(int n) {
+        int ret = 0;
+        for (int cur = 0, pre = 32; n; ++cur, n >>= 1) {
+            if (n & 1) {
+                ret = std::max(ret, cur - pre);
+                pre = cur;
             }
         }
 
-        return maxGap;
+        return ret;
     }
 };
