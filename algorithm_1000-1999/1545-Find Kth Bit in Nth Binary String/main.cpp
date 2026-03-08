3b683e7ff8891e78f1e5917dfbbc2b581c8f80f1
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-09 00:04:26 +0800

    1545: update solution

--- algorithm_1000-1999/1545-Find Kth Bit in Nth Binary String/main.cpp
+++ algorithm_1000-1999/1545-Find Kth Bit in Nth Binary String/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    char findKthBit(int n, int k) {
+    char iterative(int n, int k) {
         --k;
 
         int len = 1;
@@ -9,19 +9,40 @@ public:
         }
 
         int flip = 0;
-        while (len > 1) {
-            int sublen = (len - 1) / 2;
-            if (k == sublen) {
-                return flip & 1 ? '0' : '1';
-            }
-
-            if (k > sublen) {
-                k = sublen * 2 - k;
+        for (int i = n; i > 1; --i, len = (len - 1) / 2) {
+            int mid = len / 2;
+            if (k == mid) { return (flip & 1) ? '0' : '1';; }
+            else if (k > mid) {
+                k = mid - (k - mid);
                 ++flip;
             }
-            len = sublen;
         }
 
-        return flip & 1 ? '1' : '0';
+        return (flip & 1) ? '1' : '0';
+    }
+
+    char recursive(int n, int k) {
+        int len = 1;
+        for (int i = 1; i < n; ++i) {
+            len = len * 2 + 1;
+        }
+
+        std::function<char(int, int)> dfs = [&](int k, int len) -> char {
+            if (len == 1) { return '0'; }
+            else if (k == (len + 1) / 2) { return '1'; }
+            else if (k < (len + 1) / 2) {
+                return dfs(k, (len - 1) / 2);
+            }
+            else {
+                return '0' + ('1' - dfs(len - k + 1, (len - 1) / 2));
+            }
+        };
+
+        return dfs(k, len);
+    }
+
+    char findKthBit(int n, int k) {
+        //return iterative(n, k);
+        return recursive(n, k);
     }
 };
