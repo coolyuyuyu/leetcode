0eb4a608189d71b121116bb0fa52f7df44b9c2ca
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-09 00:03:22 +0800

    799: update solution

--- algorithm_0000-0999/799-Champagne Tower/main.cpp
+++ algorithm_0000-0999/799-Champagne Tower/main.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    // dp[i][j]: how full the jth glass in the ith row
-    double dp[101][101];
-
     double champagneTower(int poured, int query_row, int query_glass) {
         int m = query_row + 1, n = query_glass + 1;
 
+        // dp[i][j]: how full the jth glass in the ith row is
+        double dp[m + 1][n + 1];
+
+        std::fill(&dp[0][0], &dp[0][0] + (m + 1) * (n + 1), 0.0 );
+        dp[0][0] = poured;
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
-                dp[r][c] = 0.0;
-            }
-        }
-        dp[0][0] = poured;
-
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < std::min(n, (r + 1)); ++c) {
                 if (dp[r][c] <= 1.0) {
                     continue;
                 }
@@ -22,10 +17,11 @@ public:
                 double extraHalf = (dp[r][c] - 1) / 2;
                 dp[r + 1][c] += extraHalf;
                 dp[r + 1][c + 1] += extraHalf;
+
                 dp[r][c] = 1.0;
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[query_row][query_glass];
     }
 };
