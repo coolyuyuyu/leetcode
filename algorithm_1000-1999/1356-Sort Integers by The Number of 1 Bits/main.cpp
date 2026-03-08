03da0819e67938ecd6ff475e0e88d8ba04e8755f
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-09 00:03:55 +0800

    1356: update solution

--- algorithm_1000-1999/1356-Sort Integers by The Number of 1 Bits/main.cpp
+++ algorithm_1000-1999/1356-Sort Integers by The Number of 1 Bits/main.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        auto cmp = [](int num1, int num2) {
-            int cnt1 = __builtin_popcount(num1), cnt2 = __builtin_popcount(num2);
-            return cnt1 < cnt2 || (cnt1 == cnt2 && num1 < num2);
-        };
-        std::sort(arr.begin(), arr.end(), cmp);
+        std::sort(
+            arr.begin(), arr.end(),
+            [](int num1, int num2) {
+                int cnt1 = __builtin_popcount(num1), cnt2 = __builtin_popcount(num2);
+                return cnt1 < cnt2 || (cnt1 == cnt2 && num1 < num2);
+            });
         return arr;
     }
 };
