7cef304bfa3f99fe61011f16a22323a35e0e10cb
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-17 03:20:33 +0800

    1415: update solution

--- algorithm_1000-1999/1415-The k-th Lexicographical String of All Happy Strings of Length n/main.cpp
+++ algorithm_1000-1999/1415-The k-th Lexicographical String of All Happy Strings of Length n/main.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string getHappyString(int n, int k) {
+    string recursive(int n, int k) {
         string s;
         std::function<void(int, int)> f = [&](int n, int k) {
             if (n == 0) { return; }
@@ -11,7 +11,6 @@ public:
                 return;
             }
             else if (t > 2) {
-
                 s.clear();
                 return;
             }
@@ -27,5 +26,39 @@ public:
         f(n, k - 1);
 
         return s;
+    }
+
+    string iterative(int n, int k) {
+        --k;
+
+        string ret;
+        for (int i = n; i >= 1; --i) {
+            int t = k / pow(2, i - 1);
+            if (i == n) {
+                if (t >= 3) {
+                    return "";
+                }
+            }
+            else {
+                if (t >= 2) {
+                    return "";
+                }
+            }
+
+            char c = 'a' + t;
+            if (!ret.empty() && ret.back() <= c) {
+                ++c;
+            }
+            ret.push_back(c);
+
+            k -= t * pow(2, i - 1);
+        }
+
+        return ret;
+    }
+
+    string getHappyString(int n, int k) {
+        //return recursive(n, k);
+        return iterative(n, k);
     }
 };
