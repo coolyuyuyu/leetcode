35841b539b9d7d6ec217512294762ba55e20a89a
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-17 03:20:49 +0800

    3296: update solution

--- algorithm_3000-3999/3296-Minimum Number of Seconds to Make Mountain Height Zero/main.cpp
+++ algorithm_3000-3999/3296-Minimum Number of Seconds to Make Mountain Height Zero/main.cpp
@@ -3,20 +3,21 @@ public:
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
         int n = workerTimes.size();
 
-        // <next round start time, the number of time worker reduce, worker index>
-        priority_queue<tuple<long long, int, int>, vector<tuple<long long, int, int>>, std::greater<tuple<long long, int, int>>> pq;
+        // <time worker starts next round, number of times worker reduce the height, worker index>
+        priority_queue<tuple<long long, int, int>, vector<tuple<long long, int, int>>, std::greater<>> pq;
         for (int i = 0; i < n; ++i) {
-            pq.emplace(workerTimes[i], 1, i);
+            pq.emplace(workerTimes[i], 0, i);
         }
 
         long long ret = 0;
-        for (; 0 < mountainHeight--;) {
+        while (0 < mountainHeight--) {
             auto [t, cnt, i] = pq.top();
             pq.pop();
 
             ret = std::max(ret, t);
 
-            pq.emplace(t + 1LL * (cnt + 1) * workerTimes[i], cnt + 1, i);
+            ++cnt;
+            pq.emplace(t + 1LL * (cnt + 1) * workerTimes[i], cnt, i);
         }
 
         return ret;
