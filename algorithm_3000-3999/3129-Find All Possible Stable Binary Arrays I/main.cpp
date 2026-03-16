4d0b9d1a73a633423e46925ea1046432dc50bd67
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-17 03:20:38 +0800

    3129: update solution

--- algorithm_3000-3999/3129-Find All Possible Stable Binary Arrays I/main.cpp
+++ algorithm_3000-3999/3129-Find All Possible Stable Binary Arrays I/main.cpp
@@ -6,16 +6,13 @@ public:
         int dp0[zero + 1][one + 1];
         // dp1[i][j]: the number of stable binary arrays ending with 1 consisting of i zeros and j ones
         int dp1[zero + 1][one + 1];
+
+        std::fill(&dp0[0][0], &dp0[0][0] + (zero + 1) * (one + 1), 0);
         for (int i = 0; i <= zero; ++i) {
-            for (int j = 0; j <= one; ++j) {
-                dp0[i][j] = 0;
-                dp1[i][j] = 0;
-            }
-        }
-        for (int i = 1; i <= zero; ++i) {
             dp0[i][0] = (i <= limit ? 1 : 0);
         }
-        for (int j = 1; j <= one; ++j) {
+        std::fill(&dp1[0][0], &dp1[0][0] + (zero + 1) * (one + 1), 0);
+        for (int j = 0; j <= one; ++j) {
             dp1[0][j] = (j <= limit ? 1 : 0);
         }
 
