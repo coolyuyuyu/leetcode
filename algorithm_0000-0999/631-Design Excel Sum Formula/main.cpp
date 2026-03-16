c17c24720e220cff0a04d0652b2af0043ce7973e
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-17 03:20:13 +0800

    631: update solution

--- algorithm_0000-0999/631-Design Excel Sum Formula/main.cpp
+++ algorithm_0000-0999/631-Design Excel Sum Formula/main.cpp
@@ -1,53 +1,100 @@
 class Excel {
-public:
-    Excel(int height, char width)
-        : m_vals(height + 1, vector<int>(width - 'A' + 1 + 1))
-        , m_exprs(height + 1, vector<vector<string>>(width - 'A' + 1 + 1)) {
-    }
-
-    void set(int row, char col, int val) {
-        m_vals[row][col - 'A' + 1] = val;
-        m_exprs[row][col - 'A' + 1].clear();
-    }
-
-    int get(int row, char col) {
-        if (m_exprs[row][col - 'A' + 1].empty()) {
-            return m_vals[row][col - 'A' + 1];
+private:
+    struct Node {
+    public:
+        void invalidate() {
+            for (Node* nxt : nxts) {
+                nxt->invalidate();
+            }
+            valid = false;
         }
 
-        int ret = 0;
-        for (const string& expr : m_exprs[row][col - 'A' + 1]) {
-            size_t sep = expr.find(':');
+        int getValue() {
+            if (valid || pres.empty()) {
+                return val;
+            }
+
+            val = 0;
+            for (Node* pre : pres) {
+                val += pre->getValue();
+            }
+
+            valid = true;
+
+            return val;
+        }
+
+        vector<Node*> nxts;
+        vector<Node*> pres;
+        bool valid = false;
+        int val = 0;
+    };
+
+    Node m_nodes[26][26];
+
+public:
+    Excel(int height, char width)
+        : m_rowCnt(height)
+        , m_colCnt(width - 'A') {
+    }
+
+    void set(int row, char column, int val) {
+        auto [r, c] = getRowCol(row, column);
+        m_nodes[r][c].invalidate();
+        m_nodes[r][c].val = val;
+        m_nodes[r][c].pres.clear();
+    }
+
+    int get(int row, char column) {
+        auto [r, c] = getRowCol(row, column);
+        return m_nodes[r][c].getValue();
+    }
+
+    int sum(int row, char column, vector<string> numbers) {
+        auto [r, c] = getRowCol(row, column);
+        Node* cur = &m_nodes[r][c];
+        cur->invalidate();
+        cur->pres.clear();
+
+        for (const string& number : numbers) {
+            size_t sep = number.find(':');
             if (sep == string::npos) {
-                auto [r, c] = parse(expr);
-                ret += get(r, c);
+                auto [r, c] = parse(number);
+                Node* pre = &m_nodes[r][c];
+                cur->pres.push_back(pre);
+                pre->nxts.push_back(cur);
             }
             else {
-                auto [top, lft] = parse(expr.substr(0, sep));
-                auto [btm, rht] = parse(expr.substr(sep + 1));
+                auto [top, lft] = parse(number.substr(0, sep));
+                auto [btm, rht] = parse(number.substr(sep + 1));
                 for (auto r = top; r <= btm; ++r) {
                     for (auto c = lft; c <= rht; ++c) {
-                        ret += get(r, c);
+                        Node* pre = &m_nodes[r][c];
+                        cur->pres.push_back(pre);
+                        pre->nxts.push_back(cur);
                     }
                 }
             }
         }
 
-        return ret;
-    }
-
-    int sum(int row, char col, vector<string> numbers) {
-        m_exprs[row][col - 'A' + 1] = numbers;
-        return get(row, col);
+        return cur->getValue();
     }
 
 private:
-    pair<int, char> parse(const string& s) {
-        return {std::stoi(s.substr(1)), s[0]};
+    std::pair<int, int> getRowCol(int row, char column) {
+        return {row - 1, column - 'A'};
     }
 
-    vector<vector<int>> m_vals;
-    vector<vector<vector<string>>> m_exprs;
+    pair<int, int> parse(string_view s) {
+        int r;
+        std::from_chars(s.data() + 1, s.data() + s.size(), r);
+        r -= 1;
+        int c = s[0] - 'A';
+        return {r, c};
+    }
+
+    int m_rowCnt;
+    int m_colCnt;
 };
 
 /**
