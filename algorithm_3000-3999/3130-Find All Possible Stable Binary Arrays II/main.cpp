2e37b57eb2850efa2e791055c1aeffed7bfff829
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-17 03:20:43 +0800

    3130: update solution

--- algorithm_3000-3999/3130-Find All Possible Stable Binary Arrays II/main.cpp
+++ algorithm_3000-3999/3130-Find All Possible Stable Binary Arrays II/main.cpp
@@ -6,32 +6,31 @@ public:
         int dp0[zero + 1][one + 1];
         // dp1[i][j]: the number of stable binary arrays ending with 1 consisting of i zeros and j ones
         int dp1[zero + 1][one + 1];
+
         int presum0[zero + 1][one + 1];
         int presum1[zero + 1][one + 1];
+
+        std::fill(&dp0[0][0], &dp0[0][0] + (zero + 1) * (one + 1), 0);
         for (int i = 0; i <= zero; ++i) {
-            for (int j = 0; j <= one; ++j) {
-                dp0[i][j] = 0;
-                dp1[i][j] = 0;
-            }
-        }
-        for (int i = 1; i <= zero; ++i) {
             dp0[i][0] = (i <= limit ? 1 : 0);
             presum0[i][0] = dp0[i][0];
         }
-        for (int j = 1; j <= one; ++j) {
+        std::fill(&dp1[0][0], &dp1[0][0] + (zero + 1) * (one + 1), 0);
+        for (int j = 0; j <= one; ++j) {
             dp1[0][j] = (j <= limit ? 1 : 0);
             presum1[0][j] = dp1[0][j];
         }
 
         for (int i = 1; i <= zero; ++i) {
             for (int j = 1; j <= one; ++j) {
-
+                // dp0[i][j] = sum(dp1[i - 1][j] + ... + dp1[i - limit][j]);
                 dp0[i][j] = presum1[i - 1][j];
                 if (i - limit - 1 >= 0) {
                     dp0[i][j] -= presum1[i - limit - 1][j];
                     dp0[i][j] = (dp0[i][j] + M) % M;
                 }
-
+                
+                // dp1[i][j] = sum(dp0[i][j - 1] + ... + dp0[i][j - limit]);
                 dp1[i][j] = presum0[i][j - 1];
                 if (j - limit - 1 >= 0) {
                     dp1[i][j] -= presum0[i][j - limit - 1];
