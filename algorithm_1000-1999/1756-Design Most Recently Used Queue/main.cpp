9c3c6a695d7f07b275e73ad153817c7db284cee8
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-24 02:14:36 +0800

    1756: update solution

--- algorithm_1000-1999/1756-Design Most Recently Used Queue/main.cpp
+++ algorithm_1000-1999/1756-Design Most Recently Used Queue/main.cpp
@@ -1,7 +1,7 @@
-class MRUQueue {
+class ByDoublyList {
 public:
-    MRUQueue(int n)
-        : m_bucketSize(std::sqrt(n)) {
+    ByDoublyList(int n)
+        : m_bucketSize(std::ceil(std::sqrt(n))) {
         for (int elem = 1; elem <= n; ++elem) {
             m_data.push_back(elem);
             if ((elem - 1) % m_bucketSize == 0) {
@@ -32,6 +32,51 @@ private:
     list<int> m_data;
     std::vector<list<int>::const_iterator> m_itrs;
 };
+
+class BySinglyList {
+public:
+    BySinglyList(int n)
+        : m_bucketSize(std::ceil(std::sqrt(n)))
+        , m_end(m_data.before_begin()) {
+        auto itr = m_data.before_begin();
+        for (int elem = 1; elem <= n; ++elem) {
+            if ((elem - 1) % m_bucketSize == 0) {
+                m_itrs.push_back(itr);
+            }
+            itr = m_data.insert_after(itr, elem);
+        }
+        m_end = itr;
+    }
+
+    int fetch(int k) {
+        --k;
+        int bucketIdx = k / m_bucketSize;
+        auto itr = m_itrs[bucketIdx];
+        for (int dist = k % m_bucketSize; 0 < dist--;) {
+            itr = std::next(itr);
+        }
+
+        int ret = *std::next(itr);
+
+        m_end = m_data.insert_after(m_end, ret);
+        for (int i = bucketIdx + 1; i < m_itrs.size(); ++i) {
+            m_itrs[i] = std::next(m_itrs[i]);
+        }
+        m_data.erase_after(itr);
+
+        return ret;
+    }
+
+private:
+    const int m_bucketSize;
+    forward_list<int> m_data;
+    std::vector<forward_list<int>::const_iterator> m_itrs;
+    forward_list<int>::const_iterator m_end;
+};
+
+//typedef ByDoublyList MRUQueue;
+typedef BySinglyList MRUQueue;
+
 
 /**
  * Your MRUQueue object will be instantiated and called as such:
