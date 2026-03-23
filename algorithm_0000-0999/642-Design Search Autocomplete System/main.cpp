fcf82c82677dde786d9dd1ca11435571ef4129f9
coolyuyuyu <coolyuyuyu@gmail.com>
2026-03-24 02:14:25 +0800

    642: update solution

--- algorithm_0000-0999/642-Design Search Autocomplete System/main.cpp
+++ algorithm_0000-0999/642-Design Search Autocomplete System/main.cpp
@@ -1,62 +1,27 @@
 class AutocompleteSystem {
-private:
     struct Node {
-        std::array<Node*, 27> childs;
+        array<Node*, 27> nexts;
         int times;
 
+        Node() {
+            nexts.fill(nullptr);
+            times = 0;
+        }
+
         static int toIndex(char c) {
-            return c == ' ' ? 0 : (1 + c - 'a');
+            return (c == ' ' ? 26 : (c - 'a'));
         }
 
         static char toChar(int idx) {
-            return idx == 0 ? ' ' : ('a' + idx - 1);
-        }
-
-        Node() {
-            childs.fill(nullptr);
-            times = 0;
+            return (idx == 26 ? ' ' : ('a' + idx));
         }
     };
-
-    void add(const string& sentence, int times) {
-        Node* node = m_root;
-        for (char c : sentence) {
-            int idx = Node::toIndex(c);
-            if (node->childs[idx] == nullptr) {
-                node->childs[idx] = new Node();
-            }
-            node = node->childs[idx];
-        }
-        node->times = times;
-    }
-
-    template<class T>
-    void traverse(Node* node, string& prefix, int k, T& pq) {
-        assert(node);
-
-        if (node->times) {
-            pq.emplace(0 - node->times, prefix);
-            if (pq.size() > k) {
-                pq.pop();
-            }
-        }
-        for (int idx = 0; idx < node->childs.size(); ++idx) {
-            if (node->childs[idx] == nullptr) { continue; }
-            prefix.push_back(Node::toChar(idx));
-            traverse(node->childs[idx], prefix, k, pq);
-            prefix.pop_back();
-        }
-    }
-
-    Node* m_root;
-
-    Node* m_cur;
-    string m_inputs;
 
 public:
     AutocompleteSystem(vector<string>& sentences, vector<int>& times)
         : m_root(new Node())
         , m_cur(m_root) {
+
         for (int i = 0, n = sentences.size(); i < n; ++i) {
             add(sentences[i], times[i]);
         }
@@ -72,21 +37,59 @@ public:
         }
 
         int idx = Node::toIndex(c);
-        if (m_cur->childs[idx] == nullptr) {
-            m_cur->childs[idx] = new Node();
+        if (m_cur->nexts[idx] == nullptr) {
+            m_cur->nexts[idx] = new Node();
         }
-        m_cur = m_cur->childs[idx];
+        m_cur = m_cur->nexts[idx];
         m_inputs += c;
 
-        priority_queue<pair<int, string>> pq;
-        traverse(m_cur, m_inputs, 3, pq);
+        priority_queue<pair<int, string>> maxPQ;
+        traverse(m_cur, m_inputs, 3, maxPQ);
 
-        vector<string> ret(pq.size());
-        for (int i = ret.size() - 1; !pq.empty(); --i, pq.pop()) {
-            ret[i] = pq.top().second;
+        vector<string> ret(maxPQ.size());
+        for (int i = ret.size() - 1; !maxPQ.empty(); --i, maxPQ.pop()) {
+            ret[i] = maxPQ.top().second;
         }
+
         return ret;
     }
+
+private:
+    void add(const string& sentence, int time) {
+        Node* node = m_root;
+        for (char c : sentence) {
+            int idx = Node::toIndex(c);
+            if (node->nexts[idx] == nullptr) {
+                node->nexts[idx] = new Node();
+            }
+            node = node->nexts[idx];
+        }
+
+        node->times = time;
+    }
+
+    template<class T>
+    void traverse(Node* node, string& prefix, int k, T& pq) {
+        assert(node);
+
+        if (node->times) {
+            pq.emplace(0 - node->times, prefix);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        for (int idx = 0; idx < node->nexts.size(); ++idx) {
+            if (node->nexts[idx] == nullptr) { continue; }
+            prefix.push_back(Node::toChar(idx));
+            traverse(node->nexts[idx], prefix, k, pq);
+            prefix.pop_back();
+        }
+    }
+
+    Node* m_root;
+
+    Node* m_cur;
+    string m_inputs;
 };
 
 /**
