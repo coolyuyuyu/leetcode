23ddbe33a0e82d9efd3f403d13ac3d8921c49c5d
coolyuyuyu <coolyuyuyu@gmail.com>
2026-04-04 11:41:21 +0800

    379: update solution

--- algorithm_0000-0999/379-Design Phone Directory/main.cpp
+++ algorithm_0000-0999/379-Design Phone Directory/main.cpp
@@ -1,74 +1,38 @@
 class PhoneDirectory {
-private:
-    struct Node {
-        Node()
-            : val()
-            , pNext(nullptr) {
-        }
-        Node(int val_, Node* pNext_)
-            : val(val_)
-            , pNext(pNext_) {
-        }
-        int val;
-        Node* pPrev;
-        Node* pNext;
-    };
-
 public:
-    /** Initialize your data structure here
-        @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
     PhoneDirectory(int maxNumbers)
-        : m_pHead(nullptr) {
-        for (int i = 0; i < maxNumbers; ++i) {
-            Node* pNode = new Node(i, m_pHead);
-            m_pHead = pNode;
+        : m_used(maxNumbers, false) {
+        for (int num = maxNumbers; 0 < num--;) {
+            m_nums.push_front(num);
         }
     }
 
-    ~PhoneDirectory() {
-        while (m_pHead) {
-            Node* pDel = m_pHead;
-            m_pHead = pDel->pNext;
-            delete pDel;
-        }
-    }
-
-    /** Provide a number which is not assigned to anyone.
-        @return - Return an available number. Return -1 if none is available. */
     int get() {
-        if (!m_pHead) {
+        if (m_nums.empty()) {
             return -1;
         }
 
-        int number = m_pHead->val;
-        Node* pDel = m_pHead;
-        m_pHead = m_pHead->pNext;
-        delete pDel;
+        int ret = m_nums.front();
+        m_nums.pop_front();
+        m_used[ret] = true;
 
-        return number;
+        return ret;
     }
 
-    /** Check if a number is available or not. */
     bool check(int number) {
-        for (Node* pCur = m_pHead; pCur; pCur = pCur->pNext) {
-            if (pCur->val == number) {
-                return true;
-            }
-        }
-
-        return false;
+        return !m_used[number];
     }
 
-    /** Recycle or release a number. */
     void release(int number) {
         if (!check(number)) {
-            Node* pNode = new Node(number, m_pHead);
-            m_pHead = pNode;
+            m_nums.push_front(number);
+            m_used[number] = false;
         }
     }
 
 private:
-    Node* m_pHead;
+    vector<bool> m_used;
+    forward_list<int> m_nums;
 };
 
 /**
