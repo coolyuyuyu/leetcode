daa90af83db8f3291b702377d6314f73bdba556e
coolyuyuyu <coolyuyuyu@gmail.com>
2026-05-14 01:21:33 +0800

    874: update solution

--- algorithm_0000-0999/874-Walking Robot Simulation/main.cpp
+++ algorithm_0000-0999/874-Walking Robot Simulation/main.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
         set<pair<int, int>> blocks;
-        for (const vector<int>& obstacle : obstacles) {
-            blocks.emplace(obstacle[0], obstacle[1]);
+        for (const auto& obstacle : obstacles) {
+            int x = obstacle[0], y = obstacle[1];
+            blocks.emplace(x, y);
         }
 
-        int x = 0, y = 0, d = 0;
         int ret = 0;
+
+        int curX = 0, curY = 0, d = 0;
         for (int command : commands) {
             switch (command) {
-            case -2:
-                d = (d - 1 + dirs.size()) % dirs.size();
-                break;
-            case -1:
-                d = (d + 1) % dirs.size();
-                break;
-            default:
-                const auto& [dx, dy] = dirs[d];
-                for (int i = 0; i < command; ++i) {
-                    if (blocks.find({x + dx, y + dy}) != blocks.end()) {
-                        break;
+                case -2:
+                    d = (d + dirs.size() - 1) % dirs.size();
+                    break;
+                case -1:
+                    d = (d + 1) % dirs.size();
+                    break;
+                default:
+                    for (int i = 0; i < command; ++i) {
+                        const auto& [dx, dy] = dirs[d];
+                        int nxtX = curX + dx, nxtY = curY + dy;
+                        if (blocks.find({nxtX, nxtY}) != blocks.end()) {
+                            break;
+                        }
+                        curX = nxtX, curY = nxtY;
                     }
-                    x += dx, y += dy;
-                }
-                ret = std::max(ret, x*x + y*y);
-                break;
+                    ret = std::max(ret, curX * curX + curY * curY);
+                    break;
             }
         }
 
-        return ret;
+        return  ret;
     }
 };
