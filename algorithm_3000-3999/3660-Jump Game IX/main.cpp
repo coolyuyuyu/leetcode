aa59a1477bd655f958863b78b2c3ff5777438fb4
coolyuyuyu <coolyuyuyu@gmail.com>
2026-05-14 01:22:32 +0800

    3660: update solution

--- algorithm_3000-3999/3660-Jump Game IX/main.cpp
+++ algorithm_3000-3999/3660-Jump Game IX/main.cpp
@@ -3,23 +3,22 @@ public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> preMax(n), sufMin(n);
-        for (int i = 0; i < n; ++i) {
-            preMax[i] = i > 0 ? std::max(nums[i], preMax[i - 1]) : nums[i];
+        int preMax[n];
+        preMax[0] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            preMax[i] = std::max(preMax[i - 1], nums[i]);
         }
-        for (int i = n - 1; i >= 0; --i) {
-            sufMin[i] = i + 1 < n ? std::min(nums[i], sufMin[i + 1]) : nums[i];
+
+        int sufMin[n];
+        sufMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            sufMin[i] = std::min(sufMin[i + 1], nums[i]);
         }
 
         vector<int> ret(n);
         ret[n - 1] = preMax[n - 1];
         for (int i = n - 2; i >= 0; --i) {
-            if (preMax[i] > sufMin[i + 1]) {
-                ret[i] = ret[i + 1];
-            }
-            else {
-                ret[i] = preMax[i];
-            }
+            ret[i] = preMax[i] > sufMin[i + 1] ? ret[i + 1] : preMax[i];
         }
 
         return ret;
