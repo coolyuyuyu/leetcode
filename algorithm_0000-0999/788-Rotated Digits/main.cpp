6be2f51df8097be8825a8a9931936ed442aa9305
coolyuyuyu <coolyuyuyu@gmail.com>
2026-05-14 01:21:13 +0800

    788: update solution

--- algorithm_0000-0999/788-Rotated Digits/main.cpp
+++ algorithm_0000-0999/788-Rotated Digits/main.cpp
@@ -1,41 +1,79 @@
 class Solution {
 public:
-    bool checkGood(int num) {
-        bool different = false;
-        for (int n = num; n > 0; n /= 10) {
-            int digit = n % 10;
-            switch (digit) {
-            case 3:
-            case 4:
-            case 7:
-                return false;
-            case 0:
-            case 1:
-            case 8:
-                break;
-            case 2:
-            case 5:
-            case 6:
-            case 9:
-                different = true;
-                break;
-            default:
-                assert(false);
+    int byBruteForce(int n) {
+        bool same = true;
+        std::function<bool(int)> checkValid = [](int num) {
+            bool different = false;
+            for (; num; num /= 10) {
+                int d = num % 10;
+                switch (d) {
+                    case 0:
+                    case 1:
+                    case 8:
+                        break;
+                    case 2:
+                    case 5:
+                    case 6:
+                    case 9:
+                        different = true;
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            return different;
+        };
+
+        int ret = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (checkValid(i)) {
+                ++ret;
             }
         }
 
-        return different;
-
+        return ret;
     }
 
-    int rotatedDigits(int N) {
-        int count = 0;
-        for (int i = 1; i <= N; ++i) {
-            if (checkGood(i)) {
-                ++count;
-            }
+    int byDigitCount(int n) {
+        vector<int> digits;
+        for (; n; n /= 10) {
+            digits.push_back(n % 10);
         }
+        std::reverse(digits.begin(), digits.end());
 
-        return count;
+        std::function<int(int, bool, bool)> dfs = [&](int i, bool tight, bool different) {
+            if (i >= digits.size()) {
+                return different ? 1 : 0;
+            }
+
+            int ret = 0;
+            for (int d = 0, limit = (tight ? digits[i] : 9); d <= limit; ++d) {
+                switch (d) {
+                    case 0:
+                    case 1:
+                    case 8:
+                        ret += dfs(i + 1, tight && d == limit, different);
+                        break;
+                    case 2:
+                    case 5:
+                    case 6:
+                    case 9:
+                        ret += dfs(i + 1, tight && d == limit, true);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            return ret;
+        };
+
+        return dfs(0, true, false);
+    }
+
+    int rotatedDigits(int n) {
+        //return byBruteForce(n);
+        return byDigitCount(n);
     }
 };
