ddfd5c4c82c8b93654b797dc3defd00eac230535
coolyuyuyu <coolyuyuyu@gmail.com>
2026-05-14 01:22:44 +0800

    3761: update solution

--- algorithm_3000-3999/3761-Minimum Absolute Distance Between Mirror Pairs/main.cpp
+++ algorithm_3000-3999/3761-Minimum Absolute Distance Between Mirror Pairs/main.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int minMirrorPairDistance(vector<int>& nums) {
-        std::function<int(int)> reverseNum = [](int num) {
+        std::function<int(int)> reverse = [](int num) {
             int ret = 0;
             for (; num; num /= 10) {
-                ret *= 10;
-                ret += num % 10;
+                ret = ret * 10 + num % 10;
             }
             return ret;
         };
 
         int ret = INT_MAX;
-
         unordered_map<int, int> num2idx;
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            int revNum = reverseNum(nums[i]);
-            auto itr = num2idx.find(revNum);
+        for (int i = nums.size(); 0 < i--;) {
+            auto itr = num2idx.find(reverse(nums[i]));
             if (itr != num2idx.end()) {
-                ret = std::min(ret, itr->second - i);
+                ret = std::min(ret, (itr->second - i));
             }
             num2idx[nums[i]] = i;
         }
 
-        return ret != INT_MAX ? ret : -1;
+        return ret < INT_MAX ? ret : -1;
     }
 };
