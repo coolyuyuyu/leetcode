e0f76763dda44ffc176c23b11ab3dee2c39888bf
coolyuyuyu <coolyuyuyu@gmail.com>
2026-05-14 01:22:07 +0800

    2553: update solution

--- algorithm_2000-2999/2553-Separate the Digits in an Array/main.cpp
+++ algorithm_2000-2999/2553-Separate the Digits in an Array/main.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> ret;
-        for (int num : nums) {
-            string s = std::to_string(num);
-            for (char c : s) {
-                ret.push_back(c - '0');
+        for (int i = nums.size(); 0 < i--;) {
+            for (int num = nums[i]; num; num /= 10) {
+                ret.push_back(num % 10);
             }
         }
+        std::reverse(ret.begin(), ret.end());
 
         return ret;
     }
