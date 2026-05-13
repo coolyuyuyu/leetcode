edc508328a8f3e838f254d8daa71665234d34579
coolyuyuyu <coolyuyuyu@gmail.com>
2026-05-14 01:22:26 +0800

    3488: update solution

--- algorithm_3000-3999/3488-Closest Equal Element Queries/main.cpp
+++ algorithm_3000-3999/3488-Closest Equal Element Queries/main.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        int n = nums.size(), m = queries.size();
+        int n = nums.size();
 
-        unordered_map<int, vector<int>> num2indexes;
-        for (int i = 0; i < nums.size(); ++i) {
-            num2indexes[nums[i]].push_back(i);
+        unordered_map<int, vector<int>> num2idxs;
+        for (int i = 0; i < n; ++i) {
+            num2idxs[nums[i]].push_back(i);
         }
 
         std::function<int(int, int)> computeDist = [&](int a, int b) {
@@ -13,20 +13,20 @@ public:
             return std::min(b - a, n - b + a);
         };
 
-        vector<int> ret(m);
-        for (int i = 0; i < m; ++i) {
-            const vector<int>& indexes = num2indexes[nums[queries[i]]];
-            if (indexes.size() == 1) {
-                ret[i] = -1;
+        vector<int> ret(queries.size(), -1);
+        for (int i = 0; i < queries.size(); ++i) {
+            int cur = queries[i];
+            vector<int>& idxs = num2idxs[nums[cur]];
+            if (idxs.size() == 1) {
+                continue;
             }
-            else {
-                auto itr = std::lower_bound(indexes.begin(), indexes.end(), queries[i]);
-                int cur = queries[i];
-                int nxt = (cur == indexes.back() ? indexes.front() : *std::next(itr));
-                int pre = (cur == indexes.front() ? indexes.back() : *std::prev(itr));
-                ret[i] = std::min(computeDist(pre, cur), computeDist(cur, nxt));
-            }
+
+            auto itr = std::lower_bound(idxs.begin(), idxs.end(), cur);
+            int pre = cur != idxs.front() ? *std::prev(itr) : idxs.back();
+            int nxt = cur != idxs.back() ? *std::next(itr) : idxs.front();
+            ret[i] = std::min(computeDist(pre, cur), computeDist(cur, nxt));
         }
+
         return ret;
     }
 };
