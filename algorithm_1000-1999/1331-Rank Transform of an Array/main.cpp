a994c91b0a51a2fac63fd52e2f91b65ad4b88e09
coolyuyuyu <coolyuyuyu@gmail.com>
2026-07-27 14:28:37 +0800

    1331: update solution

--- algorithm_1000-1999/1331-Rank Transform of an Array/main.cpp
+++ algorithm_1000-1999/1331-Rank Transform of an Array/main.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size();
-        vector<pair<int, int>> pairs(n); // <num, idx>
-        for (int i = 0; i < n; ++i) {
-            pairs[i] = {arr[i], i};
+        if (n == 0) {
+            return {};
         }
-        std::sort(pairs.begin(), pairs.end());
+
+        int idxs[n];
+        std::iota(idxs, idxs + n, 0);
+        std::sort(idxs, idxs + n, [&](int idx1, int idx2){ return arr[idx1] < arr[idx2]; });
 
         vector<int> ret(n);
-        for (int i = 0, rank = 1; i < n; ++i) {
-            if (i > 0 && pairs[i - 1].first < pairs[i].first) {
+        for (int i = 0, pre = INT_MIN, rank = 0; i < n; ++i) {
+            int idx = idxs[i];
+            if (arr[idx] > pre) {
                 ++rank;
             }
-            ret[pairs[i].second] = rank;
+            ret[idx] = rank;
+
+            pre = arr[idx];
         }
 
         return ret;
