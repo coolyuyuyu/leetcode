f9b657a5c075ddc8fb67acbc0e74571a9d1fd4e8
coolyuyuyu <coolyuyuyu@gmail.com>
2026-07-27 14:38:06 +0800

    3286: update solution

--- algorithm_3000-3999/3286-Find a Safe Walk Through a Grid/main.cpp
+++ algorithm_3000-3999/3286-Find a Safe Walk Through a Grid/main.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
         int healths[m][n];
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                healths[r][c] = 0;
-            }
-        }
+        std::fill(&healths[0][0], &healths[0][0] + m * n, 0);
 
-        priority_queue<tuple<int, int, int>> pq; // <health, x, y>
+        priority_queue<tuple<int, int, int>> pq;
         pq.emplace(health, 0, 0);
         while (!pq.empty()) {
-            auto [hp, r, c] = pq.top();
+            auto [h, r, c] = pq.top();
             pq.pop();
 
-            hp -= (grid[r][c] == 1 ? 1 : 0);
-            if (hp <= 0) { continue; }
             if (healths[r][c] > 0) { continue; }
-            healths[r][c] = hp;
-            if (r == m - 1 && c == n - 1) { break; }
+            h -= (grid[r][c] == 1);
+            if (h == 0) { continue; }
+            healths[r][c] = h;
+
+            if (r == m - 1 && c == n - 1) {
+                return true;;
+            }
 
             for (const auto& [dr, dc] : dirs) {
                 int x = r + dr, y = c + dc;
                 if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
                 if (healths[x][y] > 0) { continue; }
-
-                pq.emplace(hp, x, y);
+                pq.emplace(h, x, y);
             }
         }
 
-        return healths[m - 1][n - 1] > 0;
+        return false;
     }
 };
