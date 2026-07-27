bff136f90f99507346f7e26404c6341d2c4cd855
coolyuyuyu <coolyuyuyu@gmail.com>
2026-07-27 14:32:52 +0800

    1732: update solution

--- algorithm_1000-1999/1732-Find the Highest Altitude/main.cpp
+++ algorithm_1000-1999/1732-Find the Highest Altitude/main.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int ret = 0, altitude = 0;
-        for (int diff : gain) {
-            altitude += diff;
+        int ret = 0;
+        for (int i = 0, altitude = 0; i < gain.size(); ++i) {
+            altitude += gain[i];
             ret = std::max(ret, altitude);
         }
 
