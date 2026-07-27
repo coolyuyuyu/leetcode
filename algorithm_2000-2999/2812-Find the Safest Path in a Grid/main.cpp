09807e4664ab819cc96f02cf24a0b9dc5b936fc0
coolyuyuyu <coolyuyuyu@gmail.com>
2026-07-27 14:37:12 +0800

    2812: update solution

--- algorithm_2000-2999/2812-Find the Safest Path in a Grid/main.cpp
+++ algorithm_2000-2999/2812-Find the Safest Path in a Grid/main.cpp
@@ -3,10 +3,10 @@ public:
     vector<pair<int, int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 
     int maximumSafenessFactor(vector<vector<int>>& grid) {
-        int n = grid.size();
+        int m = grid.size(), n = grid.empty() ? 0 : grid[0].size();
 
         queue<pair<int, int>> q;
-        for (int r = 0; r < n; ++r) {
+        for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
                 if (grid[r][c] == 1) {
                     grid[r][c] = 0;
@@ -17,54 +17,48 @@ public:
                 }
             }
         }
-        while (!q.empty()) {
-            auto [r, c] = q.front();
-            q.pop();
 
-            for (const auto& [dr, dc] : dirs) {
-                int x = r + dr, y = c + dc;
-                if (x < 0 || n <= x || y < 0 || n <= y) { continue; }
-                if (grid[x][y] <= grid[r][c] + 1) { continue; }
-                grid[x][y] = grid[r][c] + 1;
-                q.emplace(x, y);
+        while (!q.empty()) {
+            for (int i = q.size(); 0 < i--;) {
+                auto [r, c] = q.front();
+                q.pop();
+
+                for (const auto& [dr, dc] : dirs) {
+                    int x = r + dr, y = c + dc;
+                    if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
+                    if (grid[x][y] < INT_MAX) { continue; }
+                    grid[x][y] = grid[r][c] + 1;
+                    q.emplace(x, y);
+                }
             }
         }
 
         std::function<bool(int)> checkConnected = [&](int safe) {
             if (grid[0][0] < safe || grid[n - 1][n - 1] < safe) { return false; }
 
-            bool visited[n][n];
-            for (int r = 0; r < n; ++r) {
-                for (int c = 0; c < n; ++c) {
-                    visited[r][c] = false;
-                }
-            }
-
-            queue<pair<int, int>> q;
-            visited[0][0] = true;
-            q.emplace(0, 0);
-            while (!q.empty()) {
+            bool visited[m][n];
+            std::fill(&visited[0][0], &visited[0][0] + m * n, false);
+            for (queue<pair<int, int>> q({{0, 0}}); !q.empty();) {
                 auto [r, c] = q.front();
                 q.pop();
 
-                if (r + 1 == n && c + 1 == n) {
+                if (r + 1 == m && c + 1 == n) {
                     return true;
                 }
 
                 for (const auto& [dr, dc] : dirs) {
                     int x = r + dr, y = c + dc;
-                    if (x < 0 || n <= x || y < 0 || n <= y) { continue; }
-                    if (visited[x][y]) { continue; }
+                    if (x < 0 || x >= m || y < 0 || y >= n) { continue; }
                     if (grid[x][y] < safe) { continue; }
+                    if (visited[x][y]) { continue; }
                     visited[x][y] = true;
                     q.emplace(x, y);
                 }
             }
-
             return false;
         };
 
-        int lo = 0, hi = 2 * n - 1;
+        int lo = 0, hi = m + n - 1;
         while (lo < hi) {
             int mid = hi - (hi - lo) / 2;
             if (checkConnected(mid)) {
